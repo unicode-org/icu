adf7bb00ce6c4cc04a7d777b5a7d0ea0b12dfc31
Markus Scherer <markus.icu@gmail.com>
1999-10-12 21:50:30 +0000

    [ICU-14] Windows: create a batch file to make the /icu/data files X-Commit-URL: https://ssl.icu-project.org/trac/changeset/46

--- icu4c/source/tools/makeconv/mkcnvfle.bat
+++ icu4c/source/tools/makeconv/mkcnvfle.bat
@@ -5,4 +5,4 @@ rem it's purpose is to build all the converter binary files (".cnv") from the
 rem existing *".ucm" files on the system
 rem this  batch file should be passed a string either "Release" or "Debug"
 rem so the script will know where "makeconv.exe" is
-for %%i in (\icu\data\*.ucm) do \icu\source\tools\makeconv\%1\makeconv %%i
+for %%i in (%2\icu\data\*.ucm) do %2\icu\source\tools\makeconv\%1\makeconv %%i
