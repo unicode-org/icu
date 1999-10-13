c51a1aeaee6f0c347374bc1c38bf2e977b4141cc
Markus Scherer <markus.icu@gmail.com>
1999-10-13 01:10:24 +0000

    windows: genrb puts .res files into the current directory
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/15
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@47 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/tools/makedata.bat
+++ source/tools/makedata.bat
@@ -9,16 +9,19 @@ if "%1"=="" goto :error
 if "%ICU_DATA%"=="" set ICU_DATA=%1\icu\data\
 set toolversion=Debug
 
-cd %1\icu\data
-
 rem create conversion tables
-call %1\icu\source\tools\makeconv\mkcnvfle %toolversion% %1
+cd makeconv
+call mkcnvfle %toolversion% %1
 
 rem create locale resource bundles
-call %1\icu\source\tools\genrb\genrb %1\icu\source\tools\genrb\%toolversion% %1
+cd ..\genrb
+call genrb %toolversion% %1
 
 rem create binary collation tables
-%1\icu\source\tools\gencol\%toolversion%\gencol
+cd ..\gencol
+%toolversion%\gencol
+
+cd ..
 
 goto :end
 
@@ -26,6 +29,6 @@ goto :end
 echo call makedata with the absolute path to the icu directory
 echo for example, if the full path is d:\mytools\icu then call
 echo makedata d:\mytools
-echo the current directory will be changed to d:\mytools\icu\data
+echo the current directory must be the icu\source\tools directory with makedata.bat
 
 :end
