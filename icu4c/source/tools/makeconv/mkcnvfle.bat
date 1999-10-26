9b2c981241b2ae1b9b3e01ea82d8836b99019109
Steven R. Loomis <srl@icu-project.org>
1999-10-26 01:14:23 +0000

    ICU-38 Win9x: hard-code compilation of some converter files that have long names
    X-SVN-Rev: 119

--- icu4c/source/tools/makeconv/mkcnvfle.bat
+++ icu4c/source/tools/makeconv/mkcnvfle.bat
@@ -5,4 +5,4 @@ rem it's purpose is to build all the converter binary files (".cnv") from the
 rem existing *".ucm" files on the system
 rem this  batch file should be passed a string either "Release" or "Debug"
 rem so the script will know where "makeconv.exe" is
-for %%i in (%2\icu\data\*.ucm) do %2\icu\source\tools\makeconv\%1\makeconv %%i
+for %%i in (%2\icu\data\*.ucm %2\icu\data\ibm-16684.ucm %2\icu\data\ibm-16804.ucm %2\icu\data\ibm-17248.ucm %2\icu\data\ibm-21427.ucm %2\icu\data\ibm-12712.ucm) do %2\icu\source\tools\makeconv\%1\makeconv %%i
