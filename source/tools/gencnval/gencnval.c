2b9ee308e3168d8e4b4a3325a7e628155179dd0a
Markus Scherer <markus.icu@gmail.com>
1999-11-23 02:17:43 +0000

    create the binary file for convrtrs.txt
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/165
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@233 0a8b766c-62c9-45f9-954d-7f2943c9ead0

---
+++ source/tools/gencnval/gencnval.c
@@ -0,0 +1,336 @@
+/*
+*******************************************************************************
+*                                                                             *
+* COPYRIGHT:                                                                  *
+*   (C) Copyright International Business Machines Corporation, 1999           *
+*   Licensed Material - Program-Property of IBM - All Rights Reserved.        *
+*   US Government Users Restricted Rights - Use, duplication, or disclosure   *
+*   restricted by GSA ADP Schedule Contract with IBM Corp.                    *
+*                                                                             *
+*******************************************************************************
+*   file name:  gencnval.c
+*   encoding:   US-ASCII
+*   tab size:   8 (not used)
+*   indentation:4
+*
+*   created on: 1999nov05
+*   created by: Markus W. Scherer
+*
+*   This program reads convrtrs.txt and writes a memory-mappable
+*   converter name alias table to cnvalias.dat .
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "utypes.h"
+#include "cmemory.h"
+#include "cstring.h"
+#include "filestrm.h"
+#include "unewdata.h"
+
+#define STRING_STORE_SIZE 100000
+#define MAX_ALIAS_COUNT 2000
+
+#define DATA_NAME "cnvalias"
+#define DATA_TYPE "dat"
+
+#define DATA_COPYRIGHT "\n" \
+    "*******************************************************************************\n" \
+    "* COPYRIGHT:                                                                  *\n" \
+    "*   (C) Copyright International Business Machines Corporation, 1999           *\n" \
+    "*   Licensed Material - Program-Property of IBM - All Rights Reserved.        *\n" \
+    "*   US Government Users Restricted Rights - Use, duplication, or disclosure   *\n" \
+    "*   restricted by GSA ADP Schedule Contract with IBM Corp.                    *\n" \
+    "*******************************************************************************\n"
+
+/* UDataInfo cf. udata.h */
+static const UDataInfo dataInfo={
+    sizeof(UDataInfo),
+    0,
+
+    U_IS_BIG_ENDIAN,
+    U_CHARSET_FAMILY,
+    sizeof(UChar),
+    0,
+
+    0x43, 0x76, 0x41, 0x6c,     /* dataFormat="CvAl" */
+    1, 0, 0, 0,                 /* formatVersion */
+    1, 3, 1, 0                  /* dataVersion */
+};
+
+static char stringStore[STRING_STORE_SIZE];
+static uint32_t stringTop=0;
+
+typedef struct {
+    const char *alias, *converter;
+} Alias;
+
+static Alias aliases[MAX_ALIAS_COUNT];
+static uint16_t aliasCount=0;
+
+typedef struct {
+    const char *converter;
+    uint16_t aliasCount;
+} Converter;
+
+static Converter converters[MAX_ALIAS_COUNT];
+static uint16_t converterCount=0;
+
+/* prototypes --------------------------------------------------------------- */
+
+static void
+parseLine(const char *line);
+
+static void
+addAlias(const char *alias, const char *converter);
+
+static Converter *
+addConverter(const char *converter);
+
+static char *
+allocString(uint32_t length);
+
+static int
+compareAliases(const void *file1, const void *file2);
+
+static int
+compareConverters(const void *converter1, const void *converter2);
+
+/* -------------------------------------------------------------------------- */
+
+extern int
+main(int argc, char *argv[]) {
+    char line[512];
+    const char *path, *arg;
+    FileStream *in;
+    UNewDataMemory *out;
+    char *s;
+    UErrorCode errorCode=U_ZERO_ERROR;
+    int i;
+    uint16_t stringOffset;
+    bool_t haveCopyright=TRUE;
+
+    fprintf(stderr,
+        "usage: %s [-c[+|-]]\n"
+        "\tread convrtrs.txt and create " DATA_NAME "." DATA_TYPE "\n"
+        "\t\t-c[+|-]  do (not) include a copyright notice\n",
+        argv[0]);
+
+    for(i=1; i<argc; ++i) {
+        arg=argv[i];
+        if(arg[0]=='-') {
+            switch(arg[1]) {
+            case 'c':
+                haveCopyright= arg[2]=='+';
+                break;
+            default:
+                break;
+            }
+        }
+    }
+
+    path=u_getDataDirectory();
+    if(path!=NULL) {
+        icu_strcpy(line, path);
+        icu_strcat(line, "convrtrs.txt");
+        path=line;
+    } else {
+        path="convrtrs.txt";
+    }
+    in=T_FileStream_open(path, "r");
+    if(in==NULL) {
+        fprintf(stderr, "gencnval: unable to open input file convrtrs.txt\n");
+        exit(U_FILE_ACCESS_ERROR);
+    }
+
+    /* read the list of aliases */
+    while(T_FileStream_readLine(in, line, sizeof(line))!=NULL) {
+        /* remove trailing newline characters */
+        s=line;
+        while(*s!=0) {
+            if(*s=='\r' || *s=='\n') {
+                *s=0;
+                break;
+            }
+            ++s;
+        }
+
+        parseLine(line);
+    }
+
+    T_FileStream_close(in);
+
+    /* sort the aliases and the converters */
+    qsort(aliases, aliasCount, sizeof(Alias), compareAliases);
+    qsort(converters, converterCount, sizeof(Converter), compareConverters);
+
+    /* create the output file */
+    out=udata_create(DATA_TYPE, DATA_NAME, &dataInfo,
+                     haveCopyright ? DATA_COPYRIGHT : NULL, &errorCode);
+    if(U_FAILURE(errorCode)) {
+        fprintf(stderr, "gencnval: unable to open output file - error %s\n", errorName(errorCode));
+        exit(errorCode);
+    }
+
+    /* determine the length of tables for the data offset of the strings */
+    stringOffset=2+4*aliasCount+2+4*converterCount;
+
+    /* write the table of aliases */
+    udata_write16(out, aliasCount);
+    for(i=0; i<aliasCount; ++i) {
+        udata_write16(out, (uint16_t)(stringOffset+(aliases[i].alias-stringStore)));
+        udata_write16(out, (uint16_t)(stringOffset+(aliases[i].converter-stringStore)));
+    }
+
+    /* write the table of converters */
+    udata_write16(out, converterCount);
+    for(i=0; i<converterCount; ++i) {
+        udata_write16(out, (uint16_t)(stringOffset+(converters[i].converter-stringStore)));
+        udata_write16(out, converters[i].aliasCount);
+    }
+
+    /* write the strings */
+    udata_writeString(out, stringStore, stringTop);
+
+    /* finish */
+    udata_finish(out, &errorCode);
+    if(U_FAILURE(errorCode)) {
+        fprintf(stderr, "gencnval: error finishing output file - %s\n", errorName(errorCode));
+        exit(errorCode);
+    }
+
+    return 0;
+}
+
+static void
+parseLine(const char *line) {
+    Converter *cnv;
+    uint16_t pos=0, start, limit, length;
+    char *converter, *alias, *s;
+
+    /* skip leading white space */
+    while(line[pos]!=0 && isspace((unsigned char)line[pos])) {
+        ++pos;
+    }
+
+    /* is there only a comment on this line? */
+    if(line[pos]==0 || line[pos]=='#') {
+        return;
+    }
+
+    /* get the converter name */
+    start=pos;
+    while(line[pos]!=0 && line[pos]!='#' && !isspace((unsigned char)line[pos])) {
+        ++pos;
+    }
+    limit=pos;
+
+    /* store the converter name */
+    length=limit-start;
+    converter=allocString(length+1);
+    icu_strncpy(converter, line+start, length);
+    converter[length]=0;
+
+    /* add the converter to the converter table */
+    cnv=addConverter(converter);
+
+    /* add a lowercase version of the converter name as the first alias */
+    alias=s=allocString(length+1);
+    while(length>0) {
+        *s++=icu_tolower(line[start++]);
+        --length;
+    }
+    *s=0;
+
+    /* add the alias/converter pair to the alias table */
+    addAlias(alias, converter);
+
+    /* get all the real aliases */
+    for(;;) {
+        /* skip white space */
+        while(line[pos]!=0 && isspace((unsigned char)line[pos])) {
+            ++pos;
+        }
+
+        /* is there no more alias name on this line? */
+        if(line[pos]==0 || line[pos]=='#') {
+            break;
+        }
+
+        /* get an alias name */
+        start=pos;
+        while(line[pos]!=0 && line[pos]!='#' && !isspace((unsigned char)line[pos])) {
+            ++pos;
+        }
+        limit=pos;
+
+        /* store the alias name in lowercase */
+        length=limit-start;
+        alias=s=allocString(length+1);
+        while(length>0) {
+            *s++=icu_tolower(line[start++]);
+            --length;
+        }
+        *s=0;
+
+        /* add the alias/converter pair to the alias table */
+        addAlias(alias, converter);
+
+        /* count it for the converter */
+        ++cnv->aliasCount;
+
+    }
+}
+
+static void
+addAlias(const char *alias, const char *converter) {
+    if(aliasCount==MAX_ALIAS_COUNT) {
+        fprintf(stderr, "gencnval: too many aliases\n");
+        exit(U_BUFFER_OVERFLOW_ERROR);
+    }
+
+    aliases[aliasCount].alias=alias;
+    aliases[aliasCount].converter=converter;
+
+    ++aliasCount;
+}
+
+static Converter *
+addConverter(const char *converter) {
+    if(converterCount==MAX_ALIAS_COUNT) {
+        fprintf(stderr, "gencnval: too many converters\n");
+        exit(U_BUFFER_OVERFLOW_ERROR);
+    }
+
+    converters[converterCount].converter=converter;
+    converters[converterCount].aliasCount=0;
+
+    ++converterCount;
+
+    return converters+converterCount-1;
+}
+
+static char *
+allocString(uint32_t length) {
+    uint32_t top=stringTop+length;
+    char *p;
+
+    if(top>STRING_STORE_SIZE) {
+        fprintf(stderr, "gencnval: out of memory\n");
+        exit(U_MEMORY_ALLOCATION_ERROR);
+    }
+    p=stringStore+stringTop;
+    stringTop=top;
+    return p;
+}
+
+static int
+compareAliases(const void *alias1, const void *alias2) {
+    return icu_strcmp(((Alias *)alias1)->alias, ((Alias *)alias2)->alias);
+}
+
+static int
+compareConverters(const void *converter1, const void *converter2) {
+    return icu_strcmp(((Converter *)converter1)->converter, ((Converter *)converter2)->converter);
+}
