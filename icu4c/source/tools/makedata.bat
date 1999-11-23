87b22a433bc39f91a1318d7d614cb5da6ef3d5f4
Markus Scherer <markus.icu@gmail.com>
1999-11-23 02:17:43 +0000

    [ICU-165] create the binary file for convrtrs.txt X-Commit-URL: https://ssl.icu-project.org/trac/changeset/233

--- icu4c/source/tools/makedata.bat
+++ icu4c/source/tools/makedata.bat
@@ -30,16 +30,22 @@ echo create unames.dat and unames_dat.c from UnicodeData.txt
 gennames\%toolversion%\gennames -v- -c- "%ICU_DATA%UnicodeData-3.0.0.txt"
 genccode\%toolversion%\genccode "%ICU_DATA%unames.dat"
 
+echo create cnvalias.dat and cnvalias_dat.c from convrtrs.txt
+gencnval\%toolversion%\gencnval -c-
+genccode\%toolversion%\genccode "%ICU_DATA%cnvalias.dat"
+
 echo create the data DLL
-cl "/I..\..\include" /GD /c "%ICU_DATA%unames_dat.c"
+cl "/I..\..\include" /GD /c "%ICU_DATA%unames_dat.c" "%ICU_DATA%cnvalias_dat.c"
 echo "/out:%ICU_DATA%icudata.dll">mkdll.tmp
 echo unames_dat.obj>>mkdll.tmp
+echo cnvalias_dat.obj>>mkdll.tmp
 type mkdll.lk>>mkdll.tmp
 link @mkdll.tmp
 
 echo create the common, memory-mappable file
 del "%ICU_DATA%icudata.dat"
 echo %ICU_DATA%unames.dat>mkmap.tmp
+echo %ICU_DATA%cnvalias.dat>mkmap.tmp
 gencmn\%toolversion%\gencmn 1000000 mkmap.tmp
 
 goto :end
