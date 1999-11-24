5d5f3fa5e9f8a383b448920cfbc1d3577c7f378b
Markus Scherer <markus.icu@gmail.com>
1999-11-24 00:39:43 +0000

    add some more documentation comments
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/157
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@255 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/ucnv_io.h
+++ source/common/ucnv_io.h
@@ -20,21 +20,53 @@
 
 #include "utypes.h"
 
+/**
+ * Map a converter alias name to a canonical converter name.
+ * The alias is searched for case-insensitively, the converter name
+ * is returned in mixed-case.
+ * Returns NULL if the alias is not found.
+ */
 U_CFUNC const char *
 ucnv_io_getConverterName(const char *alias, UErrorCode *pErrorCode);
 
+/**
+ * Search case-insensitively for a converter alias and set aliases to
+ * a pointer to the list of aliases for the actual converter.
+ * The first "alias" is the canonical converter name.
+ * The aliases are stored consecutively, in mixed case, each NUL-terminated.
+ * There are as many strings in this list as the return value specifies.
+ * Returns the number of aliases including the canonical converter name,
+ * or 0 if the alias is not found.
+ */
 U_CFUNC uint16_t
 ucnv_io_getAliases(const char *alias, const char **aliases, UErrorCode *pErrorCode);
 
+/**
+ * Search case-insensitively for a converter alias and return
+ * the (index)th alias.
+ * Returns NULL if the alias is not found.
+ */
 U_CFUNC const char *
 ucnv_io_getAlias(const char *alias, uint16_t index, UErrorCode *pErrorCode);
 
+/**
+ * Return the number of all aliases (and converter names).
+ */
 U_CFUNC uint16_t
 ucnv_io_countAvailableAliases(UErrorCode *pErrorCode);
 
+/**
+ * Return the (index)th alias or converter name in mixed case, or NULL
+ * if there is none (typically, if the data cannot be loaded).
+ * 0<=index<=ucnv_io_countAvailableAliases().
+ */
 U_CFUNC const char *
 ucnv_io_getAvailableAlias(uint16_t index, UErrorCode *pErrorCode);
 
+/**
+ * Fill an array const char *aliases[ucnv_io_countAvailableAliases()]
+ * with pointers to all aliases and converter names in mixed-case.
+ */
 U_CFUNC void
 ucnv_io_fillAvailableAliases(const char **aliases, UErrorCode *pErrorCode);
 
