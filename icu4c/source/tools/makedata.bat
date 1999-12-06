71db8fdc01bbe1f3cfdcce2c6ab75eded6d3170d
Alan Liu <alansliu@gmail.com>
1999-12-06 06:54:34 +0000

    [ICU-157] use mkhelper to read makeconv/ucmfiles.mk and ucmlocal.mk and build those converters and add them to icudata X-Commit-URL: https://ssl.icu-project.org/trac/changeset/319

--- icu4c/source/tools/makedata.bat
+++ icu4c/source/tools/makedata.bat
@@ -12,9 +12,17 @@ rem toolversion: Debug or Release
 set toolversion=Release
 if not "%2"=="" set toolversion=%2
 
+rem setup mkhelper to read ucmfiles.mk and ucmlocal.mk
+set conv=mkhelper\%toolversion%\mkhelper -n UCM_SOURCE -n UCM_SOURCE_LOCAL makeconv\ucmfiles.mk makeconv\ucmlocal.mk
+
 echo create conversion tables
+rem delete preexisting files to prevent upper/lowercase file name problems
+del "%ICU_DATA%*.cnv"
+del "%ICU_DATA%*_cnv.c"
+del *_cnv.obj
+%conv% -p "$toolversion$\makeconv \"$ICU_DATA$" -s "\"">makeconv\mkcnvtmp.bat
 cd makeconv
-call mkcnvfle %toolversion% %1
+call mkcnvtmp.bat
 
 echo create locale resource bundles
 cd ..\genrb
@@ -38,12 +46,23 @@ echo create tz.dat from tz.txt
 gentz\%toolversion%\gentz -c- gentz\tz.txt
 genccode\%toolversion%\genccode "%ICU_DATA%tz.dat"
 
+echo create the converters
+%conv% -p "genccode\$toolversion$\genccode \"$ICU_DATA$" -s "\"" -old ".ucm" -new ".cnv">mkconv.bat
+call mkconv.bat
+
 echo create the data DLL
-cl "/I..\..\include" /GD /c "%ICU_DATA%unames_dat.c" "%ICU_DATA%cnvalias_dat.c" "%ICU_DATA%tz_dat.c"
+type mkobj.cl>mkobj.tmp
+echo "%ICU_DATA%unames_dat.c">>mkobj.tmp
+echo "%ICU_DATA%cnvalias_dat.c">>mkobj.tmp
+echo "%ICU_DATA%tz_dat.c">>mkobj.tmp
+%conv% -p "\"$ICU_DATA$" -s "\"" -old ".ucm" -new "_cnv.c">>mkobj.tmp
+cl @mkobj.tmp
+
 echo "/out:%ICU_DATA%icudata.dll">mkdll.tmp
 echo unames_dat.obj>>mkdll.tmp
 echo cnvalias_dat.obj>>mkdll.tmp
 echo tz_dat.obj>>mkdll.tmp
+%conv% -old ".ucm" -new "_cnv.obj">>mkdll.tmp
 type mkdll.lk>>mkdll.tmp
 link @mkdll.tmp
 
@@ -52,6 +71,7 @@ del "%ICU_DATA%icudata.dat"
 echo %ICU_DATA%unames.dat>mkmap.tmp
 echo %ICU_DATA%cnvalias.dat>>mkmap.tmp
 echo %ICU_DATA%tz.dat>>mkmap.tmp
+%conv% -p "$ICU_DATA$" -old ".ucm" -new ".cnv">>mkmap.tmp
 gencmn\%toolversion%\gencmn 1000000 mkmap.tmp
 
 
