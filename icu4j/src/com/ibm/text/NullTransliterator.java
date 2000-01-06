b0fa4931cd3de609b138da240e837b1129d1a914
Alan Liu <alansliu@gmail.com>
2000-01-06 17:38:25 +0000

    Add null transliterator X-Commit-URL: https://ssl.icu-project.org/trac/changeset/491

---
+++ icu4j/src/com/ibm/text/NullTransliterator.java
@@ -0,0 +1,43 @@
+package com.ibm.text;
+import java.util.*;
+
+/**
+ * A transliterator that leaves text unchanged.
+ */
+public class NullTransliterator extends Transliterator {
+    private static final String COPYRIGHT =
+        "\u00A9 IBM Corporation 2000. All rights reserved.";
+
+    /**
+     * Package accessible ID for this transliterator.
+     */
+    static String _ID = "Null";
+
+    /**
+     * Constructs a transliterator.
+     */
+    public NullTransliterator() {
+        super(_ID, null);
+    }
+
+    /**
+     * Transliterates a segment of a string.  <code>Transliterator</code> API.
+     * @param text the string to be transliterated
+     * @param start the beginning index, inclusive; <code>0 <= start
+     * <= limit</code>.
+     * @param limit the ending index, exclusive; <code>start <= limit
+     * <= text.length()</code>.
+     * @return the new limit index
+     */
+    public int transliterate(Replaceable text, int start, int limit) {
+        return limit;
+    }
+
+    /**
+     * Implements {@link Transliterator#handleKeyboardTransliterate}.
+     */
+    protected void handleKeyboardTransliterate(Replaceable text,
+                                               int[] offsets) {
+        offsets[CURSOR] = offsets[LIMIT];
+    }
+}
