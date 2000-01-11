2c0863cfd57ae33495618181e19e6b2415de14e7
Vladimir Weinstein <icu@weivsara.com>
2000-01-11 20:32:25 +0000

    [ICU-168] search for build tools moved from .mak to .bat and some confusing messages removed X-Commit-URL: https://ssl.icu-project.org/trac/changeset/535

--- icu4c/source/tools/makedata.bat
+++ icu4c/source/tools/makedata.bat
@@ -13,7 +13,12 @@ set toolversion=Release
 if not "%2"=="" set toolversion=%2
 
 nmake /f makedata.mak icup=%1 cfg=%2 %3 %4
-
+if not errorlevel 9009 goto :othererror
+echo Build tools are not on path! Please make sure that MSVC++ is setup correctly!
+goto :error
+:othererror
+if errorlevel 0 goto :end
+echo Nmake has unsuccesfully finished with errorcode %errorlevel%!
 goto :end
 
 rem setup mkhelper to read ucmfiles.mk and ucmlocal.mk
