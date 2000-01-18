c15f5ec40a546001d69b3a712a2777801ed1106c
Alan Liu <alansliu@gmail.com>
2000-01-18 18:43:09 +0000

    incorporate Mark's bug fixes
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/199
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@630 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/i18n/jamohang.cpp
+++ source/i18n/jamohang.cpp
@@ -58,24 +58,29 @@ void JamoHangulTransliterator::handleTransliterate(Replaceable& text,
      */
     int32_t cursor = offsets[CURSOR];
     int32_t limit = offsets[LIMIT];
-    if (cursor >= limit) {
-        return;
+    if (cursor >= limit) return;
+
+    int32_t count;
+    if (limit - cursor > 1) {
+        count = 5; // debugging spot
     }
-    
-    // get last character
+
     UChar last = filteredCharAt(text, cursor++);
-    UnicodeString str = UNICODE_STRING("a", 1);
-    while (cursor < limit) {
-        UChar c = filteredCharAt(text, cursor);
-        UChar replacement = composeHangul(last, c);
-        if (replacement != 0) {
+    UnicodeString str("a", 1);
+    while (cursor <= limit) {
+        UChar next = 0xFFFF; // go over end of string, just in case
+        if (cursor < limit) next = filteredCharAt(text, cursor);
+        UChar replacement = composeHangul(last, next, count);
+        if (replacement != last) {
             str[0] = replacement;
-            text.handleReplaceBetween(cursor-1, cursor+1, str);
+            text.handleReplaceBetween(cursor-1, cursor-1 + count, str);
+            limit = limit - count + 1; // fix up limit 2 => -1, 1 => 0
             last = replacement;
-            // leave cursor where it is
-            --limit; // fix up limit
+            if (next == 0xFFFF) break;
+            // don't change cursor, so we revisit char
         } else {
             ++cursor;
+            last = next;
         }
     }
 
@@ -84,8 +89,6 @@ void JamoHangulTransliterator::handleTransliterate(Replaceable& text,
 }
 
 // These constants are from the Unicode book's algorithm.
-// There's no need to make them class members, since they
-// are only used here.
 
 #define SBase   (0xAC00)
 #define LBase   (0x1100)
@@ -96,30 +99,56 @@ void JamoHangulTransliterator::handleTransliterate(Replaceable& text,
 #define TCount  (28)
 #define NCount  (VCount * TCount)   // 588
 #define SCount  (LCount * NCount)   // 11172
+#define LLimit  (0x1200)
 
 /**
- * Return composed character (if it composes)
- * 0 otherwise
+ * Return composed character (if it is a modern jamo)
+ * last otherwise.
+ * If there is a replacement, returns count[0] = 2 if ch was used, 1 otherwise
  */
-UChar JamoHangulTransliterator::composeHangul(UChar last, UChar ch) {
-  // check to see if two current characters are L and V
-  int32_t LIndex = last - LBase;
-  if (0 <= LIndex && LIndex < LCount) {
-      int32_t VIndex = ch - VBase;
-      if (0 <= VIndex && VIndex < VCount) {
-          // make syllable of form LV
-          return (UChar)(SBase + (LIndex * VCount + VIndex) * TCount);
-      }
-  }
-  // check to see if two current characters are LV and T
-  int32_t SIndex = last - SBase;
-  if (0 <= SIndex && SIndex < SCount && (SIndex % TCount) == 0) {
-      int32_t TIndex = ch - TBase;
-      if (0 <= TIndex && TIndex <= TCount) {
-          // make syllable of form LVT
-          return (UChar)(last + TIndex);
-      }
-  }
-  // if neither case was true, skip
-  return 0x0000;
+UChar JamoHangulTransliterator::composeHangul(UChar last, UChar ch, int32_t& count) {
+    count = 2; // default is replace 2 chars
+    // check to see if two current characters are L and V
+    int32_t LIndex = last - LBase;
+    if (0 <= LIndex && LIndex < LCount) {
+        int32_t VIndex = ch - VBase;
+        if (0 <= VIndex && VIndex < VCount) {
+            // make syllable of form LV
+            return (UChar)(SBase + (LIndex * VCount + VIndex) * TCount);
+        } else {
+            // it is isolated, so fix!
+            count = 1; // not using ch
+            return (UChar)(SBase + (LIndex * VCount) * TCount);
+        }
+    }
+      
+    // if neither case was true, see if we have an isolated Jamo we need to fix
+    if (LBase <= last && last < LLimit) {
+        // need to fix: it is either medial or final!
+        int32_t VIndex = last - VBase;
+        if (0 <= VIndex && VIndex < VCount) {
+            LIndex = 0x110B - LBase; // use empty consonant
+            // make syllable of form LV
+            count = 1; // not using ch
+            return (UChar)(SBase + (LIndex * VCount + VIndex) * TCount);
+        }
+        // ok, see if final. Use null consonant + a + final
+        int32_t TIndex = last - TBase;
+        if (0 <= TIndex && TIndex <= TCount) {  // need to fix!
+            count = 1; // not using ch
+            return (UChar)(0xC544 + TIndex);
+        }
+    }
+ 
+    // check to see if two current characters are LV and T
+    int32_t SIndex = last - SBase;
+    if (0 <= SIndex && SIndex < SCount && (SIndex % TCount) == 0) {
+        int32_t TIndex = ch - TBase;
+        if (0 <= TIndex && TIndex <= TCount) {
+            // make syllable of form LVT
+            return (UChar)(last + TIndex);
+        }
+    }
+      
+    return last;
 }    
