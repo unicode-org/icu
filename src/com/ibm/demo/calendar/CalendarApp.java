0f887df8517d04fe38ef98fced773d8edda6107d
(no author) <anonymous@svn.icu-project.org>
2000-03-01 17:25:22 +0000

    This commit was manufactured by cvs2svn to create tag 'worksOn1_2'.
    X-SVN-Rev: 873

---
+++ src/com/ibm/demo/calendar/CalendarApp.java
@@ -0,0 +1,50 @@
+/**
+ * $RCSfile: CalendarApp.java,v $ $Revision: 1.3 $ $Date: 2000/03/01 17:25:21 $
+ *
+ * (C) Copyright IBM Corp. 1998.  All Rights Reserved.
+ *
+ * The program is provided "as is" without any warranty express or
+ * implied, including the warranty of non-infringement and the implied
+ * warranties of merchantibility and fitness for a particular purpose.
+ * IBM will not be liable for any damages suffered by you as a result
+ * of using the Program. In no event will IBM be liable for any
+ * special, indirect or consequential damages or lost profits even if
+ * IBM has been advised of the possibility of their occurrence. IBM
+ * will not be liable for any third party claims against you.
+ */
+
+package com.ibm.demo.calendar;
+
+import com.ibm.demo.*;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.*;
+import java.net.*;
+import java.io.*;
+
+import java.text.DateFormat;
+import java.util.SimpleTimeZone;
+
+import com.ibm.util.*;
+import com.ibm.text.*;
+
+/**
+ * CalendarApp demonstrates how Calendar works.
+ */
+public class CalendarApp extends DemoApplet
+{
+    /**
+     * The main function which defines the behavior of the CalendarDemo
+     * applet when an applet is started.
+     */
+    public static void main(String argv[]) {
+
+        new CalendarApp().showDemo();
+    }
+
+    /* This creates a CalendarFrame for the demo applet. */
+    public Frame createDemoFrame(DemoApplet applet) {
+        return new CalendarFrame(applet);
+    }
+}
