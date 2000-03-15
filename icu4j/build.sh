d756ad09e04d6c65a176a641f5611ccfc5efbac2
John Fitzpatrick <johnf@svn.icu-project.org>
2000-03-15 00:40:28 +0000

    Updated to the latest version of ant X-Commit-URL: https://ssl.icu-project.org/trac/changeset/949

--- icu4j/build.sh
+++ icu4j/build.sh
@@ -5,14 +5,14 @@
 #*******************************************************************************
 #*
 #* $Source: /xsrl/Nsvn/icu/icu4j/Attic/build.sh,v $ 
-#* $Date: 2000/03/10 04:17:55 $ 
-#* $Revision: 1.2 $
+#* $Date: 2000/03/15 00:40:27 $ 
+#* $Revision: 1.3 $
 #*
 #*****************************************************************************************
 #*/
 #!/bin/sh
 
-ADDL_CLASSPATH=build/ant.jar:build/projectx-tr2.jar:build/javac.jar
+ADDL_CLASSPATH=build/icu4jtoolz.zip:jakarta-ant/lib/ant.jar:jakarta-ant/lib/xml.jar
 
 if [ "$CLASSPATH" != "" ] ; then
   CLASSPATH=$CLASSPATH:$ADDL_CLASSPATH
