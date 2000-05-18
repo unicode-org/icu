04e1a8e22ffa4b86904c9b9b36bc8171c0e048fe
Alan Liu <alansliu@gmail.com>
2000-05-18 19:09:38 +0000

    Move from topleveltest package; update to include new tests, and to use new TestFmwk API X-Commit-URL: https://ssl.icu-project.org/trac/changeset/1396

---
+++ icu4j/src/com/ibm/test/TestAll.java
@@ -0,0 +1,93 @@
+/*
+ *******************************************************************************
+ * Copyright (C) 1996-2000, International Business Machines Corporation and    *
+ * others. All Rights Reserved.                                                *
+ *******************************************************************************
+ *
+ * $Source: /xsrl/Nsvn/icu/icu4j/src/com/ibm/test/Attic/TestAll.java,v $ 
+ * $Date: 2000/05/18 19:09:38 $ 
+ * $Revision: 1.1 $
+ *
+ *****************************************************************************************
+ */
+package com.ibm.test;
+import com.ibm.test.TestFmwk;
+import java.text.*;
+import java.util.*;
+
+/**
+ * Top level test used to run all other tests as a batch.
+ */
+ 
+public class TestAll extends TestFmwk {
+
+    public static void main(String[] args) throws Exception {
+        new TestAll().run(args);
+    }
+
+    public void TestBigNumberFormat() throws Exception{
+        run(new com.ibm.test.bnf.BigNumberFormatTest());
+    }
+    
+    public void TestCompression() throws Exception{
+        run(new TestFmwk[] {
+            new com.ibm.test.compression.DecompressionTest(),
+            new com.ibm.test.compression.ExhaustiveTest()
+            });
+    }
+    
+	public void TestNormalizer() throws Exception{
+		run(new TestFmwk[] {
+            new com.ibm.test.normalizer.BasicTest(),
+            new com.ibm.test.normalizer.ExhaustiveTest()
+            });
+	}
+
+	public void TestRuleBasedNumberFormat() throws Exception {
+		run(new TestFmwk[] {
+            new com.ibm.test.rbnf.RbnfTest(),
+            new com.ibm.test.rbnf.RbnfRoundTripTest()
+            });
+	}
+
+	public void TestRuleBasedBreakIterator() throws Exception {
+		run(new TestFmwk[] {
+            new com.ibm.test.rbbi.SimpleBITest(),
+            new com.ibm.test.rbbi.BreakIteratorTest()
+            });
+	}
+
+	public void TestTranslit() throws Exception {
+		run(new TestFmwk[] {
+            new com.ibm.test.translit.TransliteratorTest(),
+            new com.ibm.test.translit.UnicodeSetTest()
+            });
+	}
+
+	public void TestSearch() throws Exception {
+		run(new com.ibm.test.search.SearchTest());
+	}
+	
+	public void TestRichEdit() throws Exception {
+	    run(new com.ibm.test.richtext.TestAll());
+	}
+
+    public void TestCalendar() throws Exception {
+	    run(new TestFmwk[] {
+            new com.ibm.test.calendar.AstroTest(),
+            new com.ibm.test.calendar.CalendarRegression(),
+            new com.ibm.test.calendar.CompatibilityTest(),
+            new com.ibm.test.calendar.HebrewTest(),
+            new com.ibm.test.calendar.IBMCalendarTest(),
+            new com.ibm.test.calendar.IslamicTest()
+            });
+    }
+
+    public void TestTimeZone() throws Exception {
+	    run(new TestFmwk[] {
+            new com.ibm.test.timezone.TimeZoneTest(),
+            new com.ibm.test.timezone.TimeZoneRegression(),
+            new com.ibm.test.timezone.TimeZoneBoundaryTest()
+            });
+    }
+}
