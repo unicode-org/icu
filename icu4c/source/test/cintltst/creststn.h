7ef69f60ed2a1b88280d5a992b3c922b958ef74b
Madhu K <madhu@svn.icu-project.org>
2000-05-24 23:42:34 +0000

    ICU-184 Tests for the API ures_getBinary() for binary collation                       data in resource bundles
    X-SVN-Rev: 1487

--- icu4c/source/test/cintltst/creststn.h
+++ icu4c/source/test/cintltst/creststn.h
@@ -3,14 +3,15 @@
  * Copyright (c) 1997-1999, International Business Machines Corporation and
  * others. All Rights Reserved.
  ********************************************************************/
-/********************************************************************************
+/***************************************************************************
 *
 * File CRESTST.H
 *
 * Modification History:
-*        Name              Date            Description            
-*     Madhu Katragadda     05/09/200       Ported Tests for New ResourceBundle API
-*********************************************************************************
+*        Name               Date               Description
+*   Madhu Katragadda    05/09/2000   Ported Tests for New ResourceBundle API
+*   Madhu Katragadda    05/24/2000   Added new tests to test RES_BINARY for collationElements
+*************************************************************************************************
 */
 #ifndef _CRESTSTN
 #define _CRESTSTN
@@ -36,11 +37,14 @@
     static void TestAliasConflict(void);
 
 	static void TestFallback(void);
+
+	static void TestBinaryCollationData(void);
+   
+    
+    
     /**
      * extensive subtests called by TestResourceBundles
      **/
-     
-   
     static UBool testTag(const char* frag, UBool in_Root, UBool in_te, UBool in_te_IN);
 
     static void record_pass(void);
