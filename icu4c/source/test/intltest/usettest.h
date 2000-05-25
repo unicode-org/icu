74a775bb67747573147dd114a7faded9998237be
Alan Liu <alansliu@gmail.com>
2000-05-25 19:28:53 +0000

    ICU-352 add tests for inversion list upgrade
    X-SVN-Rev: 1494

--- icu4c/source/test/intltest/usettest.h
+++ icu4c/source/test/intltest/usettest.h
@@ -31,7 +31,50 @@ private:
     void TestAddRemove(void);
 	void TestCloneEqualHash(void);
 
-	void doAssert(UBool, const char*);
+    /**
+     * Make sure minimal representation is maintained.
+     */
+    void TestMinimalRep(void);
+
+    void TestAPI(void);
+
+    void TestExhaustive(void);
+
+private:
+
+    void _testComplement(int32_t a);
+
+    void _testAdd(int32_t a, int32_t b);
+
+    void _testRetain(int32_t a, int32_t b);
+
+    void _testRemove(int32_t a, int32_t b);
+
+    void _testXor(int32_t a, int32_t b);
+
+    /**
+     * Check that ranges are monotonically increasing and non-
+     * overlapping.
+     */
+    void checkCanonicalRep(const UnicodeSet& set, const UnicodeString& msg);
+
+    /**
+     * Convert a bitmask to a UnicodeSet.
+     */
+    static UnicodeSet bitsToSet(int32_t a);
+
+    /**
+     * Convert a UnicodeSet to a bitmask.  Only the characters
+     * U+0000 to U+0020 are represented in the bitmask.
+     */
+    static int32_t setToBits(const UnicodeSet& x);
+
+    /**
+     * Return the representation of an inversion list based UnicodeSet
+     * as a pairs list.  Ranges are listed in ascending Unicode order.
+     * For example, the set [a-zA-M3] is represented as "33AMaz".
+     */
+    static UnicodeString getPairs(const UnicodeSet& set);
 
     void expectContainment(const UnicodeSet& set,
                            const UnicodeString& setName,
@@ -42,6 +85,8 @@ private:
                        const UnicodeString& expectedPairs);
     void expectPairs(const UnicodeSet& set,
                      const UnicodeString& expectedPairs);
+	void doAssert(UBool, const char*);
+public:
     static UnicodeString escape(const UnicodeString& s);
 };
 
