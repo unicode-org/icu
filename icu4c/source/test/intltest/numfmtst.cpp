50e96302f837f4c0243da5257aa5baffa7379626
Alan Liu <alansliu@gmail.com>
2000-06-07 00:17:23 +0000

    [ICU-417] add tests for secondary grouping size X-Commit-URL: https://ssl.icu-project.org/trac/changeset/1581

--- icu4c/source/test/intltest/numfmtst.cpp
+++ icu4c/source/test/intltest/numfmtst.cpp
@@ -18,7 +18,7 @@
 // class NumberFormatTest
 // *****************************************************************************
 
-#define CASE(id,test) case id: name = #test; if (exec) { logln(#test "---"); logln((UnicodeString)""); test(); } break;
+#define CASE(id,test) case id: name = #test; if (exec) { logln(#test "---"); logln((UnicodeString)""); test(); } break
 
 #define CHECK(status,str) if (U_FAILURE(status)) { errln(UnicodeString("FAIL: ") + str); return; }
 
@@ -26,19 +26,20 @@ void NumberFormatTest::runIndexedTest( int32_t index, UBool exec, char* &name, c
 {
     // if (exec) logln((UnicodeString)"TestSuite DateFormatTest");
     switch (index) {
-        CASE(0,TestCurrencySign)
-        CASE(1,TestCurrency)
-        CASE(2,TestParse)
-        CASE(3,TestRounding487)
-        CASE(4,TestQuotes)
-        CASE(5,TestExponential)
-        CASE(6,TestPatterns)
+        CASE(0,TestCurrencySign);
+        CASE(1,TestCurrency);
+        CASE(2,TestParse);
+        CASE(3,TestRounding487);
+        CASE(4,TestQuotes);
+        CASE(5,TestExponential);
+        CASE(6,TestPatterns);
 
         // Upgrade to alphaWorks - liu 5/99
-        CASE(7,TestExponent)
-        CASE(8,TestScientific)
-        CASE(9,TestPad)
-        CASE(10,TestPatterns2)
+        CASE(7,TestExponent);
+        CASE(8,TestScientific);
+        CASE(9,TestPad);
+        CASE(10,TestPatterns2);
+        CASE(11,TestSecondaryGrouping);
 
         default: name = ""; break;
     }
@@ -420,7 +421,69 @@ NumberFormatTest::TestRounding487(void)
     delete nf;
     if (U_FAILURE(status)) errln((UnicodeString)"FAIL: Status " + (int32_t)status);
 }
- 
+
+/**
+ * Test the functioning of the secondary grouping value.
+ */
+void NumberFormatTest::TestSecondaryGrouping(void) {
+    UErrorCode status = U_ZERO_ERROR;
+    DecimalFormatSymbols US(Locale::US, status);
+    CHECK(status, "DecimalFormatSymbols ct");
+
+    DecimalFormat f("#,##,###", US, status);
+    CHECK(status, "DecimalFormat ct");
+
+    expect(f, 123456789L, "12,34,56,789");
+    expectPat(f, "#,##,###");
+    f.applyPattern("#,###", status);
+    CHECK(status, "applyPattern");
+
+    f.setSecondaryGroupingSize(4);
+    expect(f, 123456789L, "12,3456,789");
+    expectPat(f, "#,####,###");
+    NumberFormat *g = NumberFormat::createInstance(Locale("hi", "IN"), status);
+    CHECK(status, "createInstance(hi_IN)");
+
+    UnicodeString out;
+    int32_t l = 1876543210L;
+    g->format(l, out);
+    delete g;
+    // expect "1,87,65,43,210", but with Hindi digits
+    //         01234567890123
+    UBool ok = TRUE;
+    if (out.length() != 14) {
+        ok = FALSE;
+    } else {
+        for (int32_t i=0; i<out.length(); ++i) {
+            UBool expectGroup = FALSE;
+            switch (i) {
+            case 1:
+            case 4:
+            case 7:
+            case 10:
+                expectGroup = TRUE;
+                break;
+            }
+            // Later -- fix this to get the actual grouping
+            // character from the resource bundle.
+            UBool isGroup = (out.charAt(i) == 0x002C);
+            if (isGroup != expectGroup) {
+                ok = FALSE;
+                break;
+            }
+        }
+    }
+    if (!ok) {
+        errln((UnicodeString)"FAIL  Expected " + l +
+              " x hi_IN -> \"1,87,65,43,210\" (with Hindi digits), got \"" +
+              escape(out) + "\"");
+    } else {
+        logln((UnicodeString)"Ok    " + l +
+              " x hi_IN -> \"" +
+              escape(out) + "\"");
+    }
+}
+
 // -------------------------------------
  
 void
@@ -818,8 +881,8 @@ void NumberFormatTest::expectPat(DecimalFormat& fmt, const UnicodeString& exp) {
     UnicodeString pat;
     fmt.toPattern(pat);
     if (pat == exp) {
-        logln(UnicodeString("Ok   \"") + pat + '"');
+        logln(UnicodeString("Ok   \"") + pat + "\"");
     } else {
-        errln(UnicodeString("FAIL \"") + pat + "\", expected \"" + exp + '"');
+        errln(UnicodeString("FAIL \"") + pat + "\", expected \"" + exp + "\"");
     }
 }
