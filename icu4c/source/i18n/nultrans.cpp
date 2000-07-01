056697be0eec1e3b928ec61536b417ecb2f2c494
Alan Liu <alansliu@gmail.com>
2000-07-01 00:14:12 +0000

    ICU-451 fix NullTransliterator virtual function hiding
    X-SVN-Rev: 1729

--- icu4c/source/i18n/nultrans.cpp
+++ icu4c/source/i18n/nultrans.cpp
@@ -15,11 +15,6 @@ Transliterator* NullTransliterator::clone(void) const {
     return new NullTransliterator();
 }
 
-int32_t NullTransliterator::transliterate(Replaceable&, int32_t,
-                                          int32_t limit) const {
-    return limit;
-}
-
 void NullTransliterator::handleTransliterate(Replaceable& text, UTransPosition& offsets,
                                              UBool isIncremental) const {
     offsets.start = offsets.limit;
