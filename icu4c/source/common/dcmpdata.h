09f92d448042b1465bb20ba8b693d5a1641fa4a8
Alan Liu <alansliu@gmail.com>
2000-07-12 22:02:37 +0000

    ICU-89 update normalizer data to Unicode 3.0
    X-SVN-Rev: 1823

--- icu4c/source/common/dcmpdata.h
+++ icu4c/source/common/dcmpdata.h
@@ -1,16 +1,23 @@
 /*
- *   Copyright (C) 1997-1999, International Business Machines
- *   Corporation and others.  All Rights Reserved.
- */
-
-
+************************************************************************
+*   Copyright (c) 1997-2000, International Business Machines
+*   Corporation and others.  All Rights Reserved.
+************************************************************************
+* > THIS FILE WAS MACHINE GENERATED <
+* >       DO NOT EDIT BY HAND       <
+* >      RUN TOOL TO REGENERATE     <
+* Tool: com.ibm.text.Normalizer
+* Creation date: Wed Jul 12 12:31:00 PDT 2000
+*/
 #include "unicode/utypes.h"
 #include "ucmp8.h"
 #include "ucmp16.h"
 
 struct DecompData {
-    enum { MAX_CANONICAL = 21658 };
-    enum { MAX_COMPAT = 11153 };
+    enum { MAX_CANONICAL = 21754 };
+    enum { MAX_COMPAT = 11177 };
+    enum { DECOMP_MASK = 32767 };
+    enum { DECOMP_RECURSE = 32768 };
     enum { BASE = 0 };
 
     static const uint16_t offsets_index[];
@@ -19,14 +26,11 @@ struct DecompData {
 
     static const CompactShortArray* offsets;
 
-    static const UChar contents[];
+    static const uint16_t contents[];
 
     static const uint16_t canonClass_index[];
 
     static const int8_t canonClass_values[];
 
-    static CompactByteArray *canonClass;
+    static const CompactByteArray* canonClass;
 };
-
-
-
