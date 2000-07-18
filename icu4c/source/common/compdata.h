133d4a5035030d5b89551dfe8ca06a96d13e348a
Alan Liu <alansliu@gmail.com>
2000-07-18 19:18:09 +0000

    ICU-89 new version with more compact typeMask
    X-SVN-Rev: 1933

--- icu4c/source/common/compdata.h
+++ icu4c/source/common/compdata.h
@@ -7,7 +7,7 @@
 * >       DO NOT EDIT BY HAND       <
 * >      RUN TOOL TO REGENERATE     <
 * Tool: com.ibm.text.Normalizer
-* Creation date: Tue Jul 18 11:10:21 PDT 2000
+* Creation date: Tue Jul 18 12:19:43 PDT 2000
 */
 #include "unicode/utypes.h"
 #include "ucmp8.h"
@@ -15,7 +15,7 @@
 
 struct ComposeData {
     enum { BASE_COUNT = 748 };
-    enum { COMBINING_COUNT = 226 };
+    enum { COMBINING_COUNT = 55 };
     enum { MAX_COMPAT = 4341 };
     enum { MAX_CANONICAL = 5199 };
     enum { MAX_COMPOSED = 0xFB1D };
