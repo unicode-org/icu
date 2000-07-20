c419ff629762b1d884aa7659a26fe789463a8236
Steven R. Loomis <srl@icu-project.org>
2000-07-20 20:25:46 +0000

    [ICU-351] locale update X-Commit-URL: https://ssl.icu-project.org/trac/changeset/1988

--- icu4c/source/tools/genrb/genrbfiles.mk
+++ icu4c/source/tools/genrb/genrbfiles.mk
@@ -52,7 +52,8 @@ sh.txt sh_YU.txt sk.txt sk_SK.txt sl.txt\
 sl_SI.txt sq.txt sq_AL.txt sr.txt sr_YU.txt\
 sv.txt sv_SE.txt th.txt th_TH.txt tr.txt\
 tr_TR.txt uk.txt uk_UA.txt vi.txt vi_VN.txt\
-zh.txt zh_CN.txt zh_HK.txt zh_TW.txt hi.txt hi_IN.txt mt.txt mt_MT.txt eo.txt
+zh.txt zh_CN.txt zh_HK.txt zh_TW.txt hi.txt hi_IN.txt mt.txt mt_MT.txt eo.txt \
+kok.txt kok_IN.txt ta.txt ta_IN.txt mr.txt mr_IN.txt
 
 TRANSLIT_SOURCE = fullhalf.txt translit_index.txt kana.txt kbdescl1.txt\
 larabic.txt lcyril.txt ldevan.txt\
