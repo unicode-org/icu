90592a356443f4b7062418f4c36d404ada2c0d0b
Steven R. Loomis <srl@icu-project.org>
2000-07-28 18:06:41 +0000

    ICU-372 removed language codes pending further discussion [nb & nn]372: removed language codes pending further discussion [nb & nn]372: removed language codes pending further discussion [nb & nn]
    X-SVN-Rev: 2071

--- icu4c/source/tools/genrb/genrbfiles.mk
+++ icu4c/source/tools/genrb/genrbfiles.mk
@@ -54,7 +54,7 @@ sv.txt sv_SE.txt th.txt th_TH.txt tr.txt\
 tr_TR.txt uk.txt uk_UA.txt vi.txt vi_VN.txt\
 zh.txt zh_CN.txt zh_HK.txt zh_TW.txt hi.txt hi_IN.txt mt.txt mt_MT.txt eo.txt\
 kok.txt kok_IN.txt ta.txt ta_IN.txt mr.txt mr_IN.txt\
-nb.txt sv_FI.txt nn.txt
+sv_FI.txt 
 
 TRANSLIT_SOURCE = fullhalf.txt translit_index.txt kana.txt kbdescl1.txt\
 larabic.txt lcyril.txt ldevan.txt\
