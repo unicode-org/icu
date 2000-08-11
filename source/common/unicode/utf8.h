3514d0b3b6e5c9c7565ad4f0e36d7d7f1acdfcac
George Rhoten <grhoten@svn.icu-project.org>
2000-08-11 00:02:59 +0000

    fixed some compiler warnings
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/535
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@2179 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/unicode/utf8.h
+++ source/common/unicode/utf8.h
@@ -164,17 +164,17 @@ utf8_back1SafeBody(const uint8_t *s, UTextOffset start, UTextOffset i);
         (s)[(i)++]=(uint8_t)(c); \
     } else { \
         if((uint32_t)(c)<=0x7ff) { \
-            (s)[(i)++]=(uint8_t)((c)>>6)|0xc0; \
+            (s)[(i)++]=(uint8_t)(((c)>>6)|0xc0); \
         } else { \
             if((uint32_t)(c)<=0xffff) { \
-                (s)[(i)++]=(uint8_t)((c)>>12)|0xe0; \
+                (s)[(i)++]=(uint8_t)(((c)>>12)|0xe0); \
             } else { \
-                (s)[(i)++]=(uint8_t)((c)>>18)|0xf0; \
-                (s)[(i)++]=(uint8_t)((c)>>12)&0x3f|0x80; \
+                (s)[(i)++]=(uint8_t)(((c)>>18)|0xf0); \
+                (s)[(i)++]=(uint8_t)(((c)>>12)&0x3f|0x80); \
             } \
-            (s)[(i)++]=(uint8_t)((c)>>6)&0x3f|0x80; \
+            (s)[(i)++]=(uint8_t)(((c)>>6)&0x3f|0x80); \
         } \
-        (s)[(i)++]=(uint8_t)(c)&0x3f|0x80; \
+        (s)[(i)++]=(uint8_t)((c)&0x3f|0x80); \
     } \
 }
 
