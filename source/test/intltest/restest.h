272c20877bd9c7d3158f3df71c2e9c4881690f09
George Rhoten <grhoten@svn.icu-project.org>
2000-08-23 02:53:35 +0000

    fixed some compiler warnings
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/535
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@2335 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/test/intltest/restest.h
+++ source/test/intltest/restest.h
@@ -27,6 +27,12 @@ public:
 
 private:
     /**
+     * The assignment operator have no real implementation.
+     * It is provided to make the compiler happy. Do not call.
+     */
+    ResourceBundleTest& operator=(const ResourceBundleTest&) { return *this; }
+
+    /**
      * extensive subtests called by TestResourceBundles
      **/
     UBool testTag(const char* frag, UBool in_Root, UBool in_te, UBool in_te_IN);
