d39ed7b0eb008eeeb104e390c048f9cb74b050b2
Alan Liu <alansliu@gmail.com>
2000-09-19 19:24:47 +0000

    Convert non-ASCII characters to Unicode
    X-SVN-Rev: 2452

--- icu4j/src/com/ibm/icu/impl/data/HolidayBundle_fr.java
+++ icu4j/src/com/ibm/icu/impl/data/HolidayBundle_fr.java
@@ -5,8 +5,8 @@
  *******************************************************************************
  *
  * $Source: /xsrl/Nsvn/icu/icu4j/src/com/ibm/icu/impl/data/HolidayBundle_fr.java,v $ 
- * $Date: 2000/03/10 04:18:04 $ 
- * $Revision: 1.2 $
+ * $Date: 2000/09/19 19:24:47 $ 
+ * $Revision: 1.3 $
  *
  *****************************************************************************************
  */
@@ -21,29 +21,29 @@ public class HolidayBundle_fr extends ListResourceBundle {
         {   "All Saints' Day",      "Toussaint" },
         {   "Armistice Day",        "Jour de l'Armistice" },
         {   "Ascension",            "Ascension" },
-        {   "Bastille Day",         "Fäte de la Bastille" },
-        {   "Benito Juﬂrez Day",    "Fäte de Benito Juﬂrez" },
-        {   "Boxing Day",           "Lendemain de Noãl" },
-        {   "Christmas Eve",        "Veille de Noãl" },
-        {   "Christmas",            "Noãl" },
-        {   "Easter Monday",        "PÇques lundi" },
-        {   "Easter Sunday",        "PÇques" },
-        {   "Epiphany",             "l'–piphanie" },
-        {   "Flag Day",             "Fäte du Drapeau" },
+        {   "Bastille Day",         "F\u00EAte de la Bastille" },
+        {   "Benito Ju\u00E1rez Day",    "F\u00EAte de Benito Ju\u00E1rez" },
+        {   "Boxing Day",           "Lendemain de No\u00EBl" },
+        {   "Christmas Eve",        "Veille de No\u00EBl" },
+        {   "Christmas",            "No\u00EBl" },
+        {   "Easter Monday",        "P\u00E2ques lundi" },
+        {   "Easter Sunday",        "P\u00E2ques" },
+        {   "Epiphany",             "l'\u00C9piphanie" },
+        {   "Flag Day",             "F\u00EAte du Drapeau" },
         {   "Good Friday",          "Vendredi Saint" },
         {   "Halloween",            "Veille de la Toussaint" },
         {   "All Saints' Day",      "Toussaint" },
-        {   "Independence Day",     "Fäte Indâpendance" },
+        {   "Independence Day",     "F\u00EAte Ind\u00E9pendance" },
         {   "Maundy Thursday",      "Jeudi Saint" },
-        {   "Mother's Day",         "Fäte des màres" },
-        {   "National Day",         "Fäte Nationale" },
+        {   "Mother's Day",         "F\u00EAte des m\u00E8res" },
+        {   "National Day",         "F\u00EAte Nationale" },
         {   "New Year's Day",       "Jour de l'an" },
         {   "Palm Sunday",          "les Rameaux" },
-        {   "Pentecost",            "Pentecüte" },
+        {   "Pentecost",            "Pentec\u00F4te" },
         {   "Shrove Tuesday",       "Mardi Gras" },
-        {   "St. Stephen's Day",    "Saint-–tienne" },
-        {   "Victoria Day",         "Fäte de la Victoria" },
-        {   "Victory Day",          "Fäte de la Victoire" },
+        {   "St. Stephen's Day",    "Saint-\u00C9tienne" },
+        {   "Victoria Day",         "F\u00EAte de la Victoria" },
+        {   "Victory Day",          "F\u00EAte de la Victoire" },
     };
 
     public synchronized Object[][] getContents() { return fContents; }
