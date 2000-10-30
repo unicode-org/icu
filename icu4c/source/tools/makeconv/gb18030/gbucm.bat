693315bf5973cefcf748b7f09439fa7c5ed9234f
Markus Scherer <markus.icu@gmail.com>
2000-10-30 18:02:58 +0000

    [ICU-544] add batch files to automate the generation of final mapping files X-Commit-URL: https://ssl.icu-project.org/trac/changeset/2843

---
+++ icu4c/source/tools/makeconv/gb18030/gbucm.bat
@@ -0,0 +1,9 @@
+rem gbucm.bat takes gbkuni30.txt and ranges.txt and generates a .ucm file with
+rem all mappings except for the ones listed in ranges.txt
+
+copy gbkuni30.txt+ranges.txt gbku30r.txt
+gbmake4 <gbku30r.txt >fournor.txt
+copy gbkuni30.txt+fournor.txt gb4nor.txt
+sort <gb4nor.txt | gbtoucm >gb18030.ucm
+
+rem output: gb18030.ucm
