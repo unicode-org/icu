4ea6db304d879014068b1a64c550995ae09cce5d
Steven R. Loomis <srl@icu-project.org>
2000-11-01 19:29:17 +0000

    [ICU-683] fix entry point collisions [test case] X-Commit-URL: https://ssl.icu-project.org/trac/changeset/2852

--- icu4c/source/test/cintltst/udatatst.c
+++ icu4c/source/test/cintltst/udatatst.c
@@ -16,6 +16,7 @@
 #include "unicode/utypes.h"
 #include "unicode/udata.h"
 #include "unicode/ucnv.h"
+#include "unicode/ures.h"
 #include "cmemory.h"
 #include "cstring.h"
 #include "filestrm.h"
@@ -29,6 +30,7 @@ void TestUDataOpenChoiceDemo2();
 void TestUDataGetInfo();
 void TestUDataGetMemory();
 void TestErrorConditions();
+void TestAppData();
 
 void
 addUDataTest(TestNode** root)
@@ -39,7 +41,7 @@ addUDataTest(TestNode** root)
     addTest(root, &TestUDataGetInfo,    "udatatst/TestUDataGetInfo"   );
     addTest(root, &TestUDataGetMemory,  "udatatst/TestUDataGetMemory" );
     addTest(root, &TestErrorConditions, "udatatst/TestErrorConditions");
-
+    addTest(root, &TestAppData, "udatatst/TestAppData" );
 
 }
 
@@ -564,3 +566,79 @@ void TestErrorConditions(){
 
 }
 
+/* Test whether apps and ICU can each have their own root.res */
+void TestAppData()
+{
+  UResourceBundle *icu, *app;
+  UResourceBundle *tmp = NULL;
+  
+  const UChar *appString;
+  const UChar *icuString;
+
+  int32_t len;
+
+  UErrorCode status = U_ZERO_ERROR;
+
+  char* testPath=(char*)malloc(sizeof(char) * (strlen(u_getDataDirectory()) + strlen("testdat2") +1 ) );
+
+  strcat(strcpy(testPath, u_getDataDirectory()), "testdat2");
+
+  icu = ures_open(NULL, "root", &status);
+  if(U_FAILURE(status))
+  { 
+     log_err("%s:%d: Couldn't open root ICU bundle- %s", __FILE__, __LINE__, u_errorName(status));
+     return;
+  }
+  
+  app = ures_open(testPath, "root", &status);
+  if(U_FAILURE(status))
+  { 
+     log_err("%s:%d: Couldn't open app ICU bundle [%s]- %s", __FILE__, __LINE__, testPath, u_errorName(status));
+     return;
+  }
+
+  tmp = ures_getByKey(icu, "Version", tmp, &status);
+  if(U_FAILURE(status))
+  { 
+     log_err("%s:%d: Couldn't get Version string from ICU root bundle- %s", __FILE__, __LINE__, u_errorName(status));
+     return;
+  }
+
+  icuString =  ures_getString(tmp,  &len, &status);
+  if(U_FAILURE(status))
+  { 
+     log_err("%s:%d: Couldn't get string from Version string from ICU root bundle- %s", __FILE__, __LINE__, u_errorName(status));
+     return;
+  }
+
+
+  tmp = ures_getByKey(app, "Version", tmp, &status);
+  if(U_FAILURE(status))
+  { 
+     log_err("%s:%d: Couldn't get Version string from App root bundle- %s", __FILE__, __LINE__, u_errorName(status));
+     return;
+  }
+
+  appString =  ures_getString(tmp,  &len, &status);
+  if(U_FAILURE(status))
+  { 
+     log_err("%s:%d: Couldn't get string from Version string from App root bundle- %s", __FILE__, __LINE__, u_errorName(status));
+     return;
+  }
+
+  if(!u_strcmp(icuString, appString))
+  {
+    log_err("%s:%d: Error! Expected ICU and App root version strings to be DIFFERENT but they are both %s\n", __FILE__, __LINE__, austrdup(appString));
+  }
+  else
+  {
+    log_verbose("%s:%d:  appstr=%s, icustr=%s\n", __FILE__,
+      __LINE__, austrdup(appString), austrdup(icuString));
+  }
+
+  ures_close(tmp);
+  ures_close(icu);
+  ures_close(app);
+
+  return;
+}
