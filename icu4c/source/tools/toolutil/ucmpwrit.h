f7a702a358b7b8d0414bf6723e3b41b8754699d7
Vladimir Weinstein <icu@weivsara.com>
2000-11-06 23:57:26 +0000

    ICU-96 support for collation data
    X-SVN-Rev: 2874

--- icu4c/source/tools/toolutil/ucmpwrit.h
+++ icu4c/source/tools/toolutil/ucmpwrit.h
@@ -15,18 +15,16 @@
 
 #include "unicode/utypes.h"
 #include "unewdata.h"
+#include "umemstrm.h"
 
 #include "ucmp8.h"
 #include "ucmp16.h"
 #include "ucmp32.h"
 
+/* udata filestream variants */
 U_CAPI  uint32_t U_EXPORT2 udata_write_ucmp8 (UNewDataMemory *pData, const CompactByteArray* array);
-
 U_CAPI  uint32_t U_EXPORT2 udata_write_ucmp16(UNewDataMemory *pData, const CompactShortArray* array);
-
-/*Not implemented. [yet] */
-/*U_CAPI  uint32_t U_EXPORT2 udata_write_ucmp32(UNewDataMemory *pData, const CompactIntArray* array);*/
-
+U_CAPI  uint32_t U_EXPORT2 udata_write_ucmp32(UNewDataMemory *pData, const CompactIntArray* array);
 
 #endif
 
