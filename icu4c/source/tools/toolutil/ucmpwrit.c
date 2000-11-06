6181734e57648ac5b7ba39d1e4753b9c06015f75
Vladimir Weinstein <icu@weivsara.com>
2000-11-06 23:57:26 +0000

    [ICU-96] support for collation data X-Commit-URL: https://ssl.icu-project.org/trac/changeset/2874

--- icu4c/source/tools/toolutil/ucmpwrit.c
+++ icu4c/source/tools/toolutil/ucmpwrit.c
@@ -48,15 +48,6 @@ U_CAPI  uint32_t U_EXPORT2 udata_write_ucmp8 (UNewDataMemory *pData, const Compa
 }
 
 /* internal constants*/
-#define UCMP16_kMaxUnicode_int 65535
-#define UCMP16_kUnicodeCount_int (UCMP16_kMaxUnicode_int + 1)
-#define UCMP16_kBlockShift_int 7
-#define UCMP16_kBlockCount_int (1 << UCMP16_kBlockShift_int)
-#define UCMP16_kBlockBytes_int (UCMP16_kBlockCount_int * sizeof(int16_t))
-#define UCMP16_kIndexShift_int (16 - UCMP16_kBlockShift_int)
-#define UCMP16_kIndexCount_int (1 << UCMP16_kIndexShift_int)
-#define UCMP16_kBlockMask_int (UCMP16_kBlockCount_int - 1)
-
 
 static const int32_t UCMP16_kMaxUnicode = UCMP16_kMaxUnicode_int;
 static const int32_t UCMP16_kUnicodeCount = UCMP16_kUnicodeCount_int;
@@ -117,6 +108,52 @@ U_CAPI  uint32_t U_EXPORT2 udata_write_ucmp16 (UNewDataMemory *pData, const Comp
   return size;
 }
 
+/*
+   UCMP32 format:
+   
+
+  Add format here
+
+
+   offset   size                    what
+   ---------------------------------------------
+   0        4                       ICU_UCMP32_VERSION
+   4        4                       count
+   16       512*2 = 1024            fIndex [uint16's] (UCMP16_kIndexCount*2)
+   1032     1*fCount                fArray [int32's]
+   Padding is not needed for ucmp32, since the array consists of int32's
+   +        padding (to extend fCount to the nearest multiple of 4)
+  
+ */
+
+#if (UCMP32_kIndexCount != 512)
+# error UCMP32_kIndexCount - changed size. Check to see if different pading needed.
+#endif
+
+U_CAPI  uint32_t U_EXPORT2 udata_write_ucmp32 (UNewDataMemory *pData, const CompactIntArray* array)
+{
+  int32_t size = 0;
+
+  udata_write32(pData, ICU_UCMP32_VERSION);
+  size += 4;
+  
+  udata_write32(pData, array->fCount);
+  size += 4;
+
+  udata_writeBlock(pData, array->fIndex, sizeof(array->fIndex[0])*UCMP32_kIndexCount);
+  size += sizeof(array->fIndex[0])*UCMP32_kIndexCount;
+  
+  udata_writeBlock(pData, array->fArray, sizeof(array->fArray[0])*array->fCount);
+  size += sizeof(array->fArray[0])*array->fCount;
+  
+  while(size%4) /* end padding */
+  {
+      udata_writePadding(pData, 1); /* Pad total so far to even size */
+      size += 1;
+  }
+
+  return size;
+}
 
 
 
