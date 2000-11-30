dcb91de287a29f492c82043cab5c02a70ea8e094
George Rhoten <grhoten@svn.icu-project.org>
2000-11-30 20:02:51 +0000

    Should now flush the output now
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/742
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@3065 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/tools/ctestfw/ctest.c
+++ source/tools/ctestfw/ctest.c
@@ -121,90 +121,92 @@ void addTest ( TestNode** root,
            TestFunctionPtr test,
            const char* name )
 {
-  TestNode *newNode;
-  
-  /*if this is the first Test created*/
-  if (*root == NULL) *root = createTestNode();
-  
-  newNode = addTestNode( *root, name );
-  assert(newNode != 0 );
-  /*  printf("addTest: nreName = %s\n", newNode->name );*/
-  
-  newNode->test = test;
+    TestNode *newNode;
+
+    /*if this is the first Test created*/
+    if (*root == NULL)
+        *root = createTestNode();
+
+    newNode = addTestNode( *root, name );
+    assert(newNode != 0 );
+    /*  printf("addTest: nreName = %s\n", newNode->name );*/
+
+    newNode->test = test;
 }
 
 /* non recursive insert function */
 static TestNode *addTestNode ( TestNode *root, const char *name )
 {
-  const char* nextName;
-  TestNode *nextNode, *curNode;
-  int nameLen; /* length of current 'name' */
-  
-  /* remove leading slash */
-  if ( *name == TEST_SEPARATOR )  name++;    
-  
-  curNode = root;
-  
-  for(;;)
+    const char* nextName;
+    TestNode *nextNode, *curNode;
+    int nameLen; /* length of current 'name' */
+
+    /* remove leading slash */
+    if ( *name == TEST_SEPARATOR )
+        name++;
+
+    curNode = root;
+
+    for(;;)
     {
-      /* Start with the next child */
-      nextNode = curNode->child;
-      
-      getNextLevel ( name, &nameLen, &nextName );
-      
-      /*      printf("* %s\n", name );*/
-      
-      /* if nextNode is already null, then curNode has no children
-     -- add them */
-      if( nextNode == NULL )
-    {
-      /* Add all children of the node */
-      do
+        /* Start with the next child */
+        nextNode = curNode->child;
+
+        getNextLevel ( name, &nameLen, &nextName );
+
+        /*      printf("* %s\n", name );*/
+
+        /* if nextNode is already null, then curNode has no children
+        -- add them */
+        if( nextNode == NULL )
         {
-          curNode->child = createTestNode ( );
-          
-          /* Get the next component of the name */
-          getNextLevel ( name, &nameLen, &nextName );
-          
-          /* update curName to have the next name segment */
-          strncpy ( curNode->child->name , name, nameLen );
-          curNode->child->name[nameLen] = 0;
-          /* printf("*** added %s\n", curNode->child->name );*/
-          curNode = curNode->child;
-          name = nextName;
+            /* Add all children of the node */
+            do
+            {
+                curNode->child = createTestNode ( );
+
+                /* Get the next component of the name */
+                getNextLevel ( name, &nameLen, &nextName );
+
+                /* update curName to have the next name segment */
+                strncpy ( curNode->child->name , name, nameLen );
+                curNode->child->name[nameLen] = 0;
+                /* printf("*** added %s\n", curNode->child->name );*/
+                curNode = curNode->child;
+                name = nextName;
+            }
+            while( name != NULL );
+
+            return curNode;
         }
-      while( name != NULL );
-      
-      return curNode;
-    }
-      
-      /* Search across for the name */
-      while (strncmp_nullcheck ( name, nextNode->name, nameLen) != 0 )
-    {
-      curNode = nextNode;
-      nextNode = nextNode -> sibling;
-      
-      if ( nextNode == NULL )
+
+        /* Search across for the name */
+        while (strncmp_nullcheck ( name, nextNode->name, nameLen) != 0 )
         {
-          /* Did not find 'name' on this level. */
-          nextNode = createTestNode ( );
-          strncpy( nextNode->name, name, nameLen );
-          nextNode->name[nameLen] = 0;
-          curNode->sibling = nextNode;
-          break;
+            curNode = nextNode;
+            nextNode = nextNode -> sibling;
+
+            if ( nextNode == NULL )
+            {
+                /* Did not find 'name' on this level. */
+                nextNode = createTestNode ( );
+                strncpy( nextNode->name, name, nameLen );
+                nextNode->name[nameLen] = 0;
+                curNode->sibling = nextNode;
+                break;
+            }
         }
-    }
-      
-      /* nextNode matches 'name' */
-      
-      if (nextName == NULL) /* end of the line */
-    {
-      return nextNode;
-    }
-      
-      /* Loop again with the next item */
-      name = nextName;
-      curNode = nextNode;
+
+        /* nextNode matches 'name' */
+
+        if (nextName == NULL) /* end of the line */
+        {
+            return nextNode;
+        }
+
+        /* Loop again with the next item */
+        name = nextName;
+        curNode = nextNode;
     }
 }
 
@@ -213,189 +215,198 @@ static void iterateTestsWithLevel ( const TestNode* root,
                  const TestNode** list,
                  TestMode mode)
 {
-  int i;
-  int saveIndent;
+    int i;
+    int saveIndent;
 
-  char pathToFunction[MAXTESTNAME] = "";
-  char separatorString[2] = { TEST_SEPARATOR, '\0'};
+    char pathToFunction[MAXTESTNAME] = "";
+    char separatorString[2] = { TEST_SEPARATOR, '\0'};
 
-  if ( root == NULL )    return;
+    if ( root == NULL )
+        return;
 
-  list[len++] = root;       
+    list[len++] = root;
 
-  for ( i=0;i<(len-1);i++ )
+    for ( i=0;i<(len-1);i++ )
     {
-      strcat(pathToFunction, list[i]->name);
-      strcat(pathToFunction, separatorString);
+        strcat(pathToFunction, list[i]->name);
+        strcat(pathToFunction, separatorString);
     }
 
-  strcat(pathToFunction, list[i]->name);
+    strcat(pathToFunction, list[i]->name);
 
-  INDENT_LEVEL = len;
-  if ( (mode == RUNTESTS) && (root->test != NULL)) 
+    INDENT_LEVEL = len;
+    if ( (mode == RUNTESTS) && (root->test != NULL)) 
     {
-      int myERROR_COUNT = ERROR_COUNT;
-      root->test();
-      if (myERROR_COUNT != ERROR_COUNT)
-    {
+        int myERROR_COUNT = ERROR_COUNT;
+        root->test();
+        if (myERROR_COUNT != ERROR_COUNT)
+        {
 
-      log_info("---[%d ERRORS] ", ERROR_COUNT - myERROR_COUNT);
-      strcpy(ERROR_LOG[ERRONEOUS_FUNCTION_COUNT++], pathToFunction);
+            log_info("---[%d ERRORS] ", ERROR_COUNT - myERROR_COUNT);
+            strcpy(ERROR_LOG[ERRONEOUS_FUNCTION_COUNT++], pathToFunction);
+        }
+        else
+            log_info("---[OK] ");
     }
-      else log_info("---[OK] ");
-    }
 
 
-  /* we want these messages to be at 0 indent. so just push the indent level breifly. */
-  saveIndent = INDENT_LEVEL;
-  INDENT_LEVEL = 0;
-  log_info("%s%s%c\n", (list[i]->test||mode==SHOWTESTS)?"---":"",pathToFunction, list[i]->test?' ':TEST_SEPARATOR );
-  INDENT_LEVEL = saveIndent;
+    /* we want these messages to be at 0 indent. so just push the indent level breifly. */
+    saveIndent = INDENT_LEVEL;
+    INDENT_LEVEL = 0;
+    log_info("%s%s%c\n", (list[i]->test||mode==SHOWTESTS)?"---":"",pathToFunction, list[i]->test?' ':TEST_SEPARATOR );
+    INDENT_LEVEL = saveIndent;
 
-  iterateTestsWithLevel ( root->child, len, list, mode );
+    iterateTestsWithLevel ( root->child, len, list, mode );
 
-  len--;
+    len--;
 
-  if ( len != 0 ) /* DO NOT iterate over siblings of the root. */
-      iterateTestsWithLevel ( root->sibling, len, list, mode );
+    if ( len != 0 ) /* DO NOT iterate over siblings of the root. */
+        iterateTestsWithLevel ( root->sibling, len, list, mode );
 }
 
 
 
 void showTests ( const TestNode *root )
 {
-  /* make up one for them */
-  const TestNode *aList[MAXTESTS];
+    /* make up one for them */
+    const TestNode *aList[MAXTESTS];
 
-  if (root == NULL) log_err("TEST CAN'T BE FOUND!");
-  
-  iterateTestsWithLevel ( root, 0, aList, SHOWTESTS );
-  
+    if (root == NULL)
+        log_err("TEST CAN'T BE FOUND!");
+
+    iterateTestsWithLevel ( root, 0, aList, SHOWTESTS );
+
 }
 
 void runTests ( const TestNode *root )
 {
-  int i;
-  const TestNode *aList[MAXTESTS];
-  /* make up one for them */
-  
+    int i;
+    const TestNode *aList[MAXTESTS];
+    /* make up one for them */
 
-  if (root == NULL) log_err("TEST CAN'T BE FOUND!\n");
 
-  ERRONEOUS_FUNCTION_COUNT = ERROR_COUNT = 0;  
-  iterateTestsWithLevel ( root, 0, aList, RUNTESTS );
-  
-  /*print out result summary*/
-  
-  if (ERROR_COUNT)
+    if (root == NULL)
+        log_err("TEST CAN'T BE FOUND!\n");
+
+    ERRONEOUS_FUNCTION_COUNT = ERROR_COUNT = 0;  
+    iterateTestsWithLevel ( root, 0, aList, RUNTESTS );
+
+    /*print out result summary*/
+
+    if (ERROR_COUNT)
     {
-      log_info("\nSUMMARY:\n******* [Total error count:\t%d]\n Errors in\n", ERROR_COUNT);
-      for (i=0;i < ERRONEOUS_FUNCTION_COUNT; i++) log_info("[%s]\n",ERROR_LOG[i]);
+        log_info("\nSUMMARY:\n******* [Total error count:\t%d]\n Errors in\n", ERROR_COUNT);
+        for (i=0;i < ERRONEOUS_FUNCTION_COUNT; i++)
+            log_info("[%s]\n",ERROR_LOG[i]);
     }
-  else 
+    else 
     {
-      log_info("\n[All tests passed successfully...]\n");
+        log_info("\n[All tests passed successfully...]\n");
     }
 
 }
 
 const TestNode* getTest(const TestNode* root, const char* name)
 {
-  const char* nextName;
-  TestNode *nextNode;
-  const TestNode* curNode;
-  int nameLen; /* length of current 'name' */
-  
-  if (root == NULL) log_err("TEST CAN'T BE FOUND!\n");
-  /* remove leading slash */
-  if ( *name == TEST_SEPARATOR )  name++;    
-  
-  curNode = root;
-  
-  for(;;)
+    const char* nextName;
+    TestNode *nextNode;
+    const TestNode* curNode;
+    int nameLen; /* length of current 'name' */
+
+    if (root == NULL)
+        log_err("TEST CAN'T BE FOUND!\n");
+    /* remove leading slash */
+    if ( *name == TEST_SEPARATOR )
+        name++;
+
+    curNode = root;
+
+    for(;;)
     {
-      /* Start with the next child */
-      nextNode = curNode->child;
-      
-      getNextLevel ( name, &nameLen, &nextName );
-      
-      /*      printf("* %s\n", name );*/
-      
-      /* if nextNode is already null, then curNode has no children
-     -- add them */
-      if( nextNode == NULL )
-    {
-      return NULL;
-    }
-      
-      /* Search across for the name */
-      while (strncmp_nullcheck ( name, nextNode->name, nameLen) != 0 )
-    {
-      curNode = nextNode;
-      nextNode = nextNode -> sibling;
-      
-      if ( nextNode == NULL )
+        /* Start with the next child */
+        nextNode = curNode->child;
+
+        getNextLevel ( name, &nameLen, &nextName );
+
+        /*      printf("* %s\n", name );*/
+
+        /* if nextNode is already null, then curNode has no children
+        -- add them */
+        if( nextNode == NULL )
         {
-          /* Did not find 'name' on this level. */
-          return NULL;
+            return NULL;
         }
-    }
-      
-      /* nextNode matches 'name' */
-      
-      if (nextName == NULL) /* end of the line */
-    {
-      return nextNode;
-    }
-      
-      /* Loop again with the next item */
-      name = nextName;
-      curNode = nextNode;
+
+        /* Search across for the name */
+        while (strncmp_nullcheck ( name, nextNode->name, nameLen) != 0 )
+        {
+            curNode = nextNode;
+            nextNode = nextNode -> sibling;
+
+            if ( nextNode == NULL )
+            {
+                /* Did not find 'name' on this level. */
+                return NULL;
+            }
+        }
+
+        /* nextNode matches 'name' */
+
+        if (nextName == NULL) /* end of the line */
+        {
+            return nextNode;
+        }
+
+        /* Loop again with the next item */
+        name = nextName;
+        curNode = nextNode;
     }
 }
 
 void log_err(const char* pattern, ...)
 {
-  va_list ap;
-  if(strchr(pattern, '\n') != NULL) {
-      /*
-       * Count errors only if there is a line feed in the pattern
-       * so that we do not exaggerate our error count.
-       */
-      ++ERROR_COUNT;
-  }
-  if( ERR_MSG == FALSE){
-      return;
-  }
-  va_start(ap, pattern);
-  fprintf(stdout, "%-*s", INDENT_LEVEL," " );
-  vfprintf(stderr, pattern, ap);
-  va_end(ap);
+    va_list ap;
+    if(strchr(pattern, '\n') != NULL) {
+        /*
+         * Count errors only if there is a line feed in the pattern
+         * so that we do not exaggerate our error count.
+         */
+        ++ERROR_COUNT;
+    }
+    if( ERR_MSG == FALSE){
+        return;
+    }
+    va_start(ap, pattern);
+    fprintf(stderr, "%-*s", INDENT_LEVEL," " );
+    vfprintf(stderr, pattern, ap);
+    fflush(stderr);
+    va_end(ap);
 }
 
 void log_info(const char* pattern, ...)
 {
-  va_list ap;
-  
-  va_start(ap, pattern);
-  fprintf(stdout, "%-*s", INDENT_LEVEL," " );
-  vfprintf(stdout, pattern, ap);
-  va_end(ap);
+    va_list ap;
+
+    va_start(ap, pattern);
+    fprintf(stdout, "%-*s", INDENT_LEVEL," " );
+    vfprintf(stdout, pattern, ap);
+    fflush(stdout);
+    va_end(ap);
 }
 
 void log_verbose(const char* pattern, ...)
 {
-  va_list ap;
-  
-  if ( VERBOSITY == FALSE )
-    return;
+    va_list ap;
 
-  va_start(ap, pattern);
-  fprintf(stdout, "%-*s", INDENT_LEVEL," " );
-  vfprintf(stdout, pattern, ap);
-  va_end(ap);
+    if ( VERBOSITY == FALSE )
+        return;
+
+    va_start(ap, pattern);
+    fprintf(stdout, "%-*s", INDENT_LEVEL," " );
+    vfprintf(stdout, pattern, ap);
+    fflush(stdout);
+    va_end(ap);
 }
-
 
 
 
@@ -403,108 +414,108 @@ int processArgs(const TestNode* root,
              int argc,
              const char* const argv[])
 {
-/**
- * This main will parse the l, v, h, n, and path arguments
- */
-  const TestNode*    toRun;
-  int                i;
-  int                doList = FALSE;
-  int                runAll = FALSE;
-  int                subtreeOptionSeen = FALSE;
+    /**
+     * This main will parse the l, v, h, n, and path arguments
+     */
+    const TestNode*    toRun;
+    int                i;
+    int                doList = FALSE;
+    int                runAll = FALSE;
+    int                subtreeOptionSeen = FALSE;
 
-  int                errorCount = 0;
+    int                errorCount = 0;
 
-  toRun = root;
-  VERBOSITY = FALSE;
-  ERR_MSG = TRUE;
+    toRun = root;
+    VERBOSITY = FALSE;
+    ERR_MSG = TRUE;
 
-  for( i=1; i<argc; i++)
-  {
-    if ( argv[i][0] == '/' )
+    for( i=1; i<argc; i++)
     {
-        printf("Selecting subtree '%s'\n", argv[i]);
-
-        if ( argv[i][1] == 0 )
-            toRun = root;
-        else
-            toRun = getTest(root, argv[i]);
-
-        if ( toRun == NULL )
+        if ( argv[i][0] == '/' )
         {
-            printf("* Could not find any matching subtree\n");
+            printf("Selecting subtree '%s'\n", argv[i]);
+
+            if ( argv[i][1] == 0 )
+                toRun = root;
+            else
+                toRun = getTest(root, argv[i]);
+
+            if ( toRun == NULL )
+            {
+                printf("* Could not find any matching subtree\n");
+                return -1;
+            }
+
+            if( doList == TRUE)
+                showTests(toRun);
+            else if( runAll == TRUE)
+                runTests(toRun);
+            else
+                runTests(toRun);
+
+            errorCount += ERROR_COUNT;
+
+            subtreeOptionSeen = TRUE;
+        }
+        else if (strcmp( argv[i], "-v" )==0 )
+        {
+            VERBOSITY = TRUE;
+        }
+        else if (strcmp( argv[i], "-verbose")==0 )
+        {
+            VERBOSITY = TRUE;
+        }
+        else if (strcmp( argv[i], "-l" )==0 )
+        {
+            doList = TRUE;
+        }
+        else if (strcmp( argv[i], "-all") ==0)
+        {
+            runAll = TRUE;
+        }
+        else if(strcmp( argv[i], "-a") == 0)
+        {
+            runAll = TRUE;
+        }
+        else if(strcmp( argv[i], "-n") == 0)
+        {
+            ERR_MSG = FALSE;
+        }
+        else if (strcmp( argv[i], "-no_err_msg") == 0)
+        {
+            ERR_MSG = FALSE;
+        }
+        else if (strcmp( argv[1], "-h" )==0 )
+        {
+            help( argv[0] );
+            return 0;
+        }
+        else
+        {
+            printf("* unknown option: %s\n", argv[i]);
+            help( argv[0] );
             return -1;
         }
+    }
 
+    if( subtreeOptionSeen == FALSE) /* no other subtree given, run the default */
+    {
         if( doList == TRUE)
-          showTests(toRun);
+            showTests(toRun);
         else if( runAll == TRUE)
-          runTests(toRun);
+            runTests(toRun);
         else
-          runTests(toRun);
+            runTests(toRun);
 
         errorCount += ERROR_COUNT;
-
-        subtreeOptionSeen = TRUE;
-    }
-    else if (strcmp( argv[i], "-v" )==0 )
-    {
-        VERBOSITY = TRUE;
-    }
-    else if (strcmp( argv[i], "-verbose")==0 )
-    {
-        VERBOSITY = TRUE;
-    }
-    else if (strcmp( argv[i], "-l" )==0 )
-    {
-        doList = TRUE;
-    }
-    else if (strcmp( argv[i], "-all") ==0)
-    {
-        runAll = TRUE;
-    }
-    else if(strcmp( argv[i], "-a") == 0)
-    {
-        runAll = TRUE;
-    }
-    else if(strcmp( argv[i], "-n") == 0)
-    {
-        ERR_MSG = FALSE;
-    }
-    else if (strcmp( argv[i], "-no_err_msg") == 0)
-    {
-        ERR_MSG = FALSE;
-    }
-    else if (strcmp( argv[1], "-h" )==0 )
-    {
-        help( argv[0] );
-        return 0;
     }
     else
     {
-        printf("* unknown option: %s\n", argv[i]);
-        help( argv[0] );
-        return -1;
-    }
-  }
-
-  if( subtreeOptionSeen == FALSE) /* no other subtree given, run the default */
-    {
-      if( doList == TRUE)
-    showTests(toRun);
-      else if( runAll == TRUE)
-    runTests(toRun);
-      else
-    runTests(toRun);
-      
-      errorCount += ERROR_COUNT;
-    }
-  else
-    {
-      if( ( doList == FALSE ) && ( errorCount > 0 ) )
-    printf(" Total errors: %d\n", errorCount );
+        if( ( doList == FALSE ) && ( errorCount > 0 ) )
+            printf(" Total errors: %d\n", errorCount );
     }
 
-  return errorCount; /* total error count */
+    return errorCount; /* total error count */
 }
 
 /**
