391353efd3bc6e48199038b56c3dc7bbcd610e71
Steven R. Loomis <srl@icu-project.org>
2000-12-09 02:37:17 +0000

    more documentation updates
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/647
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@3185 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/unicode/ucnv_cb.h
+++ source/common/unicode/ucnv_cb.h
@@ -14,7 +14,7 @@
 
 /**
  * \file 
- * \brief Description of callback C API for ICU's codeset conversion library
+ * \brief Description of converter callback C API
  *
  * <h2> Callback API for ICU's codeset conversion libray </h2>
  *
@@ -27,43 +27,45 @@
 #include "unicode/ucnv.h"
 #include "unicode/ucnv_err.h"
 
-/* From Unicode */
 /**
- * Used by the callback functions.  This function will write out the error
- * character(s) to the target byte buffer.
+ * Used by FromU callback functions.  Writes out the specified byte output bytes to the target
+ * byte buffer or to converter internal buffers.
  *
- * @param args : callback fromUnicode arguments
- * @param target : output target buffer of the callback result.
- * @param length : the length of target buffer
- * @param offsetIndex : the relative offset index from callback.
- * @param err error status <TT>U_BUFFER_OVERFLOW</TT>
+ * @param args callback fromUnicode arguments
+ * @param source source bytes to write
+ * @param length length of bytes to write
+ * @param offsetIndex the relative offset index from callback.
+ * @param err error status. If <TT>U_BUFFER_OVERFLOW</TT> is returned, then U_BUFFER_OVERFLOW <STRONG>must</STRONG> 
+ * be returned to the user, because it means that not all data could be written into the target buffer, and some is 
+ * in the converter error buffer.
  * @see ucnv_cbFromUWriteSub
  * @draft
  */
 
-U_CAPI void U_EXPORT2 ucnv_cbFromUWriteBytes (UConverterFromUnicodeArgs *args,
-                       const char* source,
-                       int32_t length,
-                       int32_t offsetIndex,
-                       UErrorCode * err);
+U_CAPI void U_EXPORT2
+ucnv_cbFromUWriteBytes (UConverterFromUnicodeArgs *args,
+                        const char* source,
+                        int32_t length,
+                        int32_t offsetIndex,
+                        UErrorCode * err);
 
 /**
- * Used by the callback functions.  This function will write out the error
- * character(s) to the substitution character.
+ * Used by FromU callback functions.  This function will write out the
+ * correct substitution character sequence to the target.
  *
- * @param args : callback fromUnicode arguments
- * @param target : output target buffer of the callback result.
- * @param length : the length of target buffer
- * @param offsetIndex : the relative offset index from callback.
- * @param err error status <TT>U_BUFFER_OVERFLOW</TT>
+ * @param args callback fromUnicode arguments
+ * @param offsetIndex the relative offset index from the current source pointer to be used
+ * @param err error status. If <TT>U_BUFFER_OVERFLOW</TT> is returned, then U_BUFFER_OVERFLOW <STRONG>must</STRONG> 
+ * be returned to the user, because it means that not all data could be written into the target buffer, and some is 
+ * in the converter error buffer.
  * @see ucnv_cbFromUWriteBytes
  * @draft
  */
 
-U_CAPI void U_EXPORT2 ucnv_cbFromUWriteSub (UConverterFromUnicodeArgs *args,
-                       int32_t offsetIndex,
-                       UErrorCode * err);
-
+U_CAPI void U_EXPORT2 
+ucnv_cbFromUWriteSub (UConverterFromUnicodeArgs *args,
+                      int32_t offsetIndex,
+                      UErrorCode * err);
 
 /**
  * Used by the callback functions.  This function will write out the error
