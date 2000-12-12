8dee9034701ad25dcded2c47293fabbbf4cdfbed
Markus Scherer <markus.icu@gmail.com>
2000-12-12 22:11:08 +0000

    [ICU-535] reinstate readable UTF16_..._SAFE macros, removing unwarranted new _STRICT/_LENIENT variants X-Commit-URL: https://ssl.icu-project.org/trac/changeset/3206

--- icu4c/source/common/ucnv_cnv.c
+++ icu4c/source/common/ucnv_cnv.c
@@ -143,7 +143,7 @@ ucnv_getUChar32KeepOverflow(UConverter *cnv, const UChar *buffer, int32_t length
 
     /* get the first code point in the buffer */
     i=0;
-    UTF_NEXT_CHAR_SAFE_LENIENT(buffer, i, length, c);
+    UTF_NEXT_CHAR_SAFE(buffer, i, length, c, FALSE);
     if(i<length) {
         /* there are UChars left in the buffer that need to go into the overflow buffer */
         UChar *overflow=cnv->UCharErrorBuffer;
