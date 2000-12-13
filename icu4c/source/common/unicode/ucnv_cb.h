0e4a604da9848425066d3a52949ffc81b98a2798
Steven R. Loomis <srl@icu-project.org>
2000-12-13 19:41:47 +0000

    [ICU-647] just documentation .. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/3215

--- icu4c/source/common/unicode/ucnv_cb.h
+++ icu4c/source/common/unicode/ucnv_cb.h
@@ -14,9 +14,47 @@
 
 /**
  * \file 
- * \brief Description of converter callback C API
+ * \brief C UConverter functions to aid the writers of callbacks
  *
- * <h2> Callback API for ICU's codeset conversion libray </h2>
+ * <h2> Callback API for UConverter </h2>
+ * 
+ * These functions are provided here for the convenience of the callback
+ * writer. If you are just looking for callback functions to use, please
+ * see ucnv_err.h.  DO NOT call these functions directly when you are 
+ * working with converters, unless your code has been called as a callback
+ * via ucnv_setFromUCallback or ucnv_setToUCallback !!
+ * 
+ * A note about error codes and overflow.  Unlike other ICU functions,
+ * these functions do not expect the error status to be U_ZERO_ERROR.
+ * Callbacks must be much more careful about their error codes.
+ * The error codes used here are in/out parameters, which should be passed
+ * back in the callback's error parameter.
+ * 
+ * For example, if you call ucnv_cbfromUWriteBytes to write data out 
+ * to the output codepage, it may return U_BUFFER_OVERFLOW_ERROR if 
+ * the data did not fit in the target. But this isn't a failing error, 
+ * in fact, ucnv_cbfromUWriteBytes may be called AGAIN with the error
+ * status still U_BUFFER_OVERFLOW_ERROR to attempt to write further bytes,
+ * which will also go into the internal overflow buffers.
+ * 
+ * Concerning offsets, the 'offset' parameters here are relative to the start
+ * of SOURCE.  For example, Suppose the string "ABCD" was being converted 
+ * from Unicode into a codepage which doesn't have a mapping for 'B'.
+ * 'A' will be written out correctly, but
+ * The FromU Callback will be called on an unassigned character for 'B'.
+ * At this point, this is the state of the world:
+ *    Target:    A [..]     [points after A]
+ *    Source:  A B [C] D    [points to C - B has been consumed]
+ *             0 1  2  3 
+ *    codePoint = "B"       [the unassigned codepoint] 
+ * 
+ * Now, suppose a callback wants to write the substitution character '?' to
+ * the target. It calls ucnv_cbFromUWriteBytes() to write the ?. 
+ * It should pass ZERO as the offset, because the offset as far as the 
+ * callback is concerned is relative to the SOURCE pointer [which points 
+ * before 'C'.]  If the callback goes into the args and consumes 'C' also,
+ * it would call FromUWriteBytes with an offset of 1 (and advance the source
+ * pointer).
  *
  */
 
@@ -28,8 +66,8 @@
 #include "unicode/ucnv_err.h"
 
 /**
- * Used by FromU callback functions.  Writes out the specified byte output bytes to the target
- * byte buffer or to converter internal buffers.
+ * ONLY used by FromU callback functions.
+ * Writes out the specified byte output bytes to the target byte buffer or to converter internal buffers.
  *
  * @param args callback fromUnicode arguments
  * @param source source bytes to write
@@ -50,8 +88,9 @@ ucnv_cbFromUWriteBytes (UConverterFromUnicodeArgs *args,
                         UErrorCode * err);
 
 /**
- * Used by FromU callback functions.  This function will write out the
- * correct substitution character sequence to the target.
+ * ONLY used by FromU callback functions.  
+ * This function will write out the correct substitution character sequence 
+ * to the target.
  *
  * @param args callback fromUnicode arguments
  * @param offsetIndex the relative offset index from the current source pointer to be used
@@ -68,13 +107,13 @@ ucnv_cbFromUWriteSub (UConverterFromUnicodeArgs *args,
                       UErrorCode * err);
 
 /**
- * Used by the callback functions.  This function will write out the error
- * character(s) to the target UChar buffer.
+ * ONLY used by fromU callback functions.  
+ * This function will write out the error character(s) to the target UChar buffer.
  *
- * @param args : callback fromUnicode arguments
- * @param source : pointer to pointer to first UChar to write [on exit: 1 after last UChar processed]
- * @param sourceLimit : pointer after last UChar to write
- * @param offsetIndex : the relative offset index from callback.
+ * @param args callback fromUnicode arguments
+ * @param source pointer to pointer to first UChar to write [on exit: 1 after last UChar processed]
+ * @param sourceLimit pointer after last UChar to write
+ * @param offsetIndex the relative offset index from callback which will be set
  * @param err error status <TT>U_BUFFER_OVERFLOW</TT>
  * @see ucnv_cbToUWriteSub
  * @draft
@@ -86,13 +125,14 @@ U_CAPI void U_EXPORT2 ucnv_cbFromUWriteUChars(UConverterFromUnicodeArgs *args,
                              UErrorCode * err);
 
 /**
- * Used by the callback functions.  This function will write out the error
- * character(s) to the target UChar buffer.
+ * ONLY used by ToU callback functions.
+ *  This function will write out the specified characters to the target 
+ * UChar buffer.
  *
- * @param args : callback toUnicode arguments
- * @param target : output target buffer of the callback result.
- * @param length : the length of target buffer
- * @param offsetIndex : the relative offset index from callback.
+ * @param args callback toUnicode arguments
+ * @param source source string to write
+ * @param length the length of source string
+ * @param offsetIndex the relative offset index which will be written.
  * @param err error status <TT>U_BUFFER_OVERFLOW</TT>
  * @see ucnv_cbToUWriteSub
  * @draft
@@ -105,11 +145,11 @@ U_CAPI void U_EXPORT2 ucnv_cbToUWriteUChars (UConverterToUnicodeArgs *args,
                                              UErrorCode * err);
 
 /**
- * Used by the callback functions.  This function will write out the error
- * character(s) to the substitution character.
+ * ONLY used by ToU  callback functions.  
+ * This function will write out the Unicode substitution character (U+FFFD).
  *
- * @param args : callback fromUnicode arguments
- * @param offsetIndex : the relative offset index from callback.
+ * @param args callback fromUnicode arguments
+ * @param offsetIndex the relative offset index from callback.
  * @param err error status <TT>U_BUFFER_OVERFLOW</TT>
  * @see ucnv_cbToUWriteUChars
  * @draft
@@ -119,4 +159,5 @@ U_CAPI void U_EXPORT2 ucnv_cbToUWriteSub (UConverterToUnicodeArgs *args,
                        int32_t offsetIndex,
                        UErrorCode * err);
 #endif
+
 
--- icu4c/source/common/unicode/ucnv_err.h
+++ icu4c/source/common/unicode/ucnv_err.h
@@ -9,34 +9,47 @@
  */
 /**
  * \file
- * \brief Description of UConverter error behavior C API
+ * \brief C UConverter predefined error callbacks
  *
  *  <h2> Error Behaviour Fnctions </h2>
- *  Defines error behaviour functions called by T_UConverter_{from,to}Unicode
+ *  Defines some error behaviour functions called by ucnv_{from,to}Unicode
+ *  These are provided as part of ICU and many are stable, but they
+ *  can also be considered only as an example of what can be done with
+ *  callbacks.  You may of course write your own.
  *
  *   These Functions, although public, should NEVER be called directly, they should be used as parameters to
- *   the T_UConverter_setMissing{Char,Unicode}Action API, to set the behaviour of a converter
+ *   the ucnv_setFromUCallback and ucnv_setToUCallback functions, to
+ *    set the behaviour of a converter
  *   when it encounters ILLEGAL/UNMAPPED/INVALID sequences.
  *
- *   usage example:
+ *   usage example:  'STOP' doesn't need any context, but newContext
+ *    could be set to something other than 'NULL' if needed.
  *
  *  \code
  *    UErrorCode err = U_ZERO_ERROR;
- *    UConverter* myConverter = T_UConverter_create("ibm-949", &err);
+ *    UConverter* myConverter = ucnv_open("ibm-949", &err);
+ *  void *newContext = NULL;
+ *  void *oldContext;
+ *  UConverterFromUCallback oldAction;
+ *
  *
  *    if (U_SUCCESS(err))
  *    {
- *       T_UConverter_setMissingUnicodeAction(myConverter, (MissingUnicodeAction)UCNV_FROM_U_CALLBACK_STOP, &err);
- *       T_UConverter_setMissingCharAction(myConverter, (MissingCharAction)UCNV_TO_U_CALLBACK_SUBSTITUTE, &err);
+ *  ucnv_setFromUCallBack(myConverter,
+ *                       UCNV_FROM_U_CALLBACK_STOP,
+ *                       newContext,
+ *                       &oldAction,
+ *                       &oldContext,
+ *                      &status);
  *    }
  *  \endcode
  *
  *   The code above tells "myConverter" to stop when it encounters a ILLEGAL/TRUNCATED/INVALID sequences when it is used to
  *   convert from Unicode -> Codepage.
- *   and to substitute with a codepage specific substitutions sequence when converting from Codepage -> Unicode
+ *   The behavior from Codepage to Unicode is not changed.
  */
 
-/* This file isn't designed to be included by itself. */
+/* This file isn't designed to be included all by itself. */
 #ifndef UCNV_H
 # include "unicode/ucnv.h"
  /* and the rest of this file will be ignored. */
@@ -49,21 +62,22 @@
 
 /** 
  * The process condition code to be used with the callbacks.  
- * UCNV_UNASSIGNED : the code point is unassigned.
- * UCNV_ILLEGAL : The code point is illegal.  For example,\x81\x2E is illegal 
- * because \x2E is not a valid trail byte for the \x81 lead byte in SJIS.
- * UCNV_IRREGULAR : The code point is not a regular sequence in the encoding.
- * For example,\xC0\E1 is irregular because the same character can be represented
- * as \x61.
- * UCNV_RESET : Whether the conversion operation has been reset.
- * UCNV_CLOSE : Whether the conversion operation has ended.
  */
 typedef enum {
-    UCNV_UNASSIGNED = 0,
-    UCNV_ILLEGAL = 1,
-    UCNV_IRREGULAR = 2,
-    UCNV_RESET = 3,
-    UCNV_CLOSE = 4
+    UCNV_UNASSIGNED = 0,  /* The code point is unassigned */
+    UCNV_ILLEGAL = 1,     /* The code point is illegal, for example, 
+                             \x81\x2E is illegal in SJIS because \x2E
+                             is not a valid trail byte for the \x81 
+                             lead byte. */
+    UCNV_IRREGULAR = 2,   /* The codepoint is not a regular sequence in 
+                             the encoding. For example, \xC0\xE1 would
+                             be irregular in UTF-8 because \x61 represents
+                             the same character. */
+    UCNV_RESET = 3,       /* The callback is called with this reason when a
+                             'reset' has occured. Callback should reset all
+                             state. */
+    UCNV_CLOSE = 4        /* Called when the converter is closed. The
+                             callback should release any allocated memory.*/
 } UConverterCallbackReason;
 
 
@@ -98,7 +112,9 @@ typedef struct {
 
 
 /**
- * Functor STOPS at the ILLEGAL_SEQUENCE 
+ * DO NOT CALL THIS FUNCTION DIRECTLY!
+ * This From Unicode callback STOPS at the ILLEGAL_SEQUENCE,
+ * returning the error code back to the caller immediately.
  * @stable
  */
 U_CAPI void U_EXPORT2 UCNV_FROM_U_CALLBACK_STOP (
@@ -113,7 +129,9 @@ U_CAPI void U_EXPORT2 UCNV_FROM_U_CALLBACK_STOP (
 
 
 /**
- * Functor STOPS at the ILLEGAL_SEQUENCE 
+ * DO NOT CALL THIS FUNCTION DIRECTLY!
+ * This To Unicode callback STOPS at the ILLEGAL_SEQUENCE,
+ * returning the error code back to the caller immediately.
  * @stable
  */
 U_CAPI void U_EXPORT2 UCNV_TO_U_CALLBACK_STOP (
@@ -125,7 +143,9 @@ U_CAPI void U_EXPORT2 UCNV_TO_U_CALLBACK_STOP (
 				  UErrorCode * err);
 
 /**
- * Functor SKIPs the ILLEGAL_SEQUENCE 
+ * DO NOT CALL THIS FUNCTION DIRECTLY!
+ * This From Unicode callback skips any illegal sequence, 
+ * simply ignoring those characters. 
  * @stable
  */
 U_CAPI void U_EXPORT2 UCNV_FROM_U_CALLBACK_SKIP (
@@ -138,10 +158,11 @@ U_CAPI void U_EXPORT2 UCNV_FROM_U_CALLBACK_SKIP (
 				  UErrorCode * err);
 
 /**
- * Functor Substitute the ILLEGAL SEQUENCE with the current substitution string assiciated with _this,
- * in the event target buffer is too small, it will store the extra info in the UConverter, and err
- * will be set to U_BUFFER_OVERFLOW_ERROR. The next time T_UConverter_fromUnicode is called, it will
- * store the left over data in target, before transcoding the "source Stream"
+ * DO NOT CALL THIS FUNCTION DIRECTLY!
+ * This From Unicode callback will Substitute the ILLEGAL SEQUENCE with the
+ * current substitution string for the converter. This is the default
+ * callback.
+ * @see ucnv_setSubstChars
  * @stable
  */
 
@@ -155,13 +176,14 @@ U_CAPI void U_EXPORT2 UCNV_FROM_U_CALLBACK_SUBSTITUTE (
 				  UErrorCode * err);
 
 /**
- * Functor Substitute the ILLEGAL SEQUENCE with a sequence escaped codepoints corresponding to the ILLEGAL
- * SEQUENCE (format  %UXXXX, e.g. "%uFFFE%u00AC%uC8FE"). In the Event the Converter doesn't support the
- * characters {u,%}[A-F][0-9], it will substitute  the illegal sequence with the substitution characters
- * (it will behave like the above functor).
- * in the event target buffer is too small, it will store the extra info in the UConverter, and err
- * will be set to U_BUFFER_OVERFLOW_ERROR. The next time T_UConverter_fromUnicode is called, it will
- * store the left over data in target, before transcoding the "source Stream"
+ * DO NOT CALL THIS FUNCTION DIRECTLY!
+ * This From Unicode callback will Substitute the ILLEGAL SEQUENCE with the
+ * hexadecimal representation of the illegal codepoints
+ *  (in the format  %UXXXX, e.g. "%uFFFE%u00AC%uC8FE"). In the Event the
+ * converter doesn't support the characters {u,%}[A-F][0-9], it will 
+ * substitute  the illegal sequence with the substitution characters.
+ * Note that percent (%) was chosen because backslash (\) does not exist
+ * on many converters.
  * @stable
  */
 
@@ -176,7 +198,9 @@ U_CAPI void U_EXPORT2 UCNV_FROM_U_CALLBACK_ESCAPE (
 
 
 /**
- * Functor SKIPs the ILLEGAL_SEQUENCE 
+ * DO NOT CALL THIS FUNCTION DIRECTLY!
+ * This To Unicode callback skips any illegal sequence, 
+ * simply ignoring those characters. 
  * @stable
  */
 U_CAPI void U_EXPORT2 UCNV_TO_U_CALLBACK_SKIP (
@@ -188,10 +212,9 @@ U_CAPI void U_EXPORT2 UCNV_TO_U_CALLBACK_SKIP (
 				  UErrorCode * err);
 
 /**
- * Functor Substitute the ILLEGAL SEQUENCE with the current substitution string assiciated with _this,
- * in the event target buffer is too small, it will store the extra info in the UConverter, and err
- * will be set to U_BUFFER_OVERFLOW_ERROR. The next time T_UConverter_fromUnicode is called, it will
- * store the left over data in target, before transcoding the "source Stream"
+ * DO NOT CALL THIS FUNCTION DIRECTLY!
+ * This To Unicode callback will Substitute the ILLEGAL SEQUENCE with the
+ * Unicode substitution character, U+FFFD.
  * @stable
  */
 U_CAPI void U_EXPORT2 UCNV_TO_U_CALLBACK_SUBSTITUTE (
@@ -203,11 +226,10 @@ U_CAPI void U_EXPORT2 UCNV_TO_U_CALLBACK_SUBSTITUTE (
 				  UErrorCode * err);
 
 /**
- * Functor Substitute the ILLEGAL SEQUENCE with a sequence escaped codepoints corresponding to the
- * ILLEGAL SEQUENCE (format  %XNN, e.g. "%XFF%X0A%XC8%X03").
- * in the event target buffer is too small, it will store the extra info in the UConverter, and err
- * will be set to U_BUFFER_OVERFLOW_ERROR. The next time T_UConverter_fromUnicode is called, it will
- * store the left over data in target, before transcoding the "source Stream"
+ * DO NOT CALL THIS FUNCTION DIRECTLY!
+ * This To Unicode callback will Substitute the ILLEGAL SEQUENCE with the
+ * hexadecimal representation of the illegal bytes
+ *  (in the format  %XNN, e.g. "%XFF%X0A%XC8%X03").
  * @stable
  */
 
@@ -219,4 +241,6 @@ U_CAPI void U_EXPORT2 UCNV_TO_U_CALLBACK_ESCAPE (
                   UConverterCallbackReason reason,
 				  UErrorCode * err);
 
-#endif/*UCNV_ERR_H*/ 
+#endif
+
+/*UCNV_ERR_H*/ 
