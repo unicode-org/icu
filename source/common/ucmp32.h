d42c65aeb3a8c8a7f194313284dba35a7050d018
George Rhoten <grhoten@svn.icu-project.org>
2000-12-13 19:48:01 +0000

    Documented the Compact Array API.
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/647
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@3216 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/ucmp32.h
+++ source/common/ucmp32.h
@@ -34,7 +34,7 @@
 #define UCMP32_kBlockMask   (UCMP32_kBlockCount-1)
 #define UCMP32_kUnicodeCount  65536
 
- /**
+/**
  * class CompactATypeArray : use only on primitive data types
  * Provides a compact way to store information that is indexed by Unicode
  * values, such as character properties, types, keyboard values, etc.This
@@ -77,11 +77,11 @@
  * @see                CompactIntArray
  * @see                CompactCharArray
  * @see                CompactStringArray
- * @version            $Revision: 1.14 $ 8/25/98
+ * @version            $Revision: 1.15 $ 8/25/98
  * @author             Helena Shih
  */
-/*====================================*/
-/*CompactIntArray
+/*====================================
+ *CompactIntArray
  * Provides a compact way to store information that is indexed by Unicode values,
  * such as character properties, types, keyboard values, etc.
  * The ATypes are used by value, so should be small, integers or pointers.
@@ -103,87 +103,122 @@ typedef struct CompactIntArray{
 
     
 /**
- *
  * Construct an empty CompactIntArray.
+ *
  * @param defaultValue the default value for all characters not explicitly in the array
  */
 U_CAPI CompactIntArray* U_EXPORT2 ucmp32_open(int32_t defaultValue);
 
- /**
-  * Construct a CompactIntArray from a pre-computed index and values array. The values
-  * will be adopted by the CompactIntArray. Memory is allocated with uprv_malloc.
-  * Note: for speed, the compact method will only re-use blocks in the values array
-  * that are on a block boundary. The pre-computed arrays passed in to this constructor
-  * may re-use blocks at any position in the values array.
-  * @param indexArray the index array to be adopted
-  * @param newValues the value array to be adopted
-  * @param count the number of entries in the value array
-  * @see compact
-  */
+/**
+ * Construct a CompactIntArray from a pre-computed index and values array. The values
+ * will be adopted by the CompactIntArray. Memory is allocated with uprv_malloc.
+ * Note: for speed, the compact method will only re-use blocks in the values array
+ * that are on a block boundary. The pre-computed arrays passed in to this constructor
+ * may re-use blocks at any position in the values array. The indexArray and
+ * newValues will be uprv_free'd when ucmp16_close() is called.
+ *
+ * @param indexArray the index array to be adopted
+ * @param newValues the value array to be adopted
+ * @param count the number of entries in the value array
+ * @see compact
+ */
 U_CAPI CompactIntArray* U_EXPORT2 ucmp32_openAdopt(uint16_t *indexArray,
                           int32_t *newValues,
                           int32_t count);
 
+/**
+ * Construct a CompactIntArray from a pre-computed index and values array. The values
+ * will be aliased by the CompactIntArray. Memory is allocated with uprv_malloc.
+ * Note: for speed, the compact method will only re-use blocks in the values array
+ * that are on a block boundary. The pre-computed arrays passed in to this constructor
+ * may re-use blocks at any position in the values array.
+ *
+ * @param indexArray the index array to be adopted
+ * @param newValues the value array to be adopted
+ * @param count the number of entries in the value array
+ * @see compact
+ */
 U_CAPI CompactIntArray* U_EXPORT2 ucmp32_openAlias(uint16_t *indexArray,
                           int32_t *newValues,
                           int32_t count);
- /**
-  * Initialize a CompactIntArray from a pre-computed index and values array. The values
-  * will be adopted by the CompactIntArray. No memory is allocated. Note: for speed,
-  * the compact method will only re-use blocks in the values array that are on a block
-  * boundary. The pre-computed arrays passed in to this constructor may re-use blocks
-  * at any position in the values array.
-  * @param indexArray the index array to be adopted
-  * @param newValues the value array to be adopted
-  * @param count the number of entries in the value array
-  * @see compact
-  */
+
+/**
+ * Initialize a CompactIntArray from a pre-computed index and values array. The values
+ * will be adopted by the CompactIntArray. No memory is allocated. Note: for speed,
+ * the compact method will only re-use blocks in the values array that are on a block
+ * boundary. The pre-computed arrays passed in to this constructor may re-use blocks
+ * at any position in the values array. The indexArray and
+ * newValues will be uprv_free'd when ucmp16_close() is called.
+ *
+ * @param indexArray the index array to be adopted
+ * @param newValues the value array to be adopted
+ * @param count the number of entries in the value array
+ * @see compact
+ */
 U_CAPI CompactIntArray* U_EXPORT2 ucmp32_initAdopt(CompactIntArray *this_obj,
                           uint16_t *indexArray,
                           int32_t *newValues,
                           int32_t count);
 
+/**
+ * Initialize a CompactIntArray from a pre-computed index and values array. The values
+ * will be aliased by the CompactIntArray. No memory is allocated. Note: for speed,
+ * the compact method will only re-use blocks in the values array that are on a block
+ * boundary. The pre-computed arrays passed in to this constructor may re-use blocks
+ * at any position in the values array.
+ *
+ * @param indexArray the index array to be adopted
+ * @param newValues the value array to be adopted
+ * @param count the number of entries in the value array
+ * @see compact
+ */
 U_CAPI CompactIntArray* U_EXPORT2 ucmp32_initAlias(CompactIntArray *this_obj,
                           uint16_t *indexArray,
                           int32_t *newValues,
                           int32_t count);
 
+/**
+ * Free up any allocated memory associated with this compact array.
+ * The memory that is uprv_free'd depends on how the array was initialized
+ * or opened.
+ * 
+ * @param array The compact array to close
+ */
 U_CAPI void U_EXPORT2 ucmp32_close(CompactIntArray* array);
 
 /**
-  * Returns TRUE if the creation of the compact array fails.
-  */
-
+ * Returns TRUE if the creation of the compact array fails.
+ */
 U_CAPI  UBool U_EXPORT2 ucmp32_isBogus(const CompactIntArray* array);
-/**
-  *
-  * Get the mapped value of a Unicode character.
-  * @param index the character to get the mapped value with
-  * @return the mapped value of the given character
-  */
 
+/**
+ * Get the mapped value of a Unicode character.
+ *
+ * @param index the character to get the mapped value with
+ * @return the mapped value of the given character
+ */
 #define ucmp32_get(array, index) (array->fArray[(array->fIndex[(index >> UCMP32_kBlockShift)] )+ \
                            (index & UCMP32_kBlockMask)])
 
 #define ucmp32_getu(array, index) (uint16_t)ucmp32_get(array, index)
 
 /**
-  * Set a new value for a Unicode character.
-  * Set automatically expands the array if it is compacted.
-  * @param character the character to set the mapped value with
-  * @param value the new mapped value
-  */
+ * Set a new value for a Unicode character.
+ * Set automatically expands the array if it is compacted.
+ * @param character the character to set the mapped value with
+ * @param value the new mapped value
+ */
 U_CAPI  void U_EXPORT2 ucmp32_set(CompactIntArray *array,
                   UChar character,
                   int32_t value);
 
- /**
-  *
-  * Set new values for a range of Unicode character.
-  * @param start the starting offset of the range
-  * @param end the ending offset of the range
-  * @param value the new mapped value
-  */
+/**
+ *
+ * Set new values for a range of Unicode character.
+ * @param start the starting offset of the range
+ * @param end the ending offset of the range
+ * @param value the new mapped value
+ */
 U_CAPI  void U_EXPORT2 ucmp32_setRange(CompactIntArray* array,
                    UChar start,
                    UChar end, 
@@ -197,29 +232,30 @@ U_CAPI  void U_EXPORT2 ucmp32_setRange(CompactIntArray* array,
  * same compression.
  */
 U_CAPI  void U_EXPORT2 ucmp32_compact(CompactIntArray* array, int32_t cycle);
+
 /**
  * Expands the compacted array.
  * Takes the array back to a 65536 element array
  */
-
 U_CAPI  void U_EXPORT2 ucmp32_expand(CompactIntArray* array);
+
 /**
- *
  * Get the number of elements in the value array.
+ *
  * @return the number of elements in the value array.
  */
 U_CAPI  uint32_t U_EXPORT2 ucmp32_getCount(const CompactIntArray* array);
 
 /**
- *
  * Get the address of the value array.
+ *
  * @return the address of the value array
  */
 U_CAPI  const int32_t* U_EXPORT2 ucmp32_getArray(const CompactIntArray* array);
 
 /**
- *
  * Get the address of the index array.
+ *
  * @return the address of the index array
  */
 U_CAPI  const uint16_t* U_EXPORT2 ucmp32_getIndex(const CompactIntArray* array);
@@ -234,9 +270,4 @@ U_CAPI  uint32_t U_EXPORT2 ucmp32_flattenMem(const CompactIntArray* array, UMemo
 U_CAPI  void U_EXPORT2 ucmp32_initFromData(CompactIntArray *this_obj, const uint8_t **source, UErrorCode *status);
 
 #endif
-
-
-
-
-
 
