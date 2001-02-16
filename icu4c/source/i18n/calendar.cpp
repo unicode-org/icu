a06988ae1c63c6336773bba455e1029b2c274510
George Rhoten <grhoten@users.noreply.github.com>
2001-02-16 21:46:22 +0000

    ICU-66 Moved the deletion to after the comparison
    X-SVN-Rev: 3648

--- icu4c/source/i18n/calendar.cpp
+++ icu4c/source/i18n/calendar.cpp
@@ -706,17 +706,20 @@ int32_t Calendar::stringToDayNumber(const UnicodeString& string, UErrorCode& sta
 
     int32_t len = string.length();
     char *number = new char[1 + len];
-    if (number == 0) { status = U_MEMORY_ALLOCATION_ERROR; return 0; }
+    if (number == 0) {
+        status = U_MEMORY_ALLOCATION_ERROR;
+        return 0;
+    }
     char *end;
 
     string.extract(0, len, number);
     number[len] = 0;
     int32_t value = strtol(number, &end, 10); // Radix 10
 
-    delete[] number;
-
     if (end-number != len || len == 0 || value < 1 || value > 7)
         status = U_INVALID_FORMAT_ERROR;
+
+    delete[] number;
 
     return value;
 }
