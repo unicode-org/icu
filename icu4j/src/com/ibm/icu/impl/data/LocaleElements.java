6985b50858b66a018da94d06c3c832bed49f4c8b
Alan Liu <alansliu@gmail.com>
2001-03-01 22:46:33 +0000

    Move transliterator index from LocalElements to its own file
    X-SVN-Rev: 3862

--- icu4j/src/com/ibm/icu/impl/data/LocaleElements.java
+++ icu4j/src/com/ibm/icu/impl/data/LocaleElements.java
@@ -5,8 +5,8 @@
  *******************************************************************************
  *
  * $Source: /xsrl/Nsvn/icu/icu4j/src/com/ibm/icu/impl/data/Attic/LocaleElements.java,v $ 
- * $Date: 2000/07/12 16:44:13 $ 
- * $Revision: 1.4 $
+ * $Date: 2001/03/01 22:46:33 $ 
+ * $Revision: 1.5 $
  *
  *****************************************************************************************
  */
@@ -25,81 +25,6 @@ public class LocaleElements extends ListResourceBundle {
                  * This is the language-neutral form of this resource.
                  */
                 "{0,choice,0#|1#{1}|2#{1}-{2}}", // Display name
-            },
-            { "RuleBasedTransliteratorIDs",
-                /* These IDs must have the form "From-To" or "SingleName".
-                 * The ID "From-To" will be sought in the resource bundle
-                 * TransliteratorRuleFromTo.java in the resources package.
-                 * The ID "SingleName" will be sought in
-                 * TransliteratorSingleName.java.  In addition, a reverse
-                 * transliterator may be specified using "*To-From", which
-                 * indicates TransliteratorRuleFromTo.java with a
-                 * RuleBasedTransliterator.REVERSE constructor parameter.
-                 */
-              new String[] {
-
-                  // Bidirectional rules
-
-                  "Fullwidth-Halfwidth",
-                  "*Halfwidth-Fullwidth",
-
-                  "Latin-Arabic",
-                  "*Arabic-Latin",
-
-                  "Latin-Cyrillic",
-                  "*Cyrillic-Latin",
-
-                  "Latin-Devanagari",
-                  "*Devanagari-Latin",
-
-                  "Latin-Greek",
-                  "*Greek-Latin",
-
-                  "Latin-Hebrew",
-                  "*Hebrew-Latin",
-
-                  "Latin-Jamo",
-                  "*Jamo-Latin",
-
-                  "Latin-Kana",
-                  "*Kana-Latin",
-
-                  "Hiragana-Katakana",
-                  "*Katakana-Hiragana",
-
-                  "StraightQuotes-CurlyQuotes",
-                  "*CurlyQuotes-StraightQuotes",
-
-                  // Composed rules
-
-                  "Devanagari~InterIndic",
-                  "Bengali~InterIndic",
-                  "Gurmukhi~InterIndic",
-                  "Gujarati~InterIndic",
-                  "Oriya~InterIndic",
-                  "Tamil~InterIndic",
-                  "Telugu~InterIndic",
-                  "Kannada~InterIndic",
-                  "Malayalam~InterIndic",
-                  
-                  "InterIndic~Devanagari",
-                  "InterIndic~Bengali",
-                  "InterIndic~Gurmukhi",
-                  "InterIndic~Gujarati",
-                  "InterIndic~Oriya",
-                  "InterIndic~Tamil",
-                  "InterIndic~Telugu",
-                  "InterIndic~Kannada",
-                  "InterIndic~Malayalam",
-
-                  // One way rules (forward only)
-
-                  "Han-Pinyin",
-                  "Kanji-English",
-                  "Kanji-OnRomaji",
-                  "KeyboardEscape-Latin1",
-                  "UnicodeName-UnicodeChar"
-              }
             },
         };
     }
