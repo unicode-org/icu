b32d274095e8462556262d10f23b76dfaf521d82
George Rhoten <grhoten@svn.icu-project.org>
2001-03-15 23:45:54 +0000

    reimplemented u_strncpy
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/894
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@4118 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/ustring.c
+++ source/common/ustring.c
@@ -274,17 +274,15 @@ UChar*
 u_strcat(UChar     *dst, 
     const UChar     *src)
 {
-  UChar *anchor = dst;            /* save a pointer to start of dst */
+    UChar *anchor = dst;            /* save a pointer to start of dst */
 
-  while(*dst != 0) {              /* To end of first string          */
-    ++dst;
-  }
-  while((*dst = *src) != 0) {     /* copy string 2 over              */
-    ++dst;
-    ++src;
-  }
+    while(*dst != 0) {              /* To end of first string          */
+        ++dst;
+    }
+    while((*(dst++) = *(src++)) != 0) {     /* copy string 2 over              */
+    }
 
-  return anchor;
+    return anchor;
 }
 
 UChar* 
@@ -292,25 +290,25 @@ u_strncat(UChar     *dst,
      const UChar     *src, 
      int32_t     n ) 
 {
-  if(n > 0) {
-    UChar *anchor = dst;            /* save a pointer to start of dst */
+    if(n > 0) {
+        UChar *anchor = dst;            /* save a pointer to start of dst */
 
-    while(*dst != 0) {              /* To end of first string          */
-      ++dst;
+        while(*dst != 0) {              /* To end of first string          */
+            ++dst;
+        }
+        while((*dst = *src) != 0) {     /* copy string 2 over              */
+            ++dst;
+            if(--n == 0) {
+                *dst = 0;
+                break;
+            }
+            ++src;
+        }
+
+        return anchor;
+    } else {
+        return dst;
     }
-    while((*dst = *src) != 0) {     /* copy string 2 over              */
-      ++dst;
-      if(--n == 0) {
-        *dst = 0;
-        break;
-      }
-      ++src;
-    }
-  
-    return anchor;
-  } else {
-    return dst;
-  }
 }
 
 /* ----- Text property functions --- */
@@ -319,43 +317,43 @@ int32_t
 u_strcmp(const UChar *s1, 
     const UChar *s2) 
 {
-  int32_t rc;
-  for(;;) {
-    rc = (int32_t)*s1 - (int32_t)*s2;
-    if(rc != 0 || *s1 == 0) {
-      return rc;
+    int32_t rc;
+    for(;;) {
+        rc = (int32_t)*s1 - (int32_t)*s2;
+        if(rc != 0 || *s1 == 0) {
+            return rc;
+        }
+        ++s1;
+        ++s2;
     }
-    ++s1;
-    ++s2;
-  }
 }
 
 /* String compare in code point order - u_strcmp() compares in code unit order. */
 U_CAPI int32_t U_EXPORT2
 u_strcmpCodePointOrder(const UChar *s1, const UChar *s2) {
-  static const UChar utf16Fixup[32]={
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0x2000, 0xf800, 0xf800, 0xf800, 0xf800
-  };
-  UChar c1, c2;
-  int32_t diff;
+    static const UChar utf16Fixup[32]={
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0x2000, 0xf800, 0xf800, 0xf800, 0xf800
+    };
+    UChar c1, c2;
+    int32_t diff;
 
-  /* rotate each code unit's value so that surrogates get the highest values */
-  for(;;) {
-    c1=*s1;
-    c1+=utf16Fixup[c1>>11]; /* additional "fix-up" line */
-    c2=*s2;
-    c2+=utf16Fixup[c2>>11]; /* additional "fix-up" line */
-
-    /* now c1 and c2 are in UTF-32-compatible order */
-    diff=(int32_t)c1-(int32_t)c2;
-    if(diff!=0 || c1==0 /* redundant: || c2==0 */) {
-      return diff;
+    /* rotate each code unit's value so that surrogates get the highest values */
+    for(;;) {
+        c1=*s1;
+        c1+=utf16Fixup[c1>>11]; /* additional "fix-up" line */
+        c2=*s2;
+        c2+=utf16Fixup[c2>>11]; /* additional "fix-up" line */
+        
+        /* now c1 and c2 are in UTF-32-compatible order */
+        diff=(int32_t)c1-(int32_t)c2;
+        if(diff!=0 || c1==0 /* redundant: || c2==0 */) {
+            return diff;
+        }
+        ++s1;
+        ++s2;
     }
-    ++s1;
-    ++s2;
-  }
 }
 
 int32_t  
@@ -363,33 +361,31 @@ u_strncmp(const UChar     *s1,
      const UChar     *s2, 
      int32_t     n) 
 {
-  if(n > 0) {
-    int32_t rc;
-    for(;;) {
-      rc = (int32_t)*s1 - (int32_t)*s2;
-      if(rc != 0 || *s1 == 0 || --n == 0) {
-        return rc;
-      }
-      ++s1;
-      ++s2;
+    if(n > 0) {
+        int32_t rc;
+        for(;;) {
+            rc = (int32_t)*s1 - (int32_t)*s2;
+            if(rc != 0 || *s1 == 0 || --n == 0) {
+                return rc;
+            }
+            ++s1;
+            ++s2;
+        }
+    } else {
+        return 0;
     }
-  } else {
-    return 0;
-  }
 }
 
 UChar*
 u_strcpy(UChar     *dst, 
     const UChar     *src) 
 {
-  UChar *anchor = dst;            /* save a pointer to start of dst */
+    UChar *anchor = dst;            /* save a pointer to start of dst */
 
-  while((*dst = *src) != 0) {     /* copy string 2 over              */
-    ++dst;
-    ++src;
-  }
+    while((*(dst++) = *(src++)) != 0) {     /* copy string 2 over              */
+    }
 
-  return anchor;
+    return anchor;
 }
 
 UChar* 
@@ -397,30 +393,20 @@ u_strncpy(UChar     *dst,
      const UChar     *src, 
      int32_t     n) 
 {
-  UChar *anchor = dst;            /* save a pointer to start of dst */
+    UChar *anchor = dst;            /* save a pointer to start of dst */
 
-  if(n > 0) {
-    while((*dst = *src) != 0) {   /* copy string 2 over              */
-      ++dst;
-      if(--n == 0) {
-        *dst = 0;
-        break;
-      }
-      ++src;
+    while((*(dst++) = *(src++)) != 0 && (--n > 0)) {   /* copy string 2 over              */
     }
-  } else {
-    *dst = 0;
-  }
 
-  return anchor;
+    return anchor;
 }
 
 int32_t  
 u_strlen(const UChar *s) 
 {
-# if U_SIZEOF_WCHAR_T == U_SIZEOF_UCHAR
+#if U_SIZEOF_WCHAR_T == U_SIZEOF_UCHAR
     return uprv_wcslen(s);
-# else
+#else
     const UChar *t = s;
     while(*t != 0) {
       ++t;
