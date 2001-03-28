cde50cac0b585a6affbc18a59f6add5c71684d9c
George Rhoten <grhoten@users.noreply.github.com>
2001-03-28 01:48:16 +0000

    ICU-900 Fixed some compiler warnings.
    X-SVN-Rev: 4375

--- icu4c/source/test/intltest/cppcnvt.cpp
+++ icu4c/source/test/intltest/cppcnvt.cpp
@@ -5,9 +5,6 @@
  ********************************************************************/
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 #include "unicode/locid.h"
 #include "unicode/unistr.h"
 #include "unicode/convert.h"
