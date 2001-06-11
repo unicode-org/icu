dd59c6db0d80ccee53d11aad3e4b49b82a1dc753
Alan Liu <alansliu@gmail.com>
2001-06-11 19:51:46 +0000

    ICU-965 create Any-Lower, Any-Upper, and Any-Title transliterators
    X-SVN-Rev: 4941

---
+++ icu4c/source/i18n/tolowtrn.cpp
@@ -0,0 +1,65 @@
+/*
+**********************************************************************
+*   Copyright (C) 2001, International Business Machines
+*   Corporation and others.  All Rights Reserved.
+**********************************************************************
+*   Date        Name        Description
+*   05/24/01    aliu        Creation.
+**********************************************************************
+*/
+
+#include "unicode/tolowtrn.h"
+
+const char* LowercaseTransliterator::_ID = "Any-Lower";
+
+/**
+ * Constructs a transliterator.
+ */
+LowercaseTransliterator::LowercaseTransliterator(const Locale& theLoc,
+                                                   UnicodeFilter* adoptedFilter) :
+    TransformTransliterator(_ID, adoptedFilter),
+    loc(theLoc) {
+}
+
+/**
+ * Destructor.
+ */
+LowercaseTransliterator::~LowercaseTransliterator() {}
+
+/**
+ * Copy constructor.
+ */
+LowercaseTransliterator::LowercaseTransliterator(const LowercaseTransliterator& o) :
+    TransformTransliterator(o),
+    loc(o.loc) {}
+
+/**
+ * Assignment operator.
+ */
+LowercaseTransliterator& LowercaseTransliterator::operator=(
+                             const LowercaseTransliterator& o) {
+    TransformTransliterator::operator=(o);
+    loc = o.loc;
+    return *this;
+}
+
+/**
+ * Transliterator API.
+ */
+Transliterator* LowercaseTransliterator::clone(void) const {
+    return new LowercaseTransliterator(*this);
+}
+
+/**
+ * TransformTransliterator framework method.
+ */
+UBool LowercaseTransliterator::hasTransform(UChar32 c) const {
+    return c != u_tolower(c);
+}
+
+/**
+ * TransformTransliterator framework method.
+ */
+void LowercaseTransliterator::transform(UnicodeString& s) const {
+    s.toLower(loc);
+}
---
+++ icu4c/source/i18n/toupptrn.cpp
@@ -0,0 +1,65 @@
+/*
+**********************************************************************
+*   Copyright (C) 2001, International Business Machines
+*   Corporation and others.  All Rights Reserved.
+**********************************************************************
+*   Date        Name        Description
+*   05/24/01    aliu        Creation.
+**********************************************************************
+*/
+
+#include "unicode/toupptrn.h"
+
+const char* UppercaseTransliterator::_ID = "Any-Upper";
+
+/**
+ * Constructs a transliterator.
+ */
+UppercaseTransliterator::UppercaseTransliterator(const Locale& theLoc,
+                                                   UnicodeFilter* adoptedFilter) :
+    TransformTransliterator(_ID, adoptedFilter),
+    loc(theLoc) {
+}
+
+/**
+ * Destructor.
+ */
+UppercaseTransliterator::~UppercaseTransliterator() {}
+
+/**
+ * Copy constructor.
+ */
+UppercaseTransliterator::UppercaseTransliterator(const UppercaseTransliterator& o) :
+    TransformTransliterator(o),
+    loc(o.loc) {}
+
+/**
+ * Assignment operator.
+ */
+UppercaseTransliterator& UppercaseTransliterator::operator=(
+                             const UppercaseTransliterator& o) {
+    TransformTransliterator::operator=(o);
+    loc = o.loc;
+    return *this;
+}
+
+/**
+ * Transliterator API.
+ */
+Transliterator* UppercaseTransliterator::clone(void) const {
+    return new UppercaseTransliterator(*this);
+}
+
+/**
+ * TransformTransliterator framework method.
+ */
+UBool UppercaseTransliterator::hasTransform(UChar32 c) const {
+    return c != u_toupper(c);
+}
+
+/**
+ * TransformTransliterator framework method.
+ */
+void UppercaseTransliterator::transform(UnicodeString& s) const {
+    s.toUpper(loc);
+}
