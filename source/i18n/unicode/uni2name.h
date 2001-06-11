880f45be20712312462cc6ae1089c2cd847cf9db
Alan Liu <alansliu@gmail.com>
2001-06-11 23:38:54 +0000

    implement algorithmic Any-Name, Name-Any
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/989
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@4948 0a8b766c-62c9-45f9-954d-7f2943c9ead0

---
+++ source/i18n/unicode/uni2name.h
@@ -0,0 +1,71 @@
+/*
+**********************************************************************
+*   Copyright (C) 2001, International Business Machines
+*   Corporation and others.  All Rights Reserved.
+**********************************************************************
+*   Date        Name        Description
+*   06/06/01    aliu        Creation.
+**********************************************************************
+*/
+#ifndef UNI2NAME_H
+#define UNI2NAME_H
+
+#include "unicode/translit.h"
+
+/**
+ * A transliterator that performs character to name mapping.
+ * @author Alan Liu
+ */
+class U_I18N_API UnicodeNameTransliterator : public Transliterator {
+
+    UChar32 openDelimiter;
+    UChar32 closeDelimiter;
+
+ public:
+
+    /**
+     * Constructs a transliterator.
+     */
+    UnicodeNameTransliterator(UChar32 openDelimiter, UChar32 closeDelimiter,
+                                UnicodeFilter* adoptedFilter = 0);
+
+    /**
+     * Constructs a transliterator with the default delimiters '{' and
+     * '}'.
+     */
+    UnicodeNameTransliterator(UnicodeFilter* adoptedFilter = 0);
+
+    /**
+     * Destructor.
+     */
+    virtual ~UnicodeNameTransliterator();
+
+    /**
+     * Copy constructor.
+     */
+    UnicodeNameTransliterator(const UnicodeNameTransliterator&);
+
+    /**
+     * Assignment operator.
+     */
+    UnicodeNameTransliterator& operator=(const UnicodeNameTransliterator&);
+
+    /**
+     * Transliterator API.
+     */
+    Transliterator* clone(void) const;
+
+ protected:
+
+    /**
+     * Implements {@link Transliterator#handleTransliterate}.
+     * @draft
+     */
+    virtual void handleTransliterate(Replaceable& text, UTransPosition& offset,
+                                     UBool isIncremental) const;
+
+ private:
+
+    static const char* _ID;
+};
+#endif
