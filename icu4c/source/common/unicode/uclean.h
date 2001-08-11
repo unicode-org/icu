3af0a0b31a7712638e44b253b07a53177fc3b7c3
George Rhoten <grhoten@users.noreply.github.com>
2001-08-11 00:27:31 +0000

    ICU-770 New u_cleanup() API.
    X-SVN-Rev: 5460

---
+++ icu4c/source/common/unicode/uclean.h
@@ -0,0 +1,24 @@
+/*
+******************************************************************************
+*                                                                            *
+* Copyright (C) 2001-2001, International Business Machines                   *
+*                Corporation and others. All Rights Reserved.                *
+*                                                                            *
+******************************************************************************
+*   file name:  uclean.h
+*   encoding:   US-ASCII
+*   tab size:   8 (not used)
+*   indentation:4
+*
+*   created on: 2001July05
+*   created by: George Rhoten
+*/
+
+#ifndef __UCLEAN_H__
+#define __UCLEAN_H__
+
+#include "unicode/utypes.h"
+
+U_CAPI void U_EXPORT2 u_cleanup(void);
+
+#endif
