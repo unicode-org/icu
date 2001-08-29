2c81eb71e39ff6d9f8f417e847431b0ae23a0e3d
Markus Scherer <markus.icu@gmail.com>
2001-08-29 16:32:33 +0000

    [ICU-1007] remove ComposedCharIter X-Commit-URL: https://ssl.icu-project.org/trac/changeset/5600

--- icu4c/source/test/intltest/tstnorm.h
+++ icu4c/source/test/intltest/tstnorm.h
@@ -44,7 +44,6 @@ public:
     void TestTibetan(void);
     void TestCompositionExclusion(void);
     void TestZeroIndex(void);
-    void TestComposedCharIter(void);
     void TestVerisign(void);
 
 private:
