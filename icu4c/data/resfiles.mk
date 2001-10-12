1e5b739c8c58f36609676a7e40d0d0f84643bc8e
Ram Viswanadha <rviswanadha@gmail.com>
2001-10-12 19:01:26 +0000

    ICU-1203 Updates for Euro transition.
    X-SVN-Rev: 6214

--- icu4c/data/resfiles.mk
+++ icu4c/data/resfiles.mk
@@ -33,30 +33,30 @@ ar_SY.txt ar_TN.txt ar_YE.txt\
 be.txt be_BY.txt\
 bg.txt bg_BG.txt\
 bn.txt bn_IN.txt\
-ca.txt ca_ES.txt ca_ES_EURO.txt\
+ca.txt ca_ES.txt ca_ES_PREEURO.txt\
 cs.txt cs_CZ.txt\
 da.txt da_DK.txt\
-de.txt de__PHONEBOOK.txt de_AT.txt de_AT_EURO.txt de_BE.txt de_CH.txt\
-de_DE.txt de_DE_EURO.txt de_LU.txt de_LU_EURO.txt\
-el.txt el_GR.txt el_GR_EURO.txt\
-en.txt en_AS.txt en_AU.txt en_BE.txt en_BE_EURO.txt en_BW.txt en_CA.txt en_GB.txt en_GB_EURO.txt\
-en_GU.txt en_HK.txt en_IE.txt en_IE_EURO.txt en_IN.txt en_MH.txt en_MP.txt en_NZ.txt en_PH.txt en_SG.txt\
+de.txt de__PHONEBOOK.txt de_AT.txt de_AT_PREEURO.txt de_BE.txt de_CH.txt\
+de_DE.txt de_DE_PREEURO.txt de_LU.txt de_LU_PREEURO.txt\
+el.txt el_GR.txt el_GR_PREEURO.txt\
+en.txt en_AS.txt en_AU.txt en_BE.txt en_BE_PREEURO.txt en_BW.txt en_CA.txt en_GB.txt en_GB_EURO.txt\
+en_GU.txt en_HK.txt en_IE.txt en_IE_PREEURO.txt en_IN.txt en_MH.txt en_MP.txt en_NZ.txt en_PH.txt en_SG.txt\
 en_UM.txt en_US.txt en_US_POSIX.txt en_VI.txt en_ZA.txt en_ZW.txt\
 eo.txt\
 es.txt es__TRADITIONAL.txt es_AR.txt es_BO.txt es_CL.txt es_CO.txt\
-es_CR.txt es_DO.txt es_EC.txt es_ES.txt es_ES_EURO.txt\
+es_CR.txt es_DO.txt es_EC.txt es_ES.txt es_ES_PREEURO.txt\
 es_GT.txt es_HN.txt es_MX.txt es_NI.txt es_PA.txt\
 es_PE.txt es_PR.txt es_PY.txt es_SV.txt es_US.txt\
 es_UY.txt es_VE.txt\
 et.txt et_EE.txt\
-eu.txt eu_ES.txt eu_ES_EURO.txt\
+eu.txt eu_ES.txt eu_ES_PREEURO.txt\
 fa.txt fa_IR.txt\
-fi.txt fi_FI.txt fi_FI_EURO.txt\
+fi.txt fi_FI.txt fi_FI_PREEURO.txt\
 fo.txt fo_FO.txt\
-fr.txt fr_BE.txt fr_BE_EURO.txt fr_CA.txt fr_CH.txt\
-fr_FR.txt fr_FR_EURO.txt fr_LU.txt fr_LU_EURO.txt\
-ga.txt ga_IE.txt ga_IE_EURO.txt\
-gl.txt gl_ES.txt gl_ES_EURO.txt\
+fr.txt fr_BE.txt fr_BE_PREEURO.txt fr_CA.txt fr_CH.txt\
+fr_FR.txt fr_FR_PREEURO.txt fr_LU.txt fr_LU_PREEURO.txt\
+ga.txt ga_IE.txt ga_IE_PREEURO.txt\
+gl.txt gl_ES.txt gl_ES_PREEURO.txt\
 gv.txt gv_GB.txt\
 he.txt he_IL.txt\
 hi.txt hi_IN.txt hi__DIRECT.txt\
@@ -65,7 +65,7 @@ hu.txt hu_HU.txt\
 hy.txt hy_AM.txt hy_AM_REVISED.txt\
 id.txt id_ID.txt\
 is.txt is_IS.txt\
-it.txt it_CH.txt it_IT.txt it_IT_EURO.txt\
+it.txt it_CH.txt it_IT.txt it_IT_PREEURO.txt\
 iw.txt iw_IL.txt\
 ja.txt ja_JP.txt ja_JP_JIS.txt\
 kl.txt kl_GL.txt\
@@ -79,12 +79,12 @@ mr.txt mr_IN.txt\
 mt.txt mt_MT.txt\
 ms.txt ms_MY.txt\
 nb.txt nb_NO.txt\
-nl.txt nl_BE.txt nl_BE_EURO.txt nl_NL.txt nl_NL_EURO.txt\
+nl.txt nl_BE.txt nl_BE_PREEURO.txt nl_NL.txt nl_NL_PREEURO.txt\
 nn.txt nn_NO.txt\
 no.txt no_NO.txt no_NO_NY.txt\
 om.txt om_ET.txt om_KE.txt\
 pl.txt pl_PL.txt\
-pt.txt pt_BR.txt pt_PT.txt pt_PT_EURO.txt\
+pt.txt pt_BR.txt pt_PT.txt pt_PT_PREEURO.txt\
 ro.txt ro_RO.txt\
 ru.txt ru_RU.txt ru_UA.txt\
 sh.txt sh_YU.txt\
