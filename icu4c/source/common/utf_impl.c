38b3810bae50856ff88fb92267d7e8bfdda6e80f
Markus Scherer <markus.icu@gmail.com>
2001-10-12 18:53:26 +0000

    ICU-726 tighten & fix UTF macros: non-shortest UTF-8 illegal (U 3.0.1), check U 3.1 non-characters, fix some bugs
    X-SVN-Rev: 6212

--- icu4c/source/common/utf_impl.c
+++ icu4c/source/common/utf_impl.c
@@ -40,6 +40,10 @@
  * - SUB AX, BX (result)
  * -finish:
  * (BSR: Bit Scan Reverse, scans for a 1-bit, starting from the MSB)
+ *
+ * In Unicode, all UTF-8 byte sequences with more than 4 bytes are illegal;
+ * lead bytes above 0xf4 are illegal.
+ * We keep them in this table for skipping long ISO 10646-UTF-8 sequences.
  */
 U_EXPORT const uint8_t U_EXPORT2
 utf8_countTrailBytes[256]={
@@ -62,14 +66,15 @@ utf8_countTrailBytes[256]={
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 
     2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    3, 3, 3, 3, 3, 3, 3, 3,
-    4, 4, 4, 4,
-    5, 5,
-    0, 0    /* illegal bytes 0xfe and 0xff */
+    3, 3, 3, 3, 3,
+    3, 3, 3,    /* illegal in Unicode */
+    4, 4, 4, 4, /* illegal in Unicode */
+    5, 5,       /* illegal in Unicode */
+    0, 0        /* illegal bytes 0xfe and 0xff */
 };
 
 static const UChar32
-utf8_minRegular[4]={ 0, 0x80, 0x800, 0x10000 };
+utf8_minLegal[4]={ 0, 0x80, 0x800, 0x10000 };
 
 static const UChar32
 utf8_errorValue[6]={
@@ -89,13 +94,10 @@ utf8_nextCharSafeBody(const uint8_t *s, UTextOffset *pi, UTextOffset length, UCh
         switch(count) {
         /* each branch falls through to the next one */
         case 5:
-            trail=s[(i)++];
-            (c)=((c)<<6)|(trail&0x3f);
-            illegal|=(trail&0xc0)^0x80;
         case 4:
-            trail=s[(i)++];
-            (c)=((c)<<6)|(trail&0x3f);
-            illegal|=(trail&0xc0)^0x80;
+            /* count>=4 is always illegal: no more than 3 trail bytes in Unicode's UTF-8 */
+            illegal=1;
+            break;
         case 3:
             trail=s[(i)++];
             (c)=((c)<<6)|(trail&0x3f);
@@ -103,7 +105,6 @@ utf8_nextCharSafeBody(const uint8_t *s, UTextOffset *pi, UTextOffset length, UCh
                 illegal|=(trail&0xc0)^0x80;
             } else {
                 /* code point>0x10ffff, outside Unicode */
-                i+=2;
                 illegal=1;
                 break;
             }
@@ -117,32 +118,30 @@ utf8_nextCharSafeBody(const uint8_t *s, UTextOffset *pi, UTextOffset length, UCh
             illegal|=(trail&0xc0)^0x80;
             break;
         case 0:
-            illegal=1;
+            return UTF8_ERROR_VALUE_1;
         /* no default branch to optimize switch()  - all values are covered */
-            break;
         }
 
         /*
          * All the error handling should return a value
          * that needs count bytes so that UTF8_GET_CHAR_SAFE() works right.
+         *
+         * Starting with Unicode 3.0.1, non-shortest forms are illegal.
          */
 
         /* correct sequence - all trail bytes have (b7..b6)==(10)? */
-        if(illegal) {
+        /* illegal is also set if count>=4 */
+        if(illegal || (c)<utf8_minLegal[count]) {
             /* error handling */
             uint8_t errorCount=count;
             /* don't go beyond this sequence */
-            (i)-=count;
+            i=*pi;
             while(count>0 && UTF8_IS_TRAIL(s[i])) {
                 ++(i);
                 --count;
             }
             c=utf8_errorValue[errorCount-count];
-        } else if((strict) &&
-                  (UTF_IS_SURROGATE(c) ||
-                   count>=4 || (c)<utf8_minRegular[count] ||
-                   ((c)&0xfffe)==0xfffe)
-        ) {
+        } else if((strict) && !UTF_IS_UNICODE_CHAR(c)) {
             /* irregular sequence */
             c=utf8_errorValue[count];
         }
@@ -193,7 +192,7 @@ utf8_appendCharSafeBody(uint8_t *s, UTextOffset i, UTextOffset length, UChar32 c
         s+=i;
         offset=0;
         c=utf8_errorValue[length-1];
-        UTF8_APPEND_CHAR_SAFE(s, offset, length, c);
+        UTF8_APPEND_CHAR_UNSAFE(s, offset, c);
         i=i+offset;
      }
     return i;
@@ -226,12 +225,11 @@ utf8_prevCharSafeBody(const uint8_t *s, UTextOffset start, UTextOffset *pi, UCha
                     *pi=i;
                     UTF8_MASK_LEAD_BYTE(b, count);
                     c|=(UChar32)b<<shift;
-                    if( c>0x10ffff ||
-                        (strict &&
-                            (UTF_IS_SURROGATE(c) ||
-                             count>=4 || c<utf8_minRegular[count] || (c&0xfffe)==0xfffe))
-                    ) {
-                        /* irregular sequence */
+                    if(count>=4 || c>0x10ffff || c<utf8_minLegal[count] || (strict && !UTF_IS_UNICODE_CHAR(c))) {
+                        /* illegal or irregular sequence */
+                        if(count>=4) {
+                            count=3;
+                        }
                         c=utf8_errorValue[count];
                     } else {
                         /* exit with correct c */
@@ -281,7 +279,7 @@ utf8_back1SafeBody(const uint8_t *s, UTextOffset start, UTextOffset i) {
     }
 
     /* return I if the sequence starting there is long enough to include i */
-    for(;;) {
+    do {
         b=s[I];
         if((uint8_t)(b-0x80)>=0x7e) { /* not 0x80<=b<0xfe */
             break;
@@ -291,12 +289,8 @@ utf8_back1SafeBody(const uint8_t *s, UTextOffset start, UTextOffset i) {
             } else {
                 break;
             }
-        } else if(Z<I) {
-            --I;
-        } else {
-            break;
         }
-    }
+    } while(Z<=--I);
 
     /* return i itself to be consistent with the FWD_1 macro */
     return i;
