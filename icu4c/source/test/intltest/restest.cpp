fd6b426b7100beb8580c08ac13211c152f081864
George Rhoten <grhoten@users.noreply.github.com>
2001-10-19 01:48:54 +0000

    [ICU-1211] Return 44.0 instead of just 44 X-Commit-URL: https://ssl.icu-project.org/trac/changeset/6319 Former-commit-id: c1bb4803487fc162df13178473288bf6412a0772

--- icu4c/source/test/intltest/restest.cpp
+++ icu4c/source/test/intltest/restest.cpp
@@ -291,9 +291,9 @@ ResourceBundleTest::TestConstruction()
         char *versionID1 = new char[1+strlen(version1)]; // + 1 for zero byte
         char *versionID2 = new char[1+ strlen(version2)]; // + 1 for zero byte
 
-        strcpy(versionID1, "44");  // hardcoded, please change if the default.txt file or ResourceBundle::kVersionSeparater is changed.
+        strcpy(versionID1, "44.0");  // hardcoded, please change if the default.txt file or ResourceBundle::kVersionSeparater is changed.
 
-        strcpy(versionID2, "55");  // hardcoded, please change if the te_IN.txt file or ResourceBundle::kVersionSeparater is changed.
+        strcpy(versionID2, "55.0");  // hardcoded, please change if the te_IN.txt file or ResourceBundle::kVersionSeparater is changed.
 
         logln(UnicodeString("getVersionNumber on default.txt returned ") + version1);
         logln(UnicodeString("getVersionNumber on te_IN.txt returned ") + version2);
