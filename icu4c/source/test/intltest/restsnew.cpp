c551cb4c3bbddf3d75f34cdb2550928e07af2579
George Rhoten <grhoten@users.noreply.github.com>
2001-10-19 01:48:54 +0000

    ICU-1211 Return 44.0 instead of just 44
    X-SVN-Rev: 6319

--- icu4c/source/test/intltest/restsnew.cpp
+++ icu4c/source/test/intltest/restsnew.cpp
@@ -293,9 +293,9 @@ NewResourceBundleTest::TestConstruction()
         char *versionID1 = new char[1 + strlen(U_ICU_VERSION) + strlen(version1)]; // + 1 for zero byte
         char *versionID2 = new char[1 + strlen(U_ICU_VERSION) + strlen(version2)]; // + 1 for zero byte
 
-        strcpy(versionID1, "44");  // hardcoded, please change if the default.txt file or ResourceBundle::kVersionSeparater is changed.
+        strcpy(versionID1, "44.0");  // hardcoded, please change if the default.txt file or ResourceBundle::kVersionSeparater is changed.
 
-        strcpy(versionID2, "55");  // hardcoded, please change if the te_IN.txt file or ResourceBundle::kVersionSeparater is changed.
+        strcpy(versionID2, "55.0");  // hardcoded, please change if the te_IN.txt file or ResourceBundle::kVersionSeparater is changed.
 
         logln(UnicodeString("getVersionNumber on default.txt returned ") + version1 + UnicodeString(" Expect: " ) + versionID1);
         logln(UnicodeString("getVersionNumber on te_IN.txt returned ") + version2 + UnicodeString(" Expect: " ) + versionID2);
