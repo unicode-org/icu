0d85c6fd9add4ec5a64131f238a5d8d0b47b12a7
GCL Shanghai <gclsh1@svn.icu-project.org>
2001-10-19 11:57:54 +0000

    ICU-93 DateFormat test porting and bug fixing
    X-SVN-Rev: 6330

--- icu4j/src/com/ibm/icu/dev/test/format/IntlTestDateFormat.java
+++ icu4j/src/com/ibm/icu/dev/test/format/IntlTestDateFormat.java
@@ -1,257 +1,270 @@
-package com.ibm.icu.test.format;
-
-/*
- *******************************************************************************
- * Copyright (C) 2001, International Business Machines Corporation and    
- * others. All Rights Reserved.                                                
- *******************************************************************************
- *
+/***************************************************************************************
  * $Source: /xsrl/Nsvn/icu/icu4j/src/com/ibm/icu/dev/test/format/IntlTestDateFormat.java,v $ 
- * $Date: 2001/07/23 09:32:46 $ 
- * $Revision: 1.1 $
+ * $Date: 2001/10/19 11:57:54 $ 
+ * $Revision: 1.2 $
  *
  *****************************************************************************************
  */
- 
- // Porting Notice1: from ICU4C v1.8.1
- // Porting Notice2: from format -> DateFormat -> IntlTestDateFormat
+
+/** 
+ * Port From:   JDK 1.4b1 : java.text.Format.IntlTestDateFormat
+ * Source File: java/text/format/IntlTestDateFormat.java
+ **/
+
+/*
+    @test 1.4 98/03/06
+    @summary test International Date Format
+*/
+/*
+(C) Copyright Taligent, Inc. 1996, 1997 - All Rights Reserved
+(C) Copyright IBM Corp. 1996, 1997, 2001 - All Rights Reserved
+
+  The original version of this source code and documentation is copyrighted and
+owned by Taligent, Inc., a wholly-owned subsidiary of IBM. These materials are
+provided under terms of a License Agreement between Taligent and Sun. This
+technology is protected by multiple US and International patents. This notice and
+attribution to Taligent may not be removed.
+  Taligent is a registered trademark of Taligent, Inc.
+*/
+
+package com.ibm.icu.test.format;
 
 import com.ibm.text.*;
-import com.ibm.test.TestFmwk;
 import com.ibm.util.*;
+import java.text.FieldPosition;
+import java.text.ParseException;
+import java.util.Locale;
+import java.util.Random;
+import java.util.Date;
 
 public class IntlTestDateFormat extends com.ibm.test.TestFmwk {
-	public DateFormat fDateFormat;
-	public int fLimit;
-	public java.lang.String fTestName;
-/**
- * IntlTestDateFormat constructor comment.
- */
-protected IntlTestDateFormat() {
-	super();
-	fDateFormat = DateFormat.getInstance();
-	fLimit = 3;
-	fTestName = "Generic test (Default Locale)";
+    // Values in milliseconds (== Date)
+    private static final long ONESECOND = 1000;
+    private static final long ONEMINUTE = 60 * ONESECOND;
+    private static final long ONEHOUR = 60 * ONEMINUTE;
+    private static final long ONEDAY = 24 * ONEHOUR;
+    private static final double ONEYEAR = 365.25 * ONEDAY; // Approximate
+
+    // EModes
+    private static final byte GENERIC = 0;
+    private static final byte TIME = GENERIC + 1;
+    private static final byte DATE = TIME + 1;
+    private static final byte DATE_TIME = DATE + 1;
+
+    private DateFormat fFormat = DateFormat.getInstance();
+    private String fTestName = new String("getInstance");
+    private int fLimit = 3; // How many iterations it should take to reach convergence
+    
+    public IntlTestDateFormat() {
+        //Constructure
+    }
+    
+    public static void main(String[] args) throws Exception {
+        new IntlTestDateFormat().run(args);
+    }
+
+    public void TestLocale() {
+        localeTest(Locale.getDefault(), "Default Locale");
+    }
+
+    // This test does round-trip testing (format -> parse -> format -> parse -> etc.) of DateFormat.
+    public void localeTest(final Locale locale, final String localeName) {
+        int timeStyle, dateStyle;
+
+        // For patterns including only time information and a timezone, it may take
+        // up to three iterations, since the timezone may shift as the year number
+        // is determined.  For other patterns, 2 iterations should suffice.
+        fLimit = 3;
+
+        for(timeStyle = 0; timeStyle < 4; timeStyle++) {
+            fTestName = new String("Time test " + timeStyle + " (" + localeName + ")");
+            try {
+                fFormat = DateFormat.getTimeInstance(timeStyle, locale);
+            }
+            catch(StringIndexOutOfBoundsException e) {
+                errln("FAIL: localeTest time getTimeInstance exception");
+                throw e;
+            }
+            TestFormat();
+        }
+
+        fLimit = 2;
+
+        for(dateStyle = 0; dateStyle < 4; dateStyle++) {
+            fTestName = new String("Date test " + dateStyle + " (" + localeName + ")");
+            try {
+                fFormat = DateFormat.getDateInstance(dateStyle, locale);
+            }
+            catch(StringIndexOutOfBoundsException e) {
+                errln("FAIL: localeTest date getTimeInstance exception");
+                throw e;
+            }
+            TestFormat();
+        }
+
+        for(dateStyle = 0; dateStyle < 4; dateStyle++) {
+            for(timeStyle = 0; timeStyle < 4; timeStyle++) {
+                fTestName = new String("DateTime test " + dateStyle + "/" + timeStyle + " (" + localeName + ")");
+                try {
+                    fFormat = DateFormat.getDateTimeInstance(dateStyle, timeStyle, locale);
+                }
+                catch(StringIndexOutOfBoundsException e) {
+                    errln("FAIL: localeTest date/time getDateTimeInstance exception");
+                    throw e;
+                }
+                TestFormat();
+            }
+        }
+    }
+
+    public void TestFormat() {
+        if (fFormat == null) {
+            errln("FAIL: DateFormat creation failed");
+            return;
+        }
+        //        logln("TestFormat: " + fTestName);
+        Date now = new Date();
+        tryDate(new Date(0));
+        tryDate(new Date((long) 1278161801778.0));
+        tryDate(now);
+        // Shift 6 months into the future, AT THE SAME TIME OF DAY.
+        // This will test the DST handling.
+        tryDate(new Date(now.getTime() + 6*30*ONEDAY));
+
+        Date limit = new Date(now.getTime() * 10); // Arbitrary limit
+        for (int i=0; i<2; ++i)
+            //            tryDate(new Date(floor(randDouble() * limit)));
+            tryDate(new Date((long) (randDouble() * limit.getTime())));
+    }
+
+    private void describeTest() {
+        if (fFormat == null) {
+            errln("FAIL: no DateFormat");
+            return;
+        }
+
+        // Assume it's a SimpleDateFormat and get some info
+        SimpleDateFormat s = (SimpleDateFormat) fFormat;
+        logln(fTestName + " Pattern " + s.toPattern());
+    }
+
+    private void tryDate(Date theDate) {
+        final int DEPTH = 10;
+        Date[] date = new Date[DEPTH];
+        StringBuffer[] string = new StringBuffer[DEPTH];
+
+        int dateMatch = 0;
+        int stringMatch = 0;
+        boolean dump = false;
+        int i;
+        for (i=0; i<DEPTH; ++i) string[i] = new StringBuffer();
+        for (i=0; i<DEPTH; ++i) {
+            if (i == 0) date[i] = theDate;
+            else {
+                try {
+                    date[i] = fFormat.parse(string[i-1].toString());
+                }
+                catch (ParseException e) {
+                    describeTest();
+                    errln("********** FAIL: Parse of " + string[i-1] + " failed.");
+                    dump = true;
+                    break;
+                }
+            }
+            FieldPosition position = new FieldPosition(0);
+            fFormat.format(date[i], string[i], position);
+            if (i > 0) {
+                if (dateMatch == 0 && date[i] == date[i-1]) dateMatch = i;
+                else if (dateMatch > 0 && date[i] != date[i-1]) {
+                    describeTest();
+                    errln("********** FAIL: Date mismatch after match.");
+                    dump = true;
+                    break;
+                }
+                if (stringMatch == 0 && string[i] == string[i-1]) stringMatch = i;
+                else if (stringMatch > 0 && string[i] != string[i-1]) {
+                    describeTest();
+                    errln("********** FAIL: String mismatch after match.");
+                    dump = true;
+                    break;
+                }
+            }
+            if (dateMatch > 0 && stringMatch > 0) break;
+        }
+        if (i == DEPTH) --i;
+
+        if (stringMatch > fLimit || dateMatch > fLimit) {
+            describeTest();
+            errln("********** FAIL: No string and/or date match within " + fLimit + " iterations.");
+            dump = true;
+        }
+
+        if (dump) {
+            for (int k=0; k<=i; ++k) {
+                logln("" + k + ": " + date[k] + " F> " + string[k] + " P> ");
+            }
+        }
+    }
+
+    // Return a random double from 0.01 to 1, inclusive
+    private double randDouble() {
+        // Assume 8-bit (or larger) rand values.  Also assume
+        // that the system rand() function is very poor, which it always is.
+        //        double d;
+        //        int i;
+        //        do {
+        //            for (i=0; i < sizeof(double); ++i)
+        //            {
+        //                char poke = (char*)&d;
+        //                poke[i] = (rand() & 0xFF);
+        //            }
+        //        } while (TPlatformUtilities.isNaN(d) || TPlatformUtilities.isInfinite(d));
+
+        //        if (d < 0.0) d = -d;
+        //        if (d > 0.0)
+        //        {
+        //            double e = floor(log10(d));
+        //            if (e < -2.0) d *= pow(10.0, -e-2);
+        //            else if (e > -1.0) d /= pow(10.0, e+1);
+        //        }
+        //        return d;
+        Random rand = new Random();
+        return rand.nextDouble();
+    }
+
+    public void TestAvailableLocales() {
+        final Locale[] locales = DateFormat.getAvailableLocales();
+        long count = locales.length;
+        logln("" + count + " available locales");
+        if (locales != null  &&  count != 0) {
+            StringBuffer all = new StringBuffer();
+            for (int i=0; i<count; ++i) {
+                if (i!=0) all.append(", ");
+                all.append(locales[i].getDisplayName());
+            }
+            logln(all.toString());
+        }
+        else errln("********** FAIL: Zero available locales or null array pointer");
+    }
+
+    /* This test is too slow; we disable it for now
+    public void TestMonster() {
+        final Locale[] locales = DateFormat.getAvailableLocales();
+        long count = locales.length;
+        if (locales != null  &&  count != 0) {
+            for (int i=0; i<count; ++i) {
+                String name = locales[i].getDisplayName();
+                logln("Testing " + name + "...");
+                try {
+                    localeTest(locales[i], name);
+                }
+                catch(Exception e) {
+                    errln("FAIL: TestMonster localeTest exception" + e);
+                }
+            }
+        }
+    }
+    */
 }
-/**
- * Insert the method's description here.
- * Creation date: (7/23/2001 11:29:05 AM)
- */
-public void describeTest() {
-	SimpleDateFormat s = (SimpleDateFormat)	fDateFormat;
-	logln(fTestName + s.toPattern());	
-}
-/**
- * Insert the method's description here.
- * Creation date: (7/23/2001 11:36:52 AM)
- * @param locale java.util.Locale
- * @param localeName java.lang.String
- */
-public void localeTest(final java.util.Locale locale, final String localeName) {
-	//an array of Style option
-	int[] stylePattern = {DateFormat.FULL, DateFormat.LONG, DateFormat.MEDIUM, DateFormat.SHORT};
-		
-	fLimit = 3;	
-	for (int i = 0; i < 4; i++) {
-		fTestName = "Time Test" +  i + " (" + localeName + ")";
-		fDateFormat = DateFormat.getTimeInstance(stylePattern[i], locale);
-		testFormat();
-	}
 
-	fLimit = 2;	
-	for (int i = 0; i < 4; i++) {
-		fTestName = "Date Test" +  i + " (" + localeName + ")";
-		fDateFormat = DateFormat.getDateInstance(stylePattern[i], locale);
-		testFormat();
-	}
-
-	fLimit = 1;	
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			fTestName = "Date Test" +  i + "/" + j + " (" + localeName + ")";
-			fDateFormat = DateFormat.getDateTimeInstance(stylePattern[i], stylePattern[j], locale);
-			testFormat();
-		}
-	}	
-}
-/**
- * Insert the method's description here.
- * Creation date: (7/23/2001 11:30:06 AM)
- * @param args java.lang.String[]
- */
-public static void main(String[] args) throws Exception {
-	new IntlTestDateFormat().run(args);
-}
-/**
- * Insert the method's description here.
- * Creation date: (7/23/2001 11:39:20 AM)
- */
-public void testAvailableLocales() {
-	final java.util.Locale[] locale = DateFormat.getAvailableLocales();
-	int count = locale.length;
-	logln(count + " available locales");
-
-	if (count != 0) {
-		String all = "";
-		for (int i = 0; i < count; i++) {
-			if (i != 0) {
-				all += ", ";
-			}
-			all += locale[i].getDisplayName();
-		}
-		logln(all);	
-	} else {
-		 errln("**** FAIL: Zero available locales or null array pointer");
-	}
-}
-/**
- * Insert the method's description here.
- * Creation date: (7/23/2001 11:31:18 AM)
- */
-public void testFormat() {
-	if (fDateFormat == null) {
-		errln("FAIL: DateForamt create failed, DateForamt.getInstance()");
-		return;	
-	}
-	
-	describeTest();
-
-	Calendar calendar = Calendar.getInstance();
-	java.util.Date now = calendar.getTime();
-
-	tryDate(new java.util.Date(0));
-	tryDate(new java.util.Date((long)1278161801778.0));
-	tryDate(new java.util.Date((long)5264498352317.0)); // Sunday, October 28, 2136 8:39:12 AM PST
-	tryDate(now);
-	//System.out.println(new java.util.Date((long)5264498352317.0));	
-}
-/**
- * Insert the method's description here.
- * Creation date: (7/23/2001 11:33:54 AM)
- */
-public void testLocale() {
-//an array of Style option
-	int[] stylePattern = {DateFormat.FULL, DateFormat.LONG, DateFormat.MEDIUM, DateFormat.SHORT};
-	
-	final java.util.Locale locale = java.util.Locale.getDefault();
-	final String localeName = "Default Locale";
-	
-	fLimit = 3;	
-	for (int i = 0; i < 4; i++) {
-		fTestName = "Time Test" +  i + " (" + localeName + ")";
-		fDateFormat = DateFormat.getTimeInstance(stylePattern[i], locale);
-		testFormat();
-	}
-
-	fLimit = 2;	
-	for (int i = 0; i < 4; i++) {
-		fTestName = "Date Test" +  i + " (" + localeName + ")";
-		fDateFormat = DateFormat.getDateInstance(stylePattern[i], locale);
-		testFormat();
-	}
-
-	fLimit = 1;	
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			fTestName = "Date Test" +  i + "/" + j + " (" + localeName + ")";
-			fDateFormat = DateFormat.getDateTimeInstance(stylePattern[i], stylePattern[j], locale);
-			testFormat();
-		}
-	}		
-}
-/**
- * Insert the method's description here.
- * Creation date: (7/23/2001 11:34:31 AM)
- */
-public void testMonster() {
-	final java.util.Locale[] locale = DateFormat.getAvailableLocales();
-	int count = locale.length;
-	logln(count + " available locales");
-
-	if (count != 0) {
-		String all = "";
-		
-		count = 3;
-		
-		for (int i = 0; i < count; i++) {
-			String name = locale[i].getDisplayName();
-			logln("Testing " + name + "...");
-			localeTest(locale[i], name);
-		}		
-	} else {
-		 errln("**** FAIL: Zero available locales or null array pointer");
-	}
-}
-/**
- * Insert the method's description here.
- * Creation date: (7/23/2001 11:32:56 AM)
- * @param theDate java.util.Date
- */
-public void tryDate(java.util.Date theDate) {
-	final int DEPTH = 10;
-	java.util.Date[] date = new java.util.Date[DEPTH];
-	String[] str = new String[DEPTH];
-
-	int dateMatch = 0;
-	int stringMatch = 0;
-	boolean dump = false;
-	int i;
-
-	date[0] = theDate;
-	str[0] = fDateFormat.format(date[0]);
-
-	for (i = 1; i < DEPTH; i++) {
-		try {
-			date[i] = fDateFormat.parse(str[i - 1]);
-		} catch (java.text.ParseException pe) {
-			describeTest();
-			errln("**** FAIL: Parse of " + str[i-1] + " failed.");
-			dump = true;
-			break;
-		}
-		str[i] = fDateFormat.format(date[i]);
-		
-		if (dateMatch == 0 && date[i] == date[i - 1]) {
-			dateMatch = i;
-		}else if (dateMatch > 0 && date[i] != date[i - 1]) {
-			describeTest();
-			errln("**** FAIL: Date mismatch after match for " + str[i]);
-			dump = true;
-			break;
-		}
-		
-		if (stringMatch == 0 && str[i] == str[i - 1]) {
-			stringMatch = i;
-		} else if (stringMatch > 0 && str[i] != str[i - 1]) {
-			describeTest();
-			errln("**** FAIL: String mismatch after match for " + str[i]);
-			dump = true;
-			break;
-		}
-		if (dateMatch > 0 && stringMatch > 0) {
-			break;
-		}
-	}
-	
-	if (i == DEPTH) {
-		--i;
-	}
-
-	if (stringMatch > fLimit || dateMatch > fLimit) {
-		describeTest();
-		errln("**** FAIL: No string and/or date match within " + fLimit
-			+ " iterations for the Date " + str[0] + "\t(" + theDate + ").");
-		dump = true;
-	}
-
-	if (dump)
-	{
-		for (int k=0; k<=i; ++k)
-		{
-			logln("" + k + ": " + date[k] + " F> " +
-				  str[k] + " P> ");
-		}
-	}
-
-}
-}
+//eof
