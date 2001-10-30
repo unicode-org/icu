8513e76562bac70528c0761dea5417344ecfd8aa
George Rhoten <grhoten@users.noreply.github.com>
2001-10-30 23:41:05 +0000

    ICU-1407 Properly fixed mmap checking (I think)
    X-SVN-Rev: 6502

--- icu4c/source/common/umapfile.c
+++ icu4c/source/common/umapfile.c
@@ -46,7 +46,7 @@
 #   define MAP_IMPLEMENTATION MAP_WIN32
 
 /* ### Todo: auto detect mmap(). Until then, just add your platform here. */
-#elif U_HAVE_MMAP
+#elif U_HAVE_MMAP || defined(AIX) || defined(HPUX) || defined(OS390) || defined(PTX)
     typedef size_t MemoryMap;
 
 #   define IS_MAP(map) ((map)!=0)
