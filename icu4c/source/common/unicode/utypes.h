296d55d29a3b7659e76708b22d73605120f18bb4
George Rhoten <grhoten@users.noreply.github.com>
2001-10-31 17:55:10 +0000

    [ICU-1075] Change the data entrypoint name to make it the same as Windows. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/6520 Former-commit-id: 7b88ab1767e5c64979d518c9c12123a8649d25bb

--- icu4c/source/common/unicode/utypes.h
+++ icu4c/source/common/unicode/utypes.h
@@ -147,25 +147,10 @@
  *                              the literal text U_ICU_VERSION_MAJOR_NUM into the name.
  *                              The net result will be something of the form
  *                                  #define U_ICU_ENTRY_POINT icudt19_dat
- *                              Note & TODO:  On Unixes, the name includes the endianness letter
- *                                  e.g. icudt19l_dat.  It should be removed, which will
- *                                  involve doing something in the area of the data build.
  */
-#ifdef WIN32
-#pragma warning( disable : 4003 )  /* Disable 'not enough actual parameters for macro' warning  */
 #define U_ICUDATA_ENTRY_POINT  U_DEF2_ICUDATA_ENTRY_POINT(U_ICU_VERSION_MAJOR_NUM, U_ICU_VERSION_MINOR_NUM) 
-#else
-#define U_ICUDATA_ENTRY_POINT  U_DEF2_ICUDATA_ENTRY_POINT(U_ICU_VERSION_MAJOR_NUM, U_ICU_VERSION_MINOR_NUM, U_ICUDATA_TYPE_LITLETTER)
-#endif
-#define U_DEF2_ICUDATA_ENTRY_POINT(major, minor, letter) U_DEF_ICUDATA_ENTRY_POINT(major, minor, letter)
-#define U_DEF_ICUDATA_ENTRY_POINT(major, minor, letter) icudt##major##minor##letter##_dat
-
-#if 0
-#define FOO(X) STRINGIZE(X)
-#define BAR FOO(U_ICUDATA_ENTRY_POINT)
-#define STRINGIZE(X) #X
-#pragma message("icu data entrypoint name is " BAR)
-#endif 
+#define U_DEF2_ICUDATA_ENTRY_POINT(major, minor) U_DEF_ICUDATA_ENTRY_POINT(major, minor)
+#define U_DEF_ICUDATA_ENTRY_POINT(major, minor) icudt##major##minor##_dat
 
 /**
  * \def U_CALLCONV
