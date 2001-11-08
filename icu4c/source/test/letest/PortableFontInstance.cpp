a0937aa83eb0e6f172653285ca98ac6406dff484
Eric Mader <emader@svn.icu-project.org>
2001-11-08 00:22:25 +0000

    [ICU-1253] fixed Copyright notices, and typos in the readme file. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/6683 Former-commit-id: 5949c38821a8b76ab1a881a9091581ab06738345

--- icu4c/source/test/letest/PortableFontInstance.cpp
+++ icu4c/source/test/letest/PortableFontInstance.cpp
@@ -1,23 +1,14 @@
 /*
- * @(#)PortableFontInstance.cpp	1.2 99/12/14
+ *******************************************************************************
  *
- * (C) Copyright IBM Corp. 1998 - All Rights Reserved
+ *   Copyright (C) 1999-2001, International Business Machines
+ *   Corporation and others.  All Rights Reserved.
  *
- * Portions Copyright 1998 by Sun Microsystems, Inc.,
- * 901 San Antonio Road, Palo Alto, California, 94303, U.S.A.
- * All rights reserved.
+ *******************************************************************************
+ *   file name:  PortableFontInstance.cpp
  *
- * This software is the confidential and proprietary information
- * of Sun Microsystems, Inc. ("Confidential Information").  You
- * shall not disclose such Confidential Information and shall use
- * it only in accordance with the terms of the license agreement
- * you entered into with Sun.
- *
- * The original version of this source code and documentation is
- * copyrighted and owned by IBM. These materials are provided
- * under terms of a License Agreement between IBM and Sun.
- * This technology is protected by multiple US and International
- * patents. This notice and attribution to IBM may not be removed.
+ *   created on: 11/22/1999
+ *   created by: Eric R. Mader
  */
 
 #include <stdio.h>
