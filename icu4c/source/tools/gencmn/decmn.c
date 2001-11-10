29a0fc485413f2143971825a03a982a012baee8f
Yves Arrouye <yves@svn.icu-project.org>
2001-11-10 05:32:54 +0000

    [ICU-1475] implement the -n; -C, --comment; and -d, --destination for decmn. Revamp the man page accordingly. Produce output that is compatible with gencmn. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/6735 Former-commit-id: b67c1203f2090da18234062be3324eae0c7ac463

--- icu4c/source/tools/gencmn/decmn.c
+++ icu4c/source/tools/gencmn/decmn.c
@@ -12,6 +12,7 @@
 *
 *   created on: 2001mar05
 *   created by: Markus W. Scherer
+*   changes by: Yves Arrouye
 *
 *   This tool takes an ICU common data file (icuxyz.dat),
 *   outputs a list of components,
@@ -23,8 +24,20 @@
 #include <stdlib.h>
 #include "unicode/utypes.h"
 #include "unicode/udata.h"
+#include "uoptions.h"
+#include "cstring.h"
 
 static uint8_t buffer[100000], buffer2[128*1024];
+
+static const char *pname;
+
+static UOption options[]={
+/*0*/ UOPTION_HELP_H,
+/*1*/ UOPTION_HELP_QUESTION_MARK,
+/*2*/ UOPTION_DESTDIR,
+/*3*/ UOPTION_DEF(0, 'n', UOPT_NO_ARG),
+/*4*/ UOPTION_DEF("comment", 'C', UOPT_NO_ARG),
+};
 
 static int
 compareFiles(const void *file1, const void *file2) {
@@ -38,18 +51,27 @@ compareFiles(const void *file1, const void *file2) {
 }
 
 static int
-copyFile(FILE *in, int32_t offset, int32_t size, const char *name) {
+copyFile(FILE *in, int32_t offset, int32_t size, const char *dir, const char *name) {
     FILE *out;
     int32_t length;
+    char path[512], *p;
 
     if(0!=fseek(in, offset, SEEK_SET)) {
-        fprintf(stderr, "error: cannot seek to position %ld for file \"%s\"\n",
+        fprintf(stderr, "%s: cannot seek to position %ld for file \"%s\"\n", pname,
             (long)offset, name);
         return 4;
     }
-    out=fopen(name, "wb");
+
+    uprv_strcpy(path, dir);
+    p = path + strlen(path);
+    if (p[-1] != U_FILE_SEP_CHAR) {
+	*p++ = U_FILE_SEP_CHAR;
+    }
+    uprv_strcpy(p, name);
+ 
+    out=fopen(path, "wb");
     if(out==NULL) {
-        fprintf(stderr, "error: unable to open output file \"%s\"\n", name);
+        fprintf(stderr, "%s: unable to open output file \"%s\"\n", pname, path);
         return 5;
     }
 
@@ -57,12 +79,12 @@ copyFile(FILE *in, int32_t offset, int32_t size, const char *name) {
     while(size>sizeof(buffer2)) {
         length=(int32_t)fread(buffer2, 1, sizeof(buffer2), in);
         if(length<=0) {
-            fprintf(stderr, "error: read error while copying output file \"%s\"\n", name);
+            fprintf(stderr, "%s: read error while copying output file \"%s\"\n", pname, path);
             fclose(out);
             return 4;
         }
         if(length!=(int32_t)fwrite(buffer2, 1, length, out)) {
-            fprintf(stderr, "error: write error while copying output file \"%s\"\n", name);
+            fprintf(stderr, "%s: write error while copying output file \"%s\"\n", pname, path);
             fclose(out);
             return 5;
         }
@@ -71,12 +93,12 @@ copyFile(FILE *in, int32_t offset, int32_t size, const char *name) {
     while(size>0) {
         length=(int32_t)fread(buffer2, 1, size, in);
         if(length<=0) {
-            fprintf(stderr, "error: read error while copying output file \"%s\"\n", name);
+            fprintf(stderr, "%s: read error while copying output file \"%s\"\n", pname, path);
             fclose(out);
             return 4;
         }
         if(length!=(int32_t)fwrite(buffer2, 1, length, out)) {
-            fprintf(stderr, "error: write error while copying output file \"%s\"\n", name);
+            fprintf(stderr, "%s: write error while copying output file \"%s\"\n", pname, path);
             fclose(out);
             return 5;
         }
@@ -88,41 +110,59 @@ copyFile(FILE *in, int32_t offset, int32_t size, const char *name) {
 }
 
 extern int
-main(int argc, const char *argv[]) {
+main(int argc, char *argv[]) {
     FILE *in;
     UDataInfo *info;
     uint8_t *base;
     int32_t *p;
     int32_t i, length, count, baseOffset;
-    int result;
+    int result, ishelp = 0;
 
-    if(argc!=2) {
+    pname = uprv_strchr(*argv, U_FILE_SEP_CHAR);
+#ifdef WIN32
+    if (!pname) {
+	pname = uprv_strchr(*argv, '/');
+    }
+#endif
+    if (pname) {
+	++pname;
+    } else {
+	pname = argv[0];
+    }
+
+    options[2].value = ".";
+
+    argc = u_parseArgs(argc, argv, sizeof(options) / sizeof(*options), options);
+    if ((ishelp = options[0].doesOccur || options[1].doesOccur) || argc != 2) {
         fprintf(stderr,
-                "usage: %s icu-data-filename\n"
-                "  unpackages ICU common data files:\n"
-                "  reads a memory-mappable ICU data file, outputs the list of data pieces to stdout\n"
-                "  and writes the packaged data files as separate files in the current folder.\n",
-                argv[0]);
-        return 1;
+                "%csage: %s [ -h, -?, --help ] [ -n ] [ -C, --comment ] [ -d, --destdir destination ] archive\n", ishelp ? 'U' : 'u', pname);
+	if (ishelp) {
+	    fprintf(stderr, "\nOptions: -h, -?, --help    print this message and exit\n"
+		    "         -n                do not create files\n"
+		    "         -C, --comment     print the comment embedded in the file and exit\n"
+		    "         -d, --destdir destination    create files in destination\n");
+	}
+
+        return ishelp ? 0 : 1;
     }
 
     in=fopen(argv[1], "rb");
     if(in==NULL) {
-        fprintf(stderr, "error: unable to open input file \"%s\"\n", argv[1]);
+        fprintf(stderr, "%s: unable to open input file \"%s\"\n", pname, argv[1]);
         return 2;
     }
 
     /* read the beginning of the file */
     length=(int32_t)fread(buffer, 1, sizeof(buffer), in);
     if(length<20) {
-        fprintf(stderr, "error: input file too short\n");
+        fprintf(stderr, "%s: input file too short\n", pname);
         fclose(in);
         return 3;
     }
 
     /* check the validity of the file */
     if(buffer[2]!=0xda || buffer[3]!=0x27) {
-        fprintf(stderr, "error: not an ICU data file\n");
+        fprintf(stderr, "%s: not an ICU data file\n", pname);
         fclose(in);
         return 3;
     }
@@ -130,34 +170,35 @@ main(int argc, const char *argv[]) {
     /* check the platform properties for the file */
     info=(UDataInfo *)(buffer+4);
     if(info->isBigEndian!=U_IS_BIG_ENDIAN) {
-        fprintf(stderr, "error: the file is in the opposite byte endianness\n");
+        fprintf(stderr, "%s: the file is in the wrong byte endianness\n", pname);
         fclose(in);
         return 3;
     }
     if(info->charsetFamily!=U_CHARSET_FAMILY) {
-        fprintf(stderr, "error: the file is not built for this machine's charset family\n");
+        fprintf(stderr, "%s: the file is not built for this machine's charset family\n", pname);
         fclose(in);
         return 3;
     }
 
     /* check that this is a common data file */
     if(info->dataFormat[0]!=0x43 || info->dataFormat[1]!=0x6d || info->dataFormat[2]!=0x6e || info->dataFormat[3]!=0x44) {
-        fprintf(stderr, "error: this file is not a common data file\n");
+        fprintf(stderr, "%s: this file is not a common data (archive) file\n", pname);
         fclose(in);
         return 3;
     }
 
     /* check for version 1 */
     if(info->formatVersion[0]!=1) {
-        fprintf(stderr, "error: the format version %d.%d.%d.%d is not known\n",
+        fprintf(stderr, "%s: the format version %d.%d.%d.%d is not known\n", pname,
                 info->formatVersion[0], info->formatVersion[1], info->formatVersion[2], info->formatVersion[3]);
         fclose(in);
         return 3;
     }
 
-    /* is there a comment? */
-    if(*(uint16_t *)buffer>4+info->size) {
-        printf("comment=%s\n", buffer+4+info->size);
+    /* do we want to show the comment, and is there a comment? */
+    if (options[4].doesOccur && *(uint16_t *)buffer>4+info->size) {
+        printf("%s\n", buffer+4+info->size);
+	return 0;
     }
 
     /* output all filenames */
@@ -165,12 +206,16 @@ main(int argc, const char *argv[]) {
     base=buffer+baseOffset;
     p=(int32_t *)base;
     count=*p++;
-    printf("files[%ld]\n", (long)count);
+    /* printf("files[%ld]\n", (long)count); */
     for(i=0; i<count; ++i) {
-        printf("file[%ld]=%s\n", (long)i, base+*p);
+        printf("%s\n", base+*p);
         p+=2;
     }
-    puts("endfiles");
+    /* puts("endfiles"); */
+
+    if (options[3].doesOccur) {	/* Do not extract. */
+	return 0;
+    }
 
     /* sort all files by their offsets in the common file */
     qsort(base+4, count, 8, compareFiles);
@@ -180,7 +225,7 @@ main(int argc, const char *argv[]) {
     --count;
     for(i=0; i<count; ++i) {
         /* the size is the difference between this file's offset and the next one's */
-        result=copyFile(in, baseOffset+p[1], p[3]-p[1], (const char *)(base+*p));
+        result=copyFile(in, baseOffset+p[1], p[3]-p[1], options[2].value, (const char *)(base+*p));
         if(result!=0) {
             fclose(in);
             return result;
@@ -192,10 +237,10 @@ main(int argc, const char *argv[]) {
     if(count>=0) {
         /* the size is the number of bytes to the end of the common file */
         if(0!=fseek(in, 0, SEEK_END)) {
-            fprintf(stderr, "error: unable to seek to the end of the common file\n");
+            fprintf(stderr, "%s: unable to seek to the end of the common file\n", pname);
             return 4;
         }
-        result=copyFile(in, baseOffset+p[1], (int32_t)ftell(in)-baseOffset-p[1], (const char *)(base+*p));
+        result=copyFile(in, baseOffset+p[1], (int32_t)ftell(in)-baseOffset-p[1], options[2].value, (const char *)(base+*p));
         if(result!=0) {
             fclose(in);
             return result;
