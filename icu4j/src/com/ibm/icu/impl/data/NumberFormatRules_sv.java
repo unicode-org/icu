3e81f4eba7d0f3d5c3540cbbac6dff7e3d935a30
Doug Felt <dougfelt@gmail.com>
2001-11-12 20:02:26 +0000

    ICU-1442 update to Swedish number spellout
    X-SVN-Rev: 6793

--- icu4j/src/com/ibm/icu/impl/data/NumberFormatRules_sv.java
+++ icu4j/src/com/ibm/icu/impl/data/NumberFormatRules_sv.java
@@ -5,8 +5,8 @@
  *******************************************************************************
  *
  * $Source: /xsrl/Nsvn/icu/icu4j/src/com/ibm/icu/impl/data/Attic/NumberFormatRules_sv.java,v $ 
- * $Date: 2000/03/10 04:07:29 $ 
- * $Revision: 1.2 $
+ * $Date: 2001/11/12 20:02:26 $ 
+ * $Revision: 1.3 $
  *
  *****************************************************************************************
  */
@@ -19,7 +19,7 @@ import java.util.ListResourceBundle;
  * RuleBasedNumberFormat data for Swedish
  *
  * @author Richard Gillam
- * @version $Version$ $Date: 2000/03/10 04:07:29 $
+ * @version $Version$ $Date: 2001/11/12 20:02:26 $
  */
 public class NumberFormatRules_sv extends ListResourceBundle {
     /**
@@ -34,9 +34,18 @@ public class NumberFormatRules_sv extends ListResourceBundle {
     Object[][] contents = {
         /**
          * Spellout rules for Swedish.
+         * Thanks to Kent Karlsson for cleaning up these rules.
          */
         { "SpelloutRules",
-            "noll; ett; tv\u00e5; tre; fyra; fem; sex; sjo; \u00e5tta; nio;\n"
+          "%year:\n"
+            + "=%neutrum=;\n"
+            + "1000>: <%neutrum<hundra[\u00ad>>];\n"
+            + "10,000: =%neutrum=;\n"
+          // the same as default except we change the one's digit, wish there were a better way
+          + "%neutrum:\n"
+            + "-x: minus >>;\n"
+            + "x.x: << komma >>;\n"
+            + "noll; ett; tv\u00e5; tre; fyra; fem; sex; sju; \u00e5tta; nio;\n"
             + "tio; elva; tolv; tretton; fjorton; femton; sexton; sjutton; arton; nitton;\n"
             + "20: tjugo[>>];\n"
             + "30: trettio[>>];\n"
@@ -46,17 +55,62 @@ public class NumberFormatRules_sv extends ListResourceBundle {
             + "70: sjuttio[>>];\n"
             + "80: \u00e5ttio[>>];\n"
             + "90: nittio[>>];\n"
-            + "100: hundra[>>];\n"
-            + "200: <<hundra[>>];\n"
-            + "1000: tusen[ >>];\n"
-            + "2000: << tusen[ >>];\n"
+            + "100: <<hundra[\u00ad>>];\n"
+            + "1000: ettusen[ >>];\n"
+            + "2000: <%default<\u00adtusen[ >>];\n"
             + "1,000,000: en miljon[ >>];\n"
-            + "2,000,000: << miljon[ >>];\n"
+            + "2,000,000: <%default< miljoner[ >>];\n"
             + "1,000,000,000: en miljard[ >>];\n"
-            + "2,000,000,000: << miljard[ >>];\n"
+            + "2,000,000,000: <%default< miljarder[ >>];\n"
             + "1,000,000,000,000: en biljon[ >>];\n"
-            + "2,000,000,000,000: << biljon[ >>];\n"
-            + "1,000,000,000,000,000: =#,##0=" }
-        // can someone supply me with information on negatives and decimals?
+            + "2,000,000,000,000: <%default< biljoner[ >>];\n"
+            + "1,000,000,000,000,000: en triljon[ >>];\n"
+            + "2,000,000,000,000,000: <%default< triljoner[ >>];\n"
+            + "1,000,000,000,000,000,000: =#,##0=;\n"
+          + "%default:\n"
+            + "-x: minus >>;\n"
+            + "x.x: << komma >>;\n"
+            + "noll; en; tv\u00e5; tre; fyra; fem; sex; sju; \u00e5tta; nio;\n"
+            + "tio; elva; tolv; tretton; fjorton; femton; sexton; sjutton; arton; nitton;\n"
+            + "20: tjugo[>>];\n"
+            + "30: trettio[>>];\n"
+            + "40: fyrtio[>>];\n"
+            + "50: femtio[>>];\n"
+            + "60: sextio[>>];\n"
+            + "70: sjuttio[>>];\n"
+            + "80: \u00e5ttio[>>];\n"
+            + "90: nittio[>>];\n"
+            + "100: etthundra[\u00ad>>];\n"
+            + "200: <<hundra[\u00ad>>];\n"
+            + "1000: ettusen[ >>];\n"
+            + "2000: <<\u00adtusen[ >>];\n"
+            + "1,000,000: en miljon[ >>];\n"
+            + "2,000,000: << miljoner[ >>];\n"
+            + "1,000,000,000: en miljard[ >>];\n"
+            + "2,000,000,000: << miljarder[ >>];\n"
+            + "1,000,000,000,000: en biljon[ >>];\n"
+            + "2,000,000,000,000: << biljoner[ >>];\n"
+            + "1,000,000,000,000,000: en triljon[ >>];\n"
+            + "2,000,000,000,000,000: << triljoner[ >>];\n"
+            + "1,000,000,000,000,000,000: =#,##0=;\n"
+/* 
+ * Current implementation can't handle these magnitudes)
+            + "1,000,000,000,000,000,000: en triljard[ >>];\n"
+            + "2,000,000,000,000,000,000: << triljarder[ >>];\n"
+            + "1,000,000,000,000,000,000,000: en kvartiljon[ >>];\n"
+            + "2,000,000,000,000,000,000,000: << kvartiljoner[ >>];\n"
+            + "1,000,000,000,000,000,000,000,000: en kvartiljard[ >>];\n"
+            + "2,000,000,000,000,000,000,000,000: << kvartiljarder[ >>];\n"
+            + "1,000,000,000,000,000,000,000,000,000: en kvintiljon[ >>];\n"
+            + "2,000,000,000,000,000,000,000,000,000: << kvintiljoner[ >>];\n"
+            + "1,000,000,000,000,000,000,000,000,000,000: en kvintiljard[ >>];\n"
+            + "2,000,000,000,000,000,000,000,000,000,000: << kvintiljarder[ >>];\n"
+            + "1,000,000,000,000,000,000,000,000,000,000,000: en sextiljon[ >>];\n"
+            + "2,000,000,000,000,000,000,000,000,000,000,000: << sextiljoner[ >>];\n"
+            + "1,000,000,000,000,000,000,000,000,000,000,000,000: en sextiljard[ >>];\n"
+            + "2,000,000,000,000,000,000,000,000,000,000,000,000: << sextiljarder[ >>];\n"
+            + "1,000,000,000,000,000,000,000,000,000,000,000,000,000: =#,##0="
+*/
+        }
     };
 }
