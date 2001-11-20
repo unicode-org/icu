dde96412788bcef429464a38c6f5f24167947dba
Alan Liu <alansliu@gmail.com>
2001-11-20 00:07:11 +0000

    per Doug's review, move appendNumber to a utility class
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/1533
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@6999 0a8b766c-62c9-45f9-954d-7f2943c9ead0

---
+++ source/i18n/util.cpp
@@ -0,0 +1,49 @@
+/*
+**********************************************************************
+*   Copyright (c) 2001, International Business Machines
+*   Corporation and others.  All Rights Reserved.
+**********************************************************************
+*   Date        Name        Description
+*   11/19/2001  aliu        Creation.
+**********************************************************************
+*/
+
+#include "util.h"
+
+// "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+static const UChar DIGITS[] = {
+    48,49,50,51,52,53,54,55,56,57,
+    65,66,67,68,69,70,71,72,73,74,
+    75,76,77,78,79,80,81,82,83,84,
+    85,86,87,88,89,90
+};
+
+UnicodeString& Utility::appendNumber(UnicodeString& result, int32_t n,
+                                     int32_t radix) {
+    if (radix < 2 || radix > 36) {
+        // Bogus radix
+        return result.append((UChar)63/*?*/);
+    }
+    // Handle negatives
+    if (n < 0) {
+        n = -n;
+        result.append((UChar)45/*-*/);
+    }
+    // First determine the number of digits
+    int32_t nn = n;
+    int32_t r = 1;
+    while (nn >= radix) {
+        nn /= radix;
+        r *= radix;
+    }
+    // Now generate the digits
+    while (r > 0) {
+        int32_t digit = n / r;
+        result.append(DIGITS[digit]);
+        n -= digit * r;
+        r /= radix;
+    }
+    return result;
+}
+
+//eof
