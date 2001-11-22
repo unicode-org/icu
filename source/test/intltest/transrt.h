49e75048d1f7af5040d2a086632a99a2ebf06708
Syn Wee Quek <swquek@svn.icu-project.org>
2001-11-22 02:41:06 +0000

    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/1495
    c++ port of the Java RoundTripTest.
    Please note that the tests are not all passing.
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@7076 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/test/intltest/transrt.h
+++ source/test/intltest/transrt.h
@@ -22,53 +22,17 @@ class TransliteratorRoundTripTest : public IntlTest {
     void runIndexedTest(int32_t index, UBool exec, const char* &name,
                         char* par=NULL);
 
+    void TestKana(void);
     void TestHiragana(void);
     void TestKatakana(void);
-    void TestArabic(void);
-    void TestHebrew(void);
+    void TestJamo(void);
+    void TestHangul(void);
     void TestGreek(void);
+    void TestGreekUNGEGN(void);
     void Testel(void);
     void TestCyrillic(void);
-    void TestJamo(void);
-    void TestJamoHangul(void);
-
     void TestDevanagariLatin(void);
-    void TestDevanagariBengali(void);
-    void TestDevanagariGurmukhi(void);
-    void TestDevanagariGujarati(void);
-    void TestDevanagariOriya(void);
-    void TestDevanagariTamil(void);
-    void TestDevanagariTelugu(void);
-    void TestDevanagariKannada(void);
-    void TestDevanagariMalayalam(void);
-    void TestBengaliGurmukhi(void);
-    void TestBengaliGujarati(void);
-    void TestBengaliOriya(void);
-    void TestBengaliTamil(void);
-    void TestBengaliTelugu(void);
-    void TestBengaliKannada(void);
-    void TestBengaliMalayalam(void);  
-    void TestGurmukhiGujarati(void);
-    void TestGurmukhiOriya(void);
-    void TestGurmukhiTamil(void);
-    void TestGurmukhiTelugu(void);
-    void TestGurmukhiKannada(void);
-    void TestGurmukhiMalayalam(void); 
-    void TestGujaratiOriya(void);
-    void TestGujaratiTamil(void);
-    void TestGujaratiTelugu(void);
-    void TestGujaratiKannada(void);
-    void TestGujaratiMalayalam(void);
-    void TestOriyaTamil(void);
-    void TestOriyaTelugu(void);
-    void TestOriyaKannada(void);
-    void TestOriyaMalayalam(void);
-    void TestTamilTelugu(void);
-    void TestTamilKannada(void);
-    void TestTamilMalayalam(void);
-    void TestTeluguKannada(void);
-    void TestTeluguMalayalam(void);
-    void TestKannadaMalayalam(void);
+    void TestInterIndic(void);
 };
 
 #endif
