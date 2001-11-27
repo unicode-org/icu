f6ab5ba8f700d3efc1c6e8b2296b671dd0993050
Eric Mader <emader@svn.icu-project.org>
2001-11-27 01:35:15 +0000

    [ICU-1289] changed to use ICU coding conventions. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/7094 Former-commit-id: ee293fd733897ae4b750c24327e453919cb52175

--- icu4c/source/extra/scrptrun/srtest.cpp
+++ icu4c/source/extra/scrptrun/srtest.cpp
@@ -5,14 +5,14 @@
  *
  */
 
-#include "layout/LETypes.h"
+#include "unicode/utypes.h"
 #include "unicode/uscript.h"
 
 #include "scrptrun.h"
 
 #include <stdio.h>
 
-LEUnicode testChars[] = {
+UChar testChars[] = {
             0x0020, 0x0946, 0x0939, 0x093F, 0x0928, 0x094D, 0x0926, 0x0940, 0x0020,
             0x0627, 0x0644, 0x0639, 0x0631, 0x0628, 0x064A, 0x0629, 0x0020,
             0x0420, 0x0443, 0x0441, 0x0441, 0x043A, 0x0438, 0x0439, 0x0020,
@@ -22,17 +22,17 @@ LEUnicode testChars[] = {
             0xD801, 0xDC00, 0xD801, 0xDC01, 0xD801, 0xDC02, 0xD801, 0xDC03
 };
 
-le_int32 testLength = sizeof testChars / sizeof testChars[0];
+int32_t testLength = sizeof testChars / sizeof testChars[0];
 
 void main()
 {
     ScriptRun scriptRun(testChars, 0, testLength);
 
     while (scriptRun.next()) {
-        le_int32 start = scriptRun.getScriptStart();
-        le_int32 end   = scriptRun.getScriptEnd();
-        le_int32 code  = scriptRun.getScriptCode();
+        int32_t     start = scriptRun.getScriptStart();
+        int32_t     end   = scriptRun.getScriptEnd();
+        UScriptCode code  = scriptRun.getScriptCode();
 
-		printf("Script '%s' from %d to %d.\n", uscript_getName((UScriptCode) code), start, end);
+		printf("Script '%s' from %d to %d.\n", uscript_getName(code), start, end);
 	}
 }
