4caafdc3d17c57f801b77650ba3920a6d995bb8b
Syn Wee Quek <swquek@svn.icu-project.org>
2001-11-29 18:18:10 +0000

    ICU-1559 Un-hard code the directories paths.
    X-SVN-Rev: 7171

--- icu4j/build.bat
+++ icu4j/build.bat
@@ -1,15 +1,15 @@
 @echo off
 
 REM *******************************************************************************
-REM * Copyright (C) 1997-2000, International Business Machines Corporation and    *
-REM * others. All Rights Reserved.                                                *
+REM * Copyright (C) 1997 - 2001, International Business Machines Corporation and    
+REM * others. All Rights Reserved.                                                
 REM *******************************************************************************
 
 REM $Source: /xsrl/Nsvn/icu/icu4j/Attic/build.bat,v $ 
-REM $Date: 2000/03/15 00:40:27 $ 
-REM $Revision: 1.4 $
+REM $Date: 2001/11/29 18:18:10 $ 
+REM $Revision: 1.5 $
 
 REM *******************************************************************************
 
 REM convience bat file to build with
-java -classpath "build\icu4jtools.jar;jakarta-ant\lib\ant.jar;jakarta-ant\lib\xml.jar;%CLASSPATH%" org.apache.tools.ant.Main %1 %2 %3 %4 %5
+java -classpath "%JAVA_HOME%\lib\tools.jar;%ANT_HOME%\lib\ant.jar;%CLASSPATH%" org.apache.tools.ant.Main %1 %2 %3 %4 %5
