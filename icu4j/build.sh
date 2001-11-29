308bb4da0174d88b58205156d96fdad9bdcbff3c
Syn Wee Quek <swquek@svn.icu-project.org>
2001-11-29 18:18:10 +0000

    [ICU-1559] Un-hard code the directories paths. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/7171 Former-commit-id: 0c5cceabcc5339ab217ef81a3942d5bd06406a81

--- icu4j/build.sh
+++ icu4j/build.sh
@@ -1,26 +1,15 @@
 #/*
 #*******************************************************************************
-#* Copyright (C) 1997-2000, International Business Machines Corporation and    *
-#* others. All Rights Reserved.                                                *
-#*******************************************************************************
+#* Copyright (C) 1997 - 2001, International Business Machines Corporation and    
+#* others. All Rights Reserved.                                           #*******************************************************************************
 #*
 #* $Source: /xsrl/Nsvn/icu/icu4j/Attic/build.sh,v $ 
-#* $Date: 2000/03/15 00:40:27 $ 
-#* $Revision: 1.3 $
+#* $Date: 2001/11/29 18:18:10 $ 
+#* $Revision: 1.4 $
 #*
 #*****************************************************************************************
 #*/
 #!/bin/sh
-
-ADDL_CLASSPATH=build/icu4jtoolz.zip:jakarta-ant/lib/ant.jar:jakarta-ant/lib/xml.jar
-
-if [ "$CLASSPATH" != "" ] ; then
-  CLASSPATH=$CLASSPATH:$ADDL_CLASSPATH
-else
- CLASSPATH=$ADDL_CLASSPATH
-fi
-export CLASSPATH
-
-echo Building with classpath $CLASSPATH
-
-java org.apache.tools.ant.Main $*
+CLASSPATH=$JAVA_HOME/tools.jar:$ANT_HOME/lib/ant.jar
+echo java -classpath $CLASSPATH org.apache.tools.ant.Main $* 
+java -classpath $CLASSPATH org.apache.tools.ant.Main $*
