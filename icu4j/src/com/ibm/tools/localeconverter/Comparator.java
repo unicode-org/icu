074819551395176a04e90583f083245c577ee249
Ram Viswanadha <rviswanadha@gmail.com>
2002-01-31 01:22:25 +0000

    ICU-1678 Add locale converter tool
    X-SVN-Rev: 7538

---
+++ icu4j/src/com/ibm/tools/localeconverter/Comparator.java
@@ -0,0 +1,20 @@
+/*
+ *******************************************************************************
+ * Copyright (C) 2002-2004, International Business Machines Corporation and    *
+ * others. All Rights Reserved.                                                *
+ *******************************************************************************
+ *
+ * $Source: /xsrl/Nsvn/icu/icu4j/src/com/ibm/tools/localeconverter/Attic/Comparator.java,v $ 
+ * $Date: 2002/01/31 01:21:27 $ 
+ * $Revision: 1.1 $
+ *
+ *****************************************************************************************
+ */
+package com.ibm.tools.localeconverter;
+
+public interface Comparator {
+    /**
+        returns 0 if objects are equal, -1 if a is less than b, 1 otherwise.
+    */
+    public int compare(Object a, Object b);
+}
