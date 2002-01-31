2ffde98dd23a7f17986b844e38c728c72eaf4cda
Ram Viswanadha <rviswanadha@gmail.com>
2002-01-31 01:22:25 +0000

    [ICU-1678] Add locale converter tool X-Commit-URL: https://ssl.icu-project.org/trac/changeset/7538 Former-commit-id: a2332ccf06828d276384bd415cb6300dd19350e0

---
+++ icu4j/src/com/ibm/icu/dev/tool/localeconverter/Comparable.java
@@ -0,0 +1,20 @@
+/*
+ *******************************************************************************
+ * Copyright (C) 2002-2004, International Business Machines Corporation and    *
+ * others. All Rights Reserved.                                                *
+ *******************************************************************************
+ *
+ * $Source: /xsrl/Nsvn/icu/icu4j/src/com/ibm/icu/dev/tool/localeconverter/Comparable.java,v $ 
+ * $Date: 2002/01/31 01:21:24 $ 
+ * $Revision: 1.1 $
+ *
+ *****************************************************************************************
+ */
+package com.ibm.tools.localeconverter;
+
+public interface Comparable {
+    /**
+        returns 0 if objects are equal, -1 if a is less than b, 1 otherwise.
+    */
+    public int compareTo(Object b);
+}
