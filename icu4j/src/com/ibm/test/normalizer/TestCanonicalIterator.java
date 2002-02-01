7216e09374c646a9b7450515e3ab1f981d5b68cb
Mark Davis <mark@macchiato.com>
2002-02-01 02:05:35 +0000

    [ICU-1683] added draft versions of two iterators X-Commit-URL: https://ssl.icu-project.org/trac/changeset/7549 Former-commit-id: 5dbf3de1fab424d82ea46e5387715538f68839fc

---
+++ icu4j/src/com/ibm/test/normalizer/TestCanonicalIterator.java
@@ -0,0 +1,70 @@
+/*
+ *******************************************************************************
+ * Copyright (C) 1996-2000, International Business Machines Corporation and    *
+ * others. All Rights Reserved.                                                *
+ *******************************************************************************
+ *
+ * $Source: /xsrl/Nsvn/icu/icu4j/src/com/ibm/test/normalizer/Attic/TestCanonicalIterator.java,v $ 
+ * $Date: 2002/02/01 02:05:35 $ 
+ * $Revision: 1.1 $
+ *
+ *****************************************************************************************
+ */
+package com.ibm.test.normalizer;
+
+import com.ibm.test.*;
+import com.ibm.text.*;
+import com.ibm.util.Utility;
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
+import java.util.*;
+
+// TODO: fit into test framework
+
+public class TestCanonicalIterator {
+    static final String testArray[] = {
+        "Åd\u0307\u0327",
+        "\u010d\u017E",
+        "x\u0307\u0327",
+    };
+
+    public static void main(String[] args) {
+        // set up for readable display
+        Transliterator name = Transliterator.getInstance("name");
+        Transliterator hex = Transliterator.getInstance("hex");
+        
+        // check build
+        UnicodeSet ss = CanonicalIterator.getSafeStart();
+        System.out.println("Safe Start: " + ss.toPattern(true));
+        System.out.println();
+        ss = CanonicalIterator.getStarts('a');
+        System.out.println("Characters with 'a' at the start of their decomposition: " + ss.toPattern(true));
+        
+        // check permute
+        System.out.println(collectionToString(CanonicalIterator.permute("ABC")));
+        
+        // try samples
+        for (int i = 0; i < testArray.length; ++i) {
+            System.out.println();
+            System.out.println("Results for: " + name.transliterate(testArray[i]));
+            CanonicalIterator it = new CanonicalIterator(testArray[i]);
+            int counter = 0;
+            while (true) {
+                String result = it.next();
+                if (result == null) break;
+                System.out.println(++counter + ": " + hex.transliterate(result));
+                System.out.println(" = " + name.transliterate(result));
+            }
+        }
+    }
+    
+    static String collectionToString(Collection col) {
+        StringBuffer result = new StringBuffer();
+        Iterator it = col.iterator();
+        while (it.hasNext()) {
+            if (result.length() != 0) result.append(", ");
+            result.append(it.next().toString());
+        }
+        return result.toString();
+    }
+}
---
+++ icu4j/src/com/ibm/text/UnicodeSetIterator.java
@@ -0,0 +1,119 @@
+/*
+ *******************************************************************************
+ * Copyright (C) 1996-2000, International Business Machines Corporation and    *
+ * others. All Rights Reserved.                                                *
+ *******************************************************************************
+ *
+ * $Source: /xsrl/Nsvn/icu/icu4j/src/com/ibm/text/Attic/UnicodeSetIterator.java,v $ 
+ * $Date: 2002/02/01 02:05:35 $ 
+ * $Revision: 1.1 $
+ *
+ *****************************************************************************************
+ */
+package com.ibm.text;
+
+import com.ibm.util.Utility;
+//import java.text.*;
+import java.util.*;
+import java.io.*;
+
+/**
+ * Class that allows simple iteration over a UnicodeSet.
+ * @author M. Davis
+ * @draft
+ */
+public final class UnicodeSetIterator {
+
+    /**
+     *@set set to iterate over
+     */
+    public UnicodeSetIterator(UnicodeSet set) {
+        reset(set);
+    }
+        
+    /**
+     *@return next character in the set. Returns -1 when done!
+     */
+    public int next() {
+        if (abbreviated) {
+            if (element >= startElement + 50 && element <= endElement - 50) {
+                element = endElement - 50;
+            }
+        }
+        if (element < endElement) {
+            return ++element;
+        }
+        if (range >= endRange) return -1;
+        ++range;
+        endElement = set.getRangeEnd(range);
+        startElement = set.getRangeStart(range);
+        element = set.getRangeStart(range);
+        return element;
+    }
+        
+    /**
+     *@param set the set to iterate over. This allows reuse of the iterator.
+     */
+    public void reset(UnicodeSet set) {
+        this.set = set;
+        endRange = set.getRangeCount() - 1;
+        resetInternal();
+    }
+        
+    /**
+     * Resets to the start, to allow the iteration to start over again.
+     */
+    public void reset() {
+        endRange = set.getRangeCount() - 1;
+        resetInternal();
+    }
+    
+    /**
+     * TODO: Move to UnicodeSet!
+     *@param s the string to test
+     *@return true if and only if no character from s are in the set.
+     */
+    public static boolean containsNone(UnicodeSet set, String s) {
+        int cp;
+        for (int i = 0; i < s.length(); i += UTF16.getCharCount(i)) {
+            cp = UTF16.charAt(s, i);
+            if (set.contains(cp)) return false;
+        }
+        return true;
+    }
+        
+    /**
+     * TODO: Move to UnicodeSet!
+     *@param s the string to test
+     *@return true if and only if all characters from s are in the set.
+     */
+    public static boolean containsAll(UnicodeSet set, String s) {
+        int cp;
+        for (int i = 0; i < s.length(); i += UTF16.getCharCount(i)) {
+            cp = UTF16.charAt(s, i);
+            if (!set.contains(cp)) return false;
+        }
+        return true;
+    }
+    
+    // ======================= PRIVATES ===========================
+    
+    private UnicodeSet set;
+    private int endRange = 0;
+    private int range = 0;
+    private int startElement = 0;
+    private int endElement;
+    private int element;
+    private boolean abbreviated = false;
+        
+    private void resetInternal() {
+        range = 0;
+        endElement = 0;
+        element = 0;            
+        if (endRange >= 0) {
+            element = set.getRangeStart(range);
+            endElement = set.getRangeEnd(range);
+            startElement = set.getRangeStart(range);
+        }
+    }
+}
