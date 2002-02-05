678ede2e8bbdf15e454380def31f17a2e0a05a93
George Rhoten <grhoten@svn.icu-project.org>
2002-02-05 17:43:28 +0000

    Change the converter name for the variant of euc-jp
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/1687
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@7564 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/ucnv.c
+++ source/common/ucnv.c
@@ -64,7 +64,7 @@ typedef struct UAmbiguousConverter {
 static const UAmbiguousConverter ambiguousConverters[]={
     { "ibm-942_P120-2000", 0xa5 },
     { "ibm-943_P130-2000", 0xa5 },
-    { "ibm-33722", 0xa5 },
+    { "ibm-33722_P120-2000", 0xa5 },
     { "ibm-949_P110-2000", 0x20a9 },
     { "ibm-1363_P110-2000", 0x20a9 },
     { "ISO_2022,locale=ko,version=0", 0x20a9 }
