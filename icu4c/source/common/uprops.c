b0d25f3bceedcd9b3fd278b20c8875e6459445e6
Markus Scherer <markus.icu@gmail.com>
2002-02-25 22:43:04 +0000

    ICU-1721 add uprops.c implementing APIs for additional UCD properties
    X-SVN-Rev: 7781

---
+++ icu4c/source/common/uprops.c
@@ -0,0 +1,32 @@
+/*
+*******************************************************************************
+*
+*   Copyright (C) 2002, International Business Machines
+*   Corporation and others.  All Rights Reserved.
+*
+*******************************************************************************
+*   file name:  uprops.h
+*   encoding:   US-ASCII
+*   tab size:   8 (not used)
+*   indentation:4
+*
+*   created on: 2002feb24
+*   created by: Markus W. Scherer
+*
+*   Implementations for mostly non-core Unicode character properties
+*   stored in uprops.dat.
+*/
+
+#include "unicode/utypes.h"
+#include "unicode/uchar.h"
+#include "uprops.h"
+
+U_CAPI void U_EXPORT2
+u_charAge(UChar32 c, UVersionInfo versionArray) {
+    if(versionArray!=NULL) {
+        uint32_t version=u_getUnicodeProperties(c, 0)>>UPROPS_AGE_SHIFT;
+        versionArray[0]=(uint8_t)(version>>4);
+        versionArray[1]=(uint8_t)(version&0xf);
+        versionArray[2]=versionArray[3]=0;
+    }
+}
