8d8f73c98f8b2499b3dc5a2cae1c413ccbd8ac22
George Rhoten <grhoten@users.noreply.github.com>
2002-03-01 02:42:06 +0000

    ICU-900 Fixed some compiler warnings.
    X-SVN-Rev: 7832

--- icu4c/source/common/ucln_cmn.h
+++ icu4c/source/common/ucln_cmn.h
@@ -34,7 +34,7 @@ U_CFUNC UBool uloc_cleanup(void);
 U_CFUNC UBool ustring_cleanup(void);
 
 /* @deprecated this functionality is going away */
-U_CFUNC UBool UnicodeConverter_cleanup();
+U_CFUNC UBool UnicodeConverter_cleanup(void);
 
 U_CFUNC UBool ucnv_cleanup(void);
 
