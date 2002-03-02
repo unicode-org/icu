19945d19759e7c52540860309ae76970933813d3
Alan Liu <alansliu@gmail.com>
2002-03-02 00:07:28 +0000

    [ICU-162] add DateFormat parse() and format() methods taking a Calendar X-Commit-URL: https://ssl.icu-project.org/trac/changeset/7854 Former-commit-id: 17b7234f5883c3c718b32e57faa382e1dc9cd8d8

--- icu4j/src/com/ibm/icu/text/ChineseDateFormat.java
+++ icu4j/src/com/ibm/icu/text/ChineseDateFormat.java
@@ -3,8 +3,8 @@
  * others. All Rights Reserved.
  *********************************************************************
  * $Source: /xsrl/Nsvn/icu/icu4j/src/com/ibm/icu/text/ChineseDateFormat.java,v $
- * $Date: 2002/02/16 03:06:03 $
- * $Revision: 1.4 $
+ * $Date: 2002/03/02 00:07:28 $
+ * $Revision: 1.5 $
  */
 package com.ibm.icu.text;
 import com.ibm.icu.util.*;
@@ -48,26 +48,27 @@ public class ChineseDateFormat extends SimpleDateFormat {
     }
 
     protected String subFormat(char ch, int count, int beginOffset,
-                               FieldPosition pos, DateFormatSymbols formatData)  {
+                               FieldPosition pos, DateFormatSymbols formatData,
+                               Calendar cal)  {
         switch (ch) {
         case 'G': // 'G' - ERA
-            return zeroPaddingNumber(calendar.get(Calendar.ERA), 1, 9);
+            return zeroPaddingNumber(cal.get(Calendar.ERA), 1, 9);
         case 'l': // 'l' - IS_LEAP_MONTH
             {
                 ChineseDateFormatSymbols symbols =
                     (ChineseDateFormatSymbols) formatData;
-                return symbols.getLeapMonth(calendar.get(
+                return symbols.getLeapMonth(cal.get(
                                ChineseCalendar.IS_LEAP_MONTH));
             }
         default:
-            return super.subFormat(ch, count, beginOffset, pos, formatData);
+            return super.subFormat(ch, count, beginOffset, pos, formatData, cal);
         }
     }    
 
     protected int subParse(String text, int start, char ch, int count,
-                           boolean obeyCount, boolean[] ambiguousYear) {
+                           boolean obeyCount, boolean[] ambiguousYear, Calendar cal) {
         if (ch != 'G' && ch != 'l' && ch != 'y') {
-            return super.subParse(text, start, ch, count, obeyCount, ambiguousYear);
+            return super.subParse(text, start, ch, count, obeyCount, ambiguousYear, cal);
         }
 
         ParsePosition pos = new ParsePosition(start);
@@ -102,7 +103,7 @@ public class ChineseDateFormat extends SimpleDateFormat {
                     return -start;
                 }
                 int value = number.intValue();
-                calendar.set(ch == 'G' ? Calendar.ERA : Calendar.YEAR, value);
+                cal.set(ch == 'G' ? Calendar.ERA : Calendar.YEAR, value);
                 return pos.getIndex();
             }
         case 'l': // 'l' - IS_LEAP_MONTH
@@ -110,11 +111,11 @@ public class ChineseDateFormat extends SimpleDateFormat {
                 ChineseDateFormatSymbols symbols =
                     (ChineseDateFormatSymbols) getSymbols();
                 int result = matchString(text, start, ChineseCalendar.IS_LEAP_MONTH,
-                                         symbols.isLeapMonth);
+                                         symbols.isLeapMonth, cal);
                 // Treat the absence of any matching string as setting
                 // IS_LEAP_MONTH to false.
                 if (result<0) {
-                    calendar.set(ChineseCalendar.IS_LEAP_MONTH, 0);
+                    cal.set(ChineseCalendar.IS_LEAP_MONTH, 0);
                     result = start;
                 }
                 return result;
