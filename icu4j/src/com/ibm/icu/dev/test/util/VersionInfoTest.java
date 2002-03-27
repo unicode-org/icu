bfdc8e7de3033d5fdb9a811dfddf978b7c2f84dc
Syn Wee Quek <swquek@svn.icu-project.org>
2002-03-27 01:59:46 +0000

    ICU-1741 reorganized the tests to comply with the code review suggestions
    X-SVN-Rev: 8257

--- icu4j/src/com/ibm/icu/dev/test/util/VersionInfoTest.java
+++ icu4j/src/com/ibm/icu/dev/test/util/VersionInfoTest.java
@@ -5,8 +5,8 @@
 *******************************************************************************
 *
 * $Source: /xsrl/Nsvn/icu/icu4j/src/com/ibm/icu/dev/test/util/VersionInfoTest.java,v $ 
-* $Date: 2002/03/23 00:58:20 $ 
-* $Revision: 1.4 $
+* $Date: 2002/03/27 01:59:46 $ 
+* $Revision: 1.5 $
 *
 *******************************************************************************
 */
@@ -53,60 +53,36 @@ public final class VersionInfoTest extends TestFmwk
     public void TestInstance()
     {
     	VersionInfo version;
-    	try {
-    		version = VersionInfo.getInstance("a");
-    		errln("\"a\" should produce an exception");
-    	} catch (RuntimeException e) {
+    	for (int i = 0; i < INSTANCE_INVALID_STRING_.length; i ++) {
+    		try {
+    			version = 
+    			        VersionInfo.getInstance(INSTANCE_INVALID_STRING_[i]);
+    			errln("\"" + INSTANCE_INVALID_STRING_[i] + 
+    			      "\" should produce an exception");
+    		} catch (RuntimeException e) {
+    		}
     	}
-    	try {
-    		version = VersionInfo.getInstance("2");
-    	} catch (RuntimeException e) {
-    		errln("\"2\" should produce a valid version");
+    	for (int i = 0; i < INSTANCE_VALID_STRING_.length; i ++) {
+	    	try {
+	    		version = VersionInfo.getInstance(INSTANCE_VALID_STRING_[i]);
+	    	} catch (RuntimeException e) {
+	    		errln("\"" + INSTANCE_VALID_STRING_[i] + 
+	    		      "\" should produce an valid version");
+	    	}
     	}
-    	try {
-    		version = VersionInfo.getInstance("2.0");
-    	} catch (RuntimeException e) {
-    		errln("\"2.0\" should produce a valid version");
+    	for (int i = 0; i < INSTANCE_INVALID_INT_.length; i ++) {
+	    	try {
+	    		version = getInstance(INSTANCE_INVALID_INT_[i]);
+	    		errln("invalid ints should produce an exception");
+	    	} catch (RuntimeException e) {
+	    	}
     	}
-    	try {
-    		version = VersionInfo.getInstance("2.0.0");
-    	} catch (RuntimeException e) {
-    		errln("\"2.0.0\" should produce a valid version");
-    	}
-    	try {
-    		version = VersionInfo.getInstance("-2.0.0");
-    		errln("\"-2.0.0\" should produce an exception");
-    	} catch (RuntimeException e) {
-    	}
-    	try {
-    		version = VersionInfo.getInstance("2.300.0");
-    		errln("\"2.300.0\" should produce an exception");
-    	} catch (RuntimeException e) {
-    	}
-    	try {
-    		version = VersionInfo.getInstance("2.0.0.0");
-    	} catch (RuntimeException e) {
-    		errln("\"2.0.0.0\" should produce an valid version");
-    	}
-    	try {
-    		version = VersionInfo.getInstance("2.100.100.100");
-    	} catch (RuntimeException e) {
-    		errln("\"2.100.100.100\" should produce an valid version");
-    	}
-    	try {
-    		version = VersionInfo.getInstance(-2, 0, 0, 0);
-    		errln("-2.0.0.0 should produce an exception");
-    	} catch (RuntimeException e) {
-    	}
-    	try {
-    		version = VersionInfo.getInstance(2, 300, 0, 0);
-    		errln("2.300.0.0 should produce an exception");
-    	} catch (RuntimeException e) {
-    	}
-    	try {
-    		version = VersionInfo.getInstance(2, 0, 0, 0);
-    	} catch (RuntimeException e) {
-    		errln("\"2.0.0.0\" should produce an valid version");
+    	for (int i = 0; i < INSTANCE_VALID_INT_.length; i ++) {
+	    	try {
+	    		version = getInstance(INSTANCE_VALID_INT_[i]);
+	    	} catch (RuntimeException e) {
+	    		errln("valid ints should not produce an exception");
+	    	}
     	}
     }
     
@@ -115,49 +91,53 @@ public final class VersionInfoTest extends TestFmwk
      */
     public void TestCompare()
     {
-    	VersionInfo unicode10 = VersionInfo.getInstance("1.0.0.0");
-    	VersionInfo unicode10again = VersionInfo.getInstance(1, 0, 0, 0);
-    	if (unicode10 != unicode10again || !unicode10.equals(unicode10again) ||
-    	    unicode10.compareTo(unicode10again) != 0) {
-    	    errln("Creation by string and int should produce the same VersionInfo 1.0.0.0");
+    	for (int i = 0; i < COMPARE_NOT_EQUAL_STRING_.length; i += 2) {
+    		VersionInfo v1 = 
+    		            VersionInfo.getInstance(COMPARE_NOT_EQUAL_STRING_[i]);
+    		VersionInfo v2 = 
+    		        VersionInfo.getInstance(COMPARE_NOT_EQUAL_STRING_[i + 1]);
+    		if (v1.compareTo(v2) == 0) {
+    			errln(COMPARE_NOT_EQUAL_STRING_[i] + " should not equal " +
+    			      COMPARE_NOT_EQUAL_STRING_[i + 1]);
+    		}
     	}
-    	VersionInfo unicode1 = VersionInfo.getInstance("1");
-    	VersionInfo unicode1again = VersionInfo.getInstance("1.0");
-    	if (unicode1 != unicode10 || !unicode1.equals(unicode1again) ||
-    	    unicode1.compareTo(unicode1again) != 0) {
-    	    errln("Creation by string and int should produce the same VersionInfo 1.0.0.0");
+    	for (int i = 0; i < COMPARE_NOT_EQUAL_INT_.length; i += 2) {
+    		VersionInfo v1 = getInstance(COMPARE_NOT_EQUAL_INT_[i]);
+    		VersionInfo v2 = getInstance(COMPARE_NOT_EQUAL_INT_[i + 1]);
+    		if (v1.compareTo(v2) == 0) {
+    			errln(COMPARE_NOT_EQUAL_INT_[i] + " should not equal " +
+    			      COMPARE_NOT_EQUAL_INT_[i + 1]);
+    		}
     	}
-    	VersionInfo unicode20 = VersionInfo.getInstance(2, 0, 0, 0);
-    	VersionInfo unicode20again = VersionInfo.getInstance(2, 0, 0, 0);
-    	if (unicode20 != unicode20again || !unicode20.equals(unicode20again) ||
-    	    unicode20.compareTo(unicode20again) != 0) {
-    	    errln("Creation by int should produce the same VersionInfo 2.0.0.0");
+    	for (int i = 0; i < COMPARE_EQUAL_STRING_.length - 1; i ++) {
+    		VersionInfo v1 = 
+    		            VersionInfo.getInstance(COMPARE_EQUAL_STRING_[i]);
+    		VersionInfo v2 = 
+    		        VersionInfo.getInstance(COMPARE_EQUAL_STRING_[i + 1]);
+    		if (v1.compareTo(v2) != 0) {
+    			errln(COMPARE_EQUAL_STRING_[i] + " should equal " +
+    			      COMPARE_EQUAL_STRING_[i + 1]);
+    		}
     	}
-    	VersionInfo unicode2 = VersionInfo.getInstance(2);
-    	VersionInfo unicode2again = VersionInfo.getInstance(2, 0);
-    	if (unicode20 != unicode2 || !unicode2.equals(unicode2again) ||
-    	    unicode2.compareTo(unicode2again) != 0) {
-    	    errln("Creation by int should produce the same VersionInfo 2.0.0.0");
+    	for (int i = 0; i < COMPARE_EQUAL_INT_.length - 1; i ++) {
+    		VersionInfo v1 = getInstance(COMPARE_EQUAL_INT_[i]);
+    		VersionInfo v2 = getInstance(COMPARE_EQUAL_INT_[i + 1]);
+    		if (v1.compareTo(v2) != 0) {
+    			errln(COMPARE_EQUAL_INT_[i] + " should equal " +
+    			      COMPARE_EQUAL_INT_[i + 1]);
+    		}
     	}
-    	VersionInfo unicode30 = VersionInfo.getInstance("3.0.0.0");
-    	VersionInfo unicode30again = VersionInfo.getInstance("3.0.0.0");
-    	if (unicode30 != unicode30again || !unicode30.equals(unicode30again) ||
-    	    unicode30.compareTo(unicode30again) != 0) {
-    	    errln("Creation by string should produce the same VersionInfo 3.0.0.0");
-    	}
-    	VersionInfo unicode3 = VersionInfo.getInstance("3");
-    	VersionInfo unicode3again = VersionInfo.getInstance(3, 0);
-    	if (unicode3 != unicode30 || !unicode3.equals(unicode3again) ||
-    	    unicode3.compareTo(unicode3again) != 0) {
-    	    errln("Creation by string should produce the same VersionInfo 3.0.0.0");
-    	}
-    	if (unicode10 == unicode20 || unicode10.equals(unicode20) ||
-    	    unicode10.compareTo(unicode20) >= 0) {
-    	    errln("1.0.0.0 should be less than 2.0.0.0");
-    	}
-    	if (unicode20 == unicode30 || unicode20.equals(unicode30) ||
-    	    unicode20.compareTo(unicode30) >= 0) {
-    	    errln("2.0.0.0 should be less than 3.0.0.0");
+    	for (int i = 0; i < COMPARE_LESS_.length - 1; i ++) {
+    		VersionInfo v1 = VersionInfo.getInstance(COMPARE_LESS_[i]);
+    		VersionInfo v2 = VersionInfo.getInstance(COMPARE_LESS_[i + 1]);
+    		if (v1.compareTo(v2) >= 0) {
+    			errln(COMPARE_LESS_[i] + " should be less than " + 
+    			      COMPARE_LESS_[i + 1]);
+    		}
+    		if (v2.compareTo(v1) <= 0) {
+    			errln(COMPARE_LESS_[i + 1] + " should be greater than " + 
+    			      COMPARE_LESS_[i]);
+    		}
     	}
     }
     
@@ -166,25 +146,29 @@ public final class VersionInfoTest extends TestFmwk
      */
     public void TestGetter()
     {
-    	VersionInfo unicode10 = VersionInfo.getInstance("1.0.0.0");
-    	if (unicode10.getMajor() != 1 || unicode10.getMinor() != 0 ||
-    	    unicode10.getMilli() != 0 || unicode10.getMicro() != 0) {
-    	    errln("Getter methods for 1.0.0.0 should produce 1.0.0.0");
-    	}
-    	VersionInfo unicode31 = VersionInfo.getInstance("3.1.1.0");
-    	if (unicode31.getMajor() != 3 || unicode31.getMinor() != 1 ||
-    	    unicode31.getMilli() != 1 || unicode31.getMicro() != 0) {
-    	    errln("Getter methods for 3.1.1.0 should produce 3.1.1.0");
-    	}
-    	VersionInfo unicode43 = VersionInfo.getInstance(4, 3, 2, 1);
-    	if (unicode43.getMajor() != 4 || unicode43.getMinor() != 3 ||
-    	    unicode43.getMilli() != 2 || unicode43.getMicro() != 1) {
-    	    errln("Getter methods for 4.3.2.1 should produce 4.3.2.1");
-    	}
-    	VersionInfo unicode54 = VersionInfo.getInstance(54, 43, 32, 21);
-    	if (unicode54.getMajor() != 54 || unicode54.getMinor() != 43 ||
-    	    unicode54.getMilli() != 32 || unicode54.getMicro() != 21) {
-    	    errln("Getter methods for 54.43.32.21 should produce 54.43.32.21");
+    	for (int i = 0; i < GET_STRING_.length; i ++) {
+    		VersionInfo v = VersionInfo.getInstance(GET_STRING_[i]);
+    		if (v.getMajor() != GET_RESULT_[i << 2] ||
+    		    v.getMinor() != GET_RESULT_[(i << 2) + 1] ||
+    		    v.getMilli() != GET_RESULT_[(i << 2) + 2] ||
+    		    v.getMicro() != GET_RESULT_[(i << 2) + 3]) {
+    		    errln(GET_STRING_[i] + " should return major=" + 
+    		          GET_RESULT_[i << 2] + " minor=" + 
+    		          GET_RESULT_[(i << 2) + 1] + " milli=" + 
+    		          GET_RESULT_[(i << 2) + 2] + " micro=" +
+    		          GET_RESULT_[(i << 2) + 3]);  
+    		}
+    		v = getInstance(GET_INT_[i]);
+    		if (v.getMajor() != GET_RESULT_[i << 2] ||
+    		    v.getMinor() != GET_RESULT_[(i << 2) + 1] ||
+    		    v.getMilli() != GET_RESULT_[(i << 2) + 2] ||
+    		    v.getMicro() != GET_RESULT_[(i << 2) + 3]) {
+    		    errln(GET_STRING_[i] + " should return major=" + 
+    		          GET_RESULT_[i << 2] + " minor=" + 
+    		          GET_RESULT_[(i << 2) + 1] + " milli=" + 
+    		          GET_RESULT_[(i << 2) + 2] + " micro=" +
+    		          GET_RESULT_[(i << 2) + 3]);  
+    		}
     	}
     }
     
@@ -193,13 +177,152 @@ public final class VersionInfoTest extends TestFmwk
      */
 	public void TesttoString() 
 	{
-    	VersionInfo unicode31 = VersionInfo.getInstance("3.1.1.0");
-    	if (!unicode31.toString().equals("3.1.1.0")) {
-    	    errln("toString() for 3.1.1.0 should produce 3.1.1.0");
-    	}
-    	VersionInfo unicode54 = VersionInfo.getInstance(54, 43, 32, 21);
-    	if (!unicode54.toString().equals("54.43.32.21")) {
-    	    errln("toString() for 54.43.32.21 should produce 54.43.32.21");
-    	}
+		for (int i = 0; i < TOSTRING_STRING_.length; i ++) {
+    		VersionInfo v = VersionInfo.getInstance(TOSTRING_STRING_[i]);
+    		if (!v.toString().equals(TOSTRING_RESULT_[i])) {
+    	    	errln("toString() for " + TOSTRING_STRING_[i] + 
+    	    	      " should produce " + TOSTRING_RESULT_[i]);
+    		}
+    		v = getInstance(TOSTRING_INT_[i]);
+    		if (!v.toString().equals(TOSTRING_RESULT_[i])) {
+    	    	errln("toString() for " + TOSTRING_INT_[i] + 
+    	    	      " should produce " + TOSTRING_RESULT_[i]);
+    		}
+		}
 	}
+	
+	// private methods --------------------------------------------------
+	
+	/**
+	 * int array versioninfo creation
+	 */
+	private static VersionInfo getInstance(int data[])
+	{
+		switch (data.length) {
+	    	case 1:
+	    		return VersionInfo.getInstance(data[0]);
+	    	case 2:
+	    		return VersionInfo.getInstance(data[0], data[1]);
+	    	case 3:
+	    		return VersionInfo.getInstance(data[0], data[1], data[2]);
+	    	default:
+	    		return VersionInfo.getInstance(data[0], data[1], data[2], 
+	    		                               data[3]);
+		}
+	}
+	
+	// private data members --------------------------------------------
+	
+	/**
+	 * Test instance data
+	 */
+	private static final String INSTANCE_INVALID_STRING_[] = {
+		"a",
+		"-1",
+		"-1.0",
+		"-1.0.0",
+		"-1.0.0.0",
+		"0.-1",
+		"0.0.-1",
+		"0.0.0.-1",
+		"256",
+		"256.0",
+		"256.0.0",
+		"256.0.0.0",
+		"0.256",
+		"0.0.256",
+		"0.0.0.256",
+		"1.2.3.4.5"
+	};
+	private static final String INSTANCE_VALID_STRING_[] = {
+		"255",
+		"255.255",
+		"255.255.255",
+		"255.255.255.255"
+	};
+	private static final int INSTANCE_INVALID_INT_[][] = {
+		{-1},
+		{-1, 0},
+		{-1, 0, 0},
+		{-1, 0, 0, 0},
+		{0, -1},
+		{0, 0, -1},
+		{0, 0, 0, -1},
+		{256},
+		{256, 0},
+		{256, 0, 0},
+		{256, 0, 0, 0},
+		{0, 256},
+		{0, 0, 256},
+		{0, 0, 0, 256},
+	};
+	private static final int INSTANCE_VALID_INT_[][] = {
+		{255},
+		{255, 255},
+		{255, 255, 255},
+		{255, 255, 255, 255}
+	};
+	
+	/**
+	 * Test compare data
+	 */
+	private static final String COMPARE_NOT_EQUAL_STRING_[] = {
+		"2.0.0.0", "3.0.0.0"
+	};
+	private static final int COMPARE_NOT_EQUAL_INT_[][] = {
+		{2, 0, 0, 0}, {3, 0, 0, 0}
+	};
+	private static final String COMPARE_EQUAL_STRING_[] = {
+		"2.0.0.0", "2.0.0", "2.0", "2"
+	};
+	private static final int COMPARE_EQUAL_INT_[][] = {
+		{2}, {2, 0}, {2, 0, 0}, {2, 0, 0, 0}	
+	};
+	private static final String COMPARE_LESS_[] = {
+		"0", "0.0.0.1", "0.0.1", "0.1", "1", "2", "2.1", "2.1.1", "2.1.1.1"
+	};
+	
+	/**
+	 * Test Getter data
+	 */
+	private static final String GET_STRING_[] = {
+		"0",
+		"1.1",
+		"2.1.255",
+		"3.1.255.100"
+	};
+	private static final int GET_INT_[][] = {
+		{0},
+		{1, 1},
+		{2, 1, 255},
+		{3, 1, 255, 100}
+	};
+	private static final int GET_RESULT_[] = {
+		0, 0, 0, 0,
+		1, 1, 0, 0,
+		2, 1, 255, 0,
+		3, 1, 255, 100
+	};
+	
+	/**
+	 * Test toString data
+	 */
+	private static final String TOSTRING_STRING_[] = {
+		"0",
+		"1.1",
+		"2.1.255",
+		"3.1.255.100"	
+	};
+	private static final int TOSTRING_INT_[][] = {
+		{0},
+		{1, 1},
+		{2, 1, 255},
+		{3, 1, 255, 100}
+	};
+	private static final String TOSTRING_RESULT_[] = {
+		"0.0.0.0",
+		"1.1.0.0",
+		"2.1.255.0",
+		"3.1.255.100"	
+	};
 }
