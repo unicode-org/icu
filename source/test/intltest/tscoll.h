1dfa3ee4cc049fc90003380621740e2cbcb0a3ad
Vladimir Weinstein <weiv@svn.icu-project.org>
2002-04-30 23:02:45 +0000

    moved doTest to collation test base class. All the tests should use this common function
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/1880
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@8550 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/test/intltest/tscoll.h
+++ source/test/intltest/tscoll.h
@@ -15,11 +15,16 @@
 #include "intltest.h"
 #include "unicode/coll.h"
 #include "unicode/sortkey.h"
+#include "unicode/schriter.h"
+#include "unicode/ures.h"
 
 
 class IntlTestCollator: public IntlTest {
-    void runIndexedTest( int32_t index, UBool exec, const char* &name, char* par = NULL );
+    void runIndexedTest(int32_t index, UBool exec, const char* &name, char* par = NULL );
 protected:
+    // These two should probably go down in IntlTest
+    void doTest(Collator* col, UnicodeString source, UnicodeString target, Collator::EComparisonResult result);
+    void doTestVariant(Collator* col, UnicodeString &source, UnicodeString &target, Collator::EComparisonResult result);
     virtual void reportCResult( UnicodeString &source, UnicodeString &target,
                                 CollationKey &sourceKey, CollationKey &targetKey,
                                 Collator::EComparisonResult compareResult,
