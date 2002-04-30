be61ae0a892be45641d6b4c7a8e4782b8163947f
Vladimir Weinstein <weiv@svn.icu-project.org>
2002-04-30 23:03:51 +0000

    added collation data driven test - reads resource bundle and tests collation
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/1880
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@8551 0a8b766c-62c9-45f9-954d-7f2943c9ead0

---
+++ source/test/intltest/dadrcoll.h
@@ -0,0 +1,51 @@
+/********************************************************************
+ * COPYRIGHT: 
+ * Copyright (c) 1997-2001, International Business Machines Corporation and
+ * others. All Rights Reserved.
+ ********************************************************************/
+
+/**
+ * MajorTestLevel is the top level test class for everything in the directory "IntlWork".
+ */
+
+#ifndef _INTLTESTDATADRIVENCOLLATOR
+#define _INTLTESTDATADRIVENCOLLATOR
+
+
+#include "tscoll.h"
+#include "unicode/coll.h"
+#include "unicode/tblcoll.h"
+#include "unicode/sortkey.h"
+#include "unicode/schriter.h"
+#include "unicode/ures.h"
+
+
+class DataDrivenCollatorTest: public IntlTestCollator {
+    void runIndexedTest(int32_t index, UBool exec, const char* &name, char* par = NULL );
+public:
+    DataDrivenCollatorTest();
+    ~DataDrivenCollatorTest();
+protected:
+    // These two should probably go down in IntlTest
+    const char* loadTestData(UErrorCode& err);
+    UResourceBundle* getTestBundle(const char* bundleName);
+
+    void DataDrivenTest(char *par);
+    void processTest(UResourceBundle *test, UErrorCode &status);
+    void processArguments(Collator *col, const UnicodeString &options, UErrorCode &status);
+    UBool setTestSequence(const UnicodeString &setSequence, UnicodeString &source, Collator::EComparisonResult &relation, UErrorCode &status);
+    UBool getNextInSequence(UnicodeString &source, Collator::EComparisonResult &relation, UErrorCode &status);
+    void processSequence(Collator* col, const UnicodeString &sequence, UErrorCode &status);
+private:
+  StringCharacterIterator seq;
+  UResourceBundle *testBundle;
+  UResourceBundle *testData;
+  UResourceBundle *currentTest;
+  UResourceBundle *purpose;
+  UResourceBundle *parsing;
+  int32_t numberOfTests;
+  UBool dataTestValid;
+};
+
+
+#endif
