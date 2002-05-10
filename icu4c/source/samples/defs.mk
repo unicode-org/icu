4395612ac372455bb00648411c9b1d53a4fc69da
Steven R. Loomis <srl@icu-project.org>
2002-05-10 20:32:34 +0000

    ICU-1861 samples updated to use icu-config - and also refactored, cleaned up
    X-SVN-Rev: 8593

--- icu4c/source/samples/defs.mk
+++ icu4c/source/samples/defs.mk
@@ -11,10 +11,17 @@ CPPFLAGS=$(shell icu-config --cppflags)
 CFLAGS=$(shell icu-config --cflags)
 CXXFLAGS=$(shell icu-config --cxxflags)
 LDFLAGS =$(shell icu-config --ldflags)
+LDFLAGS_USTDIO =$(shell icu-config --ldflags-ustdio)
 INVOKE=$(shell icu-config --invoke)
+GENRB=$(shell icu-config --invoke=genrb)
+PKGDATA=$(shell icu-config --invoke=pkgdata)
+SO=$(shell icu-config --so)
+PKGDATAOPTS=-v -d .
+# default - resources in same mode as ICU
+RESMODE=$(shell icu-config --icudata-mode)
+
 ####################################################################
 ### Project independent things (common) 
 ### We depend on gmake for the bulk of the work 
 
-# link with C++ compiler
-LINK.o=$(LINK.cc)
+RMV=rm -rf
