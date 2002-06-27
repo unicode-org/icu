bcbf007e906d1360b65cceae8b19730f08ea3296
Markus Scherer <markus.icu@gmail.com>
2002-06-27 01:37:52 +0000

    derive all ICU C++ classes from common UObject base class
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/1962
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@8955 0a8b766c-62c9-45f9-954d-7f2943c9ead0

---
+++ source/common/uobject.cpp
@@ -0,0 +1,24 @@
+/*
+******************************************************************************
+*
+*   Copyright (C) 2002, International Business Machines
+*   Corporation and others.  All Rights Reserved.
+*
+******************************************************************************
+*   file name:  uobject.h
+*   encoding:   US-ASCII
+*   tab size:   8 (not used)
+*   indentation:4
+*
+*   created on: 2002jun26
+*   created by: Markus W. Scherer
+*/
+
+#include "unicode/utypes.h"
+#include "unicode/uobject.h"
+
+U_NAMESPACE_BEGIN
+
+const char UObject::fgClassID=0;
+
+U_NAMESPACE_END
