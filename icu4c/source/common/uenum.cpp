9aec93818e90d7014ff1225fbb32567098388ef7
George Rhoten <grhoten@users.noreply.github.com>
2002-07-12 20:57:20 +0000

    ICU-1972 Be nicer to the user by not crashing if the original open failed
    X-SVN-Rev: 9119

--- icu4c/source/common/uenum.cpp
+++ icu4c/source/common/uenum.cpp
@@ -26,61 +26,68 @@ static const UEnumeration nullEnumeration = {
     NULL  /* reset */
 };
 
-void uenum_close(UEnumeration* en) {
-  if(en->close != NULL) {
-    en->close(en);
-  } else { // this seems dangerous, but better kill the object
-    uprv_free(en);
-  }
+void uenum_close(UEnumeration* en)
+{
+    if (en) {
+        if (en->close != NULL) {
+            en->close(en);
+        } else { // this seems dangerous, but better kill the object
+            uprv_free(en);
+        }
+    }
 }
 
-int32_t uenum_count(UEnumeration* en, UErrorCode* status) {
-  if(U_FAILURE(*status)) {
-    return -1;
-  }
-  if(en->count != NULL) {
-    return en->count(en, status);
-  } else {
-    *status = U_UNSUPPORTED_ERROR;
-    return -1;
-  }
+int32_t uenum_count(UEnumeration* en, UErrorCode* status)
+{
+    if (!en || U_FAILURE(*status)) {
+        return -1;
+    }
+    if (en->count != NULL) {
+        return en->count(en, status);
+    } else {
+        *status = U_UNSUPPORTED_ERROR;
+        return -1;
+    }
 }
 
 const UChar* uenum_unext(UEnumeration* en,
                         int32_t* resultLength,
-                        UErrorCode* status) {
-  if(U_FAILURE(*status)) {
-    return NULL;
-  }
-  if(en->uNext != NULL) {
-    return en->uNext(en, resultLength, status);
-  } else {
-    *status = U_UNSUPPORTED_ERROR;
-    return NULL;
-  }
+                        UErrorCode* status)
+{
+    if (!en || U_FAILURE(*status)) {
+        return NULL;
+    }
+    if (en->uNext != NULL) {
+        return en->uNext(en, resultLength, status);
+    } else {
+        *status = U_UNSUPPORTED_ERROR;
+        return NULL;
+    }
 }
 
 const char* uenum_next(UEnumeration* en,
                       int32_t* resultLength,
-                      UErrorCode* status) {
-  if(U_FAILURE(*status)) {
-    return NULL;
-  }
-  if(en->next != NULL) {
-    return en->next(en, resultLength, status);
-  } else {
-    *status = U_UNSUPPORTED_ERROR;
-    return NULL;
-  }
+                      UErrorCode* status)
+{
+    if (!en || U_FAILURE(*status)) {
+        return NULL;
+    }
+    if (en->next != NULL) {
+        return en->next(en, resultLength, status);
+    } else {
+        *status = U_UNSUPPORTED_ERROR;
+        return NULL;
+    }
 }
 
-void uenum_reset(UEnumeration* en, UErrorCode* status) {
-  if(U_FAILURE(*status)) {
-    return;
-  }
-  if(en->reset != NULL) {
-    en->reset(en, status);
-  } else {
-    *status = U_UNSUPPORTED_ERROR;
-  }
+void uenum_reset(UEnumeration* en, UErrorCode* status)
+{
+    if (!en || U_FAILURE(*status)) {
+        return;
+    }
+    if (en->reset != NULL) {
+        en->reset(en, status);
+    } else {
+        *status = U_UNSUPPORTED_ERROR;
+    }
 }
