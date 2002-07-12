61ba0a2fdfc71cb7e7a1d62cd9ec8274dfa660d8
Ram Viswanadha <rviswanadha@gmail.com>
2002-07-12 21:31:38 +0000

    ICU-1982 Test alias mechanism and types
    X-SVN-Rev: 9121

--- icu4j/src/com/ibm/icu/dev/test/util/ICUListResourceBundleTest.java
+++ icu4j/src/com/ibm/icu/dev/test/util/ICUListResourceBundleTest.java
@@ -1,39 +1,60 @@
 package com.ibm.icu.dev.test.util;
 
-import java.util.Enumeration;
 import java.util.ResourceBundle;
 
 import com.ibm.icu.dev.test.TestFmwk;
 import com.ibm.icu.impl.ICULocaleData;
 
+
 public final class ICUListResourceBundleTest extends TestFmwk 
 {
-    public static void main(String args[]) {
-	ICUListResourceBundleTest test = new ICUListResourceBundleTest();
-	test.TestReferences();
-	/**
-	try {
+    public static void main(String args[]) throws Exception {
+		ICUListResourceBundleTest test = new ICUListResourceBundleTest();
 	    test.run(args);
-	} catch (Exception e) {
-	    test.errln("Error testing ICUListResourceBundle: " + e);
-	}
-	*/
+
+    }
+
+    public void TestReferences() {
+		ResourceBundle rb = ICULocaleData.getLocaleElements("th");
+		System.out.println("got rb:" + rb);
+	
+		byte[] binaryData = null;//(byte[])rb.getObject("%%CollationBin");
+		Object colElem = rb.getObject("CollationElements");
+        if(colElem instanceof Object[][]){
+            Object[][] colElemArr = (Object[][])colElem;
+            if(((String)colElemArr[0][0]).equals("%%CollationBin")){   
+	           binaryData = (byte[]) colElemArr[0][1];
+            }
+        }
+        System.out.println("got binaryData: " + binaryData + " length: " + (binaryData == null ? 0 : binaryData.length));
+		Object[] stringArrayData = (Object[])rb.getObject("CollationElements");
+		//String[] collationData = new String[] {
+		 //   (String)stringArrayData[0],
+		 //   (String)stringArrayData[0]
+		//};
+		System.out.println("got stringData: " + stringArrayData + " length: " + stringArrayData.length);
+		System.out.println("got stringDataElement: " + stringArrayData[0] + " length: " + stringArrayData.length);
+		//System.out.println("got sdee: " + collationData[0]);
+		//	System.out.println("char data length: " + stringArrayData.length());
+    }
+    public void TestAliases(){
+        ResourceBundle rb = ICULocaleData.getResourceBundle("com.ibm.icu.dev.data","TestDataElements","testaliases");
+        Object o = rb.getObject("CollationElements");
+        Object o1 = rb.getObject("zoneTests");
+        
     }
     
-    public void TestReferences() {
-	ResourceBundle rb = ICULocaleData.getLocaleElements("th");
-	System.out.println("got rb:" + rb);
-
-	byte[] binaryData = (byte[])rb.getObject("%%CollationBin");
-	System.out.println("got binaryData: " + binaryData + " length: " + (binaryData == null ? 0 : binaryData.length));
-	Object[][] stringArrayData = (Object[][])rb.getObject("CollationElements");
-	String[] collationData = new String[] {
-	    (String)stringArrayData[0][0],
-	    (String)stringArrayData[0][1]
-	};
-	System.out.println("got stringData: " + stringArrayData + " length: " + stringArrayData.length);
-	System.out.println("got stringDataElement: " + stringArrayData[0] + " length: " + stringArrayData[0].length);
-	System.out.println("got sdee: " + collationData[0]);
-	//	System.out.println("char data length: " + stringArrayData.length());
+    public void TestCircularAliases(){
+        try{
+	        ResourceBundle rb = ICULocaleData.getResourceBundle("com.ibm.icu.dev.data","TestDataElements","testcircularalias");
+	        Object o = rb.getObject("aaa");
+        }catch(java.util.MissingResourceException e){
+            if(e.toString().indexOf("ircular")==-1){
+                errln("Did not get the expected Exception for circular aliases");
+            }
+            return;
+        }
+        errln("Did not get the expected Exception for circular aliases");
     }
+
 }
