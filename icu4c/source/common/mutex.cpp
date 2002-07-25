597599cfd008b04d08e35b071c3e91c97ac0406f
George Rhoten <grhoten@users.noreply.github.com>
2002-07-25 15:25:44 +0000

    [ICU-1962] Still define fgClassID when threads are disabled. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/9329 Former-commit-id: 3f0c44ef5bb76e29962d289410bf0adc7a47a234

--- icu4c/source/common/mutex.cpp
+++ icu4c/source/common/mutex.cpp
@@ -1,14 +1,19 @@
 /*
  **********************************************************************
- *   Copyright (C) 1997-2001, International Business Machines
+ *   Copyright (C) 1997-2002, International Business Machines
  *   Corporation and others.  All Rights Reserved.
  **********************************************************************
 */
 
 #include "unicode/utypes.h"
-#include "unicode/uobject.h"
 #include "mutex.h"
 #include "umutex.h"
+
+U_NAMESPACE_BEGIN
+
+const char Mutex::fgClassID=0;
+
+U_NAMESPACE_END
 
 /* Initialize the global mutex only when we can use it. */
 #if (ICU_USE_THREADS == 1)
@@ -21,11 +26,5 @@ static int GlobalMutexInitialize()
 
 static int initializesGlobalMutex = GlobalMutexInitialize();
 
-
-U_NAMESPACE_BEGIN
-
-const char Mutex::fgClassID=0;
-
-U_NAMESPACE_END
-
 #endif /* ICU_USE_THREADS==1 */
+
