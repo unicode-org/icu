c3767f7f63d5656839fc51f887f4e4fe9649baad
George Rhoten <grhoten@svn.icu-project.org>
2002-07-26 21:05:37 +0000

    Make ICU work on z/OS in archive mode.
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/1891
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@9377 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/umapfile.c
+++ source/common/umapfile.c
@@ -60,7 +60,7 @@
 #       define MAP_FAILED ((void*)-1)
 #   endif
 
-#   ifdef OS390
+#   if defined(OS390) && defined (UDATA_DLL)
         /*   No memory mapping for 390 batch mode.  Fake it using dll loading.  */
 #       include <dll.h>
 #       include "cstring.h"
