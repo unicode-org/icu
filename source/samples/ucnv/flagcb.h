b108fda3f15ab6ac96614e8b3dc385f54d6da751
Steven R. Loomis <srl@icu-project.org>
2002-08-14 00:22:30 +0000

    samples, 2.2, safeclone
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/2076
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@9708 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/samples/ucnv/flagcb.h
+++ source/samples/ucnv/flagcb.h
@@ -19,7 +19,16 @@ typedef struct
   UBool                    flag;
 } FromUFLAGContext;
 
-U_CAPI void U_EXPORT2 UCNV_FROM_U_CALLBACK_FLAG (
+/**
+ * open the context 
+ */
+
+U_CAPI FromUFLAGContext* U_EXPORT2  flagCB_fromU_openContext();
+
+/**
+ * the actual callback 
+ */
+U_CAPI void U_EXPORT2 flagCB_fromU(
                   const void *context,
                   UConverterFromUnicodeArgs *fromUArgs,
                   const UChar* codeUnits,
@@ -27,5 +36,25 @@ U_CAPI void U_EXPORT2 UCNV_FROM_U_CALLBACK_FLAG (
                   UChar32 codePoint,
                   UConverterCallbackReason reason,
 				  UErrorCode * err);
+
+
+
+typedef struct
+{
+    UConverterFromUCallback  subCallback;
+    const void               *subContext;
+    uint32_t       magic;      /* 0xC0FFEE to identify that the object is OK */
+    uint32_t       serial;     /* minted from nextSerial */
+} debugCBContext;
+
+U_CAPI void debugCB_fromU(const void *context,
+                   UConverterFromUnicodeArgs *fromUArgs,
+                   const UChar* codeUnits,
+                   int32_t length,
+                   UChar32 codePoint,
+                   UConverterCallbackReason reason,
+                   UErrorCode * err);
+
+U_CAPI debugCBContext *debugCB_openContext();
 
 #endif
