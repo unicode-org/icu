7e5f02f531f5e1281f816b038d44dfb84ad73db0
Alan Liu <alansliu@gmail.com>
2002-08-21 18:39:27 +0000

    [ICU-2060] use Calendar.isEquivalentTo() X-Commit-URL: https://ssl.icu-project.org/trac/changeset/9752 Former-commit-id: d61992c0370e0df5381f6db6af5683e127c7b89e

--- icu4j/src/com/ibm/icu/text/DateFormat.java
+++ icu4j/src/com/ibm/icu/text/DateFormat.java
@@ -673,11 +673,7 @@ public abstract class DateFormat extends Format {
         if (this == obj) return true;
         if (obj == null || getClass() != obj.getClass()) return false;
         DateFormat other = (DateFormat) obj;
-        return (// calendar.equivalentTo(other.calendar) // THIS API DOESN'T EXIST YET!
-                calendar.getFirstDayOfWeek() == other.calendar.getFirstDayOfWeek() &&
-                calendar.getMinimalDaysInFirstWeek() == other.calendar.getMinimalDaysInFirstWeek() &&
-                calendar.isLenient() == other.calendar.isLenient() &&
-                calendar.getTimeZone().equals(other.calendar.getTimeZone()) &&
+        return (calendar.isEquivalentTo(other.calendar) &&
                 numberFormat.equals(other.numberFormat));
     }
 
