faa7777b860a5f935123c729585d2e6ad26a9140
Alan Liu <alansliu@gmail.com>
2002-08-21 18:32:19 +0000

    [ICU-2060] rewrite API docs, which were way off X-Commit-URL: https://ssl.icu-project.org/trac/changeset/9748 Former-commit-id: 81e6fff7b4717d54462fb9c6bc683f6dec39a1a9

--- icu4c/source/i18n/unicode/ucal.h
+++ icu4c/source/i18n/unicode/ucal.h
@@ -554,17 +554,17 @@ ucal_setDateTime(    UCalendar*        cal,
             UErrorCode        *status);
 
 /**
-* Determine if two UCalendars represent the same date.
-* Two UCalendars may represent the same date and have different fields
-* if they are in different time zones.
-* @param cal1 The first of the UCalendars to compare.
-* @param cal2 The second of the UCalendars to compare.
-* @return TRUE if cal1 and cal2 represent the same date, FALSE otherwise.
-* @stable
-*/
+ * Returns TRUE if two UCalendars are equivalent.  Equivalent
+ * UCalendars will behave identically, but they may be set to
+ * different times.
+ * @param cal1 The first of the UCalendars to compare.
+ * @param cal2 The second of the UCalendars to compare.
+ * @return TRUE if cal1 and cal2 are equivalent, FALSE otherwise.
+ * @stable
+ */
 U_CAPI UBool U_EXPORT2 
-ucal_equivalentTo(    const UCalendar*      cal1,
-            const UCalendar*      cal2);
+ucal_equivalentTo(const UCalendar*      cal1,
+                  const UCalendar*      cal2);
 
 /**
 * Add a specified signed amount to a particular field in a UCalendar.
