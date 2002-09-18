7005ead205c94ffdb71c43a95e970823b94f4236
Markus Scherer <markus.icu@gmail.com>
2002-09-18 01:22:20 +0000

    ICU-1885 remove -s390 EBCDIC mapping table variants in favor of the new runtime swaplfnl converter option; make sure to build normal variants for minimal sets of data
    X-SVN-Rev: 9876

--- icu4c/source/data/mappings/ucmebcdic.mk
+++ icu4c/source/data/mappings/ucmebcdic.mk
@@ -1,5 +1,5 @@
 # A list of EBCDIC UCM's to build
-# ibm-37 and ibm-1047-s390 are already mentioned in makedata.mak and Makefile.in
+# ibm-37 and ibm-1047 are already mentioned in makedata.mak and Makefile.in
 
 UCM_SOURCE_EBCDIC = ebcdic-xml-us.ucm\
 ibm-1025_P100-2000.ucm ibm-1026_P100-2000.ucm ibm-1097_P100-2000.ucm\
@@ -9,9 +9,6 @@ ibm-1364_P110-2000.ucm ibm-1371.ucm ibm-1388_P103-2001.ucm\
 ibm-1390.ucm ibm-1399.ucm ibm-870_P100-2000.ucm\
 ibm-875_P100-2000.ucm ibm-9030_P100-2000.ucm ibm-918_P100-2000.ucm\
 ibm-918_X100-2000.ucm ibm-930.ucm ibm-935.ucm ibm-937.ucm ibm-939.ucm\
-ibm-1140-s390.ucm ibm-12712-s390.ucm ibm-1142-s390.ucm ibm-1143-s390.ucm\
-ibm-1144-s390.ucm ibm-1145-s390.ucm ibm-1146-s390.ucm ibm-1147-s390.ucm\
-ibm-1148-s390.ucm ibm-1149-s390.ucm ibm-1153-s390.ucm ibm-16804-s390.ucm ibm-37-s390.ucm\
 ibm-1047.ucm ibm-1123.ucm ibm-1140.ucm ibm-1141.ucm ibm-1142.ucm\
 ibm-1143.ucm ibm-1144.ucm ibm-1145.ucm ibm-1146.ucm ibm-1147.ucm\
 ibm-1148.ucm ibm-1149.ucm ibm-1153.ucm ibm-1154.ucm ibm-1155.ucm\
