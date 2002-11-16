2ace87c5c8dd29e91390a3c4977604781568135d
Markus Scherer <markus.icu@gmail.com>
2002-11-16 01:15:29 +0000

    test that behavior of bogus strings is consistent with NULL: only assignment revives a bogus string
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/2351
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@10283 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/test/intltest/ustrtest.cpp
+++ source/test/intltest/ustrtest.cpp
@@ -52,6 +52,7 @@ void UnicodeStringTest::runIndexedTest( int32_t index, UBool exec, const char* &
         case 12: name = "TestStackAllocation"; if (exec) TestStackAllocation(); break;
         case 13: name = "TestUnescape"; if (exec) TestUnescape(); break;
         case 14: name = "TestCountChar32"; if (exec) TestCountChar32(); break;
+        case 15: name = "TestBogus"; if (exec) TestBogus(); break;
 
         default: name = ""; break; //needed to end loop
     }
@@ -789,11 +790,6 @@ UnicodeStringTest::TestSpacePadding()
     expectedValue = "Hi!  How ya doi";
     if (returnVal == FALSE || test3 != expectedValue)
         errln("truncate() failed: expected \"" + expectedValue + "\", got \"" + test3 + "\".");
-
-    test3.setToBogus();
-    if(!test3.isBogus() || test3.truncate(0) || test3.isBogus() || !test3.isEmpty()) {
-        errln("setToBogus().truncate(0) did not make the string valid/empty");
-    }
 }
 
 void
@@ -902,36 +898,6 @@ UnicodeStringTest::TestMiscellaneous()
     UnicodeString   test1("This is a test");
     UnicodeString   test2("This is a test");
     UnicodeString   test3("Me too!");
-
-    // test isBogus() and setToBogus()
-    if (test1.isBogus() || test2.isBogus() || test3.isBogus()) {
-        errln("A string returned TRUE for isBogus()!");
-    }
-
-    test3.setTo(FALSE, (const UChar *)0, -1);
-    if(!test3.isBogus()) {
-        errln("A bogus string returned FALSE for isBogus()!");
-    }
-
-    if (test1.hashCode() != test2.hashCode() || test1.hashCode() == test3.hashCode())
-        errln("hashCode() failed");
-
-    if(test3.getBuffer()!=0) {
-        errln("bogus.getBuffer()!=0");
-    }
-    test3.append((UChar)0x61);
-    if(test3.isBogus() || test3.getBuffer()==0) {
-        errln("Unable to revive a bogus string");
-    }
-    test3.setToBogus();
-    if(!test3.isBogus() || test3.getBuffer()!=0) {
-        errln("setToBogus() failed to make a string bogus");
-    }
-    // make sure that bogus.setCharAt() does not crash because of fArray==0
-    // setCharAt() "revives" the string but leaves the string empty because it has length()==0
-    if(test3.setCharAt(0, 0x62).isBogus() || !test3.isEmpty()) {
-        errln("bogus.setCharAt(0, 'b') failed");
-    }
 
     // test getBuffer(minCapacity) and releaseBuffer()
     test1=UnicodeString(); // make sure that it starts with its stackBuffer
@@ -1265,5 +1231,141 @@ UnicodeStringTest::TestCountChar32(void) {
                 _testUnicodeStringHasMoreChar32Than(string, start, length-start, number);
             }
         }
+    }
+}
+
+void
+UnicodeStringTest::TestBogus() {
+    UnicodeString   test1("This is a test");
+    UnicodeString   test2("This is a test");
+    UnicodeString   test3("Me too!");
+
+    // test isBogus() and setToBogus()
+    if (test1.isBogus() || test2.isBogus() || test3.isBogus()) {
+        errln("A string returned TRUE for isBogus()!");
+    }
+
+    test3.setTo(FALSE, (const UChar *)0, -1);
+    if(!test3.isBogus()) {
+        errln("A bogus string returned FALSE for isBogus()!");
+    }
+    if (test1.hashCode() != test2.hashCode() || test1.hashCode() == test3.hashCode()) {
+        errln("hashCode() failed");
+    }
+    if(test3.getBuffer()!=0 || test3.getBuffer(20)!=0 || test3.getTerminatedBuffer()!=0) {
+        errln("bogus.getBuffer()!=0");
+    }
+
+    // verify that non-assignment modifications fail and do not revive a bogus string
+    test3.append((UChar)0x61);
+    if(!test3.isBogus() || test3.getBuffer()!=0) {
+        errln("bogus.append('a') worked but must not");
+    }
+
+    test3.findAndReplace(UnicodeString((UChar)0x61), test2);
+    if(!test3.isBogus() || test3.getBuffer()!=0) {
+        errln("bogus.findAndReplace() worked but must not");
+    }
+
+    test3.trim();
+    if(!test3.isBogus() || test3.getBuffer()!=0) {
+        errln("bogus.trim() revived bogus but must not");
+    }
+
+    test3.remove();
+    if(!test3.isBogus() || test3.getBuffer()!=0) {
+        errln("bogus.remove() revived bogus but must not");
+    }
+
+    if(!test3.setCharAt(0, 0x62).isBogus() || !test3.isEmpty()) {
+        errln("bogus.setCharAt(0, 'b') worked but must not");
+    }
+
+    if(test3.truncate(0) || !test3.isBogus() || !test3.isEmpty()) {
+        errln("bogus.truncate(0) revived bogus but must not");
+    }
+
+    // verify that assignments revive a bogus string
+    test3.setToBogus();
+    if(!test3.isBogus() || (test3=test1).isBogus() || test3!=test1) {
+        errln("bogus.operator=() failed");
+    }
+
+    test3.setToBogus();
+    if(!test3.isBogus() || test3.fastCopyFrom(test1).isBogus() || test3!=test1) {
+        errln("bogus.fastCopyFrom() failed");
+    }
+
+    test3.setToBogus();
+    if(!test3.isBogus() || test3.setTo(test1).isBogus() || test3!=test1) {
+        errln("bogus.setTo(UniStr) failed");
+    }
+
+    test3.setToBogus();
+    if(!test3.isBogus() || test3.setTo(test1, 0).isBogus() || test3!=test1) {
+        errln("bogus.setTo(UniStr, 0) failed");
+    }
+
+    test3.setToBogus();
+    if(!test3.isBogus() || test3.setTo(test1, 0, 0x7fffffff).isBogus() || test3!=test1) {
+        errln("bogus.setTo(UniStr, 0, len) failed");
+    }
+
+    test3.setToBogus();
+    if(!test3.isBogus() || test3.setTo(test1.getBuffer(), test1.length()).isBogus() || test3!=test1) {
+        errln("bogus.setTo(const UChar *, len) failed");
+    }
+
+    test3.setToBogus();
+    if(!test3.isBogus() || test3.setTo((UChar)0x2028).isBogus() || test3!=UnicodeString((UChar)0x2028)) {
+        errln("bogus.setTo(UChar) failed");
+    }
+
+    test3.setToBogus();
+    if(!test3.isBogus() || test3.setTo((UChar32)0x1d157).isBogus() || test3!=UnicodeString((UChar32)0x1d157)) {
+        errln("bogus.setTo(UChar32) failed");
+    }
+
+    test3.setToBogus();
+    if(!test3.isBogus() || test3.setTo(FALSE, test1.getBuffer(), test1.length()).isBogus() || test3!=test1) {
+        errln("bogus.setTo(readonly alias) failed");
+    }
+
+    // writable alias to another string's buffer: very bad idea, just convenient for this test
+    test3.setToBogus();
+    if(!test3.isBogus() || test3.setTo((UChar *)test1.getBuffer(), test1.length(), test1.getCapacity()).isBogus() || test3!=test1) {
+        errln("bogus.setTo(writable alias) failed");
+    }
+
+    // same with simple, documented ways to turn a bogus string into an empty one
+    test3.setToBogus();
+    if(!test3.isBogus() || (test3=UnicodeString()).isBogus() || !test3.isEmpty()) {
+        errln("bogus.operator=(UnicodeString()) failed");
+    }
+
+    test3.setToBogus();
+    if(!test3.isBogus() || test3.setTo(UnicodeString()).isBogus() || !test3.isEmpty()) {
+        errln("bogus.setTo(UnicodeString()) failed");
+    }
+
+    test3.setToBogus();
+    if(!test3.isBogus() || test3.setTo((UChar32)-1).isBogus() || !test3.isEmpty()) {
+        errln("bogus.setTo((UChar32)-1) failed");
+    }
+
+    static const UChar nul=0;
+
+    test3.setToBogus();
+    if(!test3.isBogus() || test3.setTo(&nul, 0).isBogus() || !test3.isEmpty()) {
+        errln("bogus.setTo(&nul, 0) failed");
+    }
+
+    test3.setToBogus();
+    if(!test3.isBogus() || test3.getBuffer()!=0) {
+        errln("setToBogus() failed to make a string bogus");
+    }
+
+    if(test1.isBogus() || !(test1=test3).isBogus()) {
+        errln("normal=bogus failed to make the left string bogus");
     }
 }
