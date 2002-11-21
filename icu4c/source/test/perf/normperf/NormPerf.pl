649d0357c7a02d4a9057b9646ecdca7f2104a845
Vladimir Weinstein <icu@weivsara.com>
2002-11-21 07:08:14 +0000

    [ICU-2508] perl perf framework output revision + added charperf perl driver X-Commit-URL: https://ssl.icu-project.org/trac/changeset/10313 Former-commit-id: f5a1a6f635fa043d53bb08a5d5cf766717a740c5

--- icu4c/source/test/perf/normperf/NormPerf.pl
+++ icu4c/source/test/perf/normperf/NormPerf.pl
@@ -9,13 +9,12 @@ use PerfFramework;
 
 my $options = {
 	       "title"=>"Normalization performance: ICU vs. Win",
-	       "headers"=>"ICU Win",
-	       "operationIs"=>"File size in code points",
-	       "timePerOperationIs"=>"Time per code point",
-	       #"passes"=>"10",
-	       #"time"=>"5",
+	       "headers"=>"Win ICU",
+	       "operationIs"=>"code point",
+	       "passes"=>"10",
+	       "time"=>"5",
+	       #"outputType"=>"HTML",
 	       "dataDir"=>"c:/src/perf/data",
-	       "outputType"=>"HTML",
 	       "outputDir"=>"../results"
 	      };
 
@@ -24,31 +23,31 @@ my $options = {
 my $p = "normperf.exe -b -u";
 
 my $tests = { 
-	     "NFC_NFD_Text", ["$p TestICU_NFC_NFD_Text",  "$p TestWin_NFC_NFD_Text" ],
-	     "NFC_NFC_Text", ["$p TestICU_NFC_NFC_Text",  "$p TestWin_NFC_NFC_Text" ],
-	     "NFC_Orig_Text", ["$p TestICU_NFC_Orig_Text", "$p TestWin_NFC_Orig_Text"],
-	     "NFD_NFD_Text", ["$p TestICU_NFD_NFD_Text",  "$p TestWin_NFD_NFD_Text" ],
-	     "NFD_NFC_Text", ["$p TestICU_NFD_NFC_Text",  "$p TestWin_NFD_NFC_Text" ],
-	     "NFD_Orig_Text", ["$p TestICU_NFD_Orig_Text", "$p TestWin_NFD_Orig_Text"]
+	     "NFC_NFD_Text",  ["$p TestWin_NFC_NFD_Text"  ,  "$p TestICU_NFC_NFD_Text" ],
+	     "NFC_NFC_Text",  ["$p TestWin_NFC_NFC_Text"  ,  "$p TestICU_NFC_NFC_Text" ],
+	     "NFC_Orig_Text", ["$p TestWin_NFC_Orig_Text" ,  "$p TestICU_NFC_Orig_Text"],
+	     "NFD_NFD_Text",  ["$p TestWin_NFD_NFD_Text"  ,  "$p TestICU_NFD_NFD_Text" ],
+	     "NFD_NFC_Text",  ["$p TestWin_NFD_NFC_Text"  ,  "$p TestICU_NFD_NFC_Text" ],
+	     "NFD_Orig_Text", ["$p TestWin_NFD_Orig_Text" ,  "$p TestICU_NFD_Orig_Text"]
 	    };
 
 my $dataFiles = {
 		 "",
 		 [
-#		  "TestNames_Asian.txt",
-#		  "TestNames_Chinese.txt",
+		  "TestNames_Asian.txt",
+		  "TestNames_Chinese.txt",
 		  "TestNames_Japanese.txt",
 		  "TestNames_Japanese_h.txt",
 		  "TestNames_Japanese_k.txt",
-#		  "TestNames_Korean.txt",
-#		  "TestNames_Latin.txt",
-#		  "TestNames_SerbianSH.txt",
-#		  "TestNames_SerbianSR.txt",
-#		  "TestNames_Thai.txt",
-#		  "Testnames_Russian.txt",
-#		  "th18057.txt",
-#		  "thesis.txt",
-#		  "vfear11a.txt",
+		  "TestNames_Korean.txt",
+		  "TestNames_Latin.txt",
+		  "TestNames_SerbianSH.txt",
+		  "TestNames_SerbianSR.txt",
+		  "TestNames_Thai.txt",
+		  "Testnames_Russian.txt",
+		  "th18057.txt",
+		  "thesis.txt",
+		  "vfear11a.txt",
 		 ]
 		};
 
