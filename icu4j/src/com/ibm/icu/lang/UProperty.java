ec1158092561fa04e38a49a25d4716a0094397ff
Syn Wee Quek <swquek@svn.icu-project.org>
2002-12-03 00:47:53 +0000

    ICU-2103 javadoc updates, @stable and @drafts addition
    X-SVN-Rev: 10426

--- icu4j/src/com/ibm/icu/lang/UProperty.java
+++ icu4j/src/com/ibm/icu/lang/UProperty.java
@@ -6,8 +6,8 @@
 *
 * $Source: 
 *      /usr/cvs/icu4j/icu4j/src/com/ibm/icu/text/UCharacterCategory.java $ 
-* $Date: 2002/11/06 19:48:58 $ 
-* $Revision: 1.5 $
+* $Date: 2002/12/03 00:47:53 $ 
+* $Revision: 1.6 $
 *
 *******************************************************************************
 */
@@ -30,7 +30,7 @@ package com.ibm.icu.lang;
  * 3.2, then properties marked with "new" are not or not fully 
  * available. Check UCharacter.getUnicodeVersion() to be sure.</p>
  * @author Syn Wee Quek
- * @since March 8 2002
+ * @draft ICU 2.1
  * @see com.ibm.icu.lang.UCharacter
  */
 public interface UProperty
@@ -42,20 +42,24 @@ public interface UProperty
   	 * <p>Property for UCharacter.isUAlphabetic(), different from the property 
   	 * in UCharacter.isalpha().</p>
   	 * <p>Lu + Ll + Lt + Lm + Lo + Nl + Other_Alphabetic.</p>
+     * @draft ICU 2.1
   	 */ 
     public static final int ALPHABETIC = 0; 
     /** 
      * First constant for binary Unicode properties. 
+     * @draft ICU 2.1
      */ 
     public static final int BINARY_START = ALPHABETIC;
     /** 
      * Binary property ASCII_Hex_Digit (0-9 A-F a-f).
+     * @draft ICU 2.1
      */ 
     public static final int ASCII_HEX_DIGIT = 1; 
     /** 
      * <p>Binary property Bidi_Control.</p>
      * <p>Format controls which have specific functions in the Bidi Algorithm.
      * </p> 
+     * @draft ICU 2.1
      */ 
    	public static final int BIDI_CONTROL = 2; 
     /** 
@@ -63,11 +67,13 @@ public interface UProperty
      * <p>Characters that may change display in RTL text.</p> 
      * <p>Property for UCharacter.isMirrored().</p> 
      * <p>See Bidi Algorithm; UTR 9.</p> 
+     * @draft ICU 2.1
      */ 
     public static final int BIDI_MIRRORED = 3; 
     /** 
      * <p>Binary property Dash.</p> 
      * <p>Variations of dashes.</p> 
+     * @draft ICU 2.1
      */ 
     public static final int DASH = 4; 
     /** 
@@ -77,97 +83,114 @@ public interface UProperty
      * </p>
      * <p>Codepoints (2060..206F, FFF0..FFFB, E0000..E0FFF) + 
      * Other_Default_Ignorable_Code_Point + (Cf + Cc + Cs - White_Space)</p> 
+     * @draft ICU 2.1
      */ 
     public static final int DEFAULT_IGNORABLE_CODE_POINT = 5; 
     /** 
      * <p>Binary property Deprecated (new).</p> 
      * <p>The usage of deprecated characters is strongly discouraged.</p> 
+     * @draft ICU 2.1
      */ 
     public static final int DEPRECATED = 6; 
     /** 
      * <p>Binary property Diacritic.</p> 
      * <p>Characters that linguistically modify the meaning of another 
      * character to which they apply.</p> 
+     * @draft ICU 2.1
      */ 
     public static final int DIACRITIC = 7; 
     /** 
      * <p>Binary property Extender.</p> 
      * <p>Extend the value or shape of a preceding alphabetic character, e.g.
      * length and iteration marks.</p> 
+     * @draft ICU 2.1
      */ 
     public static final int EXTENDER = 8; 
     /** 
      * <p>Binary property Full_Composition_Exclusion.</p> 
      * <p>CompositionExclusions.txt + Singleton Decompositions + 
      * Non-Starter Decompositions.</p> 
+     * @draft ICU 2.1
      */ 
     public static final int FULL_COMPOSITION_EXCLUSION = 9; 
     /** 
      * <p>Binary property Grapheme_Base (new).</p>
      * <p>For programmatic determination of grapheme cluster boundaries. 
      * [0..10FFFF]-Cc-Cf-Cs-Co-Cn-Zl-Zp-Grapheme_Link-Grapheme_Extend-CGJ</p> 
+     * @draft ICU 2.1
      */ 
     public static final int GRAPHEME_BASE = 10; 
     /** 
      * <p>Binary property Grapheme_Extend (new).</p> 
      * <p>For programmatic determination of grapheme cluster boundaries.</p> 
      * <p>Me+Mn+Mc+Other_Grapheme_Extend-Grapheme_Link-CGJ</p> 
+     * @draft ICU 2.1
      */ 
     public static final int GRAPHEME_EXTEND = 11; 
     /** 
      * <p>Binary property Grapheme_Link (new).</p> 
      * <p>For programmatic determination of grapheme cluster boundaries.</p> 
+     * @draft ICU 2.1
      */ 
     public static final int GRAPHEME_LINK = 12; 
     /** 
      * <p>Binary property Hex_Digit.</p> 
      * <p>Characters commonly used for hexadecimal numbers.</p> 
+     * @draft ICU 2.1
      */ 
     public static final int HEX_DIGIT = 13; 
     /** 
      * <p>Binary property Hyphen.</p> 
      * <p>Dashes used to mark connections between pieces of words, plus the 
      * Katakana middle dot.</p> 
+     * @draft ICU 2.1
      */ 
     public static final int HYPHEN = 14; 
     /** 
      * <p>Binary property ID_Continue.</p>
      * <p>Characters that can continue an identifier.</p> 
      * <p>ID_Start+Mn+Mc+Nd+Pc</p> 
+     * @draft ICU 2.1
      */ 
     public static final int ID_CONTINUE = 15; 
     /** 
      * <p>Binary property ID_Start.</p> 
      * <p>Characters that can start an identifier.</p> 
      * <p>Lu+Ll+Lt+Lm+Lo+Nl</p> 
+     * @draft ICU 2.1
      */ 
     public static final int ID_START = 16; 
     /** 
      * <p>Binary property Ideographic.</p> 
      * <p>CJKV ideographs.</p> 
+     * @draft ICU 2.1
      */ 
     public static final int IDEOGRAPHIC = 17; 
     /** 
      * <p>Binary property IDS_Binary_Operator (new).</p> 
      * <p>For programmatic determination of Ideographic Description Sequences.
      * </p> 
+     * @draft ICU 2.1
      */ 
     public static final int IDS_BINARY_OPERATOR = 18; 
     /** 
      * <p>Binary property IDS_Trinary_Operator (new).</p> 
      * <p?For programmatic determination of Ideographic Description 
      * Sequences.</p> 
+     * @draft ICU 2.1
      */ 
     public static final int IDS_TRINARY_OPERATOR = 19; 
     /** 
      * <p>Binary property Join_Control.</p> 
      * <p>Format controls for cursive joining and ligation.</p> 
+     * @draft ICU 2.1
      */ 
     public static final int JOIN_CONTROL = 20; 
     /** 
      * <p>Binary property Logical_Order_Exception (new).</p> 
      * <p>Characters that do not use logical order and require special 
      * handling in most processing.</p> 
+     * @draft ICU 2.1
      */ 
     public static final int LOGICAL_ORDER_EXCEPTION = 21; 
     /** 
@@ -175,44 +198,52 @@ public interface UProperty
      * <p>Same as UCharacter.isULowercase(), different from 
      * UCharacter.islower().</p> 
      * <p>Ll+Other_Lowercase</p> 
+     * @draft ICU 2.1
      */ 
     public static final int LOWERCASE = 22; 
     /** <p>Binary property Math.</p> 
      * <p>Sm+Other_Math</p> 
+     * @draft ICU 2.1
      */ 
     public static final int MATH = 23; 
     /** 
      * <p>Binary property Noncharacter_Code_Point.</p> 
      * <p>Code points that are explicitly defined as illegal for the encoding 
      * of characters.</p> 
+     * @draft ICU 2.1
      */ 
     public static final int NONCHARACTER_CODE_POINT = 24; 
     /** 
      * <p>Binary property Quotation_Mark.</p> 
+     * @draft ICU 2.1
      */ 
     public static final int QUOTATION_MARK = 25; 
     /** 
      * <p>Binary property Radical (new).</p> 
      * <p>For programmatic determination of Ideographic Description 
      * Sequences.</p> 
+     * @draft ICU 2.1
      */ 
     public static final int RADICAL = 26; 
     /** 
      * <p>Binary property Soft_Dotted (new).</p> 
      * <p>Characters with a "soft dot", like i or j.</p>
      * <p>An accent placed on these characters causes the dot to disappear.</p> 
+     * @draft ICU 2.1
      */ 
     public static final int SOFT_DOTTED = 27; 
     /** 
      * <p>Binary property Terminal_Punctuation.</p> 
      * <p>Punctuation characters that generally mark the end of textual 
      * units.</p> 
+     * @draft ICU 2.1
      */ 
     public static final int TERMINAL_PUNCTUATION = 28; 
     /** 
      * <p>Binary property Unified_Ideograph (new).</p> 
      * <p>For programmatic determination of Ideographic Description 
      * Sequences.</p> 
+     * @draft ICU 2.1
      */ 
     public static final int UNIFIED_IDEOGRAPH = 29; 
     /** 
@@ -220,6 +251,7 @@ public interface UProperty
      * <p>Same as UCharacter.isUUppercase(), different from 
      * UCharacter.isUpperCase().</p> 
      * <p>Lu+Other_Uppercase</p> 
+     * @draft ICU 2.1
      */ 
     public static final int UPPERCASE = 30; 
     /** 
@@ -227,22 +259,26 @@ public interface UProperty
      * <p>Same as UCharacter.isUWhiteSpace(), different from 
      * UCharacter.isSpace() and UCharacter.isWhitespace().</p> 
      * Space characters+TAB+CR+LF-ZWSP-ZWNBSP</p> 
+     * @draft ICU 2.1
      */ 
     public static final int WHITE_SPACE = 31; 
     /** 
      * <p>Binary property XID_Continue.</p> 
      * <p>ID_Continue modified to allow closure under normalization forms 
      * NFKC and NFKD.</p> 
+     * @draft ICU 2.1
      */ 
     public static final int XID_CONTINUE = 32; 
     /** 
      * <p>Binary property XID_Start.</p> 
      * <p>ID_Start modified to allow closure under normalization forms NFKC 
      * and NFKD.</p> 
+     * @draft ICU 2.1
      */ 
     public static final int XID_START = 33; 
     /** 
      * <p>One more than the last constant for binary Unicode properties.</p> 
+     * @draft ICU 2.1
      */ 
     public static final int BINARY_LIMIT = 34;
     /** 
@@ -442,7 +478,7 @@ public interface UProperty
      *
      * @see UCharacter#getPropertyName
      * @see UCharacter#getPropertyValueName
-     * @since ICU 2.4
+     * @draft ICU 2.4
      */
     public interface NameChoice {
         /**
