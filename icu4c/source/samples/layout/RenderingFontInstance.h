1bcfed9ba623f1bebf48c969452b593ba9af115a
Eric Mader <emader@svn.icu-project.org>
2002-12-05 22:50:29 +0000

    ICU-2243 Initial version of the high-level LayoutEngine API, added to the layout sample.
    X-SVN-Rev: 10512

--- icu4c/source/samples/layout/RenderingFontInstance.h
+++ icu4c/source/samples/layout/RenderingFontInstance.h
@@ -19,13 +19,18 @@
 #include "LEFontInstance.h"
 #include "cmaps.h"
 
-#define TABLE_CACHE_INIT 5
-#define TABLE_CACHE_GROW 5
+struct TableCacheEntry;
 
-struct TableCacheEntry
+class RenderingFontInstance;
+
+class RenderingSurface
 {
-    LETag tag;
-    void *table;
+public:
+    RenderingSurface() {};
+    virtual ~RenderingSurface() {};
+
+    virtual void drawGlyphs(const RenderingFontInstance *font, const LEGlyphID *glyphs, le_int32 count,
+                    const le_int32 *dx, le_int32 x, le_int32 y, le_int32 width, le_int32 height) const = 0;
 };
 
 enum RFIErrorCode {
@@ -40,7 +45,7 @@ enum RFIErrorCode {
 class RenderingFontInstance : public LEFontInstance
 {
 protected:
-    void *fSurface;
+    RenderingSurface *fSurface;
 
     le_int32 fPointSize;
     le_int32 fUnitsPerEM;
@@ -63,116 +68,140 @@ protected:
     virtual const void *readFontTable(LETag tableTag) const = 0;
 
 public:
-    RenderingFontInstance(void *surface, le_int16 pointSize);
+    RenderingFontInstance(RenderingSurface *surface, le_int16 pointSize);
 
     virtual ~RenderingFontInstance();
 
     virtual const void *getFontTable(LETag tableTag) const;
 
-    virtual le_bool canDisplay(LEUnicode32 ch) const
-    {
-        return fMapper->unicodeToGlyph(ch) != 0;
-    };
+    virtual le_bool canDisplay(LEUnicode32 ch) const;
 
-    virtual le_int32 getUnitsPerEM() const
-    {
-        return fUnitsPerEM;
-    };
+    virtual le_int32 getUnitsPerEM() const;
 
-    virtual le_int32 getLineHeight() const
-    {
-        return getAscent() + getDescent() + getLeading();
-    };
+    virtual le_int32 getLineHeight() const;
 
-    virtual le_int32 getAscent() const
-    {
-        return fAscent;
-    };
+    virtual le_int32 getAscent() const;
 
-    virtual le_int32 getDescent() const
-    {
-        return fDescent;
-    };
+    virtual le_int32 getDescent() const;
 
-    virtual le_int32 getLeading() const
-    {
-        return fLeading;
-    };
+    virtual le_int32 getLeading() const;
 
     virtual void mapCharsToGlyphs(const LEUnicode chars[], le_int32 offset, le_int32 count, le_bool reverse, const LECharMapper *mapper, LEGlyphID glyphs[]) const;
 
     virtual LEGlyphID mapCharToGlyph(LEUnicode32 ch, const LECharMapper *mapper) const;
 
-    virtual le_int32 getName(le_uint16 platformID, le_uint16 scriptID, le_uint16 languageID, le_uint16 nameID, LEUnicode *name) const
-    {
-        // This is only used for CDAC fonts, and we'll have to loose that support anyhow...
-        //return (le_int32) fFontObject->getName(platformID, scriptID, languageID, nameID, name);
-        if (name != NULL) {
-            *name = 0;
-        }
-
-        return 0;
-    };
+    virtual le_int32 getName(le_uint16 platformID, le_uint16 scriptID, le_uint16 languageID, le_uint16 nameID, LEUnicode *name) const;
 
     virtual void getGlyphAdvance(LEGlyphID glyph, LEPoint &advance) const = 0;
 
     virtual le_bool getGlyphPoint(LEGlyphID glyph, le_int32 pointNumber, LEPoint &point) const = 0;
 
-    virtual const void setFont(void *surface) const
-    {
-        // default implementation is to ignore this
-    };
-    
-    virtual void drawGlyphs(void *surface, LEGlyphID *glyphs, le_uint32 count, le_int32 *dx,
-        le_int32 x, le_int32 y, le_int32 width, le_int32 height) const = 0;
+    float getXPixelsPerEm() const;
 
-    float getXPixelsPerEm() const
-    {
-        return (float) fPointSize;
-    };
+    float getYPixelsPerEm() const;
 
-    float getYPixelsPerEm() const
-    {
-        return  (float) fPointSize;
-    };
+    float xUnitsToPoints(float xUnits) const;
 
-    float xUnitsToPoints(float xUnits) const
-    {
-        return (xUnits * fPointSize) / (float) fUnitsPerEM;
-    };
+    float yUnitsToPoints(float yUnits) const;
 
-    float yUnitsToPoints(float yUnits) const
-    {
-        return (yUnits * fPointSize) / (float) fUnitsPerEM;
-    };
+    void unitsToPoints(LEPoint &units, LEPoint &points) const;
 
-    void unitsToPoints(LEPoint &units, LEPoint &points) const
-    {
-        points.fX = xUnitsToPoints(units.fX);
-        points.fY = yUnitsToPoints(units.fY);
-    }
+    float xPixelsToUnits(float xPixels) const;
 
-    float xPixelsToUnits(float xPixels) const
-    {
-        return (xPixels * fUnitsPerEM) / (float) fPointSize;
-    };
+    float yPixelsToUnits(float yPixels) const;
 
-    float yPixelsToUnits(float yPixels) const
-    {
-        return (yPixels * fUnitsPerEM) / (float) fPointSize;
-    };
+    void pixelsToUnits(LEPoint &pixels, LEPoint &units) const;
 
-    void pixelsToUnits(LEPoint &pixels, LEPoint &units) const
-    {
-        units.fX = xPixelsToUnits(pixels.fX);
-        units.fY = yPixelsToUnits(pixels.fY);
-    };
-
-    void transformFunits(float xFunits, float yFunits, LEPoint &pixels) const
-    {
-        pixels.fX = xUnitsToPoints(xFunits) * fDeviceScaleX;
-        pixels.fY = yUnitsToPoints(yFunits) * fDeviceScaleY;
-    }
+    void transformFunits(float xFunits, float yFunits, LEPoint &pixels) const;
 };
+
+inline le_bool RenderingFontInstance::canDisplay(LEUnicode32 ch) const
+{
+    return fMapper->unicodeToGlyph(ch) != 0;
+}
+
+inline le_int32 RenderingFontInstance::getUnitsPerEM() const
+{
+    return fUnitsPerEM;
+}
+
+inline le_int32 RenderingFontInstance::getLineHeight() const
+{
+    return getAscent() + getDescent() + getLeading();
+}
+
+inline le_int32 RenderingFontInstance::getAscent() const
+{
+    return fAscent;
+}
+
+inline le_int32 RenderingFontInstance::getDescent() const
+{
+    return fDescent;
+}
+
+inline le_int32 RenderingFontInstance::getLeading() const
+{
+    return fLeading;
+}
+
+inline le_int32 RenderingFontInstance::getName(le_uint16 platformID, le_uint16 scriptID, le_uint16 languageID, le_uint16 nameID, LEUnicode *name) const
+{
+    // This is only used for CDAC fonts, and we'll have to loose that support anyhow...
+    //return (le_int32) fFontObject->getName(platformID, scriptID, languageID, nameID, name);
+    if (name != NULL) {
+        *name = 0;
+    }
+
+    return 0;
+}
+
+inline float RenderingFontInstance::getXPixelsPerEm() const
+{
+    return (float) fPointSize;
+}
+
+inline float RenderingFontInstance::getYPixelsPerEm() const
+{
+    return  (float) fPointSize;
+}
+
+inline float RenderingFontInstance::xUnitsToPoints(float xUnits) const
+{
+    return (xUnits * fPointSize) / (float) fUnitsPerEM;
+}
+
+inline float RenderingFontInstance::yUnitsToPoints(float yUnits) const
+{
+    return (yUnits * fPointSize) / (float) fUnitsPerEM;
+}
+
+inline void RenderingFontInstance::unitsToPoints(LEPoint &units, LEPoint &points) const
+{
+    points.fX = xUnitsToPoints(units.fX);
+    points.fY = yUnitsToPoints(units.fY);
+}
+
+inline float RenderingFontInstance::xPixelsToUnits(float xPixels) const
+{
+    return (xPixels * fUnitsPerEM) / (float) fPointSize;
+}
+
+inline float RenderingFontInstance::yPixelsToUnits(float yPixels) const
+{
+    return (yPixels * fUnitsPerEM) / (float) fPointSize;
+}
+
+inline void RenderingFontInstance::pixelsToUnits(LEPoint &pixels, LEPoint &units) const
+{
+    units.fX = xPixelsToUnits(pixels.fX);
+    units.fY = yPixelsToUnits(pixels.fY);
+}
+
+inline void RenderingFontInstance::transformFunits(float xFunits, float yFunits, LEPoint &pixels) const
+{
+    pixels.fX = xUnitsToPoints(xFunits) * fDeviceScaleX;
+    pixels.fY = yUnitsToPoints(yFunits) * fDeviceScaleY;
+}
 
 #endif
