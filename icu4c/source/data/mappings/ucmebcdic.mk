a32007efe6ea9bbf0e0c9b9fd225da54a54d972d
George Rhoten <grhoten@users.noreply.github.com>
2002-12-13 04:08:27 +0000

    ICU-2573 Change some of the converters to use UTR22 names
    X-SVN-Rev: 10656

--- icu4c/source/data/mappings/ucmebcdic.mk
+++ icu4c/source/data/mappings/ucmebcdic.mk
@@ -4,19 +4,24 @@
 # ibm-37 and ibm-1047 are already mentioned in makedata.mak and Makefile.in
 
 UCM_SOURCE_EBCDIC = ebcdic-xml-us.ucm\
-ibm-1025_P100-2000.ucm ibm-1026_P100-2000.ucm ibm-1097_P100-2000.ucm\
-ibm-1097_X100-2000.ucm ibm-1112_P100-2000.ucm ibm-1122_P100-2000.ucm\
-ibm-1130_P100-2000.ucm ibm-1132_P100-2000.ucm ibm-1137_P100-2000.ucm\
-ibm-1364_P110-2000.ucm ibm-1371.ucm ibm-1388_P103-2001.ucm\
-ibm-1390.ucm ibm-1399.ucm ibm-870_P100-2000.ucm\
-ibm-875_P100-2000.ucm ibm-9030_P100-2000.ucm ibm-918_P100-2000.ucm\
-ibm-918_X100-2000.ucm ibm-930.ucm ibm-935.ucm ibm-937.ucm ibm-939.ucm\
-ibm-1123.ucm ibm-1140.ucm ibm-1141.ucm ibm-1142.ucm\
-ibm-1143.ucm ibm-1144.ucm ibm-1145.ucm ibm-1146.ucm ibm-1147.ucm\
-ibm-1148.ucm ibm-1149.ucm ibm-1153.ucm ibm-1154.ucm ibm-1155.ucm\
-ibm-1156.ucm ibm-1157.ucm ibm-1158.ucm ibm-1159.ucm ibm-1160.ucm\
-ibm-1164.ucm ibm-12712.ucm ibm-16804.ucm ibm-273.ucm ibm-277.ucm\
-ibm-278.ucm ibm-280.ucm ibm-284.ucm ibm-285.ucm ibm-290.ucm\
-ibm-297.ucm ibm-420.ucm ibm-424.ucm ibm-4899.ucm ibm-4971.ucm\
-ibm-500.ucm ibm-5123.ucm ibm-803.ucm ibm-8482.ucm ibm-871.ucm\
-ibm-933.ucm ibm-9027.ucm ibm-16684.ucm
+ibm-1025_P100-1995.ucm ibm-1026_P100-1995.ucm ibm-1097_P100-1995.ucm\
+ibm-1112_P100-1995.ucm ibm-1122_P100-1999.ucm ibm-1130_P100-1997.ucm\
+ibm-1132_P100-1998.ucm ibm-1137_P100-1999.ucm ibm-1364_P110-2000.ucm\
+ibm-1371_P100-1999.ucm ibm-1388_P103-2001.ucm ibm-1390_P100-1999.ucm\
+ibm-1399_P100-1999.ucm ibm-870_P100-2000.ucm ibm-875_P100-2000.ucm\
+ibm-9030_P100-2000.ucm ibm-918_P100-2000.ucm ibm-930_P120-1999.ucm\
+ibm-933_P110-1995.ucm ibm-935_P110-1999.ucm ibm-937_P110-1999.ucm\
+ibm-939_P120-1999.ucm ibm-1123_P100-1995.ucm ibm-1140_P100-1997.ucm\
+ibm-1141_P100-1997.ucm ibm-1142_P100-1997.ucm ibm-1143_P100-1997.ucm\
+ibm-1144_P100-1997.ucm ibm-1145_P100-1997.ucm ibm-1146_P100-1997.ucm\
+ibm-1147_P100-1997.ucm ibm-1148_P100-1997.ucm ibm-1149_P100-1997.ucm\
+ibm-1153_P100-1999.ucm ibm-1154_P100-1999.ucm ibm-1155_P100-1999.ucm\
+ibm-1156_P100-1999.ucm ibm-1157_P100-1999.ucm ibm-1158_P100-1999.ucm\
+ibm-1159_P100-1999.ucm ibm-1160_P100-1999.ucm ibm-1164_P100-1999.ucm\
+ibm-12712_P100-1998.ucm ibm-16804_X110-1999.ucm ibm-273_P100-1995.ucm\
+ibm-277_P100-1995.ucm ibm-278_P100-1995.ucm ibm-280_P100-1995.ucm\
+ibm-284_P100-1995.ucm ibm-285_P100-1995.ucm ibm-290_P100-1995.ucm\
+ibm-297_P100-1995.ucm ibm-420_X120-1999.ucm ibm-424_P100-1995.ucm\
+ibm-4899_P100-1998.ucm ibm-4971_P100-1999.ucm ibm-500.ucm\
+ibm-5123.ucm ibm-803.ucm ibm-8482.ucm\
+ibm-871.ucm ibm-9027_P100-1999.ucm ibm-16684_P100-1999.ucm
