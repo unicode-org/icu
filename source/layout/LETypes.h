4f1382314b852aafdb00841374a4299dc1257df5
Eric Mader <emader@svn.icu-project.org>
2002-12-16 23:12:45 +0000

    Build LEScripts using ICU4J interfaces to guarantee synch w/ ICU. Add lifecycle tags.
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/2565
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@10691 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/layout/LETypes.h
+++ source/layout/LETypes.h
@@ -2,7 +2,7 @@
 /*
  * @(#)LETypes.h	1.2 00/03/15
  *
- * (C) Copyright IBM Corp. 1998, 1999, 2000 - All Rights Reserved
+ * (C) Copyright IBM Corp. 1998-2002 - All Rights Reserved
  *
  */
 
@@ -11,39 +11,128 @@
 
 #include "unicode/utypes.h"
 #include "unicode/uobject.h"
-//#include "cmemory.h"
 
 U_NAMESPACE_BEGIN
 
+/**
+ * A type used for signed, 32-bit integers.
+ *
+ * @stable ICU 2.4
+ */
 typedef int32_t le_int32;
+
+/**
+ * A type used for unsigned, 32-bit integers.
+ *
+ * @stable ICU 2.4
+ */
 typedef uint32_t le_uint32;
+
+/**
+ * A type used for signed, 16-bit integers.
+ *
+ * @stable ICU 2.4
+ */
 typedef int16_t le_int16;
+
+/**
+ * A type used for unsigned, 16-bit integers.
+ *
+ * @stable ICU 2.4
+ */
 typedef uint16_t le_uint16;
+
+/**
+ * A type used for signed, 8-bit integers.
+ *
+ * @stable ICU 2.4
+ */
 typedef int8_t le_int8;
+
+/**
+ * A type used for unsigned, 8-bit integers.
+ *
+ * @stable ICU 2.4
+ */
 typedef uint8_t le_uint8;
 
+
+/**
+ * A type used for boolean values.
+ *
+ * @stable ICU 2.4
+ */
 typedef UBool le_bool;
 
 #ifndef true
+/**
+ * Used for <code>le_bool</code> values which are <code>true</code>.
+ *
+ * @stable ICU 2.4
+ */
 #define true 1
 #endif
 
 #ifndef false
+/**
+ * Used for <code>le_bool</code> values which are <code>false</code>.
+ *
+ * @stable ICU 2.4
+ */
 #define false 0
 #endif
 
 #ifndef NULL
+/**
+ * Used to represent empty pointers.
+ *
+ * @stable ICU 2.4
+ */
 #define NULL 0
 #endif
 
+/**
+ * Used for four character tags.
+ *
+ * @stable ICU 2.4
+ */
 typedef le_uint32 LETag;
 
+/**
+ * Used for glyph indices.
+ *
+ * (NOTE: this will likely change to le_uint32)
+ *
+ * @draft ICU 2.4
+ */
 typedef le_uint16 LEGlyphID;
 
+/**
+ * Used to represent 16-bit Unicode code points.
+ *
+ * @stable ICU 2.4
+ */
 typedef UChar LEUnicode16;
-typedef UChar32 LEUnicode32;
-typedef UChar LEUnicode;    // FIXME: we should deprecate this type in favor of LEUnicode16...
 
+/**
+ * Used to represent 32-bit Unicode code points.
+ *
+ * @stable ICU 2.4
+ */
+typedef UChar32 LEUnicode32;
+
+/**
+ * Used to represent 16-bit Unicode code points.
+ *
+ * @deprecated since ICU 2.4. Use LEUnicode16 instead
+ */
+typedef UChar LEUnicode;
+
+/**
+ * Used to hold a pair of (x, y) values which represent a point.
+ *
+ * @stable ICU 2.4
+ */
 struct LEPoint
 {
     float fX;
@@ -51,11 +140,26 @@ struct LEPoint
 };
 
 #ifndef XP_CPLUSPLUS
+/**
+ * Used to hold a pair of (x, y) values which represent a point.
+ *
+ * @stable ICU 2.4
+ */
 typedef struct LEPoint LEPoint;
 #endif
 
+/**
+ * A convenience macro for copying an array.
+ *
+ * @stable ICU 2.4
+ */
 #define LE_ARRAY_COPY(dst, src, count) memcpy(dst, src, (count) * sizeof (src)[0])
 
+/**
+ * Error codes returned by the LayoutEngine.
+ *
+ * @stable ICU 2.4
+ */
 enum LEErrorCode {
     /* informational */
     // none right now...
@@ -72,10 +176,26 @@ enum LEErrorCode {
 };
 
 #ifndef XP_CPLUSPLUS
+/**
+ * Error codes returned by the LayoutEngine.
+ *
+ * @stable ICU 2.4
+ */
 typedef enum LEErrorCode LEErrorCode;
 #endif
 
+/**
+ * A convenience macro to test for the success of a LayoutEngine call.
+ *
+ * @stable ICU 2.4
+ */
 #define LE_SUCCESS(code) (U_SUCCESS((UErrorCode)code))
+
+/**
+ * A convenience macro to test for the failure of a LayoutEngine call.
+ *
+ * @stable ICU 2.4
+ */
 #define LE_FAILURE(code) (U_FAILURE((UErrorCode)code))
 
 U_NAMESPACE_END
