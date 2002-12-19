03a1aa77bcedca7b0a4810fa87a0a88db349b129
Alan Liu <alansliu@gmail.com>
2002-12-19 00:43:21 +0000

    ICU-2565 fix doxygen warnings
    X-SVN-Rev: 10758

--- icu4c/source/common/unicode/unifilt.h
+++ icu4c/source/common/unicode/unifilt.h
@@ -68,11 +68,13 @@ public:
     /**
      * UnicodeFunctor API.  Cast 'this' to a UnicodeMatcher* pointer
      * and return the pointer.
+     * @draft ICu 2.4
      */
     virtual UnicodeMatcher* toMatcher() const;
 
     /**
      * Implement UnicodeMatcher API.
+     * @draft ICu 2.4
      */
     virtual UMatchDegree matches(const Replaceable& text,
                                  int32_t& offset,
@@ -81,6 +83,7 @@ public:
 
     /**
      * UnicodeFunctor API.  Nothing to do.
+     * @draft ICu 2.4
      */
     virtual void setData(const TransliterationRuleData*) {}
 
