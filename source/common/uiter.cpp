95d53b24bce34d06e5322e64e4352a7767345000
Markus Scherer <markus.icu@gmail.com>
2003-01-13 18:03:34 +0000

    fix uiter_setUTF16BE() for big-endian machines and length=-1
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/2397
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@10846 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/uiter.cpp
+++ source/common/uiter.cpp
@@ -21,7 +21,8 @@
 #include "unicode/uiter.h"
 #include "cstring.h"
 
-#define IS_POINTER_EVEN(p) (((size_t)p&1)==0)
+#define IS_EVEN(n) (((n)&1)==0)
+#define IS_POINTER_EVEN(p) IS_EVEN((size_t)p)
 
 U_CDECL_BEGIN
 
@@ -277,19 +278,22 @@ utf16BE_strlen(const char *s) {
 
 U_CAPI void U_EXPORT2
 uiter_setUTF16BE(UCharIterator *iter, const char *s, int32_t length) {
-    if(iter!=0) {
+    if(iter!=NULL) {
         /* allow only even-length strings (the input length counts bytes) */
-        if(s!=0 && length==-1 || (length>=0 && (length&1)==0)) {
+        if(s!=NULL && (length==-1 || (length>=0 && IS_EVEN(length)))) {
+            /* length/=2, except that >>=1 also works for -1 (-1/2==0, -1>>1==-1) */
+            length>>=1;
+
             if(U_IS_BIG_ENDIAN && IS_POINTER_EVEN(s)) {
                 /* big-endian machine and 2-aligned UTF-16BE string: use normal UChar iterator */
-                uiter_setString(iter, (const UChar *)s, length/2);
+                uiter_setString(iter, (const UChar *)s, length);
                 return;
             }
 
             *iter=utf16BEIterator;
             iter->context=s;
             if(length>=0) {
-                iter->length=length/2;
+                iter->length=length;
             } else {
                 iter->length=utf16BE_strlen(s);
             }
