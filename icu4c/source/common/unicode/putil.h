c5f04aa8a21eee1ccb0707cff12c3e3dc8579396
George Rhoten <grhoten@users.noreply.github.com>
2003-02-11 22:42:18 +0000

    [ICU-2707] Fix for Red Hat Linux 8 with gcc 3.2 X-Commit-URL: https://ssl.icu-project.org/trac/changeset/11020 Former-commit-id: d0e2e9167b1145f51948d37e8175ef17c83b75d6

--- icu4c/source/common/unicode/putil.h
+++ icu4c/source/common/unicode/putil.h
@@ -302,21 +302,6 @@ U_CAPI const char*  U_EXPORT2 uprv_getDefaultCodepage(void);
  */
 U_CAPI const char*  U_EXPORT2 uprv_getDefaultLocaleID(void);
 
-#ifdef ICU_NEXTDOUBLE_USE_DEPRECATES
-/*
- * Finds the least double greater than d (if positive == true),
- * or the greatest double less than d (if positive == false).
- *
- * This is a special purpose function defined by the ChoiceFormat API
- * documentation.
- * It is not a general purpose function and not defined for NaN or Infinity
- * @return the least double greater than d (if positive == true),
- * or the greatest double less than d (if positive == false).
- * @obsolete ICU 2.2. Use the ChoiceFormat closures API instead since this API will be removed in that release.
- */
-U_CAPI double       U_EXPORT2 uprv_nextDouble(double d, UBool positive);
-#endif /* ICU_NEXTDOUBLE_USE_DEPRECATES */
-
 /**
  * Filesystem file and path separator characters.
  * Example: '/' and ':' on Unix, '\\' and ';' on Windows.
