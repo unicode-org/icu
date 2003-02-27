7868539e2be93dabf1aec8fb2c4069ca44fc9b8e
Syn Wee Quek <swquek@svn.icu-project.org>
2003-02-27 00:51:02 +0000

    [ICU-2735]array access error fixed X-Commit-URL: https://ssl.icu-project.org/trac/changeset/11173 Former-commit-id: 36e9bb98206359a214a0da6f7675c3aa2ff30615

--- icu4j/src/com/ibm/icu/dev/test/collator/CollationMiscTest.java
+++ icu4j/src/com/ibm/icu/dev/test/collator/CollationMiscTest.java
@@ -1757,7 +1757,7 @@ public class CollationMiscTest extends TestFmwk{
     
     public void TestUCARules() 
     {
-        try {
+       try {
             RuleBasedCollator coll 
                 = (RuleBasedCollator)Collator.getInstance(Locale.ENGLISH);
             String rule 
