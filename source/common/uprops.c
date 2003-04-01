b05c5641cf87cea3fb5ebdd5183273daeb0c6bef
Ram Viswanadha <rviswanadha@gmail.com>
2003-04-01 22:06:31 +0000

    fix minor bug in u_getIntPropertyValue api
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/2427
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@11431 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/uprops.c
+++ source/common/uprops.c
@@ -303,7 +303,7 @@ u_getIntPropertyMaxValue(UProperty which) {
         case UCHAR_CANONICAL_COMBINING_CLASS:
             return 0xff; /* TODO do we need to be more precise, getting the actual maximum? */
         case UCHAR_DECOMPOSITION_TYPE:
-            max=uprv_getMaxValues(0)&UPROPS_DT_MASK;
+            max=uprv_getMaxValues(1)&UPROPS_DT_MASK;
             return max!=0 ? max : (int32_t)U_DT_COUNT-1;
         case UCHAR_EAST_ASIAN_WIDTH:
             max=(uprv_getMaxValues(0)&UPROPS_EA_MASK)>>UPROPS_EA_SHIFT;
@@ -311,10 +311,10 @@ u_getIntPropertyMaxValue(UProperty which) {
         case UCHAR_GENERAL_CATEGORY:
             return (int32_t)U_CHAR_CATEGORY_COUNT-1;
         case UCHAR_JOINING_GROUP:
-            max=(uprv_getMaxValues(0)&UPROPS_JG_MASK)>>UPROPS_JG_SHIFT;
+            max=(uprv_getMaxValues(1)&UPROPS_JG_MASK)>>UPROPS_JG_SHIFT;
             return max!=0 ? max : (int32_t)U_JG_COUNT-1;
         case UCHAR_JOINING_TYPE:
-            max=(uprv_getMaxValues(0)&UPROPS_JT_MASK)>>UPROPS_JT_SHIFT;
+            max=(uprv_getMaxValues(1)&UPROPS_JT_MASK)>>UPROPS_JT_SHIFT;
             return max!=0 ? max : (int32_t)U_JT_COUNT-1;
         case UCHAR_LINE_BREAK:
             max=(uprv_getMaxValues(0)&UPROPS_LB_MASK)>>UPROPS_LB_SHIFT;
