724fa05061134ab9092ad766738b57b0abff9221
George Rhoten <grhoten@users.noreply.github.com>
2003-04-05 09:50:53 +0000

    [ICU-2781] Use more UTR22 names, add more aliases, use more windows- aliases. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/11465 Former-commit-id: fcf9089c2eebeb09f35b3cae54748ac904fbc003

--- icu4c/source/data/mappings/ucmfiles.mk
+++ icu4c/source/data/mappings/ucmfiles.mk
@@ -27,30 +27,30 @@
 
 UCM_SOURCE_FILES = ibm-367_P100-1995.ucm\
 ibm-437_P100-1995.ucm\
-ibm-813.ucm\
-ibm-850.ucm\
-ibm-851.ucm\
-ibm-852.ucm\
-ibm-855.ucm\
-ibm-856.ucm\
-ibm-857.ucm\
-ibm-858.ucm\
-ibm-860.ucm\
-ibm-861.ucm\
-ibm-862.ucm\
-ibm-863.ucm\
-ibm-864.ucm\
-ibm-865.ucm\
-ibm-866.ucm\
-ibm-867.ucm\
-ibm-868.ucm\
-ibm-869.ucm\
-ibm-874.ucm\
-ibm-878.ucm\
-ibm-901.ucm\
-ibm-902.ucm\
-ibm-921.ucm\
-ibm-922.ucm\
+ibm-813_P100-1995.ucm\
+ibm-850_P100-1995.ucm\
+ibm-851_P100-1995.ucm\
+ibm-852_P100-1995.ucm\
+ibm-855_P100-1995.ucm\
+ibm-856_P100-1995.ucm\
+ibm-857_P100-1995.ucm\
+ibm-858_P100-1997.ucm\
+ibm-860_P100-1995.ucm\
+ibm-861_P100-1995.ucm\
+ibm-862_P100-1995.ucm\
+ibm-863_P100-1995.ucm\
+ibm-864_X110-1999.ucm\
+ibm-865_P100-1995.ucm\
+ibm-866_P100-1995.ucm\
+ibm-867_P100-1998.ucm\
+ibm-868_P100-1995.ucm\
+ibm-869_P100-1995.ucm\
+ibm-874_P100-1995.ucm\
+ibm-878_P100-1996.ucm\
+ibm-901_P100-1999.ucm\
+ibm-902_P100-1999.ucm\
+ibm-921_P100-1995.ucm\
+ibm-922_P100-1999.ucm\
 ibm-1006_P100-1995.ucm\
 ibm-1051_P100-1995.ucm\
 ibm-1098_P100-1995.ucm\
@@ -76,15 +76,16 @@ ibm-1280_P100-1996.ucm\
 ibm-1281_P100-1996.ucm\
 ibm-1282_P100-1996.ucm\
 ibm-1283_P100-1996.ucm\
-ibm-942_P12A-2000.ucm\
-ibm-943_P130-2000.ucm\
-ibm-949_P110-2000.ucm\
+ibm-942_P12A-1999.ucm\
+ibm-943_P130-1999.ucm\
+ibm-949_P110-1999.ucm\
 ibm-950_P110-1999.ucm\
-ibm-1363_P110-2000.ucm\
-ibm-1381_P110-2000.ucm\
-ibm-33722_P120-2000.ucm\
+ibm-964_P110-1999.ucm\
+ibm-1363_P110-1997.ucm\
+ibm-1381_P110-1999.ucm\
+ibm-33722_P120-1999.ucm\
 jisx-201.ucm jisx-208.ucm jisx-212.ucm\
 iso-ir-165.ucm cns-11643-1992.ucm\
 ibm-5478_P100-1995.ucm\
-ibm-964_P110-2000.ucm icu-internal-25546.ucm lmb-excp.ucm
+icu-internal-25546.ucm lmb-excp.ucm
 
