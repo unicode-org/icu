a6ec3024bb7c3f53075ddb498ed980f1647a631c
George Rhoten <grhoten@users.noreply.github.com>
2003-04-10 20:34:07 +0000

    ICU-2784 Add some frequently used control codes to exemplar characters for these languages.
    X-SVN-Rev: 11509

--- icu4c/source/test/cintltst/cloctst.c
+++ icu4c/source/test/cintltst/cloctst.c
@@ -1913,7 +1913,7 @@ findStringSetMismatch(const UChar *string, int32_t langSize,
 
     for (strIdx = 0; strIdx < langSize; strIdx++) {
         if (!uset_contains(exemplarSet, string[strIdx])
-            && string[strIdx] != 0x0020 && string[strIdx] != 0x200c && string[strIdx] != 0x002e && string[strIdx] != 0x002c && string[strIdx] != 0x002d) {
+            && string[strIdx] != 0x0020 && string[strIdx] != 0x002e && string[strIdx] != 0x002c && string[strIdx] != 0x002d) {
             if (!ignoreNumbers || (ignoreNumbers && (string[strIdx] < 0x30 || string[strIdx] > 0x39))) {
                 return strIdx;
             }
