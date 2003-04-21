b8bac331e64df598727a7a18955984f16f586033
Alan Liu <alansliu@gmail.com>
2003-04-21 19:16:38 +0000

    ICU-2119 enhance and cleanup tests; add more parsing checks
    X-SVN-Rev: 11608

--- icu4c/source/test/intltest/numfmtst.h
+++ icu4c/source/test/intltest/numfmtst.h
@@ -19,68 +19,63 @@
  * Performs various in-depth test on NumberFormat
  **/
 class NumberFormatTest: public CalendarTimeZoneTest {
+
     // IntlTest override
     void runIndexedTest( int32_t index, UBool exec, const char* &name, char* par );
-public:
+ public:
+
     /**
      * Test APIs (to increase code coverage)
      */   
-    virtual void TestAPI(void);
+    void TestAPI(void);
     /**
      * Test the handling of quotes
      **/
-    virtual void TestQuotes(void);
+    void TestQuotes(void);
     /**
      * Test patterns with exponential representation
      **/
-    virtual void TestExponential(void);
+    void TestExponential(void);
     /**
      * Test handling of patterns with currency symbols
      **/
-    virtual void TestCurrencySign(void);
+    void TestCurrencySign(void);
     /**
      * Test different format patterns
      **/
-    virtual void TestPatterns(void);
+    void TestPatterns(void);
     /**
      * API coverage for DigitList
      **/
-    virtual void TestDigitListAPI(void);
+    void TestDigitListAPI(void);
     
-public: // package
-    // internal utility routine
-    static UnicodeString& escape(UnicodeString& s);
-
-public:
     /**
      * Test localized currency patterns.
      */
-    virtual void TestCurrency(void);
+    void TestCurrency(void);
 
     /**
      * Test the Currency object handling, new as of ICU 2.2.
      */
     void TestCurrencyObject(void);
 
-    void expectCurrency(NumberFormat& nf, const Locale& locale,
-                        double value, const UnicodeString& string);
-
     void TestCurrencyPatterns(void);
 
     /**
      * Do rudimentary testing of parsing.
      */
-    virtual void TestParse(void);
+    void TestParse(void);
     /**
      * Test proper rounding by the format method.
      */
-    virtual void TestRounding487(void);
+    void TestRounding487(void);
 
     // New tests for alphaWorks upgrade
-    virtual void TestExponent(void);
-    virtual void TestScientific(void);
+    void TestExponent(void);
 
-    virtual void TestSurrogateSupport(void);
+    void TestScientific(void);
+
+    void TestSurrogateSupport(void);
 
     /**
      * Test the functioning of the secondary grouping value.
@@ -89,50 +84,85 @@ public:
 
     void TestWhiteSpaceParsing(void);
         
-    void expect(NumberFormat& fmt, const UnicodeString& str, int32_t n);
-    void expect(NumberFormat& fmt, const char *str, int32_t n) {
+    void TestPad(void);
+    void TestPatterns2(void);
+
+ private:
+
+    static UBool equalValue(const Formattable& a, const Formattable& b);
+
+    void expect2(NumberFormat& fmt, const Formattable& n, const UnicodeString& str);
+
+    void expect2(NumberFormat& fmt, const Formattable& n, const char* str) {
+        expect2(fmt, n, UnicodeString(str, ""));
+    }
+
+    void expect2(NumberFormat* fmt, const Formattable& n, const UnicodeString& str, UErrorCode ec);
+
+    void expect2(NumberFormat* fmt, const Formattable& n, const char* str, UErrorCode ec) {
+        expect2(fmt, n, UnicodeString(str, ""), ec);
+    }
+
+    void expect(NumberFormat& fmt, const UnicodeString& str, const Formattable& n);
+
+    void expect(NumberFormat& fmt, const char *str, const Formattable& n) {
         expect(fmt, UnicodeString(str, ""), n);
     }
+
     void expect(NumberFormat& fmt, const Formattable& n,
-                const UnicodeString& exp);
+                const UnicodeString& exp, UBool rt=TRUE);
+
     void expect(NumberFormat& fmt, const Formattable& n,
-                const char *exp) {
-        expect(fmt, n, UnicodeString(exp, ""));
+                const char *exp, UBool rt=TRUE) {
+        expect(fmt, n, UnicodeString(exp, ""), rt);
     }
+
     void expect(NumberFormat* fmt, const Formattable& n,
                 const UnicodeString& exp, UErrorCode);
+
     void expect(NumberFormat* fmt, const Formattable& n,
                 const char *exp, UErrorCode errorCode) {
         expect(fmt, n, UnicodeString(exp, ""), errorCode);
     }
-    void TestPad(void);
-    void TestPatterns2(void);
+
+    void expectCurrency(NumberFormat& nf, const Locale& locale,
+                        double value, const UnicodeString& string);
+
+    void expectPat(DecimalFormat& fmt, const UnicodeString& exp);
+
+    void expectPat(DecimalFormat& fmt, const char *exp) {
+        expectPat(fmt, UnicodeString(exp, ""));
+    }
+
     void expectPad(DecimalFormat& fmt, const UnicodeString& pat,
                    int32_t pos);
+
     void expectPad(DecimalFormat& fmt, const char *pat,
                    int32_t pos) {
         expectPad(fmt, pat, pos, 0, (UChar)0);
     }
+
     void expectPad(DecimalFormat& fmt, const UnicodeString& pat,
                    int32_t pos, int32_t width, UChar pad);
+
     void expectPad(DecimalFormat& fmt, const char *pat,
                    int32_t pos, int32_t width, UChar pad) {
         expectPad(fmt, UnicodeString(pat, ""), pos, width, pad);
     }
+
     void expectPad(DecimalFormat& fmt, const UnicodeString& pat,
                    int32_t pos, int32_t width, const UnicodeString& pad);
+
     void expectPad(DecimalFormat& fmt, const char *pat,
                    int32_t pos, int32_t width, const UnicodeString& pad) {
         expectPad(fmt, UnicodeString(pat, ""), pos, width, pad);
     }
 
-    void expectPat(DecimalFormat& fmt, const UnicodeString& exp);
-    void expectPat(DecimalFormat& fmt, const char *exp) {
-        expectPat(fmt, UnicodeString(exp, ""));
-    }
+    // internal utility routine
+    static UnicodeString& escape(UnicodeString& s);
+
     enum { ILLEGAL = -1 };
 
-public: // package
     // internal subtest used by TestRounding487
     void roundingTest(NumberFormat& nf, double x, int32_t maxFractionDigits, const char* expected);
 };
