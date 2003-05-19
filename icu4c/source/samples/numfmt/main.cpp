c930950bcfdf321f33ca697d1837c8a7ed0be7f5
Markus Scherer <markus.icu@gmail.com>
2003-05-19 00:31:06 +0000

    [ICU-2890] add sample code for currency formatting X-Commit-URL: https://ssl.icu-project.org/trac/changeset/11998 Former-commit-id: 62435c3fd9c523fefd0cf9c593c941006caaf174

--- icu4c/source/samples/numfmt/main.cpp
+++ icu4c/source/samples/numfmt/main.cpp
@@ -1,26 +1,40 @@
 /********************************************************************
  * COPYRIGHT:
- * Copyright (c) 1999-2002, International Business Machines Corporation and
+ * Copyright (c) 1999-2003, International Business Machines Corporation and
  * others. All Rights Reserved.
  ********************************************************************/
 
+#include "unicode/utypes.h"
 #include "unicode/unistr.h"
 #include "unicode/numfmt.h"
+#include "unicode/dcfmtsym.h"
+#include "unicode/decimfmt.h"
 #include "unicode/locid.h"
 #include "unicode/uclean.h"
 #include "util.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define LENGTHOF(array) (int32_t)(sizeof(array)/sizeof((array)[0]))
 
 extern "C" void capi();
 void cppapi();
 
+static void
+showCurrencyFormatting(UBool useICU26API);
+
 int main(int argc, char **argv) {
+    printf("%s output is in UTF-8\n", argv[0]);
+
     printf("C++ API\n");
     cppapi();
 
     printf("C API\n");
     capi();
+
+    showCurrencyFormatting(FALSE);
+    showCurrencyFormatting(TRUE);
 
     u_cleanup();    // Release any additional storage held by ICU.  
 
@@ -66,4 +80,164 @@ void cppapi() {
 
     delete fmt; // Release the storage used by the formatter
     
+}
+
+// currency formatting ----------------------------------------------------- ***
+
+/*
+ * Set a currency on a NumberFormat with pre-ICU 2.6 APIs.
+ * This is a "hack" that will not work properly for all cases because
+ * only ICU 2.6 introduced a more complete framework and data for this.
+ *
+ * @param nf The NumberFormat on which to set the currency; takes effect on
+ *           currency-formatting NumberFormat instances.
+ *           The display style of the output is controlled by nf (its pattern,
+ *           usually from the display locale ID used to create this instance)
+ *           while the currency symbol and number of decimals are set for
+ *           the currency.
+ * @param currency The 3-letter ISO 4217 currency code, NUL-terminated.
+ * @param errorCode ICU error code, must pass U_SUCCESS() on input.
+ */
+static void
+setNumberFormatCurrency_2_4(NumberFormat &nf, const char *currency, UErrorCode &errorCode) {
+    if(U_FAILURE(errorCode)) {
+        return;
+    }
+    if(currency==NULL || strlen(currency)!=3) {
+        errorCode=U_ILLEGAL_ARGUMENT_ERROR;
+        return;
+    }
+
+    // map the currency code to a locale ID
+    // only the currencies in this array are supported
+    // it would be possible to map to a locale ID, instantiate a currency
+    // formatter for that and copy its values, but that would be slower,
+    // and we have to hardcode something here anyway
+    static const struct {
+        const char *currency;
+        int32_t fractionDigits;
+        double roundingIncrement;
+        UChar symbol[16];
+    } currencyMap[]={
+        { "USD", 2, 0.01, { 0x24, 0 } },
+        { "GBP", 2, 0.01, { 0xa3, 0 } },
+        { "EUR", 2, 0.01, { 0x20ac, 0 } },
+        { "JPY", 0, 1.0, { 0xa5, 0 } }
+    };
+
+    int32_t i;
+
+    for(i=0; i<LENGTHOF(currencyMap); ++i) {
+        if(strcmp(currency, currencyMap[i].currency)==0) {
+            break;
+        }
+    }
+    if(i==LENGTHOF(currencyMap)) {
+        // a more specific error code would be useful in a real application
+        errorCode=U_UNSUPPORTED_ERROR;
+        return;
+    }
+
+    // set the currency-related data into the caller's formatter
+    // assume that the formatter is a DecimalFormat instance
+
+    nf.setMinimumFractionDigits(currencyMap[i].fractionDigits);
+    nf.setMaximumFractionDigits(currencyMap[i].fractionDigits);
+
+    DecimalFormat &dnf=(DecimalFormat &)nf;
+    dnf.setRoundingIncrement(currencyMap[i].roundingIncrement);
+
+    DecimalFormatSymbols symbols(*dnf.getDecimalFormatSymbols());
+    symbols.setSymbol(DecimalFormatSymbols::kCurrencySymbol, currencyMap[i].symbol);
+    dnf.setDecimalFormatSymbols(symbols); // do not adopt symbols: Jitterbug 2889
+}
+
+/*
+ * Set a currency on a NumberFormat with ICU 2.6 APIs.
+ *
+ * @param nf The NumberFormat on which to set the currency; takes effect on
+ *           currency-formatting NumberFormat instances.
+ *           The display style of the output is controlled by nf (its pattern,
+ *           usually from the display locale ID used to create this instance)
+ *           while the currency symbol and number of decimals are set for
+ *           the currency.
+ * @param currency The 3-letter ISO 4217 currency code, NUL-terminated.
+ * @param errorCode ICU error code, must pass U_SUCCESS() on input.
+ */
+static void
+setNumberFormatCurrency_2_6(NumberFormat &nf, const char *currency, UErrorCode &errorCode) {
+    if(U_FAILURE(errorCode)) {
+        return;
+    }
+    if(currency==NULL || strlen(currency)!=3) {
+        errorCode=U_ILLEGAL_ARGUMENT_ERROR;
+        return;
+    }
+
+    // invariant-character conversion to UChars (see utypes.h and putil.h)
+    UChar uCurrency[4];
+    u_charsToUChars(currency, uCurrency, 4);
+
+    // set the currency
+    nf.setCurrency(uCurrency);
+}
+
+static const char *const
+sampleLocaleIDs[]={
+    "en_US", "en_GB", "de", "ja", "fr", "hi"
+};
+
+static const char *const
+sampleCurrencies[]={
+    "USD", "GBP", "EUR", "JPY"
+};
+
+static void
+showCurrencyFormatting(UBool useICU26API) {
+    NumberFormat *nf;
+    int32_t i, j;
+
+    UnicodeString output;
+
+    UErrorCode errorCode;
+
+    // TODO: Using printf() here assumes that the runtime encoding is ASCII-friendly
+    // and can therefore be mixed with UTF-8
+
+    for(i=0; i<LENGTHOF(sampleLocaleIDs); ++i) {
+        printf("show currency formatting (method for %s) in the locale \"%s\"\n",
+                useICU26API ? "ICU 2.6" : "before ICU 2.6",
+                sampleLocaleIDs[i]);
+
+        // get a currency formatter for this locale ID
+        errorCode=U_ZERO_ERROR;
+        nf=NumberFormat::createCurrencyInstance(sampleLocaleIDs[i], errorCode);
+        if(U_FAILURE(errorCode)) {
+            printf("NumberFormat::createCurrencyInstance(%s) failed - %s\n",
+                    sampleLocaleIDs[i], u_errorName(errorCode));
+            continue;
+        }
+
+        for(j=0; j<LENGTHOF(sampleCurrencies); ++j) {
+            printf("  - format currency \"%s\": ", sampleCurrencies[j]);
+
+            // set the actual currency to be formatted
+            if(useICU26API) {
+                setNumberFormatCurrency_2_6(*nf, sampleCurrencies[j], errorCode);
+            } else {
+                setNumberFormatCurrency_2_4(*nf, sampleCurrencies[j], errorCode);
+            }
+            if(U_FAILURE(errorCode)) {
+                printf("setNumberFormatCurrency(%s) failed - %s\n",
+                        sampleCurrencies[j], u_errorName(errorCode));
+                continue;
+            }
+
+            // output=formatted currency value
+            output.remove();
+            nf->format(12345678.93, output);
+            output+=(UChar)0x0a; // '\n'
+            uprintf(output);
+        }
+    }
 }
