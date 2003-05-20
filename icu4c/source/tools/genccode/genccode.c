a3b33cf7dc4925358b687ee92cadd4daf603f125
George Rhoten <grhoten@users.noreply.github.com>
2003-05-20 00:13:02 +0000

    ICU-2753 Use friendlier wording.
    X-SVN-Rev: 12018

--- icu4c/source/tools/genccode/genccode.c
+++ icu4c/source/tools/genccode/genccode.c
@@ -182,8 +182,9 @@ writeCCode(const char *filename, const char *destdir) {
 
 #ifdef OS400
     /*
-    WARNING! WARNING! WARNING!
-    This is a hack. This is here because this platform can't properly put
+    TODO: Fix this once the compiler implements this feature. Keep in sync with udatamem.c
+
+    This is here because this platform can't currently put
     const data into the read-only pages of an object or
     shared library (service program). Only strings are allowed in read-only
     pages, so we use char * strings to store the data.
