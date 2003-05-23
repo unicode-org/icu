fec972df93d2507394c6b4c7118e11ec334c3b85
Alan Liu <alansliu@gmail.com>
2003-05-23 23:02:34 +0000

    incorporate review changes
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/2429
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@12092 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/ucat.c
+++ source/common/ucat.c
@@ -29,8 +29,7 @@ static char*
 _catkey(char* buffer, int32_t set_num, int32_t msg_num) {
     int32_t i = 0;
     U_ASSERT(set_num>=0 && msg_num>=0);
-    T_CString_integerToString(buffer, set_num, 10);
-    i = uprv_strlen(buffer);
+    i = T_CString_integerToString(buffer, set_num, 10);
     buffer[i++] = SEPARATOR;
     T_CString_integerToString(buffer+i, msg_num, 10);
     return buffer;
