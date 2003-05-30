08b5c23397a8858ab7a3a1c311879eb51df58af8
Steven R. Loomis <srl@icu-project.org>
2003-05-30 00:03:33 +0000

    ICU-2436 use yy or YY for 2 digit years.
    X-SVN-Rev: 12187

--- icu4c/source/test/intltest/dtfmttst.cpp
+++ icu4c/source/test/intltest/dtfmttst.cpp
@@ -291,7 +291,7 @@ DateFormatTest::TestFieldPosition(void)
 
     dateFormats[0] = DateFormat::createDateTimeInstance(DateFormat::FULL, DateFormat::FULL, Locale::getUS());
     dateFormats[1] = DateFormat::createDateTimeInstance(DateFormat::FULL, DateFormat::FULL, Locale::getFrance());
-    dateFormats[2] = new SimpleDateFormat((UnicodeString)"G, y, M, d, k, H, m, s, S, E, D, F, w, W, a, h, K, z, Y, e", status);
+    dateFormats[2] = new SimpleDateFormat((UnicodeString)"G, yy, M, d, k, H, m, s, S, E, D, F, w, W, a, h, K, z, YY, e", status);
     dateFormats[3] = new SimpleDateFormat((UnicodeString)"GGGG, yyyy, MMMM, dddd, kkkk, HHHH, mmmm, ssss, SSSS, EEEE, DDDD, FFFF, wwww, WWWW, aaaa, hhhh, KKKK, zzzz, YYYY, eeee", status);
     for (j = 0, exp = 0; j < dateFormats_length;++j) {
         UnicodeString str;
