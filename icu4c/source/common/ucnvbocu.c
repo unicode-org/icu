f2c7fe09639f8ddf3acf0d3bb7e1034935508fa8
Markus Scherer <markus.icu@gmail.com>
2003-06-02 22:26:54 +0000

    [ICU-2959] update copyright notices for 2003 changes X-Commit-URL: https://ssl.icu-project.org/trac/changeset/12230 Former-commit-id: 7b60eb54f63af55fe1f42af2750d7f7bc9c559c1

--- icu4c/source/common/ucnvbocu.c
+++ icu4c/source/common/ucnvbocu.c
@@ -1,7 +1,7 @@
 /*
 ******************************************************************************
 *
-*   Copyright (C) 2002, International Business Machines
+*   Copyright (C) 2002-2003, International Business Machines
 *   Corporation and others.  All Rights Reserved.
 *
 ******************************************************************************
