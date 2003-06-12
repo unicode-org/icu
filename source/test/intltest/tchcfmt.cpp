8e7409b232e98a03c749754b7d3f3763ae0a30f1
Steven R. Loomis <srl@icu-project.org>
2003-06-12 20:34:13 +0000

    update to new test
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/2039
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@12496 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/test/intltest/tchcfmt.cpp
+++ source/test/intltest/tchcfmt.cpp
@@ -514,20 +514,20 @@ void TestChoiceFormat::TestClosures(void) {
     const double *limits2 = fmt2.getLimits(count2);
     const UBool *closures2 = fmt2.getClosures(count2);
 
-    if(count2 != 6 || !limits2 || !closures2) {
+    if((count2 != 6) || !limits2 || !closures2) {
         errln("FAIL: couldn't get limits or closures");
     } else {
         for(i=0;i<count2;i++) {
-        logln("#%d: limit %g closed %s\n",
-                i,
+          logln("#%d/%d: limit %g closed %s\n",
+                i, count2,
                 limits2[i],
                 closures2[i] ?"T":"F");
-        }
-        if(limits[i] != limits[i]) {
+          if(limits2[i] != limits[i]) {
             errln("FAIL: limit #%d = %g, should be %g\n", i, limits2[i], limits[i]);
-        }
-        if((closures2[i]!=0) != (closures[i]!=0)) {
+          }
+          if((closures2[i]!=0) != (closures[i]!=0)) {
             errln("FAIL: closure #%d = %s, should be %s\n", i, closures2[i]?"T":"F", closures[i]?"T":"F");
+          }
         }
     }
 
