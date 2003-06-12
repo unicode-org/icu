36b6fdbcfa025491bb2c877b8b758627c1550aee
Andy Heninger <andy.heninger@gmail.com>
2003-06-12 22:48:09 +0000

    Add comments regarding replication of Mutex init code in two separate functions
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/2970
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@12502 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/mutex.cpp
+++ source/common/mutex.cpp
@@ -11,6 +11,13 @@
 /* Initialize the global mutex only when we can use it. */
 #if (ICU_USE_THREADS == 1)
 
+/*
+ * NOTE:  This function replicates functionality from the start
+ *        u_init().  Any changes must be made in both places.
+ *        TODO:  combine them.
+ *
+ *        This function runs only during C++ static initialization.
+ */
 static int GlobalMutexInitialize()
 {
     UErrorCode status = U_ZERO_ERROR;
