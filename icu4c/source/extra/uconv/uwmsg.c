f356148d722037a0f2a2f84937748baf5eef6720
George Rhoten <grhoten@users.noreply.github.com>
2003-06-19 01:02:46 +0000

    ICU-3036 Initialize the pointers
    X-SVN-Rev: 12530

--- icu4c/source/extra/uconv/uwmsg.c
+++ icu4c/source/extra/uconv/uwmsg.c
@@ -204,9 +204,11 @@ static const UChar *fetchErrorName(UErrorCode err)
 {
     if (!gInfoMessages) {
         gInfoMessages = (UChar **)malloc((U_ERROR_WARNING_LIMIT-U_ERROR_WARNING_START)*sizeof(UChar*));
+        memset(gInfoMessages, 0, (U_ERROR_WARNING_LIMIT-U_ERROR_WARNING_START)*sizeof(UChar*));
     }
     if (!gErrMessages) {
         gErrMessages = (UChar **)malloc(U_ERROR_LIMIT*sizeof(UChar*));
+        memset(gErrMessages, 0, U_ERROR_LIMIT*sizeof(UChar*));
     }
     if(err>=0)
         return gErrMessages[err];
