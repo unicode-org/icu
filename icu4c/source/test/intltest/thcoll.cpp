e3490ec6ab0475bb7418927b5e3f6a36e25f5d84
Vladimir Weinstein <icu@weivsara.com>
2003-07-16 22:48:25 +0000

    ICU-3109 added some Thai tests
    X-SVN-Rev: 12638

--- icu4c/source/test/intltest/thcoll.cpp
+++ icu4c/source/test/intltest/thcoll.cpp
@@ -16,6 +16,7 @@
 #include "unicode/utypes.h"
 #include "unicode/coll.h"
 #include "unicode/sortkey.h"
+#include "unicode/ustring.h"
 #include "cstring.h"
 #include "filestrm.h"
 
@@ -70,6 +71,7 @@ void CollationThaiTest::runIndexedTest(int32_t index, UBool exec, const char* &n
         CASE(0,TestDictionary)
         CASE(1,TestCornerCases)
         CASE(2,TestNamesList)
+        CASE(3,TestInvalidThai)
         default: name = ""; break;
     }
 }
@@ -410,6 +412,73 @@ int8_t CollationThaiTest::sign(int32_t i) {
 UnicodeString& CollationThaiTest::parseChars(UnicodeString& result,
                                              const char* chars) {
     return result = CharsToUnicodeString(chars);
+}
+
+UCollator *thaiColl = NULL;
+
+U_CDECL_BEGIN
+static int U_CALLCONV
+StrCmp(const void *p1, const void *p2) {
+  return ucol_strcoll(thaiColl, *(UChar **) p1, -1,  *(UChar **)p2, -1);
+}
+U_CDECL_END
+
+
+#define LINES 6
+
+void CollationThaiTest::TestInvalidThai(void) {
+  const char *tests[LINES] = {
+    "\\u0E44\\u0E01\\u0E44\\u0E01",
+    "\\u0E44\\u0E01\\u0E01\\u0E44",
+    "\\u0E01\\u0E44\\u0E01\\u0E44",
+    "\\u0E01\\u0E01\\u0E44\\u0E44",
+    "\\u0E44\\u0E44\\u0E01\\u0E01",
+    "\\u0E01\\u0E44\\u0E44\\u0E01",
+  };
+
+  UChar strings[LINES][20];
+
+  UChar *toSort[LINES];
+
+  int32_t i = 0, j = 0, len = 0;
+
+  UErrorCode coll_status = U_ZERO_ERROR;
+  UnicodeString iteratorText;
+
+  thaiColl = ucol_open ("th_TH", &coll_status);
+  if (U_FAILURE(coll_status)) {
+    errln("Error opening Thai collator: %s", u_errorName(coll_status));
+    return;
+  }
+
+  CollationElementIterator* c = ((RuleBasedCollator *)coll)->createCollationElementIterator( iteratorText );
+
+  for(i = 0; i < sizeof(tests)/sizeof(tests[0]); i++) {
+    len = u_unescape(tests[i], strings[i], 20);
+    strings[i][len] = 0;
+    toSort[i] = strings[i];
+  }
+
+  qsort (toSort, LINES, sizeof (UChar *), StrCmp);
+
+  for (i=0; i < LINES; i++)
+  {
+    logln("%i", i);
+	  for (j=i+1; j < LINES; j++) {
+		  if (ucol_strcoll (thaiColl, toSort[i], -1, toSort[j], -1) == UCOL_GREATER)
+		  {
+			  // inconsistency ordering found!
+            errln("Inconsistent ordering between strings %i and %i", i, j);
+		  }
+	  }
+      iteratorText.setTo(toSort[i]);
+      c->setText(iteratorText, coll_status);
+      backAndForth(*c);
+  }
+
+  
+  ucol_close(thaiColl);
+  delete c;
 }
 
 #endif /* #if !UCONFIG_NO_COLLATION */
