3059b947375dc80255227a19cce62849aa849bcf
Eric Mader <emader@svn.icu-project.org>
2003-08-18 17:41:23 +0000

    trailing whitespace run will be fist in the run array for RTL lines.
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/3174
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@12859 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/ubidiln.c
+++ source/common/ubidiln.c
@@ -485,15 +485,19 @@ ubidi_getRuns(UBiDi *pBiDi) {
                 /* now add the direction flags and adjust the visualLimit's to be just that */
                 ADD_ODD_BIT_FROM_LEVEL(runs[0].logicalStart, levels[runs[0].logicalStart]);
                 limit=runs[0].visualLimit;
-                for(i=1; i<runIndex; ++i) {
+
+				/* this loop will also handle the trailing WS run */
+                for(i=1; i<runCount; ++i) {
                     ADD_ODD_BIT_FROM_LEVEL(runs[i].logicalStart, levels[runs[i].logicalStart]);
                     limit=runs[i].visualLimit+=limit;
                 }
 
-                /* same for the trailing WS run */
+                /* Set the "odd" bit for the trailing WS run. */
+				/* For a RTL paragraph, it will be the *first* run in visual order. */
                 if(runIndex<runCount) {
-                    ADD_ODD_BIT_FROM_LEVEL(runs[i].logicalStart, pBiDi->paraLevel);
-                    runs[runIndex].visualLimit+=limit;
+					int32_t trailingRun = ((pBiDi->paraLevel & 1) != 0)? 0 : runIndex;
+
+                    ADD_ODD_BIT_FROM_LEVEL(runs[trailingRun].logicalStart, pBiDi->paraLevel);
                 }
             }
         }
