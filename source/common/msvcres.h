5fee92e2eb2a784a25e885ebdb28d09945e78b3e
George Rhoten <grhoten@svn.icu-project.org>
2003-08-19 17:39:26 +0000

    Prevent the constant rebuilding of ICU when it isn't needed.
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/3190
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@12872 0a8b766c-62c9-45f9-954d-7f2943c9ead0

---
+++ source/common/msvcres.h
@@ -0,0 +1,11 @@
+//{{NO_DEPENDENCIES}}
+// Copyright (c) 2003 International Business Machines
+// Corporation and others. All Rights Reserved.
+//
+// Used by common.rc and other .rc files.
+//Do not edit with Microsoft Developer Studio because it will modify this
+//header the wrong way. This is here to prevent Visual Studio .NET from
+//unnessarily building the resource files when it's not needed.
+//
+#include "unicode/uversion.h"
+
