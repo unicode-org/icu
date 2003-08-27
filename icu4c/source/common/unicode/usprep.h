97367f98f10c73e8ba051a356fb9b971d50a7f00
Ram Viswanadha <rviswanadha@gmail.com>
2003-08-27 03:40:43 +0000

    [ICU-3050] add documentation X-Commit-URL: https://ssl.icu-project.org/trac/changeset/12961 Former-commit-id: 8b5b3036fe97091d6ef646dc7d48437231af74f1

--- icu4c/source/common/unicode/usprep.h
+++ icu4c/source/common/unicode/usprep.h
@@ -18,7 +18,38 @@
 #define __USPREP_H__
 
 #include "unicode/utypes.h"
-
+/**
+ *\file
+ * StringPrep API implements the StingPrep framework as described by RFC 3454.
+ * StringPrep prepares Unicode strings for use in network protocols.
+ * Profiles of StingPrep are set of rules and data according to with the
+ * Unicode Strings are prepared. Each profiles contains tables which describe
+ * how a code point should be treated. The tables are broadly classied into
+ * <ul>
+ *     <li> Unassinged Table: Contains code points that are unassigned 
+ *          in the Unicode Version supported by StringPrep. Currently 
+ *          RFC 3454 supports Unicode 3.2. </li>
+ *     <li> Prohibited Table: Contains code points that are prohibted from
+ *          the output of the StringPrep processing function. </li>
+ *     <li> Mapping Table: Contains code ponts that are deleted from the output or case mapped. </li>
+ * </ul>
+ * 
+ * The procedure for preparing Unicode strings:
+ * <ol>
+ *      <li> Map: For each character in the input, check if it has a mapping
+ *           and, if so, replace it with its mapping. </li>
+ *      <li> Normalize: Possibly normalize the result of step 1 using Unicode
+ *           normalization. </li>
+ *      <li> Prohibit: Check for any characters that are not allowed in the
+ *        output.  If any are found, return an error.</li>
+ *      <li> Check bidi: Possibly check for right-to-left characters, and if
+ *           any are found, make sure that the whole string satisfies the
+ *           requirements for bidirectional strings.  If the string does not
+ *           satisfy the requirements for bidirectional strings, return an
+ *           error.  </li>
+ * </ol>
+ * @author Ram Viswanadha
+ */
 #if !UCONFIG_NO_IDNA
 
 #include "unicode/parseerr.h"
