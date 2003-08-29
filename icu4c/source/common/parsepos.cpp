dc703b5f1be26771ec1508c4a95352012f43fe4e
George Rhoten <grhoten@users.noreply.github.com>
2003-08-29 21:05:48 +0000

    ICU-3221 MSVC.NET combines static const char values in the same file. Separate them into different files.
    X-SVN-Rev: 12988

---
+++ icu4c/source/common/parsepos.cpp
@@ -0,0 +1,16 @@
+/*
+**********************************************************************
+*   Copyright (C) 2003-2003, International Business Machines
+*   Corporation and others.  All Rights Reserved.
+**********************************************************************
+*/
+
+#include "unicode/parsepos.h"
+
+U_NAMESPACE_BEGIN
+
+UOBJECT_DEFINE_RTTI_IMPLEMENTATION(ParsePosition)
+
+ParsePosition::~ParsePosition() {}
+
+U_NAMESPACE_END
