bef2e7069a39339394db0395539c6df328d3612e
Ram Viswanadha <rviswanadha@gmail.com>
2003-09-16 00:09:37 +0000

    ICU-3050 Additions by Dan Krejsa.
    X-SVN-Rev: 13110

--- icu4c/source/tools/gensprep/filterRFC3454.pl
+++ icu4c/source/tools/gensprep/filterRFC3454.pl
@@ -37,6 +37,7 @@ sub main(){
            "--A1"  => \$a1,
            "--B1"  => \$b1,
            "--B2"  => \$b2,
+		   "--B3"  => \$b3,
            "--C11" => \$c11,
            "--C12" => \$c12,
            "--C21" => \$c21,
@@ -49,6 +50,7 @@ sub main(){
            "--C8"  => \$c8,
            "--C9"  => \$c9,
            "--ldh-chars" => \$writeLDHChars,
+           "--iscsi" => \$writeISCSIChars,
            );
   usage() unless defined $sourceDir;
   usage() unless defined $destDir;
@@ -67,16 +69,23 @@ sub main(){
   print $outFH  $warning;
   close($outFH);
 
+  if(defined $b2 && defined $b3){
+      die "ERROR: --B2 and --B3 are both specified\!\n";
+  }
+
   while(defined ($line=<$inFH>)){
       next unless $line=~ /Start\sTable/;
       if($line =~ /A.1/){
             createUnassignedTable($inFH,$outfile);
       }
       if($line =~ /B.1/ && defined $b1){
-            createCaseMapNoNorm($inFH,$outfile);
+            createMapToNothing($inFH,$outfile);
       }
       if($line =~ /B.2/ && defined $b2){
-            createCaseMap($inFH,$outfile);
+            createCaseMapNorm($inFH,$outfile);
+      }
+      if($line =~ /B.3/ && defined $b3){
+            createCaseMapNoNorm($inFH,$outfile);
       }
       if($line =~ /C.1.1/ && defined $c11 ){
             createProhibitedTable($inFH,$outfile,$line);
@@ -115,6 +124,9 @@ sub main(){
   if( defined $writeLDHChars){
       createLDHCharTable($inFH, $outfile);
   }
+  if( defined $writeISCSIChars){
+      create_iSCSIExtraProhibitedTable($inFH, $outfile);
+  }
   close($inFH);
 }
 
@@ -147,7 +159,7 @@ sub readPrint{
             $line = $code."; ; UNASSIGNED\n";
         }elsif ( $table =~ /B\.1/ ){
             $line =~ s/Map to nothing/MAP/;
-        }elsif ( $table =~ /B\.2/ ){
+        }elsif ( $table =~ /B\.[23]/ ){
             $line =~ s/Case map/MAP/;
             $line =~ s/Additional folding/MAP/;
         }elsif ( $table =~ /C/ ) {
@@ -188,7 +200,7 @@ sub createUnassignedTable{
     close($outFH);
 }
 #-----------------------------------------------------------------------
-sub createCaseMapNoNorm{
+sub createMapToNothing{
     ($inFH,$outfile) = @_;
     $outFH = IO::File->new($outfile,"a")
             or die  "could not open the file $outfile for writing: $! \n";
@@ -197,12 +209,21 @@ sub createCaseMapNoNorm{
     close($outFH);
 }
 #-----------------------------------------------------------------------
-sub createCaseMap{
+sub createCaseMapNorm{
     ($inFH,$outfile) = @_;
     $outFH = IO::File->new($outfile,"a")
             or die  "could not open the file $outfile for writing: $! \n";
     $comment = $warning."# This table contains code points from Table B.2 from RFC 3454\n";
     readPrint($inFH,$outFH,$comment, "B.2");
+    close($outFH);
+}
+#-----------------------------------------------------------------------
+sub createCaseMapNoNorm{
+    ($inFH,$outfile) = @_;
+    $outFH = IO::File->new($outfile,"a")
+            or die  "could not open the file $outfile for writing: $! \n";
+    $comment = $warning."# This table contains code points from Table B.3 from RFC 3454\n";
+    readPrint($inFH,$outFH,$comment, "B.3");
     close($outFH);
 }
 #-----------------------------------------------------------------------
@@ -233,6 +254,23 @@ sub createLDHCharTable{
     close($outFH);
 }
 #-----------------------------------------------------------------------
+sub create_iSCSIExtraProhibitedTable{
+    ($inFH,$outfile,$line) = @_;
+    $comment ="# Additional prohibitions from draft-ietf-ips-iscsi-string-prep-06.txt\n";
+
+    $outFH = IO::File->new($outfile, "a")
+            or die  "could not open the file $outfile for writing: $! \n";
+    print $outFH $comment;
+    print $outFH "0021..002C; ; PROHIBITED\n";
+    print $outFH "002F; ; PROHIBITED\n";
+    print $outFH "003B..0040; ; PROHIBITED\n";
+    print $outFH "005B..0060; ; PROHIBITED\n";
+    print $outFH "007B..007E; ; PROHIBITED\n";
+    print $outFH "3002; ; PROHIBITED\n";
+    print $outFH "\n# Total code points 30\n";
+    close($outFH);
+}
+#-----------------------------------------------------------------------
 sub usage {
     print << "END";
 Usage:
@@ -242,22 +280,25 @@ Options:
         --destdir=<directory>
         --src-filename=<name of RFC file>
         --dest-filename=<name of destination file>
-        --A1             Generate data for table A1
-        --B1             Generate data for table B1
-        --B2             Generate data for table B2
-        --C11            Generate data for table C11
-        --C12            Generate data for table C12
-        --C21            Generate data for table C21
-        --C22            Generate data for table C22
-        --C3             Generate data for table C3
-        --C4             Generate data for table C4
-        --C5             Generate data for table C5
-        --C6             Generate data for table C6
-        --C7             Generate data for table C7
-        --C8             Generate data for table C8
-        --C9             Generate data for table C9
+        --A1             Generate data for table A.1
+        --B1             Generate data for table B.1
+        --B2             Generate data for table B.2
+        --B3             Generate data for table B.3
+        --C11            Generate data for table C.1.1
+        --C12            Generate data for table C.1.2
+        --C21            Generate data for table C.2.1
+        --C22            Generate data for table C.2.2
+        --C3             Generate data for table C.3
+        --C4             Generate data for table C.4
+        --C5             Generate data for table C.5
+        --C6             Generate data for table C.6
+        --C7             Generate data for table C.7
+        --C8             Generate data for table C.8
+        --C9             Generate data for table C.9
         --ldh-chars      Generate data for LDH chars used in IDNA
+        --iscsi          Generate data for extra prohibited iSCSI chars
 
+Note, --B2 and --B3 are mutually exclusive.
 
 e.g.: filterRFC3454.pl --sourcedir=. --destdir=./output --src-filename=rfc3454.txt  --dest-filename=NamePrepProfile.txt --A1 --B2 --C12 --C21 --C22 --C3 --C4 --C5 --C6 --C7 --C8 --C9  --ldh-chars
 
