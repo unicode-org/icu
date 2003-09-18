a13dadf19bb0325e6289abfe2d9021f116cce2d4
Markus Scherer <markus.icu@gmail.com>
2003-09-18 23:18:23 +0000

    fix generation of options-only collation tailoring binaries
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/2235
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@13144 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/i18n/ucol.cpp
+++ source/i18n/ucol.cpp
@@ -687,7 +687,37 @@ ucol_cloneRuleData(const UCollator *coll, int32_t *length, UErrorCode *status)
         *status = U_MEMORY_ALLOCATION_ERROR;
         return NULL;
     }
-    uprv_memcpy(result, UCA->image, sizeof(UCATableHeader));
+
+    /* build the UCATableHeader with minimal entries */
+    /* do not copy the header from the UCA file because its values are wrong! */
+    /* uprv_memcpy(result, UCA->image, sizeof(UCATableHeader)); */
+
+    /* reset everything */
+    uprv_memset(result, 0, *length);
+
+    /* set the tailoring-specific values */
+    UCATableHeader *myData = (UCATableHeader *)result;
+    myData->size = *length;
+
+    /* offset for the options, the only part of the data that is present after the header */
+    myData->options = sizeof(UCATableHeader);
+
+    /* need to always set the expansion value for an upper bound of the options */
+    myData->expansion = myData->options + sizeof(UColOptionSet);
+
+    myData->magic = UCOL_HEADER_MAGIC;
+    myData->isBigEndian = U_IS_BIG_ENDIAN;
+    myData->charSetFamily = U_CHARSET_FAMILY;
+
+    /* copy UCA's version; genrb will override all but the builder version with tailoring data */
+    uprv_memcpy(myData->version, coll->image->version, sizeof(UVersionInfo));
+
+    uprv_memcpy(myData->UCAVersion, coll->image->UCAVersion, sizeof(UVersionInfo));
+    uprv_memcpy(myData->UCDVersion, coll->image->UCDVersion, sizeof(UVersionInfo));
+    uprv_memcpy(myData->formatVersion, coll->image->formatVersion, sizeof(UVersionInfo));
+    myData->jamoSpecial = coll->image->jamoSpecial;
+
+    /* copy the collator options */
     uprv_memcpy(result+paddedsize(sizeof(UCATableHeader)), coll->options, sizeof(UColOptionSet));
   }
   return result;
