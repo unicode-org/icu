a5b7a0bbd9994b937a9f168c0b1eece013658eba
Eric Mader <emader@svn.icu-project.org>
2003-09-25 15:33:02 +0000

    [ICU-2257] implement glyph insertion. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/13200 Former-commit-id: e9936754b5f1ca53ed3033cc04d61ecab9d1696f

--- icu4c/source/layout/IndicLayoutEngine.h
+++ icu4c/source/layout/IndicLayoutEngine.h
@@ -153,7 +153,7 @@ protected:
      * @internal
      */
     virtual le_int32 glyphProcessing(const LEUnicode chars[], le_int32 offset, le_int32 count, le_int32 max, le_bool rightToLeft,
-            const LETag **featureTags, LEGlyphID *&glyphs, le_int32 *&charIndices, LEErrorCode &success);
+            const LETag **&featureTags, LEGlyphID *&glyphs, le_int32 *&charIndices, LEErrorCode &success);
 
 private:
 
