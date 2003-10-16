beea326e42ecba9522061c5aa7700b987cc3107c
George Rhoten <grhoten@users.noreply.github.com>
2003-10-16 18:34:44 +0000

    ICU-3178 Create a platform indepdent flag for debug and release builds
    X-SVN-Rev: 13439

--- icu4c/source/common/uassert.h
+++ icu4c/source/common/uassert.h
@@ -21,8 +21,12 @@
 #define U_ASSERT_H
 /* utypes.h is included to get the proper define for uint8_t */
 #include "unicode/utypes.h"
+#if U_RELEASE
+#define U_ASSERT(exp)
+#else
 #include <assert.h>
 #define U_ASSERT(exp) assert(exp)
+#endif
 #endif
 
 
