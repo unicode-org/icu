700c01510a90e144ef96a42926161c04ae62ad8a
Markus Scherer <markus.icu@gmail.com>
2003-10-28 20:05:42 +0000

    update table for ibm-16684
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/1893
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@13518 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/data/mappings/ucmebcdic.mk
+++ source/data/mappings/ucmebcdic.mk
@@ -24,4 +24,4 @@ ibm-284_P100-1995.ucm ibm-285_P100-1995.ucm ibm-290_P100-1995.ucm\
 ibm-297_P100-1995.ucm ibm-420_X120-1999.ucm ibm-424_P100-1995.ucm\
 ibm-4899_P100-1998.ucm ibm-4971_P100-1999.ucm ibm-500_P100-1995.ucm\
 ibm-5123_P100-1999.ucm ibm-803_P100-1999.ucm ibm-8482_P100-1999.ucm\
-ibm-16684_P100-1999.ucm
+ibm-16684_P110-2003.ucm
