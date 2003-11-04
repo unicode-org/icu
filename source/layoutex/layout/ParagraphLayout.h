85137a7999f51a67cafb290edaae9d40f6604bf7
Eric Mader <emader@svn.icu-project.org>
2003-11-04 19:28:16 +0000

    add ParagraphLayout::Line::getWidth()
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/3219
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@13565 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/layoutex/layout/ParagraphLayout.h
+++ source/layoutex/layout/ParagraphLayout.h
@@ -347,9 +347,6 @@ public:
      * Clients can optionally specify directional runs and / or script runs. If these aren't specified
      * they will be computed.
 	 *
-	 * If any errors are encountered during construction, <code>status</code> will be set, and the object
-	 * will be set to be empty.
-     *
      * @param chars is an array of the characters in the paragraph
      *
      * @param count is the number of characters in the paragraph.
@@ -371,23 +368,20 @@ public:
      * @param paragraphLevel is the directionality of the paragraph, as in the UBiDi object.
      *
      * @param vertical is <code>true</code> if the paragraph should be set vertically.
-	 *
-	 * @param status will be set to any error code encountered during construction.
      *
      * @see ubidi.h
      * @see LEFontInstance.h
      * @see LayoutEngine.h
      * @see RunArrays.h
      *
-     * @draft ICU 2.8
+     * @draft ICU 2.6
      */
     ParagraphLayout(const LEUnicode chars[], le_int32 count,
                     const FontRuns *fontRuns,
                     const ValueRuns *levelRuns,
                     const ValueRuns *scriptRuns,
                     const LocaleRuns *localeRuns,
-                    UBiDiLevel paragraphLevel, le_bool vertical,
-					LEErrorCode &status);
+                    UBiDiLevel paragraphLevel, le_bool vertical);
 
     /**
      * The destructor. Virtual so that it works correctly with
@@ -564,7 +558,7 @@ private:
 
     void computeLocales();
 
-    void computeSubFonts(const FontRuns *fontRuns, LEErrorCode &status);
+    void computeSubFonts(const FontRuns *fontRuns);
 
     void computeMetrics();
 
