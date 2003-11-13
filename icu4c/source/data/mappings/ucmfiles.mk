9e8b274329d433c8cbdb961e09a1cdea0706e4a0
Markus Scherer <markus.icu@gmail.com>
2003-11-13 05:09:13 +0000

    ICU-3346 add some conversion tables for windows system codepages
    X-SVN-Rev: 13710

--- icu4c/source/data/mappings/ucmfiles.mk
+++ icu4c/source/data/mappings/ucmfiles.mk
@@ -88,6 +88,12 @@ ibm-1363_P110-1997.ucm\
 ibm-1381_P110-1999.ucm\
 ibm-33722_P120-1999.ucm\
 macos-2566-10.2.ucm\
+windows-874-2000.ucm\
+windows-936-2000.ucm\
+windows-949-2000.ucm\
+windows-950-2000.ucm\
+windows-1255-2000.ucm\
+windows-1256-2000.ucm\
 jisx-201.ucm jisx-208.ucm jisx-212.ucm\
 iso-ir-165.ucm cns-11643-1992.ucm\
 ibm-5478_P100-1995.ucm\
