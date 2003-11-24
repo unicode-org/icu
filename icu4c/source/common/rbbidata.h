276f4c40a02af5d0bddace6c22f8092f596cc47b
Vladimir Weinstein <icu@weivsara.com>
2003-11-24 19:48:37 +0000

    ICU-2235 remove C++ style comments in order to allow real including from .c files
    X-SVN-Rev: 13841

--- icu4c/source/common/rbbidata.h
+++ icu4c/source/common/rbbidata.h
@@ -53,72 +53,72 @@ ubrk_swap(const UDataSwapper *ds,
 
 U_NAMESPACE_BEGIN
 
-//
-//  The following structs map exactly onto the raw data from ICU common data file.
-//
+/*  */
+/*   The following structs map exactly onto the raw data from ICU common data file. */
+/*  */
 struct RBBIDataHeader {
-    uint32_t         fMagic;       // == 0xbla0
-    uint32_t         fVersion;     // == 1
-    uint32_t         fLength;      // Total length in bytes of this RBBI Data,
-                                   //     including all sections, not just the header.
-    uint32_t         fCatCount;    // Number of character categories.
+    uint32_t         fMagic;       /*  == 0xbla0 */
+    uint32_t         fVersion;     /*  == 1 */
+    uint32_t         fLength;      /*  Total length in bytes of this RBBI Data, */
+                                   /*      including all sections, not just the header. */
+    uint32_t         fCatCount;    /*  Number of character categories. */
 
-    //
-    // Offsets and sizes of each of the subsections within the RBBI data.
-    // All offsets are bytes from the start of the RBBIDataHeader.
-    // All sizes are in bytes.
-    //
-    uint32_t         fFTable;      // forward state transition table.
+    /*  */
+    /*  Offsets and sizes of each of the subsections within the RBBI data. */
+    /*  All offsets are bytes from the start of the RBBIDataHeader. */
+    /*  All sizes are in bytes. */
+    /*  */
+    uint32_t         fFTable;      /*  forward state transition table. */
     uint32_t         fFTableLen;
-    uint32_t         fRTable;      // Offset to the reverse state transition table.
+    uint32_t         fRTable;      /*  Offset to the reverse state transition table. */
     uint32_t         fRTableLen;
-    uint32_t         fSFTable;     // safe point forward transition table
+    uint32_t         fSFTable;     /*  safe point forward transition table */
     uint32_t         fSFTableLen;
-    uint32_t         fSRTable;     // safe point reverse transition table
+    uint32_t         fSRTable;     /*  safe point reverse transition table */
     uint32_t         fSRTableLen;
-    uint32_t         fTrie;        // Offset to Trie data for character categories
+    uint32_t         fTrie;        /*  Offset to Trie data for character categories */
     uint32_t         fTrieLen;
-    uint32_t         fRuleSource;  // Offset to the source for for the break
-    uint32_t         fRuleSourceLen;  //   rules.  Stored UChar *.
+    uint32_t         fRuleSource;  /*  Offset to the source for for the break */
+    uint32_t         fRuleSourceLen;  /*    rules.  Stored UChar *. */
 
-    uint32_t         fReserved[8]; // Reserved for expansion
+    uint32_t         fReserved[8]; /*  Reserved for expansion */
 
 };
 
 
 
 struct  RBBIStateTableRow {
-    int16_t          fAccepting;    // Non-zero if this row is for an accepting state.
-                                    // Value is the {nnn} value to return to calling
-                                    //    application.
-    int16_t          fLookAhead;    // Non-zero if this row is for a state that
-                                    //   corresponds to a '/' in the rule source.
-                                    //   Value is the same as the fAccepting
-                                    //     value for the rule (which will appear
-                                    //     in a different state.
-    int16_t          fTag;          // Non-zero if this row covers a {tagged} position
-                                    //    from a rule.  value is the tag number.
+    int16_t          fAccepting;    /*  Non-zero if this row is for an accepting state. */
+                                    /*  Value is the {nnn} value to return to calling */
+                                    /*     application. */
+    int16_t          fLookAhead;    /*  Non-zero if this row is for a state that */
+                                    /*    corresponds to a '/' in the rule source. */
+                                    /*    Value is the same as the fAccepting */
+                                    /*      value for the rule (which will appear */
+                                    /*      in a different state. */
+    int16_t          fTag;          /*  Non-zero if this row covers a {tagged} position */
+                                    /*     from a rule.  value is the tag number. */
     int16_t          fReserved;
-    uint16_t         fNextState[2]; // Next State, indexed by char category.
-                                    //   Array Size is fNumCols from the
-                                    //   state table header.
-                                    //   CAUTION:  see RBBITableBuilder::getTableSize()
-                                    //             before changing anything here.
+    uint16_t         fNextState[2]; /*  Next State, indexed by char category. */
+                                    /*    Array Size is fNumCols from the */
+                                    /*    state table header. */
+                                    /*    CAUTION:  see RBBITableBuilder::getTableSize() */
+                                    /*              before changing anything here. */
 };
 
 
 struct RBBIStateTable {
-    uint32_t         fNumStates;    // Number of states.
-    uint32_t         fRowLen;       // Length of a state table row, in bytes.
-    char             fTableData[4]; // First RBBIStateTableRow begins here.
-                                    //   (making it char[] simplifies ugly address
-                                    //    arithmetic for indexing variable length rows.)
+    uint32_t         fNumStates;    /*  Number of states. */
+    uint32_t         fRowLen;       /*  Length of a state table row, in bytes. */
+    char             fTableData[4]; /*  First RBBIStateTableRow begins here. */
+                                    /*    (making it char[] simplifies ugly address */
+                                    /*     arithmetic for indexing variable length rows.) */
 };
 
 
-//
-//  The reference counting wrapper class
-//
+/*  */
+/*   The reference counting wrapper class */
+/*  */
 class RBBIDataWrapper : public UMemory {
 public:
     RBBIDataWrapper(const RBBIDataHeader *data, UErrorCode &status);
@@ -134,9 +134,9 @@ public:
     void                  printData();
     void                  printTable(const char *heading, const RBBIStateTable *table);
 
-    //
-    //  Pointers to items within the data
-    //
+    /*  */
+    /*   Pointers to items within the data */
+    /*  */
     const RBBIDataHeader     *fHeader;
     const RBBIStateTable     *fForwardTable;
     const RBBIStateTable     *fReverseTable;
@@ -145,13 +145,13 @@ public:
     const UChar              *fRuleSource;
 
     UTrie               fTrie;
-    // if fLookAheadHardBreak is true, we will break at the first lookahead match
-    // the search does not go on further to look for a longer match
-    // this also allows breaks at both ends of the string
-    // e.g. rule "ABC / D; ABCDE" and 
-    // text "ABCD ABCDE ABC" will give breaks at
-    //       01234567890123
-    // {0, 3, 4, 5, 8, 9, 10, 11, 14}
+    /*  if fLookAheadHardBreak is true, we will break at the first lookahead match */
+    /*  the search does not go on further to look for a longer match */
+    /*  this also allows breaks at both ends of the string */
+    /*  e.g. rule "ABC / D; ABCDE" and  */
+    /*  text "ABCD ABCDE ABC" will give breaks at */
+    /*        01234567890123 */
+    /*  {0, 3, 4, 5, 8, 9, 10, 11, 14} */
     UBool               fLookAheadHardBreak;
 
 private:
@@ -159,8 +159,8 @@ private:
     UDataMemory        *fUDataMem;
     UnicodeString       fRuleString;
 
-    RBBIDataWrapper(const RBBIDataWrapper &other); // forbid copying of this class
-    RBBIDataWrapper &operator=(const RBBIDataWrapper &other); // forbid copying of this class
+    RBBIDataWrapper(const RBBIDataWrapper &other); /*  forbid copying of this class */
+    RBBIDataWrapper &operator=(const RBBIDataWrapper &other); /*  forbid copying of this class */
 };
 
 
