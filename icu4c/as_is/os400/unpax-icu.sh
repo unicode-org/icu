d0eb451320eb2f854dd5c098a5e4f7b3f1e2ee62
Steven R. Loomis <srl@icu-project.org>
2003-12-04 23:51:16 +0000

    ICU-2235 add more binary file types
    X-SVN-Rev: 14014

--- icu4c/as_is/os400/unpax-icu.sh
+++ icu4c/as_is/os400/unpax-icu.sh
@@ -16,7 +16,7 @@
 
 #binary_suffixes='ico ICO bmp BMP jpg JPG gif GIF brk BRK'
 #ICU specific binary files
-binary_suffixes='brk BRK bin BIN'
+binary_suffixes='brk BRK bin BIN res RES cnv CNV dat DAT icu ICU'
 data_files='icu/source/data/brkitr/* icu/source/data/locales/* icu/source/data/mappings/* icu/source/data/misc/* icu/source/data/translit/* icu/source/data/unidata/* icu/source/test/testdata/*'
 
 usage()
