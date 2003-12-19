893eb18425269324979ecd3007e32890249be068
Eric Mader <emader@svn.icu-project.org>
2003-12-19 23:18:51 +0000

    Update copyright notices for ICU 2.8.
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/3473
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@14197 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/test/perf/convperf/convperf_iml.pl
+++ source/test/perf/convperf/convperf_iml.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 #  ********************************************************************
 #  * COPYRIGHT:
-#  * Copyright (c) 2002, International Business Machines Corporation and
+#  * Copyright (c) 2002-2003, International Business Machines Corporation and
 #  * others. All Rights Reserved.
 #  ********************************************************************
 
