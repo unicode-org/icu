54c44b95d05cb80f9d8b0bfd34295ea2701501da
Eric Mader <emader@svn.icu-project.org>
2003-12-19 23:30:30 +0000

    Update copyright notices for ICU 2.8.
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/3473
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@14198 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/tools/cpysearch/cpysearch.pl
+++ source/tools/cpysearch/cpysearch.pl
@@ -1,22 +1,24 @@
 #!/usr/bin/perl -w
 #  ********************************************************************
 #  * COPYRIGHT:
-#  * Copyright (c) 2002, International Business Machines Corporation and
+#  * Copyright (c) 2002-2003, International Business Machines Corporation and
 #  * others. All Rights Reserved.
 #  ********************************************************************
 use strict;
 
-my $icuSource = "/icu/source";
-my $ignore = "CVS|\\~|\\#|Debug|Release|dsp|dsw|opt|ncb|cvsignore|cnv|res|\\.icu|exe|out|build|plg|positions|unidata";
+my $icuSource = $ARGV[0];
+my $ignore = "CVS|\\~|\\#|Debug|Release|dsp|dsw|opt|ncb|vcproj|sln|suo|cvsignore|cnv|res|\\.icu|exe|\\.out|build|plg|positions|unidata";
 
-my $command = "find $icuSource -type f";
-#my $command = "find $icuSource -type f\|grep -v \"$ignore\"";
+my ($sec, $min, $hour, , $day, $mon, $year, $wday, $yday, $isdst) = localtime;
+$year += 1900;
+
+my $command = "find $icuSource -type f -mtime -$yday";
 my @files = `$command`;
 @files = grep(!/$ignore/, @files);
 my $file;
 foreach $file (@files) {
   my @lines = `head -n 10 $file`;
-  if (grep(/copyright/i, @lines) == 0) {
+  if (grep(/copyright.*$year/i, @lines) == 0) {
     print "$file";
   }
 }
