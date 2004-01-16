615de18745679ce5a3330c10307223655cdab3b4
Vladimir Weinstein <weiv@svn.icu-project.org>
2004-01-16 07:09:34 +0000

    bumped up collation version numbers
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/3515
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@14346 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/unicode/uversion.h
+++ source/common/unicode/uversion.h
@@ -174,21 +174,23 @@ u_getVersion(UVersionInfo versionArray);
  * compression, generating quad level always when strength is quad or more 
  * version 4 - ICU 2.2 - tracking UCA changes, ignore completely ignorables 
  * in contractions, ignore primary ignorables after shifted 
+ * version 5 - ICU 2.8 - changed implicit generation code
  * This value may change in the subsequent releases of ICU
  * @draft ICU 2.4
  */
-#define UCOL_RUNTIME_VERSION 4
+#define UCOL_RUNTIME_VERSION 5
 
 /** Builder code version. When this is different, same tailoring might result
  * in assigning different collation elements to code points                  
  * version 2 was in ICU 1.8.1. added support for prefixes, tweaked canonical 
  * closure. However, the tailorings should probably get same CEs assigned    
  * version 5 - ICU 2.2 - fixed some bugs, renamed some indirect values.      
+ * version 6 - ICU 2.8 - fixed bug in builder that allowed 0xFF in primary values
  * Backward compatible with the old rules. 
  * This value may change in the subsequent releases of ICU
  * @draft ICU 2.4
  */
-#define UCOL_BUILDER_VERSION 5
+#define UCOL_BUILDER_VERSION 6
 
 /** *** Removed *** Instead we use the data we read from FractionalUCA.txt
  * This is the version of FractionalUCA.txt tailoring rules
