e2ee7dccdd169a2e7d8114acb22684e96c9afd85
Alan Liu <alansliu@gmail.com>
2004-01-21 00:06:22 +0000

    initial checkin
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/2202
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@14368 0a8b766c-62c9-45f9-954d-7f2943c9ead0

---
+++ source/common/locbased.h
@@ -0,0 +1,68 @@
+/*
+**********************************************************************
+* Copyright (c) 2004, International Business Machines
+* Corporation and others.  All Rights Reserved.
+**********************************************************************
+* Author: Alan Liu
+* Created: January 16 2004
+* Since: ICU 2.8
+**********************************************************************
+*/
+#ifndef LOCBASED_H
+#define LOCBASED_H
+
+#include "unicode/locid.h"
+
+#define U_LOCALE_BASED(varname, objname) \
+  LocaleBased varname((objname).validLocale, (objname).actualLocale);
+
+U_NAMESPACE_BEGIN
+
+class LocaleBased {
+
+ public:
+
+    inline LocaleBased(char* validAlias, char* actualAlias);
+
+    inline LocaleBased(const char* validAlias, const char* actualAlias);
+
+    /**
+     * Returns the locale for this break iterator. Two flavors are available: valid and 
+     * actual locale. 
+     * @draft ICU 2.8
+     */
+    Locale getLocale(ULocDataLocaleType type, UErrorCode& status) const;
+
+    /**
+     * Get the locale for this break iterator object. You can choose between valid and actual locale.
+     * @param type type of the locale we're looking for (valid or actual) 
+     * @param status error code for the operation
+     * @return the locale
+     * @internal
+     */
+    const char* getLocaleID(ULocDataLocaleType type, UErrorCode& status) const;
+
+    /**
+     */
+    void setLocaleIDs(const char* valid, const char* actual);
+
+ private:
+
+    char* valid;
+    
+    char* actual;
+};
+
+inline LocaleBased::LocaleBased(char* validAlias, char* actualAlias) :
+    valid(validAlias), actual(actualAlias) {
+}
+
+inline LocaleBased::LocaleBased(const char* validAlias,
+                                const char* actualAlias) :
+    // ugh: cast away const
+    valid((char*)validAlias), actual((char*)actualAlias) {
+}
+
+U_NAMESPACE_END
+
+#endif
