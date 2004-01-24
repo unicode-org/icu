b5bcd564a7f75fc0bd8ce7b63ed056e005410730
Alan Liu <alansliu@gmail.com>
2004-01-24 00:18:37 +0000

    [ICU-2202] add documentation X-Commit-URL: https://ssl.icu-project.org/trac/changeset/14403 Former-commit-id: 72cc119288a5d85b1ea7fa6908825b7ed1979ca4

--- icu4c/source/common/locbased.h
+++ icu4c/source/common/locbased.h
@@ -14,36 +14,64 @@
 #include "unicode/locid.h"
 #include "unicode/uobject.h"
 
+/**
+ * Macro to declare a locale LocaleBased wrapper object for the given
+ * object, which must have two members named `validLocale' and
+ * `actualLocale'.
+ */
 #define U_LOCALE_BASED(varname, objname) \
   LocaleBased varname((objname).validLocale, (objname).actualLocale);
 
 U_NAMESPACE_BEGIN
 
+/**
+ * A utility class that unifies the implementation of getLocale() by
+ * various ICU services.  This class is likely to be removed in the
+ * ICU 3.0 time frame in favor of an integrated approach with the
+ * services framework.
+ * @since ICU 2.8
+ */
 class U_COMMON_API LocaleBased : public UMemory {
 
  public:
 
+    /**
+     * Construct a LocaleBased wrapper around the two pointers.  These
+     * will be aliased for the lifetime of this object.
+     */
     inline LocaleBased(char* validAlias, char* actualAlias);
 
+    /**
+     * Construct a LocaleBased wrapper around the two const pointers.
+     * These will be aliased for the lifetime of this object.
+     */
     inline LocaleBased(const char* validAlias, const char* actualAlias);
 
     /**
-     * Returns the locale for this break iterator. Two flavors are available: valid and 
-     * actual locale. 
-     * @draft ICU 2.8
+     * Return locale meta-data for the service object wrapped by this
+     * object.  Either the valid or the actual locale may be
+     * retrieved.
+     * @param type either ULOC_VALID_LOCALE or ULOC_ACTUAL_LOCALE
+     * @param status input-output error code
+     * @return the indicated locale
      */
     Locale getLocale(ULocDataLocaleType type, UErrorCode& status) const;
 
     /**
-     * Get the locale for this break iterator object. You can choose between valid and actual locale.
-     * @param type type of the locale we're looking for (valid or actual) 
-     * @param status error code for the operation
-     * @return the locale
-     * @internal
+     * Return the locale ID for the service object wrapped by this
+     * object.  Either the valid or the actual locale may be
+     * retrieved.
+     * @param type either ULOC_VALID_LOCALE or ULOC_ACTUAL_LOCALE
+     * @param status input-output error code
+     * @return the indicated locale ID
      */
     const char* getLocaleID(ULocDataLocaleType type, UErrorCode& status) const;
 
     /**
+     * Set the locale meta-data for the service object wrapped by this
+     * object.  If either parameter is zero, it is ignored.
+     * @param valid the ID of the valid locale
+     * @param actual the ID of the actual locale
      */
     void setLocaleIDs(const char* valid, const char* actual);
 
