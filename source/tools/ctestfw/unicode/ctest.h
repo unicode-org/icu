77a50dedd35c008bc9a23fa84fe768aef6c9345f
George Rhoten <grhoten@svn.icu-project.org>
2004-03-23 06:37:36 +0000

    Move data based test framework to test library
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/3662
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@14733 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/tools/ctestfw/unicode/ctest.h
+++ source/tools/ctestfw/unicode/ctest.h
@@ -1,56 +1,17 @@
 /*
 *****************************************************************************************
 *
-*   Copyright (C) 1996-2003, International Business Machines
+*   Copyright (C) 1996-2004, International Business Machines
 *   Corporation and others.  All Rights Reserved.
 *
 *****************************************************************************************
 */
 
-
 #ifndef CTEST_H
 #define CTEST_H
 
-#include <stdarg.h>
-
-#include "unicode/utypes.h"
+#include "unicode/testtype.h"
 #include "unicode/utrace.h"
-
-/*Deals with imports and exports of the dynamic library*/
-#if defined(_WIN32) || defined(U_CYGWIN)
-    #define T_CTEST_EXPORT __declspec(dllexport)
-    #define T_CTEST_IMPORT __declspec(dllimport)
-#else
-    #define T_CTEST_EXPORT
-    #define T_CTEST_IMPORT
-#endif
-
-#ifdef __cplusplus
-    #define C_CTEST_API extern "C"
-#else
-    #define C_CTEST_API
-#endif
-
-#ifdef T_CTEST_IMPLEMENTATION
-    #define T_CTEST_API C_CTEST_API  T_CTEST_EXPORT
-    #define T_CTEST_EXPORT_API T_CTEST_EXPORT
-#else
-    #define T_CTEST_API C_CTEST_API  T_CTEST_IMPORT
-    #define T_CTEST_EXPORT_API T_CTEST_IMPORT
-#endif
-
-
-
-/* True and false for sanity. (removes ICU dependancy) */
-
-#ifndef FALSE
-#define FALSE 0
-#endif
-#ifndef TRUE
-#define TRUE 1
-#endif
-
-
 
 
 /* prototypes *********************************/
@@ -174,7 +135,7 @@ T_CTEST_API void log_info(const char* pattern, ...);
  * @param ap variable-arguments list
  * @internal Internal APIs for testing purpose only
  */
-T_CTEST_API void vlog_info(const char *prefix, const char *pattern, va_list ap);
+/*T_CTEST_API void vlog_info(const char *prefix, const char *pattern, va_list ap);*/
 
 /**
  * Log a verbose informational message. (printf style)
