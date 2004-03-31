a92ebaedda80f7320a03374758d8236b28c759d4
Alan Liu <alansliu@gmail.com>
2004-03-31 00:33:43 +0000

    ICU-3666 remove CVS keywords from source
    X-SVN-Rev: 14807

--- icu4j/src/com/ibm/icu/dev/test/ModuleTest.java
+++ icu4j/src/com/ibm/icu/dev/test/ModuleTest.java
@@ -3,12 +3,6 @@
  * Copyright (C) 2001-2003, International Business Machines Corporation and    *
  * others. All Rights Reserved.                                                *
  *******************************************************************************
- *
- * $Source: /xsrl/Nsvn/icu/icu4j/src/com/ibm/icu/dev/test/ModuleTest.java,v $
- * $Date: 2003/06/03 18:49:28 $
- * $Revision: 1.6 $
- *
- *******************************************************************************
  */
 package com.ibm.icu.dev.test;
 
