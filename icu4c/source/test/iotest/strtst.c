89b3116eb8d3373464a9c86d35e72c5728611658
George Rhoten <grhoten@users.noreply.github.com>
2004-04-14 19:12:58 +0000

    ICU-3499 Add some tests
    X-SVN-Rev: 14961

--- icu4c/source/test/iotest/strtst.c
+++ icu4c/source/test/iotest/strtst.c
@@ -345,14 +345,20 @@ static void TestSprintfFormat(void) {
     TestSPrintFormat("%-8C", (UChar)0x65, "%-8c", (char)0x65);
 
     TestSPrintFormat("%f", 1.23456789, "%f", 1.23456789);
+    TestSPrintFormat("%f", 12345.6789, "%f", 12345.6789);
+    TestSPrintFormat("%f", 123456.789, "%f", 123456.789);
+    TestSPrintFormat("%f", 1234567.89, "%f", 1234567.89);
     TestSPrintFormat("%10f", 1.23456789, "%10f", 1.23456789);
     TestSPrintFormat("%-10f", 1.23456789, "%-10f", 1.23456789);
     TestSPrintFormat("%10f", 123.456789, "%10f", 123.456789);
     TestSPrintFormat("%10.4f", 123.456789, "%10.4f", 123.456789);
     TestSPrintFormat("%-10f", 123.456789, "%-10f", 123.456789);
 
+    TestSPrintFormat("%g", 12345.6789, "%g", 12345.6789);
     TestSPrintFormat("%g", 123456.789, "%g", 123456.789);
+    TestSPrintFormat("%g", 1234567.89, "%g", 1234567.89);
     TestSPrintFormat("%G", 123456.789, "%G", 123456.789);
+    TestSPrintFormat("%G", 1234567.89, "%G", 1234567.89);
     TestSPrintFormat("%10g", 1.23456789, "%10g", 1.23456789);
     TestSPrintFormat("%10.4g", 1.23456789, "%10.4g", 1.23456789);
     TestSPrintFormat("%-10g", 1.23456789, "%-10g", 1.23456789);
