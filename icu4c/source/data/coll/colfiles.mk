c05f6b7cf79eb96f065eed512a31c93850090f9f
George Rhoten <grhoten@users.noreply.github.com>
2004-04-16 02:10:53 +0000

    ICU-2487 Use multiple lines for the targets for easier editing.
    X-SVN-Rev: 14987

--- icu4c/source/data/coll/colfiles.mk
+++ icu4c/source/data/coll/colfiles.mk
@@ -29,4 +29,14 @@
 COLLATION_ALIAS_SOURCE = de__PHONEBOOK.txt  es__TRADITIONAL.txt zh_TW_STROKE.txt zh__PINYIN.txt hi__DIRECT.txt
 
 # Please try to keep this list in alphabetical order
-COLLATION_SOURCE =  ar.txt be.txt bg.txt ca.txt cs.txt da.txt de.txt  el.txt en.txt en_BE.txt eo.txt es.txt et.txt fa.txt fa_AF.txt fi.txt fo.txt fr.txt gu.txt he.txt hi.txt  hr.txt hu.txt is.txt it.txt ja.txt kk.txt kl.txt kn.txt ko.txt lt.txt lv.txt mk.txt mr.txt mt.txt nb.txt nn.txt pa.txt pl.txt ps.txt ro.txt ru.txt sh.txt sk.txt sl.txt sq.txt sr.txt sv.txt ta.txt te.txt th.txt tr.txt uk.txt vi.txt zh.txt zh_HK.txt zh_MO.txt zh_TW.txt 
+COLLATION_SOURCE = \
+ar.txt be.txt bg.txt ca.txt cs.txt da.txt de.txt\
+el.txt en.txt en_BE.txt eo.txt es.txt et.txt\
+fa.txt fa_AF.txt fi.txt fo.txt fr.txt gu.txt\
+he.txt hi.txt hr.txt hu.txt is.txt it.txt ja.txt\
+kk.txt kl.txt kn.txt ko.txt lt.txt lv.txt\
+mk.txt mr.txt mt.txt nb.txt nn.txt pa.txt pl.txt ps.txt\
+ro.txt ru.txt sh.txt sk.txt sl.txt sq.txt sr.txt sv.txt\
+ta.txt te.txt th.txt tr.txt uk.txt vi.txt\
+zh.txt zh_HK.txt zh_MO.txt zh_TW.txt
+
