03ddc393db96d5338eed4128d598fa72deb6ba09
Alan Liu <alansliu@gmail.com>
2004-04-24 02:55:47 +0000

    modified ICUResourceBundleFactory to read from any resource tree using the new API, and to cache the results
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/3549
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@15040 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/iculserv.cpp
+++ source/common/iculserv.cpp
@@ -14,9 +14,13 @@
 #include "cmemory.h"
 #include "iculserv.h"
 #include "ustrfmt.h"
+#include "uhash.h"
+#include "charstr.h"
+#include "uassert.h"
 
 U_NAMESPACE_BEGIN
 
+// see LocaleUtility::getAvailableLocaleNames
 static Hashtable * LocaleUtility_cache = NULL;
 
 #define UNDERSCORE_CHAR ((UChar)0x005f)
@@ -86,39 +90,71 @@ LocaleUtility::initNameFromLocale(const Locale& locale, UnicodeString& result)
 }
 
 const Hashtable*
-LocaleUtility::getAvailableLocaleNames(const UnicodeString& /* bundleID */)
+LocaleUtility::getAvailableLocaleNames(const UnicodeString& bundleID)
 {
-    // have to ignore bundleID for the moment, since we don't have easy C++ api.
-    // assume it's the default bundle
+    // LocaleUtility_cache is a hash-of-hashes.  The top-level keys
+    // are path strings ('bundleID') passed to
+    // ures_openAvailableLocales.  The top-level values are
+    // second-level hashes.  The second-level keys are result strings
+    // from ures_openAvailableLocales.  The second-level values are
+    // garbage ((void*)1 or other random pointer).
+
+    Hashtable* cache;
+    umtx_lock(NULL);
+    cache = LocaleUtility_cache;
+    umtx_unlock(NULL);
+
+    if (cache == NULL) {
+        cache = new Hashtable();
+        if (cache == NULL) {
+            return NULL; // catastrophic failure; e.g. out of memory
+        }
+        cache->setValueDeleter(uhash_deleteHashtable);
+        Hashtable* h; // set this to final LocaleUtility_cache value
+        umtx_lock(NULL);
+        h = LocaleUtility_cache;
+        if (h == NULL) {
+            LocaleUtility_cache = h = cache;
+            cache = NULL;
+        }
+        umtx_unlock(NULL);
+        delete cache;
+        cache = h;
+    }
+
+    U_ASSERT(cache != NULL);
 
     Hashtable* htp;
     umtx_lock(NULL);
-    htp = LocaleUtility_cache;
+    htp = (Hashtable*) cache->get(bundleID);
     umtx_unlock(NULL);
 
     if (htp == NULL) {
         htp = new Hashtable();
         if (htp) {
             UErrorCode status = U_ZERO_ERROR;
-            int32_t count = uloc_countAvailable();
-            for (int32_t i = 0; i < count; ++i) {
-                UnicodeString temp(uloc_getAvailable(i), "");
-                htp->put(temp, (void*)htp, status);
-                if (U_FAILURE(status)) {
-                    delete htp;
-                    return NULL;
+            CharString cbundleID(bundleID);
+            const char* path = (const char*) cbundleID;
+            if (*path == 0) path = NULL; // empty string => NULL
+            UEnumeration *uenum = ures_openAvailableLocales(path, &status);
+            for (;;) {
+                const UChar* id = uenum_unext(uenum, NULL, &status);
+                if (id == NULL) {
+                    break;
                 }
+                htp->put(UnicodeString(id), (void*)htp, status);
+            }
+            uenum_close(uenum);
+            if (U_FAILURE(status)) {
+                delete htp;
+                return NULL;
             }
             umtx_lock(NULL);
-            if (LocaleUtility_cache == NULL) {
-                LocaleUtility_cache = htp;
-                htp = NULL;
-            }
+            cache->put(bundleID, (void*)htp, status);
             umtx_unlock(NULL);
-            delete htp;
         }
     }
-    return LocaleUtility_cache;
+    return htp;
 }
 
 UBool
