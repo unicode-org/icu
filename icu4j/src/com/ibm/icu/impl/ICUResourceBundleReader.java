59d6ee45f406575b337bcc4e435eca6a29d4f8e4
Alan Liu <alansliu@gmail.com>
2004-05-09 13:41:33 +0000

    [ICU-3517] fix compiler warnings X-Commit-URL: https://ssl.icu-project.org/trac/changeset/15222 Former-commit-id: e1aa9f6bd62d27aeb86d263a162eeb2ce69cd398

--- icu4j/src/com/ibm/icu/impl/ICUResourceBundleReader.java
+++ icu4j/src/com/ibm/icu/impl/ICUResourceBundleReader.java
@@ -2,7 +2,6 @@ package com.ibm.icu.impl;
 
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
-import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
--- icu4j/src/com/ibm/icu/text/RBBIDataWrapper.java
+++ icu4j/src/com/ibm/icu/text/RBBIDataWrapper.java
@@ -12,7 +12,6 @@ import java.io.InputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
 
-import com.ibm.icu.util.VersionInfo;
 import com.ibm.icu.impl.ICUData;
 import com.ibm.icu.impl.ICUResourceBundle;
 import com.ibm.icu.impl.Trie;
