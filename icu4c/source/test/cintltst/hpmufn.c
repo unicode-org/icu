c5705d37673996177f0b93aad5090f84ed4e0eef
Eric Mader <emader@svn.icu-project.org>
2004-05-19 21:19:13 +0000

    ICU-3770 Updated copyright notices for ICU 3.0
    X-SVN-Rev: 15413

--- icu4c/source/test/cintltst/hpmufn.c
+++ icu4c/source/test/cintltst/hpmufn.c
@@ -1,6 +1,6 @@
 /********************************************************************
  * COPYRIGHT: 
- * Copyright (c) 2003, International Business Machines Corporation and
+ * Copyright (c) 200-20043, International Business Machines Corporation and
  * others. All Rights Reserved.
  ********************************************************************/
 /*
