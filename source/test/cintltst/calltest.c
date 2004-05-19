4834dc28c9e3eb060c67a04f0caaa4a9959de5f1
Eric Mader <emader@svn.icu-project.org>
2004-05-19 21:19:13 +0000

    Updated copyright notices for ICU 3.0
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/3770
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@15413 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/test/cintltst/calltest.c
+++ source/test/cintltst/calltest.c
@@ -1,6 +1,6 @@
 /********************************************************************
  * COPYRIGHT: 
- * Copyright (c) 1996-2003, International Business Machines Corporation and
+ * Copyright (c) 1996-2004, International Business Machines Corporation and
  * others. All Rights Reserved.
  ********************************************************************/
 /********************************************************************************
