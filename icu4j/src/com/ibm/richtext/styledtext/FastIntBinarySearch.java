74de92029d9d07d3c84ffdf28b8221604ecc3f5c
Eric Mader <emader@svn.icu-project.org>
2004-05-21 19:29:31 +0000

    [ICU-3770] update copyright notices for ICU 3.0 X-Commit-URL: https://ssl.icu-project.org/trac/changeset/15462 Former-commit-id: 661ce772ab3b22281812689bdd2961a72ea31019

--- icu4j/src/com/ibm/richtext/styledtext/FastIntBinarySearch.java
+++ icu4j/src/com/ibm/richtext/styledtext/FastIntBinarySearch.java
@@ -1,5 +1,5 @@
 /*
-*   Copyright (C) 1996-2002, International Business Machines
+*   Copyright (C) 1996-2004, International Business Machines
 *   Corporation and others.  All Rights Reserved.
 */
 
