2e7f955c392765374cc04e63c7524f294de595bf
Alan Liu <alansliu@gmail.com>
2004-06-01 17:31:17 +0000

    do default conversion, not invariant conversion
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/3795
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@15651 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/test/cintltst/usettest.c
+++ source/test/cintltst/usettest.c
@@ -17,6 +17,7 @@
 static void TestAPI(void);
 static void Testj2269(void);
 static void TestSerialized(void);
+static void TestNonInvariantPattern(void);
 
 void addUSetTest(TestNode** root);
 
@@ -36,6 +37,7 @@ addUSetTest(TestNode** root) {
     TEST(TestAPI);
     TEST(Testj2269);
     TEST(TestSerialized);
+    TEST(TestNonInvariantPattern);
 }
 
 /*------------------------------------------------------------------
@@ -408,6 +410,26 @@ TestSerialized() {
         }
     }
 
+    uset_close(set);
+}
+
+/**
+ * Make sure that when non-invariant chars are passed to uset_openPattern
+ * they do not cause an ugly failure mode (e.g. assertion failure).
+ * JB#3795.
+ */
+static void
+TestNonInvariantPattern() {
+    UErrorCode ec = U_ZERO_ERROR;
+    /* The critical part of this test is that the following pattern
+       must contain a non-invariant character. */
+    static const char *pattern = "[:ccc!=0:]";
+    UChar buf[256];
+    int32_t len = u_unescape(pattern, buf, 256);
+    /* This test 'fails' by having an assertion failure within the
+       following call.  It passes by running to completion with no
+       assertion failure. */
+    USet *set = uset_openPattern(buf, len, &ec);
     uset_close(set);
 }
 
