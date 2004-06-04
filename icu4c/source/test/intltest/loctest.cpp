a70c18d79eb134da4260648a6e45acd7fd131f73
George Rhoten <grhoten@users.noreply.github.com>
2004-06-04 23:10:37 +0000

    [ICU-3699] Remove bam, which is a duplicate of bm. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/15736 Former-commit-id: 76d46c3a95b65ad03c4379f08c6f99825f57b9c3

--- icu4c/source/test/intltest/loctest.cpp
+++ icu4c/source/test/intltest/loctest.cpp
@@ -871,8 +871,8 @@ LocaleTest::TestGetLangsAndCountries()
     for(testCount = 0;test[testCount];testCount++)
       ;
 
-    if (testCount != 467)
-        errln("Expected getISOLanguages() to return 467 languages; it returned %d", testCount);
+    if (testCount != 466)
+        errln("Expected getISOLanguages() to return 466 languages; it returned %d", testCount);
     else {
         for (i = 0; i < 15; i++) {
             int32_t j;
