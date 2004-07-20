eafdc51dec67a4d1e32414459837ce19ee301061
George Rhoten <grhoten@svn.icu-project.org>
2004-07-20 00:40:25 +0000

    Make sure that data isn't exported when it is only needed internally.
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/3862
    
    A linker warning on AIX brought this issue to my attention.
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@16062 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/i18n/strrepl.cpp
+++ source/i18n/strrepl.cpp
@@ -1,6 +1,6 @@
 /*
 **********************************************************************
-*   Copyright (c) 2002-2003, International Business Machines Corporation
+*   Copyright (c) 2002-2004, International Business Machines Corporation
 *   and others.  All Rights Reserved.
 **********************************************************************
 *   Date        Name        Description
@@ -19,7 +19,7 @@
 
 U_NAMESPACE_BEGIN
 
-const UChar EMPTY[] = { 0 }; // empty string: ""
+static const UChar EMPTY[] = { 0 }; // empty string: ""
 
 UnicodeReplacer::~UnicodeReplacer() {}
 UOBJECT_DEFINE_RTTI_IMPLEMENTATION(StringReplacer)
