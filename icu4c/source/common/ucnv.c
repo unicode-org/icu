382dff8bfb9df3f6cab0373567ab54c028df923b
George Rhoten <grhoten@users.noreply.github.com>
2004-09-09 00:57:05 +0000

    [ICU-4078] Allow the majority of the converter API to not depend on ures_ API. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/16280 Former-commit-id: e7b98b902a99f3285540adc633434ca3f0be0474

--- icu4c/source/common/ucnv.c
+++ icu4c/source/common/ucnv.c
@@ -24,7 +24,6 @@
 #if !UCONFIG_NO_CONVERSION
 
 #include "unicode/ustring.h"
-#include "unicode/ures.h"
 #include "unicode/ucnv.h"
 #include "unicode/ucnv_err.h"
 #include "unicode/uset.h"
@@ -441,47 +440,6 @@ ucnv_setSubstChars (UConverter * converter,
     converter->subChar1 = 0;
     
     return;
-}
-
-U_CAPI int32_t U_EXPORT2
-ucnv_getDisplayName(const UConverter *cnv,
-                    const char *displayLocale,
-                    UChar *displayName, int32_t displayNameCapacity,
-                    UErrorCode *pErrorCode) {
-    UResourceBundle *rb;
-    const UChar *name;
-    int32_t length;
-
-    /* check arguments */
-    if(pErrorCode==NULL || U_FAILURE(*pErrorCode)) {
-        return 0;
-    }
-
-    if(cnv==NULL || displayNameCapacity<0 || (displayNameCapacity>0 && displayName==NULL)) {
-        *pErrorCode=U_ILLEGAL_ARGUMENT_ERROR;
-        return 0;
-    }
-
-    /* open the resource bundle and get the display name string */
-    rb=ures_open(NULL, displayLocale, pErrorCode);
-    if(U_FAILURE(*pErrorCode)) {
-        return 0;
-    }
-
-    /* use the internal name as the key */
-    name=ures_getStringByKey(rb, cnv->sharedData->staticData->name, &length, pErrorCode);
-    ures_close(rb);
-
-    if(U_SUCCESS(*pErrorCode)) {
-        /* copy the string */
-        u_memcpy(displayName, name, uprv_min(length, displayNameCapacity)*U_SIZEOF_UCHAR);
-    } else {
-        /* convert the internal name into a Unicode string */
-        *pErrorCode=U_ZERO_ERROR;
-        length=uprv_strlen(cnv->sharedData->staticData->name);
-        u_charsToUChars(cnv->sharedData->staticData->name, displayName, uprv_min(length, displayNameCapacity));
-    }
-    return u_terminateUChars(displayName, displayNameCapacity, length, pErrorCode);
 }
 
 /*resets the internal states of a converter
