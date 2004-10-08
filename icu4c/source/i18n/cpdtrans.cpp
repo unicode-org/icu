8cb161b581d4085359a99075adb3093b976fde0d
George Rhoten <grhoten@users.noreply.github.com>
2004-10-08 06:07:27 +0000

    [ICU-4078] Depend less on the default converter X-Commit-URL: https://ssl.icu-project.org/trac/changeset/16425 Former-commit-id: 608ff950b3227095ef2b5071ce2ef0182305d326

--- icu4c/source/i18n/cpdtrans.cpp
+++ icu4c/source/i18n/cpdtrans.cpp
@@ -26,6 +26,7 @@ static const UChar NEWLINE  = 10;
 
 // Empty string
 static const UChar EMPTY[] = {0}; //""
+static const UChar COLON_COLON[] = {0x3A, 0x3A, 0}; //"::"
 
 U_NAMESPACE_BEGIN
 
@@ -362,7 +363,7 @@ UnicodeString& CompoundTransliterator::toRules(UnicodeString& rulesSource,
         // If we are a compound RBT and if we have a global
         // filter, then emit it at the top.
         UnicodeString pat;
-        rulesSource.append("::").append(getFilter()->toPattern(pat, escapeUnprintable)).append(ID_DELIM);
+        rulesSource.append(COLON_COLON).append(getFilter()->toPattern(pat, escapeUnprintable)).append(ID_DELIM);
     }
     for (int32_t i=0; i<count; ++i) {
         UnicodeString rule;
