6610076857074787d49d3a84420a6e8c55b39ea1
Eric Mader <emader@svn.icu-project.org>
2004-10-29 19:24:42 +0000

    DeviceTable::getAdjustment() should use "~" for complement. PairPositioningFormat1Subtable::process() should check for zero pairValueCount.
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/4132
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@16677 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/layout/DeviceTables.cpp
+++ source/layout/DeviceTables.cpp
@@ -1,7 +1,7 @@
 /*
  * @(#)DeviceTables.cpp 1.5 00/03/15
  *
- * (C) Copyright IBM Corp. 1998, 1999, 2000, 2001 - All Rights Reserved
+ * (C) Copyright IBM Corp. 1998 - 2004 - All Rights Reserved
  *
  */
 
@@ -36,7 +36,7 @@ le_int16 DeviceTable::getAdjustment(le_uint16 ppem) const
         result = field;
 
         if ((field & fieldSignBits[format]) != 0) {
-            result |= ! fieldMasks[format];
+            result |= ~ fieldMasks[format];
         }
     }
 
