0d4435a55a93b50351e765926d777bf45368d7cb
George Rhoten <grhoten@users.noreply.github.com>
2004-11-13 00:13:36 +0000

    [ICU-4225] Properly remove non-ascii characters from source code. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/16849 Former-commit-id: b6ea6703b6b3bf97e1dd5b55c3aae97542a43586

--- icu4c/source/i18n/unicode/utmscale.h
+++ icu4c/source/i18n/unicode/utmscale.h
@@ -121,7 +121,7 @@
  *
  *  <tr>
  *    <td>1 sec</td>
- *    <td align="right">5.84542×10&#x00B9;&#x00B9;</td>
+ *    <td align="right">5.84542x10<sup>22</sup></td>
  *    <td align="right">285,420,920.94</td>
  *    <td align="right">136.10</td>
  *  </tr>
