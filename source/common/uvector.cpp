595278ca155551c61b6375fbe302e2c5ec809bc7
George Rhoten <grhoten@svn.icu-project.org>
2004-12-09 06:39:49 +0000

    Reduce UStack dependencies
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/4281
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@16934 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/uvector.cpp
+++ source/common/uvector.cpp
@@ -468,52 +468,5 @@ void UVector::sortedInsert(UHashTok tok, USortComparator *compare, UErrorCode& e
     }
 }
 
-UStack::UStack(UErrorCode &status) :
-    UVector(status)
-{
-}
-
-UStack::UStack(int32_t initialCapacity, UErrorCode &status) :
-    UVector(initialCapacity, status)
-{
-}
-
-UStack::UStack(UObjectDeleter *d, UKeyComparator *c, UErrorCode &status) :
-    UVector(d, c, status)
-{
-}
-
-UStack::UStack(UObjectDeleter *d, UKeyComparator *c, int32_t initialCapacity, UErrorCode &status) :
-    UVector(d, c, initialCapacity, status)
-{
-}
-
-UStack::~UStack() {}
-
-void* UStack::pop(void) {
-    int32_t n = size() - 1;
-    void* result = 0;
-    if (n >= 0) {
-        result = elementAt(n);
-        removeElementAt(n);
-    }
-    return result;
-}
-
-int32_t UStack::popi(void) {
-    int32_t n = size() - 1;
-    int32_t result = 0;
-    if (n >= 0) {
-        result = elementAti(n);
-        removeElementAt(n);
-    }
-    return result;
-}
-
-int32_t UStack::search(void* obj) const {
-    int32_t i = indexOf(obj);
-    return (i >= 0) ? size() - i : i;
-}
-
 U_NAMESPACE_END
 
