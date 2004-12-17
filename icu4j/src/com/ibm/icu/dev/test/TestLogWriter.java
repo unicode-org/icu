c3c55cbc5bafb4cae310d8897c87703fdf3570f8
Doug Felt <dougfelt@gmail.com>
2004-12-17 00:38:57 +0000

    [ICU-4289] timing, printwriter X-Commit-URL: https://ssl.icu-project.org/trac/changeset/16963 Former-commit-id: e4a2b0867ba5578c35fd494e7e84e570ba03a74f

---
+++ icu4j/src/com/ibm/icu/dev/test/TestLogWriter.java
@@ -0,0 +1,44 @@
+/**
+ *******************************************************************************
+ * Copyright (C) 2004, International Business Machines Corporation and         *
+ * others. All Rights Reserved.                                                *
+ *******************************************************************************
+ */
+package com.ibm.icu.dev.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+public final class TestLogWriter extends Writer {
+    private TestLog log;
+    private int level;
+    private boolean closed;
+
+    public TestLogWriter(TestLog log, int level) {
+	this.log = log;
+	this.level = level;
+    }
+
+    public void write(char cbuf[], int off, int len) throws IOException {
+	write(new String(cbuf, off, len));
+    }
+
+    public void write(String str) throws IOException {
+	if (closed) {
+	    throw new IOException("stream closed");
+	}
+	if ("\r\n".indexOf(str) != -1) {
+	    log.msg("", level, level == TestLog.ERR, true);
+	} else {
+	    log.msg(str, level, level == TestLog.ERR, false);
+	}
+    }
+
+    public void flush() throws IOException {
+    }
+
+    public void close() throws IOException {
+	closed = true;
+    }
+}
