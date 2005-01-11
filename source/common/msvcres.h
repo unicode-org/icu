bc40d1ca1595118a43b7b44a29bb7f3646aed5d5
George Rhoten <grhoten@svn.icu-project.org>
2005-01-11 23:35:15 +0000

    Fixes for when STLPort is first in your include path.
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/4339
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@17113 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/msvcres.h
+++ source/common/msvcres.h
@@ -7,5 +7,13 @@
 //header the wrong way. This is here to prevent Visual Studio .NET from
 //unnessarily building the resource files when it's not needed.
 //
+
+/*
+These are defined before unicode/uversion.h in order to prevent 
+STLPort's broken stddef.h from being used when rc.exe parses this file. 
+*/
+#define _STLP_OUTERMOST_HEADER_ID 0
+#define _STLP_WINCE 1
+
 #include "unicode/uversion.h"
 
