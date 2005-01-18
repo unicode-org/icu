02fbe1a61648b3a0eccc1233c02222b3f577ff8c
George Rhoten <grhoten@users.noreply.github.com>
2005-01-18 17:31:05 +0000

    [ICU-4288] Fix some compiler warnings X-Commit-URL: https://ssl.icu-project.org/trac/changeset/17143 Former-commit-id: 098e9e83dcf2d58d4228af8374f9fd1cd7a2dc57

--- icu4c/source/i18n/remtrans.cpp
+++ icu4c/source/i18n/remtrans.cpp
@@ -1,6 +1,6 @@
 /*
 **********************************************************************
-*   Copyright (c) 2001-2003, International Business Machines
+*   Copyright (c) 2001-2005, International Business Machines
 *   Corporation and others.  All Rights Reserved.
 **********************************************************************
 *   Date        Name        Description
@@ -14,7 +14,7 @@
 
 #include "remtrans.h"
 
-static const UChar ID[] = {65, 110, 121, 45, 0x52, 0x65, 0x6D, 0x6F, 0x76, 0x65, 0x00}; /* "Any-Remove" */
+static const UChar CURR_ID[] = {65, 110, 121, 45, 0x52, 0x65, 0x6D, 0x6F, 0x76, 0x65, 0x00}; /* "Any-Remove" */
 
 U_NAMESPACE_BEGIN
 
@@ -34,13 +34,13 @@ static Transliterator* RemoveTransliterator_create(const UnicodeString& /*ID*/,
  */
 void RemoveTransliterator::registerIDs() {
 
-    Transliterator::_registerFactory(::ID, RemoveTransliterator_create, integerToken(0));
+    Transliterator::_registerFactory(::CURR_ID, RemoveTransliterator_create, integerToken(0));
 
     Transliterator::_registerSpecialInverse(UNICODE_STRING_SIMPLE("Remove"),
                                             UNICODE_STRING_SIMPLE("Null"), FALSE);
 }
 
-RemoveTransliterator::RemoveTransliterator() : Transliterator(::ID, 0) {}
+RemoveTransliterator::RemoveTransliterator() : Transliterator(::CURR_ID, 0) {}
 
 RemoveTransliterator::~RemoveTransliterator() {}
 
