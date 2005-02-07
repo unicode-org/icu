7da305983e4139f71669392e8334ff50c3accccf
Ram Viswanadha <rviswanadha@gmail.com>
2005-02-07 19:54:21 +0000

    [ICU-4403] rename enum variable to iter.. enum is a keyword in JDK 1.5 X-Commit-URL: https://ssl.icu-project.org/trac/changeset/17192 Former-commit-id: c23b09ed1f6fa065280e5a5b369f4bc34c02e033

--- icu4j/src/com/ibm/icu/dev/demo/rbbi/DBBIDemo.java
+++ icu4j/src/com/ibm/icu/dev/demo/rbbi/DBBIDemo.java
@@ -46,7 +46,7 @@ class DBBIFrame extends Frame implements ItemListener
     final String right = "-->";
     final String left = "<--";
 
-    private BreakIterator enum;
+    private BreakIterator iter;
     private static boolean isctrldown_ = false;
 
 JTextArea text;
@@ -367,21 +367,21 @@ text.setWrapStyleWord(true);
         String s = bound.getSelectedItem();
         if (s.equals("Char")) {
             errorText("getCharacterInstance");
-            enum = BreakIterator.getCharacterInstance();
+            iter = BreakIterator.getCharacterInstance();
         }
         else if (s.equals("Word")) {
             errorText("getWordInstance");
-            enum = BreakIterator.getWordInstance();
+            iter = BreakIterator.getWordInstance();
         }
         else if (s.equals("Line Break")) {
             errorText("getLineInstance");
-            enum = BreakIterator.getLineInstance();
+            iter = BreakIterator.getLineInstance();
         }
         else /* if (s.equals("Sentence")) */ {
             errorText("getSentenceInstance");
-            enum = BreakIterator.getSentenceInstance();
+            iter = BreakIterator.getSentenceInstance();
         }
-        enum.setText(text.getText());
+        iter.setText(text.getText());
         selectRange(0, 0);
         //text.select(0,0);
     }
@@ -390,18 +390,18 @@ text.setWrapStyleWord(true);
     {
         try {
 //          System.out.println("entering handleForward");
-            enum.setText(text.getText());
+            iter.setText(text.getText());
             int oldStart = getSelectionStart();
             int oldEnd = getSelectionEnd();
 
 //          System.out.println("handleForward: oldStart=" + oldStart + ", oldEnd=" + oldEnd);
 
             if (oldEnd < 1) {
-                selectRange(0, enum.following(0));
+                selectRange(0, iter.following(0));
             }
             else {
-                int s = enum.following(oldEnd-1);
-                int e = enum.next();
+                int s = iter.following(oldEnd-1);
+                int e = iter.next();
                 if (e == -1) {
                     e = s;
                 }
@@ -419,15 +419,15 @@ text.setWrapStyleWord(true);
     public void handleBackward()
     {
         try {
-            enum.setText(text.getText());
+            iter.setText(text.getText());
             int oldStart = getSelectionStart();
             int oldEnd = getSelectionEnd();
             if (oldStart < 1) {
                 selectRange(0, 0);
             }
             else {
-                int e = enum.following(oldStart-1);
-                int s = enum.previous();
+                int e = iter.following(oldStart-1);
+                int s = iter.previous();
                 selectRange(s, e);
             }
             //text.select(s, e);
