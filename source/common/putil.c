187ce2d970cf0fccfec427c458d6950551ca0f76
George Rhoten <grhoten@svn.icu-project.org>
2005-03-10 03:27:07 +0000

    Fix the last checkin.
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/4425
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@17315 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/putil.c
+++ source/common/putil.c
@@ -1304,7 +1304,7 @@ u_setDataDirectory(const char *directory) {
         shared library is used, and this is a way to make sure that NULL
         is never returned.
         */
-        directory = "";
+        newDataDir = "";
     }
     else {
         length=(int32_t)uprv_strlen(directory);
