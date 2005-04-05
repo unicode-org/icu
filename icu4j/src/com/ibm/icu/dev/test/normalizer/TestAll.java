7bf48bcf7f7011611ddef553e11cf079860fe146
Doug Felt <dougfelt@gmail.com>
2005-04-05 16:02:49 +0000

    [ICU-4472] normalizer isNormalized array index out of bounds exception X-Commit-URL: https://ssl.icu-project.org/trac/changeset/17460 Former-commit-id: 5f5f502eb7506446cd0f5b22bae35a8d57209c6b

--- icu4j/src/com/ibm/icu/dev/test/normalizer/TestAll.java
+++ icu4j/src/com/ibm/icu/dev/test/normalizer/TestAll.java
@@ -24,6 +24,7 @@ public class TestAll extends TestGroup {
             "TestDeprecatedNormalizerAPI",
             "TestCanonicalIterator",
             "NormalizationMonkeyTest",
+	    "NormalizerRegressionTests",
         });
     }
 
