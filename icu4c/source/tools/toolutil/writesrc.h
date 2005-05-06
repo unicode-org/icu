346daeb6c1be509569883999fe17360c79ec7f71
Markus Scherer <markus.icu@gmail.com>
2005-05-06 21:49:48 +0000

    ICU-4497 add some function documentation
    X-SVN-Rev: 17565

--- icu4c/source/tools/toolutil/writesrc.h
+++ icu4c/source/tools/toolutil/writesrc.h
@@ -23,21 +23,42 @@
 #include "unicode/utypes.h"
 #include "utrie.h"
 
+/**
+ * Create a source text file and write a header comment with the ICU copyright.
+ */
 U_CAPI FILE * U_EXPORT2
 usrc_create(const char *path, const char *filename);
 
+/**
+ * Write the contents of an array of 8/16/32-bit words.
+ * The prefix and postfix are optional (can be NULL) and are written first/last.
+ * The prefix may contain a %ld or similar field for the array length.
+ * The {} and declaration etc. need to be included in prefix/postfix or
+ * printed before and after the array contents.
+ */
 U_CAPI void U_EXPORT2
 usrc_writeArray(FILE *f,
                 const char *prefix,
                 const void *p, int32_t width, int32_t length,
                 const char *postfix);
 
+/**
+ * Calls usrc_writeArray() for the index and data arrays of a runtime UTrie.
+ * Only the index array is written for a 16-bit UTrie. In this case, dataPrefix
+ * is ignored and can be NULL.
+ */
 U_CAPI void U_EXPORT2
 usrc_writeUTrieArrays(FILE *f,
                       const char *indexPrefix, const char *dataPrefix,
                       const UTrie *pTrie,
                       const char *postfix);
 
+/**
+ * Writes the UTrie struct values.
+ * The {} and declaration etc. need to be included in prefix/postfix or
+ * printed before and after the array contents.
+ * If getFoldingOffsetName==NULL then "utrie_defaultGetFoldingOffset" is printed.
+ */
 U_CAPI void U_EXPORT2
 usrc_writeUTrieStruct(FILE *f,
                       const char *prefix,
