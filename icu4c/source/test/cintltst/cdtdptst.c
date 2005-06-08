7448f4907f916d897fbb5b3f9484a4a55ea59bcf
Doug Felt <dougfelt@gmail.com>
2005-06-08 16:15:44 +0000

    [ICU-646] wall time support in icu - fix chech459 test to deal with CST issue X-Commit-URL: https://ssl.icu-project.org/trac/changeset/17830 Former-commit-id: 623dfd78e3545d80949e0c39792e95b41091facb

--- icu4c/source/test/cintltst/cdtdptst.c
+++ icu4c/source/test/cintltst/cdtdptst.c
@@ -1,6 +1,6 @@
 /********************************************************************
  * COPYRIGHT: 
- * Copyright (c) 1997-2003, International Business Machines Corporation and
+ * Copyright (c) 1997-2005, International Business Machines Corporation and
  * others. All Rights Reserved.
  ********************************************************************/
 /********************************************************************************
@@ -233,9 +233,11 @@ void TestCzechMonths459()
     UErrorCode status = U_ZERO_ERROR;
     UChar *date;
     
+	ctest_setTimeZone(NULL, &status);
     fmt = udat_open(UDAT_FULL, UDAT_FULL, "cs", NULL, 0, NULL, 0, &status);
     if(U_FAILURE(status)){
         log_err("Error in constructing the date format\n");
+		ctest_resetTimeZone();
         return;
     }
     lneed=0;
@@ -256,7 +258,6 @@ void TestCzechMonths459()
     ucal_setDate(cal, 1997, UCAL_JULY, 15, &status);
     july=ucal_getMillis(cal, &status);
 
-    
     juneStr = myDateFormat(fmt, june);
     julyStr = myDateFormat(fmt, july);
     pos=0;
@@ -278,6 +279,7 @@ void TestCzechMonths459()
     else
         log_verbose("Pass: Date = %s (czech month July)\n", aescstrdup(date, -1));
     
+	ctest_resetTimeZone();
     udat_close(fmt);
     ucal_close(cal);
     free(pattern);
