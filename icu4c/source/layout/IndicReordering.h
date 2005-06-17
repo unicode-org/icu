740bd07ce9df57745fcb57a05db714fe3b8ce6dc
Eric Mader <emader@svn.icu-project.org>
2005-06-17 19:20:43 +0000

    [ICU-4578] Fix warnings on Intel compiler. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/17942 Former-commit-id: 0b3cb89b5655ed2b771226600807c3f72334f133

--- icu4c/source/layout/IndicReordering.h
+++ icu4c/source/layout/IndicReordering.h
@@ -18,11 +18,54 @@
 U_NAMESPACE_BEGIN
 
 // Characters that get refered to by name...
-enum
-{
-    C_SIGN_ZWNJ     = 0x200C,
-    C_SIGN_ZWJ      = 0x200D
-};
+#define C_SIGN_ZWNJ           0x200C
+#define C_SIGN_ZWJ            0x200D
+
+// Character class values
+#define CC_RESERVED               0U
+#define CC_VOWEL_MODIFIER         1U
+#define CC_STRESS_MARK            2U
+#define CC_INDEPENDENT_VOWEL      3U
+#define CC_INDEPENDENT_VOWEL_2    4U
+#define CC_CONSONANT              5U
+#define CC_CONSONANT_WITH_NUKTA   6U
+#define CC_NUKTA                  7U
+#define CC_DEPENDENT_VOWEL        8U
+#define CC_SPLIT_VOWEL_PIECE_1    9U
+#define CC_SPLIT_VOWEL_PIECE_2   10U
+#define CC_SPLIT_VOWEL_PIECE_3   11U
+#define CC_VIRAMA                12U
+#define CC_ZERO_WIDTH_MARK       13U
+#define CC_COUNT                 14U
+
+// Character class flags
+#define CF_CLASS_MASK    0x0000FFFFU
+
+#define CF_CONSONANT     0x80000000U
+
+#define CF_REPH          0x40000000U
+#define CF_VATTU         0x20000000U
+#define CF_BELOW_BASE    0x10000000U
+#define CF_POST_BASE     0x08000000U
+#define CF_LENGTH_MARK   0x04000000U
+
+#define CF_POS_BEFORE    0x00300000U
+#define CF_POS_BELOW     0x00200000U
+#define CF_POS_ABOVE     0x00100000U
+#define CF_POS_AFTER     0x00000000U
+#define CF_POS_MASK      0x00300000U
+
+#define CF_INDEX_MASK    0x000F0000U
+#define CF_INDEX_SHIFT   16
+
+// Script flag bits
+#define SF_MATRAS_AFTER_BASE     0x80000000U
+#define SF_REPH_AFTER_BELOW      0x40000000U
+#define SF_EYELASH_RA            0x20000000U
+#define SF_MPRE_FIXUP            0x10000000U
+
+#define SF_POST_BASE_LIMIT_MASK  0x0000FFFFU
+#define SF_NO_POST_BASE_LIMIT    0x00007FFFU
 
 typedef LEUnicode SplitMatra[3];
 
@@ -31,61 +74,8 @@ class LEGlyphStorage;
 
 struct IndicClassTable
 {
-    enum CharClassValues
-    {
-        CC_RESERVED             =  0,
-        CC_VOWEL_MODIFIER       =  1,
-        CC_STRESS_MARK          =  2,
-        CC_INDEPENDENT_VOWEL    =  3,
-        CC_INDEPENDENT_VOWEL_2  =  4,
-        CC_CONSONANT            =  5,
-        CC_CONSONANT_WITH_NUKTA =  6,
-        CC_NUKTA                =  7,
-        CC_DEPENDENT_VOWEL      =  8,
-        CC_SPLIT_VOWEL_PIECE_1  =  9,
-        CC_SPLIT_VOWEL_PIECE_2  = 10,
-        CC_SPLIT_VOWEL_PIECE_3  = 11,
-        CC_VIRAMA               = 12,
-        CC_ZERO_WIDTH_MARK      = 13,
-        CC_COUNT                = 14
-    };
-
-    enum CharClassFlags
-    {
-        CF_CLASS_MASK   = 0x0000FFFF,
-
-        CF_CONSONANT    = 0x80000000,
-
-        CF_REPH         = 0x40000000,
-        CF_VATTU        = 0x20000000,
-        CF_BELOW_BASE   = 0x10000000,
-        CF_POST_BASE    = 0x08000000,
-        CF_LENGTH_MARK  = 0x04000000,
-
-        CF_POS_BEFORE   = 0x00300000,
-        CF_POS_BELOW    = 0x00200000,
-        CF_POS_ABOVE    = 0x00100000,
-        CF_POS_AFTER    = 0x00000000,
-        CF_POS_MASK     = 0x00300000,
-        
-        CF_INDEX_MASK   = 0x000F0000,
-        CF_INDEX_SHIFT  = 16
-    };
-
     typedef le_uint32 CharClass;
-
-    enum ScriptFlagBits
-    {
-        SF_MATRAS_AFTER_BASE    = 0x80000000,
-        SF_REPH_AFTER_BELOW     = 0x40000000,
-        SF_EYELASH_RA           = 0x20000000,
-        SF_MPRE_FIXUP           = 0x10000000,
-
-        SF_POST_BASE_LIMIT_MASK = 0x0000FFFF,
-        SF_NO_POST_BASE_LIMIT   = 0x00007FFF
-    };
-
-    typedef le_int32 ScriptFlags;
+    typedef le_uint32 ScriptFlags;
 
     LEUnicode firstChar;
     LEUnicode lastChar;
