1961c9b1d4b2375206bea04c99263bd580557d3d
George Rhoten <grhoten@users.noreply.github.com>
2005-06-23 22:44:44 +0000

    [ICU-4524] Fix copyright based on last change. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/18003 Former-commit-id: 10f47b74fb14f74c2ffe34382749814010f01681

--- icu4c/source/i18n/unicode/currunit.h
+++ icu4c/source/i18n/unicode/currunit.h
@@ -1,6 +1,6 @@
 /*
 **********************************************************************
-* Copyright (c) 2005, International Business Machines
+* Copyright (c) 2004-2005, International Business Machines
 * Corporation and others.  All Rights Reserved.
 **********************************************************************
 * Author: Alan Liu
