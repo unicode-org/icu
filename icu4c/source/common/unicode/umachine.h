2bcd5cb5d38aa4788e857ff1107b5d7d11abc159
George Rhoten <grhoten@users.noreply.github.com>
2005-06-24 23:24:53 +0000

    [ICU-739] Add Palm OS header. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/18046 Former-commit-id: 970273e42e6bcfc2aa83a4c0b5fc52f4367d24a8

--- icu4c/source/common/unicode/umachine.h
+++ icu4c/source/common/unicode/umachine.h
@@ -41,7 +41,9 @@
 /* which are contained in the platform-specific file platform.h             */
 /*==========================================================================*/
 
-#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+#if defined(U_PALMOS)
+#   include "unicode/ppalmos.h"
+#elif defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
 #   include "unicode/pwin32.h"
 #else
 #   include "unicode/platform.h"
