562f1d8518f0ffc90f62efcf332b96c0f58c95ef
George Rhoten <grhoten@users.noreply.github.com>
2005-07-07 22:27:00 +0000

    [ICU-4552] Test fix for new and delete for MFC on Windows. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/18168 Former-commit-id: d1b4725246435339643c8324e2ce8d82cf616602

--- icu4c/source/test/intltest/uobjtest.h
+++ icu4c/source/test/intltest/uobjtest.h
@@ -1,6 +1,6 @@
 /********************************************************************
  * COPYRIGHT: 
- * Copyright (c) 2002-2004, International Business Machines Corporation and
+ * Copyright (c) 2002-2005, International Business Machines Corporation and
  * others. All Rights Reserved.
  ********************************************************************/
 
@@ -20,6 +20,7 @@ private:
     // tests
     void testIDs();
     void testUMemory();
+    void TestMFCCompatibility();
 
     //helper
 
