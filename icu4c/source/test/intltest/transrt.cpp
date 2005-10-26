a9a77392727b4e075d9a337efd97b8838cf7e6c4
George Rhoten <grhoten@users.noreply.github.com>
2005-10-26 18:21:09 +0000

    ICU-4881 Hit the snooze alarm to go off at ICU 3.6 version change.
    X-SVN-Rev: 18719

--- icu4c/source/test/intltest/transrt.cpp
+++ icu4c/source/test/intltest/transrt.cpp
@@ -83,7 +83,7 @@ TransliteratorRoundTripTest::runIndexedTest(int32_t index, UBool exec,
 // Time bomb - allows temporary behavior that expires at a given
 //             release
 //--------------------------------------------------------------------
-static const UVersionInfo ICU_35 = {3,5,0,0};
+static const UVersionInfo ICU_36 = {3,6,0,0};
 
 static UBool isICUVersionAtLeast(const UVersionInfo x) {
     UVersionInfo v;
@@ -1143,7 +1143,7 @@ void TransliteratorRoundTripTest::TestHan() {
 
 void TransliteratorRoundTripTest::TestGreek() {
 
-    if (isICUVersionAtLeast(ICU_35)) {
+    if (isICUVersionAtLeast(ICU_36)) {
         // We temporarily filter against Unicode 4.1, but we only do this
         // before version 3.4.
         errln("FAIL: TestGreek needs to be updated to remove delete the [:Age=4.0:] filter ");
@@ -1175,7 +1175,7 @@ void TransliteratorRoundTripTest::TestGreek() {
 
 void TransliteratorRoundTripTest::TestGreekUNGEGN() {
 
-    if (isICUVersionAtLeast(ICU_35)) {
+    if (isICUVersionAtLeast(ICU_36)) {
         // We temporarily filter against Unicode 4.1, but we only do this
         // before version 3.4.
         errln("FAIL: TestGreek needs to be updated to remove delete the [:Age=4.0:] filter ");
@@ -1204,7 +1204,7 @@ void TransliteratorRoundTripTest::TestGreekUNGEGN() {
 
 void TransliteratorRoundTripTest::Testel() {
     
-    if (isICUVersionAtLeast(ICU_35)) {
+    if (isICUVersionAtLeast(ICU_36)) {
         // We temporarily filter against Unicode 4.1, but we only do this
         // before version 3.4.
         errln("FAIL: TestGreek needs to be updated to remove delete the [:Age=4.0:] filter ");
@@ -1272,7 +1272,7 @@ UBool LegalHebrew::is(const UnicodeString& sourceString)const{
     return TRUE;
 }
 void TransliteratorRoundTripTest::TestHebrew() {
-    if (isICUVersionAtLeast(ICU_35)) {
+    if (isICUVersionAtLeast(ICU_36)) {
         // We temporarily filter against Unicode 4.1, but we only do this
         // before version 3.4.
         errln("FAIL: TestHebrew needs to be updated to remove delete the [:Age=4.0:] filter ");
