89792b9d887cee52b5ca01666a65e0e0c5360cbe
Ram Viswanadha <rviswanadha@gmail.com>
2005-11-11 18:55:32 +0000

    ICU-4881 update version number
    X-SVN-Rev: 18786

--- icu4j/src/com/ibm/icu/util/VersionInfo.java
+++ icu4j/src/com/ibm/icu/util/VersionInfo.java
@@ -121,7 +121,7 @@ public final class VersionInfo
      * @draft ICU 3.0
      * @deprecated This is a draft API and might change in a future release of ICU.
      */
-    public static final String ICU_DATA_VERSION = "34b";
+    public static final String ICU_DATA_VERSION = "35b";
 
     /**
      * ICU4J collator runtime version
@@ -441,7 +441,7 @@ public final class VersionInfo
         UNICODE_4_0   = getInstance(4, 0, 0, 0);
         UNICODE_4_0_1 = getInstance(4, 0, 1, 0);
         UNICODE_4_1   = getInstance(4, 1, 0, 0);
-        ICU_VERSION = getInstance(3, 4, 0, 0);
+        ICU_VERSION   = getInstance(3, 5, 0, 0);
         UCOL_RUNTIME_VERSION = getInstance(6);
         UCOL_BUILDER_VERSION = getInstance(7);
         UCOL_TAILORINGS_VERSION = getInstance(1);
