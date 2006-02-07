a93842f01e2ce7eb43e2ebaca7f4c8d88c5aa5de
George Rhoten <grhoten@users.noreply.github.com>
2006-02-07 07:50:53 +0000

    ICU-4639 Improve the thread safety and simplify the UEnumeration
    X-SVN-Rev: 19078

--- icu4c/source/i18n/ucsdet.cpp
+++ icu4c/source/i18n/ucsdet.cpp
@@ -11,12 +11,7 @@
 #include "csdetect.h"
 #include "csmatch.h"
 
-#include "unicode/putil.h"
-
 #include "cmemory.h"
-#include "uenumimp.h"
-
-U_NAMESPACE_USE
 
 #define ARRAY_SIZE(array) (sizeof array / sizeof array[0])
 
@@ -144,113 +139,6 @@ ucsdet_detectAll(UCharsetDetector *ucsd,
 //     }
 //     return UCharsetDetector::getDetectableCount();
 // }
-U_CDECL_END
-
-typedef struct {
-    int32_t currIndex;
-    int32_t maxIndex;
-    char *currChar;
-    UChar *currUChar;
-    char **array;
-} Context;
-
-#define cont ((Context *)en->context)
-
-static void U_CALLCONV
-enumClose(UEnumeration *en) {
-    if(cont->currUChar != NULL) {
-        DELETE_ARRAY(cont->currUChar);
-        cont->currUChar = NULL;
-    }
-
-    delete en;
-}
-
-static int32_t U_CALLCONV
-enumCount(UEnumeration *en, UErrorCode *status) {
-    return cont->maxIndex;
-}
-
-static const UChar* U_CALLCONV 
-enumUNext(UEnumeration *en, int32_t *resultLength, UErrorCode *status) {
-    if(cont->currIndex >= cont->maxIndex) {
-        return NULL;
-    }
-
-    if(cont->currUChar == NULL) {
-        cont->currUChar = NEW_ARRAY(UChar, 1024);
-    }
-
-    cont->currChar = (cont->array)[cont->currIndex];
-    *resultLength = (int32_t)strlen(cont->currChar);
-    u_charsToUChars(cont->currChar, cont->currUChar, *resultLength);
-    cont->currIndex++;
-
-    return cont->currUChar;
-}
-
-static const char* U_CALLCONV
-enumNext(UEnumeration *en, int32_t *resultLength, UErrorCode *status) {
-    if(cont->currIndex >= cont->maxIndex) {
-        return NULL;
-    }
-
-    cont->currChar = (cont->array)[cont->currIndex];
-    *resultLength = (int32_t)strlen(cont->currChar);
-    cont->currIndex++;
-
-    return cont->currChar;
-}
-
-static void U_CALLCONV
-enumReset(UEnumeration *en, UErrorCode *status) {
-    cont->currIndex = 0;
-}
-
-Context enumContext = {
-    0, 0,
-    NULL, NULL,
-    NULL
-};
-
-UEnumeration enumeration = {
-    NULL,
-    &enumContext,
-    enumClose,
-    enumCount,
-    enumUNext,
-    enumNext,
-    enumReset
-};
-
-
-static UEnumeration *getEnum(const char** source, int32_t size) {
-    UEnumeration *en = new UEnumeration;
-    memcpy(en, &enumeration, sizeof(UEnumeration));
-    cont->array = (char **)source;
-    cont->maxIndex = size;
-    return en;
-}
-
-static const char** charsets = NEW_ARRAY(const char *, CharsetDetector::getDetectableCount());
-
-U_CDECL_BEGIN
-U_DRAFT  UEnumeration * U_EXPORT2
-ucsdet_getAllDetectableCharsets(const UCharsetDetector *ucsd,  UErrorCode *status)
-{
-    if(U_FAILURE(*status)) {
-        return 0;
-    }
-
-    CharsetDetector *csd = (CharsetDetector *) ucsd;
-    int32_t size = CharsetDetector::getDetectableCount();
-
-    for(int32_t index=0; index<size; index += 1) {
-        charsets[index] = csd->getCharsetName(index, *status);
-    }
-
-    return getEnum(charsets, size);
-}
 
 U_DRAFT  UBool U_EXPORT2
 ucsdet_isInputFilterEnabled(const UCharsetDetector *ucsd)
