c59aa90135558ccd3d7a56bea57035905ce10120
Doug Felt <dougfelt@gmail.com>
2006-02-14 01:02:59 +0000

    [ICU-5051] swat deprecated tag X-Commit-URL: https://ssl.icu-project.org/trac/changeset/19154 Former-commit-id: 16771b9bab032fea79a518f65bff8ac918ecc67b

--- icu4j/src/com/ibm/icu/dev/tool/docs/SwatDeprecated.java
+++ icu4j/src/com/ibm/icu/dev/tool/docs/SwatDeprecated.java
@@ -19,13 +19,14 @@ public class SwatDeprecated {
     private boolean overwrite;
     private int verbosity;
     private int cc; // changed file count
+    private boolean inPlace;
 
     private PrintWriter pw = new PrintWriter(System.out);
 
     private static FilenameFilter ff = new FilenameFilter() {
             public boolean accept(File dir, String name) {
                 return (new File(dir, name).isDirectory() && !"CVS".equals(name)) ||
-                    name.endsWith(".java");
+                    (!name.equals("SwatDeprecated.java") && name.endsWith(".java"));
             }
         };
 
@@ -66,8 +67,11 @@ public class SwatDeprecated {
                 else if (arg.equals("-noisy")) { // output names of files not modified
                     vrb = 3;
                 } 
-                else if (arg.equals("-debug")) { // output copyright debugging
+                else if (arg.equals("-copydebug")) { // output copyright debugging
                     vrb = 4;
+                }
+                else if (arg.equals("-debug")) { // output all debugging
+                    vrb = 5;
                 }
             }
         }
@@ -98,6 +102,7 @@ public class SwatDeprecated {
             throw re;
         }
 
+        this.inPlace = srcPrefix.equals(dstPrefix);
         this.cc = 0;
 
         if (verbosity >= 1) {
@@ -113,6 +118,9 @@ public class SwatDeprecated {
     }
 
     public void run() {
+        if (!srcFile.exists()) {
+            throw new RuntimeException("file " + srcFile.getPath() + " does not exist.");
+        }
         doList(srcFile);
         if (verbosity >= 1) {
             pw.println("changed " + cc + " file(s)");
@@ -121,9 +129,9 @@ public class SwatDeprecated {
     }
 
     public void doList(File file) {
-        File[] files = file.listFiles(ff);
-        for (int i = 0; i < files.length; ++i) {
-            File f = files[i];
+        String[] filenames = file.list(ff);
+        for (int i = 0; i < filenames.length; ++i) {
+            File f = new File(file, filenames[i]);
             if (f.isDirectory()) {
                 doList(f);
             } else {
@@ -132,36 +140,132 @@ public class SwatDeprecated {
         }
     }
 
-    public void processFile(File inFile) {
-        try {
-            String name = dstPrefix + inFile.getCanonicalPath().substring(srcPrefix.length());
-            File outFile = new File(name + ".tmp");
-            outFile.getParentFile().mkdirs();
+    /*
+      if infile != outfile
+      - if not overwrite and outfile exists, exit with error
+      - if outfile.tmp exists, delete it
+      --  exit with error if fail to delete
+      - create new outfile.tmp
+      - if don't need it, 
+      --  delete outfile.tmp
+      --  exit with nothing done
+      - if outfile.old exists, delete it
+      --  if fail to delete
+      ---   delete outfile.tmp
+      ---   exit with error (extra error if outfile doesn't exist)
+<     - if outfile exists, 
+<     --  rename it to outfile.old
+      - rename outfile.tmp to outfile
+      - return success
 
-            FileWriter fw = new FileWriter(outFile);
+      if infile == outfile
+      - if not overwrite (we know outfile exists) exit with error
+      - if outfile.tmp exists, delete it
+      --  exit with error if fail to delete
+>     - get infile out of the way for creation
+>     --  if infile.bak exists, delete it
+>     ---   exit with error if fail to delete
+>     --  rename infile to infile.bak
+      - create new outfile.tmp
+      - if don't need it,
+      --  delete outfile.tmp
+>     --  rename infile.bak to infile
+      --  exit with nothing done
+      - if outfile.old exists, delete it
+      --  if fail to delete
+      ---   delete outfile.tmp
+>     ---   rename infile.bak to infile
+      ---   exit with error
+>     - rename infile.bak to outfile.old
+      - rename outfile.tmp to outfile
+      - exit with success
+
+     */
+    public void processFile(File inFile) {
+        File bakFile = null;
+        File oldFile = null;
+        try {
+            String inPath = inFile.getCanonicalPath();
+            if (verbosity >= 5) {
+                pw.println("processFile: " + inPath);
+            }
+
+            String outPath = dstPrefix + inPath.substring(srcPrefix.length());
+            File outFile = new File(outPath);
+            if (!overwrite && outFile.exists()) {
+                throw new RuntimeException("no permission to overwrite file: " + outPath);
+            }
+            if (verbosity >= 5) {
+                pw.println("outFile: " + outPath);
+            }
+
+            String tmpPath = outPath + ".tmp";
+            File tmpFile = new File(tmpPath);
+            if (tmpFile.exists()) {
+                if (!tmpFile.delete()) {
+                    tmpFile = null;
+                    throw new RuntimeException("could not delete existing temporary file: " + tmpPath);
+                }
+            }
+            if (verbosity >= 5) {
+                pw.println("tmpFile: " + tmpPath);
+            }
+
+            FileReader fr = null;
+            String bakPath = inPath + ".bak";
+            if (inPlace) {
+                bakFile = new File(bakPath);
+                if (bakFile.exists()) {
+                    if (!bakFile.delete()) {
+                        bakFile = null;
+                        throw new RuntimeException("could not delete existing backup file: " + bakPath);
+                    }
+                }
+                if (!inFile.renameTo(bakFile)) {
+                    bakFile = null;
+                    throw new InternalError("can't rename: " + inPath + " to: " + bakPath);
+                }
+                if (verbosity >= 5) {
+                    pw.println("bakFile: " + bakPath);
+                    pw.println("inFile.exists(): " + inFile.exists());
+                    pw.println("bakFile.exists(): " + bakFile.exists());
+                }
+                fr = new FileReader(bakFile);
+            } else {
+                fr = new FileReader(inFile);
+            }
+
+            // ensure parent directory tree exists
+            tmpFile.getParentFile().mkdirs();
+
+            // operate on backup and temporary files
+            FileWriter fw = new FileWriter(tmpFile);
             BufferedWriter bw = new BufferedWriter(fw);
 
-            FileReader fr = new FileReader(inFile);
             BufferedReader br = new BufferedReader(fr);
             String line;
             int n = 0;
             int tc = 0;
             boolean debug = false;
             while (null != (line = br.readLine())) {
-                int temp = line.indexOf("@deprecated");
+                // int temp = line.indexOf("@deprecated");
                 int ix = line.indexOf(srcTag);
-                if (temp != -1 && ix == -1) {
-                    if (debug == false) {
-                        debug = true;
-                        pw.println("file: " + name);
-                    }
-                    pw.println("[" + n + "] " + line);
-                    pw.flush();
-                }
+//                 if (temp != -1 && ix == -1) {
+//                     if (debug == false) {
+//                         debug = true;
+//                         pw.println("file: " + name);
+//                     }
+//                     pw.println("[" + n + "] " + line);
+//                     pw.flush();
+//                 }
                 if (ix != -1) {
+//                     pw.println("[" + n + "] " + line); // debug
+
                     line = line.substring(0,ix) + trgTag;
+                    
                     ++tc;
                 } else if (n < 20) {
+                    // swat copyrights while we're at it
                     ix = line.indexOf("opyright");
                     if (ix != -1) {
                         String nline = null;
@@ -172,6 +276,11 @@ public class SwatDeprecated {
                             ix = line.indexOf("-200");
                             if (ix != -1) {
                                 nline = line.substring(0, ix) + "-2006" + line.substring(ix+5);
+                                break;
+                            }
+                            ix = line.indexOf("- 200");
+                            if (ix != -1) {
+                                nline = line.substring(0, ix) + "-2006" + line.substring(ix+6);
                                 break;
                             }
                             ix = line.indexOf("-199");
@@ -208,30 +317,58 @@ public class SwatDeprecated {
 
             if (tc == 0) { // nothing changed, forget this file
                 if (verbosity >= 3) {
-                    pw.println("no changes in file: " + name);
+                    pw.println("no changes in file: " + inPath);
                 }
-                if (!outFile.delete()) {
-                    throw new RuntimeException("unable to delete unneeded temporary file: " + name + ".tmp");
+                if (!tmpFile.delete()) {
+                    throw new RuntimeException("unable to delete unneeded temporary file: " + tmpPath);
+                }
+                if (bakFile != null ) {
+                    if (verbosity >= 5) {
+                        pw.println("trying to rename: " + bakFile.getPath() + " to: " + inFile.getPath());
+                        pw.println("bakFile.exists(): " + bakFile.exists());
+                        pw.println("inFile.exists(): " + inFile.exists());
+                    }
+
+                    if (!bakFile.renameTo(inFile)) {
+                        throw new RuntimeException("could not restore backup file: " + bakPath);
+                    }
                 }
                 return;
             }
 
-            outFile.setLastModified(inFile.lastModified());
-            File newFile = new File(name);
-            if (newFile.exists()) {
-                if (!overwrite) {
-                    throw new RuntimeException("file " + name + " already exists");
-                } else {
-                    if (!newFile.delete()) {
-                        throw new RuntimeException("could not delete existing file: " + name);
+            String oldPath = outPath + ".old";
+            oldFile = new File(oldPath);
+            if (oldFile.exists()) {
+                if (!oldFile.delete()) {
+                    tmpFile.delete(); // ignore deletion error
+                    if (bakFile != null) {
+                        if (!bakFile.renameTo(inFile)) {
+                            throw new RuntimeException("could not restore backup file: " + bakPath);
+                        }
                     }
+                    throw new RuntimeException("cannot remove existing old file: " + oldPath);
                 }
             }
-            if (!outFile.renameTo(newFile)) {
-                 throw new RuntimeException("could not rename file to " + name);
+
+            if (inPlace) {
+                if (!bakFile.renameTo(oldFile)) {
+                    throw new RuntimeException("cannot create old file from backup: " + bakPath);
+                }
+            } else if (outFile.exists()) {
+                if (!outFile.renameTo(oldFile)) {
+                    // todo: error recovery
+                    throw new RuntimeException("cannot create old file from original: " + outPath);
+                }
             }
+
+            if (!tmpFile.renameTo(outFile)) {
+                throw new RuntimeException("could not rename temporary file to output file: " + outPath);
+            }
+
+            outFile.setLastModified(inFile.lastModified());
+
             if (verbosity >= 2) {
-                pw.println(name);
+                pw.println(inPath);
                 pw.flush();
             }
         }
@@ -239,6 +376,9 @@ public class SwatDeprecated {
             RuntimeException re = new RuntimeException(e.getMessage());
             re.initCause(e);
             throw re;
+        }
+        finally {
+            pw.flush();
         }
 
         ++cc;
