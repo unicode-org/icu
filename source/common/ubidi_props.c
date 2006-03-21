03397ccbe2d0af61cee921251fcac526e556808f
George Rhoten <grhoten@svn.icu-project.org>
2006-03-21 05:04:29 +0000

    Fix some compiler warnings
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/4707
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@19387 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/ubidi_props.c
+++ source/common/ubidi_props.c
@@ -1,7 +1,7 @@
 /*
 *******************************************************************************
 *
-*   Copyright (C) 2004-2005, International Business Machines
+*   Copyright (C) 2004-2006, International Business Machines
 *   Corporation and others.  All Rights Reserved.
 *
 *******************************************************************************
@@ -199,17 +199,21 @@ ubidi_closeProps(UBiDiProps *bdp) {
 /* UBiDiProps singleton ----------------------------------------------------- */
 
 static UBiDiProps *gBdp=NULL, *gBdpDummy=NULL;
+#if !UBIDI_HARDCODE_DATA
 static UErrorCode gErrorCode=U_ZERO_ERROR;
 static int8_t gHaveData=0;
+#endif
 
 static UBool U_CALLCONV
 ubidi_cleanup(void) {
     ubidi_closeProps(gBdp);
     gBdp=NULL;
-    gErrorCode=U_ZERO_ERROR;
-    gHaveData=0;
     ubidi_closeProps(gBdpDummy);
     gBdpDummy=NULL;
+#if !UBIDI_HARDCODE_DATA
+    gErrorCode=U_ZERO_ERROR;
+    gHaveData=0;
+#endif
     return TRUE;
 }
 
