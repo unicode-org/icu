83f6ab2d96bddb8bd7a027a58ae268c158e59a33
George Rhoten <grhoten@svn.icu-project.org>
2006-03-22 00:38:45 +0000

    Move deprecated virtual inline code into the rest of the Hebrew calendar.
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/5032
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@19397 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/i18n/hebrwcal.cpp
+++ source/i18n/hebrwcal.cpp
@@ -1,9 +1,10 @@
 /*
- * Copyright (C) 2003-2006, International Business Machines Corporation
- * and others. All Rights Reserved.
- ******************************************************************************
+******************************************************************************
+* Copyright (C) 2003-2006, International Business Machines Corporation
+* and others. All Rights Reserved.
+******************************************************************************
  *
- * File HEBRWCAL.H
+ * File HEBRWCAL.CPP
  *
  * Modification History:
  *
@@ -261,6 +262,14 @@ void HebrewCalendar::add(UCalendarDateFields field, int32_t amount, UErrorCode& 
     Calendar::add(field, amount, status);
     break;
   }
+}
+
+/**
+ * @deprecated ICU 2.6 use UCalendarDateFields instead of EDateFields
+ */
+void HebrewCalendar::add(EDateFields field, int32_t amount, UErrorCode& status)
+{
+    add((UCalendarDateFields)field, amount, status);
 }
 
 /**
