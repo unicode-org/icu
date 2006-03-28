9c4ad84c24c249dfb588f7f1d566b6fec4a11489
George Rhoten <grhoten@svn.icu-project.org>
2006-03-28 07:25:09 +0000

    Fix some compiler warnings
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/4707
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@19461 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/test/intltest/rndmcoll.h
+++ source/test/intltest/rndmcoll.h
@@ -1,6 +1,6 @@
 /********************************************************************
  * COPYRIGHT: 
- * Copyright (c) 2005-2005, International Business Machines Corporation and
+ * Copyright (c) 2005-2006, International Business Machines Corporation and
  * others. All Rights Reserved.
  ********************************************************************/
 
@@ -20,7 +20,6 @@
 class RandomCollatorTest: public IntlTestCollator {
 public:
     virtual void runIndexedTest( int32_t index, UBool exec, const char* &name, char* par = NULL );
-    virtual ~RandomCollatorTest(){}
     void Test();
     void Test2();
 };
