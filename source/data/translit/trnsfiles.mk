1cfee8191ddba7aba8254672e7712f3805498081
Ram Viswanadha <rviswanadha@gmail.com>
2006-07-21 01:08:32 +0000

    update translit data
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/5247
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@19867 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/data/translit/trnsfiles.mk
+++ source/data/translit/trnsfiles.mk
@@ -1,4 +1,4 @@
-# *   Copyright (C) 1997-2005, International Business Machines
+# *   Copyright (C) 1997-2006, International Business Machines
 # *   Corporation and others.  All Rights Reserved.
 # A list of txt's to build
 # Note: 
@@ -23,6 +23,4 @@
 #
 #
 
-TRANSLIT_ALIAS_SOURCE=root.txt
-
-TRANSLIT_SOURCE=el.txt en.txt
+TRANSLIT_SOURCE=root.txt
