6fd80adca8f23d46f965e7eb439ae289b58fdf60
Eric Mader <emader@svn.icu-project.org>
2006-07-28 01:48:34 +0000

    ICU-5282 SimpleFontInstance::getGlyphAdvance: don't check for combining characters; depends on normalization.
    X-SVN-Rev: 19909

--- icu4c/source/test/letest/SimpleFontInstance.cpp
+++ icu4c/source/test/letest/SimpleFontInstance.cpp
@@ -53,11 +53,15 @@ const void *SimpleFontInstance::getFontTable(LETag tableTag) const
 
 void SimpleFontInstance::getGlyphAdvance(LEGlyphID glyph, LEPoint &advance) const
 {
+#if 0
     if (u_getCombiningClass((UChar32) glyph) == 0) {
         advance.fX = xUnitsToPoints(2048);
     } else {
         advance.fX = 0;
     }
+#else
+    advance.fX = xUnitsToPoints(2048);
+#endif
 
     advance.fY = 0;
 }
