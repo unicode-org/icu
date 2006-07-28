f4215511060276ab7b530b7cef0a49d5522c081d
George Rhoten <grhoten@svn.icu-project.org>
2006-07-28 19:35:12 +0000

    Increase code coverage
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/5032
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@19917 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/test/intltest/numfmtst.cpp
+++ source/test/intltest/numfmtst.cpp
@@ -82,6 +82,7 @@ void NumberFormatTest::runIndexedTest( int32_t index, UBool exec, const char* &n
         CASE(31,TestCoverage);
         CASE(32,TestJB3832);
         CASE(33,TestHost);
+        CASE(34,TestCurrencyFormat);
         default: name = ""; break;
     }
 }
@@ -2193,7 +2194,7 @@ void NumberFormatTest::TestJB3832(){
                 + UnicodeString( " for locale: ")+ UnicodeString(localeID) );
     }
     if (U_FAILURE(status)){
-        errln((UnicodeString)"FAIL: Status " + (int32_t)status);
+        errln("FAIL: Status %s", u_errorName(status));
     }
     delete currencyFmt;
 }
@@ -2203,6 +2204,57 @@ void NumberFormatTest::TestHost()
 #ifdef U_WINDOWS
     Win32NumberTest::testLocales(this);
 #endif
+}
+
+void NumberFormatTest::TestCurrencyFormat()
+{
+    // This test is here to increase code coverage.
+    UErrorCode status = U_ZERO_ERROR;
+    MeasureFormat *cloneObj;
+    UnicodeString str;
+    Formattable toFormat, result;
+    static const UChar ISO_CODE[4] = {0x0047, 0x0042, 0x0050, 0};
+
+    Locale  saveDefaultLocale = Locale::getDefault();
+    Locale::setDefault( Locale::getUK(), status );
+    if (U_FAILURE(status)) {
+        errln("couldn't set default Locale!");
+        return;
+    }
+
+    MeasureFormat *measureObj = MeasureFormat::createCurrencyFormat(status);
+    Locale::setDefault( saveDefaultLocale, status );
+    if (U_FAILURE(status)){
+        errln("FAIL: Status %s", u_errorName(status));
+        return;
+    }
+    cloneObj = (MeasureFormat *)measureObj->clone();
+    if (cloneObj == NULL) {
+        errln("Clone doesn't work");
+        return;
+    }
+    toFormat.adoptObject(new CurrencyAmount(1234.56, ISO_CODE, status));
+    measureObj->format(toFormat, str, status);
+    measureObj->parseObject(str, result, status);
+    if (U_FAILURE(status)){
+        errln("FAIL: Status %s", u_errorName(status));
+    }
+    if (result != toFormat) {
+        errln("measureObj does not round trip. Formatted string was \"" + str + "\" Got: " + toString(result) + " Expected: " + toString(toFormat));
+    }
+    status = U_ZERO_ERROR;
+    str.truncate(0);
+    cloneObj->format(toFormat, str, status);
+    cloneObj->parseObject(str, result, status);
+    if (U_FAILURE(status)){
+        errln("FAIL: Status %s", u_errorName(status));
+    }
+    if (result != toFormat) {
+        errln("Clone does not round trip. Formatted string was \"" + str + "\" Got: " + toString(result) + " Expected: " + toString(toFormat));
+    }
+    delete measureObj;
+    delete cloneObj;
+
 }
 
 #endif /* #if !UCONFIG_NO_FORMATTING */
