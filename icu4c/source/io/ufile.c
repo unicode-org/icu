cc3ed43b5206436c1a64e59b3d16de624dbf0666
George Rhoten <grhoten@users.noreply.github.com>
2006-08-15 01:08:28 +0000

    [ICU-5254] Special fix for Solaris 10 hybrid (upgraded) platforms X-Commit-URL: https://ssl.icu-project.org/trac/changeset/20061 Former-commit-id: 7bd2740f54e88920d0a1ceb2a5b3af5764096fbc

--- icu4c/source/io/ufile.c
+++ icu4c/source/io/ufile.c
@@ -20,7 +20,12 @@
 
 /* define for fileno.  */
 #ifndef _XOPEN_SOURCE
+#if __STDC_VERSION__ >= 199901L
+/* It is invalid to compile an XPG3, XPG4, XPG4v2 or XPG5 application using c99 */
+#define _XOPEN_SOURCE 600
+#else
 #define _XOPEN_SOURCE 4
+#endif
 #endif
 
 #include "locmap.h"
