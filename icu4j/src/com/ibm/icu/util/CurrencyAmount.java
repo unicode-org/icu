9cf34e0a84373254f4287ef69b8fce78db5b3e04
Doug Felt <dougfelt@gmail.com>
2006-08-25 23:13:15 +0000

    ICU-5051 fix mangled copyrights
    X-SVN-Rev: 20169

--- icu4j/src/com/ibm/icu/util/CurrencyAmount.java
+++ icu4j/src/com/ibm/icu/util/CurrencyAmount.java
@@ -1,6 +1,6 @@
 /*
 **********************************************************************
-* Copyright (c) 2006, International Business Machines
+* Copyright (c) 2004-2006, International Business Machines
 * Corporation and others.  All Rights Reserved.
 **********************************************************************
 * Author: Alan Liu
