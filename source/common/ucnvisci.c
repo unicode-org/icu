e21b2203890a1b9afa2bf3e540518ccb7b31784b
George Rhoten <grhoten@svn.icu-project.org>
2006-08-25 17:41:23 +0000

    Fix some compiler warnings.
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/4707
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@20164 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/ucnvisci.c
+++ source/common/ucnvisci.c
@@ -1109,7 +1109,7 @@ UConverter_toUnicode_ISCII_OFFSETS_LOGIC(UConverterToUnicodeArgs *args,
                     data->currentDeltaToUnicode = 
                         (uint16_t)(lookupTable[sourceChar & 0x0F][0] * DELTA);
                     data->currentMaskToUnicode = 
-                        lookupTable[sourceChar & 0x0F][1] ;
+                        (MaskEnum)lookupTable[sourceChar & 0x0F][1] ;
                 }
                 else if(sourceChar==DEF){
                     /* switch back to default */
