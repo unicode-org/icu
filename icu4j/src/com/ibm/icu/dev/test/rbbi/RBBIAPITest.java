28faf2c460288b87a83f18f300290840f46e099d
Andy Heninger <andy.heninger@gmail.com>
2006-09-16 00:22:54 +0000

    ICU-5374 fix incorrect new-line chars in java files.
    X-SVN-Rev: 20338

--- icu4j/src/com/ibm/icu/dev/test/rbbi/RBBIAPITest.java
+++ icu4j/src/com/ibm/icu/dev/test/rbbi/RBBIAPITest.java
@@ -1,6 +1,6 @@
 /*
  *******************************************************************************
- * Copyright (C) 2001-2005, International Business Machines Corporation and    *
+ * Copyright (C) 2001-2006, International Business Machines Corporation and    *
  * others. All Rights Reserved.                                                *
  *******************************************************************************
  */
