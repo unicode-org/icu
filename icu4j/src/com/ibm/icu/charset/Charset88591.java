8331b2abed0cdd3ceebd1a5ebc99856c8cada6b2
George Rhoten <grhoten@users.noreply.github.com>
2006-09-18 22:38:12 +0000

    [ICU-5398] Fix typo from last checkin. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/20351 Former-commit-id: 5e60e405d2a7c3caf02abdb97291724ad8f74f80

--- icu4j/src/com/ibm/icu/charset/Charset88591.java
+++ icu4j/src/com/ibm/icu/charset/Charset88591.java
@@ -22,7 +22,7 @@ import com.ibm.icu.text.UTF16;
 class Charset88591 extends CharsetICU {
     protected byte[] fromUSubstitution = new byte[]{(byte)0x1a};
     
-    public Charset88591(String icuCanonicalName, String javaCanonicalName, String[] aliases, boolean flush){
+    public Charset88591(String icuCanonicalName, String javaCanonicalName, String[] aliases){
         super(icuCanonicalName, javaCanonicalName, aliases);
         maxBytesPerChar = 1;
         minBytesPerChar = 1;
