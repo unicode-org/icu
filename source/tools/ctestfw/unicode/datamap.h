a45a371f75957cc0baba1742791091b8e2e0abfe
George Rhoten <grhoten@svn.icu-project.org>
2006-09-22 18:09:32 +0000

    These functions can never be inlined.  So remove them from the header.
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/5410
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@20385 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/tools/ctestfw/unicode/datamap.h
+++ source/tools/ctestfw/unicode/datamap.h
@@ -1,6 +1,6 @@
 /********************************************************************
  * COPYRIGHT: 
- * Copyright (c) 2002-2005, International Business Machines Corporation and
+ * Copyright (c) 2002-2006, International Business Machines Corporation and
  * others. All Rights Reserved.
  ********************************************************************/
 
@@ -26,10 +26,10 @@ U_NAMESPACE_END
  */
 class T_CTEST_EXPORT_API DataMap {
 public:
-  virtual ~DataMap() {};
+  virtual ~DataMap();
 
 protected:
-  DataMap() {};
+  DataMap();
   int32_t utoi(const UnicodeString &s) const;
 
 
