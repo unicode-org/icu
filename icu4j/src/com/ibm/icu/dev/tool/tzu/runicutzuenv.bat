280f06ea3fccdb77568f35863c3d88ca1c5252c5
Ram Viswanadha <rviswanadha@gmail.com>
2007-02-22 17:40:39 +0000

    [ICU-5605] zoneinfo.res is already available in ICU4J .. no need to check it in X-Commit-URL: https://ssl.icu-project.org/trac/changeset/21082 Former-commit-id: b568245a525216b38a4e76991676875223c47812

--- icu4j/src/com/ibm/icu/dev/tool/tzu/runicutzuenv.bat
+++ icu4j/src/com/ibm/icu/dev/tool/tzu/runicutzuenv.bat
@@ -1,3 +1,4 @@
+@echo off
 rem /*
 rem *******************************************************************************
 rem * Copyright (C) 2007, International Business Machines Corporation and         *
@@ -5,14 +6,21 @@ rem * others. All Rights Reserved.                                              
 rem *******************************************************************************
 rem */
 
+
 rem Set JAVA_HOME to the Java installation directory.
-set JAVA_HOME=C:\Program Files\Java\jre1.6.0
+IF  EXIST "%JAVA_HOME%" GOTO Next
+set JAVA_HOME=
+:Next
+@echo JAVA_HOME=%JAVA_HOME%
 
 rem Set whether the GUI will be run or not.
 set NOGUI=true
+@echo NOGUI=%NOGUI%
 
 rem Set whether the cmd-line utility will only discover update icu4j files.
 set DISCOVERONLY=false
+@echo DISCOVERONLY=%DISCOVERONLY%
 
 rem Set whether the cmd-line utility will run silently.
 set SILENTPATCH=false
+@echo SILENTPATCH=%SILENTPATCH%
