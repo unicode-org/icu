7e1a60f6b00197b6d30a5b9fea59c48ae4b8686e
George Rhoten <grhoten@svn.icu-project.org>
2007-03-05 18:26:53 +0000

    Fix some test failures due to CLDR update.
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/5566
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@21157 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/test/cintltst/creststn.c
+++ source/test/cintltst/creststn.c
@@ -1362,7 +1362,7 @@ static void TestErrorConditions(){
 
 static void TestGetVersion(){
     UVersionInfo minVersionArray = {0x01, 0x00, 0x00, 0x00};
-    UVersionInfo maxVersionArray = {0x50, 0x80, 0xcf, 0xcf};
+    UVersionInfo maxVersionArray = {0x50, 0xc0, 0xcf, 0xcf};
     UVersionInfo versionArray;
     UErrorCode status= U_ZERO_ERROR;
     UResourceBundle* resB = NULL; 
@@ -2055,7 +2055,7 @@ static void TestFallback()
         UResourceBundle* resLocID = ures_getByKey(myResB, "Version", NULL, &err);
         UResourceBundle* tResB;
         const UChar* version = NULL;
-        static const UChar versionStr[] = { 0x0031, 0x002E, 0x0033, 0x0037, 0x0000};
+        static const UChar versionStr[] = { 0x0031, 0x002E, 0x0034, 0x0032, 0x0000};
 
         if(err != U_ZERO_ERROR){
             log_data_err("Expected U_ZERO_ERROR when trying to test no_NO_NY aliased to nn_NO for Version err=%s\n",u_errorName(err));
