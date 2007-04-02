a51282830041bcb7c31c5f949c4b2b1f8aad8ae0
Deborah Goldsmith <goldsmit@apple.com>
2007-04-02 20:57:34 +0000

    Stop wrapping in month calculations
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/5666
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@21342 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/i18n/calendar.cpp
+++ source/i18n/calendar.cpp
@@ -2490,12 +2490,12 @@ int32_t Calendar::handleComputeJulianDay(UCalendarDateFields bestField)  {
     // If useMonth is true, get the day before the start of the month.
 
     // give calendar subclass a chance to have a default 'first' month
-    int8_t month;
+    int32_t month;
 
     if(isSet(UCAL_MONTH)) {
-        month = (int8_t)internalGet(UCAL_MONTH);
+        month = internalGet(UCAL_MONTH);
     } else {
-        month = (int8_t)getDefaultMonthInYear();
+        month = getDefaultMonthInYear();
     }
 
     int32_t julianDay = handleComputeMonthStart(year, useMonth ? month : 0, useMonth);
