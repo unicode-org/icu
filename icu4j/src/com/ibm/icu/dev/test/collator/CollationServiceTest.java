3ade317fbe0801efc196fb5bb3c10a245ee67856
George Rhoten <grhoten@users.noreply.github.com>
2007-04-13 06:17:56 +0000

    [ICU-5445] Fix some Eclipse warnings. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/21409 Former-commit-id: 363ca277933764a8192209ae31edfee762bc18ad

--- icu4j/src/com/ibm/icu/dev/test/collator/CollationServiceTest.java
+++ icu4j/src/com/ibm/icu/dev/test/collator/CollationServiceTest.java
@@ -1,6 +1,6 @@
 /*
  *******************************************************************************
- * Copyright (C) 2003-2005, International Business Machines Corporation and    *
+ * Copyright (C) 2003-2007, International Business Machines Corporation and    *
  * others. All Rights Reserved.                                                *
  *******************************************************************************
  */
@@ -39,6 +39,9 @@ public class CollationServiceTest extends TestFmwk {
 
             // coverage
             Collator test = Collator.getInstance(ULocale.GERMANY); // CollatorFactory.handleCreate
+            if (!test.getLocale(ULocale.VALID_LOCALE).equals(ULocale.GERMANY)) {
+                errln("Collation from Germany is really " + test.getLocale(ULocale.VALID_LOCALE));
+            }
 
             if (!Collator.unregister(key)) {
                 errln("failed to unregister french collator");
@@ -101,6 +104,9 @@ public class CollationServiceTest extends TestFmwk {
             ULocale[] locales = Collator.getAvailableULocales();
     
             Collator ncol = Collator.getInstance(ULocale.US);
+            if (!ncol.getLocale(ULocale.VALID_LOCALE).equals(ULocale.US)) {
+                errln("Collation from US is really " + ncol.getLocale(ULocale.VALID_LOCALE));
+            }
         }
     }
 
@@ -228,7 +234,11 @@ public class CollationServiceTest extends TestFmwk {
             logln("*** default name: " + name);
             Collator.unregister(key);
     
-            Collator col = Collator.getInstance(new ULocale("bar_BAR"));
+            ULocale bar_BAR = new ULocale("bar_BAR");
+            Collator col = Collator.getInstance(bar_BAR);
+            if (!col.getLocale(ULocale.VALID_LOCALE).equals(ULocale.getDefault())) {
+                errln("Collation from bar_BAR is really " + col.getLocale(ULocale.VALID_LOCALE));
+            }
         }
 
         int n1 = checkAvailable("before registerFactory");
