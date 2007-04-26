7a208a07e25372da8fdeec0b701534bc8d96363b
Andrew J Macheret <ajmacher@svn.icu-project.org>
2007-04-26 19:09:51 +0000

    [ICU-5605] added stuff to the readme, removed an old import from PathModel X-Commit-URL: https://ssl.icu-project.org/trac/changeset/21450 Former-commit-id: 5e75f65610f1a22e3aacf0285a8dd65fa606532b

--- icu4j/src/com/ibm/icu/dev/tool/tzu/PathModel.java
+++ icu4j/src/com/ibm/icu/dev/tool/tzu/PathModel.java
@@ -10,7 +10,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
