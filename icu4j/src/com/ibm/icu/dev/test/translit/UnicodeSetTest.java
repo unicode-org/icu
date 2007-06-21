09c27e4b0be0c87ad79435a5a185bca8d4fb57f6
Yoshito Umaoka <y.umaoka@gmail.com>
2007-06-21 19:47:36 +0000

    ICU-5575 Updates timebombs in ICU4J test cases for 3.7.2
    X-SVN-Rev: 21801

--- icu4j/src/com/ibm/icu/dev/test/translit/UnicodeSetTest.java
+++ icu4j/src/com/ibm/icu/dev/test/translit/UnicodeSetTest.java
@@ -60,10 +60,10 @@ public class UnicodeSetTest extends TestFmwk {
             // HACK for now
             
             if (propNum == 4098 && e1 instanceof NullPointerException) {
-              if (skipIfBeforeICU(3,7,1)) continue;
+              if (skipIfBeforeICU(3,7,2)) continue;
             }
             if (propNum == 4112 || propNum == 4113) {
-              if (skipIfBeforeICU(3,7,1)) continue;
+              if (skipIfBeforeICU(3,7,2)) continue;
             }
             
             errln("Can't get property value name for: "
@@ -92,7 +92,7 @@ public class UnicodeSetTest extends TestFmwk {
             }
           }
           if (collectedErrors.size() != 0) {
-            if (propNum == 4107 && valueNum == 0 && skipIfBeforeICU(3,7,1)) {
+            if (propNum == 4107 && valueNum == 0 && skipIfBeforeICU(3,7,2)) {
               continue; 
             }
             errln("Property Value Differs: " 
