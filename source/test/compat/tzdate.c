1112666d2fc9250a7c35e290ad91f7eab8aa1892
George Rhoten <grhoten@svn.icu-project.org>
2007-06-28 07:30:49 +0000

    Improve reliability of timezone tests.
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/5423
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@21836 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/test/compat/tzdate.c
+++ source/test/compat/tzdate.c
@@ -1,6 +1,6 @@
 /*
 **********************************************************************
-*   Copyright (C) 1998-2007, International Business Machines
+*   Copyright (C) 2007-2007, International Business Machines
 *   Corporation and others.  All Rights Reserved.
 **********************************************************************
 *
@@ -8,7 +8,7 @@
 *
 * Author:  Michael Ow
 *
-*******************************************************************************
+**********************************************************************
 */
 
 #include <stdlib.h>
@@ -27,112 +27,111 @@
 #define SIZE 80
 #define OFFSET_MONTH 1
 
-double getSystemCurrentTime(char* systime, int year, int month, int day, int useCurrentTime);
-void getICUCurrentTime(char* icutime, int year, int month, int day, int useCurrentTime, double systemtime);
+int64_t getSystemCurrentTime(char* systime, int year, int month, int day, int hour, int minute, int useCurrentTime);
+void getICUCurrentTime(char* icutime, double timeToCheck);
 void printTime(char* systime, char* icutime);
 
-int main (int argc, char** argv) {
-    char* systime;
-    char* icutime;
-    int year, month, day;
+int main(int argc, char** argv) {
+    char systime[SIZE];
+    char icutime[SIZE];
+    int year, month, day, hour, minute;
     int sysyear;
     int useCurrentTime;
-    double systemtime;
+    int64_t systemtime;
     
     sysyear = year = month = day = 0;
     
+    if (argc <= 6) {
+        fprintf(stderr, "Not enough arguments\n");
+        return -1;
+    }
+
     year = atoi(argv[1]);
     month = atoi(argv[2]);
     day = atoi(argv[3]);
-    useCurrentTime = atoi(argv[4]);
+    hour = atoi(argv[4]);
+    minute = atoi(argv[5]);
+    useCurrentTime = atoi(argv[6]);
     
     //format year for system time
     sysyear = year - 1900;
     
-    systime = malloc(sizeof(char) * SIZE);
-    icutime = malloc(sizeof(char) * SIZE);
+    systemtime = getSystemCurrentTime(systime, sysyear, month, day, hour, minute, useCurrentTime);
+    getICUCurrentTime(icutime, systemtime * U_MILLIS_PER_SECOND);
 
-	systemtime = getSystemCurrentTime(systime, sysyear, month, day, useCurrentTime);
-	getICUCurrentTime(icutime, year, month, day, useCurrentTime, systemtime * U_MILLIS_PER_SECOND);
-	
-	//print out the times if failed
-	if (strcmp(systime, icutime) != 0) {
-		printf("Failed\n");
-		printTime(systime, icutime);
-	}
-	
-	return 0;
+    //print out the times if failed
+    if (strcmp(systime, icutime) != 0) {
+        printf("Failed\n");
+        printTime(systime, icutime);
+    }
+
+    return 0;
 }
 
-void getICUCurrentTime(char* icutime, int year, int month, int day, int useCurrentTime, double systemtime) {
-	UDateFormat *fmt;
-	const UChar *tz = 0;
-	UChar *s = 0;
-  	UDateFormatStyle style = UDAT_RELATIVE;
-  	UErrorCode status = U_ZERO_ERROR;
-  	int32_t len = 0;
-  	
-  	UCalendar *c;
-  	c = NULL;
-  	
-  	fmt = udat_open(style, style, 0, tz, -1,NULL,0, &status);
-  	
-  	if (!useCurrentTime) {
-	  	c = ucal_open(0, -1, uloc_getDefault(), UCAL_TRADITIONAL, &status);
-	  	ucal_setDate(c, year, month - OFFSET_MONTH, day, &status);
-  	}
-	  	
-  	len = udat_format(fmt, (UDate)systemtime, 0, len, 0, &status);
-  	
-  	if (status == U_BUFFER_OVERFLOW_ERROR)
-  		status = U_ZERO_ERROR;
-  	
-    s = (UChar*) malloc(sizeof(UChar) * (len+1));
-    	
-    if(s == 0) 
-    	goto finish;
-    	
-    udat_format(fmt, (UDate)systemtime, s, len + 1, 0, &status);
-    
-    if(U_FAILURE(status)) 
-    	goto finish;
+void getICUCurrentTime(char* icutime, double timeToCheck) {
+    UDateFormat *fmt;
+    const UChar *tz = 0;
+    UChar *s = 0;
+    UDateFormatStyle style = UDAT_RELATIVE;
+    UErrorCode status = U_ZERO_ERROR;
+    int32_t len = 0;
 
-  	int i;
-  	for(i = 0; i < len; i++) {
-  		icutime[i] = (char)s[i];
-  	}
+    fmt = udat_open(style, style, 0, tz, -1,NULL,0, &status);
+
+    len = udat_format(fmt, timeToCheck, 0, len, 0, &status);
+
+    if (status == U_BUFFER_OVERFLOW_ERROR)
+        status = U_ZERO_ERROR;
+
+    s = (UChar*) malloc(sizeof(UChar) * (len+1));
+
+    if(s == 0) 
+        goto finish;
+
+    udat_format(fmt, timeToCheck, s, len + 1, 0, &status);
+    
+    if (U_FAILURE(status)) 
+        goto finish;
+
+    int i;
+    /* +1 to NULL terminate */
+    for(i = 0; i < len+1; i++) {
+        icutime[i] = (char)s[i];
+    }
 
 finish:
-	if (c != NULL) { 
-		ucal_close(c);
-	}
-  	udat_close(fmt);
-  	free(s);
+    udat_close(fmt);
+    free(s);
 }
 
-double getSystemCurrentTime(char* systime, int year, int month, int day, int useCurrentTime) {
-	time_t now;
-	struct tm ts;
-	
-	time(&now);
-	ts = *localtime(&now);
+int64_t getSystemCurrentTime(char* systime, int year, int month, int day, int hour, int minute, int useCurrentTime) {
+    time_t now;
+    struct tm ts;
 
-	if (!useCurrentTime){
-		ts.tm_year = year;
-		ts.tm_mon = month - OFFSET_MONTH;
-		ts.tm_mday = day;
-		
-		now = mktime(&ts);
-		
-		ts = *localtime(&now);	
-	}
-	
-	strftime(systime, sizeof(char) * 80, "%Y%m%d %I:%M %p", &ts);
-	
-	return (double)now;
+    if (useCurrentTime){
+        time(&now);
+        ts = *localtime(&now);
+    }
+    else {
+        memset(&ts, 0, sizeof(ts));
+        ts.tm_year = year;
+        ts.tm_mon = month - OFFSET_MONTH;
+        ts.tm_mday = day;
+        ts.tm_hour = hour;
+        ts.tm_min = minute;
+
+        now = mktime(&ts);
+        ts = *localtime(&now);
+    }
+
+    /* Format the string */
+    strftime(systime, sizeof(char) * 80, "%Y%m%d %I:%M %p", &ts);
+
+    return (double)now;
 }
 
 void printTime(char* systime, char* icutime) {
-	printf("System Time:  %s\n", systime);
-	printf("ICU Time:     %s\n", icutime);		
+    printf("System Time:  %s\n", systime);
+    printf("ICU Time:     %s\n", icutime);
 }
+
