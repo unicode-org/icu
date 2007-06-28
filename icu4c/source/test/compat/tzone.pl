30c0f9f3afb8fe8678631d306e3f9cd8939ec265
George Rhoten <grhoten@users.noreply.github.com>
2007-06-28 07:30:49 +0000

    ICU-5423 Improve reliability of timezone tests.
    X-SVN-Rev: 21836

--- icu4c/source/test/compat/tzone.pl
+++ icu4c/source/test/compat/tzone.pl
@@ -1,24 +1,30 @@
 #!/usr/bin/perl
+#
+#   Copyright (C) 2007-2007, International Business Machines
+#   Corporation and others.  All Rights Reserved.
+#
 
 if ($ARGV[0] eq '-h' || $ARGV[0] eq '--help') {
-	print "Usage: tzone [year month day]\n";
+	print "Usage: tzone [year month day hour minute]\n";
 	exit(0);
 }
 
 my $LIBRARY = '../../lib';
 
-my @TZONE_RAW = `locate zoneinfo`;
+my @TZONE_RAW = `locate zoneinfo | grep '^/usr/share/zoneinfo/' | grep -v 'tab\$' | grep -v '/right/' | grep -v '/posix/' | grep -v '/posixrules\$' | grep -v '/Factory\$'`;
 my @TZONE;
 my $index = 0;
 my $USECURRENT = 0;
 my $year = 0;
 my $month = 0;
 my $day = 0;
+my $hour = 0;
+my $minute = 0;
 
 
-if (scalar(@ARGV) == 3) {
-	($year, $month, $day) = @ARGV;
-	print "The date we are using is:  $month-$day-$year.\n";
+if (scalar(@ARGV) == 5) {
+	($year, $month, $day, $hour, $minute) = @ARGV;
+	print "The date we are using is:  $month-$day-$year $hour:$minute.\n";
 } else {
 	print "We are using the current date.\n";
 	$USECURRENT = 1;
@@ -27,12 +33,10 @@ if (scalar(@ARGV) == 3) {
 #filter out the time zones
 foreach my $tzone (@TZONE_RAW) {
 	chomp($tzone);
-	if ($tzone =~ m|^/usr/share/zoneinfo/| && $tzone !~ m/tab$/) {
-		if (-e $tzone) {
-			$TZONE[$index] = substr($tzone, 20);
-			$index++;
-		}
-	}
+    if (-e $tzone) {
+        $TZONE[$index] = substr($tzone, 20);
+        $index++;
+    }
 }
 
 #go through each timezone and test 
@@ -45,11 +49,11 @@ foreach my $tzone (@TZONE) {
 	#set system time zone
 	$ENV{'TZ'} = "$tzone";	
 	
-	my @result = `./tzdate $year $month $day $USECURRENT`;
+	my @result = `./tzdate $year $month $day $hour $minute $USECURRENT`;
 	
 	#if the result is wrong print the time zone information to a log file
 	if (scalar(@result) > 0) {
-		print "\n\nTIME ZONE: $tzone\n";
+		print "\nTIME ZONE: $tzone\n";
 		print "@result\n";
 		$count++;
 	}
