385a238f88ffc1a9f0fa59da1698cea396b6de0a
George Rhoten <grhoten@svn.icu-project.org>
2007-07-16 20:08:07 +0000

    Fix compiler error on Cygwin
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/5617
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@22001 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/test/intltest/regextst.h
+++ source/test/intltest/regextst.h
@@ -1,6 +1,6 @@
 /********************************************************************
  * COPYRIGHT: 
- * Copyright (c) 2002-2003, International Business Machines Corporation and
+ * Copyright (c) 2002-2007, International Business Machines Corporation and
  * others. All Rights Reserved.
  ********************************************************************/
 
@@ -32,12 +32,12 @@ public:
     virtual void PerlTests();
 
     // The following functions are internal to the regexp tests.
-    virtual UBool doRegexLMTest(const char *pat, const char *text, UBool looking, UBool match, int line);
+    virtual UBool doRegexLMTest(const char *pat, const char *text, UBool looking, UBool match, int32_t line);
     virtual void regex_find(const UnicodeString &pat, const UnicodeString &flags,
-        const UnicodeString &input, int line);
+        const UnicodeString &input, int32_t line);
     virtual void regex_err(const char *pat, int32_t errline, int32_t errcol,
-                            UErrorCode expectedStatus, int line);
-    virtual UChar *ReadAndConvertFile(const char *fileName, int &len, UErrorCode &status);
+                            UErrorCode expectedStatus, int32_t line);
+    virtual UChar *ReadAndConvertFile(const char *fileName, int32_t &len, UErrorCode &status);
     virtual const char *getPath(char buffer[2048], const char *filename);
 
 };
