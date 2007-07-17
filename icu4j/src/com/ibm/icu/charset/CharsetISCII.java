497047cd4c4b6ed627570a33c34c1336e4ddbfd1
Michael Ow <mow@svn.icu-project.org>
2007-07-17 19:45:28 +0000

    [ICU-1234] Testing copyright checking. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/22015 Former-commit-id: fe4ece797270b755c377ad0e9200138a8c553cb2

--- icu4j/src/com/ibm/icu/charset/CharsetISCII.java
+++ icu4j/src/com/ibm/icu/charset/CharsetISCII.java
@@ -1,6 +1,6 @@
 /*
  *******************************************************************************
- * Copyright (C) 2005, International Business Machines Corporation and         *
+ * Copyright (C) 2007, International Business Machines Corporation and         *
  * others. All Rights Reserved.                                                *
  *******************************************************************************
  */
