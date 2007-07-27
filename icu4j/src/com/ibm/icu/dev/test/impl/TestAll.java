00988427ae718686fceb5b2faf239cdb1e041bb4
Yoshito Umaoka <y.umaoka@gmail.com>
2007-07-27 22:32:49 +0000

    [ICU-5801] Remove LRUMapTest, which no longer exists X-Commit-URL: https://ssl.icu-project.org/trac/changeset/22187 Former-commit-id: d648879374197605586556ef912f0dd90ebae395

--- icu4j/src/com/ibm/icu/dev/test/impl/TestAll.java
+++ icu4j/src/com/ibm/icu/dev/test/impl/TestAll.java
@@ -1,6 +1,6 @@
 /*
  *******************************************************************************
- * Copyright (C) 1996-2006, International Business Machines Corporation and    *
+ * Copyright (C) 1996-2007, International Business Machines Corporation and    *
  * others. All Rights Reserved.                                                *
  *******************************************************************************
  */
@@ -22,7 +22,6 @@ public class TestAll extends TestGroup {
                   "ICUServiceTest",
                   "ICUServiceThreadTest",
                   "ICUBinaryTest",
-                  "LRUMapTest",
                   "TextTrieMapTest"
               },
               "Test miscellaneous implementation utilities");
