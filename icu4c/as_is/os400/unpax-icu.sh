9abe99787b01b92de55e38970701a24a43aa49b0
Jason Spieth <jspieth@svn.icu-project.org>
2007-08-23 03:37:11 +0000

    ICU-5821 Port of runConfigureICU script and overflow fix in unpax-icu.sh script for i5OS
    X-SVN-Rev: 22479

--- icu4c/as_is/os400/unpax-icu.sh
+++ icu4c/as_is/os400/unpax-icu.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/qsh
-#   Copyright (C) 2000-2006, International Business Machines
+#   Copyright (C) 2000-2007, International Business Machines
 #   Corporation and others.  All Rights Reserved.
 #
 # Authors:
@@ -7,6 +7,7 @@
 # Barry Novinger
 # Steven R. Loomis
 # George Rhoten
+# Jason Spieth
 #
 # Shell script to unpax ICU and convert the files to an EBCDIC codepage.
 # After extracting to EBCDIC, binary files are re-extracted without the
@@ -14,41 +15,63 @@
 #
 # Set the following variable to the list of binary file suffixes (extensions)
 
+#****************************************************************************
 #binary_suffixes='ico ICO bmp BMP jpg JPG gif GIF brk BRK'
 #ICU specific binary files
+#****************************************************************************
 binary_suffixes='brk BRK bin BIN res RES cnv CNV dat DAT icu ICU spp SPP xml XML'
 data_files='icu/source/data/brkitr/* icu/source/data/locales/* icu/source/data/coll/* icu/source/data/rbnf/* icu/source/data/mappings/* icu/source/data/misc/* icu/source/data/translit/* icu/source/data/unidata/* icu/source/test/testdata/*'
 
+#****************************************************************************
+# Function:     usage
+# Description:  Prints out text that describes how to call this script
+# Input:        None
+# Output:       None
+#****************************************************************************
 usage()
 {
   echo "Enter archive filename as a parameter: $0 icu-archive.tar"
 }
+
+#****************************************************************************
 # first make sure we at least one arg and it's a file we can read
+#****************************************************************************
+
+# check for no arguments
 if [ $# -eq 0 ]; then
   usage
   exit
 fi
+
+# tar file is argument 1
 tar_file=$1
+
+# check that the file is valid
 if [ ! -r $tar_file ]; then
   echo "$tar_file does not exist or cannot be read."
   usage
   exit
 fi
-# set up a few variables
 
+#****************************************************************************
+# Determine which directories in the data_files list
+# are included in the provided archive
+#****************************************************************************
+for data_dir in $data_files
+do
+   if (pax -f $tar_file $data_dir >/dev/null 2>&1)
+   then
+       ebcdic_data="$ebcdic_data `echo $data_dir`";
+   fi
+done
+
+#****************************************************************************
+# Extract files.  We do this in two passes.  One pass for 819 files and a
+# second pass for 37 files
+#****************************************************************************
 echo ""
 echo "Extracting from $tar_file ..."
 echo ""
-
-# determine which directories in the data_files list
-# are included in the provided archive
-for data_dir in $data_files
-do
-    if (pax -f $tar_file $data_dir >/dev/null 2>&1)
-    then
-        ebcdic_data="$ebcdic_data `echo $data_dir`";
-    fi
-done
 
 # extract everything as iso-8859-1 except these directories
 pax -C 819 -rcvf $tar_file $ebcdic_data
@@ -59,10 +82,17 @@ echo "Extracting files which must be in ibm-37 ..."
 echo ""
 pax -C 37 -rvf $tar_file $ebcdic_data
 
+#****************************************************************************
+# For files we have restored as CCSID 37, check the BOM to see if they    
+# should be processed as 819.  Also handle files with special paths. Files
+# that match will be added to binary files lists.  The lists will in turn
+# be processed to restore files as 819.
+#****************************************************************************
 echo ""
 echo "Determining binary files ..."
 echo ""
 
+# Process BOMs
 for file in `find ./icu \( -name \*.txt -print \)`; do
     bom8=`head -n 1 $file|\
           od -t x1|\
@@ -73,15 +103,27 @@ for file in `find ./icu \( -name \*.txt -print \)`; do
     #Find a converted UTF-8 BOM
     if [ "$bom8" = "057 08b 0ab" -o "$bom8" = "57 8b ab" ]
     then
-        if [ `echo $binary_files1 | wc -w` -lt 250 ]
+        file="`echo $file | cut -d / -f2-`"
+
+        if [ `echo $binary_files1 | wc -w` -lt 200 ]
         then
-            binary_files1="$binary_files1 `echo $file | cut -d / -f2-`";
+            binary_files1="$binary_files1 $file";
+        elif [ `echo $binary_files2 | wc -w` -lt 200 ]
+        then
+            binary_files2="$binary_files2 $file";
+        elif [ `echo $binary_files3 | wc -w` -lt 200 ]
+        then
+            binary_files3="$binary_files3 $file";
+        elif [ `echo $binary_files4 | wc -w` -lt 200 ]
+        then
+            binary_files4="$binary_files4 $file";
         else
-            binary_files2="$binary_files2 `echo $file | cut -d / -f2-`";
+            binary_files5="$binary_files5 $file";
         fi
     fi
 done
 
+# Process special paths
 for i in $(pax -f $tar_file 2>/dev/null)
 do
   case $i in
@@ -95,7 +137,7 @@ do
        suf=${i#*.*}
        if [ "$suf" = "$j" ]
        then
-         binary_files2="$binary_files2 $i"
+         binary_files6="$binary_files6 $i"
          break
        fi
      done
@@ -107,17 +149,85 @@ do
 done
 
 # now see if a re-extract of binary files is necessary
-if [ ${#binary_files1} -eq 0 -a ${#binary_files2} -eq 0 ]; then
+if [ ${#binary_files1} -eq 0 ] &&
+   [ ${#binary_files2} -eq 0 ] &&
+   [ ${#binary_files3} -eq 0 ] &&
+   [ ${#binary_files4} -eq 0 ] &&
+   [ ${#binary_files5} -eq 0 ] &&
+   [ ${#binary_files6} -eq 0 ]
+
+then
   echo ""
   echo "There are no binary files to restore."
 else
   echo "Restoring binary files ..."
   echo ""
-  rm $binary_files1
-  rm $binary_files2
-  pax -C 819 -rvf $tar_file $binary_files1
-  pax -C 819 -rvf $tar_file $binary_files2
+
+  if [ `echo $binary_files1 | wc -w` -gt 0 ]
+  then
+    rm $binary_files1
+  fi
+
+  if [ `echo $binary_files2 | wc -w` -gt 0 ]
+  then
+    rm $binary_files2
+  fi
+
+  if [ `echo $binary_files3 | wc -w` -gt 0 ]
+  then
+    rm $binary_files3
+  fi
+
+  if [ `echo $binary_files4 | wc -w` -gt 0 ]
+  then
+    rm $binary_files4
+  fi
+
+  if [ `echo $binary_files5 | wc -w` -gt 0 ]
+  then
+    rm $binary_files5
+  fi
+
+  if [ `echo $binary_files6 | wc -w` -gt 0 ]
+  then
+    rm $binary_files6
+  fi
+
+  if [ `echo $binary_files1 | wc -w` -gt 0 ]
+  then
+    pax -C 819 -rvf $tar_file $binary_files1
+  fi
+
+  if [ `echo $binary_files2 | wc -w` -gt 0 ]
+  then
+    pax -C 819 -rvf $tar_file $binary_files2
+  fi
+
+  if [ `echo $binary_files3 | wc -w` -gt 0 ]
+  then
+    pax -C 819 -rvf $tar_file $binary_files3
+  fi
+
+  if [ `echo $binary_files4 | wc -w` -gt 0 ]
+  then
+    pax -C 819 -rvf $tar_file $binary_files4
+  fi
+
+  if [ `echo $binary_files5 | wc -w` -gt 0 ]
+  then
+    pax -C 819 -rvf $tar_file $binary_files5
+  fi
+
+  if [ `echo $binary_files6 | wc -w` -gt 0 ]
+  then
+    pax -C 819 -rvf $tar_file $binary_files6
+  fi
+
 fi
+
+#****************************************************************************
+# Generate and run the configure script
+#****************************************************************************
 
 echo ""
 echo "Generating qsh compatible configure ..."
@@ -130,3 +240,4 @@ chmod 755 icu/source/configure
 
 echo ""
 echo "$0 has completed extracting ICU from $tar_file."
+
