5a3faa18fc2789849340dc2b78934f8a9ae34850
Markus Scherer <markus.icu@gmail.com>
2007-10-22 19:21:22 +0000

    fix test, reenable check for existing file
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/5401
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@22831 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/test/cintltst/udatatst.c
+++ source/test/cintltst/udatatst.c
@@ -199,10 +199,10 @@ static void TestUDataOpen(){
     strcat(icuDataFilePath, dirSepString);
     strcat(icuDataFilePath, U_ICUDATA_NAME);
     strcat(icuDataFilePath, "_");
-    strcat(icuDataFilePath, "unorm.icu");
+    strcat(icuDataFilePath, "cnvalias.icu");
 
     /* lots_of_mallocs(); */
-/*    if (stat(icuDataFilePath, &stat_buf) == 0)*/
+    if (stat(icuDataFilePath, &stat_buf) == 0)
     {
         int i;
         log_verbose("%s exists, so..\n", icuDataFilePath);
@@ -222,11 +222,11 @@ static void TestUDataOpen(){
             }
         }
     }
-/*    else
+    else
     {
          log_verbose("Skipping tests of udata_open() on %s.  File not present in this configuration.\n",
              icuDataFilePath);
-    }*/
+    }
 
     free(icuDataFilePath);
     icuDataFilePath = NULL;
