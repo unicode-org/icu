2ec39c599997146ba827ae615e0e87c246417c5b
Eric Mader <emader@svn.icu-project.org>
2008-01-09 20:18:29 +0000

    ICU-6104 update stale copyright notice.
    X-SVN-Rev: 23193

--- icu4c/source/i18n/unicode/dtptngen.h
+++ icu4c/source/i18n/unicode/dtptngen.h
@@ -1,6 +1,6 @@
 /*
 *******************************************************************************
-* Copyright (C) 2007, International Business Machines Corporation and
+* Copyright (C) 2007-2008, International Business Machines Corporation and
 * others. All Rights Reserved.
 *******************************************************************************
 *
