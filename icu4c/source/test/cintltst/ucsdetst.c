1d990099d46882bf973164429e179432121be686
George Rhoten <grhoten@users.noreply.github.com>
2008-02-01 07:08:13 +0000

    [ICU-6165] Prevent a reading past the end of a buffer. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/23340 Former-commit-id: 27e4fd733f1ecc0558cdd646841a52433d5ab7ad

--- icu4c/source/test/cintltst/ucsdetst.c
+++ icu4c/source/test/cintltst/ucsdetst.c
@@ -1,6 +1,6 @@
 /*
  ****************************************************************************
- * Copyright (c) 2005-2006, International Business Machines Corporation and *
+ * Copyright (c) 2005-2008, International Business Machines Corporation and *
  * others. All Rights Reserved.                                             *
  ****************************************************************************
  */
@@ -16,7 +16,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define ARRAY_SIZE(array) (sizeof array / sizeof array[0])
+#define ARRAY_SIZE(array) (sizeof(array)/sizeof(array[0]))
 
 #define NEW_ARRAY(type,count) (type *) malloc((count) * sizeof(type))
 #define DELETE_ARRAY(array) free(array)
@@ -27,6 +27,7 @@ static void TestUTF16(void);
 static void TestC1Bytes(void);
 static void TestInputFilter(void);
 static void TestChaining(void);
+static void TestBufferOverflow(void);
 
 void addUCsdetTest(TestNode** root);
 
@@ -38,6 +39,7 @@ void addUCsdetTest(TestNode** root)
     addTest(root, &TestC1Bytes, "ucsdetst/TestC1Bytes");
     addTest(root, &TestInputFilter, "ucsdetst/TestInputFilter");
     addTest(root, &TestChaining, "ucsdetst/TestErrorChaining");
+    addTest(root, &TestBufferOverflow, "ucsdetst/TestBufferOverflow");
 }
 
 static int32_t preflight(const UChar *src, int32_t length, UConverter *cnv)
@@ -353,3 +355,46 @@ static void TestChaining(void) {
         log_err("Status got changed to %s\n", u_errorName(status));
     }
 }
+
+static void TestBufferOverflow(void) {
+    UErrorCode status = U_ZERO_ERROR;
+    static const char *testStrings[] = {
+        "\x80 This is English \x1b", /* A partial ISO-2022 shift state at the end */
+        "\x80 This is English \x1b\x24", /* A partial ISO-2022 shift state at the end */
+        "\x80 This is English \x1b\x24\x28", /* A partial ISO-2022 shift state at the end */
+        "\x80 This is English \x1b\x24\x28\x44", /* A complete ISO-2022 shift state at the end with a bad one at the start */
+        "\x1b\x24\x28\x44" /* A complete ISO-2022 shift state at the end */
+    };
+    static const char *testResults[] = {
+        "windows-1252",
+        "windows-1252",
+        "windows-1252",
+        "windows-1252",
+        "ISO-2022-JP"
+    };
+    int32_t idx = 0;
+    UCharsetDetector *csd = ucsdet_open(&status);
+    const UCharsetMatch *match;
+
+    ucsdet_setDeclaredEncoding(csd, "ISO-2022-JP", -1, &status);
+
+    if (U_FAILURE(status)) {
+        log_err("Couldn't open detector. %s\n", u_errorName(status));
+        goto bail;
+    }
+
+    for (idx = 0; idx < ARRAY_SIZE(testStrings); idx++) {
+        ucsdet_setText(csd, testStrings[idx], -1, &status);
+        match = ucsdet_detect(csd, &status);
+
+        if (strcmp(ucsdet_getName(match, &status), testResults[idx]) != 0) {
+            log_err("Unexpectedly got %s instead of %s at index %d with confidence %d.\n",
+                ucsdet_getName(match, &status), testResults[idx], idx, ucsdet_getConfidence(match, &status));
+            goto bail;
+        }
+    }
+
+bail:
+    ucsdet_close(csd);
+}
+
