f8d7980e50795b67de110ef872d3823f3f3383da
George Rhoten <grhoten@users.noreply.github.com>
2008-02-18 23:31:45 +0000

    [ICU-6098] Update urename.h based on API changes. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/23456 Former-commit-id: 1dc7e78c20bc1c76a2712cc137ea9cc1bffce948

--- icu4c/source/tools/genren/genren.pl
+++ icu4c/source/tools/genren/genren.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl 
 #*
 #*******************************************************************************
-#*   Copyright (C) 2001-2007, International Business Machines
+#*   Copyright (C) 2001-2008, International Business Machines
 #*   Corporation and others.  All Rights Reserved.
 #*******************************************************************************
 #*
@@ -60,7 +60,7 @@ $HEADERDEF =~ s/\./_/;
 print HEADER <<"EndOfHeaderComment";
 /*
 *******************************************************************************
-*   Copyright (C) 2002-2007, International Business Machines
+*   Copyright (C) 2002-2008, International Business Machines
 *   Corporation and others.  All Rights Reserved.
 *******************************************************************************
 *
