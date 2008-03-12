fbfe9e74c9e42fc1d7424a4d3edf71f5f64f03e0
Steven R. Loomis <srl@icu-project.org>
2008-03-12 21:48:14 +0000

    build macosx wit --enable-strict
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/6209
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@23570 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/umutex.c
+++ source/common/umutex.c
@@ -25,8 +25,16 @@
 #if defined(U_DARWIN)
 #include <AvailabilityMacros.h>
 #if (ICU_USE_THREADS == 1) && defined(MAC_OS_X_VERSION_10_4) && defined(MAC_OS_X_VERSION_MIN_REQUIRED) && (MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_4)
+#if defined(__STRICT_ANSI__)
+#define UPRV_REMAP_INLINE
+#define inline
+#endif
 #include <libkern/OSAtomic.h>
 #define USE_MAC_OS_ATOMIC_INCREMENT 1
+#if defined(UPRV_REMAP_INLINE)
+#undef inline
+#undef UPRV_REMAP_INLINE
+#endif
 #endif
 #endif
 
