91da4e7d65f3dd80cb55fb330c99a4ab130f0c0e
Yoshito Umaoka <y.umaoka@gmail.com>
2008-04-09 20:21:57 +0000

    [ICU-6098] Update ICU4J version to 3.9.3 X-Commit-URL: https://ssl.icu-project.org/trac/changeset/23777 Former-commit-id: 1b23ddeb2b992321a5895ff4b2bb87d9cb6e1bb6

--- icu4j/src/com/ibm/icu/dev/test/format/DateTimeGeneratorTest.java
+++ icu4j/src/com/ibm/icu/dev/test/format/DateTimeGeneratorTest.java
@@ -213,7 +213,7 @@ public class DateTimeGeneratorTest extends TestFmwk {
                 if (GENERATE_TEST_DATA) {
                     logln("new String[] {\"" + testSkeleton + "\", \"" + Utility.escape(formatted) + "\"},");
                 } else if (!formatted.equals(testFormatted)) {
-                    if(skipIfBeforeICU(3,9,2)&& uLocale.equals("zh_Hans_CN") && testSkeleton.equals("HHmm")){
+                    if(skipIfBeforeICU(3,9,3)&& uLocale.equals("zh_Hans_CN") && testSkeleton.equals("HHmm")){
                         logln(uLocale + "\tformatted string doesn't match test case: " + testSkeleton + "\t generated: " +  pattern + "\t expected: " + testFormatted + "\t got: " + formatted);
                         continue;
                     }
@@ -392,7 +392,7 @@ public class DateTimeGeneratorTest extends TestFmwk {
         DateOrder order2 = getOrdering(style2, uLocale);
         if (!order1.hasSameOrderAs(order2)) {
             if (order1.monthLength == order2.monthLength) { // error if have same month length, different ordering
-                if (skipIfBeforeICU(3,9,2)) {
+                if (skipIfBeforeICU(3,9,3)) {
                     logln(showOrderComparison(uLocale, style1, style2, order1, order2));
                 } else {
                     errln(showOrderComparison(uLocale, style1, style2, order1, order2));
