d1ba59584a5ba3c7ddd32aedeebcdcd20ae63e1a
Michael Ow <mow@svn.icu-project.org>
2008-04-17 17:46:37 +0000

    Add string search performance test template to repository.
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/4362
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@23799 0a8b766c-62c9-45f9-954d-7f2943c9ead0

---
+++ source/test/perf/strsrchperf/strsrchperf.cpp
@@ -0,0 +1,18 @@
+/********************************************************************
+ * COPYRIGHT:
+ * Copyright (C) 2008 IBM, Inc.   All Rights Reserved.
+ *
+ ********************************************************************/
+/** 
+ * This program tests string search performance.
+ * APIs tested: 
+ * ICU4C 
+ */
+
+#include "strsrchperf.h"
+
+
+int main (int argc, char** argv) {
+    printf("Done!\n");
+    return 0;
+}
