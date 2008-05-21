31c0ce5aa14671b417f6a25073216a763ef4138b
Steven R. Loomis <srl@icu-project.org>
2008-05-21 23:20:54 +0000

    udeprctd.h and others
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/6297
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@23961 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/unicode/udeprctd.h
+++ source/common/unicode/udeprctd.h
@@ -1,6 +1,6 @@
 /*
 *******************************************************************************
-*   Copyright (C) 2004-2007, International Business Machines
+*   Copyright (C) 2004-2008, International Business Machines
 *   Corporation and others.  All Rights Reserved.
 *******************************************************************************
 *
@@ -34,15 +34,15 @@
 #        define utrans_open utrans_open_DEPRECATED_API_DO_NOT_USE
 #        define utrans_unregister utrans_unregister_DEPRECATED_API_DO_NOT_USE
 #    else
-#        define ucol_getContractions_3_8 ucol_getContractions_DEPRECATED_API_DO_NOT_USE
-#        define ucol_getLocale_3_8 ucol_getLocale_DEPRECATED_API_DO_NOT_USE
-#        define ures_countArrayItems_3_8 ures_countArrayItems_DEPRECATED_API_DO_NOT_USE
-#        define ures_getLocale_3_8 ures_getLocale_DEPRECATED_API_DO_NOT_USE
-#        define ures_getVersionNumber_3_8 ures_getVersionNumber_DEPRECATED_API_DO_NOT_USE
-#        define utrans_getAvailableID_3_8 utrans_getAvailableID_DEPRECATED_API_DO_NOT_USE
-#        define utrans_getID_3_8 utrans_getID_DEPRECATED_API_DO_NOT_USE
-#        define utrans_open_3_8 utrans_open_DEPRECATED_API_DO_NOT_USE
-#        define utrans_unregister_3_8 utrans_unregister_DEPRECATED_API_DO_NOT_USE
+#        define ucol_getContractions_4_0 ucol_getContractions_DEPRECATED_API_DO_NOT_USE
+#        define ucol_getLocale_4_0 ucol_getLocale_DEPRECATED_API_DO_NOT_USE
+#        define ures_countArrayItems_4_0 ures_countArrayItems_DEPRECATED_API_DO_NOT_USE
+#        define ures_getLocale_4_0 ures_getLocale_DEPRECATED_API_DO_NOT_USE
+#        define ures_getVersionNumber_4_0 ures_getVersionNumber_DEPRECATED_API_DO_NOT_USE
+#        define utrans_getAvailableID_4_0 utrans_getAvailableID_DEPRECATED_API_DO_NOT_USE
+#        define utrans_getID_4_0 utrans_getID_DEPRECATED_API_DO_NOT_USE
+#        define utrans_open_4_0 utrans_open_DEPRECATED_API_DO_NOT_USE
+#        define utrans_unregister_4_0 utrans_unregister_DEPRECATED_API_DO_NOT_USE
 #    endif /* U_DISABLE_RENAMING */
 
 #endif /* U_HIDE_DEPRECATED_API */
