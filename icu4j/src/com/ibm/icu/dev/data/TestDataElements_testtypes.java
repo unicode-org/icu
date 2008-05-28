40bba654bc0d907a6eb7f975eb5c46f31449de78
Yoshito Umaoka <y.umaoka@gmail.com>
2008-05-28 20:58:52 +0000

    ICU-5450 Removed com.ibm.icu.impl.ICULocaleData which is no longer used.  Updated the resource bundle performance tset cases to call Java ResourceBundle directly.
    X-SVN-Rev: 24044

--- icu4j/src/com/ibm/icu/dev/data/TestDataElements_testtypes.java
+++ icu4j/src/com/ibm/icu/dev/data/TestDataElements_testtypes.java
@@ -15,12 +15,12 @@ public class TestDataElements_testtypes extends ListResourceBundle {
           return  contents;
     }
 
-	private static Object[][] contents = {
+    private static Object[][] contents = {
                 {
                     "binarytest",
                     new byte[] {
                         0,   1,   2,   3,   4,   5,   6,   7,   8,   9,   10,  11,  12,  13,  14,
-					},
+                    },
                 },
                 {
                     "emptyarray",
@@ -57,7 +57,7 @@ public class TestDataElements_testtypes extends ListResourceBundle {
                     "importtest",
                     new byte[] {
                         0,   1,   2,   3,   4,   5,   6,   7,   8,   9,   10,  11,  12,  13,  14,
-					},
+                    },
                 },
                 {
                     "integerarray",
