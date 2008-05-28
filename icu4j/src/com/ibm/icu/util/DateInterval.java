ae6d1cd11be3399617953d914c1cd252e255e226
Xiaomei Ji <xji@google.com>
2008-05-28 22:45:28 +0000

    ICU-6157 merge from branch
    X-SVN-Rev: 24056

---
+++ icu4j/src/com/ibm/icu/util/DateInterval.java
@@ -0,0 +1,73 @@
+/*
+ *******************************************************************************
+ *   Copyright (C) 2008, International Business Machines
+ *   Corporation and others.  All Rights Reserved.
+ *******************************************************************************
+ */
+
+package com.ibm.icu.util;
+
+import java.io.Serializable;
+
+
+/**
+ * This class represents date interval.
+ * It is a pair of long representing from long 1 to long 2.
+ * @draft ICU 4.0
+**/
+public final class DateInterval implements Serializable {
+
+    private static final long serialVersionUID = 1;
+
+    private final long fromDate;
+    private final long toDate;
+
+    /** 
+     * Constructor given from date and to date.
+     * @param from      The from date in date interval.
+     * @param to        The to date in date interval.
+     * @draft ICU 4.0
+     */
+    public DateInterval(long from, long to)
+    {
+        fromDate = from;
+        toDate = to;
+    }
+
+    /** 
+     * Get the from date.
+     * @return  the from date in dateInterval.
+     * @draft ICU 4.0
+     */
+    public long getFromDate()
+    {
+        return fromDate;
+    }
+
+    /** 
+     * Get the to date.
+     * @return  the to date in dateInterval.
+     * @draft ICU 4.0
+     */
+    public long getToDate()
+    {
+        return toDate;
+    }
+
+    public boolean equals(Object a) {
+        if ( a instanceof DateInterval ) {
+            DateInterval di = (DateInterval)a;
+            return fromDate == di.fromDate && toDate == di.toDate;
+        }
+        return false;
+    }
+
+    public int hashCode() {
+        return (int)(fromDate + toDate);
+    }
+
+   public String toString() {
+       return String.valueOf(fromDate) + " " + String.valueOf(toDate);
+   }
+
+} // end class DateInterval
