dfa39a736e39c566422b75ee9c897524451198f6
Eric Mader <emader@svn.icu-project.org>
2008-06-10 21:02:28 +0000

    ICU-6337 run iotest serially, also redirect stderr to log file.
    X-SVN-Rev: 24143

--- tools/release/c/allTimezoneTest.sh
+++ tools/release/c/allTimezoneTest.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+#
+# Copyright (c) 2006-2008 IBM All Rights Reserved
+#
 echo "Testing $1 in all timezones"
 outfile=$1-timezone.txt
 echo "" > $outfile
 for timezone in `locate /usr/share/zoneinfo/|fgrep -v /right/|fgrep -v /posix/`; do
 timezone=${timezone#/usr/share/zoneinfo/}
 echo TZ=$timezone >> $outfile
-TZ=$timezone make check >> $outfile
+TZ=$timezone make check >> $outfile 2>&1
 done
 
 echo "Done testing $1 in all timezones"
--- tools/release/c/environmentTest.sh
+++ tools/release/c/environmentTest.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# Copyright (c) 2006-2008 IBM All Rights Reserved
+#
 # This test script enumerates all locales and all timezones installed on a
 # machine (usually Linux), and runs the existing ICU4C tests to make sure that
 # the tests pass. Not everyone is using and testing ICU4C in the en_US locale
@@ -10,8 +12,8 @@ cd $top_icu_dir/source/test/intltest
 $release_tools_dir/allLocaleTest.sh intltest &
 $release_tools_dir/allTimezoneTest.sh intltest &
 cd ../iotest
-$release_tools_dir/allLocaleTest.sh iotest &
-$release_tools_dir/allTimezoneTest.sh iotest &
+# iotest uses a fixed filename for tests, so can't run in parallel
+$release_tools_dir/allLocaleTest.sh iotest; $release_tools_dir/allTimezoneTest.sh iotest &
 cd ../cintltst
 $release_tools_dir/allLocaleTest.sh cintltst &
 $release_tools_dir/allTimezoneTest.sh cintltst &
