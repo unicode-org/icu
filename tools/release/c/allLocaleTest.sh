fd22341aab3b6396e980427e171c557dc9187422
Eric Mader <emader@svn.icu-project.org>
2008-06-12 01:02:39 +0000

    ICU-6337 Change scripits to supply separate test file names to iotest for testing locales and timezones.
    X-SVN-Rev: 24159

--- tools/release/c/allLocaleTest.sh
+++ tools/release/c/allLocaleTest.sh
@@ -7,7 +7,7 @@ outfile=$1-locale.txt
 echo "" > $outfile
 for loc in `locale -a`; do
 echo LC_ALL=$loc >> $outfile
-LC_ALL=$loc make check >> $outfile 2>&1
+LC_ALL=$loc make check $2 >> $outfile 2>&1
 done
 
 echo "Done testing $1 in all locales"
