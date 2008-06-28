30584c107b3cb5759c6390ca6e517905320e7bd4
Steven R. Loomis <srl@icu-project.org>
2008-06-28 07:08:51 +0000

    [ICU-6404] last header changes X-Commit-URL: https://ssl.icu-project.org/trac/changeset/24318 Former-commit-id: ada67430c918ac898030de0ca7f16441f5d16f1a

--- icu4c/source/common/unicode/ustring.h
+++ icu4c/source/common/unicode/ustring.h
@@ -71,6 +71,11 @@
  */
 
 /**
+ * \defgroup ustring_ustrlen String Length
+ * \ingroup ustring_strlen
+ */
+/*@{*/
+/**
  * Determine the length of an array of UChar.
  *
  * @param s The array of UChars, NULL (U+0000) terminated.
@@ -79,6 +84,7 @@
  */
 U_STABLE int32_t U_EXPORT2
 u_strlen(const UChar *s);
+/*@}*/
 
 /**
  * Count Unicode code points in the length UChar code units of the string.
