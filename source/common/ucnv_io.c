e6d8d548ff19c9b7b96bf832d93f511abf87b4b9
Andy Heninger <andy.heninger@gmail.com>
2008-08-03 05:09:13 +0000

    Fix thread safety problem in converter initialization
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/6477
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@24436 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/ucnv_io.c
+++ source/common/ucnv_io.c
@@ -1,7 +1,7 @@
 /*
 ******************************************************************************
 *
-*   Copyright (C) 1999-2007, International Business Machines
+*   Copyright (C) 1999-2008, International Business Machines
 *   Corporation and others.  All Rights Reserved.
 *
 ******************************************************************************
@@ -259,9 +259,6 @@ haveAliasData(UErrorCode *pErrorCode) {
 
         umtx_lock(NULL);
         if(gAliasData==NULL) {
-            gAliasData = data;
-            data=NULL;
-
             gMainTable.converterListSize      = sectionSizes[1];
             gMainTable.tagListSize            = sectionSizes[2];
             gMainTable.aliasListSize          = sectionSizes[3];
@@ -315,6 +312,9 @@ haveAliasData(UErrorCode *pErrorCode) {
                 ? gMainTable.stringTable : (table + currOffset));
 
             ucln_common_registerCleanup(UCLN_COMMON_UCNV_IO, ucnv_io_cleanup);
+
+            gAliasData = data;
+            data=NULL;
         }
         umtx_unlock(NULL);
 
