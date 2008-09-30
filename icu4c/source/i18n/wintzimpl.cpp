cbecf89bb94655284f02caa455865dbc1af544ba
Yoshito Umaoka <y.umaoka@gmail.com>
2008-09-30 19:18:59 +0000

    [ICU-6207] Cleaned up unused code.  Minor improvements. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/24682 Former-commit-id: 19f480e7ac4e48c2651ec05fb56ec1c6f5739216

--- icu4c/source/i18n/wintzimpl.cpp
+++ icu4c/source/i18n/wintzimpl.cpp
@@ -18,7 +18,8 @@
 #include "unicode/unistr.h"
 #include "unicode/timezone.h"
 #include "unicode/basictz.h"
-#include "unicode/calendar.h"
+#include "putilimp.h"
+#include "uassert.h"
 
 #   define WIN32_LEAN_AND_MEAN
 #   define VC_EXTRALEAN
@@ -32,93 +33,74 @@
 static UBool getSystemTimeInformation(TimeZone *tz, SYSTEMTIME &daylightDate, SYSTEMTIME &standardDate, int32_t &bias, int32_t &daylightBias, int32_t &standardBias) {
     UErrorCode status = U_ZERO_ERROR;
     UBool result = TRUE;
-    BasicTimeZone *btz = NULL;
-    Calendar *cal = NULL;
+    BasicTimeZone *btz = (BasicTimeZone*)tz; // we should check type
     InitialTimeZoneRule *initial = NULL;
-    AnnualTimeZoneRule *std = NULL;
-    AnnualTimeZoneRule *dst = NULL;
-    UDate currentTime;
-    int32_t currentYear;
-    int32_t rawOffset = tz->getRawOffset();
-    int32_t dstOffset = tz->getDSTSavings();
+    AnnualTimeZoneRule *std = NULL, *dst = NULL;
 
-    /* Offset set needs to be normalized so to Windows */
-    bias = (tz->getRawOffset() * -1)/(60000);
-    standardBias = 0;
-    
-    if (tz->useDaylightTime()) {
-        daylightBias = (tz->getDSTSavings() / 60000) * -1;
-    } else {
-        daylightBias = 0;
-    }
-
-    btz = (BasicTimeZone *)tz->clone();
-    cal = Calendar::createInstance(tz, status);
-    if (btz != NULL && cal != NULL && U_SUCCESS(status)) {
-        currentTime = cal->getTime(status);
-        currentYear = cal->get(UCAL_YEAR, status);
-        
-        btz->getSimpleRulesNear(currentTime, initial, std, dst, status);
-        if (U_SUCCESS(status)) {
-            if (std == NULL || dst == NULL) {
-                /* Generally, if std is NULL, dst will also be NULL.
-                 * This indicates that daylight saving is not observed
-                 * in this timezone so the standarDate and daylightDate
-                 * should be zero.
-                 */
-                standardDate.wYear = standardDate.wMonth  = standardDate.wDayOfWeek = standardDate.wDay = 
-                standardDate.wHour = standardDate.wMinute = standardDate.wSecond    = standardDate.wMilliseconds = 0;
-                daylightDate.wYear = daylightDate.wMonth  = daylightDate.wDayOfWeek = daylightDate.wDay =
-                daylightDate.wHour = daylightDate.wMinute = daylightDate.wSecond    = daylightDate.wMilliseconds = 0;
-            } else {
-                /* Get the UDate value for the start of the timezone standard time and daylight saving time.
-                 * Then set the standardDate and daylightDate to this. The wYear value is 0 to indicate that
-                 * this is not an absolute date but a rule. (e.g. the first Sunday in November)
-                 */
-                UDate dateTime;
-                if (std->getStartInYear(currentYear, rawOffset, 0, dateTime)) {
-                    cal->setTime(dateTime, status);
-
-                    standardDate.wYear          = 0;
-                    standardDate.wMonth         = cal->get(UCAL_MONTH, status) + 1;
-                    standardDate.wDayOfWeek     = cal->get(UCAL_DAY_OF_WEEK, status) - 1;
-                    standardDate.wDay           = cal->get(UCAL_DAY_OF_WEEK_IN_MONTH, status);
-                    standardDate.wHour          = cal->get(UCAL_HOUR_OF_DAY, status);
-                    standardDate.wMinute        = cal->get(UCAL_MINUTE, status);
-                    standardDate.wSecond        = cal->get(UCAL_SECOND, status);
-                    standardDate.wMilliseconds  = cal->get(UCAL_MILLISECOND, status);
-
-                    if (U_SUCCESS(status) && dst->getStartInYear(currentYear, rawOffset, dstOffset, dateTime)) {
-                        cal->setTime(dateTime, status);
-
-                        daylightDate.wYear          = 0;
-                        daylightDate.wMonth         = cal->get(UCAL_MONTH, status) + 1;
-                        daylightDate.wDayOfWeek     = cal->get(UCAL_DAY_OF_WEEK, status) - 1;
-                        daylightDate.wDay           = cal->get(UCAL_DAY_OF_WEEK_IN_MONTH, status);
-                        daylightDate.wHour          = cal->get(UCAL_HOUR_OF_DAY, status) + dstOffset/3600000;
-                        daylightDate.wMinute        = cal->get(UCAL_MINUTE, status);
-                        daylightDate.wSecond        = cal->get(UCAL_SECOND, status);
-                        daylightDate.wMilliseconds  = cal->get(UCAL_MILLISECOND, status);
-                    } else {
-                        result = FALSE;
-                    }
-                } else {
-                    result = FALSE;
-                }
-            } 
+    btz->getSimpleRulesNear(uprv_getUTCtime(), initial, std, dst, status);
+    if (U_SUCCESS(status)) {
+        if (std == NULL || dst == NULL) {
+            bias = -1 * (initial->getRawOffset()/60000);
+            daylightBias = 0;
+            // Do not use DST.  Set 0 to all stadardDate/daylightDate fields
+            standardDate.wYear = standardDate.wMonth  = standardDate.wDayOfWeek = standardDate.wDay = 
+            standardDate.wHour = standardDate.wMinute = standardDate.wSecond    = standardDate.wMilliseconds = 0;
+            daylightDate.wYear = daylightDate.wMonth  = daylightDate.wDayOfWeek = daylightDate.wDay =
+            daylightDate.wHour = daylightDate.wMinute = daylightDate.wSecond    = daylightDate.wMilliseconds = 0;
         } else {
-            result = FALSE;
+            U_ASSERT(std->getRule()->getDateRuleType() == DateTimeRule::DOW);
+            U_ASSERT(dst->getRule()->getDateRuleType() == DateTimeRule::DOW);
+
+            bias = -1 * (std->getRawOffset()/60000);
+            daylightBias = -1 * (dst->getDSTSavings()/60000);
+            // Always use DOW type rule
+            int32_t hour, min, sec, mil;
+            standardDate.wYear = 0;
+            standardDate.wMonth = std->getRule()->getRuleMonth() + 1;
+            standardDate.wDay = std->getRule()->getRuleWeekInMonth();
+            if (standardDate.wDay < 0) {
+                standardDate.wDay = 5;
+            }
+            standardDate.wDayOfWeek = std->getRule()->getRuleDayOfWeek() - 1;
+
+            mil = std->getRule()->getRuleMillisInDay();
+            hour = mil/3600000;
+            mil %= 3600000;
+            min = mil/60000;
+            mil %= 60000;
+            sec = mil/1000;
+            mil %= 1000;
+
+            standardDate.wHour = hour;
+            standardDate.wMinute = min;
+            standardDate.wSecond = sec;
+            standardDate.wMilliseconds = mil;
+
+            daylightDate.wYear = 0;
+            daylightDate.wMonth = dst->getRule()->getRuleMonth() + 1;
+            daylightDate.wDay = dst->getRule()->getRuleWeekInMonth();
+            if (daylightDate.wDay < 0) {
+                daylightDate.wDay = 5;
+            }
+            daylightDate.wDayOfWeek = dst->getRule()->getRuleDayOfWeek() - 1;
+
+            mil = dst->getRule()->getRuleMillisInDay();
+            hour = mil/3600000;
+            mil %= 3600000;
+            min = mil/60000;
+            mil %= 60000;
+            sec = mil/1000;
+            mil %= 1000;
+
+            daylightDate.wHour = hour;
+            daylightDate.wMinute = min;
+            daylightDate.wSecond = sec;
+            daylightDate.wMilliseconds = mil;
         }
     } else {
         result = FALSE;
     }
 
-    if (result && U_FAILURE(status)) {
-        result = FALSE;
-    }
-    
-    delete btz;
-    delete cal;
     delete initial;
     delete std;
     delete dst;
@@ -137,9 +119,7 @@ static UBool getWindowsTimeZoneInfo(TIME_ZONE_INFORMATION *zoneInfo, const UChar
         int32_t standardBias;
         SYSTEMTIME daylightDate;
         SYSTEMTIME standardDate;
-        if (!getSystemTimeInformation(tz, daylightDate, standardDate, bias, daylightBias, standardBias)) {
-            result = FALSE;
-        } else {
+        if (getSystemTimeInformation(tz, daylightDate, standardDate, bias, daylightBias, standardBias)) {
             zoneInfo->Bias          = bias;
             zoneInfo->DaylightBias  = daylightBias;
             zoneInfo->StandardBias  = standardBias;
