7cbb42851125ae00532abbb4465b3454b0d29766
Michael Ow <mow@svn.icu-project.org>
2009-01-09 22:30:44 +0000

    ICU-3933 Move flag parsing code to toolutil. Add file modification time functionality to toolutil. Update pkgdata to reflect these changes and to test mod time to see if a new library needs to be built.
    X-SVN-Rev: 25228

---
+++ icu4c/source/tools/toolutil/filestat.h
@@ -0,0 +1,30 @@
+/*
+*******************************************************************************
+*
+*   Copyright (C) 2009, International Business Machines
+*   Corporation and others.  All Rights Reserved.
+*
+*******************************************************************************
+*   file name:  flagparser.h
+*   encoding:   US-ASCII
+*   tab size:   8 (not used)
+*   indentation:4
+*
+*   created on: 2009jan09
+*   created by: Michael Ow
+*
+* Compares modification times on specified files using ICU and intended for use in ICU tests and in build tools.
+* Not suitable for production use. Not supported.
+* Not conformant. Not efficient.
+* But very small.
+*/
+
+#ifndef __FILESTAT_H__
+#define __FILESTAT_H__
+
+#include "unicode/utypes.h"
+
+U_CAPI UBool U_EXPORT2
+isFileModTimeLater(const char *filePath, const char *dirToCheckAgainst);
+
+#endif
