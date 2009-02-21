ce900d8efc3760c91ba260cd3685534937758a9e
Claire Ho <claireho@svn.icu-project.org>
2009-02-21 00:31:21 +0000

    merge new localeDisplayPattern APIs from branch to trunk.
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/6735
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@25457 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/i18n/ulocdata.c
+++ source/i18n/ulocdata.c
@@ -42,7 +42,7 @@ U_CAPI ULocaleData* U_EXPORT2
 ulocdata_open(const char *localeID, UErrorCode *status)
 {
    ULocaleData *uld;
- 
+
    if (U_FAILURE(*status)) {
        return NULL;
    }
@@ -53,7 +53,7 @@ ulocdata_open(const char *localeID, UErrorCode *status)
       return(NULL);
    }
 
-    
+
    uld->noSubstitute = FALSE;
    uld->bundle = ures_open(NULL, localeID, status);
 
@@ -71,7 +71,7 @@ ulocdata_close(ULocaleData *uld)
     if ( uld != NULL ) {
        ures_close(uld->bundle);
        uprv_free(uld);
-    } 
+    }
 }
 
 U_CAPI void U_EXPORT2
@@ -87,9 +87,9 @@ ulocdata_getNoSubstitute(ULocaleData *uld)
 }
 
 U_CAPI USet* U_EXPORT2
-ulocdata_getExemplarSet(ULocaleData *uld, USet *fillIn, 
+ulocdata_getExemplarSet(ULocaleData *uld, USet *fillIn,
                         uint32_t options, ULocaleDataExemplarSetType extype, UErrorCode *status){
-    
+
     static const char* const exemplarSetTypes[] = { "ExemplarCharacters", "AuxExemplarCharacters" };
     const UChar *exemplarChars = NULL;
     int32_t len = 0;
@@ -97,32 +97,32 @@ ulocdata_getExemplarSet(ULocaleData *uld, USet *fillIn,
 
     if (U_FAILURE(*status))
         return NULL;
-    
+
     exemplarChars = ures_getStringByKey(uld->bundle, exemplarSetTypes[extype], &len, &localStatus);
     if ( (localStatus == U_USING_DEFAULT_WARNING) && uld->noSubstitute ) {
         localStatus = U_MISSING_RESOURCE_ERROR;
     }
-       
+
     if (localStatus != U_ZERO_ERROR) {
         *status = localStatus;
     }
-    
+
     if (U_FAILURE(*status))
         return NULL;
-    
+
     if(fillIn != NULL)
-        uset_applyPattern(fillIn, exemplarChars, len, 
+        uset_applyPattern(fillIn, exemplarChars, len,
                           USET_IGNORE_SPACE | options, status);
     else
         fillIn = uset_openPatternOptions(exemplarChars, len,
                                          USET_IGNORE_SPACE | options, status);
-    
+
     return fillIn;
 
 }
 
 U_CAPI int32_t U_EXPORT2
-ulocdata_getDelimiter(ULocaleData *uld, ULocaleDataDelimiterType type, 
+ulocdata_getDelimiter(ULocaleData *uld, ULocaleDataDelimiterType type,
                       UChar *result, int32_t resultLength, UErrorCode *status){
 
     static const char* const delimiterKeys[] =  {
@@ -131,7 +131,7 @@ ulocdata_getDelimiter(ULocaleData *uld, ULocaleDataDelimiterType type,
         "alternateQuotationStart",
         "alternateQuotationEnd"
     };
- 
+
     UResourceBundle *delimiterBundle;
     int32_t len = 0;
     const UChar *delimiter = NULL;
@@ -165,26 +165,26 @@ ulocdata_getDelimiter(ULocaleData *uld, ULocaleDataDelimiterType type,
     if (localStatus != U_ZERO_ERROR) {
         *status = localStatus;
     }
-    
+
     if (U_FAILURE(*status)){
         return 0;
     }
-        
-    u_strncpy(result,delimiter,resultLength);
+
+    u_strncpy(result,delimiter, resultLength);
     return len;
 }
 
 U_CAPI UMeasurementSystem U_EXPORT2
 ulocdata_getMeasurementSystem(const char *localeID, UErrorCode *status){
-    
+
     UResourceBundle* bundle=NULL;
     UResourceBundle* measurement=NULL;
-    UMeasurementSystem system = UMS_LIMIT; 
-    
+    UMeasurementSystem system = UMS_LIMIT;
+
     if(status == NULL || U_FAILURE(*status)){
         return system;
     }
-    
+
     bundle = ures_open(NULL, localeID, status);
 
     measurement = ures_getByKey(bundle, MEASUREMENT_SYSTEM, NULL, status);
@@ -208,11 +208,11 @@ ulocdata_getPaperSize(const char* localeID, int32_t *height, int32_t *width, UEr
     if(status == NULL || U_FAILURE(*status)){
         return;
     }
-    
+
     bundle = ures_open(NULL, localeID, status);
     paperSizeBundle = ures_getByKey(bundle, PAPER_SIZE, NULL, status);
     paperSize = ures_getIntVector(paperSizeBundle, &len,  status);
-    
+
     if(U_SUCCESS(*status)){
         if(len < 2){
             *status = U_INTERNAL_PROGRAM_ERROR;
@@ -221,10 +221,10 @@ ulocdata_getPaperSize(const char* localeID, int32_t *height, int32_t *width, UEr
             *width  = paperSize[1];
         }
     }
-    
+
     ures_close(bundle);
     ures_close(paperSizeBundle);
-    
+
 }
 
 U_DRAFT void U_EXPORT2
@@ -233,4 +233,101 @@ ulocdata_getCLDRVersion(UVersionInfo versionArray, UErrorCode *status) {
     rb = ures_openDirect(NULL, "supplementalData", &status);
     ures_getVersionByKey(rb, "cldrVersion", versionArray, &status);
     ures_close(rb);
+}
+
+U_DRAFT int32_t U_EXPORT2
+ulocdata_getLocaleDisplayPattern(ULocaleData *uld,
+                                 UChar *result,
+                                 int32_t resultCapacity,
+                                 UErrorCode *status) {
+    UResourceBundle *patternBundle;
+    int32_t len = 0;
+    const UChar *pattern = NULL;
+    UErrorCode localStatus = U_ZERO_ERROR;
+
+    if (U_FAILURE(*status))
+        return 0;
+
+    patternBundle = ures_getByKey(uld->bundle, "localeDisplayPattern", NULL, &localStatus);
+
+    if ( (localStatus == U_USING_DEFAULT_WARNING) && uld->noSubstitute ) {
+        localStatus = U_MISSING_RESOURCE_ERROR;
+    }
+
+    if (localStatus != U_ZERO_ERROR) {
+        *status = localStatus;
+    }
+
+    if (U_FAILURE(*status)){
+        ures_close(patternBundle);
+        return 0;
+    }
+
+    pattern = ures_getStringByKey(patternBundle, "pattern", &len, &localStatus);
+    ures_close(patternBundle);
+
+    if ( (localStatus == U_USING_DEFAULT_WARNING) && uld->noSubstitute ) {
+        localStatus = U_MISSING_RESOURCE_ERROR;
+    }
+
+    if (localStatus != U_ZERO_ERROR) {
+        *status = localStatus;
+    }
+
+    if (U_FAILURE(*status)){
+        return 0;
+    }
+
+    u_strncpy(result, pattern, resultCapacity);
+    return len;
+
+}
+
+
+U_DRAFT int32_t U_EXPORT2
+ulocdata_getLocaleSeparator(ULocaleData *uld,
+                            UChar *result,
+                            int32_t resultCapacity,
+                            UErrorCode *status)  {
+    UResourceBundle *separatorBundle;
+    int32_t len = 0;
+    const UChar *separator = NULL;
+    UErrorCode localStatus = U_ZERO_ERROR;
+
+    if (U_FAILURE(*status))
+        return 0;
+
+    separatorBundle = ures_getByKey(uld->bundle, "localeDisplayPattern", NULL, &localStatus);
+
+    if ( (localStatus == U_USING_DEFAULT_WARNING) && uld->noSubstitute ) {
+        localStatus = U_MISSING_RESOURCE_ERROR;
+    }
+
+    if (localStatus != U_ZERO_ERROR) {
+        *status = localStatus;
+    }
+
+    if (U_FAILURE(*status)){
+        ures_close(separatorBundle);
+        return 0;
+    }
+
+    separator = ures_getStringByKey(separatorBundle, "separator", &len, &localStatus);
+    ures_close(separatorBundle);
+
+    if ( (localStatus == U_USING_DEFAULT_WARNING) && uld->noSubstitute ) {
+        localStatus = U_MISSING_RESOURCE_ERROR;
+    }
+
+    if (localStatus != U_ZERO_ERROR) {
+        *status = localStatus;
+    }
+
+    if (U_FAILURE(*status)){
+        return 0;
+    }
+
+    u_strncpy(result, separator, resultCapacity);
+    return len;
+
 }
