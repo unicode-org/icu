8880cad555dbb830607d6e026ba35d26afc3ecf9
Markus Scherer <markus.icu@gmail.com>
2009-03-11 05:27:11 +0000

    [ICU-6483] add ErrorCode class, C++ convenience wrapper for UErrorCode X-Commit-URL: https://ssl.icu-project.org/trac/changeset/25546 Former-commit-id: 972862ec5ddd232c5bd6e64788de58e8641232ad

--- icu4c/source/test/intltest/itutil.cpp
+++ icu4c/source/test/intltest/itutil.cpp
@@ -1,6 +1,6 @@
 /********************************************************************
  * COPYRIGHT: 
- * Copyright (c) 1997-2008, International Business Machines Corporation and
+ * Copyright (c) 1997-2009, International Business Machines Corporation and
  * others. All Rights Reserved.
  ********************************************************************/
 
@@ -10,6 +10,7 @@
  */
 
 #include "unicode/utypes.h"
+#include "unicode/errorcode.h"
 #include "itutil.h"
 #include "strtest.h"
 #include "loctest.h"
@@ -55,8 +56,130 @@ void IntlTestUtilities::runIndexedTest( int32_t index, UBool exec, const char* &
         CASE(11, UVectorTest); 
         CASE(12, UTextTest); 
         CASE(13, LocaleAliasTest); 
-        CASE(14, UnicodeSetTest); 
+        CASE(14, UnicodeSetTest);
+        CASE(15, ErrorCodeTest);
         default: name = ""; break; //needed to end loop
     }
 }
 
+void ErrorCodeTest::runIndexedTest(int32_t index, UBool exec, const char* &name, char* par) {
+    if (exec) logln("TestSuite Utilities: ");
+    switch (index) {
+        case 0: name = "TestErrorCode"; if (exec) TestErrorCode(); break;
+        case 1: name = "TestSubclass"; if (exec) TestSubclass(); break;
+        default: name = ""; break; //needed to end loop
+    }
+}
+
+static void RefPlusOne(UErrorCode &code) { code=(UErrorCode)(code+1); }
+static void PtrPlusTwo(UErrorCode *code) { *code=(UErrorCode)(*code+2); }
+
+void ErrorCodeTest::TestErrorCode() {
+    ErrorCode errorCode;
+    if(errorCode.get()!=U_ZERO_ERROR || !errorCode.isSuccess() || errorCode.isFailure()) {
+        errln("ErrorCode did not initialize properly");
+        return;
+    }
+    errorCode.check();
+    RefPlusOne(errorCode);
+    if(errorCode.get()!=U_ILLEGAL_ARGUMENT_ERROR || errorCode.isSuccess() || !errorCode.isFailure()) {
+        errln("ErrorCode did not yield a writable reference");
+    }
+    PtrPlusTwo(errorCode);
+    if(errorCode.get()!=U_INVALID_FORMAT_ERROR || errorCode.isSuccess() || !errorCode.isFailure()) {
+        errln("ErrorCode did not yield a writable pointer");
+    }
+    errorCode.set(U_PARSE_ERROR);
+    if(errorCode.get()!=U_PARSE_ERROR || errorCode.isSuccess() || !errorCode.isFailure()) {
+        errln("ErrorCode.set() failed");
+    }
+    if( errorCode.reset()!=U_PARSE_ERROR || errorCode.get()!=U_ZERO_ERROR ||
+        !errorCode.isSuccess() || errorCode.isFailure()
+    ) {
+        errln("ErrorCode did not reset properly");
+    }
+}
+
+class MyErrorCode: public ErrorCode {
+public:
+    MyErrorCode(int32_t &countChecks, int32_t &countDests)
+        : checks(countChecks), dests(countDests) {}
+    ~MyErrorCode() {
+        if(isFailure()) {
+            handleFailure(kDestructor);
+        }
+    }
+private:
+    virtual void handleFailure(EOrigin origin) const {
+        switch(origin) {
+        case kCheck:
+            ++checks;
+            break;
+        case kDestructor:
+            ++dests;
+            break;
+        default:
+            break;
+        }
+    }
+    int32_t &checks;
+    int32_t &dests;
+};
+
+void ErrorCodeTest::TestSubclass() {
+    int32_t countChecks=0;
+    int32_t countDests=0;
+    {
+        MyErrorCode errorCode(countChecks, countDests);
+        if( errorCode.get()!=U_ZERO_ERROR || !errorCode.isSuccess() || errorCode.isFailure() ||
+            countChecks!=0 || countDests!=0
+        ) {
+            errln("ErrorCode did not initialize properly");
+            return;
+        }
+        errorCode.check();
+        if(countChecks!=0) {
+            errln("ErrorCode.check() called handleFailure(kCheck) despite success");
+        }
+        RefPlusOne(errorCode);
+        if(errorCode.get()!=U_ILLEGAL_ARGUMENT_ERROR || errorCode.isSuccess() || !errorCode.isFailure()) {
+            errln("ErrorCode did not yield a writable reference");
+        }
+        errorCode.check();
+        if(countChecks!=1) {
+            errln("ErrorCode.check() did not handleFailure(kCheck)");
+        }
+        PtrPlusTwo(errorCode);
+        if(errorCode.get()!=U_INVALID_FORMAT_ERROR || errorCode.isSuccess() || !errorCode.isFailure()) {
+            errln("ErrorCode did not yield a writable pointer");
+        }
+        errorCode.check();
+        if(countChecks!=2) {
+            errln("ErrorCode.check() did not handleFailure(kCheck)");
+        }
+        errorCode.set(U_PARSE_ERROR);
+        if(errorCode.get()!=U_PARSE_ERROR || errorCode.isSuccess() || !errorCode.isFailure()) {
+            errln("ErrorCode.set() failed");
+        }
+        if( errorCode.reset()!=U_PARSE_ERROR || errorCode.get()!=U_ZERO_ERROR ||
+            !errorCode.isSuccess() || errorCode.isFailure()
+        ) {
+            errln("ErrorCode did not reset properly");
+        }
+        errorCode.check();
+        if(countChecks!=2) {
+            errln("ErrorCode.check() called handleFailure(kCheck) despite success");
+        }
+    }
+    if(countDests!=0) {
+        errln("ErrorCode.check() called handleFailure(kDestructor) despite success");
+    }
+    countChecks=countDests=0;
+    {
+        MyErrorCode errorCode(countChecks, countDests);
+        errorCode.set(U_PARSE_ERROR);
+    }
+    if(countDests!=1) {
+        errln("ErrorCode destructor did not handleFailure(kDestructor)");
+    }
+}
