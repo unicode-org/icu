895b31eb06b27ce45c577932beb528d69160e1d1
Steven R. Loomis <srl@icu-project.org>
2009-03-26 15:56:47 +0000

    remove duplicate 'that'
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/5078
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@25656 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/i18n/islamcal.h
+++ source/i18n/islamcal.h
@@ -1,6 +1,6 @@
 /*
  ********************************************************************************
- * Copyright (C) 2003-2008, International Business Machines Corporation
+ * Copyright (C) 2003-2009, International Business Machines Corporation
  * and others. All Rights Reserved.
  ******************************************************************************
  *
@@ -26,7 +26,7 @@ U_NAMESPACE_BEGIN
 
 /**
  * <code>IslamicCalendar</code> is a subclass of <code>Calendar</code>
- * that that implements the Islamic civil and religious calendars.  It
+ * that implements the Islamic civil and religious calendars.  It
  * is used as the civil calendar in most of the Arab world and the
  * liturgical calendar of the Islamic faith worldwide.  This calendar
  * is also known as the "Hijri" calendar, since it starts at the time
