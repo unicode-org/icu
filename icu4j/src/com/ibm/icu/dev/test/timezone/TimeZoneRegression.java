c7542e088b7b5198cb732f986e3b6a8c56a9e61e
Yoshito Umaoka <y.umaoka@gmail.com>
2009-03-31 01:44:14 +0000

    ICU-6818 setRawOffset to inherit the DST rule for the current time.
    X-SVN-Rev: 25688

--- icu4j/src/com/ibm/icu/dev/test/timezone/TimeZoneRegression.java
+++ icu4j/src/com/ibm/icu/dev/test/timezone/TimeZoneRegression.java
@@ -1071,14 +1071,10 @@ public class TimeZoneRegression extends TestFmwk {
                 errln("FAIL: Modified zone(" + tz.getID() + ") - getRawOffset returns " + offset + "/ Expected: " + newRawOffset);
             }
             // Ticket#5917
-            // Check if DST observation status is not unexpectedly changed to true.
-            // When original Olson time zone observes DST, setRawOffset may change DST observation
-            // status for some zones.  For example, Asia/Jerusalem, which currently use no DST after
-            // 2037 in tzdata 2007g.  But, the opposite change (false -> true) should never happen.
-            if (!useDst) {
-                if (tz.useDaylightTime()) {
-                    errln("FAIL: Modified zone(" + tz.getID() + ") - useDaylightTime has changed from false to true.");
-                }
+            // Check if DST observation status is not unexpectedly changed.
+            boolean newDst = tz.useDaylightTime();
+            if (useDst != newDst) {
+                errln("FAIL: Modified zone(" + tz.getID() + ") - useDaylightTime has changed from " + useDst + " to " + newDst);
             }
             // Make sure the offset is preserved in a clone
             TimeZone tzClone = (TimeZone)tz.clone();
