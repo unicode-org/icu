ace4d5e76d0329b0bc11ed35e3caa4e842404870
Yoshito Umaoka <y.umaoka@gmail.com>
2009-04-17 20:10:24 +0000

    ICU-6809 Remove unused import statement.
    X-SVN-Rev: 25817

--- icu4j/src/com/ibm/icu/dev/test/util/ICUPropertyFactory.java
+++ icu4j/src/com/ibm/icu/dev/test/util/ICUPropertyFactory.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
