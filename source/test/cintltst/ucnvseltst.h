460f2b182def18830c0eaf407cd76f774271c951
Markus Scherer <markus.icu@gmail.com>
2009-04-21 23:46:53 +0000

    partial test cleanup
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/6453
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@25858 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/test/cintltst/ucnvseltst.h
+++ source/test/cintltst/ucnvseltst.h
@@ -1,6 +1,6 @@
 /********************************************************************
  * COPYRIGHT: 
- * Copyright (c) 1997-2008, International Business Machines Corporation and
+ * Copyright (c) 1997-2009, International Business Machines Corporation and
  * others. All Rights Reserved.
  ********************************************************************/
 /********************************************************************************
@@ -19,13 +19,9 @@
 #include "cintltst.h"
 
     /**
-     * The function used to test selection for UTF8 strings
+     * The function used to test converter selection
      **/
-    static void TestConversionUTF8(void);
-    /**
-     * The function used to test selection for UTF16 strings
-     **/
-    static void TestConversionUTF16(void);
+    static void TestSelect(void);
     /**
      * The function used to test serialization and unserialization 
      **/
