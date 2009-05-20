baa0144a45450a591940411f2ebd314d46fa12fa
Yoshito Umaoka <y.umaoka@gmail.com>
2009-05-20 17:02:25 +0000

    [ICU-6952] Fix one error in the previous submission. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/26027 Former-commit-id: e47d5d49913e000ca3cd65c7aaf7f27dca05855a

--- icu4j/src/com/ibm/icu/dev/test/perf/NormalizerPerformanceTest.java
+++ icu4j/src/com/ibm/icu/dev/test/perf/NormalizerPerformanceTest.java
@@ -1,4 +1,4 @@
-//##header J2SE15
+//##header
 /*
 **********************************************************************
 * Copyright (c) 2002-2009, International Business Machines           *
@@ -694,13 +694,13 @@ public class NormalizerPerformanceTest extends PerfTest {
 
     void normalizerTest(String line, boolean compose) {
 //#if defined(J2SE15)
- sun.text.Normalizer.normalize(line, compose
-      ? sun.text.Normalizer.COMPOSE
-      : sun.text.Normalizer.DECOMP, 0);
+//## sun.text.Normalizer.normalize(line, compose
+//##      ? sun.text.Normalizer.COMPOSE
+//##      : sun.text.Normalizer.DECOMP, 0);
 //#else
-//## java.text.Normalizer.normalize(line, compose
-//##      ? java.text.Normalizer.Form.NFC
-//##      : java.text.Normalizer.Form.NFD);
+ java.text.Normalizer.normalize(line, compose
+      ? java.text.Normalizer.Form.NFC
+      : java.text.Normalizer.Form.NFD);
 //#endif
     }
 }
