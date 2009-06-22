42da1f8d6be49147cf8da6b65db827aeea442bf3
John Emmons <emmo@us.ibm.com>
2009-06-22 17:00:28 +0000

    ICU-6979 Integrate CLDR 1.7.1
    X-SVN-Rev: 26138

--- icu4c/source/test/intltest/loctest.cpp
+++ icu4c/source/test/intltest/loctest.cpp
@@ -860,8 +860,8 @@ LocaleTest::TestGetLangsAndCountries()
       ;
 
     /* TODO: Change this test to be more like the cloctst version? */
-    if (testCount != 490)
-        errln("Expected getISOLanguages() to return 490 languages; it returned %d", testCount);
+    if (testCount != 491)
+        errln("Expected getISOLanguages() to return 491 languages; it returned %d", testCount);
     else {
         for (i = 0; i < 15; i++) {
             int32_t j;
