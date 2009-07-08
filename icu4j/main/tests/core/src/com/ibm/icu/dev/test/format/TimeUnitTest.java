44ec47a4f2e1ac346394f443753c0bc9bb22ff50
John Vu <j.vu@live.com>
2009-07-08 23:01:22 +0000

    [ICU-6958] Increased code coverage for com.ibm.icu.text. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/26251 Former-commit-id: 5a80fda8f46de51a6ad84cd38d1cced3841a1b04

--- icu4j/main/tests/core/src/com/ibm/icu/dev/test/format/TimeUnitTest.java
+++ icu4j/main/tests/core/src/com/ibm/icu/dev/test/format/TimeUnitTest.java
@@ -1,12 +1,14 @@
 /*
  *******************************************************************************
- * Copyright (C) 2008, International Business Machines Corporation and         *
+ * Copyright (C) 2009, International Business Machines Corporation and         *
  * others. All Rights Reserved.                                                *
  *******************************************************************************
  */
 package com.ibm.icu.dev.test.format;
 
 import java.text.ParseException;
+import java.text.ParsePosition;
+import java.util.Locale;
 
 import com.ibm.icu.dev.test.TestFmwk;
 import com.ibm.icu.text.NumberFormat;
@@ -79,6 +81,13 @@ public class TimeUnitTest extends TestFmwk {
         format.setLocale(es);
         formatParsing(format);
         
+        format.setLocale(new Locale("pt_BR"));
+        formatParsing(format);
+        format = new TimeUnitFormat(new Locale("de"));
+        formatParsing(format);
+        format = new TimeUnitFormat(new Locale("ja"));
+        format.setNumberFormat(NumberFormat.getNumberInstance(new Locale("en")));
+        formatParsing(format);
     }
 
     private void formatParsing(TimeUnitFormat format) {
@@ -101,5 +110,100 @@ public class TimeUnitTest extends TestFmwk {
                 }
             }
         }
+    }
+    
+    /*
+     * Tests the method public TimeUnitFormat(ULocale locale, int style), public TimeUnitFormat(Locale locale, int style)
+     */
+    @SuppressWarnings("unused")
+    public void TestTimeUnitFormat() {
+        // Tests when "if (style < FULL_NAME || style >= TOTAL_STYLES)" is true
+        // TOTAL_STYLES is 2
+        int[] cases = { TimeUnitFormat.FULL_NAME - 1, TimeUnitFormat.FULL_NAME - 2, 2, 3 };
+        for (int i = 0; i < cases.length; i++) {
+            try {
+                TimeUnitFormat tuf = new TimeUnitFormat(new ULocale("en_US"), cases[i]);
+                errln("TimeUnitFormat(ULocale,int) was suppose to return an " + "exception for a style value of "
+                        + cases[i] + "passed into the constructor.");
+            } catch (Exception e) {
+            }
+        }
+        for (int i = 0; i < cases.length; i++) {
+            try {
+                TimeUnitFormat tuf = new TimeUnitFormat(new Locale("en_US"), cases[i]);
+                errln("TimeUnitFormat(ULocale,int) was suppose to return an " + "exception for a style value of "
+                        + cases[i] + "passed into the constructor.");
+            } catch (Exception e) {
+            }
+        }
+    }
+    
+    /*
+     * Tests the method public TimeUnitFormat setLocale(ULocale locale) public TimeUnitFormat setLocale(Locale locale)
+     */
+    public void TestSetLocale() {
+        // Tests when "if ( locale != this.locale )" is false
+        TimeUnitFormat tuf = new TimeUnitFormat(new ULocale("en_US"));
+        if (!tuf.setLocale(new ULocale("en_US")).equals(tuf) && !tuf.setLocale(new Locale("en_US")).equals(tuf)) {
+            errln("TimeUnitFormat.setLocale(ULocale) was suppose to "
+                    + "return the same TimeUnitFormat object if the same " + "ULocale is entered as a parameter.");
+        }
+    }
+
+    /*
+     * Tests the method public TimeUnitFormat setNumberFormat(NumberFormat format)
+     */
+    public void TestSetNumberFormat() {
+        TimeUnitFormat tuf = new TimeUnitFormat();
+
+        // Tests when "if (format == this.format)" is false
+        // Tests when "if ( format == null )" is false
+        tuf.setNumberFormat(NumberFormat.getInstance());
+
+        // Tests when "if (format == this.format)" is true
+        if (!tuf.setNumberFormat(NumberFormat.getInstance()).equals(tuf)) {
+            errln("TimeUnitFormat.setNumberFormat(NumberFormat) was suppose to "
+                    + "return the same object when the same NumberFormat is passed.");
+        }
+
+        // Tests when "if ( format == null )" is true
+        // Tests when "if ( locale == null )" is true
+        if (!tuf.setNumberFormat(null).equals(tuf)) {
+            errln("TimeUnitFormat.setNumberFormat(NumberFormat) was suppose to "
+                    + "return the same object when null is passed.");
+        }
+
+        TimeUnitFormat tuf1 = new TimeUnitFormat(new ULocale("en_US"));
+
+        // Tests when "if ( locale == null )" is false
+        tuf1.setNumberFormat(NumberFormat.getInstance());
+        tuf1.setNumberFormat(null);
+    }
+    
+    /*
+     * Tests the method public StringBuffer format(Object obj, ...
+     */
+    public void TestFormat() {
+        TimeUnitFormat tuf = new TimeUnitFormat();
+        try {
+            tuf.format(new Integer("1"), null, null);
+            errln("TimeUnitFormat.format(Object,StringBuffer,FieldPosition) "
+                    + "was suppose to return an exception because the Object "
+                    + "parameter was not of type TimeUnitAmount.");
+        } catch (Exception e) {
+        }
+    }
+    
+    /* Tests the method private void setup() from
+     * public Object parseObject(String source, ParsePosition pos)
+     * 
+     */
+    public void TestSetup(){
+        TimeUnitFormat tuf = new TimeUnitFormat();
+        tuf.parseObject("", new ParsePosition(0));
+        
+        TimeUnitFormat tuf1 = new TimeUnitFormat();
+        tuf1.setNumberFormat(NumberFormat.getInstance());
+        tuf1.parseObject("", new ParsePosition(0));
     }
 }
