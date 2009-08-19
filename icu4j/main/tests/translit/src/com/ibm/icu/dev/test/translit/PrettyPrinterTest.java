2a21b4cfb74f344392a3df8b59e74bdb73bae878
Mark Davis <mark@macchiato.com>
2009-08-19 17:25:31 +0000

    ICU-6824 Remove dependency on collator in test
    X-SVN-Rev: 26515

--- icu4j/main/tests/translit/src/com/ibm/icu/dev/test/translit/PrettyPrinterTest.java
+++ icu4j/main/tests/translit/src/com/ibm/icu/dev/test/translit/PrettyPrinterTest.java
@@ -6,11 +6,12 @@
  */
 package com.ibm.icu.dev.test.translit;
 
+import java.text.Collator;
+import java.util.Locale;
+
 import com.ibm.icu.dev.test.TestFmwk;
 import com.ibm.icu.dev.test.util.PrettyPrinter;
-import com.ibm.icu.text.Collator;
 import com.ibm.icu.text.UnicodeSet;
-import com.ibm.icu.util.ULocale;
 
 public class PrettyPrinterTest extends TestFmwk {
     public static void main(String[] args) throws Exception {
@@ -20,11 +21,13 @@ public class PrettyPrinterTest extends TestFmwk {
 
     public void TestBasicUnicodeSet() {
 
+        Collator spaceComp = Collator.getInstance(Locale.ENGLISH);
+        spaceComp.setStrength(Collator.PRIMARY);
+        
         final PrettyPrinter PRETTY_PRINTER = new PrettyPrinter()
-        .setOrdering(Collator.getInstance(ULocale.ROOT))
-        .setSpaceComparator(Collator.getInstance(ULocale.ROOT).setStrength2(Collator.PRIMARY))
-        .setToQuote(TO_QUOTE)
-        .setSpaceComparator(Collator.getInstance(ULocale.ROOT).setStrength2(Collator.PRIMARY));
+        .setOrdering(Collator.getInstance(Locale.ENGLISH))
+        .setSpaceComparator(spaceComp)
+        .setToQuote(TO_QUOTE);
 
         UnicodeSet expected = new UnicodeSet("[:L:]");
         String formatted = PRETTY_PRINTER.format(expected);
