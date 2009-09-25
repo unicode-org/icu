a64f4a885d3d78909d4be13d7185ad85819e5e04
Michael Ow <mow@svn.icu-project.org>
2009-09-25 17:16:39 +0000

    Fix function name.
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/6991
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@26689 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/i18n/brktrans.cpp
+++ source/i18n/brktrans.cpp
@@ -1,6 +1,6 @@
 /*
 **********************************************************************
-*   Copyright (C) 2008, International Business Machines
+*   Copyright (C) 2008-2009, International Business Machines
 *   Corporation and others.  All Rights Reserved.
 **********************************************************************
 *   Date        Name        Description
@@ -146,7 +146,7 @@ const UnicodeString &BreakTransliterator::getInsertion() const {
 //
 //  setInsertion()
 //
-void BreakTransliterator::setInsertsion(const UnicodeString &insertion) {
+void BreakTransliterator::setInsertion(const UnicodeString &insertion) {
     this->fInsertion = insertion;
 }
 
