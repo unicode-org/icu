21eeaf7bc79d6e80865547cf36874f53f20dd632
Michael Ow <mow@svn.icu-project.org>
2009-11-10 16:01:28 +0000

    Ignore test failures on Windows for croatian locale.
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/7230
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@26861 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/test/iotest/iotest.cpp
+++ source/test/iotest/iotest.cpp
@@ -45,6 +45,14 @@ public:
         buffer[3999] = 0; /* NULL terminate */
         log_err(buffer);
     }
+
+    virtual void logln( const UnicodeString &message ) {
+        char buffer[4000];
+        message.extract(0, message.length(), buffer, sizeof(buffer));
+        buffer[3999] = 0; /* NULL terminate */
+        log_info(buffer);
+    }
+
     virtual void dataerrln( const UnicodeString &message ) {
         char buffer[4000];
         message.extract(0, message.length(), buffer, sizeof(buffer));
