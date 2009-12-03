4bffbd06fd8e9b1a3955bcfbf0aef8609557211f
Peter Edberg <pedberg@unicode.org>
2009-12-03 08:00:33 +0000

    [ICU-7279] By default, set U_HIDE_INTERNAL_API if SHOW_DRAFT is disabled X-Commit-URL: https://ssl.icu-project.org/trac/changeset/27010 Former-commit-id: 5ff1cf881c601a2698c74b26ec87989b59827e7c

--- icu4c/source/common/unicode/utypes.h
+++ icu4c/source/common/unicode/utypes.h
@@ -45,6 +45,9 @@
 #if !U_DEFAULT_SHOW_DRAFT && !defined(U_SHOW_DRAFT_API)
 #define U_HIDE_DRAFT_API 1
 #endif
+#if !U_DEFAULT_SHOW_DRAFT && !defined(U_SHOW_INTERNAL_API)
+#define U_HIDE_INTERNAL_API 1
+#endif
 
 #ifdef U_HIDE_DRAFT_API
 #include "unicode/udraft.h"
