337bb8effe3ca5df9cbfbd1d8b4f384c8a5129e5
Yoshito Umaoka <y.umaoka@gmail.com>
2010-01-04 15:52:45 +0000

    ICU-7095 Removed an import statement no longer used
    X-SVN-Rev: 27125

--- icu4j/main/classes/core/src/com/ibm/icu/lang/UCharacter.java
+++ icu4j/main/classes/core/src/com/ibm/icu/lang/UCharacter.java
@@ -1,13 +1,12 @@
 /**
 *******************************************************************************
-* Copyright (C) 1996-2009, International Business Machines Corporation and    *
+* Copyright (C) 1996-2010, International Business Machines Corporation and    *
 * others. All Rights Reserved.                                                *
 *******************************************************************************
 */
 
 package com.ibm.icu.lang;
 
-import java.io.IOException;
 import java.lang.ref.SoftReference;
 import java.util.HashMap;
 import java.util.Locale;
