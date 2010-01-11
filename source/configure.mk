b27bab0ccf0a1f70415215dc3507a4539c0d0cb8
Steven R. Loomis <srl@icu-project.org>
2010-01-11 21:58:15 +0000

    sub-second precision (where available)
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/7112
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@27210 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/configure.mk
+++ source/configure.mk
@@ -1,4 +1,4 @@
-#  Copyright (c) 2008-2009, International Business Machines Corporation and others. All Rights Reserved.
+#  Copyright (c) 2008-2010, International Business Machines Corporation and others. All Rights Reserved.
 # 
 #
 # Makefile for regenerating configure in the face of a bad ^M
@@ -7,6 +7,11 @@
 # Usage:    MAKE -f configure.mk configure
 
 AUTOCONF=autoconf
+all: configure common/icucfg.h.in
+
+common/icucfg.h.in: configure.in
+	autoheader
+
 configure:	configure.in ./aclocal.m4
 	( $(AUTOCONF) && mv configure configure.tmp && sed -e 's%^ac_cr=.*%ac_cr=`echo X |tr X "\\015"`%'  < configure.tmp > configure && chmod a+rx $@ && rm configure.tmp ) || ( rm $@ ; "echo configure build failed" ; /usr/bin/false  )
 
