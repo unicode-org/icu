8e57ee7baceb868ec745bb61054997935f839f47
Peter Edberg <pedberg@unicode.org>
2010-01-15 23:38:12 +0000

    ICU-2223 Port weekend APIs from ICU4J to C,C++ (dummy implementation for now)
    X-SVN-Rev: 27292

--- icu4c/source/i18n/unicode/calendar.h
+++ icu4c/source/i18n/unicode/calendar.h
@@ -1,6 +1,6 @@
 /*
 ********************************************************************************
-*   Copyright (C) 1997-2009, International Business Machines
+*   Copyright (C) 1997-2010, International Business Machines
 *   Corporation and others.  All Rights Reserved.
 ********************************************************************************
 *
@@ -1189,6 +1189,59 @@ public:
      * @internal
      */
     virtual const char * getType() const = 0;
+
+    /**
+	 * Returns whether the given day of the week is a weekday, a
+	 * weekend day, or a day that transitions from one to the other,
+	 * in this calendar system. If a transition occurs at midnight,
+	 * then the days before and after the transition will have the
+	 * type UCAL_WEEKDAY or UCAL_WEEKEND. If a transition occurs at a time
+	 * other than midnight, then the day of the transition will have
+	 * the type UCAL_WEEKEND_ONSET or UCAL_WEEKEND_CEASE. In this case, the
+	 * method getWeekendTransition() will return the point of
+	 * transition.
+	 * @param dayOfWeek The day of the week whose type is desired (UCAL_SUNDAY..UCAL_SATURDAY).
+	 * @param status The error code for the operation.
+	 * @return The UCalendarWeekdayType for the day of the week.
+     * @draft ICU 4.4
+     */
+    virtual UCalendarWeekdayType getDayOfWeekType(UCalendarDaysOfWeek dayOfWeek, UErrorCode &status) const;
+
+    /**
+	 * Returns the time during the day at which the weekend begins or ends in
+	 * this calendar system.  If getDayOfWeekType() rerturns UCAL_WEEKEND_ONSET
+	 * for the specified dayOfWeek, return the time at which the weekend begins.
+	 * If getDayOfWeekType() returns UCAL_WEEKEND_CEASE for the specified dayOfWeek,
+	 * return the time at which the weekend ends. If getDayOfWeekType() returns
+	 * some other UCalendarWeekdayType for the specified dayOfWeek, is it an error condition
+	 * (U_ILLEGAL_ARGUMENT_ERROR).
+	 * @param dayOfWeek The day of the week for which the weekend transition time is
+	 * desired (UCAL_SUNDAY..UCAL_SATURDAY).
+	 * @param status The error code for the operation.
+	 * @return The milliseconds after midnight at which the weekend begins or ends.
+     * @draft ICU 4.4
+     */
+    virtual int32_t getWeekendTransition(UCalendarDaysOfWeek dayOfWeek, UErrorCode &status) const;
+
+    /**
+	 * Returns TRUE if the given UDate is in the weekend in
+	 * this calendar system.
+	 * @param date The UDate in question.
+	 * @param status The error code for the operation.
+	 * @return TRUE if the given UDate is in the weekend in
+	 * this calendar system, FALSE otherwise.
+     * @draft ICU 4.4
+     */
+    virtual UBool isWeekend(UDate date, UErrorCode &status) const;
+
+    /**
+	 * Returns TRUE if this Calendar's current date-time is in the weekend in
+	 * this calendar system.
+	 * @return TRUE if this Calendar's current date-time is in the weekend in
+	 * this calendar system, FALSE otherwise.
+     * @draft ICU 4.4
+     */
+    virtual UBool isWeekend(void) const;
 
 protected:
 
