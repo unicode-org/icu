82f1288deabed31c2fd94dbb442e0539b9280fe1
Yoshito Umaoka <y.umaoka@gmail.com>
2010-02-08 16:46:01 +0000

    ICU-7302 Adding key/type data (keyTypeData.txt and timezoneTypes.txt) generated from CLDR bcp47/*.xml.
    X-SVN-Rev: 27508

--- icu4c/source/data/misc/miscfiles.mk
+++ icu4c/source/data/misc/miscfiles.mk
@@ -23,4 +23,4 @@
 #
 
 MISC_SOURCE = \
-zoneinfo.txt supplementalData.txt likelySubtags.txt plurals.txt numberingSystems.txt icuver.txt icustd.txt metaZones.txt windowsZones.txt
+zoneinfo.txt supplementalData.txt likelySubtags.txt plurals.txt numberingSystems.txt icuver.txt icustd.txt metaZones.txt windowsZones.txt keyTypeData.txt timezoneTypes.txt
