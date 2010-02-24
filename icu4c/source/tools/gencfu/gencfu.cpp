4c024d9fca16ddc405890c6d4892150c2352ff37
Michael Ow <mow@svn.icu-project.org>
2010-02-24 22:12:19 +0000

    [ICU-7311] Fix typo. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/27655 Former-commit-id: 30cf25c6bc573832cb1d6c94c8d33e4bec1960ce

--- icu4c/source/tools/gencfu/gencfu.cpp
+++ icu4c/source/tools/gencfu/gencfu.cpp
@@ -319,7 +319,7 @@ int  main(int argc, char **argv) {
     long t = fread(result, 1, fileSize, file);
     if (t != fileSize)  {
         delete [] result;
-        close(file);
+        fclose(file);
         return NULL;
     }
     result[fileSize]=0;
