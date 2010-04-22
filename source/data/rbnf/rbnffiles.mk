c5a7f51aab159dbd5cea4be9804e350088a403a0
John Emmons <emmo@us.ibm.com>
2010-04-22 21:02:18 +0000

    ICU-7607 CLDR 1.8.1 integration
    X-SVN-Rev: 27972

--- source/data/rbnf/rbnffiles.mk
+++ source/data/rbnf/rbnffiles.mk
@@ -1,6 +1,6 @@
 # *   Copyright (C) 1998-2010, International Business Machines
 # *   Corporation and others.  All Rights Reserved.
-RBNF_CLDR_VERSION = 1.7
+RBNF_CLDR_VERSION = 1.8.1
 # A list of txt's to build
 # Note:
 #
