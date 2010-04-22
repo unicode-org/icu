bc9e38c998de6b682f140ccfed7437db3742b5bf
John Emmons <emmo@us.ibm.com>
2010-04-22 17:39:04 +0000

    ICU-7607 CLDR 1.8.1 integration
    X-SVN-Rev: 27970

--- icu4c/source/data/coll/colfiles.mk
+++ icu4c/source/data/coll/colfiles.mk
@@ -1,6 +1,6 @@
 # *   Copyright (C) 1998-2010, International Business Machines
 # *   Corporation and others.  All Rights Reserved.
-COLLATION_CLDR_VERSION = 1.7
+COLLATION_CLDR_VERSION = 1.8.1
 # A list of txt's to build
 # Note:
 #
