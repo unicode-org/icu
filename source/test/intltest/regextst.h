6ac09a55bd9fef48539c38507ba4bf6322d265c2
Peter Edberg <pedberg@svn.icu-project.org>
2010-04-28 03:55:58 +0000

    Add test case (segfaults without the fix in r27992)
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/7651
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@27994 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/test/intltest/regextst.h
+++ source/test/intltest/regextst.h
@@ -40,6 +40,7 @@ public:
     virtual void API_Replace_UTF8();
     virtual void PerlTestsUTF8();
     virtual void PreAllocatedUTextCAPI();
+    virtual void Bug7651();
 
     // The following functions are internal to the regexp tests.
     virtual void assertUText(const char *expected, UText *actual, const char *file, int line);
