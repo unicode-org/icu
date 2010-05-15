06c7d56b8ea54f7cfaca86b1485711ffc3f9bf1e
Markus Scherer <markus.icu@gmail.com>
2010-05-15 23:18:12 +0000

    ICU-7684 add CheckedArrayByteSink::Reset()
    X-SVN-Rev: 28067

--- icu4c/source/common/bytestream.cpp
+++ icu4c/source/common/bytestream.cpp
@@ -26,7 +26,12 @@ void ByteSink::Flush() {}
 
 CheckedArrayByteSink::CheckedArrayByteSink(char* outbuf, int32_t capacity)
     : outbuf_(outbuf), capacity_(capacity < 0 ? 0 : capacity),
-      size_(0), appended_(0), overflowed_(false) {
+      size_(0), appended_(0), overflowed_(FALSE) {
+}
+
+CheckedArrayByteSink& CheckedArrayByteSink::Reset() {
+  size_ = appended_ = 0;
+  overflowed_ = FALSE;
 }
 
 void CheckedArrayByteSink::Append(const char* bytes, int32_t n) {
@@ -37,7 +42,7 @@ void CheckedArrayByteSink::Append(const char* bytes, int32_t n) {
   int32_t available = capacity_ - size_;
   if (n > available) {
     n = available;
-    overflowed_ = true;
+    overflowed_ = TRUE;
   }
   if (n > 0 && bytes != (outbuf_ + size_)) {
     uprv_memcpy(outbuf_ + size_, bytes, n);
