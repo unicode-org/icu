2e27a02829b5b600c215154b092bfc2c5c71019e
Yoshito Umaoka <y.umaoka@gmail.com>
2010-05-25 19:27:49 +0000

    ICU-7706 Merging the fix for ticket#7319.
    X-SVN-Rev: 28096

--- src/com/ibm/icu/util/SimpleTimeZone.java
+++ src/com/ibm/icu/util/SimpleTimeZone.java
@@ -1,5 +1,5 @@
 /*
-*   Copyright (C) 1996-2006, International Business Machines
+*   Copyright (C) 1996-2010, International Business Machines
 *   Corporation and others.  All Rights Reserved.
 */
 
@@ -934,7 +934,7 @@ public class SimpleTimeZone extends JDKTimeZone {
             if (startMonth < Calendar.JANUARY || startMonth > Calendar.DECEMBER) {
                 throw new IllegalArgumentException();
             }
-            if (startTime < 0 || startTime >= TimeZone.MILLIS_PER_DAY ||
+            if (startTime < 0 || startTime > TimeZone.MILLIS_PER_DAY ||
                 startTimeMode < WALL_TIME || startTimeMode > UTC_TIME) {
                 throw new IllegalArgumentException();
             }
