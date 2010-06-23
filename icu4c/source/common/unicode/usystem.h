ef22b3356ab59de4f27a982893aefedf7a9cdc5b
Steven R. Loomis <srl@icu-project.org>
2010-06-23 03:13:59 +0000

    ICU-7586 update draft/deprecated. update header test.
    X-SVN-Rev: 28260

--- icu4c/source/common/unicode/usystem.h
+++ icu4c/source/common/unicode/usystem.h
@@ -32,13 +32,13 @@
 #        define uloc_getDefault uloc_getDefault_SYSTEM_API_DO_NOT_USE
 #        define uloc_setDefault uloc_setDefault_SYSTEM_API_DO_NOT_USE
 #    else
-#        define u_cleanup_4_4 u_cleanup_SYSTEM_API_DO_NOT_USE
-#        define u_setAtomicIncDecFunctions_4_4 u_setAtomicIncDecFunctions_SYSTEM_API_DO_NOT_USE
-#        define u_setMemoryFunctions_4_4 u_setMemoryFunctions_SYSTEM_API_DO_NOT_USE
-#        define u_setMutexFunctions_4_4 u_setMutexFunctions_SYSTEM_API_DO_NOT_USE
-#        define ucnv_setDefaultName_4_4 ucnv_setDefaultName_SYSTEM_API_DO_NOT_USE
-#        define uloc_getDefault_4_4 uloc_getDefault_SYSTEM_API_DO_NOT_USE
-#        define uloc_setDefault_4_4 uloc_setDefault_SYSTEM_API_DO_NOT_USE
+#        define u_cleanup_4_5 u_cleanup_SYSTEM_API_DO_NOT_USE
+#        define u_setAtomicIncDecFunctions_4_5 u_setAtomicIncDecFunctions_SYSTEM_API_DO_NOT_USE
+#        define u_setMemoryFunctions_4_5 u_setMemoryFunctions_SYSTEM_API_DO_NOT_USE
+#        define u_setMutexFunctions_4_5 u_setMutexFunctions_SYSTEM_API_DO_NOT_USE
+#        define ucnv_setDefaultName_4_5 ucnv_setDefaultName_SYSTEM_API_DO_NOT_USE
+#        define uloc_getDefault_4_5 uloc_getDefault_SYSTEM_API_DO_NOT_USE
+#        define uloc_setDefault_4_5 uloc_setDefault_SYSTEM_API_DO_NOT_USE
 #    endif /* U_DISABLE_RENAMING */
 
 #endif /* U_HIDE_SYSTEM_API */
