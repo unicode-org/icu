c2228ba03ea5118843e618d8d115ce7e0f1a86d8
Michael Ow <mow@svn.icu-project.org>
2010-07-14 19:02:54 +0000

    ICU-7649 Fix character size error on Solaris.
    X-SVN-Rev: 28306

--- icu4c/source/test/intltest/svccoll.cpp
+++ icu4c/source/test/intltest/svccoll.cpp
@@ -153,17 +153,19 @@ void CollationServiceTest::TestRegister()
         UnicodeString displayName;
         Collator::getDisplayName(fu_FU, displayName);
         /* The locale display pattern for the locale ja, ko, and zh are different. */
+        const UChar zh_fuFU_Array[] = { 0x0066, 0x0075, 0xff08, 0x0046, 0x0055, 0xff09 };
+        const UnicodeString zh_fuFU(zh_fuFU_Array);
         const Locale& defaultLocale = Locale::getDefault();
         if (displayName != "fu (FU)" &&
            ((defaultLocale == Locale::getKorean() && defaultLocale == Locale::getJapanese()) && displayName == "fu(FU)") &&
-           ((defaultLocale == Locale::getChinese()) && displayName != "fu\uff08FU\uff09")) {
+           ((defaultLocale == Locale::getChinese()) && displayName != zh_fuFU)) {
             errln(UnicodeString("found ") + displayName + " for fu_FU");
         }
 
         Collator::getDisplayName(fu_FU, fu_FU, displayName);
         if (displayName != "fu (FU)" &&
            ((defaultLocale == Locale::getKorean() && defaultLocale == Locale::getJapanese()) && displayName == "fu(FU)") &&
-           ((defaultLocale == Locale::getChinese()) && displayName != "fu\uff08FU\uff09")) {
+           ((defaultLocale == Locale::getChinese()) && displayName != zh_fuFU)) {
             errln(UnicodeString("found ") + displayName + " for fu_FU");
         }
 
