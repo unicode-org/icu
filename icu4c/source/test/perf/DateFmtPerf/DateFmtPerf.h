d40bdeea392ef56cf573293c1ed248a2ba80d13b
Alexey Gousev <agousev@svn.icu-project.org>
2010-07-29 19:09:01 +0000

    [ICU-7275] added -x option and several new performance tests X-Commit-URL: https://ssl.icu-project.org/trac/changeset/28387 Former-commit-id: 04bbe480c31a29141bc230a1aad163d21cedf14e

--- icu4c/source/test/perf/DateFmtPerf/DateFmtPerf.h
+++ icu4c/source/test/perf/DateFmtPerf/DateFmtPerf.h
@@ -15,9 +15,15 @@
 #include "unicode/calendar.h"
 #include "unicode/uclean.h"
 #include "unicode/brkiter.h"
+#include "unicode/numfmt.h"
+#include "unicode/coll.h"
+#include "unicode/utypes.h"
 #include "util.h"
+
 #include "datedata.h"
 #include "breakdata.h"
+#include "collationdata.h"
+
 #include <stdlib.h>
 #include <fstream>
 #include <string>
@@ -51,10 +57,10 @@ public:
 		BreakIterator* boundary;
 
 		if(wordIteration)
-		{	
+		{
 			for(int i = 0; i < num; i++)
 			{
-				boundary = BreakIterator::createWordInstance(Locale::getUS(), *status);
+				boundary = BreakIterator::createWordInstance("en", *status);
 				boundary->setText(str);
 
 				int32_t start = boundary->first();
@@ -66,10 +72,11 @@ public:
 				}
 			}
 		}
+
 		else // character iteration
 		{
 			for(int i = 0; i < num; i++)
-			{
+            {
 				boundary = BreakIterator::createCharacterInstance(Locale::getUS(), *status);
 				boundary->setText(str);
 
@@ -131,6 +138,7 @@ class DateFmtFunction : public UPerfFunction
 
 private:
 	int num;
+    char locale[25];
 public:
 	
 	DateFmtFunction()
@@ -138,13 +146,15 @@ public:
 		num = -1;
 	}
 
-	DateFmtFunction(int a)
+	DateFmtFunction(int a, const char* loc)
 	{
 		num = a;
+        strcpy(locale, loc);
 	}
 
 	virtual void call(UErrorCode* status)
 	{
+
 		UErrorCode status2 = U_ZERO_ERROR;		
 		Calendar *cal;
 		TimeZone *zone;
@@ -156,10 +166,10 @@ public:
 		zone = TimeZone::createTimeZone("GMT"); // Create a GMT zone
 		cal->adoptTimeZone(zone);
 		
-		Locale loc("en");
+		Locale loc(locale);
 		DateFormat *fmt;
 		fmt = DateFormat::createDateTimeInstance(
-								DateFormat::kFull, DateFormat::kFull, loc);
+								DateFormat::kShort, DateFormat::kFull, loc);
 
 		
 		// (dates are imported from datedata.h)
@@ -187,7 +197,6 @@ public:
 		delete fmt;
 		delete cal;
 		//u_cleanup();
-
 	}
 
 	virtual long getOperationsPerIteration()
@@ -218,6 +227,176 @@ public:
 
 };
 
+class NumFmtFunction : public UPerfFunction
+{
+
+private:
+	int num;
+    char locale[25];
+public:
+	
+	NumFmtFunction()
+	{
+		num = -1;
+	}
+
+	NumFmtFunction(int a, const char* loc)
+	{
+		num = a;
+        strcpy(locale, loc);
+	}
+
+	virtual void call(UErrorCode* status2)
+	{
+        Locale loc(locale);
+        UErrorCode status = U_ZERO_ERROR;
+        
+        // Create a number formatter for the locale
+        NumberFormat *fmt = NumberFormat::createInstance(loc, status);
+
+        // Parse a string.  The string uses the digits '0' through '9'
+        // and the decimal separator '.', standard in the US locale
+        
+        for(int i = 0; i < num; i++)
+        {
+            UnicodeString str("9876543210.123");
+            Formattable result;
+            fmt->parse(str, result, status);
+
+            //uprintf(formattableToString(result));
+            //printf("\n");
+
+            // Take the number parsed above, and use the formatter to
+            // format it.
+            str.remove(); // format() will APPEND to this string
+            fmt->format(result, str, status);
+
+            //uprintf(str);
+            //printf("\n");
+        }
+
+        delete fmt; // Release the storage used by the formatter
+    }
+
+    enum {
+        U_SPACE=0x20,
+        U_DQUOTE=0x22,
+        U_COMMA=0x2c,
+        U_LEFT_SQUARE_BRACKET=0x5b,
+        U_BACKSLASH=0x5c,
+        U_RIGHT_SQUARE_BRACKET=0x5d,
+        U_SMALL_U=0x75
+    };
+
+    // Create a display string for a formattable
+    UnicodeString formattableToString(const Formattable& f) {
+        switch (f.getType()) {
+        case Formattable::kDate:
+            // TODO: Finish implementing this
+            return UNICODE_STRING_SIMPLE("Formattable_DATE_TBD");
+        case Formattable::kDouble:
+            {
+                char buf[256];
+                sprintf(buf, "%gD", f.getDouble());
+                return UnicodeString(buf, "");
+            }
+        case Formattable::kLong:
+        case Formattable::kInt64:
+            {
+                char buf[256];
+                sprintf(buf, "%ldL", f.getLong());
+                return UnicodeString(buf, "");
+            }
+        case Formattable::kString:
+            return UnicodeString((UChar)U_DQUOTE).append(f.getString()).append((UChar)U_DQUOTE);
+        case Formattable::kArray:
+            {
+                int32_t i, count;
+                const Formattable* array = f.getArray(count);
+                UnicodeString result((UChar)U_LEFT_SQUARE_BRACKET);
+                for (i=0; i<count; ++i) {
+                    if (i > 0) {
+                        (result += (UChar)U_COMMA) += (UChar)U_SPACE;
+                    }
+                    result += formattableToString(array[i]);
+                }
+                result += (UChar)U_RIGHT_SQUARE_BRACKET;
+                return result;
+            }
+        default:
+            return UNICODE_STRING_SIMPLE("INVALID_Formattable");
+        }
+    }
+
+	virtual long getOperationsPerIteration()
+	{
+		return num;
+	}
+    
+    // Print the given string to stdout using the UTF-8 converter (for debugging purposes only)
+    void uprintf(const UnicodeString &str) {
+        char stackBuffer[100];
+        char *buf = 0;
+
+        int32_t bufLen = str.extract(0, 0x7fffffff, stackBuffer, sizeof(stackBuffer), "UTF-8");
+        if(bufLen < sizeof(stackBuffer)) {
+            buf = stackBuffer;
+        } else {
+            buf = new char[bufLen + 1];
+            bufLen = str.extract(0, 0x7fffffff, buf, bufLen + 1, "UTF-8");
+        }
+        printf("%s", buf);
+        if(buf != stackBuffer) {
+            delete[] buf;
+        }
+    }
+};
+
+class CollationFunction : public UPerfFunction
+{
+
+private:
+	int num;
+    char locale[25];
+public:
+	
+	CollationFunction()
+	{
+		num = -1;
+	}
+
+	CollationFunction(int a, const char* loc)
+	{
+		num = a;
+        strcpy(locale, loc);
+	}
+
+	virtual void call(UErrorCode* status2)
+	{
+        uint32_t listSize = sizeof(collation_strings)/sizeof(collation_strings[0]);
+        UErrorCode status = U_ZERO_ERROR; 
+        Collator *coll = Collator::createInstance(Locale(locale), status);
+        
+        for(int k = 0; k < num; k++)
+        {
+            uint32_t i, j;
+            for(i=listSize-1; i>=1; i--) {
+                for(j=0; j<i; j++) {
+                    if(coll->compare(collation_strings[j], collation_strings[j+1]) == UCOL_LESS) {
+                    //cout << "Success!" << endl;
+                     }
+                }
+            }
+         }
+        delete coll; 
+    }
+
+	virtual long getOperationsPerIteration()
+	{
+		return num;
+	}
+};
+
 class DateFormatPerfTest : public UPerfTest
 {
 private:
@@ -235,6 +414,10 @@ public:
 	UPerfFunction* BreakItWord10000();
 	UPerfFunction* BreakItChar250();
 	UPerfFunction* BreakItChar10000();
+    UPerfFunction* NumFmt10000();
+    UPerfFunction* NumFmt100000();
+    UPerfFunction* Collation10000();
+    UPerfFunction* Collation100000();
 };
 
 #endif // DateFmtPerf
