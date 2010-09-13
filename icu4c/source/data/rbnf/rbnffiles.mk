fbd81b912c8c7c9c11ed25d087e462edce0e461a
Peter Edberg <pedberg@unicode.org>
2010-09-13 19:12:47 +0000

    [ICU-7928] Update data files generated from CLDR X-Commit-URL: https://ssl.icu-project.org/trac/changeset/28606 Former-commit-id: 012350ed32b57ed05b8facac88a15ce659785959

--- icu4c/source/data/rbnf/rbnffiles.mk
+++ icu4c/source/data/rbnf/rbnffiles.mk
@@ -35,16 +35,16 @@ RBNF_ALIAS_SOURCE = $(RBNF_SYNTHETIC_ALIAS)
 
 # Ordinary resources
 RBNF_SOURCE = af.txt am.txt ar.txt az.txt\
- be.txt bg.txt ca.txt cs.txt cy.txt\
- da.txt de.txt el.txt en.txt eo.txt\
- es.txt et.txt fa.txt fa_AF.txt fi.txt\
- fil.txt fo.txt fr.txt fr_BE.txt fr_CH.txt\
- ga.txt he.txt hi.txt hr.txt hu.txt\
- hy.txt id.txt is.txt it.txt ja.txt\
- ka.txt kl.txt ko.txt lt.txt lv.txt\
- mk.txt ms.txt mt.txt nb.txt nl.txt\
- nn.txt pl.txt pt.txt pt_PT.txt ro.txt\
- ru.txt sk.txt sl.txt sq.txt sr.txt\
- sr_Latn.txt sv.txt ta.txt th.txt tr.txt\
- uk.txt vi.txt zh.txt zh_Hant.txt
+ be.txt bg.txt bs.txt ca.txt cs.txt\
+ cy.txt da.txt de.txt el.txt en.txt\
+ eo.txt es.txt et.txt fa.txt fa_AF.txt\
+ fi.txt fil.txt fo.txt fr.txt fr_BE.txt\
+ fr_CH.txt ga.txt he.txt hi.txt hr.txt\
+ hu.txt hy.txt id.txt is.txt it.txt\
+ ja.txt ka.txt kl.txt ko.txt lt.txt\
+ lv.txt mk.txt ms.txt mt.txt nb.txt\
+ nl.txt nn.txt pl.txt pt.txt pt_PT.txt\
+ ro.txt ru.txt sk.txt sl.txt sq.txt\
+ sr.txt sr_Latn.txt sv.txt ta.txt th.txt\
+ tr.txt uk.txt vi.txt zh.txt zh_Hant.txt
 
