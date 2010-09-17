6a80c4cc912375ab1e27282e40c5029eba6d02cd
Michael Ow <mow@svn.icu-project.org>
2010-09-17 21:25:56 +0000

    Update ambiguous converter list.
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/7839
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@28642 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/ucnv.c
+++ source/common/ucnv.c
@@ -1,7 +1,7 @@
 /*
 ******************************************************************************
 *
-*   Copyright (C) 1998-2008, International Business Machines
+*   Copyright (C) 1998-2010, International Business Machines
 *   Corporation and others.  All Rights Reserved.
 *
 ******************************************************************************
@@ -51,13 +51,15 @@ static const UAmbiguousConverter ambiguousConverters[]={
     { "ibm-943_P130-1999", 0xa5 },
     { "ibm-946_P100-1995", 0xa5 },
     { "ibm-33722_P120-1999", 0xa5 },
+    { "ibm-1041_P100-1995", 0xa5 },
     /*{ "ibm-54191_P100-2006", 0xa5 },*/
     /*{ "ibm-62383_P100-2007", 0xa5 },*/
     /*{ "ibm-891_P100-1995", 0x20a9 },*/
     { "ibm-944_P100-1995", 0x20a9 },
     { "ibm-949_P110-1999", 0x20a9 },
     { "ibm-1363_P110-1997", 0x20a9 },
-    { "ISO_2022,locale=ko,version=0", 0x20a9 }
+    { "ISO_2022,locale=ko,version=0", 0x20a9 },
+    { "ibm-1088_P100-1995", 0x20a9 }
 };
 
 /*Calls through createConverter */
