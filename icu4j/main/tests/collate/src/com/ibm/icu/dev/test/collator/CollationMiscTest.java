42bb7ee26ba642bf3f61bbebbf11caf3472d20b7
Mark Davis <mark@macchiato.com>
2010-10-13 06:38:17 +0000

    ICU-7264 add timebombs temporarily until collation files are updated.
    X-SVN-Rev: 28808

--- icu4j/main/tests/collate/src/com/ibm/icu/dev/test/collator/CollationMiscTest.java
+++ icu4j/main/tests/collate/src/com/ibm/icu/dev/test/collator/CollationMiscTest.java
@@ -66,6 +66,10 @@ public class CollationMiscTest extends TestFmwk {
 
     public void TestComposeDecompose()
     {
+        if(skipIfBeforeICU(4, 5, 2)) {
+            return;
+        }
+        
         Tester t[] = new Tester[0x30000];
         t[0] = new Tester();
         logln("Testing UCA extensively\n");
