77afd6694b41796456c22b28d4a6f6eb8867b679
Yoshito Umaoka <y.umaoka@gmail.com>
2010-10-20 06:19:12 +0000

    [ICU-7518] Workaround for 24:00 on a transition day problem. Use 23:59:59.999 as the transition time. iCal VTIMEZONE is not capable to handle such rule. Also a minor bug fix in BasicTimeZone.hasEquivalentTransitions which was found while updating the test case. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/28879 Former-commit-id: 98611d66f12385db1533ba2b671f1484249d3f66

--- icu4j/main/classes/core/src/com/ibm/icu/util/VTimeZone.java
+++ icu4j/main/classes/core/src/com/ibm/icu/util/VTimeZone.java
@@ -1624,6 +1624,18 @@ public class VTimeZone extends BasicTimeZone {
     private static void writeFinalRule(Writer writer, boolean isDst, AnnualTimeZoneRule rule,
             int fromRawOffset, int fromDSTSavings, long startTime) throws IOException{
         DateTimeRule dtrule = toWallTimeRule(rule.getRule(), fromRawOffset, fromDSTSavings);
+
+        // If the rule's mills in a day is out of range, adjust start time.
+        // Olson tzdata supports 24:00 of a day, but VTIMEZONE does not.
+        // See ticket#7008/#7518
+
+        int timeInDay = dtrule.getRuleMillisInDay();
+        if (timeInDay < 0) {
+            startTime = startTime + (0 - timeInDay);
+        } else if (timeInDay >= Grego.MILLIS_PER_DAY) {
+            startTime = startTime - (timeInDay - (Grego.MILLIS_PER_DAY - 1));
+        }
+
         int toOffset = rule.getRawOffset() + rule.getDSTSavings();
         switch (dtrule.getDateRuleType()) {
         case DateTimeRule.DOM:
