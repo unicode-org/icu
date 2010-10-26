92fb54e820b2927d01e280313bb3a4ab9ac53ba6
Peter Edberg <pedberg@svn.icu-project.org>
2010-10-26 03:34:07 +0000

    CLDR 1.9 integration pass 4, ur collation & correct cldr version
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/8046
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@28912 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/data/curr/resfiles.mk
+++ source/data/curr/resfiles.mk
@@ -1,6 +1,6 @@
 # *   Copyright (C) 1998-2010, International Business Machines
 # *   Corporation and others.  All Rights Reserved.
-CURR_CLDR_VERSION = 1.8.1
+CURR_CLDR_VERSION = 1.9
 # A list of txt's to build
 # Note:
 #
