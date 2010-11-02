211167a8f3d1691df8f2d35327e30e8cd71c2cca
Steven R. Loomis <srl@icu-project.org>
2010-11-02 01:51:32 +0000

    [ICU-8109] have icucheck.bat print a summary of pass/fail tests and return an overall exit status. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/28974 Former-commit-id: e819917735df74d2114715d812691dabc1bafe24

--- icu4c/source/allinone/icucheck.bat
+++ icu4c/source/allinone/icucheck.bat
@@ -27,24 +27,74 @@ set PATH=%ICU_BINDIR%;%PATH%
 echo testing ICU in %ICU_ICUDIR%  arch=%ICU_ARCH% type=%ICU_DBRL%
 pushd %ICU_ICUDIR%
 
-echo "====" icuinfo
-%ICU_ICUDIR%\source\tools\icuinfo\%ICU_ARCH%\%ICU_DBRL%\icuinfo.exe %ICUINFO_OPTS%
+@rem factor these out
+set ICUINFO_CMD=%ICU_ICUDIR%\source\tools\icuinfo\%ICU_ARCH%\%ICU_DBRL%\icuinfo.exe
+set INTLTEST_CMD=%ICU_ICUDIR%\source\test\intltest\%ICU_ARCH%\%ICU_DBRL%\intltest.exe
+set IOTEST_CMD=%ICU_ICUDIR%\source\test\iotest\%ICU_ARCH%\%ICU_DBRL%\iotest.exe
+set CINTLTST_CMD=%ICU_ICUDIR%\source\test\cintltst\%ICU_ARCH%\%ICU_DBRL%\cintltst.exe
+set LETEST_CMD=%ICU_ICUDIR%\source\test\letest\%ICU_ARCH%\%ICU_DBRL%\letest.exe
 
-echo "====" intltest
-cd %ICU_ICUDIR%\source\test\intltest
-%ICU_ICUDIR%\source\test\intltest\%ICU_ARCH%\%ICU_DBRL%\intltest.exe %INTLTEST_OPTS%
+set ICUFAILED=
+set ICURUN=
+set ICUFAILCNT=0
 
-echo "====" iotest
-cd %ICU_ICUDIR%\source\test\iotest
-%ICU_ICUDIR%\source\test\iotest\%ICU_ARCH%\%ICU_DBRL%\iotest.exe %IOTEST_OPTS%
+@echo on
 
-echo "====" cintltst
-cd %ICU_ICUDIR%\source\test\cintltst
-%ICU_ICUDIR%\source\test\cintltst\%ICU_ARCH%\%ICU_DBRL%\cintltst.exe %CINTLTST_OPTS%
+@set THT=icuinfo
+@echo ==== %THT% =========================================================================
+%ICUINFO_CMD% %ICUINFO_OPTS%
 
-echo "====" letest
-cd %ICU_ICUDIR%\source\test\letest
-%ICU_ICUDIR%\source\test\letest\%ICU_ARCH%\%ICU_DBRL%\letest.exe %LETEST_OPTS%
+@IF NOT ERRORLEVEL 1 GOTO OK_%THT%
+@set ICUFAILED=%ICUFAILED% %THT%
+@set ICUFAILCNT=1
+:OK_icuinfo
+@set ICURUN=%ICURUN% %THT%
+
+@set THT=intltest
+@echo ==== %THT% =========================================================================
+@cd %ICU_ICUDIR%\source\test\intltest
+%INTLTEST_CMD% %INTLTEST_OPTS%
+
+@IF NOT ERRORLEVEL 1 GOTO OK_%THT%
+@set ICUFAILED=%ICUFAILED% %THT%
+@set ICUFAILCNT=1
+:OK_intltest
+@set ICURUN=%ICURUN% %THT%
+
+@set THT=iotest
+@echo ==== %THT% =========================================================================
+@cd %ICU_ICUDIR%\source\test\iotest
+%IOTEST_CMD% %IOTEST_OPTS%
+
+@IF NOT ERRORLEVEL 1 GOTO OK_%THT%
+@set ICUFAILED=%ICUFAILED% %THT%
+@set ICUFAILCNT=1
+:OK_IOTEST
+@set ICURUN=%ICURUN% %THT%
+
+@set THT=cintltst
+@echo ==== %THT% =========================================================================
+@cd %ICU_ICUDIR%\source\test\cintltst
+%CINTLTST_CMD% %CINTLTST_OPTS%
+
+@IF NOT ERRORLEVEL 1 GOTO OK_%THT%
+@set ICUFAILED=%ICUFAILED% %THT%
+@set ICUFAILCNT=1
+:OK_cintltst
+@set ICURUN=%ICURUN% %THT%
+
+@set THT=letest
+@echo ==== %THT% =========================================================================
+@cd %ICU_ICUDIR%\source\test\letest
+%LETST_CMD% %LETEST_OPTS%
+
+@IF NOT ERRORLEVEL 1 GOTO OK_%THT%
+@set ICUFAILED=%ICUFAILED% %THT%
+@set ICUFAILCNT=1
+:OK_letest
+@set ICURUN=%ICURUN% %THT%
+
+@echo off
 
 REM clean up
 set PATH=%ICU_OPATH%
@@ -52,3 +102,21 @@ REM unset ICU_OPATH
 popd
 
 @REM done
+
+echo -
+echo -
+echo -
+echo ============================================================
+echo Summary:
+echo -
+echo Tests Run    : %ICURUN%
+
+if %ICUFAILCNT% == 0 (
+	echo " - All Passed!"
+	exit /b 0
+)
+echo Failing Tests: %ICUFAILED%
+echo -
+echo FAILED!
+
+exit /b 1
