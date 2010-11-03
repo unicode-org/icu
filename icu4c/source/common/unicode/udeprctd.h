488256ca631ad8bfb9ccd238ee31810fab26c450
Peter Edberg <pedberg@unicode.org>
2010-11-03 05:46:29 +0000

    ICU-8121 Update udeprctd.h, udraft.h, uintrnal.h, usystem.h for ICU 4.6
    X-SVN-Rev: 28994

--- icu4c/source/common/unicode/udeprctd.h
+++ icu4c/source/common/unicode/udeprctd.h
@@ -34,15 +34,15 @@
 #        define utrans_open utrans_open_DEPRECATED_API_DO_NOT_USE
 #        define utrans_unregister utrans_unregister_DEPRECATED_API_DO_NOT_USE
 #    else
-#        define ucol_getContractions_4_5 ucol_getContractions_DEPRECATED_API_DO_NOT_USE
-#        define ucol_getLocale_4_5 ucol_getLocale_DEPRECATED_API_DO_NOT_USE
-#        define ures_countArrayItems_4_5 ures_countArrayItems_DEPRECATED_API_DO_NOT_USE
-#        define ures_getLocale_4_5 ures_getLocale_DEPRECATED_API_DO_NOT_USE
-#        define ures_getVersionNumber_4_5 ures_getVersionNumber_DEPRECATED_API_DO_NOT_USE
-#        define utrans_getAvailableID_4_5 utrans_getAvailableID_DEPRECATED_API_DO_NOT_USE
-#        define utrans_getID_4_5 utrans_getID_DEPRECATED_API_DO_NOT_USE
-#        define utrans_open_4_5 utrans_open_DEPRECATED_API_DO_NOT_USE
-#        define utrans_unregister_4_5 utrans_unregister_DEPRECATED_API_DO_NOT_USE
+#        define ucol_getContractions_4_6 ucol_getContractions_DEPRECATED_API_DO_NOT_USE
+#        define ucol_getLocale_4_6 ucol_getLocale_DEPRECATED_API_DO_NOT_USE
+#        define ures_countArrayItems_4_6 ures_countArrayItems_DEPRECATED_API_DO_NOT_USE
+#        define ures_getLocale_4_6 ures_getLocale_DEPRECATED_API_DO_NOT_USE
+#        define ures_getVersionNumber_4_6 ures_getVersionNumber_DEPRECATED_API_DO_NOT_USE
+#        define utrans_getAvailableID_4_6 utrans_getAvailableID_DEPRECATED_API_DO_NOT_USE
+#        define utrans_getID_4_6 utrans_getID_DEPRECATED_API_DO_NOT_USE
+#        define utrans_open_4_6 utrans_open_DEPRECATED_API_DO_NOT_USE
+#        define utrans_unregister_4_6 utrans_unregister_DEPRECATED_API_DO_NOT_USE
 #    endif /* U_DISABLE_RENAMING */
 
 #endif /* U_HIDE_DEPRECATED_API */
