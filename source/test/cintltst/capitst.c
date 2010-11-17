a54db40e13c1ef65106c898a7f101628c972a45a
Peter Edberg <pedberg@svn.icu-project.org>
2010-11-17 04:30:05 +0000

    temp hack: make genrb exclude unihan collation unless temp option --includeUnihanColl is used
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/8131
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@29062 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/test/cintltst/capitst.c
+++ source/test/cintltst/capitst.c
@@ -2264,6 +2264,7 @@ static void TestDefaultKeyword(void) {
 }
 
 static void TestGetKeywordValuesForLocale(void) {
+#define INCLUDE_UNIHAN_COLLATION 0
 #define PREFERRED_SIZE 16
 #define MAX_NUMBER_OF_KEYWORDS 8
     const char *PREFERRED[PREFERRED_SIZE][MAX_NUMBER_OF_KEYWORDS+1] = {
@@ -2272,19 +2273,32 @@ static void TestGetKeywordValuesForLocale(void) {
             { "en_029",         "standard", "ducet", "search", NULL, NULL, NULL, NULL, NULL },
             { "de_DE",          "standard", "phonebook", "search", "ducet", NULL, NULL, NULL, NULL },
             { "de_Latn_DE",     "standard", "phonebook", "search", "ducet", NULL, NULL, NULL, NULL },
+#if INCLUDE_UNIHAN_COLLATION
             { "zh",             "pinyin", "big5han", "gb2312han", "standard", "stroke", "unihan", "ducet", "search" },
             { "zh_Hans",        "pinyin", "big5han", "gb2312han", "standard", "stroke", "unihan", "ducet", "search" },
             { "zh_CN",          "pinyin", "big5han", "gb2312han", "standard", "stroke", "unihan", "ducet", "search" },
             { "zh_Hant",        "stroke", "big5han", "gb2312han", "pinyin", "standard", "unihan", "ducet", "search" },
             { "zh_TW",          "stroke", "big5han", "gb2312han", "pinyin", "standard", "unihan", "ducet", "search" },
             { "zh__PINYIN",     "pinyin", "big5han", "gb2312han", "standard", "stroke", "unihan", "ducet", "search" },
+#else
+            { "zh",             "pinyin", "big5han", "gb2312han", "standard", "stroke", "ducet", "search", NULL },
+            { "zh_Hans",        "pinyin", "big5han", "gb2312han", "standard", "stroke", "ducet", "search", NULL },
+            { "zh_CN",          "pinyin", "big5han", "gb2312han", "standard", "stroke", "ducet", "search", NULL },
+            { "zh_Hant",        "stroke", "big5han", "gb2312han", "pinyin", "standard", "ducet", "search", NULL },
+            { "zh_TW",          "stroke", "big5han", "gb2312han", "pinyin", "standard", "ducet", "search", NULL },
+            { "zh__PINYIN",     "pinyin", "big5han", "gb2312han", "standard", "stroke", "ducet", "search", NULL },
+#endif
             { "es_ES",          "standard", "search", "traditional", "ducet", NULL, NULL, NULL, NULL },
             { "es__TRADITIONAL","traditional", "search", "standard", "ducet", NULL, NULL, NULL, NULL },
             { "und@collation=phonebook",    "standard", "ducet", "search", NULL, NULL, NULL, NULL, NULL },
             { "de_DE@collation=big5han",    "standard", "phonebook", "search", "ducet", NULL, NULL, NULL, NULL },
             { "zzz@collation=xxx",          "standard", "ducet", "search", NULL, NULL, NULL, NULL, NULL }
     };
+#if INCLUDE_UNIHAN_COLLATION
     const int32_t expectedLength[PREFERRED_SIZE] = { 3, 3, 3, 4, 4, 8, 8, 8, 8, 8, 8, 4, 4, 3, 4, 3 };
+#else
+    const int32_t expectedLength[PREFERRED_SIZE] = { 3, 3, 3, 4, 4, 7, 7, 7, 7, 7, 7, 4, 4, 3, 4, 3 };
+#endif
 
     UErrorCode status = U_ZERO_ERROR;
     UEnumeration *keywordValues = NULL;
