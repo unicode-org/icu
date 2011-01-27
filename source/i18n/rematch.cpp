3a4717f23fee6158bf288d187f772e19c940158f
Andy Heninger <andy.heninger@gmail.com>
2011-01-27 01:12:12 +0000

    Regular Expression refreshInputText added.
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/7795
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@29363 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/i18n/rematch.cpp
+++ source/i18n/rematch.cpp
@@ -1,6 +1,6 @@
 /*
 **************************************************************************
-*   Copyright (C) 2002-2010 International Business Machines Corporation  *
+*   Copyright (C) 2002-2011 International Business Machines Corporation  *
 *   and others. All rights reserved.                                     *
 **************************************************************************
 */
@@ -1963,6 +1963,41 @@ RegexMatcher &RegexMatcher::reset(int64_t position, UErrorCode &status) {
 }
 
 
+//--------------------------------------------------------------------------------
+//
+//    refresh
+//
+//--------------------------------------------------------------------------------
+RegexMatcher &RegexMatcher::refreshInputText(UText *input, UErrorCode &status) {
+    if (U_FAILURE(status)) {
+        return *this;
+    }
+    if (input == NULL) {
+        status = U_ILLEGAL_ARGUMENT_ERROR;
+        return *this;
+    }
+    if (utext_nativeLength(fInputText) != utext_nativeLength(input)) {
+        status = U_ILLEGAL_ARGUMENT_ERROR;
+        return *this;
+    }
+    int64_t  pos = utext_getNativeIndex(fInputText);
+    //  Shallow read-only clone of the new UText into the existing input UText
+    fInputText = utext_clone(fInputText, input, FALSE, TRUE, &status);
+    if (U_FAILURE(status)) {
+        return *this;
+    }
+    utext_setNativeIndex(fInputText, pos);
+
+    if (fAltInputText != NULL) {
+        pos = utext_getNativeIndex(fAltInputText);
+        fAltInputText = utext_clone(fAltInputText, input, FALSE, TRUE, &status);
+        if (U_FAILURE(status)) {
+            return *this;
+        }
+        utext_setNativeIndex(fAltInputText, pos);
+    }
+    return *this;
+}
 
 
 
