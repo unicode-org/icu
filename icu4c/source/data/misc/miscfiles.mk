7b6a0c00c235a54903026072868d8dc025b24c98
John Emmons <emmo@us.ibm.com>
2011-04-11 16:22:18 +0000

    [ICU-8347] Data changes required for territory containment APIs X-Commit-URL: https://ssl.icu-project.org/trac/changeset/29768 Former-commit-id: 4906d6fdd7aa8115739bcea994b33781779824ce

--- icu4c/source/data/misc/miscfiles.mk
+++ icu4c/source/data/misc/miscfiles.mk
@@ -1,4 +1,4 @@
-# *   Copyright (C) 2003-2010, International Business Machines
+# *   Copyright (C) 2003-2011, International Business Machines
 # *   Corporation and others.  All Rights Reserved.
 # A list of txt's to build
 # Note: 
@@ -23,4 +23,4 @@
 #
 
 MISC_SOURCE = \
-zoneinfo64.txt supplementalData.txt likelySubtags.txt plurals.txt numberingSystems.txt icuver.txt icustd.txt metaZones.txt windowsZones.txt keyTypeData.txt timezoneTypes.txt
+zoneinfo64.txt supplementalData.txt likelySubtags.txt plurals.txt numberingSystems.txt icuver.txt icustd.txt metadata.txt metaZones.txt windowsZones.txt keyTypeData.txt timezoneTypes.txt
