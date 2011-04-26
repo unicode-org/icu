3962c68aa2ccf4e77c80d83319756fbb8f52b486
Markus Scherer <markus.icu@gmail.com>
2011-04-26 04:01:09 +0000

    ICU-8319 do not use UNICODE_STRING_SIMPLE with a multi-line string literal
    X-SVN-Rev: 29896

--- icu4c/source/test/intltest/ucdtest.cpp
+++ icu4c/source/test/intltest/ucdtest.cpp
@@ -380,11 +380,11 @@ void UnicodeTest::TestPatternProperties() {
     IcuTestErrorCode errorCode(*this, "TestPatternProperties()");
     UnicodeSet syn_pp;
     UnicodeSet syn_prop(UNICODE_STRING_SIMPLE("[:Pattern_Syntax:]"), errorCode);
-    UnicodeSet syn_list(UNICODE_STRING_SIMPLE(
+    UnicodeSet syn_list(
         "[!-/\\:-@\\[-\\^`\\{-~"
         "\\u00A1-\\u00A7\\u00A9\\u00AB\\u00AC\\u00AE\\u00B0\\u00B1\\u00B6\\u00BB\\u00BF\\u00D7\\u00F7"
         "\\u2010-\\u2027\\u2030-\\u203E\\u2041-\\u2053\\u2055-\\u205E\\u2190-\\u245F\\u2500-\\u2775"
-        "\\u2794-\\u2BFF\\u2E00-\\u2E7F\\u3001-\\u3003\\u3008-\\u3020\\u3030\\uFD3E\\uFD3F\\uFE45\\uFE46]"), errorCode);
+        "\\u2794-\\u2BFF\\u2E00-\\u2E7F\\u3001-\\u3003\\u3008-\\u3020\\u3030\\uFD3E\\uFD3F\\uFE45\\uFE46]", errorCode);
     UnicodeSet ws_pp;
     UnicodeSet ws_prop(UNICODE_STRING_SIMPLE("[:Pattern_White_Space:]"), errorCode);
     UnicodeSet ws_list(UNICODE_STRING_SIMPLE("[\\u0009-\\u000D\\ \\u0085\\u200E\\u200F\\u2028\\u2029]"), errorCode);
