9eaf0fc54b269c77a3f40ea36d3bbf8c32e2f916
Markus Scherer <markus.icu@gmail.com>
2011-07-03 04:07:02 +0000

    move some collation file data structure definitions from i18n/ucol_imp.h to common/ucol_data.h (new file)
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/8619
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@30271 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/i18n/ucol_imp.h
+++ source/i18n/ucol_imp.h
@@ -46,6 +46,7 @@
 #include "unicode/unistr.h"
 #endif
 #include "unicode/ucol.h"
+#include "ucol_data.h"
 #include "utrie.h"
 #include "cmemory.h"
 
@@ -171,8 +172,7 @@
  * Header is followed by the table and continuation table.
 */
 
-/* let us know whether reserved fields are reset to zero or junked */
-#define UCOL_HEADER_MAGIC 0x20030618
+/* definition of UCOL_HEADER_MAGIC moved to common/ucol_data.h */
 
 /* UDataInfo for UCA mapping table */
 /* dataFormat="UCol"            */
@@ -855,46 +855,7 @@ typedef struct {
   uint32_t UCA_PRIMARY_SPECIAL_MAX; /*0xF0000000*/
 } UCAConstants;
 
-typedef struct {
-      int32_t size;
-      /* all the offsets are in bytes */
-      /* to get the address add to the header address and cast properly */
-      uint32_t options; /* these are the default options for the collator */
-      uint32_t UCAConsts; /* structure which holds values for indirect positioning and implicit ranges */
-      uint32_t contractionUCACombos;        /* this one is needed only for UCA, to copy the appropriate contractions */
-      uint32_t magic;            /* magic number - lets us know whether reserved data is reset or junked */
-      uint32_t mappingPosition;  /* const uint8_t *mappingPosition; */
-      uint32_t expansion;        /* uint32_t *expansion;            */
-      uint32_t contractionIndex; /* UChar *contractionIndex;        */
-      uint32_t contractionCEs;   /* uint32_t *contractionCEs;       */
-      uint32_t contractionSize;  /* needed for various closures */
-      /*int32_t latinOneMapping;*/ /* this is now handled in the trie itself *//* fast track to latin1 chars      */
-
-      uint32_t endExpansionCE;      /* array of last collation element in
-                                       expansion */
-      uint32_t expansionCESize;     /* array of maximum expansion size
-                                       corresponding to the expansion
-                                       collation elements with last element
-                                       in endExpansionCE*/
-      int32_t  endExpansionCECount; /* size of endExpansionCE */
-      uint32_t unsafeCP;            /* hash table of unsafe code points */
-      uint32_t contrEndCP;          /* hash table of final code points  */
-                                    /*   in contractions.               */
-
-      int32_t contractionUCACombosSize;     /* number of UCA contraction items. */
-                                            /*Length is contractionUCACombosSize*contractionUCACombosWidth*sizeof(UChar) */
-      UBool jamoSpecial;                    /* is jamoSpecial */
-      UBool isBigEndian;                    /* is this data big endian? from the UDataInfo header*/
-      uint8_t charSetFamily;                /* what is the charset family of this data from the UDataInfo header*/
-      uint8_t contractionUCACombosWidth;    /* width of UCA combos field */
-      UVersionInfo version;
-      UVersionInfo UCAVersion;              /* version of the UCA, read from file */
-      UVersionInfo UCDVersion;              /* UCD version, obtained by u_getUnicodeVersion */
-      UVersionInfo formatVersion;           /* format version from the UDataInfo header */
-      uint32_t scriptToLeadByte;            /* offset to script to lead collation byte mapping data */
-      uint32_t leadByteToScript;            /* offset to lead collation byte to script mapping data */
-      uint8_t reserved[76];                 /* for future use */
-} UCATableHeader;
+/* definition of UCATableHeader moved to common/ucol_data.h */
 
 #define U_UNKNOWN_STATE 0
 #define U_COLLATOR_STATE 0x01
@@ -965,15 +926,7 @@ typedef struct {
 
 U_CDECL_BEGIN
 
-typedef struct {
-  uint32_t byteSize;
-  uint32_t tableSize;
-  uint32_t contsSize;
-  uint32_t table;
-  uint32_t conts;
-  UVersionInfo UCAVersion;              /* version of the UCA, read from file */
-  uint8_t padding[8];
-} InverseUCATableHeader;
+/* definition of InverseUCATableHeader moved to common/ucol_data.h */
 
 typedef void U_CALLCONV
 ResourceCleaner(UCollator *coll);
