4faf18b0435f4d6448ba849c8f837c3b1c463556
Michael Ow <mow@svn.icu-project.org>
2011-07-08 17:21:13 +0000

    Skip the current source char when searching for the next escape character
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/8660
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@30296 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/ucnv_ct.c
+++ source/common/ucnv_ct.c
@@ -180,7 +180,7 @@ _CompoundTextgetName(const UConverter* cnv);
 static int32_t findNextEsc(const char *source, const char *sourceLimit) {
     int32_t length = sourceLimit - source;
     int32_t i;
-    for (i = 0; i < length; i++) {
+    for (i = 1; i < length; i++) {
         if (*(source + i) == 0x1B) {
             return i;
         }
