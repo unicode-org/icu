5d66666ebe7b6361638827c6ecc62147cb087cae
Michael Ow <mow@svn.icu-project.org>
2011-08-23 17:20:22 +0000

    Promote approved data version API to draft 49 in ICU4C
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/7365
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@30570 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/unicode/icudataver.h
+++ source/common/unicode/icudataver.h
@@ -14,36 +14,14 @@
 
 #ifndef U_HIDE_INTERNAL_API
 /**
- * @internal ICU 4.4
+ * @draft ICU 49
  */
 #define U_ICU_VERSION_BUNDLE "icuver"
 
 /**
- * @internal ICU 4.4
- */
-#define U_ICU_STD_BUNDLE "icustd"
-
-/**
- * @internal ICU 4.4
+ * @draft ICU 49
  */
 #define U_ICU_DATA_KEY "DataVersion"
-
-/**
- * This function loads up icuver and compares the data version to the wired-in U_ICU_DATA_VERSION.
- * If icuver shows something less than U_ICU_DATA_VERSION it returns TRUE, else FALSE. The version
- * found will be returned in the first fillin parameter (if non-null), and *isModified will be set
- * to TRUE if "icustd" is NOT found. Thus, if the data has been repackaged or modified, "icustd"
- * (standard ICU) will be missing, and the function will alert the caller that the data is not standard.
- * 
- * @param dataVersionFillin icuver data version information to be filled in if not-null
- * @param isModifiedFillin if the data is not standard if not-null
- * @param status stores the error code from the calls to resource bundle
- *
- * @return TRUE if U_ICU_DATA_VERSION is newer than icuver, else FALSE
- * 
- * @internal ICU 4.4
- */
-U_INTERNAL UBool U_EXPORT2 u_isDataOlder(UVersionInfo dataVersionFillin, UBool *isModifiedFillin, UErrorCode *status);
 
 /**
  * Retrieves the data version from icuver and stores it in dataVersionFillin.
@@ -51,7 +29,7 @@ U_INTERNAL UBool U_EXPORT2 u_isDataOlder(UVersionInfo dataVersionFillin, UBool *
  * @param dataVersionFillin icuver data version information to be filled in if not-null
  * @param status stores the error code from the calls to resource bundle
  * 
- * @internal ICU 4.4
+ * @draft ICU 49
  */
 U_INTERNAL void U_EXPORT2 u_getDataVersion(UVersionInfo dataVersionFillin, UErrorCode *status);
 #endif  /* U_HIDE_INTERNAL_API */
