f79327051583b7759d159f95d8688b10e1180533
Steven R. Loomis <srl@icu-project.org>
2011-09-23 00:58:45 +0000

    ICU-8823 bump version to 4.8.1.1 (C)
    X-SVN-Rev: 30707

--- source/common/unicode/uvernum.h
+++ source/common/unicode/uvernum.h
@@ -42,7 +42,7 @@
  *  @stable ICU 2.4
  */
 #define U_COPYRIGHT_STRING \
-  " Copyright (C) 2010, International Business Machines Corporation and others. All Rights Reserved. "
+  " Copyright (C) 2011, International Business Machines Corporation and others. All Rights Reserved. "
 
 /** The current ICU major version as an integer. 
  *  This value will change in the subsequent releases of ICU
@@ -67,7 +67,7 @@
  *  @stable ICU 4.0
  */
 #ifndef U_ICU_VERSION_BUILDLEVEL_NUM
-#define U_ICU_VERSION_BUILDLEVEL_NUM 0
+#define U_ICU_VERSION_BUILDLEVEL_NUM 1
 #endif
 
 /** Glued version suffix for renamers 
@@ -91,7 +91,7 @@
  *  This value will change in the subsequent releases of ICU
  *  @stable ICU 2.4
  */
-#define U_ICU_VERSION "4.8.1"
+#define U_ICU_VERSION "4.8.1.1"
 
 /** The current ICU library major/minor version as a string without dots, for library name suffixes. 
  *  This value will change in the subsequent releases of ICU
