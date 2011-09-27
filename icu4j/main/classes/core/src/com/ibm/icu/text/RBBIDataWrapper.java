d96782f6612e4e3a4cbc1860888618cfeaae16d3
Abhinav Gupta <mail@abhinavg.net>
2011-09-27 18:14:53 +0000

    ICU-8268 update copyrights of previously touched files
    X-SVN-Rev: 30717

--- icu4j/main/classes/core/src/com/ibm/icu/text/RBBIDataWrapper.java
+++ icu4j/main/classes/core/src/com/ibm/icu/text/RBBIDataWrapper.java
@@ -1,6 +1,6 @@
 /**
 *******************************************************************************
-* Copyright (C) 1996-2010, International Business Machines Corporation and    *
+* Copyright (C) 1996-2011, International Business Machines Corporation and    *
 * others. All Rights Reserved.                                                *
 *******************************************************************************
 */
