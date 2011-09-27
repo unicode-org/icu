93ff036a1e991eafbc19a6918fdcdd0547912dc7
Abhinav Gupta <mail@abhinavg.net>
2011-09-27 19:50:07 +0000

    [ICU-8268] infinite recursive loop fixed X-Commit-URL: https://ssl.icu-project.org/trac/changeset/30724 Former-commit-id: 3b303ddf67805cc8dde50cfc9bc35613c7f92a78

--- icu4j/tools/misc/src/com/ibm/icu/dev/tool/ime/translit/TransliteratorInputMethod.java
+++ icu4j/tools/misc/src/com/ibm/icu/dev/tool/ime/translit/TransliteratorInputMethod.java
@@ -1,6 +1,6 @@
 /*
  *******************************************************************************
- * Copyright (C) 2004-2010, International Business Machines Corporation and    *
+ * Copyright (C) 2004-2011, International Business Machines Corporation and    *
  * others. All Rights Reserved.                                                *
  *******************************************************************************
  */
@@ -713,6 +713,6 @@ class LabelComparator implements Comparator {
     }
 
     public boolean equals(Object obj1) {
-        return this.equals(obj1);
+        return compare(this, obj1) == 0;
     }
 }
