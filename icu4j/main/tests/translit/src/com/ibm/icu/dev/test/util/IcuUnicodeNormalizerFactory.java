11bdbe4463a870e1e6e73425cd0a1aa8d9dd2d41
Abhinav Gupta <mail@abhinavg.net>
2011-09-28 21:16:24 +0000

    [ICU-8856] text file line endings fixed for icu4j X-Commit-URL: https://ssl.icu-project.org/trac/changeset/30741 Former-commit-id: 14a1409e709ad6953aeca1dedf27e2519290b01a

--- icu4j/main/tests/translit/src/com/ibm/icu/dev/test/util/IcuUnicodeNormalizerFactory.java
+++ icu4j/main/tests/translit/src/com/ibm/icu/dev/test/util/IcuUnicodeNormalizerFactory.java
@@ -1,1 +1,54 @@
-/* ******************************************************************************* * Copyright (C) 2011, International Business Machines Corporation and         * * others. All Rights Reserved.                                                * ******************************************************************************* */package com.ibm.icu.dev.test.util;import com.ibm.icu.dev.test.util.UnicodeTransform.Type;import com.ibm.icu.lang.UCharacter;import com.ibm.icu.text.Normalizer2;import com.ibm.icu.text.Normalizer2.Mode;/** * @author markdavis * */public class IcuUnicodeNormalizerFactory implements UnicodeTransform.Factory {    public UnicodeTransform getInstance(Type type) {        switch (type) {        case NFC: case NFKC:            return new IcuUnicodeNormalizer(Normalizer2.getInstance(null, type.toString(), Mode.COMPOSE));        case NFD: case NFKD:            return new IcuUnicodeNormalizer(Normalizer2.getInstance(null, type == Type.NFD ? "NFC" : "NFKC", Mode.DECOMPOSE));        case CASEFOLD:            return new CaseFolder();        default:            throw new IllegalArgumentException();        }    }    private static class CaseFolder extends UnicodeTransform {        @Override        public String transform(String source) {            return UCharacter.foldCase(source.toString(), true);        }    }    private static class IcuUnicodeNormalizer extends UnicodeTransform {        private Normalizer2 normalizer;        private IcuUnicodeNormalizer(Normalizer2 normalizer) {            this.normalizer = normalizer;        }        public String transform(String src) {            return normalizer.normalize(src);        }        public boolean isTransformed(String s) {            return normalizer.isNormalized(s);        }    }}
+/* *******************************************************************************
+ * Copyright (C) 2011, International Business Machines Corporation and         *
+ * others. All Rights Reserved.                                                *
+ *******************************************************************************
+ */
+package com.ibm.icu.dev.test.util;
+
+import com.ibm.icu.dev.test.util.UnicodeTransform.Type;
+import com.ibm.icu.lang.UCharacter;
+import com.ibm.icu.text.Normalizer2;
+import com.ibm.icu.text.Normalizer2.Mode;
+
+/**
+ * @author markdavis
+ *
+ */
+public class IcuUnicodeNormalizerFactory implements UnicodeTransform.Factory {
+
+    public UnicodeTransform getInstance(Type type) {
+        switch (type) {
+        case NFC: case NFKC:
+            return new IcuUnicodeNormalizer(Normalizer2.getInstance(null, type.toString(), Mode.COMPOSE));
+        case NFD: case NFKD:
+            return new IcuUnicodeNormalizer(Normalizer2.getInstance(null, type == Type.NFD ? "NFC" : "NFKC", Mode.DECOMPOSE));
+        case CASEFOLD:
+            return new CaseFolder();
+        default:
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static class CaseFolder extends UnicodeTransform {
+        @Override
+        public String transform(String source) {
+            return UCharacter.foldCase(source.toString(), true);
+        }
+    }
+
+    private static class IcuUnicodeNormalizer extends UnicodeTransform {
+        private Normalizer2 normalizer;
+
+        private IcuUnicodeNormalizer(Normalizer2 normalizer) {
+            this.normalizer = normalizer;
+        }
+
+        public String transform(String src) {
+            return normalizer.normalize(src);
+        }
+
+        public boolean isTransformed(String s) {
+            return normalizer.isNormalized(s);
+        }
+    }
+}
