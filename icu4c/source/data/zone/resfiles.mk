867185c5c725e4baad1abdf9a6a0af6eea7954fb
John Emmons <emmo@us.ibm.com>
2011-09-29 21:52:25 +0000

    ICU-8846 Merge CLDRv21M1 data into ICU
    X-SVN-Rev: 30755

--- icu4c/source/data/zone/resfiles.mk
+++ icu4c/source/data/zone/resfiles.mk
@@ -56,36 +56,36 @@ ZONE_SOURCE = af.txt agq.txt ak.txt am.txt\
  cs.txt cy.txt da.txt dav.txt de.txt\
  dje.txt dua.txt dyo.txt ebu.txt ee.txt\
  el.txt en.txt en_AU.txt en_CA.txt en_GB.txt\
- en_GU.txt en_HK.txt en_IN.txt en_NZ.txt en_PK.txt\
- en_SG.txt en_ZA.txt en_ZW.txt eo.txt es.txt\
- es_419.txt es_AR.txt et.txt eu.txt ewo.txt\
- fa.txt ff.txt fi.txt fil.txt fo.txt\
- fr.txt fr_CA.txt ga.txt gl.txt gsw.txt\
- gu.txt guz.txt gv.txt ha.txt ha_Latn.txt\
- haw.txt he.txt hi.txt hr.txt hu.txt\
- hy.txt id.txt ig.txt ii.txt is.txt\
- it.txt ja.txt jmc.txt ka.txt kab.txt\
- kam.txt kde.txt kea.txt khq.txt ki.txt\
- kk.txt kk_Cyrl.txt kl.txt kln.txt km.txt\
- kn.txt ko.txt kok.txt ksb.txt ksf.txt\
- kw.txt lag.txt lg.txt ln.txt lt.txt\
- lu.txt luo.txt luy.txt lv.txt mas.txt\
- mer.txt mfe.txt mg.txt mgh.txt mk.txt\
- ml.txt mr.txt ms.txt mt.txt mua.txt\
- my.txt naq.txt nb.txt nd.txt ne.txt\
- nl.txt nmg.txt nn.txt nus.txt nyn.txt\
- om.txt or.txt pa.txt pa_Arab.txt pa_Guru.txt\
- pl.txt ps.txt pt.txt pt_PT.txt rm.txt\
- rn.txt ro.txt rof.txt ru.txt rw.txt\
- rwk.txt saq.txt sbp.txt seh.txt ses.txt\
- sg.txt shi.txt shi_Latn.txt shi_Tfng.txt si.txt\
- sk.txt sl.txt sn.txt so.txt sq.txt\
- sr.txt sr_Cyrl.txt sr_Latn.txt sv.txt sw.txt\
- swc.txt ta.txt te.txt teo.txt th.txt\
- ti.txt to.txt tr.txt twq.txt tzm.txt\
- tzm_Latn.txt uk.txt ur.txt uz.txt uz_Arab.txt\
- uz_Cyrl.txt uz_Latn.txt vai.txt vai_Latn.txt vai_Vaii.txt\
- vi.txt vun.txt xog.txt yav.txt yo.txt\
- zh.txt zh_Hans.txt zh_Hans_HK.txt zh_Hans_MO.txt zh_Hans_SG.txt\
- zh_Hant.txt zh_Hant_HK.txt zu.txt
+ en_GU.txt en_HK.txt en_IE.txt en_IN.txt en_NZ.txt\
+ en_PK.txt en_SG.txt en_ZA.txt en_ZW.txt eo.txt\
+ es.txt es_419.txt es_AR.txt et.txt eu.txt\
+ ewo.txt fa.txt ff.txt fi.txt fil.txt\
+ fo.txt fr.txt fr_CA.txt ga.txt gl.txt\
+ gsw.txt gu.txt guz.txt gv.txt ha.txt\
+ ha_Latn.txt haw.txt he.txt hi.txt hr.txt\
+ hu.txt hy.txt id.txt ig.txt ii.txt\
+ is.txt it.txt ja.txt jmc.txt ka.txt\
+ kab.txt kam.txt kde.txt kea.txt khq.txt\
+ ki.txt kk.txt kk_Cyrl.txt kl.txt kln.txt\
+ km.txt kn.txt ko.txt kok.txt ksb.txt\
+ ksf.txt kw.txt lag.txt lg.txt ln.txt\
+ lt.txt lu.txt luo.txt luy.txt lv.txt\
+ mas.txt mer.txt mfe.txt mg.txt mgh.txt\
+ mk.txt ml.txt mr.txt ms.txt mt.txt\
+ mua.txt my.txt naq.txt nb.txt nd.txt\
+ ne.txt nl.txt nmg.txt nn.txt nus.txt\
+ nyn.txt om.txt or.txt pa.txt pa_Arab.txt\
+ pa_Guru.txt pl.txt ps.txt pt.txt pt_PT.txt\
+ rm.txt rn.txt ro.txt rof.txt ru.txt\
+ rw.txt rwk.txt saq.txt sbp.txt seh.txt\
+ ses.txt sg.txt shi.txt shi_Latn.txt shi_Tfng.txt\
+ si.txt sk.txt sl.txt sn.txt so.txt\
+ sq.txt sr.txt sr_Cyrl.txt sr_Latn.txt sv.txt\
+ sw.txt swc.txt ta.txt te.txt teo.txt\
+ th.txt ti.txt to.txt tr.txt twq.txt\
+ tzm.txt tzm_Latn.txt uk.txt ur.txt uz.txt\
+ uz_Arab.txt uz_Cyrl.txt uz_Latn.txt vai.txt vai_Latn.txt\
+ vai_Vaii.txt vi.txt vun.txt xog.txt yav.txt\
+ yo.txt zh.txt zh_Hans.txt zh_Hans_HK.txt zh_Hans_MO.txt\
+ zh_Hans_SG.txt zh_Hant.txt zh_Hant_HK.txt zu.txt
 
