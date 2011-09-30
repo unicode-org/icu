9fc7e1f9ee6c0c960986191324a1d18fbab0bd7d
Michael Ow <mow@svn.icu-project.org>
2011-09-30 19:45:51 +0000

    Remove unused old test code.
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/7365
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@30762 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/test/cintltst/putiltst.c
+++ source/test/cintltst/putiltst.c
@@ -280,13 +280,6 @@ static void TestVersion(void)
     u_getDataVersion(version, &status);
     if (U_FAILURE(status)) {
         log_data_err("ERROR: Unable to get data version. %s\n", u_errorName(status));
-    } else {
-#if 0
-        u_isDataOlder(version, &isModified, &status);
-        if (U_FAILURE(status)) {
-            log_err("ERROR: Unable to compare data version. %s\n", u_errorName(status));
-        }
-#endif
     }
 }
 
