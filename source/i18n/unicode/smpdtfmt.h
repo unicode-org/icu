d0887a97783e8ac85d5694d3bfa70190fe72a710
Michael Ow <mow@svn.icu-project.org>
2011-12-08 18:27:54 +0000

    Update ICU4C SimpleDateFormat doc
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/8956
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@31069 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/i18n/unicode/smpdtfmt.h
+++ source/i18n/unicode/smpdtfmt.h
@@ -89,11 +89,14 @@ class TimeZoneFormat;
  * a        am/pm marker            (Text)              PM
  * k        hour in day (1~24)      (Number)            24
  * K        hour in am/pm (0~11)    (Number)            0
- * z        time zone               (Time)              Pacific Standard Time
+ * z        time zone               (Text)              PST
+ * zzzz     time zone               (Text)              Pacific Standard Time
  * Z        time zone (RFC 822)     (Number)            -0800
- * v        time zone (generic)     (Text)              Pacific Time
- * V        time zone (abreviation) (Text)              PT
- * VVVV     time zone (location)    (Text)              United States (Los Angeles)
+ * ZZZZ     time zone (RFC 822)     (Text & Number)     GMT-08:00
+ * v        time zone (generic)     (Text)              PT
+ * vvvv     time zone (generic)     (Text)              Pacific Time
+ * V        time zone (abreviation) (Text)              PST
+ * VVVV     time zone (location)    (Text)              United States Time (Los Angeles)
  * g        Julian day              (Number)            2451334
  * A        milliseconds in day     (Number)            69540000
  * q        stand alone quarter     (Text & Number)     Q2 & 02
