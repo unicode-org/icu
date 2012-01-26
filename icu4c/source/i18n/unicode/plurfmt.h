9606c413500a42042f364ec5a7bfab8340b8ca55
Markus Scherer <markus.icu@gmail.com>
2012-01-26 23:05:12 +0000

    ICU-9048 work around xlC compiler bug
    X-SVN-Rev: 31270

--- icu4c/source/i18n/unicode/plurfmt.h
+++ icu4c/source/i18n/unicode/plurfmt.h
@@ -479,8 +479,15 @@ public:
      */
      virtual UClassID getDynamicClassID() const;
 
-  private:
-
+#if defined(__xlC__)
+// Work around a compiler bug on xlC 11.1 on AIX 7.1 that would
+// prevent PluralSelectorAdapter from implementing private PluralSelector.
+// xlC error message:
+// 1540-0300 (S) The "private" member "class icu_49::PluralFormat::PluralSelector" cannot be accessed.
+public:
+#else
+private:
+#endif
      /**
       * @internal 
       */
@@ -514,6 +521,10 @@ public:
         PluralRules* pluralRules;
     };
 
+#if defined(__xlC__)
+// End of xlC bug workaround, keep remaining definitions private.
+private:
+#endif
     Locale  locale;
     MessagePattern msgPattern;
     NumberFormat*  numberFormat;
