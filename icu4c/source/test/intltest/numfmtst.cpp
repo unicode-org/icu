52af04fed76e4c6fd71a8f3177fd65db8dbfaaa6
Michael Ow <mow@svn.icu-project.org>
2012-02-13 19:23:47 +0000

    ICU-7464 Apply fix to incorrect number rounding when rounding increment is in the string pattern of the decimal formatter in ICU4C
    X-SVN-Rev: 31378

--- icu4c/source/test/intltest/numfmtst.cpp
+++ icu4c/source/test/intltest/numfmtst.cpp
@@ -116,6 +116,7 @@ void NumberFormatTest::runIndexedTest( int32_t index, UBool exec, const char* &n
         CASE(50,TestExplicitParents); 
         CASE(51,TestLenientParse);
         CASE(52,TestAvailableNumberingSystems);
+        CASE(53,TestRoundingPattern);
         default: name = ""; break;
     }
 }
@@ -2784,6 +2785,43 @@ void NumberFormatTest::TestRounding() {
             }
         }
     }
+    delete df;
+}
+
+void NumberFormatTest::TestRoundingPattern() {
+    UErrorCode status = U_ZERO_ERROR;
+    struct {
+        UnicodeString  pattern;
+        double        testCase;
+        UnicodeString expected;
+    } tests[] = {
+            { (UnicodeString)"##0.65", 1.234, (UnicodeString)"1.30" },
+            { (UnicodeString)"#50",    1230,  (UnicodeString)"1250" }
+    };
+    int32_t numOfTests = (sizeof(tests)/sizeof(tests[0]));
+    UnicodeString result;
+
+    DecimalFormat *df = (DecimalFormat*)NumberFormat::createCurrencyInstance(Locale::getEnglish(), status);
+    if (U_FAILURE(status)) {
+        dataerrln("Unable to create decimal formatter. - %s", u_errorName(status));
+        return;
+    }
+
+    for (int32_t i = 0; i < numOfTests; i++) {
+        result.remove();
+
+        df->applyPattern(tests[i].pattern, status);
+        if (U_FAILURE(status)) {
+            errln("Unable to apply pattern to decimal formatter. - %s", u_errorName(status));
+        }
+
+        df->format(tests[i].testCase, result);
+
+        if (result != tests[i].expected) {
+            errln("String Pattern Rounding Test Failed: Pattern: \"" + tests[i].pattern + "\" Number: " + tests[i].testCase + " - Got: " + result + " Expected: " + tests[i].expected);
+        }
+    }
+
     delete df;
 }
 
