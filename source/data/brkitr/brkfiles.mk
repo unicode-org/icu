3f698937b5478a6b22152b2132e66dde8bf3da41
Peter Edberg <pedberg@svn.icu-project.org>
2012-02-28 00:32:10 +0000

    C, Integrate CDLR 21.0.1 data from CLDR maint-21 branch r6616
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/9134
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@31529 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/data/brkitr/brkfiles.mk
+++ source/data/brkitr/brkfiles.mk
@@ -1,6 +1,6 @@
 # *   Copyright (C) 1998-2012, International Business Machines
 # *   Corporation and others.  All Rights Reserved.
-BRK_RES_CLDR_VERSION = 21.0
+BRK_RES_CLDR_VERSION = 21.0.1
 # A list of txt's to build
 # Note:
 #
