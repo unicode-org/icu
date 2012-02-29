72cba5a3ec6eb4590685976fe1ca16abdb9f9564
Michael Ow <mow@svn.icu-project.org>
2012-02-29 19:20:21 +0000

    [ICU-9144] Fix ifdef statement in icudataver.h X-Commit-URL: https://ssl.icu-project.org/trac/changeset/31545 Former-commit-id: 638f020f2cbc42caa8c1958cc4ef82460caeed83

--- icu4c/source/common/unicode/icudataver.h
+++ icu4c/source/common/unicode/icudataver.h
@@ -1,7 +1,7 @@
 /*
 ******************************************************************************
 *
-*   Copyright (C) 2009-2011, International Business Machines
+*   Copyright (C) 2009-2012, International Business Machines
 *   Corporation and others.  All Rights Reserved.
 *
 ******************************************************************************
@@ -12,7 +12,7 @@
 
 #include "unicode/utypes.h"
 
-#ifndef U_HIDE_INTERNAL_API
+#ifndef U_HIDE_DRAFT_API
 /**
  * @draft ICU 49
  */
@@ -31,7 +31,7 @@
  * 
  * @draft ICU 49
  */
-U_INTERNAL void U_EXPORT2 u_getDataVersion(UVersionInfo dataVersionFillin, UErrorCode *status);
-#endif  /* U_HIDE_INTERNAL_API */
+U_DRAFT void U_EXPORT2 u_getDataVersion(UVersionInfo dataVersionFillin, UErrorCode *status);
+#endif  /* U_HIDE_DRAFT_API */
 
 #endif
