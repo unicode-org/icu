6876123c0e4e000bf3d0c0e5af15cc3e504bf41d
Markus Scherer <markus.icu@gmail.com>
2012-08-28 23:37:10 +0000

    ICU-9437 update to Unicode 6.2; merge in branches/markus/uni62 r32274
    X-SVN-Rev: 32282

--- icu4c/source/test/intltest/usettest.cpp
+++ icu4c/source/test/intltest/usettest.cpp
@@ -1,6 +1,6 @@
 /*
 ********************************************************************************
-*   Copyright (C) 1999-2011 International Business Machines Corporation and
+*   Copyright (C) 1999-2012 International Business Machines Corporation and
 *   others. All Rights Reserved.
 ********************************************************************************
 *   Date        Name        Description
@@ -1049,7 +1049,7 @@ void UnicodeSetTest::TestPropertySet() {
         // Script_Extensions, new in Unicode 6.0
         "[:scx=Arab:]",
         "\\u061E\\u061F\\u0620\\u0621\\u063F\\u0640\\u0650\\u065E\\uFDF1\\uFDF2\\uFDF3",
-        "\\u061D\\u065F\\uFDEF\\uFDFE",
+        "\\u061D\\uFDEF\\uFDFE",
 
         // U+FDF2 has Script=Arabic and also Arab in its Script_Extensions,
         // so scx-sc is missing U+FDF2.
