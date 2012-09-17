a29d76d39089efa2680c4ccd9bd77638b4a0a809
Steven R. Loomis <srl@icu-project.org>
2012-09-17 22:24:17 +0000

    ICU-9586 print out status of gendict during build
    X-SVN-Rev: 32398

--- icu4c/source/tools/gendict/gendict.cpp
+++ icu4c/source/tools/gendict/gendict.cpp
@@ -35,6 +35,49 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "putilimp.h"
+UDate startTime = -1.0;
+
+static int elapsedTime() {
+  return (int)uprv_floor((uprv_getRawUTCtime()-startTime)/1000.0);
+}
+
+#if U_PLATFORM_IMPLEMENTS_POSIX && !U_PLATFORM_HAS_WIN32_API
+#include <signal.h>
+
+const char *wToolname="gendict";
+const char *wOutname="(some file)";
+
+const int firstSeconds = 5; /* seconds between notices*/
+const int nextSeconds = 15; /* seconds between notices*/
+
+static void alarm_fn(int /*n*/) {
+  printf("%s: still writing\t%s (%ds)\t...\n",    wToolname, wOutname, elapsedTime());
+  
+  alarm(nextSeconds); // reset the alarm
+}
+
+static void install_watchdog(const char *toolName, const char *outFileName) {
+  wToolname=toolName;
+  wOutname=outFileName;
+
+  if(startTime<0) { // uninitialized
+    startTime = uprv_getRawUTCtime();
+  }
+  signal(SIGALRM, &alarm_fn);
+
+  alarm(firstSeconds); // set the alarm
+}
+
+#else
+static void install_watchdog(const char*, const char*) {
+  // not implemented
+}
+#endif
+
+
+
+
 U_NAMESPACE_USE
 
 static char *progName;
@@ -255,6 +298,9 @@ int  main(int argc, char **argv) {
     const char *outFileName  = argv[2];
     const char *wordFileName = argv[1];
 
+    // set up the watchdog
+    install_watchdog(progName, outFileName);
+
     if (options[ARG_ICUDATADIR].doesOccur) {
         u_setDataDirectory(options[ARG_ICUDATADIR].value);
     }
@@ -424,7 +470,7 @@ int  main(int argc, char **argv) {
         exit(U_INTERNAL_PROGRAM_ERROR);
     }
 
-    puts("gendict: tool completed successfully.");
+    printf("%s: done writing\t%s (%ds).\n", progName, outFileName, elapsedTime());
 
 #ifdef TEST_GENDICT
     if (isBytesTrie) {
