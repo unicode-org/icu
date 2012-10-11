da804b77f35ad605e872a8d7d5a14455a33ae677
Travis Keep <rocketman@google.com>
2012-10-11 21:54:28 +0000

    [ICU-9598] Fix windows build which broke at 32600 X-Commit-URL: https://ssl.icu-project.org/trac/changeset/32610 Former-commit-id: e766fd5a805be3505b8aba2163e0b871929cdaf5

--- icu4c/source/test/cintltst/cgendtst.c
+++ icu4c/source/test/cintltst/cgendtst.c
@@ -35,11 +35,12 @@ void addGendInfoForTest(TestNode** root)
 static void TestGenderInfo(void) {
   UErrorCode status = U_ZERO_ERROR;
   const UGenderInfo* actual_gi = ugender_getInstance("fr_CA", &status);
+  UGender actual;
   if (U_FAILURE(status)) {
     log_err("Fail to create UGenderInfo - %s\n", u_errorName(status));
     return;
   }
-  UGender actual = ugender_getListGender(actual_gi, kAllFemale, LENGTHOF(kAllFemale), &status);
+  actual = ugender_getListGender(actual_gi, kAllFemale, LENGTHOF(kAllFemale), &status);
   if (U_FAILURE(status)) {
     log_err("Fail to get gender of list - %s\n", u_errorName(status));
     return;
