a9c70f7b007ee21c072b8f010529bd9258daca3d
Peter Edberg <pedberg@svn.icu-project.org>
2012-10-12 03:19:08 +0000

    Update ICU version to 50.1, update timebombs for #9156,#9490,#8982,ccldrbug3058
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/9581
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@32616 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/unicode/uvernum.h
+++ source/common/unicode/uvernum.h
@@ -63,13 +63,13 @@
  *  This value will change in the subsequent releases of ICU
  *  @stable ICU 2.6
  */
-#define U_ICU_VERSION_MINOR_NUM 0
+#define U_ICU_VERSION_MINOR_NUM 1
 
 /** The current ICU patchlevel version as an integer.  
  *  This value will change in the subsequent releases of ICU
  *  @stable ICU 2.4
  */
-#define U_ICU_VERSION_PATCHLEVEL_NUM 2
+#define U_ICU_VERSION_PATCHLEVEL_NUM 0
 
 /** The current ICU build level version as an integer.  
  *  This value is for use by ICU clients. It defaults to 0.
@@ -118,7 +118,7 @@
  *  This value will change in the subsequent releases of ICU
  *  @stable ICU 2.4
  */
-#define U_ICU_VERSION "50.0.2"
+#define U_ICU_VERSION "50.1"
 
 /** The current ICU library major/minor version as a string without dots, for library name suffixes. 
  *  This value will change in the subsequent releases of ICU
@@ -130,7 +130,7 @@
 /** Data version in ICU4C.
  * @internal ICU 4.4 Internal Use Only
  **/
-#define U_ICU_DATA_VERSION "50.0.2"
+#define U_ICU_DATA_VERSION "50.1"
 #endif  /* U_HIDE_INTERNAL_API */
 
 /*===========================================================================
