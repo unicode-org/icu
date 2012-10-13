6c28656d1ccaf6414562066ed19aadf48db01955
Roozbeh Pournader <roozbeh@svn.icu-project.org>
2012-10-13 00:57:46 +0000

    [ICU-8965] Add tests for Persian Calendar X-Commit-URL: https://ssl.icu-project.org/trac/changeset/32629 Former-commit-id: acd15cda315732f7a80e414c95bf9f362317ed44

--- icu4j/main/tests/core/src/com/ibm/icu/dev/test/calendar/TestAll.java
+++ icu4j/main/tests/core/src/com/ibm/icu/dev/test/calendar/TestAll.java
@@ -1,6 +1,6 @@
 /*
  *******************************************************************************
- * Copyright (C) 1996-2007, International Business Machines Corporation and    *
+ * Copyright (C) 1996-2012, International Business Machines Corporation and    *
  * others. All Rights Reserved.                                                *
  *******************************************************************************
  */
@@ -29,6 +29,7 @@ public class TestAll extends TestGroup {
                   "JapaneseTest",
                   "ChineseTest",
                   "IndianTest",
+                  "PersianTest",
                   "HolidayTest",
                   "DataDrivenCalendarTest"
               },
