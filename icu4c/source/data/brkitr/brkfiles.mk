68245e2a716b49a6bd341c10442a181cb995f2c3
John Emmons <emmo@us.ibm.com>
2012-10-22 17:32:28 +0000

    ICU-9676 Updated version numbers to CLDR 22.1
    X-SVN-Rev: 32686

--- icu4c/source/data/brkitr/brkfiles.mk
+++ icu4c/source/data/brkitr/brkfiles.mk
@@ -1,6 +1,6 @@
 # *   Copyright (C) 1998-2012, International Business Machines
 # *   Corporation and others.  All Rights Reserved.
-BRK_RES_CLDR_VERSION = 22.0
+BRK_RES_CLDR_VERSION = 22.1
 # A list of txt's to build
 # Note:
 #
