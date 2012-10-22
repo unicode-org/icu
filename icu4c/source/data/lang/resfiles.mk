0b47218cf4e1ab0662085444b2f7c3550ff861c0
John Emmons <emmo@us.ibm.com>
2012-10-22 17:32:28 +0000

    [ICU-9676] Updated version numbers to CLDR 22.1 X-Commit-URL: https://ssl.icu-project.org/trac/changeset/32686 Former-commit-id: 1aa973a3791bddcb9215c404c07db359e015d335

--- icu4c/source/data/lang/resfiles.mk
+++ icu4c/source/data/lang/resfiles.mk
@@ -1,6 +1,6 @@
 # *   Copyright (C) 1998-2012, International Business Machines
 # *   Corporation and others.  All Rights Reserved.
-LANG_CLDR_VERSION = 22.0
+LANG_CLDR_VERSION = 22.1
 # A list of txt's to build
 # Note:
 #
