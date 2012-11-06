a9523d62d249c0f2735b69a0c72b004b756c91b9
Travis Keep <rocketman@google.com>
2012-11-06 19:29:54 +0000

    [ICU-9725] C GenderInfo must handle single item lists correctly by returning the gender of the sole item. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/32761 Former-commit-id: e0889670724cc57f63f5dec7c239a32f3d0358ba

--- icu4c/source/i18n/gender.cpp
+++ icu4c/source/i18n/gender.cpp
@@ -173,7 +173,7 @@ UGender GenderInfo::getListGender(const UGender* genders, int32_t length, UError
   if (U_FAILURE(status)) {
     return UGENDER_OTHER;
   }
-  if (length == 0 || _style == NEUTRAL) {
+  if (length == 0) {
     return UGENDER_OTHER;
   }
   if (length == 1) {
@@ -182,6 +182,8 @@ UGender GenderInfo::getListGender(const UGender* genders, int32_t length, UError
   UBool has_female = FALSE;
   UBool has_male = FALSE;
   switch (_style) {
+    case NEUTRAL:
+      return UGENDER_OTHER;
     case MIXED_NEUTRAL:
       for (int32_t i = 0; i < length; ++i) {
         switch (genders[i]) {
