bd593361dab60d86fd3943f5ca384b1e0b3717d0
Steven R. Loomis <srl@icu-project.org>
2012-11-13 23:10:20 +0000

    bump versions to 51.0.1
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/9721
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@32811 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/common/unicode/uvernum.h
+++ source/common/unicode/uvernum.h
@@ -57,19 +57,19 @@
  *  This value will change in the subsequent releases of ICU
  *  @stable ICU 2.4
  */
-#define U_ICU_VERSION_MAJOR_NUM 50
+#define U_ICU_VERSION_MAJOR_NUM 51
 
 /** The current ICU minor version as an integer. 
  *  This value will change in the subsequent releases of ICU
  *  @stable ICU 2.6
  */
-#define U_ICU_VERSION_MINOR_NUM 1
+#define U_ICU_VERSION_MINOR_NUM 0
 
 /** The current ICU patchlevel version as an integer.  
  *  This value will change in the subsequent releases of ICU
  *  @stable ICU 2.4
  */
-#define U_ICU_VERSION_PATCHLEVEL_NUM 0
+#define U_ICU_VERSION_PATCHLEVEL_NUM 1
 
 /** The current ICU build level version as an integer.  
  *  This value is for use by ICU clients. It defaults to 0.
@@ -83,7 +83,7 @@
  *  This value will change in the subsequent releases of ICU
  *  @stable ICU 2.6
  */
-#define U_ICU_VERSION_SUFFIX _50
+#define U_ICU_VERSION_SUFFIX _51
 
 /**
  * \def U_DEF2_ICU_ENTRY_POINT_RENAME
@@ -118,19 +118,19 @@
  *  This value will change in the subsequent releases of ICU
  *  @stable ICU 2.4
  */
-#define U_ICU_VERSION "50.1"
+#define U_ICU_VERSION "51.0.1"
 
 /** The current ICU library major/minor version as a string without dots, for library name suffixes. 
  *  This value will change in the subsequent releases of ICU
  *  @stable ICU 2.6
  */
-#define U_ICU_VERSION_SHORT "50"
+#define U_ICU_VERSION_SHORT "51"
 
 #ifndef U_HIDE_INTERNAL_API
 /** Data version in ICU4C.
  * @internal ICU 4.4 Internal Use Only
  **/
-#define U_ICU_DATA_VERSION "50.1"
+#define U_ICU_DATA_VERSION "51.0.1"
 #endif  /* U_HIDE_INTERNAL_API */
 
 /*===========================================================================
