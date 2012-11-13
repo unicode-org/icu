c64a28c5a35956604606243b2ddab37397a6f9f5
Steven R. Loomis <srl@icu-project.org>
2012-11-13 23:09:18 +0000

    [ICU-9721] move two timebombs forward - but fix them in #9742 X-Commit-URL: https://ssl.icu-project.org/trac/changeset/32810 Former-commit-id: 8b67a250424da14cc6f3392ad2ed4970cd0790a4

--- icu4c/source/test/cintltst/creststn.c
+++ icu4c/source/test/cintltst/creststn.c
@@ -280,8 +280,8 @@ static void TestErrorCodes(void) {
   checkStatus(__LINE__, U_USING_DEFAULT_WARNING, status);
 
   /* we look up the resource which is aliased and at our level */
-  /* TODO: restore the following test when cldrbug 3058: is fixed */
-  if(U_SUCCESS(status) && r != NULL && isICUVersionAtLeast(51, 0, 0)) {
+  /* TODO: restore the following test when cldrbug 3058: is fixed - but CldrBug:3058 is WONTFIX */
+  if(U_SUCCESS(status) && r != NULL && isICUVersionAtLeast(51, 0, 2)) {
     status = U_USING_DEFAULT_WARNING;
     r2 = ures_getByKey(r, "Countries", r2, &status);
     checkStatus(__LINE__, U_USING_DEFAULT_WARNING, status);
