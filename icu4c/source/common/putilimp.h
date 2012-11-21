9370fd65d3639f06d325d028b77d677ff2f33949
Michael Ow <mow@svn.icu-project.org>
2012-11-21 19:52:44 +0000

    ICU-9605 Fix compiler error caused by undefined __timezone in uclibc
    X-SVN-Rev: 32876

--- icu4c/source/common/putilimp.h
+++ icu4c/source/common/putilimp.h
@@ -116,7 +116,10 @@ typedef size_t uintptr_t;
 #elif U_PLATFORM == U_PF_ANDROID
 #   define U_TIMEZONE timezone
 #elif U_PLATFORM_IS_LINUX_BASED
-#   define U_TIMEZONE __timezone
+#   if !defined(__UCLIBC__)
+    /* __timezone is only available in glibc */
+#       define U_TIMEZONE __timezone
+#   endif
 #elif U_PLATFORM_USES_ONLY_WIN32_API
 #   define U_TIMEZONE _timezone
 #elif U_PLATFORM == U_PF_BSD && !defined(__NetBSD__)
