d24f65de8819e5b473d773cc2ee9535b2422d2cb
Steven R. Loomis <srl@icu-project.org>
2012-12-05 00:04:44 +0000

    ICU-9723 bump version # 50.1.1
    X-SVN-Rev: 32923

--- source/common/unicode/uvernum.h
+++ source/common/unicode/uvernum.h
@@ -69,7 +69,7 @@
  *  This value will change in the subsequent releases of ICU
  *  @stable ICU 2.4
  */
-#define U_ICU_VERSION_PATCHLEVEL_NUM 0
+#define U_ICU_VERSION_PATCHLEVEL_NUM 1
 
 /** The current ICU build level version as an integer.  
  *  This value is for use by ICU clients. It defaults to 0.
@@ -118,7 +118,7 @@
  *  This value will change in the subsequent releases of ICU
  *  @stable ICU 2.4
  */
-#define U_ICU_VERSION "50.1"
+#define U_ICU_VERSION "50.1.1"
 
 /** The current ICU library major/minor version as a string without dots, for library name suffixes. 
  *  This value will change in the subsequent releases of ICU
