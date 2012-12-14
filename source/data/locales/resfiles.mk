e62ee453014c94629505c8632da061bebd5ef6e3
John Emmons <emmons@svn.icu-project.org>
2012-12-14 23:39:42 +0000

    Merge CLDR 22.1.1 data to trunk
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/9799
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@32974 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/data/locales/resfiles.mk
+++ source/data/locales/resfiles.mk
@@ -1,6 +1,6 @@
 # *   Copyright (C) 1998-2012, International Business Machines
 # *   Corporation and others.  All Rights Reserved.
-GENRB_CLDR_VERSION = 22.1
+GENRB_CLDR_VERSION = 22.1.1
 # A list of txt's to build
 # Note:
 #
