7586fcf7d4fea130d09ad6f6e82c056199f8d075
Yoshito Umaoka <y.umaoka@gmail.com>
2012-12-19 05:12:25 +0000

    ICU-9573 Removing Boyer-Moore string search implementation. CollData has moved from i18n package to intltest.
    X-SVN-Rev: 32994

--- icu4c/source/test/intltest/colldata.h
+++ icu4c/source/test/intltest/colldata.h
@@ -7,10 +7,17 @@
 
 /**
  * \file 
- * \brief C++ API: Collation data used to compute minLengthInChars.
+ * \brief Originally, added as C++ API for Collation data used to compute minLengthInChars
  * \internal
  */
- 
+
+/*
+ * Note: This module was incldued in ICU 4.0.1 as @internal technology preview for supporting
+ * Boyer-Moore string search API. For now, only SSearchTest depends on this module. I temporaly
+ * moved the module from i18n directory to intltest, because we have no plan to publish this
+ * as public API. (2012-12-18 yoshito)
+ */
+
 #ifndef COLL_DATA_H
 #define COLL_DATA_H
 
@@ -18,21 +25,10 @@
 
 #if !UCONFIG_NO_COLLATION
 
-#include "unicode/uobject.h"
 #include "unicode/ucol.h"
-
-U_NAMESPACE_BEGIN
-
-#ifndef U_HIDE_INTERNAL_API
-/**
- * The size of the internal buffer for the Collator's short description string.
- * @internal ICU 4.0.1 technology preview
- */
-#define KEY_BUFFER_SIZE 64
 
  /**
   * The size of the internal CE buffer in a <code>CEList</code> object
-  * @internal ICU 4.0.1 technology preview
   */
 #define CELIST_BUFFER_SIZE 4
 
@@ -40,31 +36,19 @@ U_NAMESPACE_BEGIN
  * \def INSTRUMENT_CELIST
  * Define this to enable the <code>CEList</code> objects to collect
  * statistics.
- * @internal ICU 4.0.1 technology preview
  */
-//#define INSTRUMENT_CELIST
 
  /**
   * The size of the initial list in a <code>StringList</code> object.
-  * @internal ICU 4.0.1 technology preview
   */
 #define STRING_LIST_BUFFER_SIZE 16
-
-/**
- * \def INSTRUMENT_STRING_LIST
- * Define this to enable the <code>StringList</code> objects to
- * collect statistics.
- * @internal ICU 4.0.1 technology preview
- */
-//#define INSTRUMENT_STRING_LIST
 
  /**
   * This object holds a list of CEs generated from a particular
   * <code>UnicodeString</code>
   *
-  * @internal ICU 4.0.1 technology preview
   */
-class U_I18N_API CEList : public UObject
+class CEList
 {
 public:
     /**
@@ -77,14 +61,11 @@ public:
      * Note: if on return, status is set to an error code,
      * the only safe thing to do with this object is to call
      * the destructor.
-     *
-     * @internal ICU 4.0.1 technology preview
      */
     CEList(UCollator *coll, const UnicodeString &string, UErrorCode &status);
 
     /**
      * The destructor.
-     * @internal ICU 4.0.1 technology preview
      */
     ~CEList();
 
@@ -92,8 +73,6 @@ public:
      * Return the number of CEs in the list.
      *
      * @return the number of CEs in the list.
-     *
-     * @internal ICU 4.0.1 technology preview
      */
     int32_t size() const;
 
@@ -103,8 +82,6 @@ public:
      * @param index - the index of the CE to return
      *
      * @return the CE, or <code>0</code> if <code>index</code> is out of range
-     *
-     * @internal ICU 4.0.1 technology preview
      */
     uint32_t get(int32_t index) const;
 
@@ -116,8 +93,6 @@ public:
      * @param other - the other <code>CEList</code>
      *
      * @return <code>TRUE</code> if the CEs match, <code>FALSE</code> otherwise.
-     *
-     * @internal ICU 4.0.1 technology preview
      */
     UBool matchesAt(int32_t offset, const CEList *other) const; 
 
@@ -127,21 +102,8 @@ public:
      * @param index - the index
      *
      * @return a reference to the given CE in the list
-     *
-     * @internal ICU 4.0.1 technology preview
      */
     uint32_t &operator[](int32_t index) const;
-
-    /**
-     * UObject glue...
-     * @internal ICU 4.0.1 technology preview
-     */
-    virtual UClassID getDynamicClassID() const;
-    /**
-     * UObject glue...
-     * @internal ICU 4.0.1 technology preview
-     */
-    static UClassID getStaticClassID();
 
 private:
     void add(uint32_t ce, UErrorCode &status);
@@ -150,21 +112,14 @@ private:
     uint32_t *ces;
     int32_t listMax;
     int32_t listSize;
-
-#ifdef INSTRUMENT_CELIST
-    static int32_t _active;
-    static int32_t _histogram[10];
-#endif
 };
 
 /**
  * StringList
  *
  * This object holds a list of <code>UnicodeString</code> objects.
- *
- * @internal ICU 4.0.1 technology preview
  */
-class U_I18N_API StringList : public UObject
+class StringList
 {
 public:
     /**
@@ -175,15 +130,11 @@ public:
      * Note: if on return, status is set to an error code,
      * the only safe thing to do with this object is to call
      * the destructor.
-     *
-     * @internal ICU 4.0.1 technology preview
      */
     StringList(UErrorCode &status);
 
     /**
      * The destructor.
-     *
-     * @internal ICU 4.0.1 technology preview
      */
     ~StringList();
 
@@ -192,8 +143,6 @@ public:
      *
      * @param string - the string to add
      * @param status - will be set if any errors occur. 
-     *
-     * @internal ICU 4.0.1 technology preview
      */
     void add(const UnicodeString *string, UErrorCode &status);
 
@@ -203,8 +152,6 @@ public:
      * @param chars - the address of the array of code points
      * @param count - the number of code points in the array
      * @param status - will be set if any errors occur. 
-     *
-     * @internal ICU 4.0.1 technology preview
      */
     void add(const UChar *chars, int32_t count, UErrorCode &status);
 
@@ -215,8 +162,6 @@ public:
      *
      * @return a pointer to the <code>UnicodeString</code> or <code>NULL</code> 
      *         if <code>index</code> is out of bounds.
-     *
-     * @internal ICU 4.0.1 technology preview
      */
     const UnicodeString *get(int32_t index) const;
 
@@ -224,43 +169,22 @@ public:
      * Get the number of stings in the list.
      *
      * @return the number of strings in the list.
-     *
-     * @internal ICU 4.0.1 technology preview
      */
     int32_t size() const;
-
-    /**
-     * the UObject glue...
-     * @internal ICU 4.0.1 technology preview
-     */
-    virtual UClassID getDynamicClassID() const;
-    /**
-     * the UObject glue...
-     * @internal ICU 4.0.1 technology preview
-     */
-    static UClassID getStaticClassID();
 
 private:
     UnicodeString *strings;
     int32_t listMax;
     int32_t listSize;
-
-#ifdef INSTRUMENT_STRING_LIST
-    static int32_t _lists;
-    static int32_t _strings;
-    static int32_t _histogram[101];
-#endif
 };
-#endif  /* U_HIDE_INTERNAL_API */
+
 
 /*
  * Forward references to internal classes.
  */
 class StringToCEsMap;
 class CEToStringsMap;
-class CollDataCache;
 
-#ifndef U_HIDE_INTERNAL_API
 /**
  * CollData
  *
@@ -276,10 +200,8 @@ class CollDataCache;
  * If you do not need to reuse any unreferenced objects in the cache, you can call
  * <code>CollData::flushCollDataCache</code>. If you no longer need any <code>CollData</code>
  * objects, you can call <code>CollData::freeCollDataCache</code>
- *
- * @internal ICU 4.0.1 technology preview
  */
-class U_I18N_API CollData : public UObject
+class CollData
 {
 public:
     /**
@@ -287,32 +209,18 @@ public:
      *
      * @param collator - the collator
      * @param status - will be set if any errors occur. 
-     *
-     * @return the <code>CollData</code> object. You must call
-     *         <code>close</code> when you are done using the object.
-     *
-     * Note: if on return, status is set to an error code,
-     * the only safe thing to do with this object is to call
-     * <code>CollData::close</code>.
-     *
-     * @internal ICU 4.0.1 technology preview
      */
-    static CollData *open(UCollator *collator, UErrorCode &status);
+    CollData(UCollator *collator, UErrorCode &status);
 
     /**
-     * Release a <code>CollData</code> object.
-     *
-     * @param collData - the object
-     *
-     * @internal ICU 4.0.1 technology preview
+     * The destructor.
      */
-    static void close(CollData *collData);
+    ~CollData();
 
     /**
      * Get the <code>UCollator</code> object used to create this object.
      * The object returned may not be the exact object that was used to
      * create this object, but it will have the same behavior.
-     * @internal ICU 4.0.1 technology preview
      */
     UCollator *getCollator() const;
 
@@ -325,8 +233,6 @@ public:
      * return a <code>StringList</code> object containing all
      *        the stirngs, or <code>NULL</code> if there are
      *        no such strings.
-     *
-     * @internal ICU 4.0.1 technology preview.
      */
     const StringList *getStringList(int32_t ce) const;
 
@@ -338,8 +244,6 @@ public:
      * @return a <code>CEList</code> object containt the CEs. You
      *         must call <code>freeCEList</code> when you are finished
      *         using the <code>CEList</code>/
-     *
-     * @internal ICU 4.0.1 technology preview.
      */
     const CEList *getCEList(const UnicodeString *string) const;
 
@@ -347,8 +251,6 @@ public:
      * Release a <code>CEList</code> returned by <code>getCEList</code>.
      *
      * @param list - the <code>CEList</code> to free.
-     *
-     * @internal ICU 4.0.1 technology preview
      */
     void freeCEList(const CEList *list);
 
@@ -360,8 +262,6 @@ public:
      * @param offset - the offset of the first CE in the list to use.
      *
      * @return the length of the shortest string.
-     *
-     * @internal ICU 4.0.1 technology preview
      */
     int32_t minLengthInChars(const CEList *ces, int32_t offset) const;
 
@@ -382,75 +282,18 @@ public:
      *                 the number of cEs in the <code>CEList</code>
      *
      * @return the length of the shortest string.
-     *
-     * @internal ICU 4.0.1 technology preview
      */
    int32_t minLengthInChars(const CEList *ces, int32_t offset, int32_t *history) const;
 
-   /**
-    * UObject glue...
-    * @internal ICU 4.0.1 technology preview
-    */
-    virtual UClassID getDynamicClassID() const;
-   /**
-    * UObject glue...
-    * @internal ICU 4.0.1 technology preview
-    */
-    static UClassID getStaticClassID();
-
-    /**
-     * <code>CollData</code> objects are expensive to compute, and so
-     * may be cached. This routine will free the cached objects and delete
-     * the cache.
-     *
-     * WARNING: Don't call this until you are have called <code>close</code>
-     * for each <code>CollData</code> object that you have used. also,
-     * DO NOT call this if another thread may be calling <code>flushCollDataCache</code>
-     * at the same time.
-     *
-     * @internal 4.0.1 technology preview
-     */
-    static void freeCollDataCache();
-
-    /**
-     * <code>CollData</code> objects are expensive to compute, and so
-     * may be cached. This routine will remove any unused <code>CollData</code>
-     * objects from the cache.
-     *
-     * @internal 4.0.1 technology preview
-     */
-    static void flushCollDataCache();
-
 private:
-    friend class CollDataCache;
-    friend class CollDataCacheEntry;
-
-    CollData(UCollator *collator, char *cacheKey, int32_t cachekeyLength, UErrorCode &status);
-    ~CollData();
-
-    CollData();
-
-    static char *getCollatorKey(UCollator *collator, char *buffer, int32_t bufferLength);
-
-    static CollDataCache *getCollDataCache();
-
     UCollator      *coll;
-    StringToCEsMap *charsToCEList;
     CEToStringsMap *ceToCharsStartingWith;
-
-    char keyBuffer[KEY_BUFFER_SIZE];
-    char *key;
-
-    static CollDataCache *collDataCache;
 
     uint32_t minHan;
     uint32_t maxHan;
 
     uint32_t jamoLimits[4];
 };
-#endif  /* U_HIDE_INTERNAL_API */
-
-U_NAMESPACE_END
 
 #endif // #if !UCONFIG_NO_COLLATION
 #endif // #ifndef COLL_DATA_H
