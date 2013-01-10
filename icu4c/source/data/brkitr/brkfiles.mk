29ef626fa27bb8c169d3a80cd87eb04a0e584106
Peter Edberg <pedberg@unicode.org>
2013-01-10 17:58:47 +0000

    [ICU-9840] Pre-integrate some CLDR 23 additions/changes (C) X-Commit-URL: https://ssl.icu-project.org/trac/changeset/33033 Former-commit-id: 180330c442efe0688af121bed409e0df20a8f88f

--- icu4c/source/data/brkitr/brkfiles.mk
+++ icu4c/source/data/brkitr/brkfiles.mk
@@ -1,6 +1,6 @@
-# *   Copyright (C) 1998-2012, International Business Machines
+# *   Copyright (C) 1998-2013, International Business Machines
 # *   Corporation and others.  All Rights Reserved.
-BRK_RES_CLDR_VERSION = 22.1.1
+BRK_RES_CLDR_VERSION = 23
 # A list of txt's to build
 # Note:
 #
