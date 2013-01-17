205571f9f2539667cfa5a31e401dee391f1e3618
Travis Keep <keep94@gmail.com>
2013-01-17 17:56:11 +0000

    ICU-9760 Fix year in copyright message.
    X-SVN-Rev: 33060

--- icu4j/main/classes/core/src/com/ibm/icu/impl/LocaleIDs.java
+++ icu4j/main/classes/core/src/com/ibm/icu/impl/LocaleIDs.java
@@ -1,6 +1,6 @@
 /*
  *******************************************************************************
- * Copyright (C) 2012, International Business Machines Corporation and         *
+ * Copyright (C) 2009-2013, International Business Machines Corporation and    *
  * others. All Rights Reserved.                                                *
  *******************************************************************************
  */
