37394b6fc4f432c5c61540d2f32fca3d67d9de83
Peter Edberg <pedberg@unicode.org>
2013-02-12 20:45:06 +0000

    [ICU-9925] CLDR 23 pre-integration part 3, batch 1 (J) X-Commit-URL: https://ssl.icu-project.org/trac/changeset/33195 Former-commit-id: 4f8724fa0ce094d78239d4222c161d907738b44d

--- icu4j/main/tests/collate/src/com/ibm/icu/dev/test/format/GlobalizationPreferencesTest.java
+++ icu4j/main/tests/collate/src/com/ibm/icu/dev/test/format/GlobalizationPreferencesTest.java
@@ -1,6 +1,6 @@
 /*
  *******************************************************************************
- * Copyright (C) 2004-2012, International Business Machines Corporation and    *
+ * Copyright (C) 2004-2013, International Business Machines Corporation and    *
  * others. All Rights Reserved.                                                *
  *******************************************************************************
 */
@@ -1175,8 +1175,8 @@ public class GlobalizationPreferencesTest extends TestFmwk {
         df = gp.getDateFormat(GlobalizationPreferences.DF_SHORT, GlobalizationPreferences.DF_NONE);
         pattern = ((SimpleDateFormat)df).toPattern();
         // root pattern must be used
-        if (!pattern.equals("yyyy-MM-dd")) {
-            errln("FAIL: SHORT date pattern is " + pattern + " Expected: yyyy-MM-dd");
+        if (!pattern.equals("y-MM-dd")) {
+            errln("FAIL: SHORT date pattern is " + pattern + " Expected: y-MM-dd");
         }
 
         // Set locale - fr, fr_CA, fr_FR
