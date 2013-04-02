6125cf2effca9097a0a8acaa3aa226a98b8d4cdc
Travis Keep <keep94@gmail.com>
2013-04-02 22:01:33 +0000

    ICU-10045 Formatting with significant digits to work correctly with 0 in C++
    X-SVN-Rev: 33485

--- icu4c/source/test/intltest/numfmtst.h
+++ icu4c/source/test/intltest/numfmtst.h
@@ -1,6 +1,6 @@
 /************************************************************************
  * COPYRIGHT:
- * Copyright (c) 1997-2012, International Business Machines Corporation
+ * Copyright (c) 1997-2013, International Business Machines Corporation
  * and others. All Rights Reserved.
  ************************************************************************/
 
@@ -161,6 +161,8 @@ class NumberFormatTest: public CalendarTimeZoneTest {
     void TestFormattableSize();
 
     void TestEnumSet();
+
+    void TestSignificantDigits();
 
  private:
 
