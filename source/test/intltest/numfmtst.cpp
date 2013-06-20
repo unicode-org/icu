44d89e5ebc24aaf95d988d3ad713f8fb992cd17b
Steven R. Loomis <srl@icu-project.org>
2013-06-20 22:20:51 +0000

    improve error messages"
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/10069
    
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@33839 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/test/intltest/numfmtst.cpp
+++ source/test/intltest/numfmtst.cpp
@@ -2662,8 +2662,22 @@ void NumberFormatTest::expectParseCurrency(const NumberFormat &fmt, const UChar*
         return;
     }
     UErrorCode status = U_ZERO_ERROR;
-    assertTrue("amount", amount ==  currencyAmount->getNumber().getDouble(status));
-    assertEquals("currency", currency, currencyAmount->getISOCurrency());
+
+    char theInfo[100];
+    sprintf(theInfo, "For locale %s, string \"%s\", currency ",
+            fmt.getLocale(ULOC_ACTUAL_LOCALE, status).getBaseName(),
+            text);
+    u_austrcpy(theInfo+uprv_strlen(theInfo), currency);
+    
+    char theOperation[100];
+
+    uprv_strcpy(theOperation, theInfo);
+    uprv_strcat(theOperation, ", check amount:");
+    assertTrue(theOperation, amount ==  currencyAmount->getNumber().getDouble(status));
+
+    uprv_strcpy(theOperation, theInfo);
+    uprv_strcat(theOperation, ", check currency:");
+    assertEquals(theOperation, currency, currencyAmount->getISOCurrency());
 }
   
 
