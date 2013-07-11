bcc30a4acbf4069f505410f189125d4a415325d2
Peter Edberg <pedberg@unicode.org>
2013-07-11 07:00:49 +0000

    [ICU-10244] (C) ulocdata_getLocaleSeparator now returns piece between {0} and {1} in new localeSeparator pattern X-Commit-URL: https://ssl.icu-project.org/trac/changeset/33906 Former-commit-id: cbde85192592ecb4799d057dd854d348a0ae1363

--- icu4c/source/i18n/ulocdata.c
+++ icu4c/source/i18n/ulocdata.c
@@ -305,6 +305,10 @@ ulocdata_getLocaleSeparator(ULocaleData *uld,
     int32_t len = 0;
     const UChar *separator = NULL;
     UErrorCode localStatus = U_ZERO_ERROR;
+    UChar *p0, *p1;
+    static const UChar sub0[4] = { 0x007b, 0x0030, 0x007d , 0x0000 }; /* {0} */
+    static const UChar sub1[4] = { 0x007b, 0x0031, 0x007d , 0x0000 }; /* {1} */
+    static const int32_t subLen = 3;
 
     if (U_FAILURE(*status))
         return 0;
@@ -337,6 +341,20 @@ ulocdata_getLocaleSeparator(ULocaleData *uld,
 
     if (U_FAILURE(*status)){
         return 0;
+    }
+
+    /* For backwards compatibility, if we have a pattern, return the portion between {0} and {1} */
+    p0=u_strstr(separator, sub0);
+    p1=u_strstr(separator, sub1);
+    if (p0!=NULL && p1!=NULL && p0<=p1) {
+        separator = (const UChar *)p0 + subLen;
+        len = p1 - separator;
+        /* Desired separator is no longer zero-terminated; handle that if necessary */
+        if (len < resultCapacity) {
+            u_strncpy(result, separator, len);
+            result[len] = 0;
+            return len;
+        }
     }
 
     u_strncpy(result, separator, resultCapacity);
