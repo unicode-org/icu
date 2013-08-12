8e9b5e0b7e50da4f22c75762b163ac676b70e131
Andy Heninger <andy.heninger@gmail.com>
2013-08-12 03:35:22 +0000

    ICU-10301 Remove singleton classes, convert existing usage to UInitOnce
    X-SVN-Rev: 34032

--- icu4c/source/common/mutex.cpp
+++ icu4c/source/common/mutex.cpp
@@ -13,69 +13,8 @@
 
 #include "unicode/utypes.h"
 #include "mutex.h"
-#include "uassert.h"
 
 U_NAMESPACE_BEGIN
-
-void *SimpleSingleton::getInstance(InstantiatorFn *instantiator, const void *context,
-                                   UErrorCode &errorCode) {
-    if(U_FAILURE(errorCode)) {
-        return NULL;
-    }
-    if (umtx_loadAcquire(fInitOnce.fState) == 2) {
-        return fInstance;
-    }
-    if (umtx_initImplPreInit(fInitOnce)) {
-        fInstance = instantiator(context, errorCode);
-        umtx_initImplPostInit(fInitOnce, fInstance != NULL);
-    }
-    return fInstance;
-}
-
-
-/*
- * Three states:
- *
- * Initial state: Instance creation not attempted yet.
- * fInstance=NULL && U_SUCCESS(fErrorCode)
- *
- * Instance creation run & succeeded:
- * fInstance!=NULL && U_SUCCESS(fErrorCode)
- *
- * Instance creation & failed:
- * fInstance=NULL && U_FAILURE(fErrorCode)
- * We will not attempt again to create the instance.
- *
- * The instantiator function will be called only once, whether it succeeds or fails.
- * The controlling state is maintained by the UInitOnce object, not by
- *    fInstance and fErrorCode.
- * The values of fInstance and fErrorCode must only be set between pre and post init(),
- *    where they are in a controlled memory environment.
- */
-void *TriStateSingleton::getInstance(InstantiatorFn *instantiator, const void *context,
-                                     UErrorCode &errorCode) {
-    if(U_FAILURE(errorCode)) {
-        return NULL;
-    }
-    if (umtx_loadAcquire(fInitOnce.fState) == 2) {
-        errorCode = fErrorCode;
-        return fInstance;
-    }
-    if (umtx_initImplPreInit(fInitOnce)) {
-        errorCode = U_ZERO_ERROR;
-        fInstance = instantiator(context, errorCode);
-        fErrorCode = errorCode;
-        umtx_initImplPostInit(fInitOnce, TRUE);
-    }
-    return fInstance;
-}
-
-
-void TriStateSingleton::reset() {
-    fInstance=NULL;
-    fErrorCode=U_ZERO_ERROR;
-    fInitOnce.reset();
-}
 
 #if UCONFIG_NO_SERVICE
 
