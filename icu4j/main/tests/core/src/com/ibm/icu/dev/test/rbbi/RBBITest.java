42fed1a85978db0ab51141d81cedadc98926c176
Markus Scherer <markus.icu@gmail.com>
2013-08-28 21:08:11 +0000

    [ICU-10168] Unicode 6.3 data files as of 2013-aug-27 X-Commit-URL: https://ssl.icu-project.org/trac/changeset/34104 Former-commit-id: cb46b3ffcbedfb7e5bffcc053c402317362f5d87

--- icu4j/main/tests/core/src/com/ibm/icu/dev/test/rbbi/RBBITest.java
+++ icu4j/main/tests/core/src/com/ibm/icu/dev/test/rbbi/RBBITest.java
@@ -660,10 +660,6 @@ public class RBBITest extends TestFmwk {
                 }
             }
         }
-        // KIND_WORD "en_US_POSIX"
-        final String posxWordText     = "Can't have breaks in xx:yy or struct.field for CS-types.";
-        final int[]  posxWordTOffsets = { 5, 6, 10, 11, 17, 18, 20, 21, 23, 24, 26, 27, 29, 30, 36, 37, 42, 43, 46, 47, 49, 50, 55, 56 };
-        final int[]  posxWordROffsets = { 5, 6, 10, 11, 17, 18, 20, 21, 23, 24, 26, 27, 29, 30,         42, 43, 46, 47, 49, 50, 55, 56 };
         // KIND_SENTENCE "el"
         final String elSentText     = "\u0391\u03B2, \u03B3\u03B4; \u0395 \u03B6\u03B7\u037E \u0398 \u03B9\u03BA. " +
                                       "\u039B\u03BC \u03BD\u03BE! \u039F\u03C0, \u03A1\u03C2? \u03A3";
@@ -682,8 +678,6 @@ public class RBBITest extends TestFmwk {
         //                                29,     32, 33, 35, 37, 38,     40, 41 };
         
         final TBItem[] tests = {
-            new TBItem( BreakIterator.KIND_WORD,      new ULocale("en_US_POSIX"), posxWordText, posxWordTOffsets ),
-            new TBItem( BreakIterator.KIND_WORD,      ULocale.ROOT,               posxWordText, posxWordROffsets ),
             new TBItem( BreakIterator.KIND_SENTENCE,  new ULocale("el"),          elSentText,   elSentTOffsets   ),
             new TBItem( BreakIterator.KIND_SENTENCE,  ULocale.ROOT,               elSentText,   elSentROffsets   ),
             new TBItem( BreakIterator.KIND_CHARACTER, new ULocale("th"),          thCharText,   thCharTOffsets   ),
