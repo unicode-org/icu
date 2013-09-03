d2d0d0d9f0e7ab4af26e06b4610daf6e4e8f7bb7
Scott Russell <DTownSMR@gmail.com>
2013-09-03 22:26:48 +0000

    ICU-10249 Implement new Islamic calendar variants tbla, rgsa
    X-SVN-Rev: 34172

--- icu4j/main/tests/core/src/com/ibm/icu/dev/test/calendar/IslamicTest.java
+++ icu4j/main/tests/core/src/com/ibm/icu/dev/test/calendar/IslamicTest.java
@@ -494,4 +494,56 @@ public class IslamicTest extends CalendarTest {
         }
     }
     
+    public void Test10249() {
+        try {
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");            
+            Date date = formatter.parse("1975-05-06");
+            IslamicCalendar is_cal = new IslamicCalendar();
+            is_cal.setType(CalculationType.ISLAMIC_CIVIL);
+            is_cal.setTime(date);
+            IslamicCalendar is_cal2 = new IslamicCalendar();
+            is_cal2.setType(CalculationType.ISLAMIC_TBLA);
+            is_cal2.setTime(date);
+            
+            int is_day = is_cal.get(Calendar.DAY_OF_MONTH);
+            int is_day2 = is_cal2.get(Calendar.DAY_OF_MONTH);
+            if(is_day2 - is_day != 1)
+                errln("unexpected difference between civil and tbla: "+is_day2+" : "+is_day);
+
+        }catch(Exception e){
+            errln(e.getLocalizedMessage());
+        }
+    }
+
+    public void TestCreationByLocale() {
+        ULocale islamicLoc = new ULocale("ar_SA@calendar=islamic-umalqura"); 
+        IslamicCalendar is_cal = new IslamicCalendar(islamicLoc);
+        String thisCalcType = is_cal.getType(); 
+        if(!"islamic-umalqura".equalsIgnoreCase(thisCalcType)) {
+            errln("non umalqura calc type generated - " + thisCalcType);
+        }
+
+        islamicLoc = new ULocale("ar_SA@calendar=islamic-civil"); 
+        is_cal = new IslamicCalendar(islamicLoc);
+        thisCalcType = is_cal.getType(); 
+        if(!"islamic-civil".equalsIgnoreCase(thisCalcType)) {
+            errln("non civil calc type generated - " + thisCalcType);
+        }
+
+        islamicLoc = new ULocale("ar_SA@calendar=islamic-tbla"); 
+        is_cal = new IslamicCalendar(islamicLoc);
+        thisCalcType = is_cal.getType(); 
+        if(!"islamic-tbla".equalsIgnoreCase(thisCalcType)) {
+            errln("non tbla calc type generated - " + thisCalcType);
+        }
+
+        islamicLoc = new ULocale("ar_SA@calendar=islamic-xyzzy"); 
+        is_cal = new IslamicCalendar(islamicLoc);
+        thisCalcType = is_cal.getType(); 
+        if(!"islamic".equalsIgnoreCase(thisCalcType)) {
+            errln("incorrect default calc type generated - " + thisCalcType);
+        }
+
+    }
+
 }
