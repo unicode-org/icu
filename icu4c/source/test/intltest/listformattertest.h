a99ea9c1387719c67af95ece7a9c44844ea1c11d
Travis Keep <keep94@gmail.com>
2013-09-17 18:01:18 +0000

    ICU-9946 Add test for ede case of formatting {0}, {1}, {2}
    X-SVN-Rev: 34356

--- icu4c/source/test/intltest/listformattertest.h
+++ icu4c/source/test/intltest/listformattertest.h
@@ -1,7 +1,7 @@
 /*
 *******************************************************************************
 *
-*   Copyright (C) 2012, International Business Machines
+*   Copyright (C) 2012-2013, International Business Machines
 *   Corporation and others.  All Rights Reserved.
 *
 *******************************************************************************
@@ -35,6 +35,7 @@ class ListFormatterTest : public IntlTest {
     void TestMalayalam();
     void TestZulu();
     void TestOutOfOrderPatterns();
+    void Test9946();
 
   private:
     void CheckFormatting(const ListFormatter* formatter, UnicodeString data[], int32_t data_size, const UnicodeString& expected_result);
