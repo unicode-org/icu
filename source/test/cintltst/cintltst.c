a4b1dc71a353e66107aedce62e6a969555a5f9ea
Steven R. Loomis <srl@icu-project.org>
2013-09-17 00:55:39 +0000

    remove timebomb code, update docs for logKnownIssue and log_knownIssue
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/9744
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@34341 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/test/cintltst/cintltst.c
+++ source/test/cintltst/cintltst.c
@@ -689,31 +689,5 @@ U_CFUNC UBool assertEquals(const char* message, const char* expected,
 #endif
     return TRUE;
 }
-/*--------------------------------------------------------------------
- * Time bomb - allows temporary behavior that expires at a given
- *             release
- *--------------------------------------------------------------------
- */
-
-U_CFUNC UBool isICUVersionBefore(int major, int minor, int milli) {
-  log_knownIssue("9744", "fix call to isICUVersionBefore()");
-  return TRUE;
-  /*
-    UVersionInfo iv;
-    UVersionInfo ov;
-    ov[0] = (uint8_t)major;
-    ov[1] = (uint8_t)minor;
-    ov[2] = (uint8_t)milli;
-    ov[3] = 0;
-    u_getVersion(iv);
-    return uprv_memcmp(iv, ov, U_MAX_VERSION_LENGTH) < 0;
-  */
-}
-
-U_CFUNC UBool isICUVersionAtLeast(int major, int minor, int milli) {
-  log_knownIssue("9744", "fix call to isICUVersionAtLeast()");
-  return FALSE;
-  //return !isICUVersionBefore(major, minor, milli);
-}
 
 #endif
