334e8f52b891a764a4093787acab2864fef2b4be
Yoshito Umaoka <y.umaoka@gmail.com>
2013-10-14 22:21:29 +0000

    ICU-10419 Removed unused import introduced by the previous change.
    X-SVN-Rev: 34566

--- icu4j/main/tests/core/src/com/ibm/icu/dev/test/format/NumberFormatTest.java
+++ icu4j/main/tests/core/src/com/ibm/icu/dev/test/format/NumberFormatTest.java
@@ -18,9 +18,7 @@ import java.text.FieldPosition;
 import java.text.ParseException;
 import java.text.ParsePosition;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
 import java.util.Set;
 
 import com.ibm.icu.dev.test.TestUtil;
