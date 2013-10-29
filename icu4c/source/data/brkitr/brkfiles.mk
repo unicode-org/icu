a722f507e01e5e39e38de887116125171090052a
Jonathan Lai <jali01@svn.icu-project.org>
2013-10-29 15:50:54 +0000

    [ICU-10480] ULI exception data to ICU format, ICU side X-Commit-URL: https://ssl.icu-project.org/trac/changeset/34621 Former-commit-id: 2edc33a5da569985ef8fd90a79b7d6f7f1c6246b

--- icu4c/source/data/brkitr/brkfiles.mk
+++ icu4c/source/data/brkitr/brkfiles.mk
@@ -1,6 +1,6 @@
 # *   Copyright (C) 1998-2013, International Business Machines
 # *   Corporation and others.  All Rights Reserved.
-BRK_RES_CLDR_VERSION = 24
+BRK_RES_CLDR_VERSION = 25
 # A list of txt's to build
 # Note:
 #
