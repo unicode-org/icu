2f0c821f16794fd49696877c90afe5d86b4975ec
Yoshito Umaoka <y.umaoka@gmail.com>
2013-11-06 22:09:21 +0000

    ICU-10515 Moved the top level TestAll to a new project icu4j-testall.
    X-SVN-Rev: 34644

--- icu4j/main/tests/testall/src/com/ibm/icu/dev/test/TestAll.java
+++ icu4j/main/tests/testall/src/com/ibm/icu/dev/test/TestAll.java
@@ -1,6 +1,6 @@
 /*
  *******************************************************************************
- * Copyright (C) 1996-2009, International Business Machines Corporation and    *
+ * Copyright (C) 1996-2013, International Business Machines Corporation and    *
  * others. All Rights Reserved.                                                *
  *******************************************************************************
  */
