9218c6a4cbc544008b8c5c6445453b9328ba9f20
Travis Keep <rocketman@google.com>
2013-12-18 22:32:17 +0000

    [ICU-10603] Add NUMERIC to MeasureFormat.FormatWidth. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/34797 Former-commit-id: 3aaef6ba0e10348c7641eb490237c31e795c4e2d

--- icu4j/main/tests/core/src/com/ibm/icu/dev/test/format/MeasureUnitTest.java
+++ icu4j/main/tests/core/src/com/ibm/icu/dev/test/format/MeasureUnitTest.java
@@ -70,6 +70,22 @@ public class MeasureUnitTest extends TestFmwk {
         TimeUnitAmount[] _1m_59_9996s = {
                 new TimeUnitAmount(1.0, TimeUnit.MINUTE),
                 new TimeUnitAmount(59.9996, TimeUnit.SECOND)};
+        TimeUnitAmount[] _5h_17m = {
+                new TimeUnitAmount(5.0, TimeUnit.HOUR),
+                new TimeUnitAmount(17.0, TimeUnit.MINUTE)};
+        TimeUnitAmount[] _19m_28s = {
+                new TimeUnitAmount(19.0, TimeUnit.MINUTE),
+                new TimeUnitAmount(28.0, TimeUnit.SECOND)};
+        TimeUnitAmount[] _0h_0m_17s = {
+                new TimeUnitAmount(0.0, TimeUnit.HOUR),
+                new TimeUnitAmount(0.0, TimeUnit.MINUTE),
+                new TimeUnitAmount(17.0, TimeUnit.SECOND)};
+        TimeUnitAmount[] _6h_56_92m = {
+                new TimeUnitAmount(6.0, TimeUnit.HOUR),
+                new TimeUnitAmount(56.92, TimeUnit.MINUTE)};
+        TimeUnitAmount[] _3h_5h = {
+                new TimeUnitAmount(3.0, TimeUnit.HOUR),
+                new TimeUnitAmount(5.0, TimeUnit.HOUR)};
         
         Object[][] fullData = {
                 {_1m_59_9996s, "1 minute, 59.9996 seconds"},
@@ -85,21 +101,27 @@ public class MeasureUnitTest extends TestFmwk {
                 {_1h_23_5m, "1 hr, 23.5 mins"},
                 {_1h_0m_23s, "1 hr, 0 mins, 23 secs"},
                 {_2y_5M_3w_4d, "2 yrs, 5 mths, 3 wks, 4 days"}};
+        Object[][] narrowData = {
+                {_1m_59_9996s, "1 min, 59.9996 secs"},
+                {_19m, "19 mins"},
+                {_1h_23_5s, "1 hr, 23.5 secs"},
+                {_1h_23_5m, "1 hr, 23.5 mins"},
+                {_1h_0m_23s, "1 hr, 0 mins, 23 secs"},
+                {_2y_5M_3w_4d, "2 yrs, 5 mths, 3 wks, 4 days"}};
         
-        // TODO(Travis Keep): We need to support numeric formatting. Either here or in TimeUnitFormat.
-        /*
+        
         Object[][] numericData = {
                 {_1m_59_9996s, "1:59.9996"},
-                {_19m, "19 mins"},
+                {_19m, "19m"},
                 {_1h_23_5s, "1:00:23.5"},
                 {_1h_0m_23s, "1:00:23"},
                 {_1h_23_5m, "1:23.5"},
                 {_5h_17m, "5:17"},
                 {_19m_28s, "19:28"},
-                {_2y_5M_3w_4d, "2 yrs, 5 mths, 3 wks, 4 days"},
+                {_2y_5M_3w_4d, "2y, 5m, 3w, 4d"},
                 {_0h_0m_17s, "0:00:17"},
-                {_6h_56_92m, "6:56.92"}};
-        */
+                {_6h_56_92m, "6:56.92"},
+                {_3h_5h, "3h, 5h"}};
         
         NumberFormat nf = NumberFormat.getNumberInstance(ULocale.ENGLISH);
         nf.setMaximumFractionDigits(4);
@@ -107,6 +129,8 @@ public class MeasureUnitTest extends TestFmwk {
         verifyFormatPeriod("en FULL", mf, fullData);
         mf = MeasureFormat.getInstance(ULocale.ENGLISH, FormatWidth.SHORT, nf);
         verifyFormatPeriod("en SHORT", mf, abbrevData);
+        mf = MeasureFormat.getInstance(ULocale.ENGLISH, FormatWidth.NUMERIC, nf);
+        verifyFormatPeriod("en NUMERIC", mf, numericData);   
     }
     
 
