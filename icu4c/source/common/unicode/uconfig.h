6215a951834bd0c5c1fb7279cb19f4bcd2dc923f
Michael Ow <mow@svn.icu-project.org>
2014-01-09 19:18:40 +0000

    ICU-10617 Fix library suffix macro definition
    X-SVN-Rev: 34845

--- icu4c/source/common/unicode/uconfig.h
+++ icu4c/source/common/unicode/uconfig.h
@@ -1,6 +1,6 @@
 /*  
 **********************************************************************
-*   Copyright (C) 2002-2013, International Business Machines
+*   Copyright (C) 2002-2014, International Business Machines
 *   Corporation and others.  All Rights Reserved.
 **********************************************************************
 *   file name:  uconfig.h
@@ -185,7 +185,8 @@
 #ifdef U_LIB_SUFFIX_C_NAME_STRING
     /* Use the predefined value. */
 #elif defined(U_LIB_SUFFIX_C_NAME)
-#   define U_LIB_SUFFIX_C_NAME_STRING #U_LIB_SUFFIX_C_NAME
+#   define CONVERT_TO_STRING(s) #s
+#   define U_LIB_SUFFIX_C_NAME_STRING CONVERT_TO_STRING(U_LIB_SUFFIX_C_NAME)
 #else
 #   define U_LIB_SUFFIX_C_NAME_STRING ""
 #endif
