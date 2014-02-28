78b241a52b59876e2cdc51355e2fa0d5f171c655
Markus Scherer <markus.icu@gmail.com>
2014-02-28 20:15:58 +0000

    ICU-9959 test that RBC forbids rules with a before-reset followed by a stronger relation
    X-SVN-Rev: 35263

--- icu4c/source/test/intltest/regcoll.h
+++ icu4c/source/test/intltest/regcoll.h
@@ -239,6 +239,7 @@ public:
     void TestCaseFirstCompression();
 
     void TestTrailingComment();
+    void TestBeforeWithTooStrongAfter();
 
 private:
     //------------------------------------------------------------------------
