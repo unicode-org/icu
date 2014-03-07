cf24d91085120353e451cae3704a4a79a2e784ce
Steven R. Loomis <srl@icu-project.org>
2014-03-07 01:01:49 +0000

    [ICU-10272] fix break on Windows X-Commit-URL: https://ssl.icu-project.org/trac/changeset/35378 Former-commit-id: 62d1f4ce45f3c345bc478df6c27b8745daa037ef

--- icu4c/source/common/utypeinfo.h
+++ icu4c/source/common/utypeinfo.h
@@ -21,7 +21,8 @@
 #if defined(_MSC_VER) && _HAS_EXCEPTIONS == 0
 #include <exception>
 using std::exception;
-#else
+#endif
+#if !defined(_MSC_VER)
 namespace std { class type_info; } // WORKAROUND: http://llvm.org/bugs/show_bug.cgi?id=13364
 #endif
 #include <typeinfo>  // for 'typeid' to work
