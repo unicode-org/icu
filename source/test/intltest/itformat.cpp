f6b5a9704b1bcabd15373c463d31edb7ab4c8fbd
Travis Keep <rocketman@google.com>
2014-04-09 23:00:45 +0000

    Introduce ScientificFormatHelper class.
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/10046
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@35612 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/test/intltest/itformat.cpp
+++ source/test/intltest/itformat.cpp
@@ -63,6 +63,7 @@ extern IntlTest *createCompactDecimalFormatTest();
 extern IntlTest *createGenderInfoTest();
 extern IntlTest *createRelativeDateTimeFormatterTest();
 extern IntlTest *createMeasureFormatTest();
+extern IntlTest *createScientificFormatHelperTest();
 
 #define TESTCLASS(id, TestClass)          \
     case id:                              \
@@ -174,6 +175,15 @@ void IntlTestFormat::runIndexedTest( int32_t index, UBool exec, const char* &nam
             logln("MeasureFormatTest test---");
             logln((UnicodeString)"");
             LocalPointer<IntlTest> test(createMeasureFormatTest());
+            callTest(*test, par);
+          }
+          break;
+        case 48:
+          name = "ScientificFormatHelperTest";
+          if (exec) {
+            logln("ScientificFormatHelperTest test---");
+            logln((UnicodeString)"");
+            LocalPointer<IntlTest> test(createScientificFormatHelperTest());
             callTest(*test, par);
           }
           break;
