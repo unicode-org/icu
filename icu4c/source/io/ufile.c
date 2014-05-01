5a05346a03a724099008a0bd3f8ae1376981cb7f
Michael Ow <mow@svn.icu-project.org>
2014-05-01 17:45:12 +0000

    [ICU-10038] Add u_fopen variant that accepts UTF16 filename X-Commit-URL: https://ssl.icu-project.org/trac/changeset/35681 Former-commit-id: 6feccd3f49a1e572151ace77ce05de31571d4092

--- icu4c/source/io/ufile.c
+++ icu4c/source/io/ufile.c
@@ -1,7 +1,7 @@
 /*
 ******************************************************************************
 *
-*   Copyright (C) 1998-2013, International Business Machines
+*   Copyright (C) 1998-2014, International Business Machines
 *   Corporation and others.  All Rights Reserved.
 *
 ******************************************************************************
@@ -31,6 +31,7 @@
 #include "unicode/uloc.h"
 #include "unicode/ures.h"
 #include "unicode/ucnv.h"
+#include "unicode/ustring.h"
 #include "cstring.h"
 #include "cmemory.h"
 
@@ -144,6 +145,35 @@ u_fopen(const char    *filename,
         fclose(systemFile);
     }
 
+    return result; /* not a file leak */
+}
+
+U_CAPI UFILE* U_EXPORT2
+u_fopen_u(const UChar   *filename,
+        const char    *perm,
+        const char    *locale,
+        const char    *codepage)
+{
+    UFILE     *result;
+    char buffer[256];
+
+    u_austrcpy(buffer, filename);
+
+    result = u_fopen(buffer, perm, locale, codepage);
+#if U_PLATFORM_USES_ONLY_WIN32_API
+    /* Try Windows API _wfopen if the above fails. */
+    if (!result) {
+        FILE *systemFile = _wfopen(filename, (UChar*)perm);
+        if (systemFile) {
+            result = finit_owner(systemFile, locale, codepage, TRUE);
+        }
+        if (!result) {
+            /* Something bad happened.
+               Maybe the converter couldn't be opened. */
+            fclose(systemFile);
+        }
+    }
+#endif
     return result; /* not a file leak */
 }
 
