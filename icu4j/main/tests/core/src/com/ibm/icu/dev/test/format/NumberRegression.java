6dc89ee2672a3699949f8bacacdfc7d87d52c795
Peter Edberg <pedberg@unicode.org>
2014-09-03 02:52:13 +0000

    ICU-11173 Update logKnownIssue #11234 in NumberRegression/Test4212072 to include locale "bm"
    X-SVN-Rev: 36317

--- icu4j/main/tests/core/src/com/ibm/icu/dev/test/format/NumberRegression.java
+++ icu4j/main/tests/core/src/com/ibm/icu/dev/test/format/NumberRegression.java
@@ -1548,7 +1548,8 @@ public class NumberRegression extends com.ibm.icu.dev.test.TestFmwk {
         // trip properly.  Test stream in/out integrity too.
         Locale[] avail = NumberFormat.getAvailableLocales();
         for (int i=0; i<avail.length; ++i) {
-            if (avail[i].getLanguage().equals("ji") && logKnownIssue("11234", "Symbol roundtrip issues for locale ji")) {
+            if ((avail[i].getLanguage().equals("ji") || avail[i].getLanguage().equals("bm")) &&
+                    logKnownIssue("11234", "Symbol roundtrip issues for locales ji, bm")) {
                 continue;
             }
             for (int j=0; j<3; ++j) {
