c6aff82e980d3d4e4fd746949c968d9fc909d952
Yoshito Umaoka <y.umaoka@gmail.com>
2014-09-26 20:09:39 +0000

    [ICU-10898] tzdata2014h updates in ICU4C X-Commit-URL: https://ssl.icu-project.org/trac/changeset/36592 Former-commit-id: 632fa29d7e83ddc4810edd001635e3c9592d42fa

--- icu4c/source/test/intltest/tztest.cpp
+++ icu4c/source/test/intltest/tztest.cpp
@@ -853,7 +853,7 @@ void TimeZoneTest::TestShortZoneIDs()
         {"UTC", 0, FALSE},    // Olson etcetera 0
         {"ECT", 60, TRUE},    // ICU Link - Europe/Paris
         {"MET", 60, TRUE},    // Olson europe 1:00 C-Eur
-        {"CAT", 120, FALSE},  // ICU Link - Africa/Harare
+        {"CAT", 120, FALSE},  // ICU Link - Africa/Maputo
         {"ART", 120, TRUE},   // ICU Link - Africa/Cairo
         {"EET", 120, TRUE},   // Olson europe 2:00 EU
         {"EAT", 180, FALSE},  // ICU Link - Africa/Addis_Ababa
@@ -952,7 +952,7 @@ void TimeZoneTest::TestShortZoneIDs()
         "AST", "America/Anchorage",
         "BET", "America/Sao_Paulo",
         "BST", "Asia/Dhaka", // # spelling changed in 2000h; was Asia/Dacca
-        "CAT", "Africa/Harare",
+        "CAT", "Africa/Maputo",
         "CNT", "America/St_Johns",
         "CST", "America/Chicago",
         "CTT", "Asia/Shanghai",
@@ -1974,23 +1974,32 @@ void TimeZoneTest::TestCanonicalID() {
         const char *zone;
     } excluded1[] = {
         {"Africa/Bamako", "Africa/Abidjan"},
+        {"Africa/Bangui", "Africa/Lagos"},
         {"Africa/Banjul", "Africa/Abidjan"},
-        {"Africa/Brazzaville", "Africa/Bangui"},
+        {"Africa/Blantyre", "Africa/Maputo"},
+        {"Africa/Brazzaville", "Africa/Lagos"},
+        {"Africa/Bujumbura", "Africa/Maputo"},
         {"Africa/Conakry", "Africa/Abidjan"},
         {"Africa/Dakar", "Africa/Abidjan"},
-        {"Africa/Douala", "Africa/Bangui"},
+        {"Africa/Douala", "Africa/Lagos"},
         {"Africa/Freetown", "Africa/Abidjan"},
+        {"Africa/Gaborone", "Africa/Maputo"},
+        {"Africa/Harare", "Africa/Maputo"},
         {"Africa/Khartoum", "Africa/Juba"},
-        {"Africa/Kinshasa", "Africa/Bangui"},
-        {"Africa/Lagos", "Africa/Bangui"},
-        {"Africa/Libreville", "Africa/Bangui"},
+        {"Africa/Kigali", "Africa/Maputo"},
+        {"Africa/Kinshasa", "Africa/Lagos"},
+        {"Africa/Libreville", "Africa/Lagos"},
         {"Africa/Lome", "Africa/Abidjan"},
-        {"Africa/Luanda", "Africa/Bangui"},
-        {"Africa/Malabo", "Africa/Bangui"},
-        {"Africa/Niamey", "Africa/Bangui"},
+        {"Africa/Luanda", "Africa/Lagos"},
+        {"Africa/Lubumbashi", "Africa/Maputo"},
+        {"Africa/Lusaka", "Africa/Maputo"},
+        {"Africa/Maseru", "Africa/Johannesburg"},
+        {"Africa/Malabo", "Africa/Lagos"},
+        {"Africa/Mbabane", "Africa/Johannesburg"},
+        {"Africa/Niamey", "Africa/Lagos"},
         {"Africa/Nouakchott", "Africa/Abidjan"},
         {"Africa/Ouagadougou", "Africa/Abidjan"},
-        {"Africa/Porto-Novo", "Africa/Bangui"},
+        {"Africa/Porto-Novo", "Africa/Lagos"},
         {"Africa/Sao_Tome", "Africa/Abidjan"},
         {"America/Curacao", "America/Aruba"},
         {"America/Dominica", "America/Anguilla"},
