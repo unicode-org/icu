58fc84970d139dacde1ba20468af39549bb578f2
Travis Keep <rocketman@google.com>
2014-11-18 00:55:15 +0000

    Introduce ScientificNumberFormatter.
    X-Trac-URL: https://ssl.icu-project.org/trac/ticket/10952
    
    
    git-svn-id: http://source.icu-project.org/repos/icu/icu/trunk@36746 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/i18n/scientificformathelper.cpp
+++ source/i18n/scientificformathelper.cpp
@@ -9,18 +9,10 @@
 #if !UCONFIG_NO_FORMATTING
 
 #include "unicode/scientificformathelper.h"
-#include "unicode/dcfmtsym.h"
-#include "unicode/fpositer.h"
-#include "unicode/utf16.h"
-#include "unicode/uniset.h"
+#include "unicode/scientificnumberformatter.h"
 #include "decfmtst.h"
 
 U_NAMESPACE_BEGIN
-
-static const UChar kSuperscriptDigits[] = {0x2070, 0xB9, 0xB2, 0xB3, 0x2074, 0x2075, 0x2076, 0x2077, 0x2078, 0x2079};
-
-static const UChar kSuperscriptPlusSign = 0x207A;
-static const UChar kSuperscriptMinusSign = 0x207B;
 
 ScientificFormatHelper::ScientificFormatHelper(
         const DecimalFormatSymbols &dfs, UErrorCode &status)
@@ -28,10 +20,7 @@ ScientificFormatHelper::ScientificFormatHelper(
     if (U_FAILURE(status)) {
         return;
     }
-    fPreExponent.append(dfs.getConstSymbol(
-            DecimalFormatSymbols::kExponentMultiplicationSymbol));
-    fPreExponent.append(dfs.getSymbol(DecimalFormatSymbols::kOneDigitSymbol));
-    fPreExponent.append(dfs.getSymbol(DecimalFormatSymbols::kZeroDigitSymbol));
+    ScientificNumberFormatter::getPreExponent(dfs, fPreExponent);
     fStaticSets = DecimalFormatStaticSets::getStaticSets(status);
 }
 
@@ -64,57 +53,14 @@ UnicodeString &ScientificFormatHelper::insertMarkup(
     if (U_FAILURE(status)) {
         return result;
     }
-    FieldPosition fp;
-    int32_t copyFromOffset = 0;
-    UBool exponentSymbolFieldPresent = FALSE;
-    UBool exponentFieldPresent = FALSE;
-    while (fpi.next(fp)) {
-        switch (fp.getField()) {
-        case UNUM_EXPONENT_SYMBOL_FIELD:
-            exponentSymbolFieldPresent = TRUE;
-            result.append(s, copyFromOffset, fp.getBeginIndex() - copyFromOffset);
-            copyFromOffset = fp.getEndIndex();
-            result.append(fPreExponent);
-            result.append(beginMarkup);
-            break;
-        case UNUM_EXPONENT_FIELD:
-            exponentFieldPresent = TRUE;
-            result.append(s, copyFromOffset, fp.getEndIndex() - copyFromOffset);
-            copyFromOffset = fp.getEndIndex();
-            result.append(endMarkup);
-            break;
-        default:
-            break;
-        }
-    }
-    if (!exponentSymbolFieldPresent || !exponentFieldPresent) {
-      status = U_ILLEGAL_ARGUMENT_ERROR;
-      return result;
-    }
-    result.append(s, copyFromOffset, s.length() - copyFromOffset);
-    return result;
-}
-
-static UBool copyAsSuperscript(
-        const UnicodeString &s,
-        int32_t beginIndex,
-        int32_t endIndex,
-        UnicodeString &result,
-        UErrorCode &status) {
-    if (U_FAILURE(status)) {
-        return FALSE;
-    }
-    for (int32_t i = beginIndex; i < endIndex;) {
-        UChar32 c = s.char32At(i);
-        int32_t digit = u_charDigitValue(c);
-        if (digit < 0) {
-            status = U_INVALID_CHAR_FOUND;
-            return FALSE;
-        }
-        result.append(kSuperscriptDigits[digit]);
-        i += U16_LENGTH(c);
-    }
-    return TRUE;
+    ScientificNumberFormatter::MarkupStyle style(beginMarkup, endMarkup);
+    return style.format(
+            s,
+            fpi,
+            fPreExponent,
+            *fStaticSets,
+            result,
+            status);
 }
 
 UnicodeString &ScientificFormatHelper::toSuperscriptExponentDigits(
@@ -125,55 +71,14 @@ UnicodeString &ScientificFormatHelper::toSuperscriptExponentDigits(
     if (U_FAILURE(status)) {
       return result;
     }
-    FieldPosition fp;
-    int32_t copyFromOffset = 0;
-    UBool exponentSymbolFieldPresent = FALSE;
-    UBool exponentFieldPresent = FALSE;
-    while (fpi.next(fp)) {
-        switch (fp.getField()) {
-        case UNUM_EXPONENT_SYMBOL_FIELD:
-            exponentSymbolFieldPresent = TRUE;
-            result.append(s, copyFromOffset, fp.getBeginIndex() - copyFromOffset);
-            copyFromOffset = fp.getEndIndex();
-            result.append(fPreExponent);
-            break;
-        case UNUM_EXPONENT_SIGN_FIELD:
-            {
-                int32_t beginIndex = fp.getBeginIndex();
-                int32_t endIndex = fp.getEndIndex();
-                UChar32 aChar = s.char32At(beginIndex);
-                if (fStaticSets->fMinusSigns->contains(aChar)) {
-                    result.append(s, copyFromOffset, beginIndex - copyFromOffset);
-                    result.append(kSuperscriptMinusSign);
-                } else if (fStaticSets->fPlusSigns->contains(aChar)) {
-                    result.append(s, copyFromOffset, beginIndex - copyFromOffset);
-                    result.append(kSuperscriptPlusSign);
-                } else {
-                    status = U_INVALID_CHAR_FOUND;
-                    return result;
-                }
-                copyFromOffset = endIndex;
-            }
-            break;
-        case UNUM_EXPONENT_FIELD:
-            exponentFieldPresent = TRUE;
-            result.append(s, copyFromOffset, fp.getBeginIndex() - copyFromOffset);
-            if (!copyAsSuperscript(
-                    s, fp.getBeginIndex(), fp.getEndIndex(), result, status)) {
-              return result;
-            }
-            copyFromOffset = fp.getEndIndex();
-            break;
-        default:
-            break;
-        }
-    }
-    if (!exponentSymbolFieldPresent || !exponentFieldPresent) {
-      status = U_ILLEGAL_ARGUMENT_ERROR;
-      return result;
-    }
-    result.append(s, copyFromOffset, s.length() - copyFromOffset);
-    return result;
+    ScientificNumberFormatter::SuperscriptStyle style;
+    return style.format(
+            s,
+            fpi,
+            fPreExponent,
+            *fStaticSets,
+            result,
+            status);
 }
 
 U_NAMESPACE_END
