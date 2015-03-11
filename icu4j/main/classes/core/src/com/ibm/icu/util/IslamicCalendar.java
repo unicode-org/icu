cc391cc9f41721dba5cfa9687d704d0cabf70cdc
Yoshito Umaoka <y.umaoka@gmail.com>
2015-03-11 18:54:31 +0000

    [ICU-11565] Fixed Eclipse compiler warnings. Updated ICU4J project settings to detect [potentail] resource leak as warning, except icu-demos, icu-build-tools and icu-tools. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/37213 Former-commit-id: fe0888ab4491d5deee358c919eef07af3342459e

--- icu4j/main/classes/core/src/com/ibm/icu/util/IslamicCalendar.java
+++ icu4j/main/classes/core/src/com/ibm/icu/util/IslamicCalendar.java
@@ -64,11 +64,11 @@ import com.ibm.icu.util.ULocale.Category;
  * Like the Islamic religious calendar, <em>Umm al-Qura</em> is also based 
  * on the sighting method of the crescent moon but is standardized by Saudi Arabia.
  * <p>  
- * The {@link #setType setType} method determines
+ * The {@link #setCalculationType(CalculationType) setCalculationType} method determines
  * which approach is used to determine the start of a month.  By default, the
- * fixed-cycle <em>civil</em> calendar is used.  However, if <code>setType(ISLAMIC)</code>
+ * fixed-cycle <em>civil</em> calendar is used.  However, if <code>setCalculationType(ISLAMIC)</code>
  * is called, an approximation of the true lunar calendar will be used.
- * Similarly, if <code>setType(ISLAMIC_UMALQURA)</code> is called, an approximation 
+ * Similarly, if <code>setCalculationType(ISLAMIC_UMALQURA)</code> is called, an approximation 
  * of the Umm al-Qura lunar calendar will be used.
  * <p>
  * This class should not be subclassed.</p>
