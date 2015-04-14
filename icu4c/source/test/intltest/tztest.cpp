1b0a88f5e7fa1d176676a4bbf4c612bf5a198d0d
Michael Ow <mow@svn.icu-project.org>
2015-04-14 21:24:11 +0000

    [ICU-11630] Update tzdata in ICU4C to 2015c X-Commit-URL: https://ssl.icu-project.org/trac/changeset/37335 Former-commit-id: 197a29afdaa2df20d10a16234f6a4dead87c46fc

--- icu4c/source/test/intltest/tztest.cpp
+++ icu4c/source/test/intltest/tztest.cpp
@@ -2031,6 +2031,7 @@ void TimeZoneTest::TestCanonicalID() {
         {"America/St_Lucia", "America/Port_of_Spain"},
         {"America/St_Thomas", "America/Port_of_Spain"},
         {"America/St_Vincent", "America/Port_of_Spain"},
+        {"America/Toronto", "America/Montreal"},
         {"America/Tortola", "America/Port_of_Spain"},
         {"America/Virgin", "America/Port_of_Spain"},
         {"Antarctica/South_Pole", "Antarctica/McMurdo"},
