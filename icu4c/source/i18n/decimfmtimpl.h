dd0466cdad72b0b0d85a17f97edfcc8f7502693a
Travis Keep <keep94@gmail.com>
2015-08-28 23:23:38 +0000

    ICU-11740 Add documentation to DecimalFormatImpl class.
    X-SVN-Rev: 37848

--- icu4c/source/i18n/decimfmtimpl.h
+++ icu4c/source/i18n/decimfmtimpl.h
@@ -28,6 +28,126 @@ class ValueFormatter;
 class FieldPositionHandler;
 class FixedDecimal;
 
+/**
+ * DecimalFormatImpl is the glue code between the legacy DecimalFormat class
+ * and the new decimal formatting classes. DecimalFormat still handles
+ * parsing directly. However, DecimalFormat uses attributes of this class
+ * for parsing when possible.
+ *
+ * The public API of this class closely mirrors the legacy API of the
+ * legacy DecimalFormat deviating only when the legacy API does not make
+ * sense. For example, although DecimalFormat has a
+ * getPadCharacterString() method, DecimalFormatImpl has a getPadCharacter()
+ * method because formatting uses only a single pad character for padding.
+ *
+ * Each legacy DecimalFormat instance heap allocates its own instance of
+ * this class. Most DecimalFormat methods that deal with formatting simply
+ * delegate to the DecimalFormat's DecimalFormatImpl method.
+ *
+ * Because DecimalFormat extends NumberFormat, Each instance of this class
+ * "borrows" a pointer to the NumberFormat part of its enclosing DecimalFormat
+ * instance. This way each DecimalFormatImpl instance can read or even modify
+ * the NumberFormat portion of its enclosing DecimalFormat instance.
+ *
+ * Directed acyclic graph (DAG):
+ *
+ * This class can be represented as a directed acyclic graph (DAG) where each
+ * vertex is an attribute, and each directed edge indicates that the value
+ * of the destination attribute is calculated from the value of the source
+ * attribute. Attributes with setter methods reside at the bottom of the
+ * DAG. That is, no edges point to them. We call these independent attributes
+ * because their values can be set independently of one another. The rest of
+ * the attributes are derived attributes because their values depend on the
+ * independent attributes. DecimalFormatImpl often uses the derived
+ * attributes, not the independent attributes, when formatting numbers.
+ *
+ * The independent attributes at the bottom of the DAG correspond to the legacy
+ * attributes of DecimalFormat while the attributes at the top of the DAG
+ * correspond to the attributes of the new code. The edges of the DAG
+ * correspond to the code that handles the complex interaction among all the
+ * legacy attributes of the DecimalFormat API.
+ *
+ * We use a DAG for three reasons.
+ *
+ * First, the DAG preserves backward compatibility. Clients of the legacy
+ * DecimalFormat expect existing getters and setters of each attribute to be
+ * consistent. That means if a client sets a particular attribute to a new
+ * value, the attribute should retain that value until the client sets it to
+ * a new value. The DAG allows these attributes to remain consistent even
+ * though the new code may not use them when formatting.
+ *
+ * Second, the DAG obviates the need to recalculate derived attributes with
+ * each format. Instead, the DAG "remembers" the values of all derived
+ * attributes. Only setting an independent attribute requires a recalculation.
+ * Moreover, setting an independent attribute recalculates only the affected
+ * dependent attributes rather than all dependent attributes.
+ *
+ * Third, the DAG abstracts away the complex interaction among the legacy
+ * attributes of the DecimalFormat API.
+ *
+ * Only the independent attributes of the DAG have setters and getters.
+ * Derived attributes have no setters (and often no getters either).
+ *
+ * Copy and assign:
+ *
+ * For copy and assign, DecimalFormatImpl copies and assigns every attribute
+ * regardless of whether or not it is independent. We do this for simplicity.
+ *
+ * Implementation of the DAG:
+ *
+ * The DAG consists of three smaller DAGs:
+ * 1. Grouping attributes
+ * 2. Precision attributes
+ * 3. Formatting attributes.
+ *
+ * The first two DAGs are simple in that setting any independent attribute
+ * in the DAG recalculates all the dependent attributes in that DAG.
+ * The updateGrouping() and updatePrecision() perform the respective
+ * recalculations. 
+ *
+ * Because some of the derived formatting attributes are expensive to
+ * calculate, the formatting attributes DAG is more complex. The
+ * updateFormatting() method is composed of many updateFormattingXXX()
+ * methods, each of which recalculates a single derived attribute. The
+ * updateFormatting() method accepts a bitfield of recently changed
+ * attributes and passes this bitfield by reference to each of the
+ * updateFormattingXXX() methods. Each updateFormattingXXX() method checks
+ * the bitfield to see if any of the attributes it uses to compute the XXX
+ * attribute changed. If none of them changed, it exists immediately. However,
+ * if at least one of them changed, it recalculates the XXX attribute and
+ * sets the corresponding bit in the bitfield. In this way, each
+ * updateFormattingXXX() method encodes the directed edges in the formatting
+ * DAG that point to the attribute its calculating.
+ *
+ * Maintenance of the updateFormatting() method.
+ *
+ * Use care when changing the updateFormatting() method.
+ * The updateFormatting() method must call each updateFormattingXXX() in the
+ * same partial order that the formatting DAG prescribes. That is, the
+ * attributes near the bottom of the DAG must be calculated before attributes
+ * further up. As we mentioned in the prvious paragraph, the directed edges of
+ * the formatting DAG are encoded within each updateFormattingXXX() method.
+ * Finally, adding new attributes may involve adding to the bitmap that the
+ * updateFormatting() method uses. The top most attributes in the DAG,
+ * those that do not point to any attributes but only have attributes
+ * pointing to it, need not have a slot in the bitmap.
+ *
+ * Keep in mind that most of the code that makes the legacy DecimalFormat API
+ * work the way it always has before can be found in these various updateXXX()
+ * methods. For example the updatePrecisionForScientific() method
+ * handles the complex interactions amoung the various precision attributes
+ * when formatting in scientific notation. Changing the way attributes
+ * interract, often means changing one of these updateXXX() methods.
+ *
+ * Conclusion:
+ *
+ * The DecimFmtImpl class is the glue code between the legacy and new
+ * number formatting code. It uses a direct acyclic graph (DAG) to
+ * maintain backward compatibility, to make the code efficient, and to
+ * abstract away the complex interraction among legacy attributs.
+ */
+
+
 class DecimalFormatImpl : public UObject {
 public:
 
