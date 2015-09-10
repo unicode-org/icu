3ce9cc1647ce09a5c14db9fbd5ed53d9c5cbc377
Steven R. Loomis <srl@icu-project.org>
2015-09-10 06:43:39 +0000

    [ICU-11437] endianness detection on bsd X-Commit-URL: https://ssl.icu-project.org/trac/changeset/37939 Former-commit-id: 776de0800ea51fa626d4d2063e5506f3aa49abd6

--- icu4c/source/common/unicode/platform.h
+++ icu4c/source/common/unicode/platform.h
@@ -160,6 +160,9 @@
 #       define U_PLATFORM U_PF_DARWIN
 #   endif
 #elif defined(BSD) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__MirBSD__)
+#   if defined(__FreeBSD__)
+#       include <sys/endian.h>
+#   endif
 #   define U_PLATFORM U_PF_BSD
 #elif defined(sun) || defined(__sun)
     /* Check defined(__SVR4) || defined(__svr4__) to distinguish Solaris from SunOS? */
