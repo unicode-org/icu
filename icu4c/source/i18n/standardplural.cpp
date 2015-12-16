8c6d77a3751ddc08e5f36af17eff43b3d94cde1a
Markus Scherer <markus.icu@gmail.com>
2015-12-16 04:21:28 +0000

    ICU-12031 StandardPlural from UnicodeString
    X-SVN-Rev: 38134

--- icu4c/source/i18n/standardplural.cpp
+++ icu4c/source/i18n/standardplural.cpp
@@ -13,6 +13,7 @@
 
 #if !UCONFIG_NO_FORMATTING
 
+#include "unicode/unistr.h"
 #include "cstring.h"
 #include "standardplural.h"
 #include "uassert.h"
@@ -55,6 +56,42 @@ int32_t StandardPlural::indexOrNegativeFromString(const char *keyword) {
     case 'z':
         if (uprv_strcmp(keyword, "ero") == 0) {
             return ZERO;
+        }
+        break;
+    default:
+        break;
+    }
+    return -1;
+}
+
+static const UChar gZero[] = { 0x7A, 0x65, 0x72, 0x6F };
+static const UChar gOne[] = { 0x6F, 0x6E, 0x65 };
+static const UChar gTwo[] = { 0x74, 0x77, 0x6F };
+static const UChar gFew[] = { 0x66, 0x65, 0x77 };
+static const UChar gMany[] = { 0x6D, 0x61, 0x6E, 0x79 };
+static const UChar gOther[] = { 0x6F, 0x74, 0x68, 0x65, 0x72 };
+
+int32_t StandardPlural::indexOrNegativeFromString(const UnicodeString &keyword) {
+    switch (keyword.length()) {
+    case 3:
+        if (keyword.compare(gOne, 3) == 0) {
+            return ONE;
+        } else if (keyword.compare(gTwo, 3) == 0) {
+            return TWO;
+        } else if (keyword.compare(gFew, 3) == 0) {
+            return FEW;
+        }
+        break;
+    case 4:
+        if (keyword.compare(gMany, 4) == 0) {
+            return MANY;
+        } else if (keyword.compare(gZero, 4) == 0) {
+            return ZERO;
+        }
+        break;
+    case 5:
+        if (keyword.compare(gOther, 5) == 0) {
+            return OTHER;
         }
         break;
     default:
