f01caf199b26831dbe0410f47c350954b9452aa9
Michael Ow <mow@svn.icu-project.org>
2016-02-28 21:22:24 +0000

    ICU-12139 Remove header files from pfiles.txt and update header files
    X-SVN-Rev: 38426

--- icu4c/source/common/unicode/ucharstriebuilder.h
+++ icu4c/source/common/unicode/ucharstriebuilder.h
@@ -1,6 +1,6 @@
 /*
 *******************************************************************************
-*   Copyright (C) 2010-2014, International Business Machines
+*   Copyright (C) 2010-2016, International Business Machines
 *   Corporation and others.  All Rights Reserved.
 *******************************************************************************
 *   file name:  ucharstriebuilder.h
@@ -148,6 +148,7 @@ private:
     virtual int32_t getMinLinearMatch() const { return UCharsTrie::kMinLinearMatch; }
     virtual int32_t getMaxLinearMatchLength() const { return UCharsTrie::kMaxLinearMatchLength; }
 
+#ifndef U_HIDE_INTERNAL_API
     class UCTLinearMatchNode : public LinearMatchNode {
     public:
         UCTLinearMatchNode(const UChar *units, int32_t len, Node *nextNode);
@@ -159,6 +160,7 @@ private:
 
     virtual Node *createLinearMatchNode(int32_t i, int32_t unitIndex, int32_t length,
                                         Node *nextNode) const;
+#endif /* U_HIDE_INTERNAL_API */
 
     UBool ensureCapacity(int32_t length);
     virtual int32_t write(int32_t unit);
