6357024225d3ce2c46b63536eabb5f6cacfbe1d2
George Rhoten <grhoten@users.noreply.github.com>
2016-03-02 09:02:20 +0000

    [ICU-9503] Undo removal of lenient parse data. Only English wasn't moved to CLDR. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/38462 Former-commit-id: f039b5f56c382165921187fb1b3d55b741e81367

--- icu4j/main/tests/collate/src/com/ibm/icu/dev/test/format/RbnfLenientScannerTest.java
+++ icu4j/main/tests/collate/src/com/ibm/icu/dev/test/format/RbnfLenientScannerTest.java
@@ -1,6 +1,6 @@
 /*
  *******************************************************************************
- * Copyright (C) 2009-2014, International Business Machines Corporation and    *
+ * Copyright (C) 2009-2016, International Business Machines Corporation and    *
  * others. All Rights Reserved.                                                *
  *******************************************************************************
  */
@@ -45,11 +45,7 @@ public class RbnfLenientScannerTest extends TestFmwk {
             { "2 thousand six HUNDRED   fifty-7", "2,657" },
         };
 
-        if (logKnownIssue("9503", "Lenient parse problems with English RBNF")) {
-            logln("Can't do lenient parse test due to http://bugs.icu-project.org/trac/ticket/9503");
-        } else {
-            doLenientParseTest(formatter, lpTestData);
-        }
+        doLenientParseTest(formatter, lpTestData);
     }
 
     /**
@@ -72,11 +68,7 @@ public class RbnfLenientScannerTest extends TestFmwk {
             { "fifteen hundred and zero", "1,500" }
         };
 
-        if (logKnownIssue("9503", null)) {
-            logln("Can't do lenient parse test due to http://bugs.icu-project.org/trac/ticket/9503");
-        } else {
-            doLenientParseTest(formatter, lpTestData);
-        }
+        doLenientParseTest(formatter, lpTestData);
     }
 
     /**
