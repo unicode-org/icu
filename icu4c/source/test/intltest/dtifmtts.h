c1a5e159c207b07528795e9bfd27e25b4c16fc46
Andy Heninger <andy.heninger@gmail.com>
2016-05-18 00:11:20 +0000

    [ICU-12065] DateIntervalFormat operator ==, remove from/to calendars from consideration. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/38749 Former-commit-id: ef78fea579c3de445e6f496516d367a8d35f083c

--- icu4c/source/test/intltest/dtifmtts.h
+++ icu4c/source/test/intltest/dtifmtts.h
@@ -1,5 +1,5 @@
 /********************************************************************
- * COPYRIGHT: 
+ * COPYRIGHT:
  * Copyright (c) 2008-2016 International Business Machines Corporation and
  * others. All Rights Reserved.
  ********************************************************************/
@@ -18,7 +18,7 @@
  * Test basic functionality of various API functions
  **/
 class DateIntervalFormatTest: public IntlTest {
-    void runIndexedTest( int32_t index, UBool exec, const char* &name, char* par = NULL );  
+    void runIndexedTest( int32_t index, UBool exec, const char* &name, char* par = NULL );
 
 public:
     /**
@@ -59,6 +59,8 @@ public:
     void testTicket11669();
     void threadFunc11669(int32_t threadNum);
 
+    void testTicket12065();
+
 private:
     /**
      * Test formatting against expected result
@@ -66,15 +68,15 @@ private:
     void expect(const char** data, int32_t data_length);
 
     /**
-     * Test formatting against expected result using user defined 
+     * Test formatting against expected result using user defined
      * DateIntervalInfo
      */
     void expectUserDII(const char** data, int32_t data_length);
 
     /**
-     * Stress test formatting 
+     * Stress test formatting
      */
-    void stress(const char** data, int32_t data_length, const Locale& loc, 
+    void stress(const char** data, int32_t data_length, const Locale& loc,
                 const char* locName);
 };
 
