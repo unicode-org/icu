3fe541e843a1bd5d650b5728291c863b899fb6f4
Shane Carr <shane@unicode.org>
2016-05-21 01:00:29 +0000

    ICU-11782 Optimizing storage of skeleton field strings in DateTimePatternGenerator.  Adding unit tests for 'canonical skeleton' code pathways.
    X-SVN-Rev: 38758

--- icu4j/main/tests/core/src/com/ibm/icu/dev/test/format/DateTimeGeneratorTest.java
+++ icu4j/main/tests/core/src/com/ibm/icu/dev/test/format/DateTimeGeneratorTest.java
@@ -796,6 +796,23 @@ public class DateTimeGeneratorTest extends TestFmwk {
     }
 
     /* Tests the method
+     *        public String getCanonicalSkeletonAllowingDuplicates(String pattern)
+     */
+    public void TestGetCanonicalSkeletonAllowingDuplicates(){
+        DateTimePatternGenerator dtpg = DateTimePatternGenerator.getInstance();
+        String[] cases = {"GyQMwEdaHmsSv","LegH","Legh"};
+        String[] results = {"GyQMwEdHmsSv","MEdH","MEdh"};
+        for(int i=0; i<cases.length; i++){
+            if(!dtpg.getCanonicalSkeletonAllowingDuplicates(cases[i]).equals(results[i])){
+                errln("DateTimePatternGenerator.getCanonicalSkeletonAllowingDuplicates(String) did " +
+                        "return the expected result when passing " + cases[i] +
+                        " and expected " + results[i] + " but got " +
+                        dtpg.getSkeleton(cases[i]));
+            }
+        }
+    }
+
+    /* Tests the method
      *        public String getBaseSkeleton(String pattern)
      */
     public void TestGetBaseSkeleton(){
