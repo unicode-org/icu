027e0ec41ad3caf5dea9da37d3c8496c4fd27594
Steven R. Loomis <srl@icu-project.org>
2016-06-22 19:43:23 +0000

    ICU-12556 check in test with logKnownIssue against CldrBug:9465
    X-SVN-Rev: 38874

--- icu4j/main/tests/core/src/com/ibm/icu/dev/test/format/CompactDecimalFormatTest.java
+++ icu4j/main/tests/core/src/com/ibm/icu/dev/test/format/CompactDecimalFormatTest.java
@@ -460,4 +460,14 @@ public class CompactDecimalFormatTest extends TestFmwk {
         result.setMaximumSignificantDigits(2);
         return result;
     }
+
+    @Test
+    public void TestNordic() {
+        if (logKnownIssue("cldrbug:9465","CDF(12,000) for no_NO shouldn't be 12 (12K or similar)")) {
+            return;
+        }
+        String result = CompactDecimalFormat.getInstance( new ULocale("no_NO"),
+                CompactDecimalFormat.CompactStyle.SHORT ).format(12000);
+        assertNotEquals("CDF(12,000) for no_NO shouldn't be 12 (12K or similar)", "12", result);
+    }
 }
