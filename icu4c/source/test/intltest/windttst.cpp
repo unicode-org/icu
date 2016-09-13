85f8d034a7963f38794e838e3e19f3f34902ceb6
Andy Heninger <andy.heninger@gmail.com>
2016-09-13 21:37:40 +0000

    ICU-12547 UCHAR_TYPE=char16_t on Windows, fix compilation problems.
    X-SVN-Rev: 39217

--- icu4c/source/test/intltest/windttst.cpp
+++ icu4c/source/test/intltest/windttst.cpp
@@ -151,33 +151,33 @@ void Win32DateTimeTest::testLocales(TestLog *log)
         wdf->format(icuNow, udBuffer);
         wtf->format(icuNow, utBuffer);
 
-        if (ubBuffer.indexOf(wdBuffer, wdLength - 1, 0) < 0) {
+        if (ubBuffer.indexOf((const UChar *)wdBuffer, wdLength - 1, 0) < 0) {
             UnicodeString baseName(wlocale.getBaseName());
-            UnicodeString expected(wdBuffer);
+            UnicodeString expected((const UChar *)wdBuffer);
 
             log->errln("DateTime format error for locale " + baseName + ": expected date \"" + expected +
                        "\" got \"" + ubBuffer + "\"");
         }
 
-        if (ubBuffer.indexOf(wtBuffer, wtLength - 1, 0) < 0) {
+        if (ubBuffer.indexOf((const UChar *)wtBuffer, wtLength - 1, 0) < 0) {
             UnicodeString baseName(wlocale.getBaseName());
-            UnicodeString expected(wtBuffer);
+            UnicodeString expected((const UChar *)wtBuffer);
 
             log->errln("DateTime format error for locale " + baseName + ": expected time \"" + expected +
                        "\" got \"" + ubBuffer + "\"");
         }
 
-        if (udBuffer.compare(wdBuffer) != 0) {
+        if (udBuffer.compare((const UChar *)wdBuffer) != 0) {
             UnicodeString baseName(wlocale.getBaseName());
-            UnicodeString expected(wdBuffer);
+            UnicodeString expected((const UChar *)wdBuffer);
 
             log->errln("Date format error for locale " + baseName + ": expected \"" + expected +
                        "\" got \"" + udBuffer + "\"");
         }
 
-        if (utBuffer.compare(wtBuffer) != 0) {
+        if (utBuffer.compare((const UChar *)wtBuffer) != 0) {
             UnicodeString baseName(wlocale.getBaseName());
-            UnicodeString expected(wtBuffer);
+            UnicodeString expected((const UChar *)wtBuffer);
 
             log->errln("Time format error for locale " + baseName + ": expected \"" + expected +
                        "\" got \"" + utBuffer + "\"");
