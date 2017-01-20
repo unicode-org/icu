1b9649c67bd6ac72194e2b7e1b89ba2f75a1b6a6
Andy Heninger <andy.heninger@gmail.com>
2017-01-20 00:20:31 +0000

    [ICU-12764] icu4c utf-8 source files, update Copyright notices. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/39583 Former-commit-id: a6e6f13c9a372a897f4dd2f62c235509265d022a

--- icu4c/source/i18n/regeximp.h
+++ icu4c/source/i18n/regeximp.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2016 and later: Unicode, Inc. and others.
+// © 2016 and later: Unicode, Inc. and others.
 // License & terms of use: http://www.unicode.org/copyright.html
 //
 //   Copyright (C) 2002-2015 International Business Machines Corporation
