334a2e1c20c4e4b5582cbe8363486e178aca3c9e
Markus Scherer <markus.icu@gmail.com>
2017-01-21 00:53:52 +0000

    [ICU-12410] port class Edits to Java X-Commit-URL: https://ssl.icu-project.org/trac/changeset/39590 Former-commit-id: e1d117fb4b86cb901ff6041ca1e98e70640f26ac

---
+++ icu4j/main/classes/core/src/com/ibm/icu/text/CaseMap.java
@@ -0,0 +1,11 @@
+// © 2017 and later: Unicode, Inc. and others.
+// License & terms of use: http://www.unicode.org/copyright.html#License
+package com.ibm.icu.text;
+
+/**
+ * @draft ICU 59
+ * @provisional This API might change or be removed in a future release.
+ */
+public final class CaseMap {
+
+}
