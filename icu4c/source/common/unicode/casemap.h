a07674baf1e74a35926fa140ff1c1d5343d4df92
Markus Scherer <markus.icu@gmail.com>
2017-02-14 19:46:45 +0000

    [ICU-12410] UTF-8 for new C++ source files like in [ICU-12764] X-Commit-URL: https://ssl.icu-project.org/trac/changeset/39674 Former-commit-id: a0fe27b7e541c3556a920387b189a01e3bab340a

--- icu4c/source/common/unicode/casemap.h
+++ icu4c/source/common/unicode/casemap.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2017 and later: Unicode, Inc. and others.
+// © 2017 and later: Unicode, Inc. and others.
 // License & terms of use: http://www.unicode.org/copyright.html
 
 // casemap.h
