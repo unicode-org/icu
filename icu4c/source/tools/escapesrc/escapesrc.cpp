1ad57ef2e82c7c32a4713c61f685bfa61bc4d78f
Steven R. Loomis <srl@icu-project.org>
2017-03-13 19:08:18 +0000

    [ICU-12766] add stub escapesrc X-Commit-URL: https://ssl.icu-project.org/trac/changeset/39774 Former-commit-id: 8f3a6948dff209009adeaf9d2d7e94ad5b50d402

---
+++ icu4c/source/tools/escapesrc/escapesrc.cpp
@@ -0,0 +1,9 @@
+// © 2016 and later: Unicode, Inc. and others.
+// License & terms of use: http://www.unicode.org/copyright.html
+
+#include <stdio.h>
+
+int main(int argc, const char *argv[]) {
+  puts("Hi\n");
+  return 0;
+}
