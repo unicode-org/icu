1a6e71157a0f6ae482165f8fec0a6e621512695f
Markus Scherer <markus.icu@gmail.com>
2017-04-26 22:10:48 +0000

    [ICU-12985] undo ICU 59 mix-version test hacks; 0860..086F now defaults to AL X-Commit-URL: https://ssl.icu-project.org/trac/changeset/40081 Former-commit-id: 612fb2a92d2ec328c68a0a00843247d245c781b2

--- icu4j/main/tests/core/src/com/ibm/icu/dev/test/lang/UCharacterTest.java
+++ icu4j/main/tests/core/src/com/ibm/icu/dev/test/lang/UCharacterTest.java
@@ -1542,6 +1542,8 @@ public final class UCharacterTest extends TestFmwk
             { 0x0590, UCharacterDirection.LEFT_TO_RIGHT },
             { 0x0600, UCharacterDirection.RIGHT_TO_LEFT },
             { 0x07C0, UCharacterDirection.RIGHT_TO_LEFT_ARABIC },
+            { 0x0860, UCharacterDirection.RIGHT_TO_LEFT },
+            { 0x0870, UCharacterDirection.RIGHT_TO_LEFT_ARABIC },  // Unicode 10 changes U+0860..U+086F from R to AL.
             { 0x08A0, UCharacterDirection.RIGHT_TO_LEFT },
             { 0x0900, UCharacterDirection.RIGHT_TO_LEFT_ARABIC },  /* Unicode 6.1 changes U+08A0..U+08FF from R to AL */
             { 0x20A0, UCharacterDirection.LEFT_TO_RIGHT },
@@ -1635,8 +1637,6 @@ public final class UCharacterTest extends TestFmwk
                                 shouldBeDir=defaultBidi[i][1];
                             }
 
-                            // TODO: Remove this version check, see ticket #13061.
-                            if (VersionInfo.ICU_VERSION.getMajor() != 59)
                             if (UCharacter.getDirection(c) != shouldBeDir
                                 || UCharacter.getIntPropertyValue(c,
                                                           UProperty.BIDI_CLASS)
