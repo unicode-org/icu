ed94ed49454086c035aafa579490cad1b22c3167
Shane Carr <shane@unicode.org>
2017-05-06 02:39:49 +0000

    [ICU-12985] Bringing SpoofChecker documentation back in sync with unit test. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/40115 Former-commit-id: 3a484996fda086d3d0813d0c4e8200095e3b9a11

--- icu4j/main/tests/core/src/com/ibm/icu/dev/test/text/SpoofCheckerTest.java
+++ icu4j/main/tests/core/src/com/ibm/icu/dev/test/text/SpoofCheckerTest.java
@@ -469,9 +469,12 @@ public class SpoofCheckerTest extends TestFmwk {
         // The spoof checker should only return those tests that the user requested.  This test makes sure that
         // the checker doesn't return anything the user doesn't want.  This test started passing in ICU 58.
 
-        String latn = "horario";
-        String cyrl = "һогагіо";
-        String mixed = "һоrarіо";
+        // NOTE: These strings are the same ones as in the documentation.  If the confusables data changes
+        // and this test breaks, pick a new confusables pair, update it here, and also update it in the
+        // documentation of SpoofChecker.java.
+        String latn = "desparejado";
+        String cyrl = "ԁеѕрагејаԁо";
+        String mixed = "dеsраrејаdо";
 
         Object[][] tests = {
                 // string 1, string 2, checks for spoof checker, expected output
