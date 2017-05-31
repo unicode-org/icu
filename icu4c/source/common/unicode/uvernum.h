694142470ed0b28b597aef272e21453a803ec55f
Yoshito Umaoka <y.umaoka@gmail.com>
2017-05-31 16:50:38 +0000

    ICU-13159 Updating ICU version in trunk to 60.0.1 (60m1) - merging changes from my work branch.
    X-SVN-Rev: 40145

--- icu4c/source/common/unicode/uvernum.h
+++ icu4c/source/common/unicode/uvernum.h
@@ -58,19 +58,19 @@
  *  This value will change in the subsequent releases of ICU
  *  @stable ICU 2.4
  */
-#define U_ICU_VERSION_MAJOR_NUM 59
+#define U_ICU_VERSION_MAJOR_NUM 60
 
 /** The current ICU minor version as an integer.
  *  This value will change in the subsequent releases of ICU
  *  @stable ICU 2.6
  */
-#define U_ICU_VERSION_MINOR_NUM 1
+#define U_ICU_VERSION_MINOR_NUM 0
 
 /** The current ICU patchlevel version as an integer.
  *  This value will change in the subsequent releases of ICU
  *  @stable ICU 2.4
  */
-#define U_ICU_VERSION_PATCHLEVEL_NUM 0
+#define U_ICU_VERSION_PATCHLEVEL_NUM 1
 
 /** The current ICU build level version as an integer.
  *  This value is for use by ICU clients. It defaults to 0.
@@ -84,7 +84,7 @@
  *  This value will change in the subsequent releases of ICU
  *  @stable ICU 2.6
  */
-#define U_ICU_VERSION_SUFFIX _59
+#define U_ICU_VERSION_SUFFIX _60
 
 /**
  * \def U_DEF2_ICU_ENTRY_POINT_RENAME
@@ -119,14 +119,14 @@
  *  This value will change in the subsequent releases of ICU
  *  @stable ICU 2.4
  */
-#define U_ICU_VERSION "59.1"
+#define U_ICU_VERSION "60.0.1"
 
 /** The current ICU library major/minor version as a string without dots, for library name suffixes.
  *  This value will change in the subsequent releases of ICU
  *  @stable ICU 2.6
  */
 #if U_PLATFORM_HAS_WINUWP_API == 0
-#define U_ICU_VERSION_SHORT "59"
+#define U_ICU_VERSION_SHORT "60"
 #else
 // U_DISABLE_RENAMING does not impact dat file name
 #define U_ICU_VERSION_SHORT
@@ -136,7 +136,7 @@
 /** Data version in ICU4C.
  * @internal ICU 4.4 Internal Use Only
  **/
-#define U_ICU_DATA_VERSION "59.1"
+#define U_ICU_DATA_VERSION "60.0.1"
 #endif  /* U_HIDE_INTERNAL_API */
 
 /*===========================================================================
