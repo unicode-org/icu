7351dbcf247e984e7b6f388f4bb531d0d9e5bd6b
Norbert Runge <nrunge@google.com>
2017-06-20 23:12:14 +0000

    ICU-13242 fixed uppercase-comes-first collation test
    X-SVN-Rev: 40184

--- icu4c/source/test/cintltst/cmsccoll.c
+++ icu4c/source/test/cintltst/cmsccoll.c
@@ -793,8 +793,6 @@ static void TestJ784(void) {
 }
 #endif
 
-#if 0
-/* superceded by the changes to the lv locale */
 static void TestJ831(void) {
   const static char *data[] = {
     "I",
@@ -802,9 +800,8 @@ static void TestJ831(void) {
       "Y",
       "y"
   };
-  genericLocaleStarter("lv", data, UPRV_LENGTHOF(data));
+  genericLocaleStarter("da", data, UPRV_LENGTHOF(data));
 }
-#endif
 
 static void TestJ815(void) {
   const static char *data[] = {
@@ -5894,7 +5891,7 @@ void addMiscCollTest(TestNode** root)
     TEST(TestEmptyRule);
     /*TEST(TestJ784);*/ /* 'zh' locale has changed - now it is getting tested by TestBeforePinyin */
     TEST(TestJ815);
-    /*TEST(TestJ831);*/ /* we changed lv locale */
+    TEST(TestJ831);
     TEST(TestBefore);
     TEST(TestHangulTailoring);
     TEST(TestUCARules);
