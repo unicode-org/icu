c70ebe3ce12cf4fa4a3365f595acec74c8119750
Peter Edberg <pedberg@unicode.org>
2017-06-22 23:05:41 +0000

    ICU-13159 Integrate CLDR trunk@13513 (cldr/tags/icu-milestone-60-0-1)
    X-SVN-Rev: 40193

--- icu4c/source/data/zone/resfiles.mk
+++ icu4c/source/data/zone/resfiles.mk
@@ -1,6 +1,6 @@
 # © 2016 and later: Unicode, Inc. and others.
 # License & terms of use: http://www.unicode.org/copyright.html#License
-ZONE_CLDR_VERSION = 31.0.1
+ZONE_CLDR_VERSION = 32
 # A list of txt's to build
 # Note:
 #
@@ -38,8 +38,9 @@ ZONE_SYNTHETIC_ALIAS = ar_SA.txt ars.txt az_AZ.txt az_Latn_AZ.txt\
  sr_Latn_RS.txt sr_Latn_YU.txt sr_ME.txt sr_RS.txt sr_XK.txt\
  sr_YU.txt th_TH.txt th_TH_TRADITIONAL.txt tl.txt tl_PH.txt\
  uz_AF.txt uz_Arab_AF.txt uz_Latn_UZ.txt uz_UZ.txt vai_LR.txt\
- vai_Vaii_LR.txt zh_CN.txt zh_HK.txt zh_Hans_CN.txt zh_Hant_TW.txt\
- zh_MO.txt zh_SG.txt zh_TW.txt
+ vai_Vaii_LR.txt yue_CN.txt yue_HK.txt yue_Hans_CN.txt yue_Hant_HK.txt\
+ zh_CN.txt zh_HK.txt zh_Hans_CN.txt zh_Hant_TW.txt zh_MO.txt\
+ zh_SG.txt zh_TW.txt
 
 
 # All aliases (to not be included under 'installed'), but not including root.
@@ -116,6 +117,7 @@ ZONE_SOURCE = af.txt agq.txt ak.txt am.txt\
  ur.txt ur_IN.txt uz.txt uz_Arab.txt uz_Cyrl.txt\
  uz_Latn.txt vai.txt vai_Latn.txt vai_Vaii.txt vi.txt\
  vun.txt wae.txt xog.txt yav.txt yi.txt\
- yo.txt yue.txt zgh.txt zh.txt zh_Hans.txt\
- zh_Hans_SG.txt zh_Hant.txt zh_Hant_HK.txt zh_Hant_MO.txt zu.txt
+ yo.txt yue.txt yue_Hans.txt yue_Hant.txt zgh.txt\
+ zh.txt zh_Hans.txt zh_Hans_SG.txt zh_Hant.txt zh_Hant_HK.txt\
+ zh_Hant_MO.txt zu.txt
 
