63fc841b3327d3dec35c2a5820a24e8f16fa6160
Shane Carr <shane@unicode.org>
2017-09-28 01:12:37 +0000

    [ICU-13177] Adding missing documentation for NoUnit X-Commit-URL: https://ssl.icu-project.org/trac/changeset/40496 Former-commit-id: 6d37930dd9778b7dd1d950b25441fbb838fe0326

--- icu4j/main/classes/core/src/com/ibm/icu/util/NoUnit.java
+++ icu4j/main/classes/core/src/com/ibm/icu/util/NoUnit.java
@@ -2,15 +2,41 @@
 // License & terms of use: http://www.unicode.org/copyright.html#License
 package com.ibm.icu.util;
 
+import com.ibm.icu.number.NumberFormatter;
+
+/**
+ * Dimensionless unit for percent and permille.
+ * @see NumberFormatter
+ * @draft ICU 60
+     * @provisional This API might change or be removed in a future release.
+ */
 public class NoUnit extends MeasureUnit {
     private static final long serialVersionUID = 2467174286237024095L;
 
+    /**
+     * Constant for the base unit (dimensionless and no scaling).
+     *
+     * @draft ICU 60
+     * @provisional This API might change or be removed in a future release.
+     */
     public static final NoUnit BASE =
         (NoUnit) MeasureUnit.internalGetInstance("none", "base");
 
+    /**
+     * Constant for the percent unit, or 1/100 of a base unit.
+     *
+     * @draft ICU 60
+     * @provisional This API might change or be removed in a future release.
+     */
     public static final NoUnit PERCENT =
         (NoUnit) MeasureUnit.internalGetInstance("none", "percent");
 
+    /**
+     * Constant for the permille unit, or 1/100 of a base unit.
+     *
+     * @draft ICU 60
+     * @provisional This API might change or be removed in a future release.
+     */
     public static final NoUnit PERMILLE =
         (NoUnit) MeasureUnit.internalGetInstance("none", "permille");
 
