985d9312dae71d8a8346f7637816e3a5117f565f
Peter Edberg <pedberg@unicode.org>
2017-10-31 03:36:22 +0000

    [ICU-12770] Update/fix comments about TZDEFAULT target on various systems X-Commit-URL: https://ssl.icu-project.org/trac/changeset/40659 Former-commit-id: 30e09e4f175e9eb2f9bfd95efd54948f9b4e9bb4

--- icu4c/source/common/putil.cpp
+++ icu4c/source/common/putil.cpp
@@ -676,8 +676,8 @@ extern U_IMPORT char *U_TZNAME[];
 #if !UCONFIG_NO_FILE_IO && ((U_PLATFORM_IS_DARWIN_BASED && (U_PLATFORM != U_PF_IPHONE || defined(U_TIMEZONE))) || U_PLATFORM_IS_LINUX_BASED || U_PLATFORM == U_PF_BSD || U_PLATFORM == U_PF_SOLARIS)
 /* These platforms are likely to use Olson timezone IDs. */
 /* common targets of the symbolic link at TZDEFAULT are:
- * "/usr/share/zoneinfo/<olsonID>" default, older Linus distros, macOS to 10.12
- * "../usr/share/zoneinfo/<olsonID>" newer Linux distros: Red Hat Enterprise Linux 7, Ubuntu, SuSe Linux
+ * "/usr/share/zoneinfo/<olsonID>" default, older Linux distros, macOS to 10.12
+ * "../usr/share/zoneinfo/<olsonID>" newer Linux distros: Red Hat Enterprise Linux 7, Ubuntu 16, SuSe Linux 12
  * "/usr/share/lib/zoneinfo/<olsonID>" Solaris
  * "../usr/share/lib/zoneinfo/<olsonID>" Solaris
  * "/var/db/timezone/zoneinfo/<olsonID>" macOS 10.13
