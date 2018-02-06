623621b5b4a72da3b4ccd51d0c644120faf980b6
Shane Carr <shane@unicode.org>
2018-02-06 07:52:58 +0000

    [ICU-13574] Porting the parsing utility classes StringSegment and UnicodeSetStaticCache to C++. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/40841 Former-commit-id: 002c8cc623ca9e4878d103a18844d0e750bd1073

---
+++ icu4c/source/i18n/numparse_types.h
@@ -0,0 +1,22 @@
+// © 2018 and later: Unicode, Inc. and others.
+// License & terms of use: http://www.unicode.org/copyright.html
+
+#include "unicode/utypes.h"
+
+#if !UCONFIG_NO_FORMATTING && !UPRV_INCOMPLETE_CPP11_SUPPORT
+#ifndef __NUMPARSE_TYPES_H__
+#define __NUMPARSE_TYPES_H__
+
+#include "unicode/uobject.h"
+
+U_NAMESPACE_BEGIN
+namespace numparse {
+namespace impl {
+
+
+} // namespace impl
+} // namespace numparse
+U_NAMESPACE_END
+
+#endif //__NUMPARSE_TYPES_H__
+#endif /* #if !UCONFIG_NO_FORMATTING */
