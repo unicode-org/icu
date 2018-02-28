38479cb775554dbf6f69438e7c5e84da714bbe88
Jeff Genovy <Jeff.Genovy@microsoft.com>
2018-02-28 05:51:54 +0000

    [ICU-11318] Fix Windows build break on ARM with UWP projects. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/41006 Former-commit-id: 41daf47d4abffe90e6177ba83e9743f936e32cc8

--- icu4c/source/i18n/double-conversion-utils.h
+++ icu4c/source/i18n/double-conversion-utils.h
@@ -71,8 +71,9 @@ inline void abort_noreturn() { abort(); }
 // the output of the division with the expected result. (Inlining must be
 // disabled.)
 // On Linux,x86 89255e-22 != Div_double(89255.0/1e22)
+// ICU PATCH: Enable ARM builds for Windows with 'defined(_M_ARM)'.
 #if defined(_M_X64) || defined(__x86_64__) || \
-    defined(__ARMEL__) || defined(__avr32__) || \
+    defined(__ARMEL__) || defined(__avr32__) || defined(_M_ARM) || \
     defined(__hppa__) || defined(__ia64__) || \
     defined(__mips__) || \
     defined(__powerpc__) || defined(__ppc__) || defined(__ppc64__) || \
