e1907060808a56ed34d11af99b9f35146b9986ed
Shane Carr <shane@unicode.org>
2018-03-07 18:24:34 +0000

    ICU-13574 Reverting change of DecimalFormatSymbols::getConstSymbol to @draft from @internal.
    X-SVN-Rev: 41076

--- icu4c/source/i18n/unicode/dcfmtsym.h
+++ icu4c/source/i18n/unicode/dcfmtsym.h
@@ -406,10 +406,12 @@ public:
      * returning a const reference to one of the symbol strings.
      * The returned reference becomes invalid when the symbol is changed
      * or when the DecimalFormatSymbols are destroyed.
+     * ### TODO markus 2002oct11: Consider proposing getConstSymbol() to be really public.
+     * Note: moved #ifndef U_HIDE_INTERNAL_API after this, since this is needed for inline in DecimalFormat
      *
      * @param symbol Constant to indicate a number format symbol.
      * @return the format symbol by the param 'symbol'
-     * @draft ICU 61
+     * @internal
      */
     inline const UnicodeString& getConstSymbol(ENumberFormatSymbol symbol) const;
 
@@ -499,6 +501,7 @@ DecimalFormatSymbols::getSymbol(ENumberFormatSymbol symbol) const {
     return *strPtr;
 }
 
+// See comments above for this function. Not hidden with #ifdef U_HIDE_INTERNAL_API
 inline const UnicodeString &
 DecimalFormatSymbols::getConstSymbol(ENumberFormatSymbol symbol) const {
     const UnicodeString *strPtr;
