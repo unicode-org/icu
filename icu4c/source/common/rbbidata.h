77dcf6879939c11ec0684e4411d2a52fa8e8e78f
Andy Heninger <andy.heninger@gmail.com>
2018-03-28 22:07:24 +0000

    [ICU-13194] RBBI, bump data version number. New reverse tables are not compatible. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/41168 Former-commit-id: 8ef1689f418b65c86d9756c12bb58c3d6b896eaa

--- icu4c/source/common/rbbidata.h
+++ icu4c/source/common/rbbidata.h
@@ -58,7 +58,7 @@ ubrk_swap(const UDataSwapper *ds,
 U_NAMESPACE_BEGIN
 
 // The current RBBI data format version.
-static const uint8_t RBBI_DATA_FORMAT_VERSION[] = {4, 0, 0, 0};
+static const uint8_t RBBI_DATA_FORMAT_VERSION[] = {5, 0, 0, 0};
 
 /*  
  *   The following structs map exactly onto the raw data from ICU common data file. 
