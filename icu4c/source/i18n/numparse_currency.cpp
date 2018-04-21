09ddd5c3985890d49cdbc527131fa3a747cbabb9
Shane Carr <shane@unicode.org>
2018-04-21 08:55:58 +0000

    [ICU-13634] Porting some minor ICU4C performance enhancements to Java. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/41259 Former-commit-id: 82e19040d0ebbde9a47e05d21699058aac8aa941

--- icu4c/source/i18n/numparse_currency.cpp
+++ icu4c/source/i18n/numparse_currency.cpp
@@ -30,6 +30,8 @@ CombinedCurrencyMatcher::CombinedCurrencyMatcher(const CurrencySymbols& currency
     utils::copyCurrencyCode(fCurrencyCode, currencySymbols.getIsoCode());
 
     // TODO: Figure out how to make this faster and re-enable.
+    // Computing the "lead code points" set for fastpathing is too slow to use in production.
+    // See http://bugs.icu-project.org/trac/ticket/13584
 //    // Compute the full set of characters that could be the first in a currency to allow for
 //    // efficient smoke test.
 //    fLeadCodePoints.add(fCurrency1.char32At(0));
@@ -41,8 +43,8 @@ CombinedCurrencyMatcher::CombinedCurrencyMatcher(const CurrencySymbols& currency
 //    fLeadCodePoints.freeze();
 }
 
-bool CombinedCurrencyMatcher::match(StringSegment& segment, ParsedNumber& result,
-                                    UErrorCode& status) const {
+bool
+CombinedCurrencyMatcher::match(StringSegment& segment, ParsedNumber& result, UErrorCode& status) const {
     if (result.currencyCode[0] != 0) {
         return false;
     }
