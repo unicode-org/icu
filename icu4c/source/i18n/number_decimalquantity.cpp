c9680037ccfcf5f7e989dab5a6de8912b39efe2f
Shane Carr <shane@unicode.org>
2018-04-28 07:38:41 +0000

    ICU-12572 Fixing signed overflow in decimalquantity.
    X-SVN-Rev: 41293

--- icu4c/source/i18n/number_decimalquantity.cpp
+++ icu4c/source/i18n/number_decimalquantity.cpp
@@ -328,7 +328,10 @@ bool DecimalQuantity::isZero() const {
 DecimalQuantity &DecimalQuantity::setToInt(int32_t n) {
     setBcdToZero();
     flags = 0;
-    if (n < 0) {
+    if (n == INT32_MIN) {
+        flags |= NEGATIVE_FLAG;
+        // leave as INT32_MIN; handled below in _setToInt()
+    } else if (n < 0) {
         flags |= NEGATIVE_FLAG;
         n = -n;
     }
