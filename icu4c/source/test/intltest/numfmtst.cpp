1786933953d6b3f07c73fcd6737d62af1681494c
Andy Heninger <andy.heninger@gmail.com>
2018-05-07 23:16:16 +0000

    [ICU-11646] DecimalFormat equality test, add error check. X-Commit-URL: https://ssl.icu-project.org/trac/changeset/41344 Former-commit-id: f0510fadb60e47b3d63cf779caecc2e576ba114d

--- icu4c/source/test/intltest/numfmtst.cpp
+++ icu4c/source/test/intltest/numfmtst.cpp
@@ -9215,6 +9215,7 @@ void NumberFormatTest::Test11646_Equality() {
     DecimalFormatSymbols symbols(Locale::getEnglish(), status);
     UnicodeString pattern(u"\u00a4\u00a4\u00a4 0.00 %\u00a4\u00a4");
     DecimalFormat fmt(pattern, symbols, status);
+    if (!assertSuccess("", status)) return;
 
     // Test equality with affixes. set affix methods can't capture special
     // characters which is why equality should fail.
