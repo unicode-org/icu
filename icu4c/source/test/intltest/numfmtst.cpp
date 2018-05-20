19adede0656eaeb6f833b60ba935ac302e6af2d1
Andy Heninger <andy.heninger@gmail.com>
2018-05-20 20:46:10 +0000

    [ICU-11640] fix memory leak in Test11640_TripleCurrencySymbol() X-Commit-URL: https://ssl.icu-project.org/trac/changeset/41417 Former-commit-id: e2d8223532aadcc268bdf28f08929a43f60ba44e

--- icu4c/source/test/intltest/numfmtst.cpp
+++ icu4c/source/test/intltest/numfmtst.cpp
@@ -9030,14 +9030,14 @@ void NumberFormatTest::Test11020_RoundingInScientificNotation() {
 void NumberFormatTest::Test11640_TripleCurrencySymbol() {
     IcuTestErrorCode status(*this, "Test11640_TripleCurrencySymbol");
     UnicodeString actual;
-    DecimalFormat *dFormat = new DecimalFormat("¤¤¤ 0", status);
+    DecimalFormat dFormat(u"¤¤¤ 0", status);
     if (U_FAILURE(status)) {
         dataerrln("Failure creating DecimalFormat %s", u_errorName(status));
         return;
     }
-    dFormat->setCurrency(u"USD");
+    dFormat.setCurrency(u"USD");
     UnicodeString result;
-    dFormat->getPositivePrefix(result);
+    dFormat.getPositivePrefix(result);
     assertEquals("Triple-currency should give long name on getPositivePrefix",
                 "US dollars ", result);
 }
