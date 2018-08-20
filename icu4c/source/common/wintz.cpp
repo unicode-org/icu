f7265dc984bacfd651256dc800ee718d0518ff39
mingyzha <zhaomingyan888@gmail.com>
2018-08-20 15:36:57 -0700

    Some fixes

--- icu4c/source/common/wintz.cpp
+++ icu4c/source/common/wintz.cpp
@@ -18,7 +18,7 @@
 #include "wintz.h"
 #include "cmemory.h"
 #include "cstring.h"
-#include "cwchar.h"
+
 #include "unicode/ures.h"
 #include "unicode/ustring.h"
 
@@ -34,45 +34,90 @@
 
 #define MAX_LENGTH_ID 40
 
-/*
-  This code attempts to detect the Windows time zone directly, as set in the Windows Date and Time control panel. 
-  Call GetDynamicTimeZoneInformation to get dynamic time zone info, which contains the TimeZoneKeyName.
-
-  Author: Alan Liu
-  Since: ICU 2.6
-  Based on original code by Carl Brown <cbrown@xnetinc.com>
-
-  Modified on 8/13/2018
-  ICU ticket: ICU-13842 Windows time zone should be read from a per-user registry instead of system-wide registry?
-*/
-
 /**
- * Main Windows time zone detection function.  Returns the Windows
- * time zone, translated to an ICU time zone, or NULL upon failure.
- */
+* Main Windows time zone detection function.  Returns the Windows
+* time zone, translated to an ICU time zone, or NULL upon failure.
+* It is GetDynamicTimeZoneInformation to get the current time zone info.
+* The API returns non-localized 
+*/
 U_CFUNC const char* U_EXPORT2
-uprv_detectWindowsTimeZone() 
+uprv_detectWindowsTimeZone()
 {
+    UErrorCode status = U_ZERO_ERROR;
+    UResourceBundle* bundle = NULL;
     char* icuid = NULL;
-    size_t len;
+    char dynamicTZKeyName[MAX_LENGTH_ID];
+    char tmpid[MAX_LENGTH_ID];
+    int32_t len;
+    int id;
+    int errorCode;
+    wchar_t ISOcodeW[3]; /* 2 letter iso code in UTF-16*/
+    char  ISOcodeA[3]; /* 2 letter iso code in ansi */
+
     DYNAMIC_TIME_ZONE_INFORMATION dynamicTZI;
 
-    /* Obtain DYNAMIC_TIME_ZONE_INFORMATION from the API */
+    /* Obtain TIME_ZONE_INFORMATION from the API and get the non-localized time zone name. */
     uprv_memset(&dynamicTZI, 0, sizeof(dynamicTZI));
     GetDynamicTimeZoneInformation(&dynamicTZI);
 
-    /* Copy the timezone ID to icuid to be returned. */
+    tmpid[0] = 0;
+
+    id = GetUserGeoID(GEOCLASS_NATION);
+    errorCode = GetGeoInfoW(id, GEO_ISO2, ISOcodeW, 3, 0);
+    u_strToUTF8(ISOcodeA, 3, NULL, (const UChar *)ISOcodeW, 3, &status);
+
+    bundle = ures_openDirect(NULL, "windowsZones", &status);
+    ures_getByKey(bundle, "mapTimezones", bundle, &status);
+
+    /* Convert the wchar_t* standard name to char* */
+    uprv_memset(dynamicTZKeyName, 0, sizeof(dynamicTZKeyName));
+    wcstombs(dynamicTZKeyName, dynamicTZI.TimeZoneKeyName, MAX_LENGTH_ID);
     if (dynamicTZI.TimeZoneKeyName[0] != 0)
     {
-        len = uprv_wcslen(dynamicTZI.TimeZoneKeyName);
-        icuid = (char*)uprv_calloc(len + 1, sizeof(char));
-        if (icuid != NULL) 
+        UResourceBundle* winTZ = ures_getByKey(bundle, dynamicTZKeyName, NULL, &status);
+        if (U_SUCCESS(status))
         {
-            uprv_wcstombs(icuid, dynamicTZI.TimeZoneKeyName, len + 1);
+            const UChar* icuTZ = NULL;
+            if (errorCode != 0)
+            {
+                icuTZ = ures_getStringByKey(winTZ, ISOcodeA, &len, &status);
+            }
+            if (errorCode == 0 || icuTZ == NULL)
+            {
+                /* fallback to default "001" and reset status */
+                status = U_ZERO_ERROR;
+                icuTZ = ures_getStringByKey(winTZ, "001", &len, &status);
+            }
+
+            if (U_SUCCESS(status))
+            {
+                int index = 0;
+                while (!(*icuTZ == '\0' || *icuTZ == ' '))
+                {
+                    tmpid[index++] = (char)(*icuTZ++);  /* safe to assume 'char' is ASCII compatible on windows */
+                }
+                tmpid[index] = '\0';
+            }
+        }
+        ures_close(winTZ);
+    }
+
+    /*
+    * Copy the timezone ID to icuid to be returned.
+    */
+    if (tmpid[0] != 0)
+    {
+        len = uprv_strlen(tmpid);
+        icuid = (char*)uprv_calloc(len + 1, sizeof(char));
+        if (icuid != NULL)
+        {
+            uprv_strcpy(icuid, tmpid);
         }
     }
-    
+
+    ures_close(bundle);
+
     return icuid;
 }
 
-#endif /* U_PLATFORM_USES_ONLY_WIN32_API || U_PLATFORM_HAS_WINUWP_API*/
+#endif /* U_PLATFORM_USES_ONLY_WIN32_API || U_PLATFORM_HAS_WINUWP_API */
