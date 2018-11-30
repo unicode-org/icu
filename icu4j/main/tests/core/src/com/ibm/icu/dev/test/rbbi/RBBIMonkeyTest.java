e1e5f363a09144aac359b1b68552bb25e4251fd9
Andy Heninger <andy.heninger@gmail.com>
2018-11-29 17:50:18 -0800

    ICU-20288 add line_cj.txt to RBBI Monkey test.

--- icu4j/main/tests/core/src/com/ibm/icu/dev/test/rbbi/RBBIMonkeyTest.java
+++ icu4j/main/tests/core/src/com/ibm/icu/dev/test/rbbi/RBBIMonkeyTest.java
@@ -1005,7 +1005,7 @@ public class RBBIMonkeyTest extends TestFmwk {
     //
     @Test
     public void TestMonkey() {
-        String tests[] = {"grapheme.txt", "word.txt", "line.txt", "sentence.txt", "line_normal.txt",
+        String tests[] = {"grapheme.txt", "word.txt", "line.txt", "line_cj.txt", "sentence.txt", "line_normal.txt",
                 "line_normal_cj.txt", "line_loose.txt", "line_loose_cj.txt", "word_POSIX.txt"
         };
 
