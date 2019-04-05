f722ff650bc866e5f02bd86361be92685bcac20e
yumaoka <y.umaoka@gmail.com>
2019-04-05 17:30:27 -0400

    ICU-20536 Japanese era Reiwa support in ICU4C 57

--- icu4c/source/test/intltest/incaltst.h
+++ icu4c/source/test/intltest/incaltst.h
@@ -32,7 +32,8 @@ public:
     void TestJapanese(void);
     void TestJapaneseFormat(void);
     void TestJapanese3860(void);
-    
+    void TestJapaneseHeiseiToReiwa(void);
+
     void TestPersian(void);
     void TestPersianFormat(void);
 
