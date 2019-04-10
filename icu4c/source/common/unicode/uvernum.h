017f1f9f5e01948f36f94e542eb8cfcd100b2f74
Jeff Genovy <29107334+jefgen@users.noreply.github.com>
2019-04-10 13:37:38 -0700

    ICU-20536 ICU 62.2 release tasks + tzdata2019a

--- icu4c/source/common/unicode/uvernum.h
+++ icu4c/source/common/unicode/uvernum.h
@@ -64,7 +64,7 @@
  *  This value will change in the subsequent releases of ICU
  *  @stable ICU 2.6
  */
-#define U_ICU_VERSION_MINOR_NUM 1
+#define U_ICU_VERSION_MINOR_NUM 2
 
 /** The current ICU patchlevel version as an integer.
  *  This value will change in the subsequent releases of ICU
@@ -119,7 +119,7 @@
  *  This value will change in the subsequent releases of ICU
  *  @stable ICU 2.4
  */
-#define U_ICU_VERSION "62.1"
+#define U_ICU_VERSION "62.2"
 
 /**
  * The current ICU library major version number as a string, for library name suffixes.
@@ -138,7 +138,7 @@
 /** Data version in ICU4C.
  * @internal ICU 4.4 Internal Use Only
  **/
-#define U_ICU_DATA_VERSION "62.1"
+#define U_ICU_DATA_VERSION "62.2"
 #endif  /* U_HIDE_INTERNAL_API */
 
 /*===========================================================================
