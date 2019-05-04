3fcd91d3d0c1dc7049dfffee530c80287c0a4ad5
Shane Carr <shane@unicode.org>
2019-05-03 19:08:14 -0700

    ICU-20593 Buildtool: adding some options and improving exec mode.

--- icu4c/source/data/buildtool/renderers/common_exec.py
+++ icu4c/source/data/buildtool/renderers/common_exec.py
@@ -9,25 +9,41 @@ from ..request_types import *
 import os
 import shutil
 import subprocess
+import sys
 
-def run(build_dirs, requests, common_vars, **kwargs):
+def run(build_dirs, requests, common_vars, verbose=True, **kwargs):
     for bd in build_dirs:
-        os.makedirs(bd.format(**common_vars), exist_ok=True)
+        makedirs(bd.format(**common_vars))
     for request in requests:
-        status = run_helper(request, common_vars, **kwargs)
+        status = run_helper(request, common_vars, verbose=verbose, **kwargs)
         if status != 0:
             print("!!! ERROR executing above command line: exit code %d" % status)
             return 1
-    print("All data build commands executed")
+    if verbose:
+        print("All data build commands executed")
     return 0
 
-def run_helper(request, common_vars, is_windows, tool_dir, tool_cfg=None, **kwargs):
+def makedirs(dirs):
+    """makedirs compatible between Python 2 and 3"""
+    try:
+        # Python 3 version
+        os.makedirs(dirs, exist_ok=True)
+    except TypeError as e:
+        # Python 2 version
+        try:
+            os.makedirs(dirs)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise e
+
+def run_helper(request, common_vars, platform, tool_dir, verbose, tool_cfg=None, **kwargs):
     if isinstance(request, PrintFileRequest):
         output_path = "{DIRNAME}/{FILENAME}".format(
             DIRNAME = utils.dir_for(request.output_file).format(**common_vars),
             FILENAME = request.output_file.filename,
         )
-        print("Printing to file: %s" % output_path)
+        if verbose:
+            print("Printing to file: %s" % output_path)
         with open(output_path, "w") as f:
             f.write(request.content)
         return 0
@@ -40,7 +56,8 @@ def run_helper(request, common_vars, is_windows, tool_dir, tool_cfg=None, **kwar
             DIRNAME = utils.dir_for(request.output_file).format(**common_vars),
             FILENAME = request.output_file.filename,
         )
-        print("Copying file to: %s" % output_path)
+        if verbose:
+            print("Copying file to: %s" % output_path)
         shutil.copyfile(input_path, output_path)
         return 0
     if isinstance(request, VariableRequest):
@@ -48,19 +65,27 @@ def run_helper(request, common_vars, is_windows, tool_dir, tool_cfg=None, **kwar
         return 0
 
     assert isinstance(request.tool, IcuTool)
-    if is_windows:
+    if platform == "windows":
         cmd_template = "{TOOL_DIR}/{TOOL}/{TOOL_CFG}/{TOOL}.exe {{ARGS}}".format(
             TOOL_DIR = tool_dir,
             TOOL_CFG = tool_cfg,
             TOOL = request.tool.name,
             **common_vars
         )
-    else:
+    elif platform == "unix":
         cmd_template = "{TOOL_DIR}/{TOOL} {{ARGS}}".format(
             TOOL_DIR = tool_dir,
             TOOL = request.tool.name,
             **common_vars
         )
+    elif platform == "bazel":
+        cmd_template = "{TOOL_DIR}/{TOOL}/{TOOL} {{ARGS}}".format(
+            TOOL_DIR = tool_dir,
+            TOOL = request.tool.name,
+            **common_vars
+        )
+    else:
+        raise ValueError("Unknown platform: %s" % platform)
 
     if isinstance(request, RepeatedExecutionRequest):
         for loop_vars in utils.repeated_execution_request_looper(request):
@@ -70,13 +95,12 @@ def run_helper(request, common_vars, is_windows, tool_dir, tool_cfg=None, **kwar
                 loop_vars,
                 common_vars
             )
-            if is_windows:
+            if platform == "windows":
                 # Note: this / to \ substitution may be too aggressive?
                 command_line = command_line.replace("/", "\\")
-            print("Running: %s" % command_line)
-            res = subprocess.run(command_line, shell=True)
-            if res.returncode != 0:
-                return res.returncode
+            returncode = run_shell_command(command_line, verbose)
+            if returncode != 0:
+                return returncode
         return 0
     if isinstance(request, SingleExecutionRequest):
         command_line = utils.format_single_request_command(
@@ -84,10 +108,26 @@ def run_helper(request, common_vars, is_windows, tool_dir, tool_cfg=None, **kwar
             cmd_template,
             common_vars
         )
-        if is_windows:
+        if platform == "windows":
             # Note: this / to \ substitution may be too aggressive?
             command_line = command_line.replace("/", "\\")
-        print("Running: %s" % command_line)
-        res = subprocess.run(command_line, shell=True)
-        return res.returncode
+        returncode = run_shell_command(command_line, verbose)
+        return returncode
     assert False
+
+def run_shell_command(command_line, verbose):
+    if verbose:
+        print("Running: %s" % command_line)
+        return subprocess.call(
+            command_line,
+            shell = True
+        )
+    else:
+        # Pipe output to /dev/null in quiet mode
+        with open(os.devnull, "w") as devnull:
+            return subprocess.call(
+                command_line,
+                shell = True,
+                stdout = devnull,
+                stderr = devnull
+            )
