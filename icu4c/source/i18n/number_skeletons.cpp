4fb47b12a70737ee12326220e71c2d73c5ec658f
Fredrik Roubert <roubert@google.com>
2019-08-14 23:05:37 +0200

    ICU-20601 Wrap ICU implementation compound macros in do { } while.
    This does the same for the ICU implementation code as was done for the
    public ICU API in commit 480bec3ea652886f46d4afad0d7446542d2fbf05.

--- icu4c/source/i18n/number_skeletons.cpp
+++ icu4c/source/i18n/number_skeletons.cpp
@@ -120,17 +120,17 @@ inline void appendMultiple(UnicodeString& sb, UChar32 cp, int32_t count) {
 
 
 #define CHECK_NULL(seen, field, status) (void)(seen); /* for auto-format line wrapping */ \
-{ \
+UPRV_BLOCK_MACRO_BEGIN { \
     if ((seen).field) { \
         (status) = U_NUMBER_SKELETON_SYNTAX_ERROR; \
         return STATE_NULL; \
     } \
     (seen).field = true; \
-}
+} UPRV_BLOCK_MACRO_END
 
 
 #define SKELETON_UCHAR_TO_CHAR(dest, src, start, end, status) (void)(dest); \
-{ \
+UPRV_BLOCK_MACRO_BEGIN { \
     UErrorCode conversionStatus = U_ZERO_ERROR; \
     (dest).appendInvariantChars({FALSE, (src).getBuffer() + (start), (end) - (start)}, conversionStatus); \
     if (conversionStatus == U_INVARIANT_CONVERSION_ERROR) { \
@@ -141,7 +141,7 @@ inline void appendMultiple(UnicodeString& sb, UChar32 cp, int32_t count) {
         (status) = conversionStatus; \
         return; \
     } \
-}
+} UPRV_BLOCK_MACRO_END
 
 
 } // anonymous namespace
