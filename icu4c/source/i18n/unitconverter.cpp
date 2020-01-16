0901393d64f5a74714c8d0da6f69bff1a4a7da20
younies <younies@chromium.org>
2020-01-16 17:16:19 +0100

    ICU-20568 Skeleton for Testing Units
    PR: https://github.com/sffc/icu/pull/6
    Commit: 9b3ca66937c40044055bf9a8e13db57bcdc75fec

---
+++ icu4c/source/i18n/unitconverter.cpp
@@ -0,0 +1,19 @@
+// © 2020 and later: Unicode, Inc. and others.
+// License & terms of use: http://www.unicode.org/copyright.html
+
+#include "unicode/utypes.h"
+
+#if !UCONFIG_NO_FORMATTING
+
+U_NAMESPACE_BEGIN
+
+
+
+
+
+
+
+
+U_NAMESPACE_END
+
+#endif /* #if !UCONFIG_NO_FORMATTING */
