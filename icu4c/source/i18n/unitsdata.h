e186831278d21f4cdae0c2a665320ec1371ba004
Hugo van der Merwe <17109322+hugovdm@users.noreply.github.com>
2020-06-10 00:40:49 +0200

    Drop 'const', it's by-value anyway.

--- icu4c/source/i18n/unitsdata.h
+++ icu4c/source/i18n/unitsdata.h
@@ -115,7 +115,7 @@ class U_I18N_API UnitPreferenceMetadata : public UMemory {
   public:
     UnitPreferenceMetadata(){};
     // Constructor, makes copies of the parameters passed to it.
-    UnitPreferenceMetadata(const StringPiece category, const StringPiece usage, const StringPiece region,
+    UnitPreferenceMetadata(StringPiece category, StringPiece usage, StringPiece region,
                            int32_t prefsOffset, int32_t prefsCount, UErrorCode &status);
 
     // Unit category (e.g. "length", "mass", "electric-capacitance").
@@ -177,7 +177,7 @@ class U_I18N_API UnitPreferences {
      *
      * TODO(hugovdm): maybe replace `UnitPreference **&outPrefrences` with a slice class?
      */
-    void getPreferencesFor(const StringPiece category, const StringPiece usage, const StringPiece region,
+    void getPreferencesFor(StringPiece category, StringPiece usage, StringPiece region,
                            const UnitPreference *const *&outPreferences, int32_t &preferenceCount,
                            UErrorCode &status) const;
 
