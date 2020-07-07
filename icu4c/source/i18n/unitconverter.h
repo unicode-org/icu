e8042785681f955f812a284b0a2f5e875ae9b26d
Hugo van der Merwe <17109322+hugovdm@users.noreply.github.com>
2020-07-07 17:11:34 +0200

    Signum, not SigNum.

--- icu4c/source/i18n/unitconverter.h
+++ icu4c/source/i18n/unitconverter.h
@@ -30,10 +30,10 @@ enum Constants {
     CONSTANTS_COUNT
 };
 
-typedef enum SigNum {
+typedef enum Signum {
     NEGATIVE = -1,
     POSITIVE = 1,
-} SigNum;
+} Signum;
 
 /* Represents a conversion factor */
 struct Factor {
@@ -60,7 +60,7 @@ struct Factor {
 /*
  * Adds a single factor element to the `Factor`. e.g "ft3m", "2.333" or "cup2m3". But not "cup2m3^3".
  */
-void U_I18N_API addSingleFactorConstant(StringPiece baseStr, int32_t power, SigNum sigNum,
+void U_I18N_API addSingleFactorConstant(StringPiece baseStr, int32_t power, Signum sigNum,
                                         Factor &factor, UErrorCode &status);
 
 /**
