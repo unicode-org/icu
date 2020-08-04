4c808456646b43146f00a64d8e87549ec6a15eda
Hugo van der Merwe <17109322+hugovdm@users.noreply.github.com>
2020-08-04 02:13:15 +0200

    unitsrouter: save targetUnit in prefs and results

--- icu4c/source/i18n/unitsrouter.h
+++ icu4c/source/i18n/unitsrouter.h
@@ -28,8 +28,13 @@ struct RouteResult : UMemory {
     MaybeStackVector<Measure> measures;
     UnicodeString precision;
 
-    RouteResult(MaybeStackVector<Measure> measures, UnicodeString precision)
-        : measures(std::move(measures)), precision(std::move(precision)) {}
+    // The output unit for this RouteResult. This may be a MIXED unit - for
+    // example: "yard-and-foot-and-inch", for which `measures` will have three
+    // elements.
+    MeasureUnitImpl outputUnit;
+
+    RouteResult(MaybeStackVector<Measure> measures, UnicodeString precision, MeasureUnitImpl outputUnit)
+        : measures(std::move(measures)), precision(std::move(precision)), outputUnit(std::move(outputUnit)) {}
 };
 
 /**
@@ -46,6 +51,10 @@ struct ConverterPreference : UMemory {
     double limit;
     UnicodeString precision;
 
+    // The output unit for this ConverterPreference. This may be a MIXED unit -
+    // for example: "yard-and-foot-and-inch".
+    MeasureUnitImpl targetUnit;
+
     // In case there is no limit, the limit will be -inf.
     ConverterPreference(const MeasureUnitImpl &source, const MeasureUnitImpl &complexTarget,
                         UnicodeString precision, const ConversionRates &ratesInfo, UErrorCode &status)
@@ -56,7 +65,7 @@ struct ConverterPreference : UMemory {
                         double limit, UnicodeString precision, const ConversionRates &ratesInfo,
                         UErrorCode &status)
         : converter(source, complexTarget, ratesInfo, status), limit(limit),
-          precision(std::move(precision)) {}
+          precision(std::move(precision)), targetUnit(complexTarget.copy(status)) {}
 };
 
 /**
