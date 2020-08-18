34c40262c1e2142350726736dcbffe680b350f66
Younies Mahmoud <younies.mahmoud@gmail.com>
2020-08-18 20:37:04 +0400

    UnitRouter Test Cases PAss

---
+++ icu4j/main/tests/core/src/com/ibm/icu/dev/test/impl/UnitRouterTest.java
@@ -0,0 +1,16 @@
+package com.ibm.icu.dev.test.impl;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class UnitRouterTest {
+    @Test
+    public void testUnitPreferencesFromUnitTests() throws IOException {
+        class TestCase {
+            
+        }
+
+
+    }
+}
