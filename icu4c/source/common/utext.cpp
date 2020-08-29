21a2910da772500b5ccbd7e5553b4493850f5a71
Rosen Penev <rosenp@gmail.com>
2020-08-29 15:33:13 -0700

    ICU-21253 remove std from max_align_t
    Causes compilation failure under CentOS 7 whose GCC (4.8.2) has a
    cstddef header that does not place max_align_t under the std namespace.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- icu4c/source/common/utext.cpp
+++ icu4c/source/common/utext.cpp
@@ -569,7 +569,7 @@ enum {
 
 struct ExtendedUText {
     UText               ut;
-    std::max_align_t    extension;
+    max_align_t    extension;
 };
 
 static const UText emptyText = UTEXT_INITIALIZER;
@@ -584,7 +584,7 @@ utext_setup(UText *ut, int32_t extraSpace, UErrorCode *status) {
         // We need to heap-allocate storage for the new UText
         int32_t spaceRequired = sizeof(UText);
         if (extraSpace > 0) {
-            spaceRequired = sizeof(ExtendedUText) + extraSpace - sizeof(std::max_align_t);
+            spaceRequired = sizeof(ExtendedUText) + extraSpace - sizeof(max_align_t);
         }
         ut = (UText *)uprv_malloc(spaceRequired);
         if (ut == NULL) {
