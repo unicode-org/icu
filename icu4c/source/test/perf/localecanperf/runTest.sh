bc864aeac6cd6279ef99b43468ab7799b32f8ce6
Frank Tang <ftang@chromium.org>
2020-08-31 22:14:37 +0000

    ICU-21051 Add copyright Remove License from sh
    See #1281

--- icu4c/source/test/perf/localecanperf/runTest.sh
+++ icu4c/source/test/perf/localecanperf/runTest.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Copyright (C) 2020 and later: Unicode, Inc. and others.
+# License & terms of use: http://www.unicode.org/copyright.html
 
 # run the test with 1, 10, 100, 1000, 10000, 100000 iterations
 LD_LIBRARY_PATH=lib:stubdata:tools/ctestfw:../../lib:../../stubdata:../../tools/ctestfw:$LD_LIBRARY_PATH:../../../lib:../../../stubdata:../../../tools/ctestfw:$LD_LIBRARY_PATH \
