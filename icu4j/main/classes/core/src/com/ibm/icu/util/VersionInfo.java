10bf5ea9cd9e1209ad5ca154665157468d71fd2c
Markus Scherer <markus.icu@gmail.com>
2020-10-02 11:08:39 -0700

    ICU-20901 VersionInfo.compareTo() compare signed version int as unsigned

--- icu4j/main/classes/core/src/com/ibm/icu/util/VersionInfo.java
+++ icu4j/main/classes/core/src/com/ibm/icu/util/VersionInfo.java
@@ -471,7 +471,15 @@ public final class VersionInfo implements Comparable<VersionInfo>
     @Override
     public int compareTo(VersionInfo other)
     {
-        return m_version_ - other.m_version_;
+        // m_version_ is an int, a signed 32-bit integer.
+        // When the major version is >=128, then the version int is negative.
+        // Compare it in two steps to simulate an unsigned-int comparison.
+        // (Alternatively we could turn each int into a long and reset the upper 32 bits.)
+        // Compare the upper bits first, using logical shift right (unsigned).
+        int diff = (m_version_ >>> 1) - (other.m_version_ >>> 1);
+        if (diff != 0) { return diff; }
+        // Compare the remaining bits.
+        return (m_version_ & 1) - (other.m_version_ & 1);
     }
 
     // private data members ----------------------------------------------
