157467ea16abc14b408d36f84217b6b2f6a5b809
Frank Tang <ftang@chromium.org>
2020-12-07 19:43:09 -0800

    ICU-21430 propagate error code

--- icu4c/source/test/intltest/loctest.cpp
+++ icu4c/source/test/intltest/loctest.cpp
@@ -6393,4 +6393,5 @@ void LocaleTest::TestLeak21419() {
     IcuTestErrorCode status(*this, "TestLeak21419");
     Locale l = Locale("s-yU");
     l.canonicalize(status);
+    status.expectErrorAndReset(U_ILLEGAL_ARGUMENT_ERROR);
 }
