30b4dd78433c07c150d1a04ddc31ca53a37ea2aa
Mojca Miklavec <mojca.miklavec.lists@gmail.com>
2021-02-28 09:34:30 +0100

    ICU-21513 check if TARGET_OS_SIMULATOR has been defined
    clang 11 throws an error when using undefined variables starting with
    TARGET_OS_, and TARGET_OS_SIMULATOR is missing in earlier versions
    of macOS, so the code would not compile without an additional guard.
    
    See also ICU-13440.

--- icu4c/source/common/putil.cpp
+++ icu4c/source/common/putil.cpp
@@ -1361,7 +1361,7 @@ uprv_pathIsAbsolute(const char *path)
 
 /* Backup setting of ICU_DATA_DIR_PREFIX_ENV_VAR
    (needed for some Darwin ICU build environments) */
-#if U_PLATFORM_IS_DARWIN_BASED && TARGET_OS_SIMULATOR
+#if U_PLATFORM_IS_DARWIN_BASED && defined(TARGET_OS_SIMULATOR) && TARGET_OS_SIMULATOR
 # if !defined(ICU_DATA_DIR_PREFIX_ENV_VAR)
 #  define ICU_DATA_DIR_PREFIX_ENV_VAR "IPHONE_SIMULATOR_ROOT"
 # endif
