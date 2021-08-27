5b96c2e84778411c58dca21a82aca15bb36eb008
Jeff Genovy <29107334+jefgen@users.noreply.github.com>
2021-08-26 19:23:16 -0700

    ICU-21710 Additional clean up after removing BOYER_MOORE code from usearch.cpp
    Changes:
    - We can completely remove the shift tables and related fields from
      data structs.
    
    - Creation of UStringSearch objects should be faster now,
      as it doesn't waste time computing the unused shift tables.
    
    - The sizeof(UStringSearch) is decreased from 5240 to 3192, so
      this should help to reduce memory for applications that create many string search objects.
    
    Note regarding the comments on initialize(). It actually does not set illegal argument error if pattern is all ignoreables. Added a test case for it.

--- icu4c/source/test/cintltst/usrchtst.c
+++ icu4c/source/test/cintltst/usrchtst.c
@@ -302,7 +302,7 @@ static void TestInitialization(void)
 {
           UErrorCode      status = U_ZERO_ERROR;
           UChar           pattern[512];
-    const UChar           text[] = {0x61, 0x62, 0x63, 0x64, 0x65, 0x66};
+    const UChar           text[] = u"abcdef";
     int32_t i = 0;
     UStringSearch  *result;
 
@@ -332,6 +332,15 @@ static void TestInitialization(void)
         log_err("Error opening search %s\n", u_errorName(status));
     }
     usearch_close(result);
+
+    /* testing that a pattern with all ignoreables doesn't fail initialization with an error */
+    UChar patternIgnoreables[] = u"\u200b"; // Zero Width Space
+    result = usearch_openFromCollator(patternIgnoreables, 1, text, 3, EN_US_, NULL, &status);
+    if (U_FAILURE(status)) {
+        log_err("Error opening search %s\n", u_errorName(status));
+    }
+    usearch_close(result);
+
     close();
 }
 
