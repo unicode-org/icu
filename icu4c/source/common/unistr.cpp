55a51fa9bde41598fb70c5931d313e908f21ca65
Daniel Bevenius <daniel.bevenius@gmail.com>
2021-10-04 13:10:02 +0200

    ICU-21784 suppress -Wunused-function warning in unistr.cpp
    This commit adds an ignore of the unused function
    uprv_UnicodeStringDummy.

--- icu4c/source/common/unistr.cpp
+++ icu4c/source/common/unistr.cpp
@@ -1977,7 +1977,12 @@ The vector deleting destructor is already a part of UObject,
 but defining it here makes sure that it is included with this object file.
 This makes sure that static library dependencies are kept to a minimum.
 */
+#if defined(__clang__) || U_GCC_MAJOR_MINOR >= 1100
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-function"
 static void uprv_UnicodeStringDummy(void) {
     delete [] (new UnicodeString[2]);
 }
+#pragma GCC diagnostic pop
+#endif
 #endif
