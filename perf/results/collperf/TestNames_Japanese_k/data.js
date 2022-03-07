0c4fefd29647a71173b814895e4f01df95cd1926
github-action-benchmark <github@users.noreply.github.com>
2022-03-07 21:04:39 +0000

    add Benchmark (ndjson) benchmark result for 80ee559205dd165c2d647610376d6f9a06822ae4

---
+++ perf/results/collperf/TestNames_Japanese_k/data.js
@@ -0,0 +1,62 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1646687079860,
+  "repoUrl": "https://github.com/unicode-org/icu",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "nrunge@google.com",
+            "name": "gnrunge",
+            "username": "gnrunge"
+          },
+          "committer": {
+            "email": "41129501+gnrunge@users.noreply.github.com",
+            "name": "Norbert Runge",
+            "username": "gnrunge"
+          },
+          "distinct": true,
+          "id": "80ee559205dd165c2d647610376d6f9a06822ae4",
+          "message": "ICU-21843 Add ICU4C performance tests to continuous integration\n\nSee #1987",
+          "timestamp": "2022-03-07T12:53:44-08:00",
+          "tree_id": "7da686bd1662079612215dc8b0f27437626720c3",
+          "url": "https://github.com/unicode-org/icu/commit/80ee559205dd165c2d647610376d6f9a06822ae4"
+        },
+        "date": 1646687078283,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestIcu_KeyGen_null",
+            "value": 315.465,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_qsort_strcoll_null",
+            "value": 26902892.3728,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_qsort_usekey",
+            "value": 6712488.0426,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_BinarySearch_strcoll_null",
+            "value": 28213131.8586,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_BinarySearch_usekey",
+            "value": 7533823.788,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      }
+    ]
+  }
+}
