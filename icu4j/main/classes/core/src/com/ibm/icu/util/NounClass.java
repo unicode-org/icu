6109dadfff45e72e29cd19b26511b5ee9672ca62
Yoshito Umaoka <yumaoka@users.noreply.github.com>
2022-03-16 08:33:05 -0400

    ICU-21900 BRS71 ICU4J API signature file

--- icu4j/main/classes/core/src/com/ibm/icu/util/NounClass.java
+++ icu4j/main/classes/core/src/com/ibm/icu/util/NounClass.java
@@ -6,15 +6,39 @@ package com.ibm.icu.util;
 /**
  * Represents all the grammatical noun classes that are supported by CLDR.
  *
- * @draft ICU 71.
+ * @draft ICU 71
  */
 public enum NounClass {
+    /**
+     * @draft ICU 71
+     */
     OTHER,
+    /**
+     * @draft ICU 71
+     */
     NEUTER,
+    /**
+     * @draft ICU 71
+     */
     FEMININE,
+    /**
+     * @draft ICU 71
+     */
     MASCULINE,
+    /**
+     * @draft ICU 71
+     */
     ANIMATE,
+    /**
+     * @draft ICU 71
+     */
     INANIMATE,
+    /**
+     * @draft ICU 71
+     */
     PERSONAL,
+    /**
+     * @draft ICU 71
+     */
     COMMON,
 }
