a25545539dce699e3b46ad2b76e0bea111378c4d
Rich Gillam <62772518+richgillam@users.noreply.github.com>
2022-03-25 14:15:40 -0700

    ICU-20715 Fix misplaced member initializer.

--- icu4c/source/i18n/collationdatabuilder.cpp
+++ icu4c/source/i18n/collationdatabuilder.cpp
@@ -292,10 +292,10 @@ CollationDataBuilder::CollationDataBuilder(UErrorCode &errorCode)
           ce32s(errorCode), ce64s(errorCode), conditionalCE32s(errorCode),
           modified(FALSE),
           fastLatinEnabled(FALSE), fastLatinBuilder(NULL),
-          collIter(NULL),
           lastContextIndex(0),
           lastContextCE32(0),
-          lastContextWasSuffix(false) {
+          lastContextWasSuffix(false),
+          collIter(NULL) {
     // Reserve the first CE32 for U+0000.
     ce32s.addElement(0, errorCode);
     conditionalCE32s.setDeleter(uprv_deleteConditionalCE32);
