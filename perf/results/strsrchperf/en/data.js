7e0ca390f5c2bb592c4a0b4e03f71943feb917c9
github-action-benchmark <github@users.noreply.github.com>
2022-07-29 22:56:37 +0000

    add Benchmark (ndjson) benchmark result for dcd19ae9bc0914f8be439a2de1a5fbc17e7b5447

--- perf/results/strsrchperf/en/data.js
+++ perf/results/strsrchperf/en/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1659050821706,
+  "lastUpdate": 1659135397887,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -1402,6 +1402,42 @@ window.BENCHMARK_DATA = {
           {
             "name": "Test_ICU_Backward_Search",
             "value": 55.5282,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pedberg@unicode.org",
+            "name": "Peter Edberg",
+            "username": "pedberg-icu"
+          },
+          "committer": {
+            "email": "42151464+pedberg-icu@users.noreply.github.com",
+            "name": "Peter Edberg",
+            "username": "pedberg-icu"
+          },
+          "distinct": true,
+          "id": "dcd19ae9bc0914f8be439a2de1a5fbc17e7b5447",
+          "message": "ICU-21957 integrate CLDR release-42-alpha0 (first with Survey Tool data) to ICU main for 72 (#2142)",
+          "timestamp": "2022-07-29T15:32:45-07:00",
+          "tree_id": "6e9b3f20e3d80a16de21313da72a5cb5b6ac2361",
+          "url": "https://github.com/unicode-org/icu/commit/dcd19ae9bc0914f8be439a2de1a5fbc17e7b5447"
+        },
+        "date": 1659135393910,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "Test_ICU_Forward_Search",
+            "value": 28.1509,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "Test_ICU_Backward_Search",
+            "value": 62.178,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
