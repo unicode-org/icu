734b43084c2b63558701e7f68d676d4e9ba5e01d
github-action-benchmark <github@users.noreply.github.com>
2022-08-11 22:20:27 +0000

    add Benchmark (ndjson) benchmark result for 3ef03a408714cf0be1f6be62e3fad57757403da3

--- perf/results/j_dateformatperf/sw_KE/TestICUFormat/1/data.js
+++ perf/results/j_dateformatperf/sw_KE/TestICUFormat/1/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1660231954619,
+  "lastUpdate": 1660256427180,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -538,6 +538,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 260.69223154005937,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "elango@unicode.org",
+            "name": "Elango",
+            "username": "echeran"
+          },
+          "distinct": true,
+          "id": "3ef03a408714cf0be1f6be62e3fad57757403da3",
+          "message": "ICU-21947 Replace FixedDecimal with DecimalQuantity in PluralRule sample parsing\n\nSee #2007",
+          "timestamp": "2022-08-11T15:10:37-07:00",
+          "tree_id": "2216414cc87171c10282454fcf0a35d917a80fbd",
+          "url": "https://github.com/unicode-org/icu/commit/3ef03a408714cf0be1f6be62e3fad57757403da3"
+        },
+        "date": 1660256422976,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 290.09813512420476,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
