a931ca5de485eea7bfcd5f4fcf029ccea8e27e1b
github-action-benchmark <github@users.noreply.github.com>
2022-08-16 23:00:12 +0000

    add Benchmark (ndjson) benchmark result for 59615c93f694227eaacf242d22a549c316557cdf

--- perf/results/j_dateformatperf/en_US/TestICUConstruction/2/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUConstruction/2/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1660670731303,
+  "lastUpdate": 1660690812846,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -898,6 +898,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUConstruction",
             "value": 2579.2740887021323,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "hsivonen@hsivonen.fi",
+            "name": "Henri Sivonen",
+            "username": "hsivonen"
+          },
+          "committer": {
+            "email": "elango@unicode.org",
+            "name": "Elango",
+            "username": "echeran"
+          },
+          "distinct": true,
+          "id": "59615c93f694227eaacf242d22a549c316557cdf",
+          "message": "ICU-22115 Merge passthrough and canonical combining class data into the NFD trie for ICU4X",
+          "timestamp": "2022-08-16T15:53:56-07:00",
+          "tree_id": "4f51bc73e1477d7f636740f5506076ee6756f1af",
+          "url": "https://github.com/unicode-org/icu/commit/59615c93f694227eaacf242d22a549c316557cdf"
+        },
+        "date": 1660690809557,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUConstruction",
+            "value": 2178.8945412045246,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
