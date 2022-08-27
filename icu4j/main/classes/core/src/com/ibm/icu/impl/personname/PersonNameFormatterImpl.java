df93dd00bf93bdbb97d520d13290285390dd83b3
Rich Gillam <62772518+richgillam@users.noreply.github.com>
2022-08-26 18:21:21 -0700

    ICU-22081 Addressed review comments: - Changed all public Javadoc to use the proper verbiage for identifying this code as   a tech preview. - Changed all references to ULocale in the API to use Locale instead. - Changed PersonNameFormatter and SimplePersonName so that they're constructed by way   of separate builder objects. - Added an internal constructor for PersonNameFormatter just for the use of the unit tests,   and added some unit tests that take advantage of the new constructor to test the internals   more thoroughly. - Renamed format() to formatToString(), to make room to add a format() method later that   returns a FormattedPersonName. - Added logic to guess the locale of a name object when the name object doesn't supply   a locale. - Moved the function that constructs a SimplePersonName from a single string containing   key-value pairs out of the API and into the unit tests. - Various smaller changes in response to API- and code-review comments.

--- icu4j/main/classes/core/src/com/ibm/icu/impl/personname/PersonNameFormatterImpl.java
+++ icu4j/main/classes/core/src/com/ibm/icu/impl/personname/PersonNameFormatterImpl.java
@@ -5,12 +5,15 @@ package com.ibm.icu.impl.personname;
 import com.ibm.icu.impl.ICUData;
 import com.ibm.icu.impl.ICUResourceBundle;
 import com.ibm.icu.lang.UScript;
-import com.ibm.icu.text.FormattedValue;
 import com.ibm.icu.text.PersonNameFormatter;
 import com.ibm.icu.util.ULocale;
 import com.ibm.icu.util.UResourceBundle;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
 
 import static com.ibm.icu.util.UResourceBundle.ARRAY;
 import static com.ibm.icu.util.UResourceBundle.STRING;
@@ -19,7 +22,7 @@ import static com.ibm.icu.util.UResourceBundle.STRING;
  * Actual implementation class for PersonNameFormatter.
  */
 public class PersonNameFormatterImpl {
-    private final ULocale locale;
+    private final Locale locale;
     private final PersonNamePattern[] gnFirstPatterns;
     private final PersonNamePattern[] snFirstPatterns;
     private final Set<String> gnFirstLocales;
@@ -34,7 +37,7 @@ public class PersonNameFormatterImpl {
     private final PersonNameFormatter.Formality formality;
     private final Set<PersonNameFormatter.Options> options;
 
-    public PersonNameFormatterImpl(ULocale locale,
+    public PersonNameFormatterImpl(Locale locale,
                                    PersonNameFormatter.Length length,
                                    PersonNameFormatter.Usage usage,
                                    PersonNameFormatter.Formality formality,
@@ -71,7 +74,7 @@ public class PersonNameFormatterImpl {
         }
 
         // load the actual formatting patterns-- since we don't know the name order until formatting time (it can be
-        // different for different names), load patterns for both GN-first and SN-first names.  (If the user has
+        // different for different names), load patterns for both given-first and surname-first names.  (If the user has
         // specified SORTING, we don't need to do this-- we just load the "sorting" patterns and ignore the name's order.)
         final String RESOURCE_PATH_PREFIX = "personNames/namePattern/";
         String resourceNameBody = length.toString().toLowerCase() + "-" + usage.toString().toLowerCase() + "-"
@@ -97,17 +100,41 @@ public class PersonNameFormatterImpl {
         }
     }
 
-    public String format(PersonNameFormatter.PersonName name) {
+    /**
+     * THIS IS A DUMMY CONSTRUCTOR JUST FOR THE USE OF THE UNIT TESTS TO CHECK SOME OF THE INTERNAL IMPLEMENTATION!
+     */
+    public PersonNameFormatterImpl(Locale locale, String[] patterns) {
+        // first, set dummy values for the other fields
+        snFirstPatterns = null;
+        gnFirstLocales = null;
+        snFirstLocales = null;
+        length = PersonNameFormatter.Length.MEDIUM;
+        usage = PersonNameFormatter.Usage.REFERRING;
+        formality = PersonNameFormatter.Formality.FORMAL;
+        options = Collections.emptySet();
+        initialPattern = "{0}.";
+        initialSequencePattern = "{0} {1}";
+        capitalizeSurname = false;
+        foreignSpaceReplacement = " ";
+        formatterLocaleUsesSpaces = true;
+
+        // then, set values for the fields we actually care about
+        this.locale = locale;
+        gnFirstPatterns = PersonNamePattern.makePatterns(patterns, this);
+
+    }
+
+    public String formatToString(PersonNameFormatter.PersonName name) {
         // TODO: Should probably return a FormattedPersonName object
 
         // if the formatter is for a language that doesn't use spaces between words and the name is from a language
         // that does, create a formatter for the NAME'S locale and use THAT to format the name
-        ULocale nameLocale = name.getNameLocale();
+        Locale nameLocale = getNameLocale(name);
         boolean nameLocaleUsesSpaces = !LOCALES_THAT_DONT_USE_SPACES.contains(nameLocale.getLanguage());
         if (!formatterLocaleUsesSpaces && nameLocaleUsesSpaces) {
             PersonNameFormatterImpl nativeFormatter = new PersonNameFormatterImpl(nameLocale, this.length,
                     this.usage, this.formality, this.options);
-            String result = nativeFormatter.format(name);
+            String result = nativeFormatter.formatToString(name);
 
             // BUT, if the name is actually written in the formatter locale's script, replace any spaces in the name
             // with the foreignSpaceReplacement character
@@ -117,8 +144,8 @@ public class PersonNameFormatterImpl {
             return result;
         }
 
-        // if we get down to here, we're just doing normal formatting-- if we have both GN-first and SN-first rules,
-        // choose which one to use based on the name's locale and preferred field order
+        // if we get down to here, we're just doing normal formatting-- if we have both given-first and surname-first
+        // rules, choose which one to use based on the name's locale and preferred field order
         if (snFirstPatterns == null || nameIsGnFirst(name)) {
             return getBestPattern(gnFirstPatterns, name).format(name);
         } else {
@@ -126,9 +153,17 @@ public class PersonNameFormatterImpl {
         }
     }
 
-    public ULocale getLocale() {
+    public Locale getLocale() {
         return locale;
     }
+
+    public PersonNameFormatter.Length getLength() { return length; }
+
+    public PersonNameFormatter.Usage getUsage() { return usage; }
+
+    public PersonNameFormatter.Formality getFormality() { return formality; }
+
+    public Set<PersonNameFormatter.Options> getOptions() { return options; }
 
     public String getInitialPattern() {
         return initialPattern;
@@ -142,7 +177,7 @@ public class PersonNameFormatterImpl {
         return capitalizeSurname;
     }
 
-    private final Set<String> LOCALES_THAT_DONT_USE_SPACES = new HashSet<>(Arrays.asList("ja", "zh", "th", "yue"));
+    private final Set<String> LOCALES_THAT_DONT_USE_SPACES = new HashSet<>(Arrays.asList("ja", "zh", "th", "yue", "km", "lo"));
 
     /**
      * Returns the value of the resource, as a string array.
@@ -164,7 +199,7 @@ public class PersonNameFormatterImpl {
      * Returns the field order to use when formatting this name, taking into account the name's preferredOrder
      * field, as well as the name and formatter's respective locales.
      * @param name The name to be formatted.
-     * @return If true, use GN-first order to format the name; if false, use SN-first order.
+     * @return If true, use given-first order to format the name; if false, use surname-first order.
      */
     private boolean nameIsGnFirst(PersonNameFormatter.PersonName name) {
         // the name can declare its order-- check that first (it overrides any locale-based calculation)
@@ -180,7 +215,7 @@ public class PersonNameFormatterImpl {
             }
         }
 
-        String localeStr = name.getNameLocale().toString();
+        String localeStr = getNameLocale(name).toString();
         do {
             if (gnFirstLocales.contains(localeStr)) {
                 return true;
@@ -229,11 +264,52 @@ public class PersonNameFormatterImpl {
     }
 
     /**
+     * Internal function to figure out the name's locale when the name doesn't specify it.
+     * (Note that this code assumes that if the locale is specified, it includes a language
+     * code.)
+     * @param name The name for which we need the locale
+     * @return The name's (real or guessed) locale.
+     */
+    private Locale getNameLocale(PersonNameFormatter.PersonName name) {
+        // if the name specifies its locale, we can just return it
+        Locale nameLocale = name.getNameLocale();
+        if (nameLocale == null) {
+            // if not, we look at the characters in the name.  If their script matches the default script for the formatter's
+            // locale, we use the formatter's locale as the name's locale
+            int formatterScript = UScript.getCodeFromName(ULocale.addLikelySubtags(ULocale.forLocale(locale)).getScript());
+            String givenName = name.getFieldValue(PersonNameFormatter.NameField.GIVEN, new HashSet<PersonNameFormatter.FieldModifier>());
+            int nameScript = UScript.INVALID_CODE;
+            for (int i = 0; nameScript == UScript.INVALID_CODE && i < givenName.length(); i++) {
+                // the script of the name is the script of the first character in the name whose script isn't
+                // COMMON or INHERITED
+                int script = UScript.getScript(givenName.charAt(i));
+                if (script != UScript.COMMON && script != UScript.INHERITED) {
+                    nameScript = script;
+                }
+            }
+            if (formatterScript == nameScript) {
+                nameLocale = this.locale;
+            } else {
+                // if the name's script is different from the formatter's script, we use addLikelySubtags() to find the
+                // default language for the name's script and use THAT as the name's locale
+                nameLocale = new Locale(ULocale.addLikelySubtags(new ULocale("und_" + UScript.getShortName(nameScript))).getLanguage());
+            }
+            // TODO: This algorithm has a few deficiencies: First, it assumes the script of the string is the script of the first
+            // character in the string that's not COMMON or INHERITED.  This won't work well for some languages, such as Japanese,
+            // that use multiple scripts.  Doing better would require adding a new getScript(String) method on UScript, which
+            // might be something we want.  Second, we only look at the given-name field.  This field should always be populated,
+            // but if it isn't, we're stuck.  Looking at all the fields requires API on PersonName that we don't need anywhere
+            // else.
+        }
+        return nameLocale;
+    }
+
+    /**
      * Returns true if the script of `s` is one of the default scripts for `locale`.
      * This function only checks the script of the first character whose script isn't "common,"
      * so it probably won't work right on mixed-script strings.
      */
-    private boolean scriptMatchesLocale(String s, ULocale locale) {
+    private boolean scriptMatchesLocale(String s, Locale locale) {
         int[] localeScripts = UScript.getCode(locale);
         int stringScript = UScript.COMMON;
         for (int i = 0; stringScript == UScript.COMMON && i < s.length(); i++) {
