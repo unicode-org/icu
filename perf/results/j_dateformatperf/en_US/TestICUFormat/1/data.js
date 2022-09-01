a755b485eb61ade88dee8e87eff3bc0f42ade168
github-action-benchmark <github@users.noreply.github.com>
2022-09-01 17:08:57 +0000

    add Benchmark (ndjson) benchmark result for 9acba58c493123234b9c3ed4326d35d8f73cbcc1

--- perf/results/j_dateformatperf/en_US/TestICUFormat/1/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUFormat/1/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1661988170003,
+  "lastUpdate": 1662052137372,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -1198,6 +1198,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 265.6378012688902,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "yumaoka@users.noreply.github.com",
+            "name": "Yoshito Umaoka",
+            "username": "yumaoka"
+          },
+          "committer": {
+            "email": "yumaoka@users.noreply.github.com",
+            "name": "Yoshito Umaoka",
+            "username": "yumaoka"
+          },
+          "distinct": true,
+          "id": "9acba58c493123234b9c3ed4326d35d8f73cbcc1",
+          "message": "ICU-22116 Updating minimum Java runtime support to Java 8",
+          "timestamp": "2022-09-01T13:02:27-04:00",
+          "tree_id": "3effaf2286da632e64849215f3216eb05a4448f1",
+          "url": "https://github.com/unicode-org/icu/commit/9acba58c493123234b9c3ed4326d35d8f73cbcc1"
+        },
+        "date": 1662052132654,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 238.11659739134961,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
