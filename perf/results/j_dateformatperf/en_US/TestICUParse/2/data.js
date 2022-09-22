df5eb2003dc351ba1e0ec4d811e1444cd76e75e2
github-action-benchmark <github@users.noreply.github.com>
2022-09-22 21:30:16 +0000

    add Benchmark (ndjson) benchmark result for cd12cebb8768edc4d47c158c903be40c32af4cde

--- perf/results/j_dateformatperf/en_US/TestICUParse/2/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUParse/2/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1663880915891,
+  "lastUpdate": 1663882216333,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -2098,6 +2098,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 236.06574629048723,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pedberg@unicode.org",
+            "name": "Peter Edberg",
+            "username": "pedberg-icu"
+          },
+          "committer": {
+            "email": "42151464+pedberg-icu@users.noreply.github.com",
+            "name": "Peter Edberg",
+            "username": "pedberg-icu"
+          },
+          "distinct": true,
+          "id": "cd12cebb8768edc4d47c158c903be40c32af4cde",
+          "message": "ICU-21879 Fix UserGuide link to info in Korean transliteration",
+          "timestamp": "2022-09-22T13:54:40-07:00",
+          "tree_id": "92eec4df02a7500358b90dad09e005b9fd90cac1",
+          "url": "https://github.com/unicode-org/icu/commit/cd12cebb8768edc4d47c158c903be40c32af4cde"
+        },
+        "date": 1663882203720,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 203.3480161906148,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
