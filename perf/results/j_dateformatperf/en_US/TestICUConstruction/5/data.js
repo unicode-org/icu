38981c03da1c805df18e5bd708572d4d4a2984d3
github-action-benchmark <github@users.noreply.github.com>
2022-09-27 23:08:47 +0000

    add Benchmark (ndjson) benchmark result for e72233f8b7526bbb840ae7742948a892686acfee

--- perf/results/j_dateformatperf/en_US/TestICUConstruction/5/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUConstruction/5/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1664234870097,
+  "lastUpdate": 1664320127006,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -2368,6 +2368,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUConstruction",
             "value": 2473.434942575143,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "e72233f8b7526bbb840ae7742948a892686acfee",
+          "message": "ICU-21959 fix file-level doxygen issues",
+          "timestamp": "2022-09-27T15:58:43-07:00",
+          "tree_id": "e935fae45795ccdd00637839bd409b521f5265ff",
+          "url": "https://github.com/unicode-org/icu/commit/e72233f8b7526bbb840ae7742948a892686acfee"
+        },
+        "date": 1664320116907,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUConstruction",
+            "value": 2376.295350538752,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
