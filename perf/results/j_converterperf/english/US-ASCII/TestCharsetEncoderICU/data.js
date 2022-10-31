7998afaa4d26d63600763a7504f47878e6f8b654
github-action-benchmark <github@users.noreply.github.com>
2022-10-31 23:21:10 +0000

    add Benchmark (ndjson) benchmark result for 436f5a7df59c4780dedc0149c6756c16f4c803ce

--- perf/results/j_converterperf/english/US-ASCII/TestCharsetEncoderICU/data.js
+++ perf/results/j_converterperf/english/US-ASCII/TestCharsetEncoderICU/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1667231936184,
+  "lastUpdate": 1667258470054,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -3118,6 +3118,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestCharsetEncoderICU",
             "value": 0.8199778088464633,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "436f5a7df59c4780dedc0149c6756c16f4c803ce",
+          "message": "ICU-22194 runConfigureICU computer->compiler\n\n@josephshen found that the help text here has the wrong word, and I dropped the ball on his PR #2217 :-(",
+          "timestamp": "2022-10-31T16:18:18-07:00",
+          "tree_id": "451b22446725ab19d85013e389d39011e4932d3e",
+          "url": "https://github.com/unicode-org/icu/commit/436f5a7df59c4780dedc0149c6756c16f4c803ce"
+        },
+        "date": 1667258456232,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestCharsetEncoderICU",
+            "value": 0.7615695540806612,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
