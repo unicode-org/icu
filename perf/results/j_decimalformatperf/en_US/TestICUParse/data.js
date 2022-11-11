f3d5e872d5161b2cb37ce4f2676a73690bc1271d
github-action-benchmark <github@users.noreply.github.com>
2022-11-11 22:53:00 +0000

    add Benchmark (ndjson) benchmark result for 9c1fb785b30ad8ce3177f1a6dab13ba224ef54f9

--- perf/results/j_decimalformatperf/en_US/TestICUParse/data.js
+++ perf/results/j_decimalformatperf/en_US/TestICUParse/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1668029102779,
+  "lastUpdate": 1668207180706,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -3478,6 +3478,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 409.18693971567706,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ftang@chromium.org",
+            "name": "Frank Tang",
+            "username": "FrankYFTang"
+          },
+          "committer": {
+            "email": "ftang@google.com",
+            "name": "Frank Yung-Fong Tang",
+            "username": "FrankYFTang"
+          },
+          "distinct": true,
+          "id": "9c1fb785b30ad8ce3177f1a6dab13ba224ef54f9",
+          "message": "ICU-22164 Replace switch+getType with subclass\n\nSee #2215\n\nfix",
+          "timestamp": "2022-11-11T14:49:16-08:00",
+          "tree_id": "73d8753f29e0fd31bc6b0aaa0d0c66b5e01658a2",
+          "url": "https://github.com/unicode-org/icu/commit/9c1fb785b30ad8ce3177f1a6dab13ba224ef54f9"
+        },
+        "date": 1668207169534,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 407.0214283244223,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
