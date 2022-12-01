a1b624ac2af1403fa9541e26cf837705be9360c0
Peter Edberg <pedberg@unicode.org>
2022-12-01 12:33:01 -0800

    ICU-22220 BRS73RC Update version number to 73.0.1

--- icu4j/main/tests/core/src/com/ibm/icu/dev/test/util/DebugUtilitiesData.java
+++ icu4j/main/tests/core/src/com/ibm/icu/dev/test/util/DebugUtilitiesData.java
@@ -10,7 +10,7 @@
 package com.ibm.icu.dev.test.util;
 
 public class DebugUtilitiesData extends Object {
-    public static final String ICU4C_VERSION="72.1";
+    public static final String ICU4C_VERSION="73.0.1";
     public static final int UDebugEnumType = 0;
     public static final int UCalendarDateFields = 1;
     public static final int UCalendarMonths = 2;
