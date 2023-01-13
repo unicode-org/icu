00d26c45af6f3a3413d3b15ad016a0ab5961cdc9
github-action-benchmark <github@users.noreply.github.com>
2023-01-13 23:16:17 +0000

    add Benchmark (ndjson) benchmark result for e7530bd9ff2e7317710a1611f6d6de264143bf6a

--- perf/results/j_normperf/TestNames_Chinese/TestICU_NFC_Orig_Text/data.js
+++ perf/results/j_normperf/TestNames_Chinese/TestICU_NFC_Orig_Text/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1673642821766,
+  "lastUpdate": 1673651777000,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -4138,6 +4138,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFC_Orig_Text",
             "value": 6.331969711282495,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "distinct": true,
+          "id": "e7530bd9ff2e7317710a1611f6d6de264143bf6a",
+          "message": "ICU-12811 Fix CI cache retain workflow's cron schedule string",
+          "timestamp": "2023-01-13T14:57:51-08:00",
+          "tree_id": "8f682b0352a1e935f54d6bf6533a94d6ee722c88",
+          "url": "https://github.com/unicode-org/icu/commit/e7530bd9ff2e7317710a1611f6d6de264143bf6a"
+        },
+        "date": 1673651762875,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFC_Orig_Text",
+            "value": 5.175537435259161,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
