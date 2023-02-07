f2067a1622749a77392f30eeee83a885013452d2
github-action-benchmark <github@users.noreply.github.com>
2023-02-07 02:05:42 +0000

    add Benchmark (ndjson) benchmark result for 39dfee39b88a09fcafb33992a5495c4117490644

--- perf/results/j_dateformatperf/en_US/TestICUParse/2/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUParse/2/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1675727826875,
+  "lastUpdate": 1675735542184,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -4678,6 +4678,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 174.3288469721154,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "39dfee39b88a09fcafb33992a5495c4117490644",
+          "message": "ICU-22257 BRS doc: J with little-endian data automatic",
+          "timestamp": "2023-02-06T17:52:40-08:00",
+          "tree_id": "f7b750f20376f0be1ea10ec21548b44276a631e2",
+          "url": "https://github.com/unicode-org/icu/commit/39dfee39b88a09fcafb33992a5495c4117490644"
+        },
+        "date": 1675735526842,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 191.976713965159,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
