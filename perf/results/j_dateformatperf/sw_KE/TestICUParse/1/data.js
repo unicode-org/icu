5816cca61966dc77c4d99cc273589e9c707f518b
github-action-benchmark <github@users.noreply.github.com>
2023-02-16 02:23:43 +0000

    add Benchmark (ndjson) benchmark result for bd065d4704bb212dfe3414c9d1ef105fb2a4df75

--- perf/results/j_dateformatperf/sw_KE/TestICUParse/1/data.js
+++ perf/results/j_dateformatperf/sw_KE/TestICUParse/1/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1676510144252,
+  "lastUpdate": 1676514223340,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -5398,6 +5398,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 507.83353926463207,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "distinct": true,
+          "id": "bd065d4704bb212dfe3414c9d1ef105fb2a4df75",
+          "message": "ICU-22220 Automate BRS tasks\n\nSee #2318",
+          "timestamp": "2023-02-15T21:18:58-05:00",
+          "tree_id": "811adba53a48b4f444702294bf9c98c462633c37",
+          "url": "https://github.com/unicode-org/icu/commit/bd065d4704bb212dfe3414c9d1ef105fb2a4df75"
+        },
+        "date": 1676514201435,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 654.9238859760419,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
