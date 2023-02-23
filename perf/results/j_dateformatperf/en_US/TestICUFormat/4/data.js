1c52076f434aaf651224738907b0decd2ce3aa08
github-action-benchmark <github@users.noreply.github.com>
2023-02-23 01:25:01 +0000

    add Benchmark (ndjson) benchmark result for d86b1cebe192004759b6c875b0f831b97ccdae34

--- perf/results/j_dateformatperf/en_US/TestICUFormat/4/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUFormat/4/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1677102679690,
+  "lastUpdate": 1677115500861,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -5338,6 +5338,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 279.9830222911238,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "d86b1cebe192004759b6c875b0f831b97ccdae34",
+          "message": "ICU-22220 update root collation from CLDR 43",
+          "timestamp": "2023-02-22T17:13:13-08:00",
+          "tree_id": "a8524f42361f61a8d752effc0f6ac4b89edecd2c",
+          "url": "https://github.com/unicode-org/icu/commit/d86b1cebe192004759b6c875b0f831b97ccdae34"
+        },
+        "date": 1677115481753,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 272.4284481041238,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
