8a1f19face7eeaab1976e3eeee60364170341411
github-action-benchmark <github@users.noreply.github.com>
2023-03-01 23:40:13 +0000

    add Benchmark (ndjson) benchmark result for 28643799377ecf654564f6f31854b02788cebe33

--- perf/results/j_dateformatperf/en_US/TestICUFormat/1/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUFormat/1/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1677713613604,
+  "lastUpdate": 1677714012681,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -5758,6 +5758,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 262.28901873085545,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "28643799377ecf654564f6f31854b02788cebe33",
+          "message": "ICU-21833 replace U_NOEXCEPT with C++11 standard noexcept",
+          "timestamp": "2023-03-01T15:24:34-08:00",
+          "tree_id": "270c042f0a59e17ed55a92489897e2ee8f42f78e",
+          "url": "https://github.com/unicode-org/icu/commit/28643799377ecf654564f6f31854b02788cebe33"
+        },
+        "date": 1677713993950,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 252.38013463313263,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
