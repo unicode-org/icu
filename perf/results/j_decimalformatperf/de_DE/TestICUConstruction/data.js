dcbf2c18225eb6110f6e8f7aa2955272a000633b
github-action-benchmark <github@users.noreply.github.com>
2023-03-03 20:01:51 +0000

    add Benchmark (ndjson) benchmark result for 461ec392a5b0779434d8e8e4751216af547ee08e

--- perf/results/j_decimalformatperf/de_DE/TestICUConstruction/data.js
+++ perf/results/j_decimalformatperf/de_DE/TestICUConstruction/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1677785643970,
+  "lastUpdate": 1677873711666,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -5908,6 +5908,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUConstruction",
             "value": 544.0652580526574,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ccornelius@google.com",
+            "name": "Craig",
+            "username": "sven-oly"
+          },
+          "committer": {
+            "email": "cwcornelius@gmail.com",
+            "name": "Craig Cornelius",
+            "username": "sven-oly"
+          },
+          "distinct": true,
+          "id": "461ec392a5b0779434d8e8e4751216af547ee08e",
+          "message": "ICU-22220 ICU4J API change report",
+          "timestamp": "2023-03-03T19:47:31Z",
+          "tree_id": "9df1568e5bee95e936fef335c1dacabc5740f014",
+          "url": "https://github.com/unicode-org/icu/commit/461ec392a5b0779434d8e8e4751216af547ee08e"
+        },
+        "date": 1677873692313,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUConstruction",
+            "value": 437.701910712579,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
