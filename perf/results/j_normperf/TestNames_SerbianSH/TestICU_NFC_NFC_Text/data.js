adf7ce17332db2f2723057864fd232d7dff2cc76
github-action-benchmark <github@users.noreply.github.com>
2023-03-04 02:34:11 +0000

    add Benchmark (ndjson) benchmark result for bca85d4641978596cbefb0269ebd7f9834345d67

--- perf/results/j_normperf/TestNames_SerbianSH/TestICU_NFC_NFC_Text/data.js
+++ perf/results/j_normperf/TestNames_SerbianSH/TestICU_NFC_NFC_Text/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1677896939481,
+  "lastUpdate": 1677897251111,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -5818,6 +5818,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFC_NFC_Text",
             "value": 3.1885268052727604,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ccornelius@google.com",
+            "name": "Craig",
+            "username": "sven-oly"
+          },
+          "committer": {
+            "email": "cwcornelius@gmail.com",
+            "name": "Craig Cornelius",
+            "username": "sven-oly"
+          },
+          "distinct": true,
+          "id": "bca85d4641978596cbefb0269ebd7f9834345d67",
+          "message": "ICU-22220 Update ICU4J API status",
+          "timestamp": "2023-03-04T02:18:24Z",
+          "tree_id": "9cce2afd94c02304e7f988156cd28466dbfec93e",
+          "url": "https://github.com/unicode-org/icu/commit/bca85d4641978596cbefb0269ebd7f9834345d67"
+        },
+        "date": 1677897227445,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFC_NFC_Text",
+            "value": 2.7400334620461804,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
