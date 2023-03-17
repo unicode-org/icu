e243313999ddab15b8e2d8fb421c41b68e8c8d55
github-action-benchmark <github@users.noreply.github.com>
2023-03-17 16:05:25 +0000

    add Benchmark (ndjson) benchmark result for 74641a344e231c01570ed99025ef4319a2186be6

--- perf/results/j_normperf/TestNames_Asian/TestICU_NFD_Orig_Text/data.js
+++ perf/results/j_normperf/TestNames_Asian/TestICU_NFD_Orig_Text/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679068494279,
+  "lastUpdate": 1679069125071,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -6808,6 +6808,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFD_Orig_Text",
             "value": 17.366264471970762,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "74641a344e231c01570ed99025ef4319a2186be6",
+          "message": "ICU-12725 adjust API docs for new impl != JDK",
+          "timestamp": "2023-03-17T08:48:42-07:00",
+          "tree_id": "be6d51e4869ff2f00298db834c887868e9b47b07",
+          "url": "https://github.com/unicode-org/icu/commit/74641a344e231c01570ed99025ef4319a2186be6"
+        },
+        "date": 1679069098772,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFD_Orig_Text",
+            "value": 15.398868098424758,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
