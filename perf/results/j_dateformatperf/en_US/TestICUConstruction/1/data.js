70ab6208a70d96dd77268b70a57e5970829216c6
github-action-benchmark <github@users.noreply.github.com>
2023-03-23 16:08:24 +0000

    add Benchmark (ndjson) benchmark result for cb65f7573978edecd8a598ab4137327ed7000844

--- perf/results/j_dateformatperf/en_US/TestICUConstruction/1/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUConstruction/1/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679587088297,
+  "lastUpdate": 1679587704451,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -7348,6 +7348,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUConstruction",
             "value": 2204.3421207241636,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "distinct": true,
+          "id": "cb65f7573978edecd8a598ab4137327ed7000844",
+          "message": "ICU-22220 BRS 73rc ICU4J Serialization test data for ICU 73.1",
+          "timestamp": "2023-03-23T08:45:38-07:00",
+          "tree_id": "a44935926111d32d475f7b1f25e0f34eef09aa7c",
+          "url": "https://github.com/unicode-org/icu/commit/cb65f7573978edecd8a598ab4137327ed7000844"
+        },
+        "date": 1679587677634,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUConstruction",
+            "value": 2197.3238479076927,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
