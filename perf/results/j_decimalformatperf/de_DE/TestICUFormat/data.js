92b4f5089d4c3006325344b678c859149ca720d5
github-action-benchmark <github@users.noreply.github.com>
2023-03-23 18:04:16 +0000

    add Benchmark (ndjson) benchmark result for b31579920e11fe42eca67ccebe58d9dfd33edd80

--- perf/results/j_decimalformatperf/de_DE/TestICUFormat/data.js
+++ perf/results/j_decimalformatperf/de_DE/TestICUFormat/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679589621738,
+  "lastUpdate": 1679594656922,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -7318,6 +7318,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 782.3715311538701,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "y.umaoka@gmail.com",
+            "name": "yumaoka",
+            "username": "yumaoka"
+          },
+          "committer": {
+            "email": "yumaoka@users.noreply.github.com",
+            "name": "Yoshito Umaoka",
+            "username": "yumaoka"
+          },
+          "distinct": true,
+          "id": "b31579920e11fe42eca67ccebe58d9dfd33edd80",
+          "message": "ICU-22327 Update tzdata to 2023a",
+          "timestamp": "2023-03-23T13:56:40-04:00",
+          "tree_id": "c505f625913b6a0f0e88d34bd308788dfe5c2ac8",
+          "url": "https://github.com/unicode-org/icu/commit/b31579920e11fe42eca67ccebe58d9dfd33edd80"
+        },
+        "date": 1679594620891,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 800.2741915337012,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
