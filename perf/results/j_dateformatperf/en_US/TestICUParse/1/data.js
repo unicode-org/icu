508bcd4757515acf15bb5524666a7f98ea8c091e
github-action-benchmark <github@users.noreply.github.com>
2023-04-13 02:58:30 +0000

    add Benchmark (ndjson) benchmark result for 12c5fe04c06961676abc35b4e9ca5d0bdddd45f7

--- perf/results/j_dateformatperf/en_US/TestICUParse/1/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUParse/1/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681340746499,
+  "lastUpdate": 1681354710676,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -7948,6 +7948,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 498.3595684888223,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "103115900+rp9-next@users.noreply.github.com",
+            "name": "rp9-next",
+            "username": "rp9-next"
+          },
+          "committer": {
+            "email": "103115900+rp9-next@users.noreply.github.com",
+            "name": "Rahul Pandey",
+            "username": "rp9-next"
+          },
+          "distinct": true,
+          "id": "12c5fe04c06961676abc35b4e9ca5d0bdddd45f7",
+          "message": "ICU-22351 Update PGP key information for release signing",
+          "timestamp": "2023-04-13T08:17:47+05:30",
+          "tree_id": "03d5a685a37890828ed2b9d9e53798abaf3334bd",
+          "url": "https://github.com/unicode-org/icu/commit/12c5fe04c06961676abc35b4e9ca5d0bdddd45f7"
+        },
+        "date": 1681354676721,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 557.3813642404716,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
