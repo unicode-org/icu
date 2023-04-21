2063e4bb6cd1558465ed77bc1236c0f4623e7376
github-action-benchmark <github@users.noreply.github.com>
2023-04-21 02:49:55 +0000

    add Benchmark (ndjson) benchmark result for 5be2ea84e59e63e4d68ebcd4082996d6db3af9f0

--- perf/results/j_decimalformatperf/de_DE/TestICUConstruction/data.js
+++ perf/results/j_decimalformatperf/de_DE/TestICUConstruction/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1682007861810,
+  "lastUpdate": 1682045395840,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -8218,6 +8218,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUConstruction",
             "value": 478.4442493303139,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ftang@chromium.org",
+            "name": "Frank Tang",
+            "username": "FrankYFTang"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "5be2ea84e59e63e4d68ebcd4082996d6db3af9f0",
+          "message": "ICU-22349 Bump the cygwin timeout to 50 minutes",
+          "timestamp": "2023-04-20T19:32:34-07:00",
+          "tree_id": "a4299e363ed4dfa61dd3e105076e6b7566daaae7",
+          "url": "https://github.com/unicode-org/icu/commit/5be2ea84e59e63e4d68ebcd4082996d6db3af9f0"
+        },
+        "date": 1682045360081,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUConstruction",
+            "value": 449.8909472629285,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
