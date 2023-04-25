a524857e51b209d54af61858051ea84ad706c20d
github-action-benchmark <github@users.noreply.github.com>
2023-04-25 11:22:12 +0000

    add Benchmark (ndjson) benchmark result for 85fd7389e0fdd89b5a0aafe07429dd2452f1d310

--- perf/results/j_decimalformatperf/en_US/TestICUParse/data.js
+++ perf/results/j_decimalformatperf/en_US/TestICUParse/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1682374220455,
+  "lastUpdate": 1682421732465,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -8368,6 +8368,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 405.8732416463878,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ftang@chromium.org",
+            "name": "Frank Tang",
+            "username": "FrankYFTang"
+          },
+          "committer": {
+            "email": "103115900+rp9-next@users.noreply.github.com",
+            "name": "Rahul Pandey",
+            "username": "rp9-next"
+          },
+          "distinct": true,
+          "id": "85fd7389e0fdd89b5a0aafe07429dd2452f1d310",
+          "message": "ICU-22349 Speed up pre-merge CI\n\nSee #2420",
+          "timestamp": "2023-04-19T08:06:59+05:30",
+          "tree_id": "d4d1708e8d9e0574d02ea37967030c10232cea0e",
+          "url": "https://github.com/unicode-org/icu/commit/85fd7389e0fdd89b5a0aafe07429dd2452f1d310"
+        },
+        "date": 1682421697788,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 412.25110478281164,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
