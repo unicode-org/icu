64705658d442cfddd59ec3bff4d0f714e38c52b1
Victor Chang <vichang@google.com>
2023-04-25 15:57:58 +0100

    ICU-22371 Fix ULocale.getISOLanguages() API docs

--- icu4c/source/common/unicode/locid.h
+++ icu4c/source/common/unicode/locid.h
@@ -984,7 +984,10 @@ public:
     static const char* const* U_EXPORT2 getISOCountries();
 
     /**
-     * Gets a list of all available language codes defined in ISO 639.  This is a pointer
+     * Returns a list of all unique language codes defined in ISO 639.
+     * They can be 2 or 3 letter codes, as defined by
+     * <a href="https://www.ietf.org/rfc/bcp/bcp47.html#section-2.2.1">
+     * BCP 47, section 2.2.1</a>. This is a pointer
      * to an array of pointers to arrays of char.  All of these pointers are owned
      * by ICU-- do not delete them, and do not write through them.  The array is
      * terminated with a null pointer.
