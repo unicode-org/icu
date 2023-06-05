2b60ae30e57083e42b85612f07c14fcd1b2dbc32
github-action-benchmark <github@users.noreply.github.com>
2023-06-05 03:22:25 +0000

    add Benchmark (ndjson) benchmark result for f4234577ad723ec27c251a5b88733728aad212e8

--- perf/results/j_dateformatperf/en_US/TestICUParse/2/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUParse/2/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1685644838288,
+  "lastUpdate": 1685935345615,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -8938,6 +8938,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 236.2302044222118,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "f4234577ad723ec27c251a5b88733728aad212e8",
+          "message": "ICU-22404 initial changes.txt for Unicode 15.1\n\nSee #2490\r\n- copy 15.0 change log to the top, unchanged\r\n- adjust changes.txt for 15.1, incl. diffs from CLDR 43 root collation update",
+          "timestamp": "2023-06-04T20:16:39-07:00",
+          "tree_id": "f5884e4e3844adf95356942ec886bd2e02dd3df7",
+          "url": "https://github.com/unicode-org/icu/commit/f4234577ad723ec27c251a5b88733728aad212e8"
+        },
+        "date": 1685935306271,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 182.17279305521873,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
