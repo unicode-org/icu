af51707bc0c9a7e1549d395e97b122ee562183f7
github-action-benchmark <github@users.noreply.github.com>
2023-07-10 23:58:54 +0000

    add Benchmark (ndjson) benchmark result for 70d308731ae01a4187fe49f7ae1c8e1ad1570be6

--- perf/results/j_dateformatperf/en_US/TestICUFormat/1/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUFormat/1/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1688653903113,
+  "lastUpdate": 1689033534938,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -9748,6 +9748,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 265.881158321036,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pedberg@unicode.org",
+            "name": "Peter Edberg",
+            "username": "pedberg-icu"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "70d308731ae01a4187fe49f7ae1c8e1ad1570be6",
+          "message": "ICU-22325 BRS 74 front-load update version to 74.0.1",
+          "timestamp": "2023-07-10T16:45:41-07:00",
+          "tree_id": "d8cc5bf0d7548806dcc1445da79c881e06d6c46a",
+          "url": "https://github.com/unicode-org/icu/commit/70d308731ae01a4187fe49f7ae1c8e1ad1570be6"
+        },
+        "date": 1689033488317,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 255.04202150152489,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
