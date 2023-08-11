1c59feafcb2e415d29264b3e002c9e68b12ea2d3
github-action-benchmark <github@users.noreply.github.com>
2023-08-11 00:57:48 +0000

    add Benchmark (ndjson) benchmark result for b70b2d0433d5004f72b5d7590a82879673527ae0

--- perf/results/j_dateformatperf/en_US/TestICUParse/4/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUParse/4/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1691706571409,
+  "lastUpdate": 1691715468547,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -10348,6 +10348,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 922.6187117304821,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "chachoi@blackberry.com",
+            "name": "James Choi",
+            "username": "chachoi"
+          },
+          "committer": {
+            "email": "62772518+richgillam@users.noreply.github.com",
+            "name": "Rich Gillam",
+            "username": "richgillam"
+          },
+          "distinct": true,
+          "id": "b70b2d0433d5004f72b5d7590a82879673527ae0",
+          "message": "ICU-22363 Add support for QNX 7.1\n\nSigned-off-by: James Choi <chachoi@blackberry.com>",
+          "timestamp": "2023-08-10T17:46:01-07:00",
+          "tree_id": "a41aa016f03b75259a403b5549008671b4835d54",
+          "url": "https://github.com/unicode-org/icu/commit/b70b2d0433d5004f72b5d7590a82879673527ae0"
+        },
+        "date": 1691715416492,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 1067.0157818835373,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
