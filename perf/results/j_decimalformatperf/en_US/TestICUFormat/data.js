8d224af60a818fc941872766e58c127863c50f4a
github-action-benchmark <github@users.noreply.github.com>
2023-09-15 16:58:36 +0000

    add Benchmark (ndjson) benchmark result for 832997c57e7ad4a128447d8c90ee338d90a0faf3

--- perf/results/j_decimalformatperf/en_US/TestICUFormat/data.js
+++ perf/results/j_decimalformatperf/en_US/TestICUFormat/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1694713968258,
+  "lastUpdate": 1694797116594,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -11848,6 +11848,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 400.2339890061083,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "distinct": true,
+          "id": "832997c57e7ad4a128447d8c90ee338d90a0faf3",
+          "message": "ICU-22325 Disable tests as workaround for failures following CI changes\n\nSee #2601",
+          "timestamp": "2023-09-15T09:46:23-07:00",
+          "tree_id": "a48e6c696f2019b5ed215dd5f721c75b7c779ff5",
+          "url": "https://github.com/unicode-org/icu/commit/832997c57e7ad4a128447d8c90ee338d90a0faf3"
+        },
+        "date": 1694797059393,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 421.78648312041025,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
