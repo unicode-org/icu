51980033c82ae66caebf384238a91ffdb687767e
Craig <ccornelius@google.com>
2023-09-20 17:22:10 -0700

    ICU-22325 BRS#19 ICU4J 74 Status and Change Report Change FormattedNumber.getNounClass to @stable ICU 72

--- icu4j/main/core/src/main/java/com/ibm/icu/text/DisplayOptions.java
+++ icu4j/main/core/src/main/java/com/ibm/icu/text/DisplayOptions.java
@@ -21,7 +21,7 @@ import java.util.List;
  *                             .build();
  *                             }
  *
- * @draft ICU 72
+ * @stable ICU 72
  */
 public final class DisplayOptions {
     private final GrammaticalCase grammaticalCase;
@@ -46,7 +46,7 @@ public final class DisplayOptions {
      * Creates a builder with the {@code UNDEFINED} value for all the parameters.
      *
      * @return Builder
-     * @draft ICU 72
+     * @stable ICU 72
      */
     public static Builder builder() {
         return new Builder();
@@ -56,7 +56,7 @@ public final class DisplayOptions {
      * Creates a builder with the same parameters from this object.
      *
      * @return Builder
-     * @draft ICU 72
+     * @stable ICU 72
      */
     public Builder copyToBuilder() {
         return new Builder(this);
@@ -66,7 +66,7 @@ public final class DisplayOptions {
      * Gets the grammatical case.
      *
      * @return GrammaticalCase
-     * @draft ICU 72
+     * @stable ICU 72
      */
     public GrammaticalCase getGrammaticalCase() {
         return this.grammaticalCase;
@@ -76,7 +76,7 @@ public final class DisplayOptions {
      * Gets the noun class.
      *
      * @return NounClass
-     * @draft ICU 72
+     * @stable ICU 72
      */
     public NounClass getNounClass() {
         return this.nounClass;
@@ -86,7 +86,7 @@ public final class DisplayOptions {
      * Gets the plural category.
      *
      * @return PluralCategory
-     * @draft ICU 72
+     * @stable ICU 72
      */
     public PluralCategory getPluralCategory() {
         return this.pluralCategory;
@@ -96,7 +96,7 @@ public final class DisplayOptions {
      * Gets the capitalization.
      *
      * @return Capitalization
-     * @draft ICU 72
+     * @stable ICU 72
      */
     public Capitalization getCapitalization() {
         return this.capitalization;
@@ -106,7 +106,7 @@ public final class DisplayOptions {
      * Gets the name style.
      *
      * @return NameStyle
-     * @draft ICU 72
+     * @stable ICU 72
      */
     public NameStyle getNameStyle() {
         return this.nameStyle;
@@ -116,7 +116,7 @@ public final class DisplayOptions {
      * Gets the display length.
      *
      * @return DisplayLength
-     * @draft ICU 72
+     * @stable ICU 72
      */
     public DisplayLength getDisplayLength() {
         return this.displayLength;
@@ -126,7 +126,7 @@ public final class DisplayOptions {
      * Gets the substitute handling.
      *
      * @return SubstituteHandling
-     * @draft ICU 72
+     * @stable ICU 72
      */
     public SubstituteHandling getSubstituteHandling() {
         return this.substituteHandling;
@@ -135,7 +135,7 @@ public final class DisplayOptions {
     /**
      * Responsible for building {@code DisplayOptions}.
      *
-     * @draft ICU 72
+     * @stable ICU 72
      */
     public static class Builder {
         private GrammaticalCase grammaticalCase;
@@ -149,7 +149,7 @@ public final class DisplayOptions {
         /**
          * Creates a {@code DisplayOptions.Builder} with the default values.
          *
-         * @draft ICU 72
+         * @stable ICU 72
          */
         private Builder() {
             this.grammaticalCase = GrammaticalCase.UNDEFINED;
@@ -165,7 +165,7 @@ public final class DisplayOptions {
          * Creates a {@code Builder} with all the information from a {@code DisplayOptions}.
          *
          * @param displayOptions Options to be copied.
-         * @draft ICU 72
+         * @stable ICU 72
          */
         private Builder(DisplayOptions displayOptions) {
             this.grammaticalCase = displayOptions.grammaticalCase;
@@ -182,7 +182,7 @@ public final class DisplayOptions {
          *
          * @param grammaticalCase The grammatical case.
          * @return Builder
-         * @draft ICU 72
+         * @stable ICU 72
          */
         public Builder setGrammaticalCase(GrammaticalCase grammaticalCase) {
             this.grammaticalCase = grammaticalCase;
@@ -194,7 +194,7 @@ public final class DisplayOptions {
          *
          * @param nounClass The noun class.
          * @return Builder
-         * @draft ICU 72
+         * @stable ICU 72
          */
         public Builder setNounClass(NounClass nounClass) {
             this.nounClass = nounClass;
@@ -206,7 +206,7 @@ public final class DisplayOptions {
          *
          * @param pluralCategory The plural category.
          * @return Builder
-         * @draft ICU 72
+         * @stable ICU 72
          */
         public Builder setPluralCategory(PluralCategory pluralCategory) {
             this.pluralCategory = pluralCategory;
@@ -218,7 +218,7 @@ public final class DisplayOptions {
          *
          * @param capitalization The capitalization.
          * @return Builder
-         * @draft ICU 72
+         * @stable ICU 72
          */
         public Builder setCapitalization(Capitalization capitalization) {
             this.capitalization = capitalization;
@@ -230,7 +230,7 @@ public final class DisplayOptions {
          *
          * @param nameStyle The name style.
          * @return Builder
-         * @draft ICU 72
+         * @stable ICU 72
          */
         public Builder setNameStyle(NameStyle nameStyle) {
             this.nameStyle = nameStyle;
@@ -242,7 +242,7 @@ public final class DisplayOptions {
          *
          * @param displayLength The display length.
          * @return Builder
-         * @draft ICU 72
+         * @stable ICU 72
          */
         public Builder setDisplayLength(DisplayLength displayLength) {
             this.displayLength = displayLength;
@@ -254,7 +254,7 @@ public final class DisplayOptions {
          *
          * @param substituteHandling The substitute handling.
          * @return Builder
-         * @draft ICU 72
+         * @stable ICU 72
          */
         public Builder setSubstituteHandling(SubstituteHandling substituteHandling) {
             this.substituteHandling = substituteHandling;
@@ -265,7 +265,7 @@ public final class DisplayOptions {
          * Builds the display options.
          *
          * @return DisplayOptions
-         * @draft ICU 72
+         * @stable ICU 72
          */
         public DisplayOptions build() {
             DisplayOptions displayOptions = new DisplayOptions(this);
@@ -276,46 +276,46 @@ public final class DisplayOptions {
     /**
      * Represents all the grammatical noun classes that are supported by CLDR.
      *
-     * @draft ICU 72
+     * @stable ICU 72
      */
     public enum NounClass {
         /**
          * A possible setting for NounClass. The noun class context to be used is unknown (this is the
          * default value).
          *
-         * @draft ICU 72
+         * @stable ICU 72
          */
         UNDEFINED("undefined"),
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         OTHER("other"),
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         NEUTER("neuter"),
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         FEMININE("feminine"),
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         MASCULINE("masculine"),
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         ANIMATE("animate"),
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         INANIMATE("inanimate"),
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         PERSONAL("personal"),
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         COMMON("common");
 
@@ -328,14 +328,14 @@ public final class DisplayOptions {
         /**
          * Unmodifiable List of all noun classes constants. List version of {@link #values()}.
          *
-         * @draft ICU 72
+         * @stable ICU 72
          */
         public static final List<NounClass> VALUES =
                 Collections.unmodifiableList(Arrays.asList(NounClass.values()));
 
         /**
          * @return the lowercase CLDR keyword string for the noun class.
-         * @draft ICU 72
+         * @stable ICU 72
          */
         public final String getIdentifier() {
             return this.identifier;
@@ -344,7 +344,7 @@ public final class DisplayOptions {
         /**
          * @param identifier in lower case such as "feminine" or "masculine"
          * @return the plural category corresponding to the identifier, or {@code UNDEFINED}
-         * @draft ICU 72
+         * @stable ICU 72
          */
         public static final NounClass fromIdentifier(String identifier) {
             if (identifier == null) {
@@ -364,35 +364,35 @@ public final class DisplayOptions {
     /**
      * Represents all the name styles.
      *
-     * @draft ICU 72
+     * @stable ICU 72
      */
     public enum NameStyle {
         /**
          * A possible setting for NameStyle. The NameStyle context to be used is unknown (this is the
          * default value).
          *
-         * @draft ICU 72
+         * @stable ICU 72
          */
         UNDEFINED,
         /**
          * Use standard names when generating a locale name, e.g. en_GB displays as 'English (United
          * Kingdom)'.
          *
-         * @draft ICU 72
+         * @stable ICU 72
          */
         STANDARD_NAMES,
 
         /**
          * Use dialect names, when generating a locale name, e.g. en_GB displays as 'British English'.
          *
-         * @draft ICU 72
+         * @stable ICU 72
          */
         DIALECT_NAMES;
 
         /**
          * Unmodifiable List of all name styles constants. List version of {@link #values()}.
          *
-         * @draft ICU 72
+         * @stable ICU 72
          */
         public static final List<NameStyle> VALUES =
                 Collections.unmodifiableList(Arrays.asList(NameStyle.values()));
@@ -401,35 +401,35 @@ public final class DisplayOptions {
     /**
      * Represents all the substitute handlings.
      *
-     * @draft ICU 72
+     * @stable ICU 72
      */
     public enum SubstituteHandling {
         /**
          * A possible setting for SubstituteHandling. The SubstituteHandling context to be used is
          * unknown (this is the default value).
          *
-         * @draft ICU 72
+         * @stable ICU 72
          */
         UNDEFINED,
         /**
          * Returns a fallback value (e.g., the input code) when no data is available. This is the
          * default behaviour.
          *
-         * @draft ICU 72
+         * @stable ICU 72
          */
         SUBSTITUTE,
 
         /**
          * Returns a null value when no data is available.
          *
-         * @draft ICU 72
+         * @stable ICU 72
          */
         NO_SUBSTITUTE;
 
         /**
          * Unmodifiable List of all substitute handlings constants. List version of {@link #values()}.
          *
-         * @draft ICU 72
+         * @stable ICU 72
          */
         public static final List<SubstituteHandling> VALUES =
                 Collections.unmodifiableList(Arrays.asList(SubstituteHandling.values()));
@@ -438,34 +438,34 @@ public final class DisplayOptions {
     /**
      * Represents all the display lengths.
      *
-     * @draft ICU 72
+     * @stable ICU 72
      */
     public enum DisplayLength {
         /**
          * A possible setting for DisplayLength. The DisplayLength context to be used is unknown (this
          * is the default value).
          *
-         * @draft ICU 72
+         * @stable ICU 72
          */
         UNDEFINED,
         /**
          * Uses full names when generating a locale name, e.g. "United States" for US.
          *
-         * @draft ICU 72
+         * @stable ICU 72
          */
         LENGTH_FULL,
 
         /**
          * Use short names when generating a locale name, e.g. "U.S." for US.
          *
-         * @draft ICU 72
+         * @stable ICU 72
          */
         LENGTH_SHORT;
 
         /**
          * Unmodifiable List of all display lengths constants. List version of {@link #values()}.
          *
-         * @draft ICU 72
+         * @stable ICU 72
          */
         public static final List<DisplayLength> VALUES =
                 Collections.unmodifiableList(Arrays.asList(DisplayLength.values()));
@@ -474,14 +474,14 @@ public final class DisplayOptions {
     /**
      * Represents all the capitalization options.
      *
-     * @draft ICU 72
+     * @stable ICU 72
      */
     public enum Capitalization {
         /**
          * A possible setting for Capitalization. The capitalization context to be used is unknown (this
          * is the default value).
          *
-         * @draft ICU 72
+         * @stable ICU 72
          */
         UNDEFINED,
 
@@ -489,7 +489,7 @@ public final class DisplayOptions {
          * The capitalization context if a date, date symbol or display name is to be formatted with
          * capitalization appropriate for the beginning of a sentence.
          *
-         * @draft ICU 72
+         * @stable ICU 72
          */
         BEGINNING_OF_SENTENCE,
 
@@ -497,7 +497,7 @@ public final class DisplayOptions {
          * The capitalization context if a date, date symbol or display name is to be formatted with
          * capitalization appropriate for the middle of a sentence.
          *
-         * @draft ICU 72
+         * @stable ICU 72
          */
         MIDDLE_OF_SENTENCE,
 
@@ -506,7 +506,7 @@ public final class DisplayOptions {
          * capitalization appropriate for stand-alone usage such as an isolated name on a calendar
          * page.
          *
-         * @draft ICU 72
+         * @stable ICU 72
          */
         STANDALONE,
 
@@ -514,14 +514,14 @@ public final class DisplayOptions {
          * The capitalization context if a date, date symbol or display name is to be formatted with
          * capitalization appropriate for a user-interface list or menu item.
          *
-         * @draft ICU 72
+         * @stable ICU 72
          */
         UI_LIST_OR_MENU;
 
         /**
          * Unmodifiable List of all the capitalizations constants. List version of {@link #values()}.
          *
-         * @draft ICU 72
+         * @stable ICU 72
          */
         public static final List<Capitalization> VALUES =
                 Collections.unmodifiableList(Arrays.asList(Capitalization.values()));
@@ -530,44 +530,44 @@ public final class DisplayOptions {
     /**
      * Standard CLDR plural category constants. See http://www.unicode.org/reports/tr35/tr35-numbers.html#Language_Plural_Rules
      *
-     * @draft ICU 72
+     * @stable ICU 72
      */
     public enum PluralCategory {
         /**
          * A possible setting for PluralCategory. The plural category context to be used is unknown
          * (this is the default value).
          *
-         * @draft ICU 72
+         * @stable ICU 72
          */
         UNDEFINED("undefined"),
 
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         ZERO("zero"),
 
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         ONE("one"),
 
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         TWO("two"),
 
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         FEW("few"),
 
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         MANY("many"),
 
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         OTHER("other");
 
@@ -580,14 +580,14 @@ public final class DisplayOptions {
         /**
          * Unmodifiable List of all plural categories constants. List version of {@link #values()}.
          *
-         * @draft ICU 72
+         * @stable ICU 72
          */
         public static final List<PluralCategory> VALUES =
                 Collections.unmodifiableList(Arrays.asList(PluralCategory.values()));
 
         /**
          * @return the lowercase CLDR keyword string for the plural category
-         * @draft ICU 72
+         * @stable ICU 72
          */
         public final String getIdentifier() {
             return this.identifier;
@@ -596,7 +596,7 @@ public final class DisplayOptions {
         /**
          * @param identifier in lower case such as "few" or "other"
          * @return the plural category corresponding to the identifier, or {@code UNDEFINED}
-         * @draft ICU 72
+         * @stable ICU 72
          */
         public static final PluralCategory fromIdentifier(String identifier) {
             if (identifier == null) {
@@ -616,84 +616,84 @@ public final class DisplayOptions {
     /**
      * Represents all the grammatical cases that are supported by CLDR.
      *
-     * @draft ICU 72
+     * @stable ICU 72
      */
     public enum GrammaticalCase {
         /**
          * A possible setting for GrammaticalCase. The grammatical case context to be used is unknown
          * (this is the default value).
          *
-         * @draft ICU 72
+         * @stable ICU 72
          */
         UNDEFINED("undefined"),
 
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         ABLATIVE("ablative"),
 
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         ACCUSATIVE("accusative"),
 
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         COMITATIVE("comitative"),
 
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         DATIVE("dative"),
 
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         ERGATIVE("ergative"),
 
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         GENITIVE("genitive"),
 
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         INSTRUMENTAL("instrumental"),
 
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         LOCATIVE("locative"),
 
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         LOCATIVE_COPULATIVE("locative_copulative"),
 
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         NOMINATIVE("nominative"),
 
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         OBLIQUE("oblique"),
 
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         PREPOSITIONAL("prepositional"),
 
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         SOCIATIVE("sociative"),
 
         /**
-         * @draft ICU 72
+         * @stable ICU 72
          */
         VOCATIVE("vocative");
 
@@ -706,14 +706,14 @@ public final class DisplayOptions {
         /**
          * Unmodifiable List of all grammatical cases constants. List version of {@link #values()}.
          *
-         * @draft ICU 72
+         * @stable ICU 72
          */
         public static final List<GrammaticalCase> VALUES =
                 Collections.unmodifiableList(Arrays.asList(GrammaticalCase.values()));
 
         /**
          * @return the lowercase CLDR keyword string for the grammatical case.
-         * @draft ICU 72
+         * @stable ICU 72
          */
         public final String getIdentifier() {
             return this.identifier;
@@ -722,7 +722,7 @@ public final class DisplayOptions {
         /**
          * @param identifier in lower case such as "dative" or "nominative"
          * @return the plural category corresponding to the identifier, or {@code UNDEFINED}
-         * @draft ICU 72
+         * @stable ICU 72
          */
         public static final GrammaticalCase fromIdentifier(String identifier) {
             if (identifier == null) {
