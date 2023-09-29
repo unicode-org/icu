9f183c9cf9175bf910798d41a12a41d0d9c2eb73
github-action-benchmark <github@users.noreply.github.com>
2023-09-29 00:46:30 +0000

    add Benchmark (ndjson) benchmark result for 76b9e089be35dbcbd374111ad9eadccd0d8907bb

--- perf/results/j_dateformatperf/sw_KE/TestICUParse/1/data.js
+++ perf/results/j_dateformatperf/sw_KE/TestICUParse/1/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1695936979882,
+  "lastUpdate": 1695948390320,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -13138,6 +13138,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 517.8575074621126,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mnita@google.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "42151464+pedberg-icu@users.noreply.github.com",
+            "name": "Peter Edberg",
+            "username": "pedberg-icu"
+          },
+          "distinct": true,
+          "id": "76b9e089be35dbcbd374111ad9eadccd0d8907bb",
+          "message": "ICU-22324 Mavenization, publish the root pom to Maven",
+          "timestamp": "2023-09-28T17:32:30-07:00",
+          "tree_id": "589208cb6a7823818d2f3a1816b1242246266144",
+          "url": "https://github.com/unicode-org/icu/commit/76b9e089be35dbcbd374111ad9eadccd0d8907bb"
+        },
+        "date": 1695948301567,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 640.9928907712862,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
