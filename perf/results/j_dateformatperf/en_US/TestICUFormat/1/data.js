94f669b5589f08e1265c850fad53e39e2e5feb2c
github-action-benchmark <github@users.noreply.github.com>
2023-10-31 22:28:05 +0000

    add Benchmark (ndjson) benchmark result for 1bb711ad20427182566d769471fe0cda951fe297

--- perf/results/j_dateformatperf/en_US/TestICUFormat/1/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUFormat/1/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1698721468064,
+  "lastUpdate": 1698791285233,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -14038,6 +14038,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 267.8126515725057,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ftang@chromium.org",
+            "name": "Frank Tang",
+            "username": "FrankYFTang"
+          },
+          "committer": {
+            "email": "ftang@google.com",
+            "name": "Frank Yung-Fong Tang",
+            "username": "FrankYFTang"
+          },
+          "distinct": true,
+          "id": "1bb711ad20427182566d769471fe0cda951fe297",
+          "message": "ICU-22540 Add new CLDR units ronto, ronna, quecto, and quetta",
+          "timestamp": "2023-10-31T14:58:52-07:00",
+          "tree_id": "d5cd4dce7a9d55b7f5df27fdb641e44663eeb9d3",
+          "url": "https://github.com/unicode-org/icu/commit/1bb711ad20427182566d769471fe0cda951fe297"
+        },
+        "date": 1698791199520,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 346.48838577479563,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
