853a85b38ea242ca14b765a82b6598de3237113b
github-action-benchmark <github@users.noreply.github.com>
2023-11-10 23:44:33 +0000

    add Benchmark (ndjson) benchmark result for 511e5efe562667dfe3f57c8d4cd8eb3af5b2137b

--- perf/results/j_decimalformatperf/de_DE/TestICUParse/data.js
+++ perf/results/j_decimalformatperf/de_DE/TestICUParse/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1699575632016,
+  "lastUpdate": 1699659873402,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -14038,6 +14038,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 459.58951999111827,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "distinct": true,
+          "id": "511e5efe562667dfe3f57c8d4cd8eb3af5b2137b",
+          "message": "ICU-22533 Update BRS instructions for tagging release",
+          "timestamp": "2023-11-10T15:33:31-08:00",
+          "tree_id": "3fc88a64ac768951fe26db0482c36fc94d1f049a",
+          "url": "https://github.com/unicode-org/icu/commit/511e5efe562667dfe3f57c8d4cd8eb3af5b2137b"
+        },
+        "date": 1699659822220,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 275.2419572331153,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
