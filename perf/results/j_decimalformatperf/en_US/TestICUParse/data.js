1b36b4c5706756d3582879c764a4aa164794adc2
github-action-benchmark <github@users.noreply.github.com>
2023-11-16 23:05:39 +0000

    add Benchmark (ndjson) benchmark result for 102ed8b6bd5a80b556349f62275c4edb60fd100b

--- perf/results/j_decimalformatperf/en_US/TestICUParse/data.js
+++ perf/results/j_decimalformatperf/en_US/TestICUParse/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1699659918550,
+  "lastUpdate": 1700175939244,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -14278,6 +14278,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 436.82499831265636,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ftang@chromium.org",
+            "name": "Frank Tang",
+            "username": "FrankYFTang"
+          },
+          "committer": {
+            "email": "ftang@google.com",
+            "name": "Frank Yung-Fong Tang",
+            "username": "FrankYFTang"
+          },
+          "distinct": true,
+          "id": "102ed8b6bd5a80b556349f62275c4edb60fd100b",
+          "message": "ICU-22563 Limit the size for calendar fuzzer\n\nLimit to 1000 bytes of valid test data so the fuzzer will\nnot timeout because of running many operations.\n\nICU-22563 fix comment",
+          "timestamp": "2023-11-16T14:55:00-08:00",
+          "tree_id": "1b8dacfb6e0a1158df30ccdd82822783f277bf56",
+          "url": "https://github.com/unicode-org/icu/commit/102ed8b6bd5a80b556349f62275c4edb60fd100b"
+        },
+        "date": 1700175885680,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 330.6541421899765,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
