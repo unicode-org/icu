363e5ed3af2d22258b5d5e9f2206cd5c0a911040
github-action-benchmark <github@users.noreply.github.com>
2023-11-29 22:57:02 +0000

    add Benchmark (ndjson) benchmark result for 0bfa5f4c44ef3344982a23c1e1aa1bd6af6f8427

--- perf/results/j_normperf/TestNames_Chinese/TestICU_NFD_NFC_Text/data.js
+++ perf/results/j_normperf/TestNames_Chinese/TestICU_NFD_NFC_Text/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1701298540680,
+  "lastUpdate": 1701298622663,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -28648,6 +28648,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFD_NFC_Text",
             "value": 2.9387704494501605,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ftang@chromium.org",
+            "name": "Frank Tang",
+            "username": "FrankYFTang"
+          },
+          "committer": {
+            "email": "ftang@google.com",
+            "name": "Frank Yung-Fong Tang",
+            "username": "FrankYFTang"
+          },
+          "distinct": true,
+          "id": "0bfa5f4c44ef3344982a23c1e1aa1bd6af6f8427",
+          "message": "ICU-22549 Add DateTimePatternGenerator fuzzer\n\nSee #2708",
+          "timestamp": "2023-11-29T14:45:21-08:00",
+          "tree_id": "32717b9001f415957dd8723f0681946ad8568594",
+          "url": "https://github.com/unicode-org/icu/commit/0bfa5f4c44ef3344982a23c1e1aa1bd6af6f8427"
+        },
+        "date": 1701298566151,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFD_NFC_Text",
+            "value": 3.0714538380957603,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
