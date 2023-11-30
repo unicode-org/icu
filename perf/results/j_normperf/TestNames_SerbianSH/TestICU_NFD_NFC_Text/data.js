ec83aba0ac0ec5e22a89be25bff734bdad6c5de6
github-action-benchmark <github@users.noreply.github.com>
2023-11-30 16:43:45 +0000

    add Benchmark (ndjson) benchmark result for 368d67316c8413b7c8539e050acdbf8e64daf9e3

--- perf/results/j_normperf/TestNames_SerbianSH/TestICU_NFD_NFC_Text/data.js
+++ perf/results/j_normperf/TestNames_SerbianSH/TestICU_NFD_NFC_Text/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1701355561531,
+  "lastUpdate": 1701362625610,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -28708,6 +28708,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFD_NFC_Text",
             "value": 9.27336008625008,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ftang@chromium.org",
+            "name": "Frank Tang",
+            "username": "FrankYFTang"
+          },
+          "committer": {
+            "email": "ftang@google.com",
+            "name": "Frank Yung-Fong Tang",
+            "username": "FrankYFTang"
+          },
+          "distinct": true,
+          "id": "368d67316c8413b7c8539e050acdbf8e64daf9e3",
+          "message": "ICU-22549 Add UnicodeSet fuzzer",
+          "timestamp": "2023-11-30T08:32:09-08:00",
+          "tree_id": "18cbd3c57a2579aab308f879f9dfd0dc99e33a1e",
+          "url": "https://github.com/unicode-org/icu/commit/368d67316c8413b7c8539e050acdbf8e64daf9e3"
+        },
+        "date": 1701362569265,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFD_NFC_Text",
+            "value": 9.114092486188175,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
