1fbf28df2ab8727248719c950e9847353cbab2bb
github-action-benchmark <github@users.noreply.github.com>
2023-12-01 23:19:50 +0000

    add Benchmark (ndjson) benchmark result for e8e19454dac630d874fa980cfa8de8495c7bd268

--- perf/results/j_dateformatperf/en_US/TestICUFormat/3/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUFormat/3/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1701362525384,
+  "lastUpdate": 1701472790038,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -14248,6 +14248,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 233.43201048582694,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ftang@chromium.org",
+            "name": "Frank Tang",
+            "username": "FrankYFTang"
+          },
+          "committer": {
+            "email": "ftang@google.com",
+            "name": "Frank Yung-Fong Tang",
+            "username": "FrankYFTang"
+          },
+          "distinct": true,
+          "id": "e8e19454dac630d874fa980cfa8de8495c7bd268",
+          "message": "ICU-22549 Add fuzzer for NumberFormatter",
+          "timestamp": "2023-12-01T15:04:22-08:00",
+          "tree_id": "d75c8643351c5109f01dc32a19ce96afb6642360",
+          "url": "https://github.com/unicode-org/icu/commit/e8e19454dac630d874fa980cfa8de8495c7bd268"
+        },
+        "date": 1701472732090,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 247.6822842312982,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
