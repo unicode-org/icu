46f179f9b0a3a9a13499c601e0ecce66335cfd1d
github-action-benchmark <github@users.noreply.github.com>
2023-12-12 22:50:49 +0000

    add Benchmark (ndjson) benchmark result for 4a7d61d2617b2c165d2a58841d99762757f70f75

--- perf/results/j_dateformatperf/en_US/TestICUParse/3/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUParse/3/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1702338194698,
+  "lastUpdate": 1702421449893,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -14878,6 +14878,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 882.8506330667819,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ftang@chromium.org",
+            "name": "Frank Tang",
+            "username": "FrankYFTang"
+          },
+          "committer": {
+            "email": "ftang@google.com",
+            "name": "Frank Yung-Fong Tang",
+            "username": "FrankYFTang"
+          },
+          "distinct": true,
+          "id": "4a7d61d2617b2c165d2a58841d99762757f70f75",
+          "message": "ICU-22579 Fix Null deref while Unicode Set only has string",
+          "timestamp": "2023-12-12T14:39:12-08:00",
+          "tree_id": "cebbbfc602168758597444336d5838e860d95fbe",
+          "url": "https://github.com/unicode-org/icu/commit/4a7d61d2617b2c165d2a58841d99762757f70f75"
+        },
+        "date": 1702421371382,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 857.4278106967114,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
