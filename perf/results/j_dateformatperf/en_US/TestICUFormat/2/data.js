1908fb5c520417a88e79b76856b3d79980ce3fb2
github-action-benchmark <github@users.noreply.github.com>
2023-12-14 20:01:44 +0000

    add Benchmark (ndjson) benchmark result for 6f2e37ecb5db4c511473b38f5309d7fcd048ac51

--- perf/results/j_dateformatperf/en_US/TestICUFormat/2/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUFormat/2/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1702581793610,
+  "lastUpdate": 1702584104602,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -14848,6 +14848,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 195.5006821709264,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "roubert@google.com",
+            "name": "Fredrik Roubert",
+            "username": "roubert"
+          },
+          "committer": {
+            "email": "fredrik@roubert.name",
+            "name": "Fredrik Roubert",
+            "username": "roubert"
+          },
+          "distinct": true,
+          "id": "6f2e37ecb5db4c511473b38f5309d7fcd048ac51",
+          "message": "ICU-22590 Delete useless source file.",
+          "timestamp": "2023-12-14T20:41:44+01:00",
+          "tree_id": "a08c0b43d9f46a32a2e41895b7e7dc10e5ac73c6",
+          "url": "https://github.com/unicode-org/icu/commit/6f2e37ecb5db4c511473b38f5309d7fcd048ac51"
+        },
+        "date": 1702584044922,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 202.20236348646458,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
