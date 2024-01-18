a3bd6b52fc3b23eae82066003bb0c572bb362445
github-action-benchmark <github@users.noreply.github.com>
2024-01-18 23:37:42 +0000

    add Benchmark (ndjson) benchmark result for c5160765d40cf18751f4b3407e1cc7e8800c1cbe

--- perf/results/j_dateformatperf/en_US/TestICUParse/1/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUParse/1/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1705620157841,
+  "lastUpdate": 1705621062118,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -16258,6 +16258,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 370.2943892443905,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "roubert@google.com",
+            "name": "Fredrik Roubert",
+            "username": "roubert"
+          },
+          "committer": {
+            "email": "fredrik@roubert.name",
+            "name": "Fredrik Roubert",
+            "username": "roubert"
+          },
+          "distinct": true,
+          "id": "c5160765d40cf18751f4b3407e1cc7e8800c1cbe",
+          "message": "ICU-22639 Clone the calendar so we don't mess with the real one.",
+          "timestamp": "2024-01-19T08:07:22+09:00",
+          "tree_id": "392cd07c74d4cd17a45694c2bd1cf12de9aeeff5",
+          "url": "https://github.com/unicode-org/icu/commit/c5160765d40cf18751f4b3407e1cc7e8800c1cbe"
+        },
+        "date": 1705620993682,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 373.870933772322,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
