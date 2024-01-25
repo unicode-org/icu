edf0c520fb9205d9308b33322012d14810300642
github-action-benchmark <github@users.noreply.github.com>
2024-01-25 20:23:55 +0000

    add Benchmark (ndjson) benchmark result for 8f80c62aa2a3307d0d6248fbb2116851edcdcce4

--- perf/results/j_dateformatperf/en_US/TestICUParse/5/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUParse/5/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1706047766341,
+  "lastUpdate": 1706214235150,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -16498,6 +16498,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 333.628986937078,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ftang@chromium.org",
+            "name": "Frank Tang",
+            "username": "FrankYFTang"
+          },
+          "committer": {
+            "email": "ftang@google.com",
+            "name": "Frank Yung-Fong Tang",
+            "username": "FrankYFTang"
+          },
+          "distinct": true,
+          "id": "8f80c62aa2a3307d0d6248fbb2116851edcdcce4",
+          "message": "ICU-22638 Fix cast overflow issue",
+          "timestamp": "2024-01-25T12:11:56-08:00",
+          "tree_id": "492c598550f87b2b9b0f1f5eb9b3a758be6cb673",
+          "url": "https://github.com/unicode-org/icu/commit/8f80c62aa2a3307d0d6248fbb2116851edcdcce4"
+        },
+        "date": 1706214162259,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 329.2788386268376,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
