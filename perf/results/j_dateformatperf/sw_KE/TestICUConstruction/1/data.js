3750267fc3b31205362dc4ccb53e823a2b5d8f31
github-action-benchmark <github@users.noreply.github.com>
2024-01-30 11:17:56 +0000

    add Benchmark (ndjson) benchmark result for ae9cc8cbd1f0f70d429ceb1c5c369d5481829661

--- perf/results/j_dateformatperf/sw_KE/TestICUConstruction/1/data.js
+++ perf/results/j_dateformatperf/sw_KE/TestICUConstruction/1/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1706558944772,
+  "lastUpdate": 1706613476267,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -16528,6 +16528,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUConstruction",
             "value": 3108.437596408164,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "roubert@google.com",
+            "name": "Fredrik Roubert",
+            "username": "roubert"
+          },
+          "committer": {
+            "email": "fredrik@roubert.name",
+            "name": "Fredrik Roubert",
+            "username": "roubert"
+          },
+          "distinct": true,
+          "id": "ae9cc8cbd1f0f70d429ceb1c5c369d5481829661",
+          "message": "ICU-22520 Replace char arrays with icu::CharString.",
+          "timestamp": "2024-01-30T12:04:53+01:00",
+          "tree_id": "0ece6fb5f0d2ea395bfe0105bb8acbb92608419d",
+          "url": "https://github.com/unicode-org/icu/commit/ae9cc8cbd1f0f70d429ceb1c5c369d5481829661"
+        },
+        "date": 1706613390240,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUConstruction",
+            "value": 3098.0378171515144,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
