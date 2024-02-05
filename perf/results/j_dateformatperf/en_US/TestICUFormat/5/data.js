91380011f52026e6fd1b42cb88c8de3fe61a326f
github-action-benchmark <github@users.noreply.github.com>
2024-02-05 13:27:30 +0000

    add Benchmark (ndjson) benchmark result for 6fa113eaa8e73088399995ac79fdc332a7499c1c

--- perf/results/j_dateformatperf/en_US/TestICUFormat/5/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUFormat/5/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1706899063569,
+  "lastUpdate": 1707139650314,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -16948,6 +16948,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 258.37598375957504,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "roubert@google.com",
+            "name": "Fredrik Roubert",
+            "username": "roubert"
+          },
+          "committer": {
+            "email": "fredrik@roubert.name",
+            "name": "Fredrik Roubert",
+            "username": "roubert"
+          },
+          "distinct": true,
+          "id": "6fa113eaa8e73088399995ac79fdc332a7499c1c",
+          "message": "ICU-22651 Refactor U_DEFINE_LOCAL_OPEN_POINTER into a template.",
+          "timestamp": "2024-02-05T14:15:15+01:00",
+          "tree_id": "d629c9f9201fe85e1c9a2514b5ce1db420b7841d",
+          "url": "https://github.com/unicode-org/icu/commit/6fa113eaa8e73088399995ac79fdc332a7499c1c"
+        },
+        "date": 1707139536421,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 246.2439035341262,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
