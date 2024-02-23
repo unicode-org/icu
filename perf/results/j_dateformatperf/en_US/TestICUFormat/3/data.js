71ab6da08a517019a3e1297a31d9d548f0de9823
github-action-benchmark <github@users.noreply.github.com>
2024-02-23 22:49:07 +0000

    add Benchmark (ndjson) benchmark result for fd5d6c97b1d0cff4a07db3c7e7ab04b20099e124

--- perf/results/j_dateformatperf/en_US/TestICUFormat/3/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUFormat/3/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1708630106324,
+  "lastUpdate": 1708728547123,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -17368,6 +17368,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 235.4583284415398,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "distinct": true,
+          "id": "fd5d6c97b1d0cff4a07db3c7e7ab04b20099e124",
+          "message": "ICU-22532 Improve commit checker instructions\n\nSee #2622",
+          "timestamp": "2024-02-23T16:38:14-06:00",
+          "tree_id": "60a62e4a47c60e625c4950bc7593f7307213164f",
+          "url": "https://github.com/unicode-org/icu/commit/fd5d6c97b1d0cff4a07db3c7e7ab04b20099e124"
+        },
+        "date": 1708728435878,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 232.7161344018876,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
