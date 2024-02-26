f237e15c654a0c8fba635f8d71927d8b6122e031
github-action-benchmark <github@users.noreply.github.com>
2024-02-26 20:41:49 +0000

    add Benchmark (ndjson) benchmark result for ea1c6da07fa345dd485408caee51703bc95c0454

--- perf/results/j_dateformatperf/en_US/TestICUParse/4/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUParse/4/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1708967248339,
+  "lastUpdate": 1708980109110,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -17518,6 +17518,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 877.1254407367434,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "srl295@gmail.com",
+            "name": "Steven R. Loomis",
+            "username": "srl295"
+          },
+          "committer": {
+            "email": "srl295@gmail.com",
+            "name": "Steven R. Loomis",
+            "username": "srl295"
+          },
+          "distinct": true,
+          "id": "ea1c6da07fa345dd485408caee51703bc95c0454",
+          "message": "ICU-22677 update LICENSE and README.md and pom.xml\n\n- https://github.com/unicode-org/.github/issues/15\n- use Unicode-3.0 in pom instead of raw license link\n- https://github.com/unicode-org/.github/issues/15",
+          "timestamp": "2024-02-26T14:34:59-06:00",
+          "tree_id": "f428ed2e6edb51b10fcbfb60669bba6fa40e4b79",
+          "url": "https://github.com/unicode-org/icu/commit/ea1c6da07fa345dd485408caee51703bc95c0454"
+        },
+        "date": 1708980019288,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 841.841713947779,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
