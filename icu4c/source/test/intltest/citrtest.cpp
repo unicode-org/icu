3c82e6857c0bc0bbe93c0cdeaf03be3844dcad96
Rahul Pandey <103115900+rp9-next@users.noreply.github.com>
2024-02-26 16:48:37 +0530

    ICU-22676 Undefine move32 since it is interpreted system call with MSVC ARM64

--- icu4c/source/test/intltest/citrtest.cpp
+++ icu4c/source/test/intltest/citrtest.cpp
@@ -101,6 +101,11 @@ public:
 
         return pos;
     }
+
+#ifdef move32
+    // One of the system headers right now is sometimes defining a conflicting macro we don't use
+#undef move32
+#endif
     virtual int32_t move32(int32_t delta, CharacterIterator::EOrigin origin) override {
         switch(origin) {
         case kStart:
