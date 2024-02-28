b8e37b59212d1314e8d4ee8e4ac50ceff8126d44
github-action-benchmark <github@users.noreply.github.com>
2024-02-28 18:05:20 +0000

    add Benchmark (ndjson) benchmark result for 0d7cedc0dd7cbe595ba415bbad226262a5358a3d

--- perf/results/j_converterperf/s-chinese/EUC-CN/TestCharsetEncoderICU/data.js
+++ perf/results/j_converterperf/s-chinese/EUC-CN/TestCharsetEncoderICU/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1709139463460,
+  "lastUpdate": 1709143519986,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -18028,6 +18028,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestCharsetEncoderICU",
             "value": 6.309755728709569,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "distinct": true,
+          "id": "0d7cedc0dd7cbe595ba415bbad226262a5358a3d",
+          "message": "ICU-22532 Trigger CI workflows when workflow definitions change",
+          "timestamp": "2024-02-28T12:50:43-05:00",
+          "tree_id": "fb2b0a51f868dc33626949c35cb4fd4a7f59aaaa",
+          "url": "https://github.com/unicode-org/icu/commit/0d7cedc0dd7cbe595ba415bbad226262a5358a3d"
+        },
+        "date": 1709143436622,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestCharsetEncoderICU",
+            "value": 6.302828715357662,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
