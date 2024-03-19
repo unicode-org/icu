9c1527a40e59355402bf44ac149b8003eada6636
github-action-benchmark <github@users.noreply.github.com>
2024-03-19 23:42:36 +0000

    add Benchmark (ndjson) benchmark result for 6b67715a94be204218338bc484e66ee2169ee95b

--- perf/results/j_normperf/TestNames_SerbianSH/TestICU_NFD_NFD_Text/data.js
+++ perf/results/j_normperf/TestNames_SerbianSH/TestICU_NFD_NFD_Text/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1710860868432,
+  "lastUpdate": 1710891756075,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -19648,6 +19648,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFD_NFD_Text",
             "value": 3.328432699098854,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ftang@chromium.org",
+            "name": "Frank Tang",
+            "username": "FrankYFTang"
+          },
+          "committer": {
+            "email": "ftang@google.com",
+            "name": "Frank Yung-Fong Tang",
+            "username": "FrankYFTang"
+          },
+          "distinct": true,
+          "id": "6b67715a94be204218338bc484e66ee2169ee95b",
+          "message": "ICU-22633 Fix hang on large negative day in hebrew calendar\n\nCheck error status and return error early in the loop",
+          "timestamp": "2024-03-19T16:32:06-07:00",
+          "tree_id": "caec82bb8621a8138f7041e11dc2d0e54e730684",
+          "url": "https://github.com/unicode-org/icu/commit/6b67715a94be204218338bc484e66ee2169ee95b"
+        },
+        "date": 1710891648058,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFD_NFD_Text",
+            "value": 2.9322969366964475,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
