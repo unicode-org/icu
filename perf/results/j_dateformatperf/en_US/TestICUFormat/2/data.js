e35d3986e509aee70a34e0f99be5ad38adae92d5
github-action-benchmark <github@users.noreply.github.com>
2024-03-21 22:29:11 +0000

    add Benchmark (ndjson) benchmark result for 37bdffb24041e78c23f36d315bf3f527fe7a6e4e

--- perf/results/j_dateformatperf/en_US/TestICUFormat/2/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUFormat/2/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711046669148,
+  "lastUpdate": 1711060151506,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -19768,6 +19768,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 190.18113820942594,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ftang@chromium.org",
+            "name": "Frank Tang",
+            "username": "FrankYFTang"
+          },
+          "committer": {
+            "email": "ftang@google.com",
+            "name": "Frank Yung-Fong Tang",
+            "username": "FrankYFTang"
+          },
+          "distinct": true,
+          "id": "37bdffb24041e78c23f36d315bf3f527fe7a6e4e",
+          "message": "ICU-22679 Fix broken header test\n\nSee #2918",
+          "timestamp": "2024-03-21T15:12:10-07:00",
+          "tree_id": "c9be8a174ddb754bce6cd8d117edeeca9bf7146f",
+          "url": "https://github.com/unicode-org/icu/commit/37bdffb24041e78c23f36d315bf3f527fe7a6e4e"
+        },
+        "date": 1711060053923,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 191.79077110565603,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
