cc5f85b91f3ab1f9773b203dd8a9d2db11e97196
github-action-benchmark <github@users.noreply.github.com>
2024-03-27 14:17:27 +0000

    add Benchmark (ndjson) benchmark result for d4bc9bef26e57da2a609ab8c1ed19b6e457cc80a

--- perf/results/j_dateformatperf/en_US/TestICUConstruction/5/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUConstruction/5/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711524146220,
+  "lastUpdate": 1711549047208,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -20188,6 +20188,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUConstruction",
             "value": 1985.5508504224272,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "richard_gillam@apple.com",
+            "name": "Rich Gillam",
+            "username": "richgillam"
+          },
+          "committer": {
+            "email": "62772518+richgillam@users.noreply.github.com",
+            "name": "Rich Gillam",
+            "username": "richgillam"
+          },
+          "distinct": true,
+          "id": "d4bc9bef26e57da2a609ab8c1ed19b6e457cc80a",
+          "message": "ICU-22611 Fixed failures in EnvTest.",
+          "timestamp": "2024-03-26T16:59:48-07:00",
+          "tree_id": "e67644c5c11a22f64b73a3bea830b054eba7533c",
+          "url": "https://github.com/unicode-org/icu/commit/d4bc9bef26e57da2a609ab8c1ed19b6e457cc80a"
+        },
+        "date": 1711548944624,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUConstruction",
+            "value": 1955.9836747235813,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
