3d8bdbbf6be4658b1c65466a3fc75f67a2666162
github-action-benchmark <github@users.noreply.github.com>
2024-03-28 18:41:35 +0000

    add Benchmark (ndjson) benchmark result for d83d26cc5db4ae190c3d015b83b8f377cbb0de9c

--- perf/results/ustrperf/TestNames_Korean/data.js
+++ perf/results/ustrperf/TestNames_Korean/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711649971368,
+  "lastUpdate": 1711651295375,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -74302,6 +74302,102 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestScan2",
             "value": 38.8056,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ftang@chromium.org",
+            "name": "Frank Tang",
+            "username": "FrankYFTang"
+          },
+          "committer": {
+            "email": "ftang@google.com",
+            "name": "Frank Yung-Fong Tang",
+            "username": "FrankYFTang"
+          },
+          "distinct": true,
+          "id": "d83d26cc5db4ae190c3d015b83b8f377cbb0de9c",
+          "message": "ICU-9972 Fix Chinese/Dangi Calendar getActualMaximum(UCAL_DAY_OF_YEAR)\n\nAlso fix ICU-12620 which is mark duplicate of ICU-9972 just now.\nand fix ICU-22258.\n\nSeparate the new year and winter solstice cache since the calculated\nvalue for these two calendar are mostly but not always the same due\nto slightly different observation timeZone.\n\nRemove the epochYear and zoneAstroCalc from the member data\nand instead return them from a getStting() method with the two caches\nsince all four of them are constant per subclass of ChineseCalendar\nand do not need to be different per object.\n\nThe known issues in the TestLimit is caused by both Calendar get/put the\nvalue from the same cache while the calculated result depends on the\ntimeZone zoneAstroCalc.",
+          "timestamp": "2024-03-28T11:02:21-07:00",
+          "tree_id": "c838cfdfac714928118b8f24bd4dbfc7b8f0fc0f",
+          "url": "https://github.com/unicode-org/icu/commit/d83d26cc5db4ae190c3d015b83b8f377cbb0de9c"
+        },
+        "date": 1711651190871,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestCtor",
+            "value": 19.2851,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestCtor1",
+            "value": 20.5374,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestCtor2",
+            "value": 23.9328,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestCtor3",
+            "value": 28.5151,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestAssign",
+            "value": 37.0481,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestAssign1",
+            "value": 31.9162,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestAssign2",
+            "value": 27.6547,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestGetch",
+            "value": 16.7368,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestCatenate",
+            "value": 91.2054,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestScan",
+            "value": 20.8389,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestScan1",
+            "value": 37.4732,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestScan2",
+            "value": 38.5527,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
