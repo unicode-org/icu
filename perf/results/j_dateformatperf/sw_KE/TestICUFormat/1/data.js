c38e406d680aff7d0e1ec349ad76330112f58b87
github-action-benchmark <github@users.noreply.github.com>
2024-04-22 22:44:09 +0000

    add Benchmark (ndjson) benchmark result for 0312308566b5a232102d1bce41070b0498cd8579

--- perf/results/j_dateformatperf/sw_KE/TestICUFormat/1/data.js
+++ perf/results/j_dateformatperf/sw_KE/TestICUFormat/1/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1713817021210,
+  "lastUpdate": 1713825849072,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -20848,6 +20848,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 239.1863630160607,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "distinct": true,
+          "id": "0312308566b5a232102d1bce41070b0498cd8579",
+          "message": "ICU-22721 Prevent inconsistent ICU4J Maven deploys via CI",
+          "timestamp": "2024-04-22T15:28:56-07:00",
+          "tree_id": "05e4284185557af4393ea84306911ca7e8365b49",
+          "url": "https://github.com/unicode-org/icu/commit/0312308566b5a232102d1bce41070b0498cd8579"
+        },
+        "date": 1713825739601,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 238.57953148383945,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
