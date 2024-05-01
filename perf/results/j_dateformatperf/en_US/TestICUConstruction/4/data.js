9135a82635c12a8bcd1ebed72fefdf87b8a0c7e9
github-action-benchmark <github@users.noreply.github.com>
2024-05-01 02:15:18 +0000

    add Benchmark (ndjson) benchmark result for 6041c249f4612ee556d56ae4cd056450258e460e

--- perf/results/j_dateformatperf/en_US/TestICUConstruction/4/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUConstruction/4/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1714435951690,
+  "lastUpdate": 1714529718596,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -21268,6 +21268,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUConstruction",
             "value": 1935.443795300664,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mnita@google.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "distinct": true,
+          "id": "6041c249f4612ee556d56ae4cd056450258e460e",
+          "message": "ICU-21757 Remove ICU dependencies on CollectionUtilities",
+          "timestamp": "2024-04-30T19:10:06-07:00",
+          "tree_id": "6c86af21b977184d7338072417d88f025f19c3ea",
+          "url": "https://github.com/unicode-org/icu/commit/6041c249f4612ee556d56ae4cd056450258e460e"
+        },
+        "date": 1714529611311,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUConstruction",
+            "value": 1972.1880358887302,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
