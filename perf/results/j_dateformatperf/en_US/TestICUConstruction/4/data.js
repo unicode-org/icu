e933f7f1d2cad09ab78df6cbdaeec10c08306d78
github-action-benchmark <github@users.noreply.github.com>
2024-05-06 18:28:39 +0000

    add Benchmark (ndjson) benchmark result for c92c188caccabe6a77a8bb949f46809f00d8aea2

--- perf/results/j_dateformatperf/en_US/TestICUConstruction/4/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUConstruction/4/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1715009850153,
+  "lastUpdate": 1715020119584,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -21508,6 +21508,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUConstruction",
             "value": 1975.275179372847,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mnita@google.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "distinct": true,
+          "id": "c92c188caccabe6a77a8bb949f46809f00d8aea2",
+          "message": "ICU-21757 Replace UOption with commons-cli in XLIFF2ICUConverter",
+          "timestamp": "2024-05-06T11:22:13-07:00",
+          "tree_id": "b738b17bed644341286453ef4e6aa19a1be54dfc",
+          "url": "https://github.com/unicode-org/icu/commit/c92c188caccabe6a77a8bb949f46809f00d8aea2"
+        },
+        "date": 1715020001672,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUConstruction",
+            "value": 2008.439030426058,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
