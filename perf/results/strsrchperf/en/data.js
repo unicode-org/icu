3217c5ff043ec02e57f215ec14c2ca670eaa9cf6
github-action-benchmark <github@users.noreply.github.com>
2024-05-13 22:06:38 +0000

    add Benchmark (ndjson) benchmark result for 6d5555a739179b5d177e73db7c111c5ef1cac22d

--- perf/results/strsrchperf/en/data.js
+++ perf/results/strsrchperf/en/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1715627603389,
+  "lastUpdate": 1715637998321,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -26782,6 +26782,42 @@ window.BENCHMARK_DATA = {
           {
             "name": "Test_ICU_Backward_Search",
             "value": 47.8324,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tjc@igalia.com",
+            "name": "Tim Chevalier",
+            "username": "catamorphism"
+          },
+          "committer": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "distinct": true,
+          "id": "6d5555a739179b5d177e73db7c111c5ef1cac22d",
+          "message": "ICU-22746 Import ICU4J tests\n\nIncludes code fixes for `numberingSystem`, `percent`,\nand `precision` options in `:number`\n\nAlso includes a code fix for number selection:\n  Refactor code to conform more closely to the steps in the spec,\n  and call the number formatter before the selector so that a FormattedNumber\n  with the right options is selected on\n\nSome modifications were needed to add missing params\nand to mark some tests as ignored (see ICU-22754).\nAlso added decimal arguments in JSON test reader\n\nFinally, some redundant tests are removed:\nall tests in messageformat2test_features and\nmessageformat2test_icu, and\nmessageformat2test_builtin are now covered by JSON tests",
+          "timestamp": "2024-05-13T14:51:14-07:00",
+          "tree_id": "2ec4275f6f6122b813de2a87ea9cb416dce79385",
+          "url": "https://github.com/unicode-org/icu/commit/6d5555a739179b5d177e73db7c111c5ef1cac22d"
+        },
+        "date": 1715637883435,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "Test_ICU_Forward_Search",
+            "value": 21.2596,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "Test_ICU_Backward_Search",
+            "value": 49.3185,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
