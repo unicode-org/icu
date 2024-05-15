ae43abe3e8be39b948568f25a03b8a9cdd981a51
github-action-benchmark <github@users.noreply.github.com>
2024-05-15 18:31:28 +0000

    add Benchmark (ndjson) benchmark result for 398890853d4937e8ea886d44776edb2cc49f1081

--- perf/results/j_dateformatperf/sw_KE/TestICUConstruction/1/data.js
+++ perf/results/j_dateformatperf/sw_KE/TestICUConstruction/1/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1715717448648,
+  "lastUpdate": 1715797887942,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -21778,6 +21778,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUConstruction",
             "value": 2958.435695932584,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "hsivonen@hsivonen.fi",
+            "name": "Henri Sivonen",
+            "username": "hsivonen"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "398890853d4937e8ea886d44776edb2cc49f1081",
+          "message": "ICU-22718 Export disallowed/ignored UTS 46 data for ICU4X",
+          "timestamp": "2024-05-15T11:20:09-07:00",
+          "tree_id": "6e6ed82255a9a0ec91985a25805cbe0be7bf7d8a",
+          "url": "https://github.com/unicode-org/icu/commit/398890853d4937e8ea886d44776edb2cc49f1081"
+        },
+        "date": 1715797770745,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUConstruction",
+            "value": 3029.4558258604357,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
