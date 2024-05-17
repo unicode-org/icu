e37dad69be3db40fe904f6b7f1494643def90557
github-action-benchmark <github@users.noreply.github.com>
2024-05-17 21:53:08 +0000

    add Benchmark (ndjson) benchmark result for 0ef4da943c1cfc694e84fcb85cee5c78bae89d71

--- perf/results/j_dateformatperf/en_US/TestICUFormat/5/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUFormat/5/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1715866580371,
+  "lastUpdate": 1715982788403,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -22228,6 +22228,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 259.0482418376077,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aszasz@users.noreply.github.com",
+            "name": "aszasz",
+            "username": "aszasz"
+          },
+          "committer": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "distinct": true,
+          "id": "0ef4da943c1cfc694e84fcb85cee5c78bae89d71",
+          "message": "ICU-22722 Fix broken link to faq in docs/userguide/icu/index.md\n\nSee #3006",
+          "timestamp": "2024-05-17T14:43:28-07:00",
+          "tree_id": "4f02fd9d47f9f78577b0db9e62a5522f7fc034cc",
+          "url": "https://github.com/unicode-org/icu/commit/0ef4da943c1cfc694e84fcb85cee5c78bae89d71"
+        },
+        "date": 1715982586589,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 245.75707635816926,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
