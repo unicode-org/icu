abc4277a721362e87dd09bb6e24e16af89aa12af
github-action-benchmark <github@users.noreply.github.com>
2024-05-17 22:01:23 +0000

    add Benchmark (ndjson) benchmark result for 0ef4da943c1cfc694e84fcb85cee5c78bae89d71

--- perf/results/collperf/TestNames_SerbianSH/data.js
+++ perf/results/collperf/TestNames_SerbianSH/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1715866966558,
+  "lastUpdate": 1715983283668,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -44764,6 +44764,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestIcu_BinarySearch_usekey",
             "value": 16987971.3802,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aszasz@users.noreply.github.com",
+            "name": "aszasz",
+            "username": "aszasz"
+          },
+          "committer": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "distinct": true,
+          "id": "0ef4da943c1cfc694e84fcb85cee5c78bae89d71",
+          "message": "ICU-22722 Fix broken link to faq in docs/userguide/icu/index.md\n\nSee #3006",
+          "timestamp": "2024-05-17T14:43:28-07:00",
+          "tree_id": "4f02fd9d47f9f78577b0db9e62a5522f7fc034cc",
+          "url": "https://github.com/unicode-org/icu/commit/0ef4da943c1cfc694e84fcb85cee5c78bae89d71"
+        },
+        "date": 1715983165715,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestIcu_KeyGen_null",
+            "value": 315.3101,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_qsort_strcoll_null",
+            "value": 34417105.3927,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_qsort_usekey",
+            "value": 14642659.0459,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_BinarySearch_strcoll_null",
+            "value": 34385864.6968,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_BinarySearch_usekey",
+            "value": 16415505.1062,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
