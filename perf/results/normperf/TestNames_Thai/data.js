ff7ddfca2516a87ccdb6ac4df1dfe128e6d1ded0
github-action-benchmark <github@users.noreply.github.com>
2024-05-17 22:03:43 +0000

    add Benchmark (ndjson) benchmark result for 0ef4da943c1cfc694e84fcb85cee5c78bae89d71

--- perf/results/normperf/TestNames_Thai/data.js
+++ perf/results/normperf/TestNames_Thai/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1715867171678,
+  "lastUpdate": 1715983423642,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -49618,6 +49618,66 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFD_Orig_Text",
             "value": 7.3063,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aszasz@users.noreply.github.com",
+            "name": "aszasz",
+            "username": "aszasz"
+          },
+          "committer": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "distinct": true,
+          "id": "0ef4da943c1cfc694e84fcb85cee5c78bae89d71",
+          "message": "ICU-22722 Fix broken link to faq in docs/userguide/icu/index.md\n\nSee #3006",
+          "timestamp": "2024-05-17T14:43:28-07:00",
+          "tree_id": "4f02fd9d47f9f78577b0db9e62a5522f7fc034cc",
+          "url": "https://github.com/unicode-org/icu/commit/0ef4da943c1cfc694e84fcb85cee5c78bae89d71"
+        },
+        "date": 1715983306818,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFC_NFD_Text",
+            "value": 6.899,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFC_NFC_Text",
+            "value": 6.8975,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFC_Orig_Text",
+            "value": 6.9,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFD_NFD_Text",
+            "value": 6.9309,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFD_NFC_Text",
+            "value": 7.028,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFD_Orig_Text",
+            "value": 6.9923,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
