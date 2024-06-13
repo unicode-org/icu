4662311542d1ac4324a2faa8b573591ab6a3231f
Frank Yung-Fong Tang <ftang@google.com>
2024-06-13 12:50:25 -0700

    Update messagepattern.h
    bump up limit

--- icu4c/source/common/unicode/messagepattern.h
+++ icu4c/source/common/unicode/messagepattern.h
@@ -821,7 +821,7 @@ public:
 
         static const int32_t MAX_LENGTH=0xffff;
         static const int32_t MAX_VALUE=0x7fff;
-        static const int32_t MAX_NESTED_LEVELS=0x01ff;
+        static const int32_t MAX_NESTED_LEVELS=0x07ff;
 
         // Some fields are not final because they are modified during pattern parsing.
         // After pattern parsing, the parts are effectively immutable.
