a28583f92fa3bf9d2e3f680d6c4b82a30b569895
github-action-benchmark <github@users.noreply.github.com>
2024-06-13 13:46:09 +0000

    add Benchmark (ndjson) benchmark result for 64f529e113a369afbde9a16d31c73c3e2153753e

--- perf/results/j_dateformatperf/en_US/TestICUFormat/4/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUFormat/4/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718285367514,
+  "lastUpdate": 1718286369564,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -22498,6 +22498,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 222.90149778537946,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "roubert@google.com",
+            "name": "Fredrik Roubert",
+            "username": "roubert"
+          },
+          "committer": {
+            "email": "fredrik@roubert.name",
+            "name": "Fredrik Roubert",
+            "username": "roubert"
+          },
+          "distinct": true,
+          "id": "64f529e113a369afbde9a16d31c73c3e2153753e",
+          "message": "ICU-22310 Update configure files from configure.ac using autoreconf.",
+          "timestamp": "2024-06-13T15:09:29+02:00",
+          "tree_id": "ff9280e5142ea87cbe0533d0df12a9e272ebb880",
+          "url": "https://github.com/unicode-org/icu/commit/64f529e113a369afbde9a16d31c73c3e2153753e"
+        },
+        "date": 1718286244475,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 226.13547495584706,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
