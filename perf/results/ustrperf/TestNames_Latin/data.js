1b0e2f19ecbb3e5b52c9d006ded853d290caca3b
github-action-benchmark <github@users.noreply.github.com>
2024-07-19 18:37:31 +0000

    add Benchmark (ndjson) benchmark result for 8ca6bc754599a01689751e8f1c68f482ff5997e8

--- perf/results/ustrperf/TestNames_Latin/data.js
+++ perf/results/ustrperf/TestNames_Latin/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1720702239025,
+  "lastUpdate": 1721414251437,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -82845,6 +82845,102 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestScan2",
             "value": 40.1776,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "clopez@igalia.com",
+            "name": "Carlos Alberto Lopez Perez",
+            "username": "clopez"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "8ca6bc754599a01689751e8f1c68f482ff5997e8",
+          "message": "ICU-22813 Rise the size of the buffers used for the command strings at pkgdata\n\nThe tool pkgdata uses snprintf() to build the strings of the commands that\nwill execute later during the install process. But the maximum size of this\nbuffers is not enough when there is a long path.\n\nThis has caused issues on some CI systems that use very long paths, causing\nthe install process to produce a wrong result.\n\nThe maximum path on Linux is 4096 (defined as PATH_MAX at <linux/limits.h>)\nSo the size of SMALL_BUFFER_MAX_SIZE should be 4096 to avoid errors related\nto truncated paths.",
+          "timestamp": "2024-07-19T18:02:53Z",
+          "tree_id": "b4bdc4589ecebdb32d0f5d81693eea5b8e4eccad",
+          "url": "https://github.com/unicode-org/icu/commit/8ca6bc754599a01689751e8f1c68f482ff5997e8"
+        },
+        "date": 1721414120987,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestCtor",
+            "value": 20.007,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestCtor1",
+            "value": 20.744,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestCtor2",
+            "value": 24.2984,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestCtor3",
+            "value": 33.9724,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestAssign",
+            "value": 43.8307,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestAssign1",
+            "value": 36.808,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestAssign2",
+            "value": 29.0437,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestGetch",
+            "value": 17.0438,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestCatenate",
+            "value": 108.115,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestScan",
+            "value": 22.3356,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestScan1",
+            "value": 37.7344,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestScan2",
+            "value": 39.6214,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
