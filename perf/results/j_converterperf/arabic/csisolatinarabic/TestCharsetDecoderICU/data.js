237d02899c4d81fc6dd9daadefb0fecd7d9c1fa9
github-action-benchmark <github@users.noreply.github.com>
2024-08-09 16:08:24 +0000

    add Benchmark (ndjson) benchmark result for b5b3e16afac61f9aa9b775aaf497f8cc88ce9481

--- perf/results/j_converterperf/arabic/csisolatinarabic/TestCharsetDecoderICU/data.js
+++ perf/results/j_converterperf/arabic/csisolatinarabic/TestCharsetDecoderICU/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1723181904779,
+  "lastUpdate": 1723219704212,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -24057,6 +24057,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestCharsetDecoderICU",
             "value": 0.012052138258948023,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mnita@google.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "distinct": true,
+          "id": "b5b3e16afac61f9aa9b775aaf497f8cc88ce9481",
+          "message": "ICU-22845 Better iterations for the ICU4J UnicodeSet",
+          "timestamp": "2024-08-09T08:56:25-07:00",
+          "tree_id": "c1e3517413d5486216c70caa2effaeb466f344f4",
+          "url": "https://github.com/unicode-org/icu/commit/b5b3e16afac61f9aa9b775aaf497f8cc88ce9481"
+        },
+        "date": 1723219561842,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestCharsetDecoderICU",
+            "value": 0.012072619632230833,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
