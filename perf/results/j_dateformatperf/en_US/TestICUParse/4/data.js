c303562575d8c928ff11e56b03536b02e7bd221d
github-action-benchmark <github@users.noreply.github.com>
2024-08-09 16:01:36 +0000

    add Benchmark (ndjson) benchmark result for b5b3e16afac61f9aa9b775aaf497f8cc88ce9481

--- perf/results/j_dateformatperf/en_US/TestICUParse/4/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUParse/4/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1723181747200,
+  "lastUpdate": 1723219296340,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -23847,6 +23847,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 795.503909643788,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mnita@google.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "distinct": true,
+          "id": "b5b3e16afac61f9aa9b775aaf497f8cc88ce9481",
+          "message": "ICU-22845 Better iterations for the ICU4J UnicodeSet",
+          "timestamp": "2024-08-09T08:56:25-07:00",
+          "tree_id": "c1e3517413d5486216c70caa2effaeb466f344f4",
+          "url": "https://github.com/unicode-org/icu/commit/b5b3e16afac61f9aa9b775aaf497f8cc88ce9481"
+        },
+        "date": 1723219160744,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 775.5611946851617,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
