6568b04c7017473667dfb8d8b5de7bf07499d073
Markus Scherer <markus.icu@gmail.com>
2024-08-12 19:17:23 -0700

    U16Iterator op*() returns U16OneSeq

--- icu4c/source/common/unicode/utf16cppiter.h
+++ icu4c/source/common/unicode/utf16cppiter.h
@@ -39,67 +39,46 @@ enum U16IllFormedBehavior {
     U16_BEHAVIOR_SURROGATE
 };
 
-// Validating iterator over the code points in a Unicode 16-bit string.
-// TODO: all @draft ICU 76
-template<typename Unit16, U16IllFormedBehavior behavior>
-class U16Iterator {
+/**
+ * A code unit sequence for one code point returned by U16Iterator.
+ *
+ * TODO: check doxygen syntax for template parameters
+ * @param Unit16 char16_t or uint16_t or (on Windows) wchar_t
+ * @draft ICU 76
+ */
+template<typename Unit16>
+class U16OneSeq {
 public:
-    // TODO: make private, make friends
-    U16Iterator(const Unit16 *start, const Unit16 *p, const Unit16 *limit) :
-            start(start), p(p), limit(limit) {
-        if (p != limit) {
-            readOneForward();
-        }
-    }
-    // TODO: We might try to support limit==nullptr, similar to U16_ macros supporting length<0.
-    // Test pointers for == or != but not < or >.
+    U16OneSeq(const U16OneSeq &other) = default;
 
-    U16Iterator(const U16Iterator &other) = default;
-    U16Iterator(U16Iterator &&other) noexcept = default;
+    const Unit16 *data() { return p; }
+    int32_t length() const { return len; }
 
-    bool operator==(const U16Iterator &other) const { return p == other.p; }
-    bool operator!=(const U16Iterator &other) const { return !operator==(other); }
-
-    UChar32 operator*() const {
-        return c;
-    }
-
-    // TODO: good function names?
-    // It would be nice to avoid a prefix like "current", "one", "cp",
-    // but just length() on the iterator could be confusing.
-    int32_t currentLength() const { return len; }
-
-    std::basic_string_view<Unit16> currentView() const {
+    std::basic_string_view<Unit16> stringView() const {
         return std::basic_string_view<Unit16>(p, len);
     }
 
-    bool currentIsWellFormed() const { return ok; }
+    bool isWellFormed() const { return ok; }
 
-    U16Iterator &operator++() {  // pre-increment
-        // TODO: think about switching directions etc.
-        // Assume that readOneForward() was called and set `len`.
-        // Skip the current code point, then read the next one.
-        p += len;
-        if (p != limit) {
-            readOneForward();
-        }
-        return *this;
-    }
+    UChar32 codePoint() const { return c; }
 
-    U16Iterator operator++(int) {  // post-increment
-        U16Iterator result(*this);
-        // TODO: think about switching directions etc.
-        // Assume that readOneForward() was called and set `len`.
-        // Skip the current code point, then read the next one.
-        p += len;
-        if (p != limit) {
-            readOneForward();
-        }
-        return result;
-    }
+    // TODO: std::optional<UChar32> maybeCodePoint() const ? (nullopt if !ok)
 
 private:
-    void readOneForward() {
+    // TODO: Why can't we just use Unit16 here?
+    // error: declaration of 'Unit16' shadows template parameter
+    template<typename SomeOtherUnit16, U16IllFormedBehavior behavior>
+    friend class U16Iterator;
+
+    U16OneSeq(const Unit16 *p) : p(p) {}
+
+    void fwd1() { p += len; }
+
+    void readOneForward(const Unit16 *limit) {
+        if (p == limit) {
+            len = 0;
+            return;
+        }
         // see U16_NEXT_OR_FFFD()
         c = *p;
         len = 1;
@@ -117,14 +96,65 @@ private:
         }
     }
 
-    // In a validating iterator, we need start & limit so that when we read a code point
-    // (forward or backward) we can test if there are enough code units.
-    const Unit16 *start;
     const Unit16 *p;
-    const Unit16 *limit;
     UChar32 c = 0;
     int8_t len = 0;
     bool ok = false;
+};
+
+/**
+ * Validating iterator over the code points in a Unicode 16-bit string.
+ *
+ * TODO: check doxygen syntax for template parameters
+ * @param Unit16 char16_t or uint16_t or (on Windows) wchar_t
+ * @param U16IllFormedBehavior TODO
+ * @draft ICU 76
+ */
+template<typename Unit16, U16IllFormedBehavior behavior>
+class U16Iterator {
+public:
+    // TODO: make private, make friends
+    U16Iterator(const Unit16 *start, const Unit16 *p, const Unit16 *limit) :
+            start(start), limit(limit), seq(p) {
+        seq.readOneForward(limit);
+    }
+    // TODO: We might try to support limit==nullptr, similar to U16_ macros supporting length<0.
+    // Test pointers for == or != but not < or >.
+
+    U16Iterator(const U16Iterator &other) = default;
+
+    bool operator==(const U16Iterator &other) const { return seq.p == other.seq.p; }
+    bool operator!=(const U16Iterator &other) const { return !operator==(other); }
+
+    const U16OneSeq<Unit16> &operator*() const {
+        return seq;
+    }
+
+    U16Iterator &operator++() {  // pre-increment
+        // TODO: think about switching directions etc.
+        // Assume that readOneForward() was called and set seq.len.
+        // Skip the current code point, then read the next one.
+        seq.fwd1();
+        seq.readOneForward(limit);
+        return *this;
+    }
+
+    U16Iterator operator++(int) {  // post-increment
+        U16Iterator result(*this);
+        // TODO: think about switching directions etc.
+        // Assume that readOneForward() was called and set seq.len.
+        // Skip the current code point, then read the next one.
+        seq.fwd1();
+        seq.readOneForward(limit);
+        return result;
+    }
+
+private:
+    // In a validating iterator, we need start & limit so that when we read a code point
+    // (forward or backward) we can test if there are enough code units.
+    const Unit16 *start;
+    const Unit16 *limit;
+    U16OneSeq<Unit16> seq;
 };
 
 // ------------------------------------------------------------------------- ***
