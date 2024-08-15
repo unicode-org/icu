398ead5f39e8539c0d4e40a377de9f05e93f5183
github-action-benchmark <github@users.noreply.github.com>
2024-08-15 00:20:36 +0000

    add Benchmark (ndjson) benchmark result for 045350e7c1e21861d9a664a586a33ad70a7ad387

--- perf/results/j_dateformatperf/en_US/TestICUFormat/1/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUFormat/1/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1723610768399,
+  "lastUpdate": 1723681236285,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -24147,6 +24147,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 242.2274613584823,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "dragan@unicode.org",
+            "name": "DraganBesevic",
+            "username": "DraganBesevic"
+          },
+          "committer": {
+            "email": "131725218+DraganBesevic@users.noreply.github.com",
+            "name": "DraganBesevic",
+            "username": "DraganBesevic"
+          },
+          "distinct": true,
+          "id": "045350e7c1e21861d9a664a586a33ad70a7ad387",
+          "message": "ICU-22723 Integrate CLDR 46 release alpha0, part 4, fixes for exausting tests",
+          "timestamp": "2024-08-14T17:09:35-07:00",
+          "tree_id": "0086847282e134f70965b7394e64b27fbf3e0446",
+          "url": "https://github.com/unicode-org/icu/commit/045350e7c1e21861d9a664a586a33ad70a7ad387"
+        },
+        "date": 1723681098899,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 232.8873827345485,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
