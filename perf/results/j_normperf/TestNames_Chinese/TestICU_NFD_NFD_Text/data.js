52c3e65500fcfca8d62b586045c484fd823fb663
github-action-benchmark <github@users.noreply.github.com>
2024-08-15 22:05:32 +0000

    add Benchmark (ndjson) benchmark result for f4a04631cd079ff141176f3c4d523198f151eb9e

--- perf/results/j_normperf/TestNames_Chinese/TestICU_NFD_NFD_Text/data.js
+++ perf/results/j_normperf/TestNames_Chinese/TestICU_NFD_NFD_Text/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1723681325904,
+  "lastUpdate": 1723759532331,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -24477,6 +24477,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFD_NFD_Text",
             "value": 5.2149367032923175,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "distinct": true,
+          "id": "f4a04631cd079ff141176f3c4d523198f151eb9e",
+          "message": "ICU-22722 Bump the github-actions group across 1 directory with 4 updates\n\nSee #3075",
+          "timestamp": "2024-08-15T17:52:40-04:00",
+          "tree_id": "e6ad589b7b9b1fc7cd5a87436b7f29dceb2f7405",
+          "url": "https://github.com/unicode-org/icu/commit/f4a04631cd079ff141176f3c4d523198f151eb9e"
+        },
+        "date": 1723759390561,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFD_NFD_Text",
+            "value": 5.2421095629034,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
