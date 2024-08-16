85b503cea10350b222f2c9502c76cfc82335d1b7
github-action-benchmark <github@users.noreply.github.com>
2024-08-16 20:46:32 +0000

    add Benchmark (ndjson) benchmark result for 40189ffe575c66e2acef294765acbf23e653fe76

--- perf/results/j_dateformatperf/en_US/TestICUParse/4/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUParse/4/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1723832759873,
+  "lastUpdate": 1723841192789,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -24147,6 +24147,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 789.3608083624156,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mnita@google.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "distinct": true,
+          "id": "40189ffe575c66e2acef294765acbf23e653fe76",
+          "message": "ICU-21205 Fix Eclipse failing to import the icu4j maven project",
+          "timestamp": "2024-08-16T13:38:56-07:00",
+          "tree_id": "13f2177fa9abbfc67e096cb152bbcaf377537ec4",
+          "url": "https://github.com/unicode-org/icu/commit/40189ffe575c66e2acef294765acbf23e653fe76"
+        },
+        "date": 1723841046872,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 791.9489090287783,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
