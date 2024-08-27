aac6365772c4169c03689f2b37e46c07cf80b001
github-action-benchmark <github@users.noreply.github.com>
2024-08-27 22:07:43 +0000

    add Benchmark (ndjson) benchmark result for 1414e80f6c1376afa65eb388aaaafb53169fc3dc

--- perf/results/j_dateformatperf/en_US/TestICUFormat/5/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUFormat/5/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724781921390,
+  "lastUpdate": 1724796463197,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -24747,6 +24747,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 252.4636487886198,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "cwcornelius@gmail.com",
+            "name": "Craig Cornelius",
+            "username": "sven-oly"
+          },
+          "committer": {
+            "email": "cwcornelius@gmail.com",
+            "name": "Craig Cornelius",
+            "username": "sven-oly"
+          },
+          "distinct": true,
+          "id": "1414e80f6c1376afa65eb388aaaafb53169fc3dc",
+          "message": "ICU-22722 Add RelativeDateTimeFormatter to main API page via docmain.h\n\nSee #3119",
+          "timestamp": "2024-08-27T14:53:04-07:00",
+          "tree_id": "e84b2248d38536d80b263a110ece7a350041b33a",
+          "url": "https://github.com/unicode-org/icu/commit/1414e80f6c1376afa65eb388aaaafb53169fc3dc"
+        },
+        "date": 1724796317953,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 249.00361118808007,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
