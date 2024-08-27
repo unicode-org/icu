a11c9eee71cafb7b17f7a96ec46cc46d0a2f2a37
github-action-benchmark <github@users.noreply.github.com>
2024-08-27 17:48:11 +0000

    add Benchmark (ndjson) benchmark result for 967babdc5f19967bb8a5ce7c76fbb15a820b1b6d

--- perf/results/j_normperf/TestNames_SerbianSH/TestICU_NFC_NFC_Text/data.js
+++ perf/results/j_normperf/TestNames_SerbianSH/TestICU_NFC_NFC_Text/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724627239334,
+  "lastUpdate": 1724780891830,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -24447,6 +24447,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFC_NFC_Text",
             "value": 1.5207658000675823,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "richard_gillam@apple.com",
+            "name": "Rich Gillam",
+            "username": "richgillam"
+          },
+          "committer": {
+            "email": "62772518+richgillam@users.noreply.github.com",
+            "name": "Rich Gillam",
+            "username": "richgillam"
+          },
+          "distinct": true,
+          "id": "967babdc5f19967bb8a5ce7c76fbb15a820b1b6d",
+          "message": "ICU-22669 Fix circular dependency in DateTimePatternGenerator::addICUPatterns().",
+          "timestamp": "2024-08-27T10:37:19-07:00",
+          "tree_id": "5293219cc4229dcca64b3266848256634b57cd7d",
+          "url": "https://github.com/unicode-org/icu/commit/967babdc5f19967bb8a5ce7c76fbb15a820b1b6d"
+        },
+        "date": 1724780746265,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFC_NFC_Text",
+            "value": 1.5092492529367123,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
