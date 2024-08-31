85918755b6445050d27ef2b8388228ee073b4bd1
github-action-benchmark <github@users.noreply.github.com>
2024-08-31 23:02:09 +0000

    add Benchmark (ndjson) benchmark result for edbf02094f6105f45873947a3509ebcbc43ddf14

--- perf/results/ustrperf/TestNames_Russian/data.js
+++ perf/results/ustrperf/TestNames_Russian/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725084479992,
+  "lastUpdate": 1725145329601,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -87933,6 +87933,102 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestScan2",
             "value": 44.9047,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mnita@google.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "distinct": true,
+          "id": "edbf02094f6105f45873947a3509ebcbc43ddf14",
+          "message": "ICU-22606 Correct urls in the scm entry; fix JCite styles in javadoc\n\n1. The flatten plugin appended the relative module paths to the URLs in <scm>.\nThe same kind of problem solved earlier with ${proj.url}, and fixed the same way.\nI was unable to find any way to tell the plugin to not do that append, even after\nreading some of the plugin code.\n\n2. The stylesheet containing the JCite styles was not copied in the generated javadoc.\nThe first part of our custom stylesheet is 100% the same as the oficial one.\nSo the only part that was visually affected was the code produced by JCite.\nThe code still used <pre> and <code>, so it was monospaced and well indented / wrapped,\nbut no syntax colors.",
+          "timestamp": "2024-08-31T15:37:10-07:00",
+          "tree_id": "710920e32e050d2cabaa7a75d9b7a5e99011fbed",
+          "url": "https://github.com/unicode-org/icu/commit/edbf02094f6105f45873947a3509ebcbc43ddf14"
+        },
+        "date": 1725145184577,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestCtor",
+            "value": 19.9427,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestCtor1",
+            "value": 22.0404,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestCtor2",
+            "value": 26.2406,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestCtor3",
+            "value": 41.5533,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestAssign",
+            "value": 49.4261,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestAssign1",
+            "value": 35.487,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestAssign2",
+            "value": 24.5449,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestGetch",
+            "value": 15.0879,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestCatenate",
+            "value": 89.8978,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestScan",
+            "value": 22.266,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestScan1",
+            "value": 41.1482,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestScan2",
+            "value": 44.5391,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
