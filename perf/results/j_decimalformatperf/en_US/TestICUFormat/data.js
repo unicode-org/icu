3ab221cbff5e637d3556d01a905d904329f32cd2
github-action-benchmark <github@users.noreply.github.com>
2024-09-06 10:10:24 +0000

    add Benchmark (ndjson) benchmark result for 6d67afcf013e1facf4169c91f7b0d63680008ee1

--- perf/results/j_decimalformatperf/en_US/TestICUFormat/data.js
+++ perf/results/j_decimalformatperf/en_US/TestICUFormat/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725612397413,
+  "lastUpdate": 1725617423969,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -25047,6 +25047,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 308.26550432853713,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "103115900+rp9-next@users.noreply.github.com",
+            "name": "Rahul Pandey",
+            "username": "rp9-next"
+          },
+          "committer": {
+            "email": "103115900+rp9-next@users.noreply.github.com",
+            "name": "Rahul Pandey",
+            "username": "rp9-next"
+          },
+          "distinct": true,
+          "id": "6d67afcf013e1facf4169c91f7b0d63680008ee1",
+          "message": "ICU-22874 Fix building with -Werror on macOS clang",
+          "timestamp": "2024-09-06T15:26:36+05:30",
+          "tree_id": "1ac86cc52449fff1cc081508e6092813113811ab",
+          "url": "https://github.com/unicode-org/icu/commit/6d67afcf013e1facf4169c91f7b0d63680008ee1"
+        },
+        "date": 1725617274096,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 304.7672583454905,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
