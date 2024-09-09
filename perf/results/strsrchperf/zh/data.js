6807e03d617caf0ad9a16bfb4bf5e51e82480fe0
github-action-benchmark <github@users.noreply.github.com>
2024-09-09 12:24:04 +0000

    add Benchmark (ndjson) benchmark result for 6c9d39a08c469a755fcbec1719bcd0d3af217e7f

--- perf/results/strsrchperf/zh/data.js
+++ perf/results/strsrchperf/zh/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725660448981,
+  "lastUpdate": 1725884644719,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -30777,6 +30777,42 @@ window.BENCHMARK_DATA = {
           {
             "name": "Test_ICU_Backward_Search",
             "value": 52.9575,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "roubert@google.com",
+            "name": "Fredrik Roubert",
+            "username": "roubert"
+          },
+          "committer": {
+            "email": "fredrik@roubert.name",
+            "name": "Fredrik Roubert",
+            "username": "roubert"
+          },
+          "distinct": true,
+          "id": "6c9d39a08c469a755fcbec1719bcd0d3af217e7f",
+          "message": "ICU-22843 Enable constructing UnicodeString from literal in fixed time.\n\nWhen passing a string literal to any of the legacy constructors that\ntake just a plain pointer to a UTF-16 string it becomes necessary to\niterate through the string to find its length, even though this length\nwas known to the compiler (which just has no way of passing it on to the\nconstructor).\n\nBut when calling the new templated string view constructor instead it\nbecomes possible for the compiler to use the known length of a string\nliteral to directly create a string view of the correct size and pass\nthis on to the constructor.\n\nBy replacing the legacy constructors with the new constructor this is\nmade the default behaviour.",
+          "timestamp": "2024-09-09T13:50:58+02:00",
+          "tree_id": "3f87d48a016403e9fa8c90e5b63985748feb618e",
+          "url": "https://github.com/unicode-org/icu/commit/6c9d39a08c469a755fcbec1719bcd0d3af217e7f"
+        },
+        "date": 1725884491704,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "Test_ICU_Forward_Search",
+            "value": 25.2766,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "Test_ICU_Backward_Search",
+            "value": 50.2254,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
