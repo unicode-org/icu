58a1c3d9607bbf75a28465e026323a68ba8dc951
github-action-benchmark <github@users.noreply.github.com>
2024-09-12 22:52:34 +0000

    add Benchmark (ndjson) benchmark result for 5447a230701c156383fa6c6887d8f0babf0fd34d

--- perf/results/j_decimalformatperf/de_DE/TestICUParse/data.js
+++ perf/results/j_decimalformatperf/de_DE/TestICUParse/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726179164916,
+  "lastUpdate": 1726181554708,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -25737,6 +25737,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 286.15489743641365,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "5447a230701c156383fa6c6887d8f0babf0fd34d",
+          "message": "ICU-22876 add U_SHOW_CPLUSPLUS_HEADER_API for C++ header-only APIs",
+          "timestamp": "2024-09-12T15:36:40-07:00",
+          "tree_id": "0fd0ba22589dbc3194b605322fe3b9ce45af0e93",
+          "url": "https://github.com/unicode-org/icu/commit/5447a230701c156383fa6c6887d8f0babf0fd34d"
+        },
+        "date": 1726181402711,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 291.9547602999309,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
