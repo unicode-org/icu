b7bd4c4a5176b64de050bf4bee4ce56ffdf071f0
github-action-benchmark <github@users.noreply.github.com>
2024-09-12 16:34:46 +0000

    add Benchmark (ndjson) benchmark result for 4f246ab17fdab0299bd07f7511f6bae60bdd0246

--- perf/results/j_normperf/TestNames_Asian/TestICU_NFD_NFD_Text/data.js
+++ perf/results/j_normperf/TestNames_Asian/TestICU_NFD_NFD_Text/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726155115128,
+  "lastUpdate": 1726158886397,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -25707,6 +25707,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFD_NFD_Text",
             "value": 5.328700702934261,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ftang@chromium.org",
+            "name": "Frank Tang",
+            "username": "FrankYFTang"
+          },
+          "committer": {
+            "email": "ftang@google.com",
+            "name": "Frank Yung-Fong Tang",
+            "username": "FrankYFTang"
+          },
+          "distinct": true,
+          "id": "4f246ab17fdab0299bd07f7511f6bae60bdd0246",
+          "message": "ICU-22716 Test VTimeZone in fuzzer",
+          "timestamp": "2024-09-10T22:51:12-07:00",
+          "tree_id": "f90d343fef52854c57e2c3da8c991e9404594e52",
+          "url": "https://github.com/unicode-org/icu/commit/4f246ab17fdab0299bd07f7511f6bae60bdd0246"
+        },
+        "date": 1726158729961,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFD_NFD_Text",
+            "value": 5.384439495799189,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
