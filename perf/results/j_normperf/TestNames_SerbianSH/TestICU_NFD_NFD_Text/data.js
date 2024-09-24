baeca00fae967e3576e32477a4f959ba4cbe377e
github-action-benchmark <github@users.noreply.github.com>
2024-09-24 18:14:23 +0000

    add Benchmark (ndjson) benchmark result for 0334d92394abd17006c9cc61d54822b06cbe92f8

--- perf/results/j_normperf/TestNames_SerbianSH/TestICU_NFD_NFD_Text/data.js
+++ perf/results/j_normperf/TestNames_SerbianSH/TestICU_NFD_NFD_Text/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727199927452,
+  "lastUpdate": 1727201663132,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -27357,6 +27357,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFD_NFD_Text",
             "value": 3.151269509951598,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "0334d92394abd17006c9cc61d54822b06cbe92f8",
+          "message": "ICU-22721 simpler check for platforms that dont have char16_t",
+          "timestamp": "2024-09-24T10:56:14-07:00",
+          "tree_id": "d1408e826eaa081b414ebe0cd5eda9d1e3d7ac3d",
+          "url": "https://github.com/unicode-org/icu/commit/0334d92394abd17006c9cc61d54822b06cbe92f8"
+        },
+        "date": 1727201487779,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFD_NFD_Text",
+            "value": 3.020878125750745,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
