7c6da6c7e56ef18e97a2b1796d5cafb4c07c4994
github-action-benchmark <github@users.noreply.github.com>
2024-09-25 21:37:25 +0000

    add Benchmark (ndjson) benchmark result for 0924c725b0a1d0de314a48d2ded3dd5b7dbe6488

--- perf/results/j_decimalformatperf/de_DE/TestICUParse/data.js
+++ perf/results/j_decimalformatperf/de_DE/TestICUParse/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727297185855,
+  "lastUpdate": 1727300245382,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -27597,6 +27597,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 266.22592574773745,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "0924c725b0a1d0de314a48d2ded3dd5b7dbe6488",
+          "message": "ICU-22722 update PR template for ICU 77\n\nSee #3213",
+          "timestamp": "2024-09-25T14:31:10-07:00",
+          "tree_id": "d1676cd41a125a4047db1ce6ae889bb19f568d95",
+          "url": "https://github.com/unicode-org/icu/commit/0924c725b0a1d0de314a48d2ded3dd5b7dbe6488"
+        },
+        "date": 1727300079234,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 267.5002315033854,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
