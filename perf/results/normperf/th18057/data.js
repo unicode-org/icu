007572ced8f489b87d0b3e725ea0f98d39dcee31
github-action-benchmark <github@users.noreply.github.com>
2024-09-26 14:59:20 +0000

    add Benchmark (ndjson) benchmark result for 60d6bd71efc0cde8f861b109ff87dbbf9fc96586

--- perf/results/normperf/th18057/data.js
+++ perf/results/normperf/th18057/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727306262027,
+  "lastUpdate": 1727362759932,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -60357,6 +60357,66 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFD_Orig_Text",
             "value": 18.1541,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "yselkowi@redhat.com",
+            "name": "Yaakov Selkowitz",
+            "username": "yselkowitz"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "60d6bd71efc0cde8f861b109ff87dbbf9fc96586",
+          "message": "ICU-22721 Update filtration_test for Python 3.13\n\nSee #3046",
+          "timestamp": "2024-09-23T18:41:34-07:00",
+          "tree_id": "577defc1d3365711f1f083da0366625d707320ca",
+          "url": "https://github.com/unicode-org/icu/commit/60d6bd71efc0cde8f861b109ff87dbbf9fc96586"
+        },
+        "date": 1727362584444,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFC_NFD_Text",
+            "value": 17.0424,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFC_NFC_Text",
+            "value": 17.0446,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFC_Orig_Text",
+            "value": 17.0083,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFD_NFD_Text",
+            "value": 17.1719,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFD_NFC_Text",
+            "value": 17.1228,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFD_Orig_Text",
+            "value": 17.0288,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
