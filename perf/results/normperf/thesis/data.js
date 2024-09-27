a65bd0eba4d85e016a94ddca001e92fce5edd0f8
github-action-benchmark <github@users.noreply.github.com>
2024-09-27 20:30:02 +0000

    add Benchmark (ndjson) benchmark result for bd9ffc38f012e038117c1e991253575c52e61b64

--- perf/results/normperf/thesis/data.js
+++ perf/results/normperf/thesis/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727440116711,
+  "lastUpdate": 1727469001927,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -60777,6 +60777,66 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFD_Orig_Text",
             "value": 3.2583,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "distinct": true,
+          "id": "bd9ffc38f012e038117c1e991253575c52e61b64",
+          "message": "ICU-22930 The -sources.jar files in icu4j version 76.1 are AGAIN a lot bigger than before",
+          "timestamp": "2024-09-27T13:07:13-07:00",
+          "tree_id": "e5acf2b8995f6d2a9f93befe1bcd7b228908b2a0",
+          "url": "https://github.com/unicode-org/icu/commit/bd9ffc38f012e038117c1e991253575c52e61b64"
+        },
+        "date": 1727468765260,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFC_NFD_Text",
+            "value": 3.2089,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFC_NFC_Text",
+            "value": 3.196,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFC_Orig_Text",
+            "value": 3.1873,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFD_NFD_Text",
+            "value": 3.2482,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFD_NFC_Text",
+            "value": 3.2519,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFD_Orig_Text",
+            "value": 3.259,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
