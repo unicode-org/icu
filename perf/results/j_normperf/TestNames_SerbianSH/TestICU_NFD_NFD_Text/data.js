2c3a5a7460b87fa9859d601fd54ae15b5e835178
github-action-benchmark <github@users.noreply.github.com>
2024-09-27 20:12:38 +0000

    add Benchmark (ndjson) benchmark result for bd9ffc38f012e038117c1e991253575c52e61b64

--- perf/results/j_normperf/TestNames_SerbianSH/TestICU_NFD_NFD_Text/data.js
+++ perf/results/j_normperf/TestNames_SerbianSH/TestICU_NFD_NFD_Text/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727439351278,
+  "lastUpdate": 1727467958950,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -28017,6 +28017,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFD_NFD_Text",
             "value": 3.11486451364574,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "distinct": true,
+          "id": "bd9ffc38f012e038117c1e991253575c52e61b64",
+          "message": "ICU-22930 The -sources.jar files in icu4j version 76.1 are AGAIN a lot bigger than before",
+          "timestamp": "2024-09-27T13:07:13-07:00",
+          "tree_id": "e5acf2b8995f6d2a9f93befe1bcd7b228908b2a0",
+          "url": "https://github.com/unicode-org/icu/commit/bd9ffc38f012e038117c1e991253575c52e61b64"
+        },
+        "date": 1727467787366,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFD_NFD_Text",
+            "value": 3.0747657570104403,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
