405c0e646a4d625101968604fa22878dfc018259
github-action-benchmark <github@users.noreply.github.com>
2024-10-03 12:54:07 +0000

    add Benchmark (ndjson) benchmark result for 73626da0ca673eff9f51a5664b5348cd4c3cfac5

--- perf/results/j_converterperf/s-chinese/UTF-8/TestCharsetDecoderICU/data.js
+++ perf/results/j_converterperf/s-chinese/UTF-8/TestCharsetDecoderICU/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727904492883,
+  "lastUpdate": 1727960047402,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -28137,6 +28137,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestCharsetDecoderICU",
             "value": 1.790487959863354,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mnita@google.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "distinct": true,
+          "id": "73626da0ca673eff9f51a5664b5348cd4c3cfac5",
+          "message": "ICU-22606 Create full release from CI - review feedback",
+          "timestamp": "2024-09-30T13:44:26-07:00",
+          "tree_id": "902a1b6ecdad823633a5d249866e71a85579020d",
+          "url": "https://github.com/unicode-org/icu/commit/73626da0ca673eff9f51a5664b5348cd4c3cfac5"
+        },
+        "date": 1727959865642,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestCharsetDecoderICU",
+            "value": 1.79816242379983,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
