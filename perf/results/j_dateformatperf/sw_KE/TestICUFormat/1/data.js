22453b7f5a8964e2736e28d57d9495ed900c18e8
github-action-benchmark <github@users.noreply.github.com>
2024-10-03 12:47:28 +0000

    add Benchmark (ndjson) benchmark result for 73626da0ca673eff9f51a5664b5348cd4c3cfac5

--- perf/results/j_dateformatperf/sw_KE/TestICUFormat/1/data.js
+++ perf/results/j_dateformatperf/sw_KE/TestICUFormat/1/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727904024391,
+  "lastUpdate": 1727959648317,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -27987,6 +27987,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 236.63148840728593,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mnita@google.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "distinct": true,
+          "id": "73626da0ca673eff9f51a5664b5348cd4c3cfac5",
+          "message": "ICU-22606 Create full release from CI - review feedback",
+          "timestamp": "2024-09-30T13:44:26-07:00",
+          "tree_id": "902a1b6ecdad823633a5d249866e71a85579020d",
+          "url": "https://github.com/unicode-org/icu/commit/73626da0ca673eff9f51a5664b5348cd4c3cfac5"
+        },
+        "date": 1727959466858,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 238.6870301275895,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
