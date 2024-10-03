99d80fa34aef5dbf03db42ba82945578908ab097
github-action-benchmark <github@users.noreply.github.com>
2024-10-03 12:45:48 +0000

    add Benchmark (ndjson) benchmark result for 73626da0ca673eff9f51a5664b5348cd4c3cfac5

--- perf/results/j_normperf/TestNames_Asian/TestICU_NFC_Orig_Text/data.js
+++ perf/results/j_normperf/TestNames_Asian/TestICU_NFC_Orig_Text/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727904251978,
+  "lastUpdate": 1727959548720,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -28167,6 +28167,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFC_Orig_Text",
             "value": 3.970701178143402,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mnita@google.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "distinct": true,
+          "id": "73626da0ca673eff9f51a5664b5348cd4c3cfac5",
+          "message": "ICU-22606 Create full release from CI - review feedback",
+          "timestamp": "2024-09-30T13:44:26-07:00",
+          "tree_id": "902a1b6ecdad823633a5d249866e71a85579020d",
+          "url": "https://github.com/unicode-org/icu/commit/73626da0ca673eff9f51a5664b5348cd4c3cfac5"
+        },
+        "date": 1727959366606,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFC_Orig_Text",
+            "value": 3.9983001348560063,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
