7a1d42b3a1abdc3908d62e37ca223c6195fa8846
github-action-benchmark <github@users.noreply.github.com>
2024-10-08 22:20:29 +0000

    add Benchmark (ndjson) benchmark result for ca9fcca3c7edbb04eab7ffa3eed2047667916b03

--- perf/results/j_normperf/TestNames_Chinese/TestICU_NFD_NFC_Text/data.js
+++ perf/results/j_normperf/TestNames_Chinese/TestICU_NFD_NFC_Text/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1728426020834,
+  "lastUpdate": 1728426029013,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -56606,6 +56606,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFD_NFC_Text",
             "value": 5.18944895003306,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "egg.robin.leroy@gmail.com",
+            "name": "Robin Leroy",
+            "username": "eggrobin"
+          },
+          "committer": {
+            "email": "egg.robin.leroy@gmail.com",
+            "name": "Robin Leroy",
+            "username": "eggrobin"
+          },
+          "distinct": true,
+          "id": "ca9fcca3c7edbb04eab7ffa3eed2047667916b03",
+          "message": "ICU-21097 Remove LineBreakTest.txt workarounds",
+          "timestamp": "2024-10-09T00:03:34+02:00",
+          "tree_id": "805aebb69e23859bc4db7e78dc00eef73deb8ab5",
+          "url": "https://github.com/unicode-org/icu/commit/ca9fcca3c7edbb04eab7ffa3eed2047667916b03"
+        },
+        "date": 1728425847117,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFD_NFC_Text",
+            "value": 5.238310150490849,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
