88b0e8aa86a87498c0df7c05935c4e2fb0c000fa
github-action-benchmark <github@users.noreply.github.com>
2024-11-09 01:12:14 +0000

    add Benchmark (ndjson) benchmark result for 44ea9278b97768ee8c5df9d247f9afd43be1ed52

--- perf/results/j_dateformatperf/en_US/TestICUFormat/2/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUFormat/2/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1731027591583,
+  "lastUpdate": 1731114734692,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -28587,6 +28587,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 212.31442307102986,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ftang@chromium.org",
+            "name": "Frank Tang",
+            "username": "FrankYFTang"
+          },
+          "committer": {
+            "email": "ftang@google.com",
+            "name": "Frank Yung-Fong Tang",
+            "username": "FrankYFTang"
+          },
+          "distinct": true,
+          "id": "44ea9278b97768ee8c5df9d247f9afd43be1ed52",
+          "message": "ICU-22962 fix int32_t overflow inside handleComputeJulianDay\n\ntest",
+          "timestamp": "2024-11-08T16:56:16-08:00",
+          "tree_id": "21830f75788761e25f882daadc47c7b1ddfe4600",
+          "url": "https://github.com/unicode-org/icu/commit/44ea9278b97768ee8c5df9d247f9afd43be1ed52"
+        },
+        "date": 1731114554532,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 198.30159584648933,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
