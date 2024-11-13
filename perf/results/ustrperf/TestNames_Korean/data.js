ba0f3e1b13b76410bebbf76cae9b26e5194dc9af
github-action-benchmark <github@users.noreply.github.com>
2024-11-13 22:41:40 +0000

    add Benchmark (ndjson) benchmark result for 8b28c3843f20641e2eca54198e208745d5baaffa

--- perf/results/ustrperf/TestNames_Korean/data.js
+++ perf/results/ustrperf/TestNames_Korean/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1731406946308,
+  "lastUpdate": 1731537700423,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -99933,6 +99933,102 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestScan2",
             "value": 38.7588,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "distinct": true,
+          "id": "8b28c3843f20641e2eca54198e208745d5baaffa",
+          "message": "ICU-22927 Duplicate (back) the MF2 test data between icu4c and icu4j\n\nThe C++ and Java implementations are done by two different people different companies.\nWith different time constraints and availability.\nThe spec is still not final (although it is close), and the implementations are\nstill quite a bit behind.\nSharing these test files slows down development, by forcing any C++ and Java changes\nto happen in the same time.\n\nThere are other components that share test files that are not shared yet,\neven if they are more stable. So I don't know why we would force this on MF2 only.\n\nThis is temporary, and the data files will be de-duplicated again at a later time,\nwhen the two implementations are more stable.\nThat de-duplication will include other shared files, and in a slightly different structure\n(we have a doc and a discution on this topic).",
+          "timestamp": "2024-11-13T14:09:42-08:00",
+          "tree_id": "dc23f81678eb878a826f68d4d820761556412d05",
+          "url": "https://github.com/unicode-org/icu/commit/8b28c3843f20641e2eca54198e208745d5baaffa"
+        },
+        "date": 1731537506200,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestCtor",
+            "value": 18.7008,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestCtor1",
+            "value": 20.5616,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestCtor2",
+            "value": 23.9498,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestCtor3",
+            "value": 26.1604,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestAssign",
+            "value": 37.0605,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestAssign1",
+            "value": 32.1363,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestAssign2",
+            "value": 27.782,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestGetch",
+            "value": 16.7195,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestCatenate",
+            "value": 79.2644,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestScan",
+            "value": 21.5422,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestScan1",
+            "value": 36.1068,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestScan2",
+            "value": 38.9584,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
