5179f18e80ef8bc6ca47fbf0792671642c389ca9
github-action-benchmark <github@users.noreply.github.com>
2024-11-13 22:21:53 +0000

    add Benchmark (ndjson) benchmark result for 8b28c3843f20641e2eca54198e208745d5baaffa

--- perf/results/j_converterperf/arabic/csisolatinarabic/TestCharsetDecoderICU/data.js
+++ perf/results/j_converterperf/arabic/csisolatinarabic/TestCharsetDecoderICU/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1731405929316,
+  "lastUpdate": 1731536513731,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -28887,6 +28887,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestCharsetDecoderICU",
             "value": 0.011965703242405428,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "distinct": true,
+          "id": "8b28c3843f20641e2eca54198e208745d5baaffa",
+          "message": "ICU-22927 Duplicate (back) the MF2 test data between icu4c and icu4j\n\nThe C++ and Java implementations are done by two different people different companies.\nWith different time constraints and availability.\nThe spec is still not final (although it is close), and the implementations are\nstill quite a bit behind.\nSharing these test files slows down development, by forcing any C++ and Java changes\nto happen in the same time.\n\nThere are other components that share test files that are not shared yet,\neven if they are more stable. So I don't know why we would force this on MF2 only.\n\nThis is temporary, and the data files will be de-duplicated again at a later time,\nwhen the two implementations are more stable.\nThat de-duplication will include other shared files, and in a slightly different structure\n(we have a doc and a discution on this topic).",
+          "timestamp": "2024-11-13T14:09:42-08:00",
+          "tree_id": "dc23f81678eb878a826f68d4d820761556412d05",
+          "url": "https://github.com/unicode-org/icu/commit/8b28c3843f20641e2eca54198e208745d5baaffa"
+        },
+        "date": 1731536318507,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestCharsetDecoderICU",
+            "value": 0.011957319615493517,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
