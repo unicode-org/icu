92cd65995171c247e9b24c1716651a288d58cb14
github-action-benchmark <github@users.noreply.github.com>
2024-11-14 00:00:53 +0000

    add Benchmark (ndjson) benchmark result for 06a23f8d37ce0c3a985bb676cbc05eb7bd5f3573

--- perf/results/collperf/TestNames_Chinese/data.js
+++ perf/results/collperf/TestNames_Chinese/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1731537051237,
+  "lastUpdate": 1731542452920,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -56751,6 +56751,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestIcu_BinarySearch_usekey",
             "value": 2106986.9107,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "distinct": true,
+          "id": "06a23f8d37ce0c3a985bb676cbc05eb7bd5f3573",
+          "message": "ICU-22965 Fix the problems we had with the maven deploy for ICU 76.1\n\nThe blocker was the missing `<name>` element, which is mandatory\nSee https://central.sonatype.org/publish/requirements/#project-name-description-and-url\n\nThe second problem was that all modules were staged, not just the three we normally publish.",
+          "timestamp": "2024-11-13T15:38:08-08:00",
+          "tree_id": "45d56cf26405e8f80e96a551b3967ac8d8fe67ee",
+          "url": "https://github.com/unicode-org/icu/commit/06a23f8d37ce0c3a985bb676cbc05eb7bd5f3573"
+        },
+        "date": 1731542261197,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestIcu_KeyGen_null",
+            "value": 151.6724,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_qsort_strcoll_null",
+            "value": 8561250.6997,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_qsort_usekey",
+            "value": 2098805.872,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_BinarySearch_strcoll_null",
+            "value": 7567767.9366,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_BinarySearch_usekey",
+            "value": 2098461.7524,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
