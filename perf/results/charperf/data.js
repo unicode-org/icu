e6d7b9d23e42022f753e0e2d983327160270b2ad
github-action-benchmark <github@users.noreply.github.com>
2024-11-14 00:04:19 +0000

    add Benchmark (ndjson) benchmark result for 06a23f8d37ce0c3a985bb676cbc05eb7bd5f3573

--- perf/results/charperf/data.js
+++ perf/results/charperf/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1731537269897,
+  "lastUpdate": 1731542659085,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -93957,6 +93957,96 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestIsWhiteSpace",
             "value": 4.5885,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "distinct": true,
+          "id": "06a23f8d37ce0c3a985bb676cbc05eb7bd5f3573",
+          "message": "ICU-22965 Fix the problems we had with the maven deploy for ICU 76.1\n\nThe blocker was the missing `<name>` element, which is mandatory\nSee https://central.sonatype.org/publish/requirements/#project-name-description-and-url\n\nThe second problem was that all modules were staged, not just the three we normally publish.",
+          "timestamp": "2024-11-13T15:38:08-08:00",
+          "tree_id": "45d56cf26405e8f80e96a551b3967ac8d8fe67ee",
+          "url": "https://github.com/unicode-org/icu/commit/06a23f8d37ce0c3a985bb676cbc05eb7bd5f3573"
+        },
+        "date": 1731542465737,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestIsAlpha",
+            "value": 4.2819,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIsUpper",
+            "value": 4.5767,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIsLower",
+            "value": 4.2811,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIsDigit",
+            "value": 4.2612,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIsSpace",
+            "value": 4.9036,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIsAlphaNumeric",
+            "value": 4.2638,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIsPrint",
+            "value": 4.2638,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIsControl",
+            "value": 4.2812,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestToLower",
+            "value": 5.4475,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestToUpper",
+            "value": 6.4134,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIsWhiteSpace",
+            "value": 4.5868,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
