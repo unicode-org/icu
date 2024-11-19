ca06f0d761abc34421eb756f2c17951de0e1111b
github-action-benchmark <github@users.noreply.github.com>
2024-11-19 18:00:55 +0000

    add Benchmark (ndjson) benchmark result for 38c046b60cb990646e402afc03f89b4daa4b2e6b

--- perf/results/j_decimalformatperf/en_US/TestICUFormat/data.js
+++ perf/results/j_decimalformatperf/en_US/TestICUFormat/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1731541544371,
+  "lastUpdate": 1732039255564,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -29007,6 +29007,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 315.5132885664494,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vignesbr@amazon.com",
+            "name": "Vignesh Subramani",
+            "username": "vignesh28s1995"
+          },
+          "committer": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "distinct": true,
+          "id": "38c046b60cb990646e402afc03f89b4daa4b2e6b",
+          "message": "ICU-22888 Enhance XML Processor Configuration for Tools Directory in ICU4J Package\n\nSee #3243",
+          "timestamp": "2024-11-19T09:54:51-08:00",
+          "tree_id": "c0c5853439a97f95f63ced75b877aebde7d9b05f",
+          "url": "https://github.com/unicode-org/icu/commit/38c046b60cb990646e402afc03f89b4daa4b2e6b"
+        },
+        "date": 1732039071067,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 309.926114424746,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
