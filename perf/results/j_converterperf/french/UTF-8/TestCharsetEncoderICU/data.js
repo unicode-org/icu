27a6067fb7b23b3663db3340f6d42df0909dc453
github-action-benchmark <github@users.noreply.github.com>
2024-11-22 18:27:52 +0000

    add Benchmark (ndjson) benchmark result for 424d6a3e8b993cb2a3d4450bb2449b4057726251

--- perf/results/j_converterperf/french/UTF-8/TestCharsetEncoderICU/data.js
+++ perf/results/j_converterperf/french/UTF-8/TestCharsetEncoderICU/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1732230657718,
+  "lastUpdate": 1732300072358,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -29157,6 +29157,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestCharsetEncoderICU",
             "value": 1.8276905196883946,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "roubert@google.com",
+            "name": "Fredrik Roubert",
+            "username": "roubert"
+          },
+          "committer": {
+            "email": "fredrik@roubert.name",
+            "name": "Fredrik Roubert",
+            "username": "roubert"
+          },
+          "distinct": true,
+          "id": "424d6a3e8b993cb2a3d4450bb2449b4057726251",
+          "message": "ICU-22901 Update ulocimp_getSubtags() &co. to use std::string_view.",
+          "timestamp": "2024-11-22T19:05:03+01:00",
+          "tree_id": "02e501417daeecb40c29337d5a343a6f68fd9d82",
+          "url": "https://github.com/unicode-org/icu/commit/424d6a3e8b993cb2a3d4450bb2449b4057726251"
+        },
+        "date": 1732299879628,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestCharsetEncoderICU",
+            "value": 1.8294308497758598,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
