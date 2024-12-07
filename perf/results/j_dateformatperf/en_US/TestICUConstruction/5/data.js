bba275a635739d0bd01a3bd137fface747f5066c
github-action-benchmark <github@users.noreply.github.com>
2024-12-07 00:37:39 +0000

    add Benchmark (ndjson) benchmark result for 2e57f07cdf1ff34cae8ad68fbe5f0c7bb443ba64

--- perf/results/j_dateformatperf/en_US/TestICUConstruction/5/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUConstruction/5/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1733429039397,
+  "lastUpdate": 1733531859109,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -29127,6 +29127,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUConstruction",
             "value": 1990.6025961848832,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ftang@chromium.org",
+            "name": "Frank Tang",
+            "username": "FrankYFTang"
+          },
+          "committer": {
+            "email": "ftang@google.com",
+            "name": "Frank Yung-Fong Tang",
+            "username": "FrankYFTang"
+          },
+          "distinct": true,
+          "id": "2e57f07cdf1ff34cae8ad68fbe5f0c7bb443ba64",
+          "message": "ICU-22736 Address Markus' feedback",
+          "timestamp": "2024-12-06T16:24:57-08:00",
+          "tree_id": "876acd818f33b31d91fd3ae9a9ea3c38a9afc719",
+          "url": "https://github.com/unicode-org/icu/commit/2e57f07cdf1ff34cae8ad68fbe5f0c7bb443ba64"
+        },
+        "date": 1733531664502,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUConstruction",
+            "value": 1994.3343324494042,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
