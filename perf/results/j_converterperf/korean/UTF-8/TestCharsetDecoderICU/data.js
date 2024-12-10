207d3511fb488dcc352733addf4b526e7dd938c2
github-action-benchmark <github@users.noreply.github.com>
2024-12-10 16:35:22 +0000

    add Benchmark (ndjson) benchmark result for 62b95bb5f95fbf469414add7e1c16a4cc2a1b9a3

--- perf/results/j_converterperf/korean/UTF-8/TestCharsetDecoderICU/data.js
+++ perf/results/j_converterperf/korean/UTF-8/TestCharsetDecoderICU/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1733837663744,
+  "lastUpdate": 1733848522144,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -29307,6 +29307,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestCharsetDecoderICU",
             "value": 2.0324681533267706,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tjc@igalia.com",
+            "name": "Tim Chevalier",
+            "username": "catamorphism"
+          },
+          "committer": {
+            "email": "tjc@igalia.com",
+            "name": "Tim Chevalier",
+            "username": "catamorphism"
+          },
+          "distinct": true,
+          "id": "62b95bb5f95fbf469414add7e1c16a4cc2a1b9a3",
+          "message": "ICU-22908 MF2: Update spec tests and update implementation for recent spec changes\n\nUpdating the spec tests requires two implementation changes:\n* Match on variables rather than expressions --\n  see https://github.com/unicode-org/message-format-wg/pull/877\n* Require attribute values to be literals (if present) --\n  see https://github.com/unicode-org/message-format-wg/pull/894\n\nThis updates the spec tests to commit 6c3704f41a9c24427126429fb43992b03609dfc8\nin https://github.com/unicode-org/message-format-wg/ . Any changes\nfollowing that commit will be addressed in a future PR.",
+          "timestamp": "2024-12-09T09:38:28-08:00",
+          "tree_id": "36407eae7eb7fb7b5ea9cc9f7586abb74077ec9b",
+          "url": "https://github.com/unicode-org/icu/commit/62b95bb5f95fbf469414add7e1c16a4cc2a1b9a3"
+        },
+        "date": 1733848324343,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestCharsetDecoderICU",
+            "value": 2.1463470415438866,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
