bbe943d165b151016fb652f82650387b31686893
github-action-benchmark <github@users.noreply.github.com>
2024-12-10 16:44:25 +0000

    add Benchmark (ndjson) benchmark result for 62b95bb5f95fbf469414add7e1c16a4cc2a1b9a3

--- perf/results/normperf/TestNames_SerbianSR/data.js
+++ perf/results/normperf/TestNames_SerbianSR/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1733838221945,
+  "lastUpdate": 1733849065814,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -63897,6 +63897,66 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFD_Orig_Text",
             "value": 7.1504,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tjc@igalia.com",
+            "name": "Tim Chevalier",
+            "username": "catamorphism"
+          },
+          "committer": {
+            "email": "tjc@igalia.com",
+            "name": "Tim Chevalier",
+            "username": "catamorphism"
+          },
+          "distinct": true,
+          "id": "62b95bb5f95fbf469414add7e1c16a4cc2a1b9a3",
+          "message": "ICU-22908 MF2: Update spec tests and update implementation for recent spec changes\n\nUpdating the spec tests requires two implementation changes:\n* Match on variables rather than expressions --\n  see https://github.com/unicode-org/message-format-wg/pull/877\n* Require attribute values to be literals (if present) --\n  see https://github.com/unicode-org/message-format-wg/pull/894\n\nThis updates the spec tests to commit 6c3704f41a9c24427126429fb43992b03609dfc8\nin https://github.com/unicode-org/message-format-wg/ . Any changes\nfollowing that commit will be addressed in a future PR.",
+          "timestamp": "2024-12-09T09:38:28-08:00",
+          "tree_id": "36407eae7eb7fb7b5ea9cc9f7586abb74077ec9b",
+          "url": "https://github.com/unicode-org/icu/commit/62b95bb5f95fbf469414add7e1c16a4cc2a1b9a3"
+        },
+        "date": 1733848872046,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFC_NFD_Text",
+            "value": 6.4077,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFC_NFC_Text",
+            "value": 6.389,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFC_Orig_Text",
+            "value": 6.4346,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFD_NFD_Text",
+            "value": 6.9592,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFD_NFC_Text",
+            "value": 6.9739,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFD_Orig_Text",
+            "value": 7.0951,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
