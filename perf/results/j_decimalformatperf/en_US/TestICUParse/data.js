e35a74df13e077014cc7342995d7c6f5a359ed02
github-action-benchmark <github@users.noreply.github.com>
2024-12-15 09:25:47 +0000

    add Benchmark (ndjson) benchmark result for 4ff5d6a0703f733eb34b329a443e221858c230bb

--- perf/results/j_decimalformatperf/en_US/TestICUParse/data.js
+++ perf/results/j_decimalformatperf/en_US/TestICUParse/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1734035345259,
+  "lastUpdate": 1734254747237,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -29787,6 +29787,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 310.44140653165124,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "distinct": true,
+          "id": "4ff5d6a0703f733eb34b329a443e221858c230bb",
+          "message": "ICU-22920 Fix raw type warnings in icu4j core tests",
+          "timestamp": "2024-12-15T01:18:47-08:00",
+          "tree_id": "3720c98913ccfc798262a3a49fbf741be05e2e04",
+          "url": "https://github.com/unicode-org/icu/commit/4ff5d6a0703f733eb34b329a443e221858c230bb"
+        },
+        "date": 1734254554623,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 318.12321327352055,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
