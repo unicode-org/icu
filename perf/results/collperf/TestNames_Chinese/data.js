b25d52e671d22a372b16b7c0dedebecd176a4fd9
github-action-benchmark <github@users.noreply.github.com>
2025-01-21 08:20:15 +0000

    add Benchmark (ndjson) benchmark result for 384c54ce66eeb933a523103b01c322840164428a

--- perf/results/collperf/TestNames_Chinese/data.js
+++ perf/results/collperf/TestNames_Chinese/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1736980796459,
+  "lastUpdate": 1737447615100,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -59667,6 +59667,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestIcu_BinarySearch_usekey",
             "value": 2107282.1222,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "20001722@protonmail.com",
+            "name": "Taichi Haradaguchi",
+            "username": "th1722"
+          },
+          "committer": {
+            "email": "fredrik@roubert.name",
+            "name": "Fredrik Roubert",
+            "username": "roubert"
+          },
+          "distinct": true,
+          "id": "384c54ce66eeb933a523103b01c322840164428a",
+          "message": "ICU-22966 Fix -Wshadow warnings in unicode/uset.h\n\nFix -Wshadow warnings displayed\nwhen compiling ICU4C 76.1 or later.\n\nCo-authored-by: Xisco Fauli <xiscofauli@libreoffice.org>",
+          "timestamp": "2025-01-21T16:53:06+09:00",
+          "tree_id": "e1300791aa835bba8a0936adf70ceba0a8c07447",
+          "url": "https://github.com/unicode-org/icu/commit/384c54ce66eeb933a523103b01c322840164428a"
+        },
+        "date": 1737447394980,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestIcu_KeyGen_null",
+            "value": 153.4725,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_qsort_strcoll_null",
+            "value": 8738882.5249,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_qsort_usekey",
+            "value": 2090110.2388,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_BinarySearch_strcoll_null",
+            "value": 7746091.0037,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_BinarySearch_usekey",
+            "value": 2114907.4431,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
