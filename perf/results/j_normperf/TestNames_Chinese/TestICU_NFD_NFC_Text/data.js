01efd94c9e5861abcbb658f73a6036e6665f4e12
github-action-benchmark <github@users.noreply.github.com>
2025-01-21 08:11:38 +0000

    add Benchmark (ndjson) benchmark result for 384c54ce66eeb933a523103b01c322840164428a

--- perf/results/j_normperf/TestNames_Chinese/TestICU_NFD_NFC_Text/data.js
+++ perf/results/j_normperf/TestNames_Chinese/TestICU_NFD_NFC_Text/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1737446979305,
+  "lastUpdate": 1737447098699,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -61106,6 +61106,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFD_NFC_Text",
             "value": 5.280052874338325,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "20001722@protonmail.com",
+            "name": "Taichi Haradaguchi",
+            "username": "th1722"
+          },
+          "committer": {
+            "email": "fredrik@roubert.name",
+            "name": "Fredrik Roubert",
+            "username": "roubert"
+          },
+          "distinct": true,
+          "id": "384c54ce66eeb933a523103b01c322840164428a",
+          "message": "ICU-22966 Fix -Wshadow warnings in unicode/uset.h\n\nFix -Wshadow warnings displayed\nwhen compiling ICU4C 76.1 or later.\n\nCo-authored-by: Xisco Fauli <xiscofauli@libreoffice.org>",
+          "timestamp": "2025-01-21T16:53:06+09:00",
+          "tree_id": "e1300791aa835bba8a0936adf70ceba0a8c07447",
+          "url": "https://github.com/unicode-org/icu/commit/384c54ce66eeb933a523103b01c322840164428a"
+        },
+        "date": 1737446881832,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFD_NFC_Text",
+            "value": 5.2128518972153195,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
