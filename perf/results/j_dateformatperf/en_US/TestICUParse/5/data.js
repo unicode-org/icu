955470ac601e5f710c8a2a7a2ce26ed01b279b08
github-action-benchmark <github@users.noreply.github.com>
2025-01-25 00:05:03 +0000

    add Benchmark (ndjson) benchmark result for 4df4eb419b81ecedfb55f65dfdc23953cc083a4d

--- perf/results/j_dateformatperf/en_US/TestICUParse/5/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUParse/5/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1737760838647,
+  "lastUpdate": 1737763503313,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -30447,6 +30447,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 368.9668330958325,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "younies.mahmoud@gmail.com",
+            "name": "Younies Mahmoud",
+            "username": "younies"
+          },
+          "committer": {
+            "email": "younies.mahmoud@gmail.com",
+            "name": "Younies Mahmoud",
+            "username": "younies"
+          },
+          "distinct": true,
+          "id": "4df4eb419b81ecedfb55f65dfdc23953cc083a4d",
+          "message": "ICU-22781 Add support for constant denominators in unit conversion\n\nImplement handling of constant denominators in unit conversion:\n- Add divide method to Factor class in UnitsConverter\n- Update ConversionRates to apply constant denominator during conversion\n- Add comprehensive test cases for units with constant denominators",
+          "timestamp": "2025-01-25T00:54:37+01:00",
+          "tree_id": "c1c51c9974748fbe2496faadc08488859cd2ace6",
+          "url": "https://github.com/unicode-org/icu/commit/4df4eb419b81ecedfb55f65dfdc23953cc083a4d"
+        },
+        "date": 1737763294513,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 345.34043517242645,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
