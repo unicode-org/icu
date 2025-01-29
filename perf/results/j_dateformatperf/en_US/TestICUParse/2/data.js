c4af5a5eb2fcbc9bbaa36e81a387a45c8fdebe3a
github-action-benchmark <github@users.noreply.github.com>
2025-01-29 09:23:49 +0000

    add Benchmark (ndjson) benchmark result for 8dfcbb5d092d9806e765f8ff58af610e1ad8b6ed

--- perf/results/j_dateformatperf/en_US/TestICUParse/2/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUParse/2/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1738112074942,
+  "lastUpdate": 1738142629679,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -30747,6 +30747,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 139.65891629274086,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "egg.robin.leroy@gmail.com",
+            "name": "Robin Leroy",
+            "username": "eggrobin"
+          },
+          "committer": {
+            "email": "egg.robin.leroy@gmail.com",
+            "name": "Robin Leroy",
+            "username": "eggrobin"
+          },
+          "distinct": true,
+          "id": "8dfcbb5d092d9806e765f8ff58af610e1ad8b6ed",
+          "message": "ICU-22984 Generate the Java UAX29 monkeys",
+          "timestamp": "2025-01-29T10:03:26+01:00",
+          "tree_id": "1949b8891f7abfea94688714a096aa51d8a690ad",
+          "url": "https://github.com/unicode-org/icu/commit/8dfcbb5d092d9806e765f8ff58af610e1ad8b6ed"
+        },
+        "date": 1738142411523,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 135.5512066960136,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
