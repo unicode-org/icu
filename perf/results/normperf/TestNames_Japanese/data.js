51901aa5002b8a5c9a6a0b2248db5b2f78ee2786
github-action-benchmark <github@users.noreply.github.com>
2025-01-30 19:31:12 +0000

    add Benchmark (ndjson) benchmark result for fb64693c281b97b2c7ce9619fda32f03a9e18235

--- perf/results/normperf/TestNames_Japanese/data.js
+++ perf/results/normperf/TestNames_Japanese/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1738178215489,
+  "lastUpdate": 1738265472103,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -66897,6 +66897,66 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFD_Orig_Text",
             "value": 13.5087,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "arthur.j.odwyer@gmail.com",
+            "name": "Arthur O'Dwyer",
+            "username": "Quuxplusone"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "fb64693c281b97b2c7ce9619fda32f03a9e18235",
+          "message": "ICU-22920 Avoid \"return by const value\" antipattern\n\nReturning a const-qualified prvalue doesn't do anything useful, but it does\nturn an assignment such as `v = rb.getLocale();` from a move-assignment\ninto a copy-assignment (because it's forbidden to move-from a const value,\neven if it's a const prvalue). Each affected site was diagnosed mechanically\nby my fork of Clang. E.g.:\n\n    warning: 'const' type qualifier on return type is a bad idea [-Wqual-class-return-type]\n      391 | const Locale ResourceBundle::getLocale(ULocDataLocaleType type, UErrorCode &status) const\n          | ^~~~~",
+          "timestamp": "2025-01-30T10:58:25-08:00",
+          "tree_id": "3d86f61476e5d32d6d1e78fb971161bd0ca08746",
+          "url": "https://github.com/unicode-org/icu/commit/fb64693c281b97b2c7ce9619fda32f03a9e18235"
+        },
+        "date": 1738265251562,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFC_NFD_Text",
+            "value": 13.6979,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFC_NFC_Text",
+            "value": 9.1789,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFC_Orig_Text",
+            "value": 9.1826,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFD_NFD_Text",
+            "value": 10.7107,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFD_NFC_Text",
+            "value": 11.7079,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFD_Orig_Text",
+            "value": 11.6942,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
