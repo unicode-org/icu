8a455e52897e1d215d2f66aad00d6d717bb06959
github-action-benchmark <github@users.noreply.github.com>
2025-02-04 23:58:37 +0000

    add Benchmark (ndjson) benchmark result for 0112e4292e5293c6211471a131552469ad6f9595

--- perf/results/j_converterperf/japanese/csiso2022jp/TestCharsetDecoderICU/data.js
+++ perf/results/j_converterperf/japanese/csiso2022jp/TestCharsetDecoderICU/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1738662598936,
+  "lastUpdate": 1738713517864,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -31347,6 +31347,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestCharsetDecoderICU",
             "value": 0.00923055697101263,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ftang@chromium.org",
+            "name": "Frank Tang",
+            "username": "FrankYFTang"
+          },
+          "committer": {
+            "email": "ftang@google.com",
+            "name": "Frank Yung-Fong Tang",
+            "username": "FrankYFTang"
+          },
+          "distinct": true,
+          "id": "0112e4292e5293c6211471a131552469ad6f9595",
+          "message": "ICU-22962 Fix int32_t overflow issue when month is large",
+          "timestamp": "2025-02-04T15:39:32-08:00",
+          "tree_id": "6cd05ba6b5af3741616dad5c1effdb8563179897",
+          "url": "https://github.com/unicode-org/icu/commit/0112e4292e5293c6211471a131552469ad6f9595"
+        },
+        "date": 1738713286068,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestCharsetDecoderICU",
+            "value": 0.009077822032691831,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
