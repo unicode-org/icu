097308d30e6b67d17f04e08c0c3c5b7150f38d42
github-action-benchmark <github@users.noreply.github.com>
2025-02-05 00:10:39 +0000

    add Benchmark (ndjson) benchmark result for 0112e4292e5293c6211471a131552469ad6f9595

--- perf/results/normperf/th18057/data.js
+++ perf/results/normperf/th18057/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1738663510386,
+  "lastUpdate": 1738714239623,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -67377,6 +67377,66 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFD_Orig_Text",
             "value": 13.8588,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ftang@chromium.org",
+            "name": "Frank Tang",
+            "username": "FrankYFTang"
+          },
+          "committer": {
+            "email": "ftang@google.com",
+            "name": "Frank Yung-Fong Tang",
+            "username": "FrankYFTang"
+          },
+          "distinct": true,
+          "id": "0112e4292e5293c6211471a131552469ad6f9595",
+          "message": "ICU-22962 Fix int32_t overflow issue when month is large",
+          "timestamp": "2025-02-04T15:39:32-08:00",
+          "tree_id": "6cd05ba6b5af3741616dad5c1effdb8563179897",
+          "url": "https://github.com/unicode-org/icu/commit/0112e4292e5293c6211471a131552469ad6f9595"
+        },
+        "date": 1738714019721,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFC_NFD_Text",
+            "value": 13.3726,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFC_NFC_Text",
+            "value": 13.2417,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFC_Orig_Text",
+            "value": 13.3077,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFD_NFD_Text",
+            "value": 13.8898,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFD_NFC_Text",
+            "value": 13.8532,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFD_Orig_Text",
+            "value": 13.9417,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
