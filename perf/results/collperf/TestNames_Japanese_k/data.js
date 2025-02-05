76ce1706b5c615b7e2186e9f8664de6fec3ee91a
github-action-benchmark <github@users.noreply.github.com>
2025-02-05 00:07:28 +0000

    add Benchmark (ndjson) benchmark result for 0112e4292e5293c6211471a131552469ad6f9595

--- perf/results/collperf/TestNames_Japanese_k/data.js
+++ perf/results/collperf/TestNames_Japanese_k/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1738663204672,
+  "lastUpdate": 1738714047885,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -60909,6 +60909,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestIcu_BinarySearch_usekey",
             "value": 5909831.9628,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ftang@chromium.org",
+            "name": "Frank Tang",
+            "username": "FrankYFTang"
+          },
+          "committer": {
+            "email": "ftang@google.com",
+            "name": "Frank Yung-Fong Tang",
+            "username": "FrankYFTang"
+          },
+          "distinct": true,
+          "id": "0112e4292e5293c6211471a131552469ad6f9595",
+          "message": "ICU-22962 Fix int32_t overflow issue when month is large",
+          "timestamp": "2025-02-04T15:39:32-08:00",
+          "tree_id": "6cd05ba6b5af3741616dad5c1effdb8563179897",
+          "url": "https://github.com/unicode-org/icu/commit/0112e4292e5293c6211471a131552469ad6f9595"
+        },
+        "date": 1738713823377,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestIcu_KeyGen_null",
+            "value": 232.3591,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_qsort_strcoll_null",
+            "value": 20762938.261,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_qsort_usekey",
+            "value": 5405117.0735,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_BinarySearch_strcoll_null",
+            "value": 19712211.8495,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_BinarySearch_usekey",
+            "value": 5908297.3794,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
