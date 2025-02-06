d9151aaa6ac074e6c9b5ddae3072b3b8a87c26c4
github-action-benchmark <github@users.noreply.github.com>
2025-02-06 15:53:25 +0000

    add Benchmark (ndjson) benchmark result for 362a4aa94b6c38f9ef4b7c13f3ff78ef3f6fd59b

--- perf/results/j_normperf/TestNames_SerbianSH/TestICU_NFC_Orig_Text/data.js
+++ perf/results/j_normperf/TestNames_SerbianSH/TestICU_NFC_Orig_Text/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1738841144370,
+  "lastUpdate": 1738857205503,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -31467,6 +31467,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFC_Orig_Text",
             "value": 1.5345548205077968,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "younies.mahmoud@gmail.com",
+            "name": "Younies Mahmoud",
+            "username": "younies"
+          },
+          "committer": {
+            "email": "younies.mahmoud@gmail.com",
+            "name": "Younies Mahmoud",
+            "username": "younies"
+          },
+          "distinct": true,
+          "id": "362a4aa94b6c38f9ef4b7c13f3ff78ef3f6fd59b",
+          "message": "ICU-23032 Update MeasureUnit documentation for `forIdentifier` method\n\nSee #3360",
+          "timestamp": "2025-02-06T16:37:58+01:00",
+          "tree_id": "2b4670b1e6b3346b8ba7b15f395afdc642de9179",
+          "url": "https://github.com/unicode-org/icu/commit/362a4aa94b6c38f9ef4b7c13f3ff78ef3f6fd59b"
+        },
+        "date": 1738856979611,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFC_Orig_Text",
+            "value": 1.5074282806120136,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
