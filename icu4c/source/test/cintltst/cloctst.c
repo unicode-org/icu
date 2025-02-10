unicode-org/icu

commit 678d5c127373d699ac4f1be55f8c017aeb0493d5

ICU-22520 Replace use of ulocimp_forLanguageTag() in uloc_getVariant().
Originally added by commit 24055f858542f6e044cce10224ab9227a933c41b
for ICU-7882, converting any language tag with a BCP-47 extension into a
legacy Unicode locale ID was a simple way to make the existing code keep
working unchanged also with BCP-47 extensions.

But the only thing that uloc_getVariant() needs is being able to find
out where variants end and extensions begin, for which converting the
entire language tag is unnecessary, it's much more straightforward to
instead just check for the -t-, -u- or -x- marker that indicates the
start of a BCP-47 extension.

icu4c/source/common/uloc.cpp
@@ -1103,6 +1103,18 @@ ulocimp_setKeywordValue(const char* keywords,
  */
 #define _isTerminator(a)  ((a==0)||(a=='.')||(a=='@'))
 
+namespace {
+
+inline bool _isBCP47Extension(const char* p) {
+    return p[0] == '-' &&
+           (p[1] == 't' || p[1] == 'T' ||
+            p[1] == 'u' || p[1] == 'U' ||
+            p[1] == 'x' || p[1] == 'X') &&
+           p[2] == '-';
+}
+
+}  // namespace
+
 /**
  * Lookup 'key' in the array 'list'.  The array 'list' should contain
  * a nullptr entry, followed by more entries, and a second nullptr entry.
@@ -1820,61 +1832,52 @@ uloc_getVariant(const char* localeID,
                 int32_t variantCapacity,
                 UErrorCode* err)
 {
-    CharString tempBuffer;
-    const char* tmpLocaleID;
     int32_t i=0;
 
     if(err==nullptr || U_FAILURE(*err)) {
         return 0;
     }
 
-    if (_hasBCP47Extension(localeID)) {
-        CharStringByteSink sink(&tempBuffer);
-        ulocimp_forLanguageTag(localeID, -1, sink, nullptr, err);
-        tmpLocaleID = U_SUCCESS(*err) && !tempBuffer.isEmpty() ? tempBuffer.data() : localeID;
-    } else {
-        if (localeID==nullptr) {
-           localeID=uloc_getDefault();
-        }
-        tmpLocaleID=localeID;
+    if (localeID == nullptr) {
+       localeID = uloc_getDefault();
     }
 
     /* Skip the language */
-    ulocimp_getLanguage(tmpLocaleID, &tmpLocaleID, *err);
+    ulocimp_getLanguage(localeID, &localeID, *err);
     if (U_FAILURE(*err)) {
         return 0;
     }
 
-    if(_isIDSeparator(*tmpLocaleID)) {
+    if (_isIDSeparator(*localeID)) {
         const char *scriptID;
         /* Skip the script if available */
-        ulocimp_getScript(tmpLocaleID+1, &scriptID, *err);
+        ulocimp_getScript(localeID+1, &scriptID, *err);
         if (U_FAILURE(*err)) {
             return 0;
         }
-        if(scriptID != tmpLocaleID+1) {
+        if (scriptID != localeID+1) {
             /* Found optional script */
-            tmpLocaleID = scriptID;
+            localeID = scriptID;
         }
         /* Skip the Country */
-        if (_isIDSeparator(*tmpLocaleID)) {
+        if (_isIDSeparator(*localeID)) {
             const char *cntryID;
-            ulocimp_getCountry(tmpLocaleID+1, &cntryID, *err);
+            ulocimp_getCountry(localeID+1, &cntryID, *err);
             if (U_FAILURE(*err)) {
                 return 0;
             }
-            if (cntryID != tmpLocaleID+1) {
+            if (cntryID != localeID+1) {
                 /* Found optional country */
-                tmpLocaleID = cntryID;
+                localeID = cntryID;
             }
-            if(_isIDSeparator(*tmpLocaleID)) {
+            if (_isIDSeparator(*localeID) && !_isBCP47Extension(localeID)) {
                 /* If there was no country ID, skip a possible extra IDSeparator */
-                if (tmpLocaleID != cntryID && _isIDSeparator(tmpLocaleID[1])) {
-                    tmpLocaleID++;
+                if (localeID != cntryID && _isIDSeparator(localeID[1])) {
+                    localeID++;
                 }
 
                 CheckedArrayByteSink sink(variant, variantCapacity);
-                _getVariant(tmpLocaleID+1, *tmpLocaleID, sink, false);
+                _getVariant(localeID+1, *localeID, sink, false);
 
                 i = sink.NumberOfBytesAppended();
 

commit aca1c83d82cbb94f7b04c88e49c000cc77ad1fe2

ICU-23031 Reinstate special case for "-u-va-posix" lost by ICU-22520.
Inside of locimp_forLanguageTag() in _appendKeywords() in uloc_tag.cpp
there's a hardcoded special case for "-u-va-posix" which appends the
"_POSIX" variant but this was missed during the refactoring made for
ICU-22520 (there isn't any test case that covers this).

So the call to locimp_forLanguageTag() did more than previously
understood, but we still don't want to have to call that for every
language tag that has BCP-47 extensions just in order to get to this
special case. Instead, add a special case also to ulocimp_getSubtags().

For this to work nicely, the loop in _getVariant() that copies variants
needs to be refactored so that it easily can break when encountering the
start of any BCP-47 extension (which also has the welcome side-effect of
making it more efficient, being able to append an entire variant at once
to the output sink).

This was broken by commit 678d5c127373d699ac4f1be55f8c017aeb0493d5.

icu4c/source/common/uloc.cpp
@@ -1341,7 +1341,7 @@ _getVariant(std::string_view localeID,
             ByteSink* sink,
             bool needSeparator,
             UErrorCode& status) {
-    if (U_FAILURE(status)) return 0;
+    if (U_FAILURE(status) || localeID.empty()) return 0;
 
     // Reasonable upper limit for variants
     // There are no strict limitation of the syntax of variant in the legacy
@@ -1357,27 +1357,43 @@ _getVariant(std::string_view localeID,
     size_t index = 0;
     if (_isIDSeparator(prev)) {
         /* get a variant string after a '-' or '_' */
-        for (; index < localeID.size() && !_isTerminator(localeID[index]); index++) {
-            if (index >= MAX_VARIANTS_LENGTH) { // same as length > MAX_VARIANTS_LENGTH
+        for (std::string_view sub = localeID;;) {
+            size_t next = sub.find_first_of(".@_-");
+            // For historical reasons, a trailing separator is included in the variant.
+            bool finished = next == std::string_view::npos || next + 1 == sub.length();
+            size_t limit = finished ? sub.length() : next;
+            index += limit;
+            if (index > MAX_VARIANTS_LENGTH) {
                 status = U_ILLEGAL_ARGUMENT_ERROR;
                 return 0;
             }
-            if (needSeparator) {
-                if (sink != nullptr) {
-                    sink->Append("_", 1);
-                }
-                needSeparator = false;
-            }
-            if (sink != nullptr) {
-                char c = uprv_toupper(localeID[index]);
-                if (c == '-') c = '_';
-                sink->Append(&c, 1);
-            }
-        }
-    }
 
-    if (index > 0) {
-        return index;
+            if (sink != nullptr) {
+                if (needSeparator) {
+                    sink->Append("_", 1);
+                } else {
+                    needSeparator = true;
+                }
+
+                int32_t length = static_cast<int32_t>(limit);
+                int32_t minCapacity = uprv_min(length, MAX_VARIANTS_LENGTH);
+                char scratch[MAX_VARIANTS_LENGTH];
+                int32_t capacity = 0;
+                char* buffer = sink->GetAppendBuffer(
+                        minCapacity, minCapacity, scratch, UPRV_LENGTHOF(scratch), &capacity);
+
+                for (size_t i = 0; i < limit; ++i) {
+                    buffer[i] = uprv_toupper(sub[i]);
+                }
+                sink->Append(buffer, length);
+            }
+
+            if (finished) { return index; }
+            sub.remove_prefix(next);
+            if (_isTerminator(sub.front()) || _isBCP47Extension(sub)) { return index; }
+            sub.remove_prefix(1);
+            index++;
+        }
     }
 
     size_t skip = 0;
@@ -1574,6 +1590,8 @@ ulocimp_getSubtags(
 
     if ((variant == nullptr && pEnd == nullptr) || localeID.empty()) { return; }
 
+    bool hasVariant = false;
+
     if (_isIDSeparator(localeID.front()) && !_isBCP47Extension(localeID)) {
         std::string_view sub = localeID;
         /* If there was no country ID, skip a possible extra IDSeparator */
@@ -1581,7 +1599,38 @@ ulocimp_getSubtags(
         sub.remove_prefix(skip);
         size_t len = _getVariant(sub, localeID[0], variant, false, status);
         if (U_FAILURE(status)) { return; }
-        if (len > 0 && pEnd != nullptr) { *pEnd = localeID.data() + skip + len; }
+        if (len > 0) {
+            hasVariant = true;
+            localeID.remove_prefix(skip + len);
+            if (pEnd != nullptr) { *pEnd = localeID.data(); }
+        }
+    }
+
+    if ((variant == nullptr && pEnd == nullptr) || localeID.empty()) { return; }
+
+    if (_isBCP47Extension(localeID)) {
+        localeID.remove_prefix(2);
+        constexpr char vaposix[] = "-va-posix";
+        constexpr size_t length = sizeof vaposix - 1;
+        for (size_t next;; localeID.remove_prefix(next)) {
+            next = localeID.find('-', 1);
+            if (next == std::string_view::npos) { break; }
+            next = localeID.find('-', next + 1);
+            bool finished = next == std::string_view::npos;
+            std::string_view sub = localeID;
+            if (!finished) { sub.remove_suffix(sub.length() - next); }
+
+            if (sub.length() == length && uprv_strnicmp(sub.data(), vaposix, length) == 0) {
+                if (variant != nullptr) {
+                    if (hasVariant) { variant->Append("_", 1); }
+                    constexpr char posix[] = "POSIX";
+                    variant->Append(posix, sizeof posix - 1);
+                }
+                if (pEnd != nullptr) { *pEnd = localeID.data() + length; }
+            }
+
+            if (finished) { break; }
+        }
     }
 }
 

icu4c/source/test/cintltst/cloctst.c
@@ -288,6 +288,9 @@ void addLocaleTest(TestNode** root)
     TESTCASE(TestBug20321UnicodeLocaleKey);
     TESTCASE(TestUsingDefaultWarning);
     TESTCASE(TestBug21449InfiniteLoop);
+    TESTCASE(TestBug23031VaPosix);
+    TESTCASE(TestBug23031VaPosixManyExtensions);
+    TESTCASE(TestBug23031VaPosixManyVariants);
     TESTCASE(TestExcessivelyLongIDs);
 #if !UCONFIG_NO_FORMATTING
     TESTCASE(TestUldnNameVariants);
@@ -7445,6 +7448,63 @@ static void TestBug21449InfiniteLoop(void) {
     // The issue causes an infinite loop to occur when looking up a non-existent resource for the invalid locale ID,
     // so the test is considered passed if the call to the API below returns anything at all.
     uloc_getDisplayLanguage(invalidLocaleId, invalidLocaleId, NULL, 0, &status);
+}
+
+// Test case for ICU-23031
+static void TestBug23031VaPosix(void) {
+    static const char tag[] = "en-US-u-va-posIX";
+    static const char expected[] = "POSIX";
+
+    UErrorCode status = U_ZERO_ERROR;
+    char actual[32];
+    int32_t len = uloc_getVariant(tag, actual, UPRV_LENGTHOF(actual), &status);
+    if (U_FAILURE(status)) {
+        log_err("ERROR: in uloc_getVariant  %s\n", myErrorName(status));
+    }
+    if (len < 1) {
+        log_err("FAIL: uloc_getVariant() returned %d\n", len);
+    }
+    if (uprv_strcmp(actual, expected) != 0) {
+        log_err("FAIL: uloc_getVariant() Wanted %s, got %s\n", expected, actual);
+    }
+}
+
+// Test case for ICU-23031
+static void TestBug23031VaPosixManyExtensions(void) {
+    static const char tag[] = "en-US-u-co-search-va-posIX-kc";
+    static const char expected[] = "POSIX";
+
+    UErrorCode status = U_ZERO_ERROR;
+    char actual[32];
+    int32_t len = uloc_getVariant(tag, actual, UPRV_LENGTHOF(actual), &status);
+    if (U_FAILURE(status)) {
+        log_err("ERROR: in uloc_getVariant  %s\n", myErrorName(status));
+    }
+    if (len < 1) {
+        log_err("FAIL: uloc_getVariant() returned %d\n", len);
+    }
+    if (uprv_strcmp(actual, expected) != 0) {
+        log_err("FAIL: uloc_getVariant() Wanted %s, got %s\n", expected, actual);
+    }
+}
+
+// Test case for ICU-23031
+static void TestBug23031VaPosixManyVariants(void) {
+    static const char tag[] = "en-US-fonIPA-u-va-posIX";
+    static const char expected[] = "FONIPA_POSIX";
+
+    UErrorCode status = U_ZERO_ERROR;
+    char actual[32];
+    int32_t len = uloc_getVariant(tag, actual, UPRV_LENGTHOF(actual), &status);
+    if (U_FAILURE(status)) {
+        log_err("ERROR: in uloc_getVariant  %s\n", myErrorName(status));
+    }
+    if (len < 1) {
+        log_err("FAIL: uloc_getVariant() returned %d\n", len);
+    }
+    if (uprv_strcmp(actual, expected) != 0) {
+        log_err("FAIL: uloc_getVariant() Wanted %s, got %s\n", expected, actual);
+    }
 }
 
 // rdar://79296849 and https://unicode-org.atlassian.net/browse/ICU-21639

icu4c/source/test/cintltst/cloctst.h
@@ -146,6 +146,9 @@ static void TestToLegacyType(void);
 static void TestBug20149(void);
 static void TestCDefaultLocale(void);
 static void TestBug21449InfiniteLoop(void);
+static void TestBug23031VaPosix(void);
+static void TestBug23031VaPosixManyExtensions(void);
+static void TestBug23031VaPosixManyVariants(void);
 
 
 /**