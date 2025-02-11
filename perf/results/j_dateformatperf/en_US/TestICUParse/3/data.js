9763c6f506d57c480892f0824c4ff76ccd6f1415
github-action-benchmark <github@users.noreply.github.com>
2025-02-11 18:47:10 +0000

    add Benchmark (ndjson) benchmark result for c9ca3cd5544eab3992a449aca1675c920c8140d0

--- perf/results/j_dateformatperf/en_US/TestICUParse/3/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUParse/3/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1739268216864,
+  "lastUpdate": 1739299630455,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -31797,6 +31797,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 861.7690397576882,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "roubert@google.com",
+            "name": "Fredrik Roubert",
+            "username": "roubert"
+          },
+          "committer": {
+            "email": "fredrik@roubert.name",
+            "name": "Fredrik Roubert",
+            "username": "roubert"
+          },
+          "distinct": true,
+          "id": "c9ca3cd5544eab3992a449aca1675c920c8140d0",
+          "message": "ICU-22920 Migrate from setup-bazelisk to setup-bazel.",
+          "timestamp": "2025-02-11T19:27:07+01:00",
+          "tree_id": "62ebeee40c8a1238379aab93c31d38bffaf347fe",
+          "url": "https://github.com/unicode-org/icu/commit/c9ca3cd5544eab3992a449aca1675c920c8140d0"
+        },
+        "date": 1739299396684,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 882.7899373770613,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
