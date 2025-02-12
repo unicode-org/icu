8ad9db73d54307fdc190e53cd0fdc64a96f15286
github-action-benchmark <github@users.noreply.github.com>
2025-02-12 06:28:03 +0000

    add Benchmark (ndjson) benchmark result for 030c79fe8805ce6a410b0bf0974297aec41d64c5

--- perf/results/j_decimalformatperf/de_DE/TestICUFormat/data.js
+++ perf/results/j_decimalformatperf/de_DE/TestICUFormat/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1739322051790,
+  "lastUpdate": 1739341683857,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -32007,6 +32007,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 554.7777614955735,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "younies.mahmoud@gmail.com",
+            "name": "Younies Mahmoud",
+            "username": "younies"
+          },
+          "committer": {
+            "email": "younies.mahmoud@gmail.com",
+            "name": "Younies Mahmoud",
+            "username": "younies"
+          },
+          "distinct": true,
+          "id": "030c79fe8805ce6a410b0bf0974297aec41d64c5",
+          "message": "ICU-22781 Enhance portion unit formatting tests in NumberFormatTest\n\n- Expanded test cases for portion unit formatting\n- Added support for different locales (en-US, de-DE)\n- Improved test coverage for portion unit formatting\n- Added handling for known CLDR data limitations",
+          "timestamp": "2025-02-11T23:38:54+01:00",
+          "tree_id": "5805025e409be28ba0534a211282104b1edbd2d9",
+          "url": "https://github.com/unicode-org/icu/commit/030c79fe8805ce6a410b0bf0974297aec41d64c5"
+        },
+        "date": 1739341459029,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 558.0510514477521,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
