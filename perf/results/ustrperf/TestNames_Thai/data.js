83114ae8a06b5c7cf1a8ec0afe38e690fbce0343
github-action-benchmark <github@users.noreply.github.com>
2025-02-13 08:28:33 +0000

    add Benchmark (ndjson) benchmark result for 83726260ef3f42063984fbf693ea3ad34bd387fa

--- perf/results/ustrperf/TestNames_Thai/data.js
+++ perf/results/ustrperf/TestNames_Thai/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1739410554671,
+  "lastUpdate": 1739435313414,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -110109,6 +110109,102 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestScan2",
             "value": 39.855,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "roubert@google.com",
+            "name": "Fredrik Roubert",
+            "username": "roubert"
+          },
+          "committer": {
+            "email": "fredrik@roubert.name",
+            "name": "Fredrik Roubert",
+            "username": "roubert"
+          },
+          "distinct": true,
+          "id": "83726260ef3f42063984fbf693ea3ad34bd387fa",
+          "message": "ICU-23031 Reinstate special case for \"-u-va-posix\" lost by ICU-22520.\n\nInside of locimp_forLanguageTag() in _appendKeywords() in uloc_tag.cpp\nthere's a hardcoded special case for \"-u-va-posix\" which appends the\n\"_POSIX\" variant but this was missed during the refactoring made for\nICU-22520 (there isn't any test case that covers this).\n\nSo the call to locimp_forLanguageTag() did more than previously\nunderstood, but we still don't want to have to call that for every\nlanguage tag that has BCP-47 extensions just in order to get to this\nspecial case. Instead, add a special case also to ulocimp_getSubtags().\n\nFor this to work nicely, the loop in _getVariant() that copies variants\nneeds to be refactored so that it easily can break when encountering the\nstart of any BCP-47 extension (which also has the welcome side-effect of\nmaking it more efficient, being able to append an entire variant at once\nto the output sink).\n\nThis was broken by commit 678d5c127373d699ac4f1be55f8c017aeb0493d5.",
+          "timestamp": "2025-02-13T08:50:17+01:00",
+          "tree_id": "2bef03ba62a8a1ec809ad75e8573269ef26bfd0a",
+          "url": "https://github.com/unicode-org/icu/commit/83726260ef3f42063984fbf693ea3ad34bd387fa"
+        },
+        "date": 1739435085412,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestCtor",
+            "value": 19.6006,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestCtor1",
+            "value": 20.2994,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestCtor2",
+            "value": 23.6702,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestCtor3",
+            "value": 31.052,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestAssign",
+            "value": 42.3826,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestAssign1",
+            "value": 35.8872,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestAssign2",
+            "value": 29.0313,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestGetch",
+            "value": 16.8131,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestCatenate",
+            "value": 98.2636,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestScan",
+            "value": 22.0832,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestScan1",
+            "value": 36.21,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestScan2",
+            "value": 38.7849,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
