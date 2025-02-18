66c6771a03487d3e7ec50af000d8df89381cc237
sven-oly <cwcornelius@gmail.com>
2025-02-18 21:29:35 +0000

    ICU-22922 Update ICU4J status
    See #3398

--- icu4j/main/core/src/main/java/com/ibm/icu/util/MeasureUnit.java
+++ icu4j/main/core/src/main/java/com/ibm/icu/util/MeasureUnit.java
@@ -128,14 +128,14 @@ public class MeasureUnit implements Serializable {
         /**
          * SI prefix: quetta, 10^30.
          *
-         * @draft ICU 75
+         * @stable ICU 75
          */
         QUETTA(30, "quetta", 10),
 
         /**
          * SI prefix: ronna, 10^27.
          *
-         * @draft ICU 75
+         * @stable ICU 75
          */
         RONNA(27, "ronna", 10),
 
@@ -289,14 +289,14 @@ public class MeasureUnit implements Serializable {
         /**
          * SI prefix: ronto, 10^-27.
          *
-         * @draft ICU 75
+         * @stable ICU 75
          */
         RONTO(-27, "ronto", 10),
 
         /**
          * SI prefix: quecto, 10^-30.
          *
-         * @draft ICU 75
+         * @stable ICU 75
          */
         QUECTO(-30, "quecto", 10),
 
