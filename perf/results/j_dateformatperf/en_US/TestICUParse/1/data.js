66100fbca8d48851c3d916a546573612a43f384d
github-action-benchmark <github@users.noreply.github.com>
2025-02-19 22:47:34 +0000

    add Benchmark (ndjson) benchmark result for 215131c1a4f41953f2024cf93b261b0dea180596

--- perf/results/j_dateformatperf/en_US/TestICUParse/1/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUParse/1/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1739992330066,
+  "lastUpdate": 1740005253902,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -32457,6 +32457,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 365.4629206699103,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "egg.robin.leroy@gmail.com",
+            "name": "Robin Leroy",
+            "username": "eggrobin"
+          },
+          "committer": {
+            "email": "egg.robin.leroy@gmail.com",
+            "name": "Robin Leroy",
+            "username": "eggrobin"
+          },
+          "distinct": true,
+          "id": "215131c1a4f41953f2024cf93b261b0dea180596",
+          "message": "ICU-22984 Remove some obnoxious tests from 2003",
+          "timestamp": "2025-02-19T23:30:34+01:00",
+          "tree_id": "f04500f6c151e9c0938c00570f6fa2ae12c50d8b",
+          "url": "https://github.com/unicode-org/icu/commit/215131c1a4f41953f2024cf93b261b0dea180596"
+        },
+        "date": 1740005011635,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 361.1751251046238,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
