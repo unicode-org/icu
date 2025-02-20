46b8ca4e83737db21e3f529f5ed815764c38b030
github-action-benchmark <github@users.noreply.github.com>
2025-02-20 12:31:48 +0000

    add Benchmark (ndjson) benchmark result for b42e7250f0bde441b7d0f1a727d5a33aef629647

--- perf/results/j_normperf/TestNames_Chinese/TestICU_NFC_Orig_Text/data.js
+++ perf/results/j_normperf/TestNames_Chinese/TestICU_NFC_Orig_Text/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1740013626282,
+  "lastUpdate": 1740054708380,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -32547,6 +32547,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFC_Orig_Text",
             "value": 4.787357092593609,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "cwcornelius@gmail.com",
+            "name": "sven-oly",
+            "username": "sven-oly"
+          },
+          "committer": {
+            "email": "cwcornelius@gmail.com",
+            "name": "Craig Cornelius",
+            "username": "sven-oly"
+          },
+          "distinct": true,
+          "id": "b42e7250f0bde441b7d0f1a727d5a33aef629647",
+          "message": "ICU-22922 ICU4J RC APIChangeReport.html",
+          "timestamp": "2025-02-19T16:47:37-08:00",
+          "tree_id": "655025588c05987e7b3ffb60ecf5d7629c16ea56",
+          "url": "https://github.com/unicode-org/icu/commit/b42e7250f0bde441b7d0f1a727d5a33aef629647"
+        },
+        "date": 1740054469322,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFC_Orig_Text",
+            "value": 4.789704146546465,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
