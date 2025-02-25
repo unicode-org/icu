24e2ea5d7de38b39a7221d599ddd7e6856042acd
github-action-benchmark <github@users.noreply.github.com>
2025-02-25 21:21:40 +0000

    add Benchmark (ndjson) benchmark result for 0748442ed6764b4debe456d922c5fa67f9b88bb8

--- perf/results/ustrperf/TestNames_Japanese_k/data.js
+++ perf/results/ustrperf/TestNames_Japanese_k/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1740432727853,
+  "lastUpdate": 1740518500010,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -112221,6 +112221,102 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestScan2",
             "value": 39.0437,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tjc@igalia.com",
+            "name": "Tim Chevalier",
+            "username": "catamorphism"
+          },
+          "committer": {
+            "email": "tjc@igalia.com",
+            "name": "Tim Chevalier",
+            "username": "catamorphism"
+          },
+          "distinct": true,
+          "id": "0748442ed6764b4debe456d922c5fa67f9b88bb8",
+          "message": "ICU-23059 ICU4C MF2: Update spec tests\n\nThis updates the MF2 spec tests to https://github.com/unicode-org/message-format-wg/commit/943479b6022993bb1713ab028cbe34f9e6f0b79f with the following exceptions:\n\n- functions/currency.json and functions/math.json are omitted because these are not yet implemented\n\n- bidi.json will be handled in a future PR\n\n- u-options.json will be handled in a future PR\n\nChanges include:\n* `:integer` now returns a value encapsulating the rounded numeric value of the argument, rather than\n  the value itself.\n* Fallbacks are handled according to the current spec.\n* Fallback values are not passed into functions.\n* Characters inside literal fallbacks are properly escaped.\n* The test runner skips null values properly.\n* The test runner handles boolean `expErrors` in defaultTestProperties.\n* `:string` normalizes its input and normalizeNFC() has been refactored so it can be called there.",
+          "timestamp": "2025-02-25T12:24:07-08:00",
+          "tree_id": "d25f57d653eb30e578d306566c89b6a78d27db0d",
+          "url": "https://github.com/unicode-org/icu/commit/0748442ed6764b4debe456d922c5fa67f9b88bb8"
+        },
+        "date": 1740518253846,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestCtor",
+            "value": 18.9665,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestCtor1",
+            "value": 21.1226,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestCtor2",
+            "value": 24.5278,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestCtor3",
+            "value": 28.2498,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestAssign",
+            "value": 39.1122,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestAssign1",
+            "value": 35.9207,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestAssign2",
+            "value": 26.9198,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestGetch",
+            "value": 15.9611,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestCatenate",
+            "value": 87.5733,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestScan",
+            "value": 21.5291,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestScan1",
+            "value": 36.5153,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestScan2",
+            "value": 38.7964,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
