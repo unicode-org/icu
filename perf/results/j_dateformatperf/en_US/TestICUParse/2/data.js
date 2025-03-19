9f72eccf6c0aa6245e96c7281c1265446742c0e3
github-action-benchmark <github@users.noreply.github.com>
2025-03-19 23:43:22 +0000

    add Benchmark (ndjson) benchmark result for b65650bc5f4344ec4da3f0ae2552607d65c551fa

--- perf/results/j_dateformatperf/en_US/TestICUParse/2/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUParse/2/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1742418272667,
+  "lastUpdate": 1742427801873,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -32937,6 +32937,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 140.8142442861603,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ftang@chromium.org",
+            "name": "Frank Tang",
+            "username": "FrankYFTang"
+          },
+          "committer": {
+            "email": "ftang@google.com",
+            "name": "Frank Yung-Fong Tang",
+            "username": "FrankYFTang"
+          },
+          "distinct": true,
+          "id": "b65650bc5f4344ec4da3f0ae2552607d65c551fa",
+          "message": "ICU-23077 Correct the internalGet call inside Calendar\n\nSee #3439",
+          "timestamp": "2025-03-19T16:22:43-07:00",
+          "tree_id": "eee23315376bfb94e1d9e07a6538de417fb15591",
+          "url": "https://github.com/unicode-org/icu/commit/b65650bc5f4344ec4da3f0ae2552607d65c551fa"
+        },
+        "date": 1742427561032,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 127.91473681097516,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
