f07056bb012f6db35d8b2723d2148953425a70b8
github-action-benchmark <github@users.noreply.github.com>
2025-03-25 00:34:30 +0000

    add Benchmark (ndjson) benchmark result for f6894e28d2d9ecb4f663bd1a1427c89d5bc96806

--- perf/results/j_dateformatperf/en_US/TestICUParse/3/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUParse/3/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1742859885642,
+  "lastUpdate": 1742862870066,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -33327,6 +33327,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 876.9504724022083,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vichang@google.com",
+            "name": "Victor Chang",
+            "username": "gvictor"
+          },
+          "committer": {
+            "email": "ftang@google.com",
+            "name": "Frank Yung-Fong Tang",
+            "username": "FrankYFTang"
+          },
+          "distinct": true,
+          "id": "f6894e28d2d9ecb4f663bd1a1427c89d5bc96806",
+          "message": "ICU-23083 Optimize LSR loaded from langinfo.res\n\nIt uses string pools to reduce the memory impact",
+          "timestamp": "2025-03-24T17:26:46-07:00",
+          "tree_id": "73945588d1a635300a71c772a4a447a61d0f6a90",
+          "url": "https://github.com/unicode-org/icu/commit/f6894e28d2d9ecb4f663bd1a1427c89d5bc96806"
+        },
+        "date": 1742862549918,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 851.5972090200959,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
