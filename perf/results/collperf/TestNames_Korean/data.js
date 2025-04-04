a76d1533ff1c32f02308aa8236608a94dc512061
github-action-benchmark <github@users.noreply.github.com>
2025-04-04 21:05:20 +0000

    add Benchmark (ndjson) benchmark result for 572d03f85af47f62057ea3f7c069254a4737d1b7

--- perf/results/collperf/TestNames_Korean/data.js
+++ perf/results/collperf/TestNames_Korean/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1743794552298,
+  "lastUpdate": 1743800720057,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -65229,6 +65229,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestIcu_BinarySearch_usekey",
             "value": 12715546.9259,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ftang@chromium.org",
+            "name": "Frank Tang",
+            "username": "FrankYFTang"
+          },
+          "committer": {
+            "email": "ftang@google.com",
+            "name": "Frank Yung-Fong Tang",
+            "username": "FrankYFTang"
+          },
+          "distinct": true,
+          "id": "572d03f85af47f62057ea3f7c069254a4737d1b7",
+          "message": "ICU-23052 Fix addLikelySubtags\n\nRemove hack in the conversion tool by fixing the code\n\nICU-23052 Assert the value will never be 0",
+          "timestamp": "2025-04-04T13:36:14-07:00",
+          "tree_id": "8f423d116d57bfd4f30454e02fc2ea27603f01c6",
+          "url": "https://github.com/unicode-org/icu/commit/572d03f85af47f62057ea3f7c069254a4737d1b7"
+        },
+        "date": 1743800470087,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestIcu_KeyGen_null",
+            "value": 218.9789,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_qsort_strcoll_null",
+            "value": 56192885.6239,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_qsort_usekey",
+            "value": 12085072.6962,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_BinarySearch_strcoll_null",
+            "value": 52126532.1308,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_BinarySearch_usekey",
+            "value": 12680757.2614,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
