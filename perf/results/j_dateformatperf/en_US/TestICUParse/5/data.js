23415bdb8eeba4eeef06e8f1cec42f2f074789e7
github-action-benchmark <github@users.noreply.github.com>
2025-04-15 18:57:22 +0000

    add Benchmark (ndjson) benchmark result for 14d69b02ecf9eb90fbe1c9d073b019ec59d1ba33

--- perf/results/j_dateformatperf/en_US/TestICUParse/5/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUParse/5/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744672195322,
+  "lastUpdate": 1744743442719,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -33627,6 +33627,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 363.0843853347309,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "distinct": true,
+          "id": "14d69b02ecf9eb90fbe1c9d073b019ec59d1ba33",
+          "message": "ICU-23048 Remove links to LFS in CI\n\nSee #3474",
+          "timestamp": "2025-04-15T11:28:16-07:00",
+          "tree_id": "d375e31d1932eb1ece7b16d0decad94843848d75",
+          "url": "https://github.com/unicode-org/icu/commit/14d69b02ecf9eb90fbe1c9d073b019ec59d1ba33"
+        },
+        "date": 1744743188030,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 356.75081739942635,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
