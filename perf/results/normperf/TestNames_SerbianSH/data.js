126adcae74a225d2215fa149d94d97e4446e528f
github-action-benchmark <github@users.noreply.github.com>
2025-05-28 14:25:08 +0000

    add Benchmark (ndjson) benchmark result for 55c7cea9ca4bac34f012748867d49032de638690

--- perf/results/normperf/TestNames_SerbianSH/data.js
+++ perf/results/normperf/TestNames_SerbianSH/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1748436059283,
+  "lastUpdate": 1748442307952,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -74757,6 +74757,66 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFD_Orig_Text",
             "value": 7.4656,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "roubert@google.com",
+            "name": "Fredrik Roubert",
+            "username": "roubert"
+          },
+          "committer": {
+            "email": "fredrik@roubert.name",
+            "name": "Fredrik Roubert",
+            "username": "roubert"
+          },
+          "distinct": true,
+          "id": "55c7cea9ca4bac34f012748867d49032de638690",
+          "message": "ICU-23128 Don't clobber UErrorCode on expected U_BUFFER_OVERFLOW_ERROR.\n\nWhen it's expected that a function call might result in setting\nU_BUFFER_OVERFLOW_ERROR upon which a new buffer is allocated and the\nfunction call is retried, this should upon success not overwrite any\nalready set value of UErrorCode, as this could accidentally overwrite\nsome valid and meaningful value when this was other than U_ZERO_ERROR.",
+          "timestamp": "2025-05-28T15:47:03+02:00",
+          "tree_id": "c5ebbfb455c655f34263152bb2cef3386130dcb8",
+          "url": "https://github.com/unicode-org/icu/commit/55c7cea9ca4bac34f012748867d49032de638690"
+        },
+        "date": 1748442048263,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFC_NFD_Text",
+            "value": 8.6674,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFC_NFC_Text",
+            "value": 4.9639,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFC_Orig_Text",
+            "value": 5.0188,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFD_NFD_Text",
+            "value": 6.4336,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFD_NFC_Text",
+            "value": 7.0097,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFD_Orig_Text",
+            "value": 7.0868,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
