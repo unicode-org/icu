efe7fdd70879bd2e7715f1b699e81b964e213152
Mihai Nita <nmihai_2000@yahoo.com>
2025-06-02 21:53:48 +0000

    ICU-12717 Improve errorprone reporting

--- icu4j/tools/build/src/main/java/com/ibm/icu/dev/tool/errorprone/ErrorProneUtils.java
+++ icu4j/tools/build/src/main/java/com/ibm/icu/dev/tool/errorprone/ErrorProneUtils.java
@@ -6,8 +6,8 @@ package com.ibm.icu.dev.tool.errorprone;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
-import com.google.common.collect.ImmutableBiMap;
 import com.google.errorprone.BugCheckerInfo;
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.scanner.BuiltInCheckerSuppliers;
@@ -32,7 +32,7 @@ class ErrorProneUtils {
      * So to "get back" the proper severity level we depend on the errorprone library,
      * get all known errors, and then use the `defaultSeverity` field within each `BugCheckerInfo`.
      */
-    private static final ImmutableBiMap<String, BugCheckerInfo> KNOWN_ERRORS =
+    private static final Map<String, BugCheckerInfo> KNOWN_ERRORS =
             BuiltInCheckerSuppliers.allChecks().getAllChecks();
     static final String SEVERITY_UNKNOWN = "UNKNOWN";
     // Some special severity classes, to group what ICU considers important to fix.
@@ -55,10 +55,46 @@ class ErrorProneUtils {
             BugPattern.SeverityLevel.SUGGESTION.toString());
 
     // A special severity class, where we show first what ICU considers important to fix.
-    static final Map<String, String> ICU_SPECIAL_SEVERITIES = Map.of(
+    static final Map<String, String> ICU_SPECIAL_SEVERITIES = Map.ofEntries(
             // Example:
-            // "MissingFail", SEVERITY_ICU_PRI1,
-            // "ReferenceEquality", SEVERITY_ICU_PRI2
+            // Map.entry("MissingFail", SEVERITY_ICU_PRI1),
+            // Map.entry("ReferenceEquality", SEVERITY_ICU_PRI2)
+            Map.entry("DefaultCharset", SEVERITY_ICU_PRI1),
+            Map.entry("Finally", SEVERITY_ICU_PRI1),
+            Map.entry("InvalidBlockTag", SEVERITY_ICU_PRI1),
+            Map.entry("JdkObsolete", SEVERITY_ICU_PRI1),
+            Map.entry("MissingFail", SEVERITY_ICU_PRI1),
+            Map.entry("MutablePublicArray", SEVERITY_ICU_PRI1),
+            Map.entry("ObjectToString", SEVERITY_ICU_PRI1),
+            Map.entry("OperatorPrecedence", SEVERITY_ICU_PRI1),
+            Map.entry("ReferenceEquality", SEVERITY_ICU_PRI1),
+            Map.entry("StringSplitter", SEVERITY_ICU_PRI1),
+            Map.entry("SynchronizeOnNonFinalField", SEVERITY_ICU_PRI1),
+            Map.entry("UnicodeEscape", SEVERITY_ICU_PRI1),
+            Map.entry("UnusedMethod", SEVERITY_ICU_PRI1),
+            Map.entry("UnusedVariable", SEVERITY_ICU_PRI1),
+            Map.entry("AlmostJavadoc", SEVERITY_ICU_PRI2),
+            Map.entry("BadImport", SEVERITY_ICU_PRI2),
+            Map.entry("ClassCanBeStatic", SEVERITY_ICU_PRI2),
+            Map.entry("EmptyCatch", SEVERITY_ICU_PRI2),
+            Map.entry("EqualsGetClass", SEVERITY_ICU_PRI2),
+            Map.entry("EqualsUnsafeCast", SEVERITY_ICU_PRI2),
+            Map.entry("FallThrough", SEVERITY_ICU_PRI2),
+            Map.entry("Finalize", SEVERITY_ICU_PRI2),
+            Map.entry("FloatingPointLiteralPrecision", SEVERITY_ICU_PRI2),
+            Map.entry("IncrementInForLoopAndHeader", SEVERITY_ICU_PRI2),
+            Map.entry("JavaUtilDate", SEVERITY_ICU_PRI2),
+            Map.entry("LabelledBreakTarget", SEVERITY_ICU_PRI2),
+            Map.entry("LockOnNonEnclosingClassLiteral", SEVERITY_ICU_PRI2),
+            Map.entry("NarrowCalculation", SEVERITY_ICU_PRI2),
+            Map.entry("NarrowingCompoundAssignment", SEVERITY_ICU_PRI2),
+            Map.entry("ShortCircuitBoolean", SEVERITY_ICU_PRI2),
+            Map.entry("StaticAssignmentInConstructor", SEVERITY_ICU_PRI2),
+            Map.entry("StringCaseLocaleUsage", SEVERITY_ICU_PRI2),
+            Map.entry("StringCharset", SEVERITY_ICU_PRI2),
+            Map.entry("UndefinedEquals", SEVERITY_ICU_PRI2),
+            Map.entry("UnnecessaryStringBuilder", SEVERITY_ICU_PRI2),
+            Map.entry("UnsynchronizedOverridesSynchronized", SEVERITY_ICU_PRI2)
     );
 
     /**
@@ -89,5 +125,21 @@ class ErrorProneUtils {
     static String getUrl(String errorType) {
         BugCheckerInfo found = KNOWN_ERRORS.get(errorType);
         return found == null ? null : found.linkUrl();
+    }
+
+    /**
+     * Given an error type (for example `BadImport`, `UnusedVariable`)
+     * it returns the tags associtated with it (`FragileCode`, `Style`, etc.)
+     *
+     * @param errorType the error type, as reported by errorprone
+     * @return the url to a public explanation page
+     */
+    static String getTags(String errorType) {
+        BugCheckerInfo found = KNOWN_ERRORS.get(errorType);
+        if (found == null) {
+            return null;
+        }
+        Set<String> tags = found.getTags();
+        return tags.isEmpty() ? null : tags.toString();
     }
 }
