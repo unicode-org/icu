cb7a258d60a72ec1c4aaa82929e61d5c4297eba0
github-action-benchmark <github@users.noreply.github.com>
2025-06-05 21:56:49 +0000

    add Benchmark (ndjson) benchmark result for af17808838dba659a168021818dee359ec1cc7fd

--- perf/results/normperf/TestNames_Asian/data.js
+++ perf/results/normperf/TestNames_Asian/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1749155949883,
+  "lastUpdate": 1749160609427,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -75297,6 +75297,66 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFD_Orig_Text",
             "value": 11.9803,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tjc@igalia.com",
+            "name": "Tim Chevalier",
+            "username": "catamorphism"
+          },
+          "committer": {
+            "email": "tjc@igalia.com",
+            "name": "Tim Chevalier",
+            "username": "catamorphism"
+          },
+          "distinct": true,
+          "id": "af17808838dba659a168021818dee359ec1cc7fd",
+          "message": "ICU-22763 MF2: Handle time zones correctly in :datetime\n\nAdds a new `DateInfo` struct, which is now the return type of `Formattable::getDate()`. This struct pairs a raw timestamp with a time zone string.",
+          "timestamp": "2025-06-05T14:24:58-07:00",
+          "tree_id": "8f14c1cac90f6e88e7aae6d17ad74b7849f6c099",
+          "url": "https://github.com/unicode-org/icu/commit/af17808838dba659a168021818dee359ec1cc7fd"
+        },
+        "date": 1749160334056,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFC_NFD_Text",
+            "value": 13.6842,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFC_NFC_Text",
+            "value": 10.3745,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFC_Orig_Text",
+            "value": 10.3603,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFD_NFD_Text",
+            "value": 11.2656,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFD_NFC_Text",
+            "value": 12.0166,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestICU_NFD_Orig_Text",
+            "value": 12.0141,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
