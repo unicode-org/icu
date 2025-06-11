1a926bb544b75f8f8638e347fbceee2ac4aa7319
github-action-benchmark <github@users.noreply.github.com>
2025-06-11 14:59:48 +0000

    add Benchmark (ndjson) benchmark result for 0b253b86acecb862d6be75d98e50d0a5282794e9

--- perf/results/j_dateformatperf/en_US/TestICUParse/4/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUParse/4/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1749494448365,
+  "lastUpdate": 1749653987995,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -35007,6 +35007,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 791.5386764358398,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "egg.robin.leroy@gmail.com",
+            "name": "Robin Leroy",
+            "username": "eggrobin"
+          },
+          "committer": {
+            "email": "egg.robin.leroy@gmail.com",
+            "name": "Robin Leroy",
+            "username": "eggrobin"
+          },
+          "distinct": true,
+          "id": "0b253b86acecb862d6be75d98e50d0a5282794e9",
+          "message": "ICU-23132 Include the source files, not their copies",
+          "timestamp": "2025-06-11T16:42:11+02:00",
+          "tree_id": "71361b981c662a5a870027847675427f733e656c",
+          "url": "https://github.com/unicode-org/icu/commit/0b253b86acecb862d6be75d98e50d0a5282794e9"
+        },
+        "date": 1749653717756,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 787.8831175866094,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
