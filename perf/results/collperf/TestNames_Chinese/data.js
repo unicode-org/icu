4ea4930a2f2304baa370c68da26021a8799c22f8
github-action-benchmark <github@users.noreply.github.com>
2025-06-17 23:08:09 +0000

    add Benchmark (ndjson) benchmark result for aef7ad283d5e64ba2c7172f03873da526ddacad5

--- perf/results/collperf/TestNames_Chinese/data.js
+++ perf/results/collperf/TestNames_Chinese/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750090786882,
+  "lastUpdate": 1750201689193,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -68361,6 +68361,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestIcu_BinarySearch_usekey",
             "value": 2123077.7282,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "egg.robin.leroy@gmail.com",
+            "name": "Robin Leroy",
+            "username": "eggrobin"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "aef7ad283d5e64ba2c7172f03873da526ddacad5",
+          "message": "ICU-23054 Make the MSVC stdcpplatest build work as intended",
+          "timestamp": "2025-06-17T15:37:32-07:00",
+          "tree_id": "989fbc2c9c6e7bfb3fb2459314a293c68f1e2a07",
+          "url": "https://github.com/unicode-org/icu/commit/aef7ad283d5e64ba2c7172f03873da526ddacad5"
+        },
+        "date": 1750201425677,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestIcu_KeyGen_null",
+            "value": 160.1923,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_qsort_strcoll_null",
+            "value": 8731908.665,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_qsort_usekey",
+            "value": 2117832.8292,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_BinarySearch_strcoll_null",
+            "value": 7704787.8749,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_BinarySearch_usekey",
+            "value": 2089197.9946,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
