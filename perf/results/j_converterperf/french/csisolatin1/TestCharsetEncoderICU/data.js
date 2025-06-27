2d7da908c2ff1e3ef70123b8efd424d2e967d1ba
github-action-benchmark <github@users.noreply.github.com>
2025-06-27 22:00:54 +0000

    add Benchmark (ndjson) benchmark result for 88db9958d9db5e4b496dc9106fb8e85910d9d6e2

--- perf/results/j_converterperf/french/csisolatin1/TestCharsetEncoderICU/data.js
+++ perf/results/j_converterperf/french/csisolatin1/TestCharsetEncoderICU/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750974148381,
+  "lastUpdate": 1751061653996,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -35577,6 +35577,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestCharsetEncoderICU",
             "value": 0.4372915253464733,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "distinct": true,
+          "id": "88db9958d9db5e4b496dc9106fb8e85910d9d6e2",
+          "message": "ICU-23148 Migrate ICU4J publishing from Legacy OSSRH to Central Portal",
+          "timestamp": "2025-06-27T14:34:58-07:00",
+          "tree_id": "b5a02a144d3be02e5f473756d61d2464e6413fb0",
+          "url": "https://github.com/unicode-org/icu/commit/88db9958d9db5e4b496dc9106fb8e85910d9d6e2"
+        },
+        "date": 1751061375675,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestCharsetEncoderICU",
+            "value": 0.4366663855335677,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
