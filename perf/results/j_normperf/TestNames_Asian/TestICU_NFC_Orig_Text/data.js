4e8da3d1924b21949541b18b39070afa48e9012c
github-action-benchmark <github@users.noreply.github.com>
2025-07-01 22:00:17 +0000

    add Benchmark (ndjson) benchmark result for cb40bf3afb70b2a67f5bcd6b0aed0e66f7640938

--- perf/results/j_normperf/TestNames_Asian/TestICU_NFC_Orig_Text/data.js
+++ perf/results/j_normperf/TestNames_Asian/TestICU_NFC_Orig_Text/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1751323567728,
+  "lastUpdate": 1751407216950,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -35607,6 +35607,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFC_Orig_Text",
             "value": 3.123411834197135,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "cb40bf3afb70b2a67f5bcd6b0aed0e66f7640938",
+          "message": "ICU-23004 utfiterator.h include <range>",
+          "timestamp": "2025-07-01T21:43:54Z",
+          "tree_id": "aba8778349ded73de867ec01dc1d6ba1a97df53a",
+          "url": "https://github.com/unicode-org/icu/commit/cb40bf3afb70b2a67f5bcd6b0aed0e66f7640938"
+        },
+        "date": 1751406939378,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFC_Orig_Text",
+            "value": 3.2957043054636754,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
