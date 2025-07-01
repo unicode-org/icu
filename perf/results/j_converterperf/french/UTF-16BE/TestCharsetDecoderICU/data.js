51f3ff1f6d6988728afd9c5f4b95c2b8a28f0c4f
github-action-benchmark <github@users.noreply.github.com>
2025-07-01 22:12:15 +0000

    add Benchmark (ndjson) benchmark result for cb40bf3afb70b2a67f5bcd6b0aed0e66f7640938

--- perf/results/j_converterperf/french/UTF-16BE/TestCharsetDecoderICU/data.js
+++ perf/results/j_converterperf/french/UTF-16BE/TestCharsetDecoderICU/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1751324038370,
+  "lastUpdate": 1751407935869,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -35247,6 +35247,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestCharsetDecoderICU",
             "value": 3.8535141430561253,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "cb40bf3afb70b2a67f5bcd6b0aed0e66f7640938",
+          "message": "ICU-23004 utfiterator.h include <range>",
+          "timestamp": "2025-07-01T21:43:54Z",
+          "tree_id": "aba8778349ded73de867ec01dc1d6ba1a97df53a",
+          "url": "https://github.com/unicode-org/icu/commit/cb40bf3afb70b2a67f5bcd6b0aed0e66f7640938"
+        },
+        "date": 1751407656856,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestCharsetDecoderICU",
+            "value": 3.850155875668742,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
