19bb47e30497c914ddc6707d59bdeae39b981c86
github-action-benchmark <github@users.noreply.github.com>
2025-07-09 13:14:41 +0000

    add Benchmark (ndjson) benchmark result for d5bb6d0a266a9fdbdec61a877ab8d76aaa1a515a

--- perf/results/j_dateformatperf/en_US/TestICUFormat/2/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUFormat/2/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1752064545875,
+  "lastUpdate": 1752066881583,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -35547,6 +35547,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 192.51145276571577,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "distinct": true,
+          "id": "d5bb6d0a266a9fdbdec61a877ab8d76aaa1a515a",
+          "message": "ICU-23140 Use covariant return types in clone() in ICU4J",
+          "timestamp": "2025-07-08T14:27:35-07:00",
+          "tree_id": "e33593dcc7d179a487c5da5540981f7c7193c64b",
+          "url": "https://github.com/unicode-org/icu/commit/d5bb6d0a266a9fdbdec61a877ab8d76aaa1a515a"
+        },
+        "date": 1752066599429,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 169.83082892596457,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
