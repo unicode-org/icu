f84cc094b578222bf3530a82c67de8c0b0273d31
github-action-benchmark <github@users.noreply.github.com>
2025-07-09 21:54:22 +0000

    add Benchmark (ndjson) benchmark result for 6f069854938a87be7562570be99f22ce128d5845

--- perf/results/collperf/TestNames_Thai/data.js
+++ perf/results/collperf/TestNames_Thai/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1752067889224,
+  "lastUpdate": 1752098062523,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -69117,6 +69117,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestIcu_BinarySearch_usekey",
             "value": 2113257.2033,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "distinct": true,
+          "id": "6f069854938a87be7562570be99f22ce128d5845",
+          "message": "ICU-23140 Add PMD checking",
+          "timestamp": "2025-07-09T14:28:07-07:00",
+          "tree_id": "2f2991a342b1f3b85005efa7509fb3312b72b7c0",
+          "url": "https://github.com/unicode-org/icu/commit/6f069854938a87be7562570be99f22ce128d5845"
+        },
+        "date": 1752097788758,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestIcu_KeyGen_null",
+            "value": 376.1398,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_qsort_strcoll_null",
+            "value": 12563055.0481,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_qsort_usekey",
+            "value": 2113980.3932,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_BinarySearch_strcoll_null",
+            "value": 11282178.435,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "TestIcu_BinarySearch_usekey",
+            "value": 2033426.8508,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
