3cc172288d0417f899266a85fc0f073b779b2c5b
github-action-benchmark <github@users.noreply.github.com>
2025-07-14 18:57:05 +0000

    add Benchmark (ndjson) benchmark result for 8914869ba2e1b0d222ac2c3ce602b4feee5cc472

--- perf/results/j_converterperf/arabic/UTF-8/TestCharsetDecoderICU/data.js
+++ perf/results/j_converterperf/arabic/UTF-8/TestCharsetDecoderICU/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1752297646333,
+  "lastUpdate": 1752519424996,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -35697,6 +35697,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestCharsetDecoderICU",
             "value": 1.4011389624284354,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "distinct": true,
+          "id": "8914869ba2e1b0d222ac2c3ce602b4feee5cc472",
+          "message": "ICU-12717 Generate errorprone reports as markup and tab-separated-values",
+          "timestamp": "2025-07-14T18:42:05Z",
+          "tree_id": "6598b9af044f265301d1448b74b81578bbac2306",
+          "url": "https://github.com/unicode-org/icu/commit/8914869ba2e1b0d222ac2c3ce602b4feee5cc472"
+        },
+        "date": 1752519131426,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestCharsetDecoderICU",
+            "value": 1.4127452471793496,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
