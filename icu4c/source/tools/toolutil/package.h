038489701b9cb8275733a4ee95bd91ef699012c2
Christian Maurer <c.maurer@gmx.at>
2025-11-26 16:19:06 +0100

    ICU-23276 Increase toolutil's STRING_STORE_SIZE

--- icu4c/source/tools/toolutil/package.h
+++ icu4c/source/tools/toolutil/package.h
@@ -27,7 +27,7 @@
 
 // .dat package file representation ---------------------------------------- ***
 
-#define STRING_STORE_SIZE 100000
+#define STRING_STORE_SIZE 300000
 #define MAX_PKG_NAME_LENGTH 64
 
 typedef void CheckDependency(void *context, const char *itemName, const char *targetName);
