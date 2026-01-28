e58a18bab3f8e183b89a0bf63073398d3ee57bab
github-action-benchmark <github@users.noreply.github.com>
2026-01-28 22:24:39 +0000

    add Benchmark (ndjson) benchmark result for 649262a75ecddb15a0e58d71f637a8a32eaabd43

--- perf/results/j_decimalformatperf/de_DE/TestICUParse/data.js
+++ perf/results/j_decimalformatperf/de_DE/TestICUParse/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1769435974813,
+  "lastUpdate": 1769639079483,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -42297,6 +42297,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 302.67724566856805,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "younies.mahmoud@gmail.com",
+            "name": "Younies Mahmoud",
+            "username": "younies"
+          },
+          "committer": {
+            "email": "younies.mahmoud@gmail.com",
+            "name": "Younies Mahmoud",
+            "username": "younies"
+          },
+          "distinct": true,
+          "id": "649262a75ecddb15a0e58d71f637a8a32eaabd43",
+          "message": "ICU-23264 Add validateAndGet function to MeasureUnit for efficient unit validation\n\nSee #3780",
+          "timestamp": "2026-01-28T22:59:49+01:00",
+          "tree_id": "998910101024d810d017a88a55d95d3b113c7155",
+          "url": "https://github.com/unicode-org/icu/commit/649262a75ecddb15a0e58d71f637a8a32eaabd43"
+        },
+        "date": 1769638675510,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 321.02639915346055,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
