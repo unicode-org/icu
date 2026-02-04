fab9f00aed389b4773083866c40cc18132919179
github-action-benchmark <github@users.noreply.github.com>
2026-02-04 22:14:02 +0000

    add Benchmark (ndjson) benchmark result for 0494a6f2740cf82bd844303ff3dbbbceb587d0a0

--- perf/results/j_dateformatperf/en_US/TestICUFormat/4/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUFormat/4/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1769811803014,
+  "lastUpdate": 1770243242058,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -42057,6 +42057,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 246.36384027333347,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "1707552+eranl@users.noreply.github.com",
+            "name": "eranl",
+            "username": "eranl"
+          },
+          "committer": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "distinct": true,
+          "id": "0494a6f2740cf82bd844303ff3dbbbceb587d0a0",
+          "message": "ICU-23292 add `toString` method to `ICUResourceBundle`",
+          "timestamp": "2026-02-04T14:00:41-08:00",
+          "tree_id": "1d59bd9c6b3b126b0e1361d7e9b1dce3564b8678",
+          "url": "https://github.com/unicode-org/icu/commit/0494a6f2740cf82bd844303ff3dbbbceb587d0a0"
+        },
+        "date": 1770242832951,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 238.07086734992433,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
