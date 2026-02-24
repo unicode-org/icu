faedd4d6364815a7dbbe21bfc9ed4c23fde59a80
github-action-benchmark <github@users.noreply.github.com>
2026-02-24 20:04:56 +0000

    add Benchmark (ndjson) benchmark result for 69df400bac006607e080993350bac314b8471375

--- perf/results/j_converterperf/japanese/csiso2022jp/TestCharsetDecoderICU/data.js
+++ perf/results/j_converterperf/japanese/csiso2022jp/TestCharsetDecoderICU/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1771908977408,
+  "lastUpdate": 1771963496135,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -42627,6 +42627,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestCharsetDecoderICU",
             "value": 0.0061576657295991325,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "committer": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "distinct": true,
+          "id": "69df400bac006607e080993350bac314b8471375",
+          "message": "ICU-23303 C++ iter doc: fix navigation",
+          "timestamp": "2026-02-24T11:41:18-08:00",
+          "tree_id": "415732e1d5e1fd1ce6975e04b0faeaca2f84f05e",
+          "url": "https://github.com/unicode-org/icu/commit/69df400bac006607e080993350bac314b8471375"
+        },
+        "date": 1771963092056,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestCharsetDecoderICU",
+            "value": 0.006140555395989951,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
