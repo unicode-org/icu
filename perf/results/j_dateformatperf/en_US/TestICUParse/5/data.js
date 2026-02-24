8a025fd83b7ddd835238675135ab8fee682ab3f5
github-action-benchmark <github@users.noreply.github.com>
2026-02-24 04:38:25 +0000

    add Benchmark (ndjson) benchmark result for 55615ca487806e94c769258d15e7d7b9db065e71

--- perf/results/j_dateformatperf/en_US/TestICUParse/5/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUParse/5/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1771890356754,
+  "lastUpdate": 1771907905319,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -42477,6 +42477,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 325.6427384126462,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "55615ca487806e94c769258d15e7d7b9db065e71",
+          "message": "ICU-23318 fix UTF-8 encoding of idna_conf.txt\n\nalso find & remove nameutf8 ≠ namebase\r\n\r\nSee #3866",
+          "timestamp": "2026-02-24T04:18:39Z",
+          "tree_id": "d5914192144e9a4386d70c6234ed630cb43497fb",
+          "url": "https://github.com/unicode-org/icu/commit/55615ca487806e94c769258d15e7d7b9db065e71"
+        },
+        "date": 1771907502057,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 376.27188028702847,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
