1af6d77cd5fb26bc1e3ba58bc7795abe8bd560cc
github-action-benchmark <github@users.noreply.github.com>
2026-03-09 19:22:27 +0000

    add Benchmark (ndjson) benchmark result for 426cea1b85e82e632dc5c0b35c7d329c0eb4af7b

--- perf/results/j_decimalformatperf/en_US/TestICUFormat/data.js
+++ perf/results/j_decimalformatperf/en_US/TestICUFormat/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773083170541,
+  "lastUpdate": 1773084147289,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -43137,6 +43137,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 308.1658111542146,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "distinct": true,
+          "id": "426cea1b85e82e632dc5c0b35c7d329c0eb4af7b",
+          "message": "ICU-23247 CLDR-to-ICU: using `parallel` sometimes fails on Windows",
+          "timestamp": "2026-03-09T12:12:57-07:00",
+          "tree_id": "260f5cd9c19f6ae4888a391f4eccd1e3039b093c",
+          "url": "https://github.com/unicode-org/icu/commit/426cea1b85e82e632dc5c0b35c7d329c0eb4af7b"
+        },
+        "date": 1773083757618,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 298.895473622594,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
