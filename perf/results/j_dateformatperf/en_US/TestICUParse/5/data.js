731257f54d03a147106a6e3359b459289fe3d464
github-action-benchmark <github@users.noreply.github.com>
2026-03-18 19:46:26 +0000

    add Benchmark (ndjson) benchmark result for 3d9b5752848a99d0e528c97627988475afe04fac

--- perf/results/j_dateformatperf/en_US/TestICUParse/5/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUParse/5/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773843823236,
+  "lastUpdate": 1773863186231,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -43047,6 +43047,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 391.214417331895,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "distinct": true,
+          "id": "3d9b5752848a99d0e528c97627988475afe04fac",
+          "message": "ICU-23248 Further update BRS release tag instructions",
+          "timestamp": "2026-03-18T12:33:52-07:00",
+          "tree_id": "29c993988208d0c42d91c29aed8f213a38d450c9",
+          "url": "https://github.com/unicode-org/icu/commit/3d9b5752848a99d0e528c97627988475afe04fac"
+        },
+        "date": 1773862769649,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 370.3216077059522,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
