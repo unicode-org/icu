16ccd47c6dc1930271e100526919e4a1f5ea417f
github-action-benchmark <github@users.noreply.github.com>
2026-03-18 14:25:43 +0000

    add Benchmark (ndjson) benchmark result for 4ff953c69cf21a8cc33df889c06b698b56cca645

--- perf/results/j_decimalformatperf/de_DE/TestICUFormat/data.js
+++ perf/results/j_decimalformatperf/de_DE/TestICUFormat/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773792316577,
+  "lastUpdate": 1773843943383,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -43287,6 +43287,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 543.9382665169072,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "egg.robin.leroy@gmail.com",
+            "name": "Robin Leroy",
+            "username": "eggrobin"
+          },
+          "committer": {
+            "email": "egg.robin.leroy@gmail.com",
+            "name": "Robin Leroy",
+            "username": "eggrobin"
+          },
+          "distinct": true,
+          "id": "4ff953c69cf21a8cc33df889c06b698b56cca645",
+          "message": "ICU-23247 BRS 78: Bump the runs-on value of all GitHub workflows to the current versioned values corresponding to -latest",
+          "timestamp": "2026-03-18T15:03:16+01:00",
+          "tree_id": "06337c031c683fc4787057051013e63b0345d698",
+          "url": "https://github.com/unicode-org/icu/commit/4ff953c69cf21a8cc33df889c06b698b56cca645"
+        },
+        "date": 1773843509048,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 530.9878865281826,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
