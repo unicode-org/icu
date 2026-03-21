6f57fa8152987d2cfd8dc28ea1f683f26de865ec
Andrew Szeto <aszeto@google.com>
2026-03-20 17:28:37 -0700

    ICU-23321 Add SimpleCache concurrency tests
    These tests exercise concurrent access to SimpleCache (via
    NumberFormat.getInstance and getCurrencyInstance). They should have
    been included in the batch 1 PR (#3863) which replaced
    Collections.synchronizedMap with ConcurrentHashMap, but were
    inadvertently omitted.

---
+++ icu4j/main/core/src/test/java/com/ibm/icu/dev/test/util/CacheConcurrencyTest.java
@@ -0,0 +1,50 @@
+// © 2026 and later: Unicode, Inc. and others.
+// License & terms of use: http://www.unicode.org/copyright.html
+package com.ibm.icu.dev.test.util;
+
+import com.ibm.icu.text.NumberFormat;
+import com.ibm.icu.util.ULocale;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Concurrency regression tests for ICU4J caches: SimpleCache. */
+@RunWith(JUnit4.class)
+public class CacheConcurrencyTest extends ConcurrencyTest {
+
+    @Test
+    public void testSimpleCacheConcurrentPuts() throws Exception {
+        ULocale[] locales = {ULocale.US, ULocale.GERMANY, ULocale.JAPAN, ULocale.FRANCE};
+
+        runConcurrent(
+                "SimpleCachePuts",
+                tid -> {
+                    for (int i = 0; i < ITERATIONS; i++) {
+                        ULocale loc = locales[(tid + i) % locales.length];
+                        NumberFormat nf = NumberFormat.getInstance(loc);
+                        assertNotNull("NumberFormat should not be null", nf);
+                        String result = nf.format(12345.67);
+                        assertFalse(
+                                "Formatted result should not be empty",
+                                result == null || result.isEmpty());
+                    }
+                });
+    }
+
+    @Test
+    public void testSimpleCacheConcurrentReadsAndPuts() throws Exception {
+        ULocale[] locales = {
+            ULocale.US, ULocale.GERMANY, ULocale.JAPAN, ULocale.FRANCE, ULocale.CHINA, ULocale.KOREA
+        };
+
+        runConcurrent(
+                "SimpleCacheReadsAndPuts",
+                tid -> {
+                    for (int i = 0; i < ITERATIONS; i++) {
+                        ULocale loc = locales[(tid + i) % locales.length];
+                        NumberFormat nf = NumberFormat.getCurrencyInstance(loc);
+                        assertNotNull("CurrencyInstance should not be null", nf);
+                    }
+                });
+    }
+}
