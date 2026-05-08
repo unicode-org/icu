fdae75a4f93d11594c506e0aa8fe66b14d794208
Elango Cheran <elango@unicode.org>
2026-05-08 16:50:21 -0700

    ICU-23398 Add test file with TODOs

---
+++ icu4c/source/test/intltest/segmenter_segments_test.cpp
@@ -0,0 +1,4 @@
+// © 2025 and later: Unicode, Inc. and others.
+// License & terms of use: https://www.unicode.org/copyright.html
+
+
---
+++ icu4c/source/test/intltest/segmenter_segments_test.h
@@ -0,0 +1,20 @@
+// © 2025 and later: Unicode, Inc. and others.
+// License & terms of use: https://www.unicode.org/copyright.html
+
+#ifndef SEGMENTER_SEGMENTS_TEST
+#define SEGMENTER_SEGMENTS_TEST
+
+#include "intltest.h"
+
+// TODO: include in RBBI test along with other test classes. copy other test class setup
+
+/**
+ * Segments test
+ */
+class SegmentsTest: public IntlTest {
+public:
+    SegmentsTest();
+    ~SegmentsTest();
+};
+
+#endif
