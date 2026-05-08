8617b867a411cd236c944b54b7384be1049c1063
Elango Cheran <elango@unicode.org>
2026-05-08 14:14:58 -0700

    ICU-23398 Add temporary stub to compile header, configs to compile stub

---
+++ icu4c/source/i18n/segmenter_stub_deleteme.cpp
@@ -0,0 +1,27 @@
+// © 2026 and later: Unicode, Inc. and others.
+// License & terms of use: http://www.unicode.org/copyright.html
+
+#include "unicode/utypes.h"
+
+#if !UCONFIG_NO_FORMATTING
+
+#if U_SHOW_CPLUSPLUS_API
+
+#ifndef U_HIDE_DRAFT_API
+
+#include "segmenter.h"
+#include "segmenter_stub_deleteme.h"
+
+namespace segmenter {  // icu::segmenter
+
+SegmenterStubDeleteme::SegmenterStubDeleteme() : {
+
+}
+
+}  // namespace segmenter
+
+#endif // U_HIDE_DRAFT_API
+
+#endif /* U_SHOW_CPLUSPLUS_API */
+
+#endif /* #if !UCONFIG_NO_FORMATTING */
---
+++ icu4c/source/i18n/segmenter_stub_deleteme.h
@@ -0,0 +1,36 @@
+// © 2026 and later: Unicode, Inc. and others.
+// License & terms of use: http://www.unicode.org/copyright.html
+
+#include "unicode/utypes.h"
+
+#if !UCONFIG_NO_FORMATTING
+
+#if U_SHOW_CPLUSPLUS_API
+
+#ifndef U_HIDE_DRAFT_API
+
+#ifndef __SEGMENTER_STUB_DELETEME_H__
+#define __SEGMENTER_STUB_DELETEME_H__
+
+#include "segmenter.h"
+
+U_NAMESPACE_BEGIN
+namespace segmenter {
+
+    class SegmenterStubDeleteme {
+        public:
+            SegmenterStubDeleteme();
+        
+    };
+
+
+}  // namespace segmenter
+U_NAMESPACE_END
+
+#endif //__SEGMENTER_STUB_DELETEME_H__
+
+#endif // U_HIDE_DRAFT_API
+
+#endif /* U_SHOW_CPLUSPLUS_API */
+
+#endif /* #if !UCONFIG_NO_FORMATTING */
