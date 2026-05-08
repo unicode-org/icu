d8d5957b94402296491438a173c18cbf3a0ca992
Elango Cheran <elango@unicode.org>
2026-05-08 16:49:05 -0700

    ICU-23398 Fix ICU namespace declaration

--- icu4c/source/i18n/unicode/segmenter.h
+++ icu4c/source/i18n/unicode/segmenter.h
@@ -9,25 +9,35 @@
 
 #ifndef U_HIDE_DRAFT_API
 
+// TODO: rewrite file to match brkiter.h
 #ifndef __SEGEMENTER_H__
 #define __SEGEMENTER_H__
 
 #include "unicode/unistr.h"
 
-using namespace icu;
+U_NAMESPACE_BEGIN
 
 namespace segmenter {  // icu::segmenter
 
-class Segmenter {
+class U_COMMON_API_CLASS Segmenter : public UObject {
 public:
+    ~Segmenter() override;
+    // TODO: discuss if we want to take input type of UnicodeString or std::u16string_view
     virtual Segments segment(const UnicodeString &s) = 0;
+
 };
 
 class Segments {
     virtual bool isBoundary(int32_t offset) = 0;
 };
 
+class Segment {
+
+};
+
 }  // namespace segmenter
+
+U_NAMESPACE_END
 
 #endif // __SEGEMENTER_H__
 
