bb3a42d19738c19ece9733dddccae28f23265359
github-action-benchmark <github@users.noreply.github.com>
2026-05-13 17:19:07 +0000

    add Benchmark (ndjson) benchmark result for f3862f69b47ea0cdadf430dcc06946f0140443b8

--- perf/results/j_converterperf/s-chinese/EUC-CN/TestCharsetDecoderICU/data.js
+++ perf/results/j_converterperf/s-chinese/EUC-CN/TestCharsetDecoderICU/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778635736176,
+  "lastUpdate": 1778692747101,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -45027,6 +45027,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestCharsetDecoderICU",
             "value": 0.029335859137109493,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "egg.robin.leroy@gmail.com",
+            "name": "Robin Leroy",
+            "username": "eggrobin"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "f3862f69b47ea0cdadf430dcc06946f0140443b8",
+          "message": "ICU-23247 ICU-TC 2026-02-12: ColumnLimit=100 (not 105)",
+          "timestamp": "2026-05-13T09:50:00-07:00",
+          "tree_id": "0aabaaeb748872d80298510dcd1184ed840a91da",
+          "url": "https://github.com/unicode-org/icu/commit/f3862f69b47ea0cdadf430dcc06946f0140443b8"
+        },
+        "date": 1778692289586,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestCharsetDecoderICU",
+            "value": 0.027282804606023184,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
