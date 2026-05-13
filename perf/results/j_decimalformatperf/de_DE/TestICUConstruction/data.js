1d9e0bc9bbea8a9da3e3ee9f823aa1f49758cb1e
github-action-benchmark <github@users.noreply.github.com>
2026-05-13 17:06:19 +0000

    add Benchmark (ndjson) benchmark result for f3862f69b47ea0cdadf430dcc06946f0140443b8

--- perf/results/j_decimalformatperf/de_DE/TestICUConstruction/data.js
+++ perf/results/j_decimalformatperf/de_DE/TestICUConstruction/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778634631885,
+  "lastUpdate": 1778691979235,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -45147,6 +45147,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUConstruction",
             "value": 326.72115833340825,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "egg.robin.leroy@gmail.com",
+            "name": "Robin Leroy",
+            "username": "eggrobin"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "f3862f69b47ea0cdadf430dcc06946f0140443b8",
+          "message": "ICU-23247 ICU-TC 2026-02-12: ColumnLimit=100 (not 105)",
+          "timestamp": "2026-05-13T09:50:00-07:00",
+          "tree_id": "0aabaaeb748872d80298510dcd1184ed840a91da",
+          "url": "https://github.com/unicode-org/icu/commit/f3862f69b47ea0cdadf430dcc06946f0140443b8"
+        },
+        "date": 1778691510427,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUConstruction",
+            "value": 321.1236861558905,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
