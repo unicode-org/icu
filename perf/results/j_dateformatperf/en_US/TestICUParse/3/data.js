00bd9762ea388728e8585a1734d0ad7990325732
github-action-benchmark <github@users.noreply.github.com>
2026-05-15 16:59:11 +0000

    add Benchmark (ndjson) benchmark result for 957fa1c2af3e251883d886aa1b6c843c1fd95c8c

--- perf/results/j_dateformatperf/en_US/TestICUParse/3/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUParse/3/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778708357574,
+  "lastUpdate": 1778864351372,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -45087,6 +45087,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 904.5284226278111,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "y.umaoka@gmail.com",
+            "name": "Yoshito Umaoka",
+            "username": "yumaoka"
+          },
+          "committer": {
+            "email": "yumaoka@users.noreply.github.com",
+            "name": "Yoshito Umaoka",
+            "username": "yumaoka"
+          },
+          "distinct": true,
+          "id": "957fa1c2af3e251883d886aa1b6c843c1fd95c8c",
+          "message": "ICU-23317 Rounding seconds in metazone mapping date data in cldr-to-icu",
+          "timestamp": "2026-05-15T12:46:01-04:00",
+          "tree_id": "32b3a20a55a5b896a528804f86512a03ba0c710b",
+          "url": "https://github.com/unicode-org/icu/commit/957fa1c2af3e251883d886aa1b6c843c1fd95c8c"
+        },
+        "date": 1778863881749,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 939.313919781953,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
