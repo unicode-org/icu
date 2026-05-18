cf160355cbafd73149a8af4d3b3d8c1a62662f5c
github-action-benchmark <github@users.noreply.github.com>
2026-05-18 15:35:46 +0000

    add Benchmark (ndjson) benchmark result for 01e5b1660bab3b016ef39bc95d379da309ba73cc

--- perf/results/j_dateformatperf/en_US/TestICUFormat/3/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUFormat/3/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779113483330,
+  "lastUpdate": 1779118546764,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -45087,6 +45087,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 230.04887290026568,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "01e5b1660bab3b016ef39bc95d379da309ba73cc",
+          "message": "ICU-23339 Change the MessageFormat.format argument to Map<String,?>",
+          "timestamp": "2026-05-15T15:47:56-07:00",
+          "tree_id": "1a96d2e2c4c689405a8f1a1a0f7f7f890978b8c5",
+          "url": "https://github.com/unicode-org/icu/commit/01e5b1660bab3b016ef39bc95d379da309ba73cc"
+        },
+        "date": 1779118055932,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 216.05721867453335,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
