2784a46d77ee60ce6b7b964ef01634ab78ae0f73
github-action-benchmark <github@users.noreply.github.com>
2026-06-04 20:18:57 +0000

    add Benchmark (ndjson) benchmark result for 8a74ef0a308e395af0710f84f86f79d53aa84d4e

--- perf/results/j_dateformatperf/en_US/TestICUFormat/4/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUFormat/4/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1780596583564,
+  "lastUpdate": 1780604337578,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -45507,6 +45507,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 220.47875065035618,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "committer": {
+            "email": "markus.icu@gmail.com",
+            "name": "Markus Scherer",
+            "username": "markusicu"
+          },
+          "distinct": true,
+          "id": "8a74ef0a308e395af0710f84f86f79d53aa84d4e",
+          "message": "ICU-23421 code point iterators & ranges: add base(), front(), back()",
+          "timestamp": "2026-06-04T12:55:18-07:00",
+          "tree_id": "9c472c3fdf57222d2f2b58493e6d92fe5e65e37b",
+          "url": "https://github.com/unicode-org/icu/commit/8a74ef0a308e395af0710f84f86f79d53aa84d4e"
+        },
+        "date": 1780603865952,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 221.6403104277507,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
