dbce6573e967ee443803ee4ea3c451e6dd8cf4ad
github-action-benchmark <github@users.noreply.github.com>
2026-06-23 03:15:05 +0000

    add Benchmark (ndjson) benchmark result for d5fd603fd451adab0e4cf848bcbda42c58478d03

--- perf/results/j_dateformatperf/en_US/TestICUFormat/5/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUFormat/5/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1782163644450,
+  "lastUpdate": 1782184504604,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -46287,6 +46287,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 246.9087159765462,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "distinct": true,
+          "id": "d5fd603fd451adab0e4cf848bcbda42c58478d03",
+          "message": "ICU-23425 Fix broken links in documentation site",
+          "timestamp": "2026-06-22T20:00:55-07:00",
+          "tree_id": "f8e1685f5d4f6d7f8ae98642da46fee50652c241",
+          "url": "https://github.com/unicode-org/icu/commit/d5fd603fd451adab0e4cf848bcbda42c58478d03"
+        },
+        "date": 1782183869021,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 275.03075433872493,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
