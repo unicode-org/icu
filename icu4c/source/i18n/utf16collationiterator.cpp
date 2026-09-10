e4e47ec843b3579ef403ffaab237bc7631e3d035
Yoshito Umaoka <y.umaoka@gmail.com>
2026-09-10 14:22:07 -0400

    ICU-22511 Fixed an infinite loop problem caused by backward collation code point iteration problem

--- icu4c/source/i18n/utf16collationiterator.cpp
+++ icu4c/source/i18n/utf16collationiterator.cpp
@@ -295,9 +295,21 @@ FCDUTF16CollationIterator::previousCodePoint(UErrorCode &errorCode) {
                 return U_SENTINEL;
             }
             c = *--pos;
-            if(CollationFCD::hasLccc(c)) {
-                if(CollationFCD::maybeTibetanCompositeVowel(c) ||
-                        (pos != start && CollationFCD::hasTccc(*(pos - 1)))) {
+            if (CollationFCD::hasLccc(c)) {
+                UBool normalizePrevSegment = CollationFCD::maybeTibetanCompositeVowel(c);
+                if (!normalizePrevSegment && pos != start) {
+                    if (U16_IS_TRAIL(*(pos - 1))) {
+                        // CollationFCD::hasTccc(<low-surrogate>) always returns false.
+                        // To test possible trailing ccc, we need to check high surrogate
+                        // (or previous character for broken surrogate pair).
+                        if (pos - 1 != start) {
+                            normalizePrevSegment = U16_IS_LEAD(*(pos - 2)) && CollationFCD::hasTccc(*(pos - 2));
+                        }
+                    } else {
+                        normalizePrevSegment = CollationFCD::hasTccc(*(pos - 1));
+                    }
+                }
+                if (normalizePrevSegment) {
                     ++pos;
                     if(!previousSegment(errorCode)) {
                         return U_SENTINEL;
