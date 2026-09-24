fd613cf048e786ae8d1c186e98078d86ffc0695d
Mihai Nita <nmihai_2000@yahoo.com>
2026-09-24 19:37:25 +0000

    ICU-23524 Sync hardcoded language lists between C++ and Java

--- icu4c/source/test/intltest/loctest.cpp
+++ icu4c/source/test/intltest/loctest.cpp
@@ -997,7 +997,7 @@ LocaleTest::TestGetLangsAndCountries()
       ;
 
     /* TODO: Change this test to be more like the cloctst version? */
-    if (testCount != 601)
+    if (testCount != 645)
         errln("Expected getISOLanguages() to return 601 languages; it returned %d", testCount);
     else {
         for (i = 0; i < 15; i++) {
